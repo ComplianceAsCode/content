9eb12ec27ccc21e177d28f6f4bb6d4f4fad4d428
Jeffrey Blank <blank@eclipse.ncsc.mil>
2013-05-15 23:43:58 -0400

    updates to the CCE verification script to be more informative

--- RHEL6/utils/verify-cce.py
+++ RHEL6/utils/verify-cce.py
@@ -19,29 +19,33 @@ granted_ids = []
 # print the list of assigned CCEs
 print "Assigned CCEs:"
 for item in cces_assigned:
-    print item.text
-    assigned_ids.append(item.text)
+	print item.text
+	assigned_ids.append(item.text)
 print "-------------"
+
+# check for duplicates in the assigned CCE list
+dup_assigned_ids = [item for item in cces_assigned if cces_assigned.count(item) > 1]
+for item in dup_assigned_ids:
+	print "Duplicate assignment of CCE: %s" % item
 
 # open the available CCE file
 with open('../references/cce-rhel6-avail.txt', 'r') as f:
-   for line in f:
-       granted_ids = [line.rstrip('\n') for line in f]
-#       print granted_ids
-
-# print CCEs that appear in content but are not valid
-   for item in assigned_ids:
-       if item not in granted_ids:
-           print "Invalid CCE: %s" % item
+	for line in f:
+		granted_ids = [line.rstrip('\n') for line in f]
 
 # print CCEs that are available (i.e. in granted but not assigned)
-   for item in granted_ids:
-       if item not in assigned_ids:
-           print "Available CCE: %s" % item
+	for item in granted_ids:
+		if item not in assigned_ids:
+			print "Available CCE: %s" % item
 
-# check for duplicates in the assigned CCE list                
-if len(set(assigned_ids)) != len(assigned_ids):
-	print "duplicate"
+for rule in tree.findall("//{%s}Rule" % xccdf_ns):
+#	print "rule is " + rule.get("id")
+	items = rule.findall("{%s}ident[@system='http://cce.mitre.org']" % xccdf_ns)
+	if len(items) > 1:
+		print "Rule with multiple CCEs assigned: %s" % rule.get("id")
+	for item in items:
+		if item.text not in granted_ids:
+			print "Invalid CCE: %s in %s" % (item.text, rule.get("id"))
 
 sys.exit()
 
