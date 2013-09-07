321f2a43fddc1a523711ac968c7a9217f68a0746
Shawn Wells <shawn@datadesigners.io>
2013-09-07 12:19:33 -0400

    OVAL + remediation: sshd_do_not_permit_user_env - Updated OVAL ID and filename to match XCCDF - filename -> filepath OVAL - Added remediation
    Remediation testing:
    [root@rhel6 checks]# vim /etc/ssh/sshd_config
    [root@rhel6 checks]# grep PermitUserEnvir /etc/ssh/sshd_config ; ./testcheck.py sshd_do_not_permit_user_env.xml
    PermitUserEnvironment yes
    Evaluating with OVAL tempfile : /tmp/sshd_do_not_permit_user_envSWoGgf.xml
    Writing results to : /tmp/sshd_do_not_permit_user_envSWoGgf.xml-results
    Definition oval:scap-security-guide.testing:def:165: false
    Definition oval:scap-security-guide.testing:def:127: false
    Definition oval:scap-security-guide.testing:def:125: false
    Evaluation done.
    [root@rhel6 checks]# bash ../fixes/bash/sshd_do_not_permit_user_env.sh
    [root@rhel6 checks]# grep PermitUserEnvir /etc/ssh/sshd_config ; ./testcheck.py sshd_do_not_permit_user_env.xml
    PermitUserEnvironment no
    Evaluating with OVAL tempfile : /tmp/sshd_do_not_permit_user_envo8hpvV.xml
    Writing results to : /tmp/sshd_do_not_permit_user_envo8hpvV.xml-results
    Definition oval:scap-security-guide.testing:def:165: true
    Definition oval:scap-security-guide.testing:def:127: false
    Definition oval:scap-security-guide.testing:def:125: false
    Evaluation done.

---
+++ RHEL6/input/fixes/bash/sshd_do_not_permit_user_env.sh
@@ -0,0 +1,5 @@
+grep -q ^PermitUserEnvironment /etc/ssh/sshd_config && \
+  sed -i "s/PermitUserEnvironment.*/PermitUserEnvironment no/g" /etc/ssh/sshd_config
+if ! [ $? -eq 0 ]; then
+    echo "PermitUserEnvironment no" >> /etc/ssh/sshd_config
+fi
