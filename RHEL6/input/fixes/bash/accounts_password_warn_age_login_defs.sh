59d383ab46c8b8538b357e38b52b0d279475e56b
Shawn Wells <shawn@redhat.com>
2013-09-18 15:09:34 -0400

    OVAL signoff + remediation: accounts_password_warn_age_login_defs  - OVAL signoff  - Added remediation
    TESTING:
    [root at SSG-RHEL6 checks]# var_password_warn_age=7 ; export var_password_warn_age
    [root at SSG-RHEL6 checks]# sed  -i 's/^PASS_WARN_AGE.*/PASS_WARN_AGE   4/' /etc/login.defs
    [root at SSG-RHEL6 checks]# ./testcheck.py accounts_password_warn_age_login_defs.xml
    external_variable with id : var_password_warn_age
    Evaluating with OVAL tempfile : /tmp/accounts_password_warn_age_login_defsIn0GFd.xml
    Writing results to : /tmp/accounts_password_warn_age_login_defsIn0GFd.xml-results
    Definition oval:scap-security-guide.testing:def:138: false
    Evaluation done.
    [root at SSG-RHEL6 checks]# sed  -i 's/^PASS_WARN_AGE.*/PASS_WARN_AGE   10/' /etc/login.defs
    [root at SSG-RHEL6 checks]# ./testcheck.py accounts_password_warn_age_login_defs.xml
    external_variable with id : var_password_warn_age
    Evaluating with OVAL tempfile : /tmp/accounts_password_warn_age_login_defsxC78D6.xml
    Writing results to : /tmp/accounts_password_warn_age_login_defsxC78D6.xml-results
    Definition oval:scap-security-guide.testing:def:138: true
    Evaluation done.

---
+++ RHEL6/input/fixes/bash/accounts_password_warn_age_login_defs.sh
@@ -0,0 +1,8 @@
+source ./templates/support.sh
+populate var_password_warn_age
+
+grep -q ^PASS_WARN_DAYS /etc/login.defs && \
+  sed -i "s/PASS_WARN_DAYS.*/PASS_WARN_DAYS     $var_password_warn_age/g" /etc/login.defs
+if ! [ $? -eq 0 ]; then
+    echo "PASS_WARN_DAYS      $var_password_warn_age"
+fi
