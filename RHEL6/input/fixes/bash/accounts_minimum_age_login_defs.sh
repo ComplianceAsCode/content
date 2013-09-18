f2801566693b91669943508e03e14cea8e8bd454
Shawn Wells <shawn@datadesigners.io>
2013-09-18 15:14:37 -0400

    accounts_* remediation update  Needed to append filename after echo statement

--- RHEL6/input/fixes/bash/accounts_minimum_age_login_defs.sh
+++ RHEL6/input/fixes/bash/accounts_minimum_age_login_defs.sh
@@ -4,5 +4,5 @@ populate var_password_min_age
 grep -q ^PASS_MIN_AGE /etc/login.defs && \
   sed -i "s/PASS_MIN_AGE.*/PASS_MIN_AGE     $var_password_min_age/g" /etc/login.defs
 if ! [ $? -eq 0 ]; then
-    echo "PASS_MIN_AGE      $var_password_min_age"
+    echo "PASS_MIN_AGE      $var_password_min_age" >> /etc/login.defs
 fi
--- RHEL6/input/fixes/bash/accounts_password_warn_age_login_defs.sh
+++ RHEL6/input/fixes/bash/accounts_password_warn_age_login_defs.sh
@@ -4,5 +4,5 @@ populate var_password_warn_age
 grep -q ^PASS_WARN_DAYS /etc/login.defs && \
   sed -i "s/PASS_WARN_DAYS.*/PASS_WARN_DAYS     $var_password_warn_age/g" /etc/login.defs
 if ! [ $? -eq 0 ]; then
-    echo "PASS_WARN_DAYS      $var_password_warn_age"
+    echo "PASS_WARN_DAYS      $var_password_warn_age" >> /etc/login.defs
 fi
