bce5a0649fc44c7c56e343297928a602d0ee0bb3
Shawn Wells <shawn@redhat.com>
2013-09-18 15:08:47 -0400

    Updated filepaths  Just noticed these two were using /etc/sshd... updated!

--- RHEL6/input/fixes/bash/accounts_password_minlen_login_defs.sh
+++ RHEL6/input/fixes/bash/accounts_password_minlen_login_defs.sh
@@ -2,7 +2,7 @@ source ./templates/support.sh
 populate var_password_min_len
 
 grep -q ^PASS_MIN_LEN /etc/login.defs && \
-  sed -i "s/PASS_MIN_LEN.*/PASS_MIN_LEN     $var_password_min_len/g" /etc/ssh/sshd_config
+  sed -i "s/PASS_MIN_LEN.*/PASS_MIN_LEN     $var_password_min_len/g" /etc/login.defs
 if ! [ $? -eq 0 ]; then
     echo "PASS_MIN_LEN      $var_password_min_len"
 fi
