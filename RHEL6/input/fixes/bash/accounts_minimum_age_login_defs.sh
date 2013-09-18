990fecf9d1b997c7b701eb8037180f3aa894fb82
Shawn Wells <shawn@datadesigners.io>
2013-09-18 15:54:55 -0400

    Updated XCCDF/OVAL namings

--- RHEL6/input/fixes/bash/accounts_minimum_age_login_defs.sh
+++ RHEL6/input/fixes/bash/accounts_minimum_age_login_defs.sh
@@ -1,8 +1,8 @@
 source ./templates/support.sh
-populate var_password_min_age
+populate var_accounts_minimum_age_login_defs
 
-grep -q ^PASS_MIN_AGE /etc/login.defs && \
-  sed -i "s/PASS_MIN_AGE.*/PASS_MIN_AGE     $var_password_min_age/g" /etc/login.defs
+grep -q ^PASS_MIN_DAYS /etc/login.defs && \
+  sed -i "s/PASS_MIN_DAYS.*/PASS_MIN_DAYS     $var_accounts_minimum_age_login_defs/g" /etc/login.defs
 if ! [ $? -eq 0 ]; then
-    echo "PASS_MIN_AGE      $var_password_min_age" >> /etc/login.defs
+    echo "PASS_MIN_DAYS      $var_accounts_minimum_age_login_defs" >> /etc/login.defs
 fi
