903f0b91c90051e99c5299970b3d939675386752
Shawn Wells <shawn@datadesigners.io>
2013-09-18 15:11:08 -0400

    Added remediation: set_sysctl_kernel_dmesg_restrict  From template

---
+++ RHEL6/input/fixes/bash/set_sysctl_kernel_dmesg_restrict.sh
@@ -0,0 +1,16 @@
+#
+# Set runtime for kernel.dmesg_restrict
+#
+sysctl -q -n -w kernel.dmesg_restrict=1
+
+#
+# If kernel.dmesg_restrict present in /etc/sysctl.conf, change value to "1"
+#	else, add "kernel.dmesg_restrict = 1" to /etc/sysctl.conf
+#
+if grep --silent ^kernel.dmesg_restrict /etc/sysctl.conf ; then
+	sed -i 's/^kernel.dmesg_restrict.*/kernel.dmesg_restrict = 1/g' /etc/sysctl.conf
+else
+	echo "" >> /etc/sysctl.conf
+	echo "# Set kernel.dmesg_restrict to 1 per security requirements" >> /etc/sysctl.conf
+	echo "kernel.dmesg_restrict = 1" >> /etc/sysctl.conf
+fi
