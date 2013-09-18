524f62304ad26edcc328546db9392a8f01fd1725
Shawn Wells <shawn@redhat.com>
2013-09-18 15:14:37 -0400

    accounts_* remediation update  Needed to append filename after echo statement

--- RHEL6/input/fixes/bash/accounts_password_minlen_login_defs.sh
+++ RHEL6/input/fixes/bash/accounts_password_minlen_login_defs.sh
@@ -4,5 +4,5 @@ populate var_password_min_len
 grep -q ^PASS_MIN_LEN /etc/login.defs && \
   sed -i "s/PASS_MIN_LEN.*/PASS_MIN_LEN     $var_password_min_len/g" /etc/login.defs
 if ! [ $? -eq 0 ]; then
-    echo "PASS_MIN_LEN      $var_password_min_len"
+    echo "PASS_MIN_LEN      $var_password_min_len" >> /etc/login.defs
 fi
