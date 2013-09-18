8e56c6960f71c1fed1cfa7e1fafed382ce2c1d87
Shawn Wells <shawn@redhat.com>
2013-09-18 15:54:55 -0400

    Updated XCCDF/OVAL namings

--- RHEL6/input/fixes/bash/accounts_maximum_age_login_defs.sh
+++ RHEL6/input/fixes/bash/accounts_maximum_age_login_defs.sh
@@ -1,8 +1,8 @@
 source ./templates/support.sh
-populate var_password_max_age
+populate var_accounts_maximum_age_login_defs
 
 grep -q ^PASS_MAX_DAYS /etc/login.defs && \
-  sed -i "s/PASS_MAX_DAYS.*/PASS_MAX_DAYS     $var_password_max_age/g" /etc/login.defs
+  sed -i "s/PASS_MAX_DAYS.*/PASS_MAX_DAYS     $var_accounts_maximum_age_login_defs/g" /etc/login.defs
 if ! [ $? -eq 0 ]; then
-    echo "PASS_MAX_DAYS      $var_password_max_age" >> /etc/login.defs
+    echo "PASS_MAX_DAYS      $var_accounts_maximum_age_login_defs" >> /etc/login.defs
 fi
--- RHEL6/input/fixes/bash/accounts_password_minlen_login_defs.sh
+++ RHEL6/input/fixes/bash/accounts_password_minlen_login_defs.sh
@@ -1,8 +1,8 @@
 source ./templates/support.sh
-populate var_password_min_len
+populate var_accounts_password_minlen_login_defs
 
 grep -q ^PASS_MIN_LEN /etc/login.defs && \
-  sed -i "s/PASS_MIN_LEN.*/PASS_MIN_LEN     $var_password_min_len/g" /etc/login.defs
+  sed -i "s/PASS_MIN_LEN.*/PASS_MIN_LEN     $var_accounts_password_minlen_login_defs/g" /etc/login.defs
 if ! [ $? -eq 0 ]; then
-    echo "PASS_MIN_LEN      $var_password_min_len" >> /etc/login.defs
+    echo "PASS_MIN_LEN      $var_accounts_password_minlen_login_defs" >> /etc/login.defs
 fi
