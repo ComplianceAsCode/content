221c3b8aca8cb50698e39c6154da1a4345ed7a7b
Jan Lieskovsky <jlieskov@redhat.com>
2013-12-16 17:19:01 +0100

    [RHEL6] Fix SSH remediation scripts to be case-insensitive

--- RHEL6/input/fixes/bash/sshd_set_keepalive.sh
+++ RHEL6/input/fixes/bash/sshd_set_keepalive.sh
@@ -1,5 +1,5 @@
-grep -q ^ClientAliveCountMax /etc/ssh/sshd_config && \
-  sed -i "s/ClientAliveCountMax.*/ClientAliveCountMax 0/g" /etc/ssh/sshd_config
+grep -qi ^ClientAliveCountMax /etc/ssh/sshd_config && \
+  sed -i "s/ClientAliveCountMax.*/ClientAliveCountMax 0/gI" /etc/ssh/sshd_config
 if ! [ $? -eq 0 ]; then
     echo "ClientAliveCountMax 0" >> /etc/ssh/sshd_config
 fi
