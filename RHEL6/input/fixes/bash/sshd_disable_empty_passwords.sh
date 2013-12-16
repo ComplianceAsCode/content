4ad52ae969b7dd0df55e9180d613f461a1fd75da
Jan Lieskovsky <jlieskov@redhat.com>
2013-12-16 17:19:01 +0100

    [RHEL6] Fix SSH remediation scripts to be case-insensitive

--- RHEL6/input/fixes/bash/sshd_disable_empty_passwords.sh
+++ RHEL6/input/fixes/bash/sshd_disable_empty_passwords.sh
@@ -1,5 +1,5 @@
-grep -q ^PermitEmptyPasswords /etc/ssh/sshd_config && \
-  sed -i "s/PermitEmptyPasswords.*/PermitEmptyPasswords no/g" /etc/ssh/sshd_config
+grep -qi ^PermitEmptyPasswords /etc/ssh/sshd_config && \
+  sed -i "s/PermitEmptyPasswords.*/PermitEmptyPasswords no/gI" /etc/ssh/sshd_config
 if ! [ $? -eq 0 ]; then
     echo "PermitEmptyPasswords no" >> /etc/ssh/sshd_config
 fi
--- RHEL6/input/fixes/bash/sshd_disable_root_login.sh
+++ RHEL6/input/fixes/bash/sshd_disable_root_login.sh
@@ -1,5 +1,5 @@
-grep -q ^PermitRootLogin /etc/ssh/sshd_config && \
-  sed -i "s/PermitRootLogin.*/PermitRootLogin no/g" /etc/ssh/sshd_config
+grep -qi ^PermitRootLogin /etc/ssh/sshd_config && \
+  sed -i "s/PermitRootLogin.*/PermitRootLogin no/gI" /etc/ssh/sshd_config
 if ! [ $? -eq 0 ]; then
     echo "PermitRootLogin "no >> /etc/ssh/sshd_config
 fi
