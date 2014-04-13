53661ae60479e97596077aaf51eced7027e9b186
Shawn Wells <shawn@datadesigners.io>
2014-04-13 01:01:00 -0400

    Moved RHEL6 selinux_state.sh to shared
    [shawn@SSG-RHEL6 bash]$ mv selinux_state.sh ../../../../../shared/fixes/bash/
    [shawn@SSG-RHEL6 bash]$ ln -s ../../../../../shared/fixes/bash/selinux_state.sh .
    [shawn@SSG-RHEL6 bash]$ git add ../../../../../shared/fixes/bash/selinux_state.sh ; git add selinux_state.sh ; git commit

--- RHEL/6/input/fixes/bash/selinux_state.sh
+++ RHEL/6/input/fixes/bash/selinux_state.sh
@@ -1,8 +1,1 @@
-source ./templates/support.sh
-populate var_selinux_state
-
-grep -q ^SELINUX= /etc/selinux/config && \
-  sed -i "s/SELINUX=.*/SELINUX=$var_selinux_state/g" /etc/selinux/config
-if ! [ $? -eq 0 ]; then
-    echo "SELINUX=$var_selinux_state" >> /etc/selinux/config
-fi
+../../../../../shared/fixes/bash/selinux_state.sh
