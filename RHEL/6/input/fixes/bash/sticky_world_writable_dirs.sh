742eeb98ac10d6d69ddbad9f93003aec908ed9ec
Shawn Wells <shawn@redhat.com>
2014-04-13 00:42:26 -0400

    New Remediation: sticky_world_writable_dirs.sh

---
+++ RHEL/6/input/fixes/bash/sticky_world_writable_dirs.sh
@@ -0,0 +1,1 @@
+../../../../../shared/fixes/bash/sticky_world_writable_dirs.sh
---
+++ shared/fixes/bash/sticky_world_writable_dirs.sh
@@ -0,0 +1,4 @@
+df --local -P | awk {'if (NR!=1) print $6'} \
+| xargs -I '{}' find '{}' -xdev -type d \
+( -perm -0002 -a ! -perm -1000 \) 2>/dev/null \
+| xargs chmod a+t
