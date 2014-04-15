6d8d3b510c9261c022f70efa633d45b1acb1f26a
Shawn Wells <shawn@redhat.com>
2014-04-14 23:28:58 -0400

    Updating commit per Jan's feedback
    - Updated rule titles
    - Fixed sed rule for character sensitivities
    - Fixed stickbit remediation

--- shared/fixes/bash/enable_selinux_bootloader.sh
+++ shared/fixes/bash/enable_selinux_bootloader.sh
@@ -1,2 +1,2 @@
-sed -i "s/[Ss][Ee][Ll][Ii][Nn][Uu][Xx]=0//g" /etc/grub.conf
-sed -i "s/[Ee][Nn][Ff][Oo][Rr][Cc][Ii][Nn][Gg]=0//g" /etc/grub.conf
+sed -i "s/selinux=0//gI" /etc/grub.conf
+sed -i "s/enforcing=0//gI" /etc/grub.conf
