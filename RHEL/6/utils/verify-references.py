dde525fcc73e4dc7efd3634179fc064fb0a97272
David Smith <dsmith@secure-innovations.net>
2014-08-01 08:12:46 -0700

    [bugfix]  Modified verify-references.py to reflect 800-53 rev4

--- RHEL/6/utils/verify-references.py
+++ RHEL/6/utils/verify-references.py
@@ -15,7 +15,7 @@ xccdf_ns = "http://checklists.nist.gov/xccdf/1.1"
 oval_ns = "http://oval.mitre.org/XMLSchema/oval-definitions-5"
 
 # we use these strings to look for references within the XCCDF rules
-nist_ref_href = "http://csrc.nist.gov/publications/nistpubs/800-53-Rev3/sp800-53-rev3-final.pdf"
+nist_ref_href = "http://nvlpubs.nist.gov/nistpubs/SpecialPublications/NIST.SP.800-53r4.pdf"
 disa_ref_href = "http://iase.disa.mil/cci/index.html"
 
 # default exit value - success
