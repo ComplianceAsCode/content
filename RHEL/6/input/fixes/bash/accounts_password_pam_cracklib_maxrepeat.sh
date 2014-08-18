89b1900fc585e23127472793f569b1d9a4d2ec5e
Shawn Wells <shawn@redhat.com>
2014-08-18 11:45:00 -0400

    New OVAL + Remediation for accounts_password_pam_cracklib_maxrepeat
    testing evidence:
    [root@ssg-rhel6-devbox checks]# var_password_pam_cracklib_maxrepeat=3; export var_password_pam_cracklib_maxrepeat
    [root@ssg-rhel6-devbox checks]# ./testcheck.py accounts_password_pam_cracklib_maxrepeat.xml
    external_variable with id : var_password_pam_cracklib_maxrepeat
    Evaluating with OVAL tempfile : /tmp/accounts_password_pam_cracklib_maxrepeatHsM_Zr.xml
    Writing results to : /tmp/accounts_password_pam_cracklib_maxrepeatHsM_Zr.xml-results
    Definition oval:scap-security-guide.testing:def:138: false
    Evaluation done.
    [root@ssg-rhel6-devbox checks]# grep maxrepeat /etc/pam.d/system-auth
    [root@ssg-rhel6-devbox checks]# cd ../fixes/bash/
    [root@ssg-rhel6-devbox bash]# bash accounts_password_pam_cracklib_maxrepeat.sh
    [root@ssg-rhel6-devbox bash]# cd -
    /var/www/html/gh/scap-security-guide/RHEL/6/input/checks
    [root@ssg-rhel6-devbox checks]# ./testcheck.py accounts_password_pam_cracklib_maxrepeat.xml
    external_variable with id : var_password_pam_cracklib_maxrepeat
    Evaluating with OVAL tempfile : /tmp/accounts_password_pam_cracklib_maxrepeatW0rRbK.xml
    Writing results to : /tmp/accounts_password_pam_cracklib_maxrepeatW0rRbK.xml-results
    Definition oval:scap-security-guide.testing:def:138: true
    Evaluation done.
    [root@ssg-rhel6-devbox checks]# grep maxrepeat /etc/pam.d/system-auth
    password    requisite     pam_cracklib.so try_first_pass retry=3 type= dcredit=-1 maxrepeat=3
    [root@ssg-rhel6-devbox checks]# sed -i --follow-symlinks 's/maxrepeat=3/maxrepeat=500/g' /etc/pam.d/system-auth
    [root@ssg-rhel6-devbox checks]# ./testcheck.py accounts_password_pam_cracklib_maxrepeat.xml
    external_variable with id : var_password_pam_cracklib_maxrepeat
    Evaluating with OVAL tempfile : /tmp/accounts_password_pam_cracklib_maxrepeatZlo4Ro.xml
    Writing results to : /tmp/accounts_password_pam_cracklib_maxrepeatZlo4Ro.xml-results
    Definition oval:scap-security-guide.testing:def:138: false
    Evaluation done.
    [root@ssg-rhel6-devbox checks]# cd -
    /var/www/html/gh/scap-security-guide/RHEL/6/input/fixes/bash
    [root@ssg-rhel6-devbox bash]# bash accounts_password_pam_cracklib_maxrepeat.sh
    [root@ssg-rhel6-devbox bash]# cd -
    /var/www/html/gh/scap-security-guide/RHEL/6/input/checks
    [root@ssg-rhel6-devbox checks]# ./testcheck.py accounts_password_pam_cracklib_maxrepeat.xml
    external_variable with id : var_password_pam_cracklib_maxrepeat
    Evaluating with OVAL tempfile : /tmp/accounts_password_pam_cracklib_maxrepeatRd9i_g.xml
    Writing results to : /tmp/accounts_password_pam_cracklib_maxrepeatRd9i_g.xml-results
    Definition oval:scap-security-guide.testing:def:138: true
    Evaluation done.

---
+++ RHEL/6/input/fixes/bash/accounts_password_pam_cracklib_maxrepeat.sh
@@ -0,0 +1,8 @@
+source ./templates/support.sh
+populate var_password_pam_cracklib_maxrepeat
+
+if grep -q "maxrepeat=" /etc/pam.d/system-auth; then   
+	sed -i --follow-symlink "s/\(maxrepeat *= *\).*/\1$var_password_pam_cracklib_maxrepeat/" /etc/pam.d/system-auth
+else
+	sed -i --follow-symlink "/pam_cracklib.so/ s/$/ maxrepeat=$var_password_pam_cracklib_maxrepeat/" /etc/pam.d/system-auth
+fi
