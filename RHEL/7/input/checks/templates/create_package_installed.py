632e0dc19b055c6cf71c0b109db3ac5e39dd0c15
Gabe <redhatrises@gmail.com>
2014-09-25 13:59:33 -0600

    [bugfix] Update create_package_installed.py
    - Updates create_package_installed.py to match existing python scripts

--- RHEL/7/input/checks/templates/create_package_installed.py
+++ RHEL/7/input/checks/templates/create_package_installed.py
@@ -11,35 +11,38 @@
 # PKGNAME - the name of the package that should be installed
 #
 
-import sys, csv
+import sys
+import csv
+
 
 def output_check(package_info):
     pkgname = package_info[0]
     if pkgname:
         with open("./template_OVAL_package_installed",
-                                   'r') as oval_template_file:
+                  'r') as oval_template_file:
             filestring = oval_template_file.read()
             filestring = filestring.replace("PKGNAME", pkgname)
             with open("./output/package_" + pkgname +
-                            "_installed.xml", 'w+') as oval_output_file:
+                      "_installed.xml", 'w+') as oval_output_file:
                 oval_output_file.write(filestring)
                 oval_output_file.close()
             with open("./template_BASH_package_installed",
-                                             'r') as bash_template_file:
+                      'r') as bash_template_file:
                 filestring = bash_template_file.read()
                 filestring = filestring.replace("PKGNAME", pkgname)
         with open("./output/package_" + pkgname +
-                             "_installed.sh", 'w+') as bash_output_file:
+                  "_installed.sh", 'w+') as bash_output_file:
             bash_output_file.write(filestring)
             bash_output_file.close()
     else:
         print "ERROR: input violation: the package name must be defined"
 
+
 def main():
     if len(sys.argv) < 2:
-        print "usage: %s <CSV_FILE_PATH>" % sys.argv[0]
-        print "   the csv file should contain lines of the format:"
-        print "   PACKAGE_NAME"
+        print("usage: %s <CSV_FILE_PATH>" % sys.argv[0])
+        print("   the csv file should contain lines of the format:")
+        print("   PACKAGE_NAME")
         sys.exit(1)
     with open(sys.argv[1], 'r') as csv_file:
         csv_lines = csv.reader(csv_file)
