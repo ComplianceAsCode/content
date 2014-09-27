f280b7994979ecfaf0da73d5942993f5626afc02
Shawn Wells <shawn@redhat.com>
2014-09-26 21:15:19 -0400

    updated naming accounts_password_pam_cracklib_ocredit.xml -> shared/oval/accounts_password_pam_ocredit.xml

--- RHEL/6/input/fixes/bash/accounts_password_pam_cracklib_ocredit.sh
+++
@@ -1,8 +0,0 @@
-source ./templates/support.sh
-populate var_password_pam_cracklib_ocredit
-
-if grep -q "ocredit=" /etc/pam.d/system-auth; then   
-	sed -i --follow-symlink "s/\(ocredit *= *\).*/\1$var_password_pam_cracklib_ocredit/" /etc/pam.d/system-auth
-else
-	sed -i --follow-symlink "/pam_cracklib.so/ s/$/ ocredit=$var_password_pam_cracklib_ocredit/" /etc/pam.d/system-auth
-fi
