141466d643c3eed24da2b17f9f10b96c133ade7f
Jan Lieskovsky <jlieskov@redhat.com>
2014-10-01 17:09:44 +0200

    [RHEL/6, RHEL/7] Fix Limit Password Reuse remediation script

--- shared/fixes/bash/accounts_password_pam_unix_remember.sh
+++ shared/fixes/bash/accounts_password_pam_unix_remember.sh
@@ -4,5 +4,5 @@ populate var_password_pam_unix_remember
 if grep -q "remember=" /etc/pam.d/system-auth; then   
 	sed -i --follow-symlink "s/\(remember *= *\).*/\1$var_password_pam_unix_remember/" /etc/pam.d/system-auth
 else
-	sed -i --follow-symlink "/^password[\s]sufficient[\s]pam_unix.so/ s/$/ remember=$var_password_pam_unix_remember/" /etc/pam.d/system-auth
+	sed -i --follow-symlink "/^password[[:space:]]\+sufficient[[:space:]]\+pam_unix.so/ s/$/ remember=$var_password_pam_unix_remember/" /etc/pam.d/system-auth
 fi
