a0cd398413fc259a1d5a8f299bca7bc7c9edb7a8
Jan Lieskovsky <jlieskov@redhat.com>
2014-11-13 17:09:49 +0100

    [Enhancement] [RHEL/6] Set GUI Warning Banner Text: * update OVAL check ID to start using 'gconf_'... prefix, * couple of OVAL check cosmetic changes (change title & description   they not to be copied from 'Enable GUI Warning Banner' check, update   test_attestation etc.), * change rule ID in XCCDF & profile references to use the OVAL one, * add remediation

---
+++ RHEL/6/input/fixes/bash/gconf_gdm_set_login_banner_text.sh
@@ -0,0 +1,17 @@
+source ./templates/support.sh
+populate login_banner_text
+
+# Install GConf2 package if not installed
+if ! rpm -q GConf2; then
+  yum -y install GConf2
+fi
+
+# Expand the login_banner_text value - there was a regular-expression
+# matching various banners, needs to be expanded
+banner_expanded=$(echo "$login_banner_text" | sed 's/\[\\s\\n\][*+]/ /g;s/\\//g;')
+
+# Set the text shown by the GNOME Display Manager in the login screen
+gconftool-2 --direct \
+            --config-source "xml:readwrite:/etc/gconf/gconf.xml.mandatory" \
+            --type string \
+            --set /apps/gdm/simple-greeter/banner_message_text "${banner_expanded}"
