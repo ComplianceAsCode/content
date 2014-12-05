f5012828c522925ec5278f95cc729f39b1ac5cc7
Jan Lieskovsky <jlieskov@redhat.com>
2014-12-05 18:12:08 +0100

    [RHEL/6] Add (RHEL-6) remediation for "Disable Support for RPC IPv6" rule

---
+++ RHEL/6/input/fixes/bash/network_ipv6_disable_rpc.sh
@@ -0,0 +1,9 @@
+
+# Drop 'tcp6' and 'udp6' entries from /etc/netconfig to prevent RPC
+# services for NFSv4 from attempting to start IPv6 network listeners
+declare -a IPV6_RPC_ENTRIES=("tcp6" "udp6")
+
+for rpc_entry in ${IPV6_RPC_ENTRIES[@]}
+do
+	sed -i "/^$rpc_entry[[:space:]]\+tpi\_.*inet6.*/d" /etc/netconfig
+done
