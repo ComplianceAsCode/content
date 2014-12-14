61c1cd63ba08b7aa729a7340fb497b892b1096b1
Shawn Wells <shawn@datadesigners.io>
2014-12-14 13:02:07 -0500

    updating per comments from redhatrises

--- shared/fixes/bash/no_rsh_trust_files.sh
+++ shared/fixes/bash/no_rsh_trust_files.sh
@@ -1,8 +1,2 @@
-for homedir in `awk -F: '{ print $6 }' /etc/passwd`
-do
-	if [ -e $homedir ]; then
-		rm -f $homedir/.rhosts
-	fi
-done
-
+find -type f -name .rhosts -exec rm -f '{}' \;
 rm /etc/hosts.equiv
