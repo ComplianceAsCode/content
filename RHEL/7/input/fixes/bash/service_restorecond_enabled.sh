6e117081f885327cfe2613bd2d1681a37bcc348a
Gabe <redhatrises@gmail.com>
2014-12-19 07:43:59 -0700

    [RHEL/7] Remove restorecond checks
    - Restorecond has been dropped in RHEL7 so remove OVAL/XDDF
    - Fixes #258

--- RHEL/7/input/fixes/bash/service_restorecond_enabled.sh
+++
@@ -1,9 +0,0 @@
-#
-# Enable restorecond.service for all systemd targets
-#
-systemctl enable restorecond.service
-
-#
-# Start restorecond.service if not currently running
-#
-systemctl start restorecond.service
