ba57bce5fb1c1addb12ef57e192fd799478940e7
Shawn Wells <shawn@datadesigners.io>
2014-12-28 00:37:49 -0500

    update ctl-alt-del remediation per comments from gabe

--- shared/fixes/bash/disable_ctrlaltdel_reboot.sh
+++ shared/fixes/bash/disable_ctrlaltdel_reboot.sh
@@ -1,9 +1,5 @@
-cat > /etc/init/control-alt-delete.conf << EOF
-#
-# This task is run whenever the Control-Alt-Delete key combination is
-# pressed.  Usually used to shut down the machine.
-
-start on control-alt-delete
-
-exec /usr/bin/logger -p security.info "Control-Alt-Delete pressed"
-EOF
+# Starting in RHEL6, the behavior to mask ctrl-alt-del
+# has changed. Ref:
+# RHEL6 Ref: https://access.redhat.com/solutions/449373
+# RHEL7 Ref: https://access.redhat.com/solutions/1123873
+ln -sf /dev/null /etc/systemd/system/ctrl-alt-del.target
