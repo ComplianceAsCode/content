d2b686713e09b33c7a92db5811cbd16cdcef66ee
Shawn Wells <shawn@redhat.com>
2014-12-28 15:43:52 -0500

    Updated RHEL7 remediation for ctrl+alt+del

--- shared/fixes/bash/disable_ctrlaltdel_reboot.sh
+++
@@ -1,5 +0,0 @@
-# Starting in RHEL6, the behavior to mask ctrl-alt-del
-# has changed. Ref:
-# RHEL6 Ref: https://access.redhat.com/solutions/449373
-# RHEL7 Ref: https://access.redhat.com/solutions/1123873
-ln -sf /dev/null /etc/systemd/system/ctrl-alt-del.target
