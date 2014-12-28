e10827e23dfae5a03563d8c9827009edc805a32d
Shawn Wells <shawn@datadesigners.io>
2014-12-28 15:43:52 -0500

    Updated RHEL7 remediation for ctrl+alt+del

--- RHEL/7/input/fixes/bash/disable_ctrlaltdel_reboot.sh
+++ RHEL/7/input/fixes/bash/disable_ctrlaltdel_reboot.sh
@@ -1,1 +1,3 @@
-../../../../../shared/fixes/bash/disable_ctrlaltdel_reboot.sh
+# The process to disable ctrl+alt+del has changed in RHEL7. 
+# Reference: https://access.redhat.com/solutions/1123873
+ln -sf /dev/null /etc/systemd/system/ctrl-alt-del.target
