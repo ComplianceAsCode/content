dcc2703592179286b03091a94fce9393b5ccb152
Yuli Khodorkovskiy <ykhodorkovskiy@tresys.com>
2015-02-10 14:02:06 -0500

    [RHEL/6] [RHEL/7] Improve the formatting for setting the login banner
    The login banner will now fit to an 80 character screen and have new
    lines between bullet points.

--- RHEL/6/input/fixes/bash/set_system_login_banner.sh
+++ RHEL/6/input/fixes/bash/set_system_login_banner.sh
@@ -2,8 +2,11 @@ source ./templates/support.sh
 populate login_banner_text
 
 # There was a regular-expression matching various banners, needs to be expanded
-expanded=$(echo "$login_banner_text" | sed 's/\[\\s\\n\][*+]/ /g;s/\\//g;')
+expanded=$(echo "$login_banner_text" | sed 's/\[\\s\\n\][+*]/ /g;s/\\//g;s/[^-]- /\n\n-/g')
+formatted=`echo "$expanded" | fold -sw 80`
 
 cat <<EOF >/etc/issue
-$expanded
+$formatted
 EOF
+
+printf "\n" >> /etc/issue
