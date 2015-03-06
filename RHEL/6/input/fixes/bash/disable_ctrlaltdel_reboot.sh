e0e5bc5a4a088026a06e8ab29ecc4389e4ce663c
Shawn Wells <shawn@datadesigners.io>
2015-03-05 22:16:36 -0500

    updating ctlaltdel script to reflect .override file

--- RHEL/6/input/fixes/bash/disable_ctrlaltdel_reboot.sh
+++ RHEL/6/input/fixes/bash/disable_ctrlaltdel_reboot.sh
@@ -1,4 +1,5 @@
+if [ -f /etc/init/control-alt-delete.conf ]; then
+	cp /etc/init/control-alt-delete.conf /etc/init/control-alt-delete.override
+fi
 
-sed -i 's,^exec.*,exec /usr/bin/logger -p authpriv.notice -t init "Ctrl-Alt-Del was pressed and ignored",'  FILE
-
-
+sed -i 's,^exec.*,exec /usr/bin/logger -p authpriv.notice -t init "Ctrl-Alt-Del was pressed and ignored",' /etc/init/control-alt-delete.override
