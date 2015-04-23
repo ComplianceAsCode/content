269a2b677bb7002c434dc1ed50619dac1fddefb3
Gabe <redhatrises@gmail.com>
2015-04-23 15:16:19 -0600

    [Enhancement][Bugfix] Update Java content
    - Fix various path issues
    - Update STIG reference file
    - Add OCIL tags and content
    - Update bash scripts, XCCDF, and OVAL to use /etc/.java instead of ${deployment.java.home}
      as /etc/.java has precendence over ${deployment.java.home}
    - Remove OS template files
    - Update CPE to use generic JAVA tags

---
+++ Java/input/fixes/bash/java_jre_deployment_config_configured.sh
@@ -0,0 +1,14 @@
+JAVA_CONFIG="/etc/.java/deployment/deployment.config"
+JAVA_PROPERTIES="/etc/.java/deployment/deployment.properties"
+
+grep -q "^deployment.system.config=file://${JAVA_CONFIG}$" ${JAVA_CONFIG} && \
+sed -i "s;deployment.system.config=.*;deployment.system.config=file:\/\/${JAVA_PROPERTIES};g" ${JAVA_CONFIG}
+if ! [ $? -eq 0 ] ; then
+  echo "deployment.system.config=file://${JAVA_PROPERTIES}" >> ${JAVA_CONFIG}
+fi
+
+grep -q "^deployment.system.config.mandatory=false$" ${JAVA_CONFIG} && \
+sed -i "s/deployment.system.config.mandatory=.*/deployment.system.config.mandatory=false/g" ${JAVA_CONFIG}
+if ! [ $? -eq 0 ] ; then
+  echo "deployment.system.config.mandatory=false" >> ${JAVA_CONFIG}
+fi
---
+++ Java/input/fixes/bash/java_jre_deployment_properties_exists.sh
@@ -0,0 +1,12 @@
+JAVA_PROPERTIES="/etc/.java/deployment/deployment.properties"
+JAVA_DIR="/etc/.java/deployment"
+
+if [ ! -d ${JAVA_DIR} ] ; then
+  mkdir -p -m 755 ${JAVA_DIR}
+fi
+
+if [ ! -e ${JAVA_PROPERTIES} ]; then
+  touch ${JAVA_PROPERTIES} && chmod 644 ${JAVA_PROPERTIES}
+fi
+
+chmod 644 ${JAVA_PROPERTIES}
