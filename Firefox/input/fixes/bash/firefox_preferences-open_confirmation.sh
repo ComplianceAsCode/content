20d11b77fc6fb67212333aac47a0acee729088c4
Gabe <redhatrises@gmail.com>
2015-04-30 10:16:17 -0600

    [Enhancement][Bugfix] Update Firefox content
    - Fix various path issues
    - Update STIG reference file
    - Add OCIL tags and content
    - Remove OS template files

--- Firefox/input/fixes/bash/firefox_preferences-open_confirmation.sh
+++ Firefox/input/fixes/bash/firefox_preferences-open_confirmation.sh
@@ -1,15 +1,13 @@
+source ./templates/support.sh
+populate var_required_file_types
+
 FIREFOX_DIRs="/usr/lib/firefox /usr/lib64/firefox /usr/local/lib/firefox /usr/local/lib64/firefox"
 for FIREFOX_DIR in ${FIREFOX_DIRs}; do
-	if [ -e ${FIREFOX_DIR} ]; then
-		if [ ! -e ${FIREFOX_DIR}/mozilla.cfg ] || [ "$(grep -c '^lockPref\("plugin.disable_full_page_plugin_for_types", "PDF, FDF, XFDF, LSL, LSO, LSS, IQY, RQY, XLK, XLS, XLT, POT PPS, PPT, DOS, DOT, WKS, BAT, PS, EPS, WCH, WCM, WB1, WB3, RTF, DOC, MDB, MDE, WBK, WB1, WCH, WCM, AD, ADP"\);' ${FIREFOX_DIR}/mozilla.cfg 2>/dev/null)" = "0" ]; then
-			if [ ! -e ${FIREFOX_DIR}/mozilla.cfg ]; then
-				echo -e '\nlockPref("plugin.disable_full_page_plugin_for_types", "PDF, FDF, XFDF, LSL, LSO, LSS, IQY, RQY, XLK, XLS, XLT, POT PPS, PPT, DOS, DOT, WKS, BAT, PS, EPS, WCH, WCM, WB1, WB3, RTF, DOC, MDB, MDE, WBK, WB1, WCH, WCM, AD, ADP");' >> ${FIREFOX_DIR}/mozilla.cfg
-			elif [ "$(grep -c '\"plugin.disable_full_page_plugin_for_types\"' ${FIREFOX_DIR}/mozilla.cfg 2>/dev/null)" != "0" ]; then
-				sed -i '/\"plugin.disable_full_page_plugin_for_types\"/d' ${FIREFOX_DIR}/mozilla.cfg
-				echo 'lockPref("plugin.disable_full_page_plugin_for_types", "PDF, FDF, XFDF, LSL, LSO, LSS, IQY, RQY, XLK, XLS, XLT, POT PPS, PPT, DOS, DOT, WKS, BAT, PS, EPS, WCH, WCM, WB1, WB3, RTF, DOC, MDB, MDE, WBK, WB1, WCH, WCM, AD, ADP");' >> ${FIREFOX_DIR}/mozilla.cfg
-			else
-				echo 'lockPref("plugin.disable_full_page_plugin_for_types", "PDF, FDF, XFDF, LSL, LSO, LSS, IQY, RQY, XLK, XLS, XLT, POT PPS, PPT, DOS, DOT, WKS, BAT, PS, EPS, WCH, WCM, WB1, WB3, RTF, DOC, MDB, MDE, WBK, WB1, WCH, WCM, AD, ADP");' >> ${FIREFOX_DIR}/mozilla.cfg
-			fi
-		fi
-	fi
+  if [ -d ${FIREFOX_DIR} ]; then
+    grep -q '^lockPref(\"plugin.disable_full_page_plugin_for_types\"' ${FIREFOX_DIR}/mozilla.cfg && \
+    sed -i "s;lockPref(\"plugin.disable_full_page_plugin_for_types\".*;lockPref(\"plugin.disable_full_page_plugin_for_types\", \"${var_required_file_types}\")\;;g" ${FIREFOX_DIR}/mozilla.cfg
+    if ! [ $? -eq 0 ] ; then
+      echo "lockPref(\"plugin.disable_full_page_plugin_for_types\", \"${var_required_file_types}\");" >> ${FIREFOX_DIR}/mozilla.cfg
+    fi
+  fi
 done
--- Firefox/input/fixes/bash/firefox_preferences-search_update.sh
+++ Firefox/input/fixes/bash/firefox_preferences-search_update.sh
@@ -1,15 +1,10 @@
 FIREFOX_DIRs="/usr/lib/firefox /usr/lib64/firefox /usr/local/lib/firefox /usr/local/lib64/firefox"
 for FIREFOX_DIR in ${FIREFOX_DIRs}; do
-	if [ -e ${FIREFOX_DIR} ]; then
-		if [ ! -e ${FIREFOX_DIR}/mozilla.cfg ] || [ "$(grep -c '^lockPref("browser.search.update", false);' ${FIREFOX_DIR}/mozilla.cfg 2>/dev/null)" = "0" ]; then
-			if [ ! -e ${FIREFOX_DIR}/mozilla.cfg ]; then
-				echo -e '\nlockPref("browser.search.update", false);' >> ${FIREFOX_DIR}/mozilla.cfg
-			elif [ "$(grep -c '\"browser.search.update\"' ${FIREFOX_DIR}/mozilla.cfg 2>/dev/null)" != "0" ]; then
-				sed -i '/\"browser.search.update\"/d' ${FIREFOX_DIR}/mozilla.cfg
-				echo 'lockPref("browser.search.update", false);' >> ${FIREFOX_DIR}/mozilla.cfg
-			else
-				echo 'lockPref("browser.search.update", false);' >> ${FIREFOX_DIR}/mozilla.cfg
-			fi
-		fi
-	fi
+  if [ -d ${FIREFOX_DIR} ]; then
+    grep -q '^lockPref(\"browser.search.update\", false);' ${FIREFOX_DIR}/mozilla.cfg && \
+    sed -i 's/lockPref(\"browser.search.update\".*/lockPref(\"browser.search.update\", false);/g' ${FIREFOX_DIR}/mozilla.cfg
+    if ! [ $? -eq 0 ] ; then
+      echo 'lockPref("browser.search.update", false);' >> ${FIREFOX_DIR}/mozilla.cfg
+    fi
+  fi
 done
