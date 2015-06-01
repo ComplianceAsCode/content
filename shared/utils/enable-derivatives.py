194a018ad81b5aa3d3b39e632ac9ad02871dad2f
Gabe <redhatrises@gmail.com>
2015-06-01 09:21:02 -0600

    [enhancement] add enable-derivatives.py script and enable for RHEL5/6/7
    - Add enable-derivatives.py script to handle multiple RHEL derivative OS(s)
    - Remove enable-centos.py and enable-sl.py

--- shared/utils/enable-derivatives.py
+++ shared/utils/enable-derivatives.py
@@ -1,21 +1,33 @@
 #!/usr/bin/python2
 
 """
-Takes given XCCDF or DataStream and adds Scientific Linux CPE name next to RHEL CPE names.
-Can automatically recognize RHEL5, 6, 7 CPEs and adds the Scientifc Linux ones next to
-those accordingly.
+Takes given XCCDF or DataStream and adds RHEL derivative operating system(s) CPE name next
+to RHEL CPE names. Can automatically recognize RHEL5, 6, 7 CPEs and adds the derivitive OS ones
+next to those accordingly.
 
 Apart from adding the CPEs it adds a notice informing the user that the content
-has been enabled for Scientifc Linux and what are the implications.
+has been enabled for the derivative operating systems and what are the implications.
 
 Author: Martin Preisler <mpreisle@redhat.com>
 """
 
-from xml.etree import cElementTree as ElementTree
+try:
+    from xml.etree import cElementTree as ElementTree
+except ImportError:
+    import cElementTree as ElementTree
+
 import sys
+from optparse import OptionParser
 
 XCCDF11_NS = "http://checklists.nist.gov/xccdf/1.1"
 XCCDF12_NS = "http://checklists.nist.gov/xccdf/1.2"
+
+RHEL_CENTOS_CPE_MAPPING = {
+    "cpe:/o:redhat:enterprise_linux:4": "cpe:/o:centos:centos:4",
+    "cpe:/o:redhat:enterprise_linux:5": "cpe:/o:centos:centos:5",
+    "cpe:/o:redhat:enterprise_linux:6": "cpe:/o:centos:centos:6",
+    "cpe:/o:redhat:enterprise_linux:7": "cpe:/o:centos:centos:7",
+}
 
 RHEL_SL_CPE_MAPPING = {
     "cpe:/o:redhat:enterprise_linux:4": "cpe:/o:scientificlinux:scientificlinux:4",
@@ -23,6 +35,41 @@ RHEL_SL_CPE_MAPPING = {
     "cpe:/o:redhat:enterprise_linux:6": "cpe:/o:scientificlinux:scientificlinux:6",
     "cpe:/o:redhat:enterprise_linux:7": "cpe:/o:scientificlinux:scientificlinux:7",
 }
+
+CENTOS_NOTICE = \
+    "<div xmlns=\"http://www.w3.org/1999/xhtml\">\n" \
+    "<p>This benchmark is a direct port of a <i>SCAP Security Guide </i> " \
+    "benchmark developed for <i>Red Hat Enterprise Linux</i>. It has been " \
+    "modified through an automated process to remove specific dependencies " \
+    "on <i>Red Hat Enterprise Linux</i> and to function with <i>CentOS</i>. " \
+    "The result is a generally useful <i>SCAP Security Guide</i> benchmark " \
+    "with the following caveats:</p>\n" \
+    "<ul>\n" \
+    "<li><i>CentOS</i> is not an exact copy of " \
+    "<i>Red Hat Enterprise Linux</i>. There may be configuration differences " \
+    "that produce false positives and/or false negatives. If this occurs " \
+    "please file a bug report.</li>\n" \
+    "\n" \
+    "<li><i>CentOS</i> has its own build system, compiler options, patchsets, " \
+    "and is a community supported, non-commercial operating system. " \
+    "<i>CentOS</i> does not inherit " \
+    "certifications or evaluations from <i>Red Hat Enterprise Linux</i>. As " \
+    "such, some configuration rules (such as those requiring " \
+    "<i>FIPS 140-2</i> encryption) will continue to fail on <i>CentOS</i>.</li>\n" \
+    "</ul>\n" \
+    "\n" \
+    "<p>Members of the <i>CentOS</i> community are invited to participate in " \
+    "<a href=\"http://open-scap.org\">OpenSCAP</a> and " \
+    "<a href=\"https://github.com/OpenSCAP/scap-security-guide\">" \
+    "SCAP Security Guide</a> development. Bug reports and patches " \
+    "can be sent to GitHub: " \
+    "<a href=\"https://github.com/OpenSCAP/scap-security-guide\">" \
+    "https://github.com/OpenSCAP/scap-security-guide</a>. " \
+    "The mailing list is at " \
+    "<a href=\"https://fedorahosted.org/mailman/listinfo/scap-security-guide\">" \
+    "https://fedorahosted.org/mailman/listinfo/scap-security-guide</a>" \
+    ".</p>" \
+    "</div>"
 
 SL_NOTICE = \
     "<div xmlns=\"http://www.w3.org/1999/xhtml\">\n" \
@@ -64,27 +111,31 @@ SL_NOTICE = \
     ".</p>" \
     "</div>"
 
+CENTOS_NOTICE_ELEMENT = ElementTree.fromstring(CENTOS_NOTICE)
 SL_NOTICE_ELEMENT = ElementTree.fromstring(SL_NOTICE)
 
+CENTOS_WARNING = 'centos_warning'
+SL_WARNING = 'sl_warning'
 
-def add_sl_cpes(elem, namespace):
-    """Adds Scientifc Linux CPEs next to RHEL ones, checks XCCDF elements of given
+
+def add_derivative_cpes(elem, namespace, mapping):
+    """Adds derivative CPEs next to RHEL ones, checks XCCDF elements of given
     namespace.
     """
 
     affected = False
 
     for child in list(elem):
-        affected = affected or add_sl_cpes(child, namespace)
+        affected = affected or add_derivative_cpes(child, namespace, mapping)
 
     # precompute this so that we can affect the tree while iterating
     children = list(elem.findall(".//{%s}platform" % (namespace)))
 
     for child in children:
         idref = child.get("idref")
-        if idref in RHEL_SL_CPE_MAPPING:
+        if idref in mapping:
             new_platform = ElementTree.Element("{%s}platform" % (namespace))
-            new_platform.set("idref", RHEL_SL_CPE_MAPPING[idref])
+            new_platform.set("idref", mapping[idref])
             # this is done for the newline and indentation
             new_platform.tail = child.tail
 
@@ -97,8 +148,8 @@ def add_sl_cpes(elem, namespace):
     return affected
 
 
-def add_sl_notice(benchmark, namespace):
-    """Adds Scientifc Linux notice as the first notice to given benchmark.
+def add_derivative_notice(benchmark, namespace, notice, warning):
+    """Adds derivative notice as the first notice to given benchmark.
     """
 
     index = -1
@@ -123,8 +174,8 @@ def add_sl_notice(benchmark, namespace):
         )
 
     elem = ElementTree.Element("{%s}notice" % (namespace))
-    elem.set("id", "sl_warning")
-    elem.append(SL_NOTICE_ELEMENT)
+    elem.set("id", warning)
+    elem.append(notice)
     # this is done for the newline and indentation
     elem.tail = prev_element.tail
     benchmark.insert(index, elem)
@@ -133,19 +184,39 @@ def add_sl_notice(benchmark, namespace):
 
 
 def main():
-    # the first arg is the script name
-    if len(sys.argv) != 1 + 2:
-        print("Usage: enable-sl.py INPUT OUTPUT")
-        print("")
-        print("INPUT can be XCCDF or Source DataStream.")
-
+    usage = "usage: %prog [options]"
+    parser = OptionParser(usage=usage)
+    parser.add_option("--enable-centos", dest="centos", default=False,
+                      action="store_true", help="Enable CentOS")
+    parser.add_option("--enable-sl", dest="sl", default=False,
+                      action="store_true", help="Enable Scientific Linux")
+    parser.add_option("-i", "--input", dest="input_content", default=False,
+                      action="store", help="INPUT can be XCCDF or Source DataStream")
+    parser.add_option("-o", "--output", dest="output", default=False,
+                      action="store", help="XML Tree content")
+    (options, args) = parser.parse_args()
+     
+    if options.centos and options.sl:
+        print "Cannot enable two derivative OS(s) at the same time"
+        parser.print_help()
         sys.exit(1)
 
-    input_ = sys.argv[1]
-    output = sys.argv[2]
+    if not options.output and not options.input_content:
+        parser.print_help()
+        sys.exit(1)
+
+    if options.centos:
+        mapping = RHEL_CENTOS_CPE_MAPPING
+        notice = CENTOS_NOTICE_ELEMENT
+        warning = CENTOS_WARNING
+
+    if options.sl:
+        mapping = RHEL_SL_CPE_MAPPING
+        notice = SL_NOTICE_ELEMENT
+        warning = SL_WARNING
 
     tree = ElementTree.ElementTree()
-    tree.parse(input_)
+    tree.parse(options.input_content)
 
     root = tree.getroot()
 
@@ -160,18 +231,18 @@ def main():
     ])
 
     for namespace, benchmark in benchmarks:
-        if not add_sl_cpes(benchmark, namespace):
+        if not add_derivative_cpes(benchmark, namespace, mapping):
             raise RuntimeError(
-                "Could not add Scientifc Linux CPEs to Benchmark '%s'." % (benchmark)
+                "Could not add derivative OS CPEs to Benchmark '%s'." % (benchmark)
             )
 
-        if not add_sl_notice(benchmark, namespace):
+        if not add_derivative_notice(benchmark, namespace, notice, warning):
             raise RuntimeError(
-                "Managed to add Scientifc Linux CPEs but failed to add the notice to "
+                "Managed to add derivative OS CPEs but failed to add the notice to "
                 "affected XCCDF Benchmark '%s'." % (benchmark)
             )
 
-    tree.write(output)
+    tree.write(options.output)
 
 if __name__ == "__main__":
     main()
