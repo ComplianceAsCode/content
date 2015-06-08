ComplianceAsCode/content

commit b37a13dc4efe1f9b36575c511750db66294190e9

[BugFix] [shared] Deduplicate OVAL entity identifiers in the testcheck.py script too
Don't include the underlying OVAL entity ID when creating final OVAL XML
check file via testcheck.py script in the case particular OVAL entity
(definition, object, state, or variable) with the same ID has been already
included in the check. This prevents creation of an invalid OVAL content file.

Fixes issue reported by Gabe Alford in: https://github.com/OpenSCAP/scap-security-guide/pull/536#issuecomment-99113460

shared/modules/testcheck_module.py
@@ -38,6 +38,18 @@ states = ET.Element("states")
 variables = ET.Element("variables")
 
 
+# append new child ONLY if it's not a duplicate
+def append(element, newchild):
+    newid = newchild.get("id")
+    existing = element.find(".//*[@id='" + newid + "']")
+    if existing is not None:
+        sys.stderr.write("Notification: this ID is used more than once " +
+                         "and should represent equivalent elements: " +
+                         newid + "\n")
+    else:
+        element.append(newchild)
+
+
 def add_oval_elements(body):
     """add oval elements to the global Elements defined above"""
 
@@ -50,7 +62,7 @@ def add_oval_elements(body):
         if childnode.tag is ET.Comment:
             continue
         if childnode.tag == (ovalns + "definition"):
-            definitions.append(childnode)
+            append(definitions, childnode)
             defname = childnode.get("id")
             # extend_definition is a special case:  must include a whole other
             # definition
@@ -61,13 +73,13 @@ def add_oval_elements(body):
                 # recursively add the elements in the other file
                 add_oval_elements(includedbody)
         if childnode.tag.endswith("_test"):
-            tests.append(childnode)
+            append(tests, childnode)
         if childnode.tag.endswith("_object"):
-            objects.append(childnode)
+            append(objects, childnode)
         if childnode.tag.endswith("_state"):
-            states.append(childnode)
+            append(states, childnode)
         if childnode.tag.endswith("_variable"):
-            variables.append(childnode)
+            append(variables, childnode)
     return defname
 
 
@@ -90,7 +102,7 @@ def replace_external_vars(tree):
         node.tag = ovalns + "local_variable"
         literal = ET.Element("literal_component")
         literal.text = os.environ[extvar_id]
-        node.append(literal)
+        append(node, literal)
         # TODO: assignment of external_variable via environment vars, for
         # testing
     return tree

commit 41b295738a8460a64fea516baddbad757054dac9

[BugFix] [Infrastructure] Fix testcheck.py bug when dealing with external variables
Fixes the following issue (introduced by https://github.com/OpenSCAP/scap-security-guide/commit/b37a13dc4efe1f9b36575c511750db66294190e9):

[iankko@host scap-security-guide]$ cd shared/oval/
[iankko@host oval]$ ./testcheck.py account_disable_post_pw_expiration.xml
External_variable with id : var_account_disable_post_pw_expiration
External_variable specified, but no value provided via environment variable
[iankko@host oval]$ export var_account_disable_post_pw_expiration=10
[iankko@host oval]$ ./testcheck.py account_disable_post_pw_expiration.xml
External_variable with id : var_account_disable_post_pw_expiration
Traceback (most recent call last):
  File "./testcheck.py", line 11, in <module>
    testcheck_module.main()
  File "../modules/testcheck_module.py", line 154, in main
    defname = add_oval_elements(body)
  File "../modules/testcheck_module.py", line 58, in add_oval_elements
    tree = replace_external_vars(tree)
  File "../modules/testcheck_module.py", line 107, in replace_external_vars
    append(node, literal)
  File "../modules/testcheck_module.py", line 44, in append
    existing = element.find(".//*[@id='" + newid + "']")
TypeError: cannot concatenate 'str' and 'NoneType' objects
[iankko@host oval]$

After this change is applied, the output for same check is as follows:

[iankko@host scap-security-guide]$ cd shared/oval/
[iankko@host oval]$ ./testcheck.py account_disable_post_pw_expiration.xml
External_variable with id : var_account_disable_post_pw_expiration
Evaluating with OVAL tempfile : /tmp/account_disable_post_pw_expirationwoxAT0.xml
Writing results to : /tmp/account_disable_post_pw_expirationwoxAT0.xml-results
Definition oval:scap-security-guide.testing:def:100: false
Evaluation done.

shared/modules/testcheck_module.py
@@ -104,7 +104,7 @@ def replace_external_vars(tree):
         node.tag = ovalns + "local_variable"
         literal = ET.Element("literal_component")
         literal.text = os.environ[extvar_id]
-        append(node, literal)
+        node.append(literal)
         # TODO: assignment of external_variable via environment vars, for
         # testing
     return tree