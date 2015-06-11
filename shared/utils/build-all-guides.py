c832388c24da721831fbecfc130510304e2913f9
Martin Preisler <mpreisle@redhat.com>
2015-06-11 16:40:04 +0200

    Implemented the build-all-guides.py script
    It takes an XCCDF 1.1, 1.2 or source datastream, scrapes all profiles
    and builds a guide for every profile not in the blacklist. All the
    guides are placed next to the input file.

---
+++ shared/utils/build-all-guides.py
@@ -0,0 +1,185 @@
+#!/usr/bin/python2
+
+"""
+Takes given XCCDF or DataStream and for every profile in it it generates one
+OpenSCAP HTML guide. Also generates an index file that lists all the profiles
+and allows the user to navigate between them.
+
+Author: Martin Preisler <mpreisle@redhat.com>
+"""
+
+try:
+    from xml.etree import cElementTree as ElementTree
+except ImportError:
+    import cElementTree as ElementTree
+
+import os.path
+from optparse import OptionParser
+import subprocess
+
+OSCAP_PATH = "oscap"
+
+XCCDF11_NS = "http://checklists.nist.gov/xccdf/1.1"
+XCCDF12_NS = "http://checklists.nist.gov/xccdf/1.2"
+
+# if a profile ID ends with a string listed here we skip it
+PROFILE_ID_BLACKLIST = ["test"]
+# filler XCCDF 1.2 prefix which we will strip to avoid very long filenames
+PROFILE_ID_PREFIX = "xccdf_org.ssgproject.content_profile_"
+
+
+def get_profile_choices_for_input(input_file, tailoring_file):
+    """Returns a dictionary that maps profile_ids to their respective titles.
+    """
+
+    # Ideally oscap would have a command line to do this, but as of now it
+    # doesn't so we have to implement it ourselves. Importing openscap Python
+    # bindings is nasty and overkill for this.
+
+    ret = {}
+
+    def scrape_profiles(root_element, namespace, dest):
+        for elem in root_element.findall(".//{%s}Profile" % (namespace)):
+            id_ = elem.get("id")
+            if id_ is None:
+                continue
+
+            title = "<unknown>"
+            for element in elem.findall("{%s}title" % (namespace)):
+                title = element.text
+                break
+
+            dest[id_] = title
+
+    input_tree = ElementTree.parse(input_file)
+    input_root = input_tree.getroot()
+
+    scrape_profiles(
+        input_root, XCCDF11_NS, ret
+    )
+    scrape_profiles(
+        input_root, XCCDF12_NS, ret
+    )
+
+    if tailoring_file is not None:
+        tailoring_tree = ElementTree.parse(tailoring_file)
+        tailoring_root = tailoring_tree.getroot()
+
+        scrape_profiles(
+            tailoring_root, XCCDF11_NS, ret
+        )
+        scrape_profiles(
+            tailoring_root, XCCDF12_NS, ret
+        )
+
+    return ret
+
+
+def get_profile_short_id(long_id):
+    """If given profile ID is the XCCDF 1.2 long ID this function shortens it
+    """
+
+    if long_id.startswith(PROFILE_ID_PREFIX):
+        return long_id[len(PROFILE_ID_PREFIX):]
+
+    return long_id
+
+
+def subprocess_check_output(*popenargs, **kwargs):
+    # Backport of subprocess.check_output taken from
+    # https://gist.github.com/edufelipe/1027906
+    #
+    # Originally from Python 2.7 stdlib under PSF, compatible with LGPL2+
+    # Copyright (c) 2003-2005 by Peter Astrand <astrand@lysator.liu.se>
+    # Changes by Eduardo Felipe
+
+    process = subprocess.Popen(stdout=subprocess.PIPE, *popenargs, **kwargs)
+    output, unused_err = process.communicate()
+    retcode = process.poll()
+    if retcode:
+        cmd = kwargs.get("args")
+        if cmd is None:
+            cmd = popenargs[0]
+        error = subprocess.CalledProcessError(retcode, cmd)
+        error.output = output
+        raise error
+    return output
+
+
+if hasattr(subprocess, "check_output"):
+    # if available we just use the real function
+    subprocess_check_output = subprocess.check_output
+
+
+def generate_guide_for_input_content(input_content, profile_id):
+    """Returns HTML guide for given input_content and profile_id
+    combination. This function assumes only one Benchmark exists
+    in given input_content!
+    """
+
+    args = [OSCAP_PATH, "xccdf", "generate", "guide"]
+    args.extend(["--profile", profile_id])
+    args.append(input_content)
+
+    ret = subprocess_check_output(args).decode("utf-8")
+
+    return ret
+
+
+def main():
+    usage = "usage: %prog [options]"
+    parser = OptionParser(usage=usage)
+    parser.add_option(
+        "-i", "--input", dest="input_content", default=False,
+        action="store", help="INPUT can be XCCDF or Source DataStream"
+    )
+    (options, args) = parser.parse_args()
+
+    parent_dir = os.path.dirname(os.path.abspath(options.input_content))
+    path_base, _ = os.path.splitext(os.path.basename(options.input_content))
+    # avoid -ds and -xccdf suffices in guide filenames
+    if path_base.endswith("-ds"):
+        path_base = path_base[:-3]
+    elif path_base.endswith("-xccdf"):
+        path_base = path_base[:-6]
+
+    profiles = get_profile_choices_for_input(options.input_content, None)
+
+    # TODO: Make the index file nicer, allow switching between profiles, etc...
+    index_source = "<html><body>"
+
+    for profile_id, profile_title in profiles.iteritems():
+        skip = False
+        for blacklisted_id in PROFILE_ID_BLACKLIST:
+            if profile_id.endswith(blacklisted_id):
+                skip = True
+
+        if skip:
+            break
+
+        guide_html = generate_guide_for_input_content(
+            options.input_content, profile_id
+        )
+
+        guide_filename = \
+            "%s-guide-%s.html" % (path_base, get_profile_short_id(profile_id))
+        guide_path = os.path.join(parent_dir, guide_filename)
+        with open(guide_path, "w") as f:
+            f.write(guide_html.encode("utf-8"))
+
+        index_source += "<a href=\"%s\">%s</a><br>" % \
+            (guide_filename, profile_title)
+
+        print(
+            "Generated '%s' for profile ID '%s'." %
+            (guide_filename, profile_id)
+        )
+
+    index_source += "</body></html>"
+
+    index_path = os.path.join(parent_dir, "%s-guide-index.html" % (path_base))
+    with open(index_path, "w") as f:
+        f.write(index_source.encode("utf-8"))
+
+if __name__ == "__main__":
+    main()
