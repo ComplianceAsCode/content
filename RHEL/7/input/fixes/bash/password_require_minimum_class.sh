9c96008d34dc5a8f546d97d021b26ae9124bee2d
Jan Lieskovsky <jlieskov@redhat.com>
2015-06-12 11:45:30 +0200

    [BugFix] [RHEL/7] [Fedora] Drop mention about pam_passwdqc PAM module from RHEL-7's XCCDF prose (pam_passwdqc isn't available for RHEL-7)
    Also replace occurrences of 'pam_cracklib' module with 'pam_pwquality'
    module in RHEL-7 and Fedora content (in XCCDF and in selected /shared
    OVAL checks)
    
    This partly [*] fixes downstream bug:
      [1] https://bugzilla.redhat.com/show_bug.cgi?id=1178967
    
    --
    
    [*] Partly because there have been more patches in the past already
        replacing 'pam_cracklib' occurrences with 'pam_pwquality' for RHEL-7
        and Fedora. This patch is just fixing what's got omitted before.

--- RHEL/7/input/fixes/bash/password_require_minimum_class.sh
+++ RHEL/7/input/fixes/bash/password_require_minimum_class.sh
@@ -1,6 +1,6 @@
 grep -q minclass /etc/pam.d/system-auth
 if [ $? = "0" ]; then
-    sed --follow-symlinks -i "/pam_cracklib.so/s/minclass=[0-4]/minclass=3/" /etc/pam.d/system-auth
+    sed --follow-symlinks -i "/pam_pwquality.so/s/minclass=[0-4]/minclass=3/" /etc/pam.d/system-auth
 else
-    sed --follow-symlinks -i "/pam_cracklib.so/s/pam_cracklib.so /pam_cracklib.so minclass=3 /" /etc/pam.d/system-auth
+    sed --follow-symlinks -i "/pam_pwquality.so/s/pam_pwquality.so /pam_pwquality.so minclass=3 /" /etc/pam.d/system-auth
 fi
