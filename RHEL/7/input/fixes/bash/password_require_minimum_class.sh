a5316f0b9f58332fa09996d8c71de689f5210bad
Jan Lieskovsky <jlieskov@redhat.com>
2015-06-13 16:28:29 +0200

    [BugFix] [RHEL/7]
    * Rename remediation script for 'accounts_password_pam_minclass' XCCDF rule
      from invalid name 'password_require_minimum_class' to proper name
      'accounts_password_pam_minclass' (so it would be actually called when
      remediation is attempted / requested),
    
    * Fix remediation script for 'accounts_password_pam_minclass' rule
      to act on /etc/security/pwquality.conf configuration file, rather
      than on /etc/pam.d/system-auth file. This fixes issue noticed by Gabe
      pointed out at:
        [1] https://github.com/OpenSCAP/scap-security-guide/pull/579#discussion_r32313442
    
    Thank you for the catch, Gabe!

--- RHEL/7/input/fixes/bash/password_require_minimum_class.sh
+++
@@ -1,6 +0,0 @@
-grep -q minclass /etc/pam.d/system-auth
-if [ $? = "0" ]; then
-    sed --follow-symlinks -i "/pam_pwquality.so/s/minclass=[0-4]/minclass=3/" /etc/pam.d/system-auth
-else
-    sed --follow-symlinks -i "/pam_pwquality.so/s/pam_pwquality.so /pam_pwquality.so minclass=3 /" /etc/pam.d/system-auth
-fi
