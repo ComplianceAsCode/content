4a7979ba15418587cc1857f7d2246aa5edb0329e
Jan Lieskovsky <jlieskov@redhat.com>
2015-07-01 18:16:15 +0200

    [Enhancement] [RHEL/7] Add new remediation for the 'audit_rules_networkconfig_modification' rule
    [BugFix] [RHEL/6] While on that fix one ShellCheck
    warning in the RHEL-6 version of that remediation,
    and enhance the allowed key value in the pattern
    
    Testing report:
    ---------------
    * RHEL-7 verified manually it works correctly for both
      'auditctl' && 'augenrules' configuration cases,
    * RHEL-6 retested there it still works properly

--- RHEL/7/input/fixes/bash/audit_rules_networkconfig_modification.sh
+++ RHEL/7/input/fixes/bash/audit_rules_networkconfig_modification.sh
@@ -12,11 +12,18 @@ do
 	# Use escaped BRE regex to specify rule group
 	GROUP="set\(host\|domain\)name"
 	FULL_RULE="-a always,exit -F arch=$ARCH -S sethostname -S setdomainname -k audit_rules_networkconfig_modification"
+	# Perform the remediation for both possible tools: 'auditctl' and 'augenrules'
 	fix_audit_syscall_rule "auditctl" "$PATTERN" "$GROUP" "$ARCH" "$FULL_RULE"
+	fix_audit_syscall_rule "augenrules" "$PATTERN" "$GROUP" "$ARCH" "$FULL_RULE"
 done
 
 # Then perform the remediations for the watch rules
+# Perform the remediation for both possible tools: 'auditctl' and 'augenrules'
 fix_audit_watch_rule "auditctl" "/etc/issue" "wa" "audit_rules_networkconfig_modification"
+fix_audit_watch_rule "augenrules" "/etc/issue" "wa" "audit_rules_networkconfig_modification"
 fix_audit_watch_rule "auditctl" "/etc/issue.net" "wa" "audit_rules_networkconfig_modification"
+fix_audit_watch_rule "augenrules" "/etc/issue.net" "wa" "audit_rules_networkconfig_modification"
 fix_audit_watch_rule "auditctl" "/etc/hosts" "wa" "audit_rules_networkconfig_modification"
+fix_audit_watch_rule "augenrules" "/etc/hosts" "wa" "audit_rules_networkconfig_modification"
 fix_audit_watch_rule "auditctl" "/etc/sysconfig/network" "wa" "audit_rules_networkconfig_modification"
+fix_audit_watch_rule "augenrules" "/etc/sysconfig/network" "wa" "audit_rules_networkconfig_modification"
