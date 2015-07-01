9757d58f15af871745d7da62c99379a9feeee59a
Jan Lieskovsky <jlieskov@redhat.com>
2015-07-01 16:32:30 +0200

    [BugFix] [RHEL/6] Start using the 'fix_audit_syscall_rule' remediation function for the following RHEL-6 audit DAC 'chown' group remediations: * audit_rules_dac_modification_chown, * audit_rules_dac_modification_fchown, * audit_rules_dac_modification_fchownat, * audit_rules_dac_modification_lchown.
    This change replaces the current 'audit_rules_dac_modification_chown'
    RHEL-6 remediation script own implementation by calling the 'fix_audit_syscall_rule'
    universal remediation function with proper arguments.
    
    Former symbolic links for the other three remediations have been replaced
    with own copies of the very same file (symbolic links shouldn't be used).
    
    Testing report:
    ---------------
    Tested the proposed change manually on RHEL-6 system, and it seems
    to be working fine.

--- RHEL/6/input/fixes/bash/audit_rules_dac_modification_fchownat.sh
+++ RHEL/6/input/fixes/bash/audit_rules_dac_modification_fchownat.sh
@@ -1,1 +1,15 @@
-audit_rules_dac_modification_chown.sh
+
+# Include source function library.
+. /usr/share/scap-security-guide/remediation_functions
+
+# Perform the remediation for the syscall rule
+# Retrieve hardware architecture of the underlying system
+[ $(getconf LONG_BIT) = "32" ] && RULE_ARCHS=("b32") || RULE_ARCHS=("b32" "b64")
+
+for ARCH in ${RULE_ARCHS[@]}
+do
+	PATTERN="-a always,exit -F arch=$ARCH -S .* -F auid>=500 -F auid!=4294967295 -k *"
+	GROUP="chown"
+	FULL_RULE="-a always,exit -F arch=$ARCH -S chown -S fchown -S fchownat -S lchown -F auid>=500 -F auid!=4294967295 -k perm_mod"
+	fix_audit_syscall_rule "auditctl" "$PATTERN" "$GROUP" "$ARCH" "$FULL_RULE"
+done
