e426d19f8838f426f4ff86d71379026ef46cdcf7
Jan Lieskovsky <jlieskov@redhat.com>
2015-07-01 13:51:10 +0200

    [Enhancement] [RHEL/6] [RHEL/7] New RHEL-6 && RHEL-7 remediation for 'audit_rules_session_events' rule
    Testing report:
    ---------------
    Tested manually on both systems -- RHEL-6 and RHEL-7 (with both
    'auditctl' and 'augenrules' settings in this case) and verified
    the proposed changes works fine on both of them.

---
+++ RHEL/6/input/fixes/bash/audit_rules_session_events.sh
@@ -0,0 +1,8 @@
+
+# Include source function library.
+. /usr/share/scap-security-guide/remediation_functions
+
+# Perform the remediation
+fix_audit_watch_rule "auditctl" "/var/run/utmp" "wa" "session"
+fix_audit_watch_rule "auditctl" "/var/log/btmp" "wa" "session"
+fix_audit_watch_rule "auditctl" "/var/log/wtmp" "wa" "session"
