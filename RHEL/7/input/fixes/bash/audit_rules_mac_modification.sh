595b205b9b4c2ddfbaa6ec85470184dcf24a2db7
Jan Lieskovsky <jlieskov@redhat.com>
2015-07-01 17:36:46 +0200

    [Enhancement] [RHEL/7] New RHEL-7 remediation for the 'audit_rules_mac_modification' rule
    Testing report:
    ---------------
    Verified manually on RHEL-7 host (testing both 'auditctl' and 'augenrules'
    settings to load audit config files) that the proposed change is working
    fine.

---
+++ RHEL/7/input/fixes/bash/audit_rules_mac_modification.sh
@@ -0,0 +1,7 @@
+
+# Include source function library.
+. /usr/share/scap-security-guide/remediation_functions
+
+# Perform the remediation for both possible tools: 'auditctl' and 'augenrules'
+fix_audit_watch_rule "auditctl" "/etc/selinux/" "wa" "MAC-policy"
+fix_audit_watch_rule "augenrules" "/etc/selinux/" "wa" "MAC-policy"
