9a8c2a2816ee9b0eb7ef2cafec50e811b6b98a03
Jan Lieskovsky <jlieskov@redhat.com>
2015-07-01 11:08:02 +0200

    [Enhancement] [RHEL/7] New remediation for 'audit_rules_sysadmin_actions' rule
    Also update remediation fixes for 'audit_rules_file_deletion_events' and
    'audit_rules_kernel_module_loading' they to start using same layout

---
+++ RHEL/7/input/fixes/bash/audit_rules_sysadmin_actions.sh
@@ -0,0 +1,7 @@
+
+# Include source function library.
+. /usr/share/scap-security-guide/remediation_functions
+
+# Perform the remediation for both possible tools: 'auditctl' and 'augenrules'
+fix_audit_watch_rule "auditctl" "/etc/sudoers" "wa" "actions"
+fix_audit_watch_rule "augenrules" "/etc/sudoers" "wa" "actions"
