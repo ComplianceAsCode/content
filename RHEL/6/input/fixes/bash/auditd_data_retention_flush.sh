92f0f227a9983b07b06f0dd3c27aca35a6ad712a
drax <applezip@gmail.com>
2015-07-23 18:33:37 -0400

    fixed logic for auditd flush remediation and moved fix script to shared

--- RHEL/6/input/fixes/bash/auditd_data_retention_flush.sh
+++ RHEL/6/input/fixes/bash/auditd_data_retention_flush.sh
@@ -1,17 +1,1 @@
-source ./templates/support.sh
-populate var_auditd_flush
-
-# if flush is present, flush param edited to var_auditd_flush
-# else flush param is defined by var_auditd_flush
-#
-# the freq param is disabled if var_auditd_flush isn't incremental
-
-grep -q ^flush /etc/audit/auditd.conf && \
-  sed -i 's/^flush.*/flush = '"$var_auditd_flush"'/g' /etc/audit/auditd.conf
-if ! [ $? -eq 0 ]; then
-  echo "flush = $var_auditd_flush" >> /etc/audit/auditd.conf
-fi
-
-if ! [ "$var_auditd_flush" == "incremental" ]; then
-  sed -i 's/^freq/##freq/g' /etc/audit/auditd.conf
-fi
+../../../../../shared/fixes/bash/auditd_data_retention_flush.sh
---
+++ shared/fixes/bash/auditd_data_retention_flush.sh
@@ -0,0 +1,29 @@
+source ./templates/support.sh
+populate var_auditd_flush
+
+AUDITCONFIG=/etc/audit/auditd.conf
+
+# if flush is present, flush param edited to var_auditd_flush
+# else flush param is defined by var_auditd_flush
+#
+# the freq param is only used value 'incremental' and will be
+# commented out if flush != incremental
+#
+# if flush == incremental && freq param is not defined, it 
+# will be defined as the package-default value of 20
+
+grep -q ^flush $AUDITCONFIG && \
+  sed -i 's/^flush.*/flush = '"$var_auditd_flush"'/g' $AUDITCONFIG
+if ! [ $? -eq 0 ]; then
+  echo "flush = $var_auditd_flush" >> $AUDITCONFIG
+fi
+
+if ! [ "$var_auditd_flush" == "incremental" ]; then
+  sed -i 's/^freq/##freq/g' $AUDITCONFIG
+elif [ "$var_auditd_flush" == "incremental" ]; then
+  grep -q freq $AUDITCONFIG && \
+    sed -i 's/^#\+freq/freq/g' $AUDITCONFIG
+  if ! [ $? -eq 0 ]; then
+    echo "freq = 20" >> $AUDITCONFIG
+  fi
+fi
