80146345ef842cfe479083d103df12f529c59a57
Jan Lieskovsky <jlieskov@redhat.com>
2015-08-19 10:16:09 +0200

    [Enhancement] [RHEL/7] New RHEL-7 remediation for 'auditd_audispd_syslog_plugin_activated' rule
    Testing report:
    --------------
    Verified manually this change (together with corresponding OVAL check change from:
      [1] https://github.com/iankko/scap-security-guide/tree/various_rhel7_pci_dss_oval_addons
    
    which is not merged yet) works fine on RHEL-7 system.

---
+++ RHEL/7/input/fixes/bash/auditd_audispd_syslog_plugin_activated.sh
@@ -0,0 +1,6 @@
+
+grep -q ^active /etc/audisp/plugins.d/syslog.conf && \
+  sed -i "s/active.*/active = yes/g" /etc/audisp/plugins.d/syslog.conf
+if ! [ $? -eq 0 ]; then
+    echo "active = yes" >> /etc/audisp/plugins.d/syslog.conf
+fi
