a912ed41f706b01bba7f1109b7e959bbca1ecc3e
Gabe <redhatrises@gmail.com>
2015-10-06 06:50:40 -0600

    [Enhancement] update remdiation scripts to add new platform tag
    - remove symlinks

--- shared/fixes/bash/require_singleuser_auth.sh
+++ shared/fixes/bash/require_singleuser_auth.sh
@@ -1,3 +1,4 @@
+# platform = Red Hat Enterprise Linux 7
 grep -q sulogin /usr/lib/systemd/system/rescue.service 
 if ! [ $? -eq 0 ]; then
     sed -i "s/-c \"/-c \"\/sbin\/sulogin; /g" /usr/lib/systemd/system/rescue.service
