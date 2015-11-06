9bc54f0eace14a70928f142fd087afba7653794c
Gabe <redhatrises@gmail.com>
2015-11-06 11:21:47 -0700

    [Enhancement][RHEL6/7] Add audit permission scripts and update XCCDF/OVAL content
    - Add file_permissions_var_log_audit remediation script
    - Add file_ownership_var_log_audit remediation script
    - Fixes #708
    - Fixes #482

--- shared/remediations/bash/file_permissions_var_log_audit.sh
+++ shared/remediations/bash/file_permissions_var_log_audit.sh
@@ -1,3 +1,20 @@
 # platform = multi_platform_rhel
-chmod -R 640 /var/log/audit/*
-chmod 640 /etc/audit/audit.rules
+
+if `grep -q ^log_group /etc/audit/auditd.conf` ; then
+  GROUP=$(awk -F "=" '/log_group/ {print $2}' /etc/audit/auditd.conf | tr -d ' ')
+  if ! [ "${GROUP}" == 'root' ] ; then
+    chmod 0640 /var/log/audit/audit.log
+    chmod 0440 /var/log/audit/audit.log.*
+  else
+    chmod 0600 /var/log/audit/audit.log
+    chmod 0400 /var/log/audit/audit.log.*
+  fi
+
+  chmod 0640 /etc/audit/audit*
+  chmod 0640 /etc/audit/rules.d/*
+else
+  chmod 0600 /var/log/audit/audit.log
+  chmod 0400 /var/log/audit/audit.log.*
+  chmod 0640 /etc/audit/audit*
+  chmod 0640 /etc/audit/rules.d/*
+fi
