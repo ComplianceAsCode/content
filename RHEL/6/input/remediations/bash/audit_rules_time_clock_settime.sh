79418ee0d63dada5adba906efe091b19ce1cf24d
Jan Lieskovsky <jlieskov@redhat.com>
2015-11-06 16:12:15 +0100

    [BugFix] [RHEL/6] [RHEL/7] [Fedora] Fix existing remediations for 'audit_rules_time_clock_settime' rule they to properly configure also the'-F a0=0x0' option, which is required per:   [1] https://github.com/OpenSCAP/scap-security-guide/issues/818
    This is the remediation scripts fix part for:
      https://github.com/OpenSCAP/scap-security-guide/issues/818
    
    [BugFix] [RHEL/5] Delete the existing RHEL-5 & RHEL-4 remediation for
    'audit_rules_time_clock_settime' rule, since it's wrong:
    * inserts just arch specific version of the rule into 'audit.rules'
      file (while it should be inserting -F arch=b32 version of the rule
      on 32-bit arch, but both -F arch=b32 and -F arch=b64 versions of the
      rules on 64-bit architectures),
    * it's able only to insert expected rules into 'audit.rules', but not
      correct wrong rule if already present,
    * doesn't insert -F a0=0x0 argument for RHEL-5 version of the rule.
    
    In other words, it needs complete rewrite (yet even distinguish the
    RHEL-4 vs RHEL-5 version syntax).
    
    Therefore deleting the RHEL-5 remediation for this rule for now
    (wrong remediation is even worse than no remediation, since it might
    break many systems in instant time).

--- RHEL/6/input/remediations/bash/audit_rules_time_clock_settime.sh
+++ RHEL/6/input/remediations/bash/audit_rules_time_clock_settime.sh
@@ -5,12 +5,12 @@
 
 # First perform the remediation of the syscall rule
 # Retrieve hardware architecture of the underlying system
-[ $(getconf LONG_BIT) = "32" ] && RULE_ARCHS=("b32") || RULE_ARCHS=("b32" "b64")
+[ "$(getconf LONG_BIT)" = "32" ] && RULE_ARCHS=("b32") || RULE_ARCHS=("b32" "b64")
 
-for ARCH in ${RULE_ARCHS[@]}
+for ARCH in "${RULE_ARCHS[@]}"
 do
-	PATTERN="-a always,exit -F arch=$ARCH -S .* -k audit_time_rules"
+	PATTERN="-a always,exit -F arch=$ARCH -S clock_settime -F a0=.* \(-F key=\|-k \).*"
 	GROUP="clock_settime"
-	FULL_RULE="-a always,exit -F arch=$ARCH -S clock_settime -k audit_time_rules"
+	FULL_RULE="-a always,exit -F arch=$ARCH -S clock_settime -F a0=0x0 -k time-change"
 	fix_audit_syscall_rule "auditctl" "$PATTERN" "$GROUP" "$ARCH" "$FULL_RULE"
 done
--- shared/remediations/bash/audit_rules_time_clock_settime.sh
+++ shared/remediations/bash/audit_rules_time_clock_settime.sh
@@ -1,17 +1,17 @@
-# platform = Red Hat Enterprise Linux 7
+# platform = Red Hat Enterprise Linux 7, multi_platform_fedora
 
 # Include source function library.
 . /usr/share/scap-security-guide/remediation_functions
 
 # First perform the remediation of the syscall rule
 # Retrieve hardware architecture of the underlying system
-[ $(getconf LONG_BIT) = "32" ] && RULE_ARCHS=("b32") || RULE_ARCHS=("b32" "b64")
+[ "$(getconf LONG_BIT)" = "32" ] && RULE_ARCHS=("b32") || RULE_ARCHS=("b32" "b64")
 
 for ARCH in "${RULE_ARCHS[@]}"
 do
-	PATTERN="-a always,exit -F arch=$ARCH -S .* -k .*"
+	PATTERN="-a always,exit -F arch=$ARCH -S clock_settime -F a0=.* \(-F key=\|-k \).*"
 	GROUP="clock_settime"
-	FULL_RULE="-a always,exit -F arch=$ARCH -S clock_settime -k audit_time_rules"
+	FULL_RULE="-a always,exit -F arch=$ARCH -S clock_settime -F a0=0x0 -k time-change"
 	# Perform the remediation for both possible tools: 'auditctl' and 'augenrules'
 	fix_audit_syscall_rule "auditctl" "$PATTERN" "$GROUP" "$ARCH" "$FULL_RULE"
 	fix_audit_syscall_rule "augenrules" "$PATTERN" "$GROUP" "$ARCH" "$FULL_RULE"
