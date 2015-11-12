65160d2aa8442e398e257eca8399154aef8aefa2
Jan Lieskovsky <jlieskov@redhat.com>
2015-11-12 19:12:12 +0100

    [Enhancement] [RHEL/6] Add new RHEL-6 remediation for 'accounts_password_pam_minlen' rule
    Fixes: https://github.com/OpenSCAP/scap-security-guide/issues/835
    
    Testing report:
    --------------
    Verified on recent RHEL-6 system the provided remediation script
    works as epxected.

---
+++ RHEL/6/input/remediations/bash/accounts_password_pam_minlen.sh
@@ -0,0 +1,10 @@
+# platform = Red Hat Enterprise Linux 6
+source ./templates/support.sh
+populate var_password_pam_minlen
+
+if grep -q "minlen=" /etc/pam.d/system-auth
+then
+	sed -i --follow-symlink "s/\(minlen *= *\).*/\1$var_password_pam_minlen/" /etc/pam.d/system-auth
+else
+	sed -i --follow-symlink "/pam_cracklib.so/ s/$/ minlen=$var_password_pam_minlen/" /etc/pam.d/system-auth
+fi
