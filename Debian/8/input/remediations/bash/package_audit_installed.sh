8b6e8822b01d6425df9ddcc60200e41fd01e4669
Philippe Thierry <phil@internal.reseau-libre.net>
2015-11-25 17:12:24 +0100

    initial integration of Debian 8 in ssg. Not yet functional. Targets added in root makefile.

---
+++ Debian/8/input/remediations/bash/package_audit_installed.sh
@@ -0,0 +1,4 @@
+# platform = multi_platform_fedora
+. /usr/share/scap-security-guide/remediation_functions
+
+package_command install audit
---
+++ Debian/8/input/remediations/bash/service_chronyd_enabled.sh
@@ -0,0 +1,4 @@
+# platform = multi_platform_fedora
+. /usr/share/scap-security-guide/remediation_functions
+
+service_command enable chronyd.service
