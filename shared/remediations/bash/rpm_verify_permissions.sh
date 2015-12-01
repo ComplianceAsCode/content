a6fd379f74459b5776ae9ff1784b699ad1d387d7
Jan Lieskovsky <jlieskov@redhat.com>
2015-12-01 16:34:31 +0100

    [Enhancement] [RHEL/6] [RHEL/7] Add remediation for 'rpm_verify_permissions' rule
    Fixes: https://github.com/OpenSCAP/scap-security-guide/issues/834
    
    Testing report:
    ---------------
    The proposed change has been tested manually on recent RHEL-7 system &
    AFAICT from testing it's working fine.
    
    Note:
    -----
    This remediation script will NOT put the system in question into 'fixed'
    state. This is because even after performing the remediation (calling
    permissions that saved in the RPM database there will remain one unfixed
    file path, namely "/var/log/gdm". The issue here being that this is known
    bug:
      [1] https://bugzilla.redhat.com/show_bug.cgi?id=1277603
    
    reported to downstream bugzilla (till calling "# rpm --setperms gdm"
    won't fix the problem also for the "/var/log/gdm" location, the returned
    result of this remediation script will be 'error' instead of 'fixed').
    
    But the proper work of this remediation can be verified by running the command:
    
    before and after the remediation and comparing the results (the count of
    files reported before the remediation will be higher than count of files
    having incorrect permissions after the remediation has finished -- the
    only unfixed exception should be "/var/log/gdm" file path and we have
    bugs reported downstream for these).

---
+++ shared/remediations/bash/rpm_verify_permissions.sh
@@ -0,0 +1,28 @@
+# platform = multi_platform_rhel
+
+# Declare array to hold list of RPM packages we need to correct permissions for
+declare -a SETPERMS_RPM_LIST
+
+# Create a list of files on the system having permissions different from what
+# is expected by the RPM database
+FILES_WITH_INCORRECT_PERMS=($(rpm -Va | grep '^.M'))
+
+# For each file path from that list:
+# * Determine the RPM package the file path is shipped by,
+# * Include it into SETPERMS_RPM_LIST array
+
+for FILE_PATH in "${FILES_WITH_INCORRECT_PERMS[@]}"
+do
+	RPM_PACKAGE=$(rpm -qf "$FILE_PATH")
+	SETPERMS_RPM_LIST=("${SETPERMS_RPM_LIST[@]}" "$RPM_PACKAGE")
+done
+
+# Remove duplicate mention of same RPM in $SETPERMS_RPM_LIST (if any)
+SETPERMS_RPM_LIST=( $(echo "${SETPERMS_RPM_LIST[@]}" | sort -n | uniq) )
+
+# For each of the RPM packages left in the list -- reset its permissions to the
+# correct values
+for RPM_PACKAGE in "${SETPERMS_RPM_LIST[@]}"
+do
+	rpm --setperms "${RPM_PACKAGE}"
+done
