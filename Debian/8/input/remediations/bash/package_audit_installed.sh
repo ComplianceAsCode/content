3400ab6c3cb82ce606d799896e7750294f18f58f
Jean-Baptiste Donnette <donnet_j@epita.fr>
2015-12-01 10:51:19 +0100

    taking old Fedora and apply change for debian, tests are not useful (test on yum)

--- Debian/8/input/remediations/bash/package_audit_installed.sh
+++
@@ -1,4 +0,0 @@
-# platform = multi_platform_fedora
-. /usr/share/scap-security-guide/remediation_functions
-
-package_command install audit
--- Debian/8/input/remediations/bash/service_chronyd_enabled.sh
+++
@@ -1,4 +0,0 @@
-# platform = multi_platform_fedora
-. /usr/share/scap-security-guide/remediation_functions
-
-service_command enable chronyd.service
