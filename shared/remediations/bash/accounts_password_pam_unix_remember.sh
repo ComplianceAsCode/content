c99e4f5376ab105e4d03ccb50228fcbd459757e4
Chris Reynolds <c.reynolds82@gmail.com>
2015-12-02 19:03:49 +0000

    Update accounts_password_pam_unix_remember.sh
    Testing failure on my part: my fix did not update values in the original state.
    
    New line takes care of both states.

--- shared/remediations/bash/accounts_password_pam_unix_remember.sh
+++ shared/remediations/bash/accounts_password_pam_unix_remember.sh
@@ -3,7 +3,7 @@ source ./templates/support.sh
 populate var_password_pam_unix_remember
 
 if grep -q "remember=" /etc/pam.d/system-auth; then   
-	sed -i --follow-symlink "s/\(remember *= *\).*[[:space:]]/\1$var_password_pam_unix_remember /" /etc/pam.d/system-auth
+	sed -i --follow-symlinks "s/\(^password.*sufficient.*pam_unix.so.*\)\(\(remember *= *\)[^ $]*\)/\1remember=$var_password_pam_unix_remember/" /etc/pam.d/system-auth
 else
 	sed -i --follow-symlink "/^password[[:space:]]\+sufficient[[:space:]]\+pam_unix.so/ s/$/ remember=$var_password_pam_unix_remember/" /etc/pam.d/system-auth
 fi
