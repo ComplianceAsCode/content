6361d63f1354c4aa14a9e2c29acb165f00da11ab
Gautam Satish <gautams@hpe.com>
2015-12-21 22:45:41 -0800

    Fix for issue #932

--- RHEL/6/input/remediations/bash/sysctl_net_ipv4_conf_all_accept_source_route.sh
+++ RHEL/6/input/remediations/bash/sysctl_net_ipv4_conf_all_accept_source_route.sh
@@ -1,16 +1,19 @@
 # platform = Red Hat Enterprise Linux 6
+. /usr/share/scap-security-guide/remediation_functions
+populate sysctl_net_ipv4_conf_all_accept_source_route_value
+
 #
 # Set runtime for net.ipv4.conf.all.accept_source_route
 #
-/sbin/sysctl -q -n -w net.ipv4.conf.all.accept_source_route=0
+/sbin/sysctl -q -n -w net.ipv4.conf.all.accept_source_route=$sysctl_net_ipv4_conf_all_accept_source_route_value
 
 #
-# If net.ipv4.conf.all.accept_source_route present in /etc/sysctl.conf, change value to "0"
-#	else, add "net.ipv4.conf.all.accept_source_route = 0" to /etc/sysctl.conf
+# If net.ipv4.conf.all.accept_source_route present in /etc/sysctl.conf, change value to appropriate value
+#	else, add "net.ipv4.conf.all.accept_source_route = value" to /etc/sysctl.conf
 #
 if grep --silent ^net.ipv4.conf.all.accept_source_route /etc/sysctl.conf ; then
-	sed -i 's/^net.ipv4.conf.all.accept_source_route.*/net.ipv4.conf.all.accept_source_route = 0/g' /etc/sysctl.conf
+	sed -i "s/^net.ipv4.conf.all.accept_source_route.*/net.ipv4.conf.all.accept_source_route = $sysctl_net_ipv4_conf_all_accept_source_route_value/g" /etc/sysctl.conf
 else
-	echo -e "\n# Set net.ipv4.conf.all.accept_source_route to 0 per security requirements" >> /etc/sysctl.conf
-	echo "net.ipv4.conf.all.accept_source_route = 0" >> /etc/sysctl.conf
+	echo -e "\n# Set net.ipv4.conf.all.accept_source_route to $sysctl_net_ipv4_conf_all_accept_source_route_value per security requirements" >> /etc/sysctl.conf
+	echo "net.ipv4.conf.all.accept_source_route = $sysctl_net_ipv4_conf_all_accept_source_route_value" >> /etc/sysctl.conf
 fi
