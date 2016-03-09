834cd7ddc777516ea5c0922b999f277ba1429024
Gabe <redhatrises@gmail.com>
2016-03-09 09:58:12 -0700

    [Enhancement][Firefox] Use the new Firefox remediation functions

--- Firefox/input/remediations/bash/firefox_preferences-autofill_passwords.sh
+++ Firefox/input/remediations/bash/firefox_preferences-autofill_passwords.sh
@@ -1,11 +1,4 @@
 # platform = Mozilla Firefox
-FIREFOX_DIRS="/usr/lib/firefox /usr/lib64/firefox /usr/local/lib/firefox /usr/local/lib64/firefox"
-for FIREFOX_DIR in ${FIREFOX_DIRS}; do
-  if [ -d "${FIREFOX_DIR}" ]; then
-    grep -q '^lockPref(\"signon.prefillForms\", false);' "${FIREFOX_DIR}"/mozilla.cfg && \
-    sed -i 's/lockPref(\"signon.prefillForms\".*/lockPref(\"signon.prefillForms\", false);/g' "${FIREFOX_DIR}"/mozilla.cfg
-    if ! [ $? -eq 0 ] ; then 
-      echo 'lockPref("signon.prefillForms", false);' >> "${FIREFOX_DIR}"/mozilla.cfg
-    fi
-  fi
-done
+. /usr/share/scap-security-guide/remediation_functions
+
+firefox_cfg_setting "stig.cfg" "signon.prefillForms" "false"
--- Firefox/input/remediations/bash/firefox_preferences-ssl_version_2.sh
+++ Firefox/input/remediations/bash/firefox_preferences-ssl_version_2.sh
@@ -1,11 +1,4 @@
 # platform = Mozilla Firefox
-FIREFOX_DIRS="/usr/lib/firefox /usr/lib64/firefox /usr/local/lib/firefox /usr/local/lib64/firefox"
-for FIREFOX_DIR in ${FIREFOX_DIRS}; do
-  if [ -d "${FIREFOX_DIR}" ]; then
-    grep -q '^lockPref(\"security.enable_ssl2\", false);' "${FIREFOX_DIR}"/mozilla.cfg && \
-    sed -i 's/lockPref(\"security.enable_ssl2\".*/lockPref(\"security.enable_ssl2\", false);/g' "${FIREFOX_DIR}"/mozilla.cfg
-    if ! [ $? -eq 0 ] ; then
-      echo 'lockPref("security.enable_ssl2", false);' >> "${FIREFOX_DIR}"/mozilla.cfg
-    fi
-  fi
-done
+. /usr/share/scap-security-guide/remediation_functions
+
+firefox_cfg_setting "stig.cfg" "security.enable_ssl2" "false"
