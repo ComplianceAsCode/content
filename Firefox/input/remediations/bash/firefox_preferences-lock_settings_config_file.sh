1cd10aaa742efb5c3469f60cb5fdd19aa386e283
Gabe <redhatrises@gmail.com>
2016-03-15 13:08:03 -0600

    [bugfix] Send parenthesis to function for firefox_preferences-lock_settings_config_file.sh

--- Firefox/input/remediations/bash/firefox_preferences-lock_settings_config_file.sh
+++ Firefox/input/remediations/bash/firefox_preferences-lock_settings_config_file.sh
@@ -1,4 +1,4 @@
 # platform = Mozilla Firefox
 . /usr/share/scap-security-guide/remediation_functions
 
-firefox_js_setting "stig_settings.js" "general.config.filename" "mozilla.cfg"
+firefox_js_setting "stig_settings.js" "general.config.filename" "\"stig.cfg\""
