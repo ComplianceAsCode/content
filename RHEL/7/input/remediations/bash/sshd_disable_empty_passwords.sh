ae197930ac6e702a8e718386221743813e0044b1
Nick Carboni <ncarboni@redhat.com>
2016-04-14 10:24:33 -0400

    Use replace_or_append function to edit sshd_config

--- RHEL/7/input/remediations/bash/sshd_disable_empty_passwords.sh
+++ RHEL/7/input/remediations/bash/sshd_disable_empty_passwords.sh
@@ -1,6 +1,6 @@
 # platform = Red Hat Enterprise Linux 7
-grep -qi ^PermitEmptyPasswords /etc/ssh/sshd_config && \
-  sed -i "s/PermitEmptyPasswords.*/PermitEmptyPasswords no/gI" /etc/ssh/sshd_config
-if ! [ $? -eq 0 ]; then
-    echo "PermitEmptyPasswords no" >> /etc/ssh/sshd_config
-fi
+
+# Include source function library.
+. /usr/share/scap-security-guide/remediation_functions
+
+replace_or_append '/etc/ssh/sshd_config' '^PermitEmptyPasswords' 'no' 'CCENUM' '%s %s'
