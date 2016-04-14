dc62478d8b6d7cf6bdcbe5f41f2944ea0d84d77d
Nick Carboni <ncarboni@redhat.com>
2016-04-14 10:24:33 -0400

    Use replace_or_append function to edit sshd_config

--- RHEL/7/input/remediations/bash/sshd_enable_warning_banner.sh
+++ RHEL/7/input/remediations/bash/sshd_enable_warning_banner.sh
@@ -1,7 +1,6 @@
 # platform = Red Hat Enterprise Linux 7
-grep -qi ^Banner /etc/ssh/sshd_config && \
-  sed -i "s/Banner.*/Banner \/etc\/issue/gI" /etc/ssh/sshd_config
-if ! [ $? -eq 0 ]; then
-    echo "" >> /etc/ssh/sshd_config
-    echo "Banner /etc/issue" >> /etc/ssh/sshd_config
-fi
+
+# Include source function library.
+. /usr/share/scap-security-guide/remediation_functions
+
+replace_or_append '/etc/ssh/sshd_config' '^Banner' '/etc/issue' 'CCENUM' '%s %s'
