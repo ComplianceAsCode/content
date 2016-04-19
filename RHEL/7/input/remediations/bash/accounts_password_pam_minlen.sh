1d7a79b3282e8cab212921f1e5963f63d53c84ae
Jan Lieskovsky <jlieskov@redhat.com>
2016-04-19 19:22:00 +0200

    [BugFix] [RHEL/7] Replace buggy implementation of accounts_passwords_pam_*.sh remediation scripts with use of SSG internal replace_or_append() remediation function
    Fixes: https://github.com/OpenSCAP/scap-security-guide/issues/1085
           (all the rules except "accounts_password_pam_retry" which needs slightly
            more testing => part of future PR)
    
    Fixes (downstream): https://bugzilla.redhat.com/show_bug.cgi?id=1309037

--- RHEL/7/input/remediations/bash/accounts_password_pam_minlen.sh
+++ RHEL/7/input/remediations/bash/accounts_password_pam_minlen.sh
@@ -2,8 +2,4 @@
 . /usr/share/scap-security-guide/remediation_functions
 populate var_password_pam_minlen
 
-if egrep -q ^minlen[[:space:]]*=[[:space:]]*[[:digit:]]+ /etc/security/pwquality.conf; then
-	sed -i "s/^\(minlen *= *\).*/\1$var_password_pam_minlen/" /etc/security/pwquality.conf
-else
-	sed -i "/\(minlen *= *\).*/a minlen = $var_password_pam_minlen" /etc/security/pwquality.conf
-fi
+replace_or_append '/etc/security/pwquality.conf' '^minlen' $var_password_pam_minlen 'CCE-27293-0' '%s = %s'
