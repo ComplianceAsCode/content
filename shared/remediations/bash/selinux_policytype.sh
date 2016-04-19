fc4ea1904813c8fb813692b46cbbeb6fffa5f50e
Jan Lieskovsky <jlieskov@redhat.com>
2016-04-19 17:07:32 +0200

    [BugFix] Drop the duplicate inclusion of the remediation_functions library from the following remediation scripts: * selinux_policytype.sh * selinux_state.sh
    since to include the functions library two times is not necessary
    (one time is enough)

--- shared/remediations/bash/selinux_policytype.sh
+++ shared/remediations/bash/selinux_policytype.sh
@@ -1,8 +1,7 @@
 # platform = multi_platform_rhel
-. /usr/share/scap-security-guide/remediation_functions
-populate var_selinux_policy_name
-
+#
 # Include source function library.
 . /usr/share/scap-security-guide/remediation_functions
+populate var_selinux_policy_name
 
 replace_or_append '/etc/sysconfig/selinux' '^SELINUXTYPE=' $var_selinux_policy_name 'CCENUM' '%s=%s'
