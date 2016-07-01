d3af664f4ff0bd9576febaac22d9fda3e8208292
Jan Lieskovsky <jlieskov@redhat.com>
2016-07-01 17:19:49 +0200

    [BugFix] [RHEL/7] RHEL-7 remediation for 'no_empty_passwords' rule is missing --follow-symlinks currently. Fix that and unify the remediations

--- RHEL/5/input/remediations/bash/no_empty_passwords.sh
+++
@@ -1,4 +0,0 @@
-sed -i 's/ nullok//g' /etc/pam.d/system-auth
-if [ -e /etc/pam.d/system-auth-ac ]; then
-	sed -i 's/ nullok//g' /etc/pam.d/system-auth-ac
-fi
--- shared/remediations/bash/no_empty_passwords.sh
+++ shared/remediations/bash/no_empty_passwords.sh
@@ -1,2 +1,2 @@
-# platform = Red Hat Enterprise Linux 6
+# platform = Red Hat Enterprise Linux 5, multi_platform_rhel
 sed --follow-symlinks -i 's/\<nullok\>//g' /etc/pam.d/system-auth
