bdf2cbd92edb65890cad4c280e3251fb9ae59d2c
Molly Jo Bault <Molly.Jo.Bault@ballardtech.com>
2016-07-20 12:43:58 -0400

    Adding accounts_tmout.sh

---
+++ RHEL/7/input/remediations/bash/accounts_tmout.sh
@@ -0,0 +1,7 @@
+# platform = Red Hat Enterprise Linux 7
+#. /usr/share/scap-security-guide/remediation_functions
+#popu#late var_#accounts_#tmout
+
+#echo "TMOUT=$var_accounts_tmout" >> /etc/profile
+
+echo "TMOUT=600" >> /etc/profile
