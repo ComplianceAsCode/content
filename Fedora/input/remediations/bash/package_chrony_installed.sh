2325cc4441fb5421e9f104ddb3ad7a626dc4be74
Jan Lieskovsky <jlieskov@redhat.com>
2016-07-28 15:42:33 +0200

    [BugFix] [Infrastructure] Remove unnecessary / unused remediation scripts
    All of the scripts are created as part of being created automatically via
    templates (where one CSV file is used to instruct the build system to create
    both - OVAL checks and remediations).
    
    Though OVAL checks for these services are necessary (since service_*_enabled /
    disabled OVAl depends on them), this is not the case for the remediations
    (since there exists remediations for particular service_*_enabled / disabled
    rules). None of these remediations is referenced by corresponding XCCDF rule
    (there isn't XCCDF rule that would truly require them). As such, they are
    not necessary to be present, only result into warnings to be displayed during
    the build process.
    
    Thus remove them from the repository.

--- Fedora/input/remediations/bash/package_chrony_installed.sh
+++
@@ -1,2 +0,0 @@
-# platform = multi_platform_fedora
-yum -y install chrony
--- shared/remediations/bash/package_abrt_removed.sh
+++
@@ -1,6 +0,0 @@
-# CAUTION: This remediation script will remove abrt
-#	   from the system, and may remove any packages
-#	   that depend on abrt. Execute this
-#	   remediation AFTER testing on a non-production
-#	   system!
-yum -y erase abrt
