b5d49417e869248eea727f610f16bbf0f6c011c6
Shawn Wells <shawn@redhat.com>
2016-07-29 23:05:45 -0400

    make accounts_tmout idempotent

--- RHEL/7/input/remediations/bash/accounts_tmout.sh
+++ RHEL/7/input/remediations/bash/accounts_tmout.sh
@@ -1,7 +1,10 @@
 # platform = Red Hat Enterprise Linux 7
-#. /usr/share/scap-security-guide/remediation_functions
-#popu#late var_#accounts_#tmout
+. /usr/share/scap-security-guide/remediation_functions
+populate var_accounts_tmout
 
-#echo "TMOUT=$var_accounts_tmout" >> /etc/profile
-
-echo "TMOUT=600" >> /etc/profile
+if grep --silent ^TMOUT /etc/profile ; then
+        sed -i "s/^TMOUT.*/TMOUT = $var_accounts_tmout/g" /etc/profile
+else
+        echo -e "\n# Set TMOUT to $var_accounts_tmout per security requirements" >> /etc/profile
+        echo "TMOUT = $var_accounts_tmout" >> /etc/profile
+fi
