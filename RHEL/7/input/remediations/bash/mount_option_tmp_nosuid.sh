8333fc90677a07ddd7e5b16105853aa036f921e0
Shawn Wells <shawn@redhat.com>
2016-07-29 23:21:16 -0400

    cleaning up mount option scripts

--- RHEL/7/input/remediations/bash/mount_option_tmp_nosuid.sh
+++ RHEL/7/input/remediations/bash/mount_option_tmp_nosuid.sh
@@ -1,14 +1,12 @@
 # platform = Red Hat Enterprise Linux 7
-FSTAB=/etc/fstab
-SED=`which sed`
 NEW_OPT="nosuid"
 
-if [ $(grep " \/tmp " ${FSTAB} | grep -c "$NEW_OPT" ) -eq 0 ]; then
-        MNT_OPTS=$(grep " \/tmp " ${FSTAB} | awk '{print $4}')
-        ${SED} -i "s/\( \/tmp.*${MNT_OPTS}\)/\1,${NEW_OPT}/" ${FSTAB}
+if [ $(grep " \/tmp " /etc/fstab | grep -c "$NEW_OPT" ) -eq 0 ]; then
+        MNT_OPTS=$(grep " \/tmp " /etc/fstab | awk '{print $4}')
+        sed -i "s/\( \/tmp.*${MNT_OPTS}\)/\1,${NEW_OPT}/" /etc/fstab
         
         if [ $MNT_OPTS = "defaults" ]
         then
-        	${SED} -i "s/defaults,//" ${FSTAB}
+        	sed -i "s/defaults,//" /etc/fstab
         fi
 fi
