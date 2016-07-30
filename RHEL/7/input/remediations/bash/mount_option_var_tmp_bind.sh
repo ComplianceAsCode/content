913cf6eac272c7955348b0eb2aa38e87a68f5b10
Shawn Wells <shawn@redhat.com>
2016-07-29 23:20:59 -0400

    cleaning up variables, but this remediation still needs work

--- RHEL/7/input/remediations/bash/mount_option_var_tmp_bind.sh
+++ RHEL/7/input/remediations/bash/mount_option_var_tmp_bind.sh
@@ -1,3 +1,2 @@
 # platform = Red Hat Enterprise Linux 7
-FSTAB=/etc/fstab
-echo "/tmp     /var/tmp     none     rw,nodev,noexec,nosuid,bind     0 0" >> $FSTAB
+echo "/tmp     /var/tmp     none     rw,nodev,noexec,nosuid,bind     0 0" >> /etc/fstab
