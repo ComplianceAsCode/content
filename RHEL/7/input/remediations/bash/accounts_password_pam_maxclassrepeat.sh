3fbc04fc86365831c90e78abb262cec4e483a31c
Shawn Wells <shawn@redhat.com>
2016-07-29 22:53:29 -0400

    Updating to follow remediation template

--- RHEL/7/input/remediations/bash/accounts_password_pam_maxclassrepeat.sh
+++ RHEL/7/input/remediations/bash/accounts_password_pam_maxclassrepeat.sh
@@ -2,6 +2,4 @@
 . /usr/share/scap-security-guide/remediation_functions
 populate var_password_pam_maxclassrepeat
 
-if grep -q "maxclassrepeat=" /etc/security/pwquality.conf ; then   
-	echo "maxclassrepeat = $var_password_pam_maxclassrepeat" >> /etc/security/pwquality.conf
-fi
+replace_or_append '/etc/security/pwquality.conf' '^maxclassrepeat' $var_password_pam_maxclassrepeat '27512-3' '%s = %s'
