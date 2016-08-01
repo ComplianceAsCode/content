ae24612b1569d8c7c65630db892505541b29f7e0
Zbynek Moravec <zmoravec@redhat.com>
2016-08-01 13:14:03 +0200

    Update files by generated versions

--- shared/remediations/bash/package_rsh_removed.sh
+++ shared/remediations/bash/package_rsh_removed.sh
@@ -1,7 +1,12 @@
-# platform = multi_platform_rhel
+# platform = multi_platform_rhel, multi_platform_fedora
+# THIS FILE IS GENERATED. DO NOT EDIT!
 # CAUTION: This remediation script will remove rsh
 #	   from the system, and may remove any packages
 #	   that depend on rsh. Execute this
 #	   remediation AFTER testing on a non-production
 #	   system!
-yum -y erase rsh
+
+# Include source function library.
+. /usr/share/scap-security-guide/remediation_functions
+
+package_command remove rsh
