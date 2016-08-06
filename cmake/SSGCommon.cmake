aa58f61090c4c00189a3d2287985541c2ad722f9
Martin Preisler <mpreisle@redhat.com>
2016-08-06 12:24:45 -0400

    Detect whether oscap supports SVG and act accordingly

--- cmake/SSGCommon.cmake
+++ cmake/SSGCommon.cmake
@@ -1,5 +1,6 @@
 set(SSG_SHARED "${CMAKE_SOURCE_DIR}/shared")
 set(SSG_SHARED_TRANSFORMS "${SSG_SHARED}/transforms")
+set(SSG_SHARED_UTILS "${SSG_SHARED}/utils")
 
 macro(ssg_xsltproc INPUT XSLT OUTPUT)
     add_custom_command(
@@ -11,9 +12,17 @@ macro(ssg_xsltproc INPUT XSLT OUTPUT)
 endmacro()
 
 macro(ssg_build_guide_xml PRODUCT)
-    ssg_xsltproc(
-        ${CMAKE_CURRENT_SOURCE_DIR}/input/guide.xml
-        ${SSG_SHARED_TRANSFORMS}/includelogo.xslt
-        ${CMAKE_CURRENT_BINARY_DIR}/guide.xml
-    )
+    if(OSCAP_SVG_SUPPORT EQUAL 0)
+        ssg_xsltproc(
+            ${CMAKE_CURRENT_SOURCE_DIR}/input/guide.xml
+            ${SSG_SHARED_TRANSFORMS}/includelogo.xslt
+            ${CMAKE_CURRENT_BINARY_DIR}/guide.xml
+        )
+    else()
+        add_custom_command(
+            OUTPUT ${OUTPUT}
+            COMMAND ${CMAKE_COMMAND} -E copy ${INPUT} ${OUTPUT}
+            MAIN_DEPENDENCY ${INPUT}
+        )
+    endif()
 endmacro()
