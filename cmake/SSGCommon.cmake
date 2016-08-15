00359dca361edf83f9dc9069f6c6192fec9b17f1
Martin Preisler <mpreisle@redhat.com>
2016-08-15 11:52:47 -0400

    Enabled unlinked-oval.xml building for Fedora using the cmake build system

--- cmake/SSGCommon.cmake
+++ cmake/SSGCommon.cmake
@@ -89,31 +89,37 @@ macro(ssg_build_xccdf_with_remediations PRODUCT)
 endmacro()
 
 macro(ssg_build_unlinked_oval PRODUCT)
-    file(GLOB OVAL_DEPS "${CMAKE_CURRENT_SOURCE_DIR}/oval/*.xml")
-    file(GLOB SHARED_OVAL_DEPS "${SSG_SHARED}/oval/*.xml")
+    set(OVAL_DEPS_DIR "${CMAKE_CURRENT_SOURCE_DIR}/input/oval")
+    file(GLOB OVAL_DEPS "${OVAL_DEPS_DIR}/*.xml")
+    set(SHARED_OVAL_DEPS_DIR "${SSG_SHARED}/oval")
+    file(GLOB SHARED_OVAL_DEPS "${SHARED_OVAL_DEPS_DIR}/*.xml")
 
     if(OSCAP_OVAL_511_SUPPORT EQUAL 0)
-        file(GLOB OVAL_511_DEPS "${CMAKE_CURRENT_SOURCE_DIR}/oval/oval_5.11/*.xml")
-        file(GLOB SHARED_OVAL_511_DEPS "${SSG_SHARED}/oval/oval_5.11/*.xml")
+        set(OVAL_511_DEPS_DIR "${CMAKE_CURRENT_SOURCE_DIR}/input/oval/oval_5.11")
+        file(GLOB OVAL_511_DEPS "${OVAL_511_DEPS_DIR}/*.xml")
+        set(SHARED_OVAL_511_DEPS_DIR "${SSG_SHARED}/oval/oval_5.11")
+        file(GLOB SHARED_OVAL_511_DEPS "${SHARED_OVAL_511_DEPS_DIR}/*.xml")
 
         add_custom_command(
             OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/unlinked-oval.xml
             # TODO: config
-            COMMAND ${SSG_SHARED_TRANSFORMS}/combineovals.py ${CMAKE_SOURCE_DIR}/config ${PRODUCT} ${OVAL_DEPS} ${OVAL_511_DEPS} ${SHARED_OVAL_DEPS} ${SHARED_OVAL_511_DEPS} ${CMAKE_CURRENT_BINARY_DIR}/unlinked-oval.xml
+            COMMAND ${SSG_SHARED_TRANSFORMS}/combineovals.py ${CMAKE_SOURCE_DIR}/config ${PRODUCT} ${OVAL_DEPS_DIR} ${OVAL_511_DEPS_DIR} ${SHARED_OVAL_DEPS_DIR} ${SHARED_OVAL_511_DEPS_DIR} > ${CMAKE_CURRENT_BINARY_DIR}/unlinked-oval.xml
             COMMAND ${XMLLINT_EXECUTABLE} --format --output ${CMAKE_CURRENT_BINARY_DIR}/unlinked-oval.xml ${CMAKE_CURRENT_BINARY_DIR}/unlinked-oval.xml
             DEPENDS ${OVAL_DEPS}
             DEPENDS ${OVAL_511_DEPS}
             DEPENDS ${SHARED_OVAL_DEPS}
             DEPENDS ${SHARED_OVAL_511_DEPS}
+            VERBATIM
         )
     else()
         add_custom_command(
             OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/unlinked-oval.xml
             # TODO: config
-            COMMAND ${SSG_SHARED_TRANSFORMS}/combineovals.py ${CMAKE_SOURCE_DIR}/config ${PRODUCT} ${OVAL_DEPS} ${SHARED_OVAL_DEPS} ${CMAKE_CURRENT_BINARY_DIR}/unlinked-oval.xml
+            COMMAND ${SSG_SHARED_TRANSFORMS}/combineovals.py ${CMAKE_SOURCE_DIR}/config ${PRODUCT} ${OVAL_DEPS_DIR} ${SHARED_OVAL_DEPS_DIR} > ${CMAKE_CURRENT_BINARY_DIR}/unlinked-oval.xml
             COMMAND ${XMLLINT_EXECUTABLE} --format --output ${CMAKE_CURRENT_BINARY_DIR}/unlinked-oval.xml ${CMAKE_CURRENT_BINARY_DIR}/unlinked-oval.xml
             DEPENDS ${OVAL_DEPS}
             DEPENDS ${SHARED_OVAL_DEPS}
+            VERBATIM
         )
     endif()
 endmacro()
@@ -126,5 +132,5 @@ macro(ssg_build_product PRODUCT)
     ssg_build_xccdf_ocilrefs(${PRODUCT})
     ssg_build_bash_remediations(${PRODUCT})
     ssg_build_xccdf_with_remediations(${PRODUCT})
-    #ssg_build_unlinked_oval(${PRODUCT})
+    ssg_build_unlinked_oval(${PRODUCT})
 endmacro()
