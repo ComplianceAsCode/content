10a3489dae40ecb680df05d1e0a7ab7fdef3807b
Zbynek Moravec <zmoravec@redhat.com>
2016-08-15 16:03:10 +0200

    RHEL6: Generate kernel_module_disabled

--- RHEL/6/templates/static/bash/kernel_module_udf_disabled.sh
+++
@@ -1,2 +0,0 @@
-# platform = Red Hat Enterprise Linux 6
-echo "install udf /bin/true" > /etc/modprobe.d/udf.conf
--- RHEL/6/templates/static/bash/kernel_module_usb-storage_disabled.sh
+++
@@ -1,2 +0,0 @@
-# platform = Red Hat Enterprise Linux 6
-echo "install usb-storage /bin/true" > /etc/modprobe.d/usb-storage.conf
