ab74a4ac914bbb85c80637993d0146769745a409
Zbynek Moravec <zmoravec@redhat.com>
2016-08-15 12:57:55 +0200

    Fix comment

--- RHEL/5/templates/static/bash/sysctl_net_ipv4_tcp_max_syn_backlog.sh
+++ RHEL/5/templates/static/bash/sysctl_net_ipv4_tcp_max_syn_backlog.sh
@@ -11,6 +11,6 @@ if grep --silent ^net.ipv4.tcp_max_syn_backlog /etc/sysctl.conf ; then
 	sed -i 's/^net.ipv4.tcp_max_syn_backlog.*/net.ipv4.tcp_max_syn_backlog = 1280/g' /etc/sysctl.conf
 else
 	echo "" >> /etc/sysctl.conf
-	echo "# Set net.ipv4.tcp_max_syn_backlog to 1 per security requirements" >> /etc/sysctl.conf
+	echo "# Set net.ipv4.tcp_max_syn_backlog to 1280 per security requirements" >> /etc/sysctl.conf
 	echo "net.ipv4.tcp_max_syn_backlog = 1280" >> /etc/sysctl.conf
 fi
