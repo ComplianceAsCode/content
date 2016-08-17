6d09d9903b903f77d4e88a763d88803f59e1855c
Zbynek Moravec <zmoravec@redhat.com>
2016-08-17 23:06:40 +0200

    Shared: generate package_installed

--- shared/templates/static/bash/package_audit_installed.sh
+++
@@ -1,5 +0,0 @@
-# platform = multi_platform_rhel, multi_platform_fedora
-# THIS FILE IS GENERATED. DO NOT EDIT!
-# Include source function library.
-. /usr/share/scap-security-guide/remediation_functions
-package_command install audit
