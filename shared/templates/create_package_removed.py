28b2e4c4924434aafc01ca6a98b300cd9739317b
Zbynek Moravec <zmoravec@redhat.com>
2016-08-30 17:27:11 +0200

    create_package_removed: merge

--- shared/templates/create_package_removed.py
+++ shared/templates/create_package_removed.py
@@ -3,43 +3,45 @@
 #
 # create_package_removed.py
 #   automatically generate checks for removed packages
-#
-# NOTE: The file 'template_package_removed' should be located in the same
-# working directory as this script. The template contains the following tags
-# that *must* be replaced successfully in order for the checks to work.
-#
-# PKGNAME - the name of the package that should be removed
-#
 
 import sys
 
 from template_common import *
 
-def output_check(package_info):
-	pkgname = package_info[0]
-	if pkgname:
+def output_checkfile(target, package_info):
+    pkgname = package_info[0]
+    if pkgname:
+        if target == "oval":
 
-		file_from_template(
-			"./template_package_removed",
-			{ "PKGNAME" : pkgname},
-			"./output/oval/package_{0}_removed.xml", pkgname
-		)
+            file_from_template(
+                "./template_package_removed",
+                { "PKGNAME" : pkgname},
+                "./output/oval/package_{0}_removed.xml", pkgname
+            )
 
-	else:
-		print "ERROR: input violation: the package name must be defined"
+        elif target == "bash":
+            file_from_template(
+                "./template_BASH_package_removed",
+                { "PKGNAME" : pkgname },
+                "./output/bash/package_{0}_removed.sh", pkgname
+            )
 
+        elif target == "ansible":
+            file_from_template(
+                "./template_ANSIBLE_package_removed",
+                { "PKGNAME" : pkgname },
+                "./output/ansible/package_{0}_removed.yml", pkgname
+            )
 
-def main():
-    if len(sys.argv) < 2:
-        print "usage: %s <CSV_FILE_PATH>" % sys.argv[0]
-        print "   the csv file should contain lines of the format:"
-        print "   PACKAGE_NAME"
-        sys.exit(1)
+        else:
+            raise ValueError("Unknown target " + target)
+    else:
+        print "ERROR: input violation: the package name must be defined"
 
-    filename = sys.argv[1]
-    csv_map(filename, output_check, skip_comments = True)
-
-    sys.exit(0)
+def help():
+    print("Usage:\n\t" + __file__ + " <bash/oval/ansible> <csv file>")
+    print("CSV should contains lines of the format: " +
+               "PACKAGE_NAME")
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv, help, output_checkfile)
