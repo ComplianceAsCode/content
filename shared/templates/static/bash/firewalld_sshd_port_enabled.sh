26e4e393c8c29707d7cf69877aa2d26f27929e9e
Gabe <redhatrises@gmail.com>
2016-09-08 08:09:31 -0600

    [Enhancement][RHEL/7] Add external_variable to firewalld ssh port check content

--- shared/templates/static/bash/firewalld_sshd_port_enabled.sh
+++ shared/templates/static/bash/firewalld_sshd_port_enabled.sh
@@ -1,3 +1,11 @@
-# platform = Red Hat Enterprise Linux 7
+# platform = Red Hat Enterprise Linux 7A
+#
+# Include source function library.
+. /usr/share/scap-security-guide/remediation_functions
+populate sshd_listening_port
 
-firewall-cmd --permanent --add-service=ssh
+if [ $sshd_listening_port -ne 22] ; then
+  firewall-cmd --permanent --add-port=$sshd_listening_port/tcp
+else
+  firewall-cmd --permanent --add-service=ssh
+fi
