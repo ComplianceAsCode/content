2cf6806a778654d7755b5c73e5c2058647c80389
Zbynek Moravec <zmoravec@redhat.com>
2016-09-12 15:29:32 +0200

    rhel7: Delete generate accounts_password_*

--- RHEL/7/templates/static/bash/accounts_password_pam_dcredit.sh
+++
@@ -1,5 +0,0 @@
-# platform = Red Hat Enterprise Linux 7
-. /usr/share/scap-security-guide/remediation_functions
-populate var_password_pam_dcredit
-
-replace_or_append '/etc/security/pwquality.conf' '^dcredit' $var_password_pam_dcredit 'CCE-27214-6' '%s = %s'
