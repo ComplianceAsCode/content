e7e58b6403175500e56f0718abef296f107b65a5
Zbynek Moravec <zmoravec@redhat.com>
2016-09-12 15:25:32 +0200

    templates: Generate accounts_password

---
+++ shared/templates/create_accounts_password.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python2
+
+#
+# create_accounts_password.py
+#        generate template-based remediation for account passwords
+
+import sys
+import re
+
+from template_common import *
+
+def output_checkfile(target, pam_info):
+    VARIABLE, CCE = pam_info
+
+    if not CCE:
+        CCE = "CCENUM"
+
+    if target == "bash":
+
+        file_from_template(
+            "./template_BASH_accounts_password",
+            {
+                "VARIABLE":   VARIABLE,
+                "CCE_NUMBER": CCE,
+            },
+            "./output/bash/accounts_password_pam_{0}.sh", VARIABLE
+        )
+
+    else:
+        raise ValueError("Unknown target " + target)
+
+
+def help():
+    print("Usage:\n\t" + __file__ + " <bash> <csv file>")
+    print("CSV should contains lines of the format: " +
+               "VARIABLE,CCE (CCE is optional)")
+
+if __name__ == "__main__":
+    main(sys.argv, help, output_checkfile)
