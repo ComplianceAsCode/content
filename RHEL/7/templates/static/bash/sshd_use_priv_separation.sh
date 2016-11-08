38dcc4fe693a6c7c7fa28664a02975f3704e8c5a
Shawn Wells <shawn@redhat.com>
2016-11-07 22:36:34 -0500

    remediation for sshd_use_priv_separation

---
+++ RHEL/7/templates/static/bash/sshd_use_priv_separation.sh
@@ -0,0 +1,6 @@
+# platform = Red Hat Enterprise Linux 7
+
+# Include source function library.
+. /usr/share/scap-security-guide/remediation_functions
+
+replace_or_append '/etc/ssh/sshd_config' '^UsePrivilegeSeparation' 'yes' '$CCENUM' '%s %s'
