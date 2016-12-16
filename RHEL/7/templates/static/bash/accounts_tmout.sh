8c15c9ded7af82b275bccf3861b02a5e4ad6b161
Gabe <redhatrises@gmail.com>
2016-12-16 11:54:17 -0700

    Expand some XCCDF descriptions and fixes
    - Expand some XCCDF descriptions to be more clear of what to set/verify
    - Fix incorrect <sub idref/> variable reference
    - Fix accounts_tmout.sh

--- RHEL/7/templates/static/bash/accounts_tmout.sh
+++ RHEL/7/templates/static/bash/accounts_tmout.sh
@@ -3,8 +3,8 @@
 populate var_accounts_tmout
 
 if grep --silent ^TMOUT /etc/profile ; then
-        sed -i "s/^TMOUT.*/TMOUT = $var_accounts_tmout/g" /etc/profile
+        sed -i "s/^TMOUT.*/TMOUT=$var_accounts_tmout/g" /etc/profile
 else
         echo -e "\n# Set TMOUT to $var_accounts_tmout per security requirements" >> /etc/profile
-        echo "TMOUT = $var_accounts_tmout" >> /etc/profile
+        echo "TMOUT=$var_accounts_tmout" >> /etc/profile
 fi
