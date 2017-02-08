8f113ab26f5835df5828ca54e7cf09d786535439
Philippe Thierry <philippe.thierry@reseau-libre.net>
2017-02-08 13:59:16 +0100

    corrected some pylint Warns

--- shared/templates/create_partitions_rights.py
+++ shared/templates/create_partitions_rights.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python2
 
 #
-# create_permission.py
+# create_partitions_rights.py
 #        generate template-based checks for partition mount rights
 
 
@@ -15,18 +15,18 @@ def output_checkfile(target, path_info):
     # the csv file contains lines that match the following layout:
     #    directory,file_name,uid,gid,mode
     #dir_path, file_name, uid, gid, mode = path_info
-	mount_point, mount_flag = path_info
+    mount_point, mount_flag = path_info
 
     # build a string out of the path that is suitable for use in id tags
     # example:	/var/log --> _var_log
     # path_id maps to MOUNTPOINTID in the template
-	point_id = re.sub('[-\./]', '_', mount_point)
-	flag_id = re.sub('[-\./]', '_', mount_flag)
+    point_id = re.sub('[-\./]', '_', mount_point)
+    flag_id = re.sub('[-\./]', '_', mount_flag)
 
-	if target == "oval":
+    if target == "oval":
         # we are ready to create the check
         # open the template and perform the conversions
-		file_from_template(
+        file_from_template(
             "./template_partitions_rights",
             {
                 "MOUNTPOINT":       mount_point,
@@ -36,12 +36,12 @@ def output_checkfile(target, path_info):
             },
             "./oval/partitions_rights{0}.xml", point_id + '_' + flag_id
         )
-	else:
-		raise UnknownTargetError(target)
+    else:
+        raise UnknownTargetError(target)
 
 
 def help():
-    print("Usage:\n\t" + __file__ + " <bash/oval/ansible> <csv file>")
+    print "Usage:\n\t" + __file__ + " <bash/oval/ansible> <csv file>"
     print("CSV should contains lines of the format: "
           "directory path,file name,owner uid (numeric),group "
           "owner gid (numeric),mode")
