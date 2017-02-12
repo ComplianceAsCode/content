84900e5bc941a69ed4cccb3a7be4454e8d989bbb
Martin Preisler <mpreisle@redhat.com>
2017-02-12 17:27:44 -0500

    Scribbled some notes about the SSG cmake build system

--- cmake/SSGCommon.cmake
+++ cmake/SSGCommon.cmake
@@ -1,3 +1,56 @@
+# Important developer notes:
+
+# Minimal build targets
+# ============================================================================
+#
+# For the build system to be efficient it makes sense to separate build
+# targets as much as possible. Please do not be lazy and group multiple
+# files into one add_custom_command. This prevents parallelization and slows
+# down the builds!
+#
+#
+# Avoid input output overlap
+# ============================================================================
+#
+# If there is any overlap in inputs and/or outputs of build targets the build
+# system will needlessly rebuild the target every time you run the build.
+# Please avoid this because it slows down incremental builds. Incremental
+# builds are done all the time by SSG developers so it makes sense to have
+# them as fast as possible.
+#
+#
+# Wrapper targets
+# ============================================================================
+#
+# Notice that most (if not all) add_custom_command calls are immediately
+# followed with a wrapper add_custom_target. We do that to generate proper
+# dependency directed graphs so that dependencies can be shared. Without
+# this wrapper you wouldn't have been able to do parallel builds of multiple
+# targets at once. E.g.:
+#
+# $ make -j 4 rhel7-guides rhel7-validate
+#
+# Without the wrapper targets the command above would start generating the
+# XCCDF, OVAL and OCIL files 2 times in parallel which would result in
+# broken files.
+#
+# Please keep this in mind when modifying the build system.
+#
+# Read:
+# https://samthursfield.wordpress.com/2015/11/21/cmake-dependencies-between-targets-and-files-and-custom-commands/
+# for more info.
+#
+#
+# Folders should not be build inputs or outputs
+# ============================================================================
+#
+# It may be tempting to mark an entire folder as build output but doing that
+# has unexpected consequences. Please avoid that and always list the files.
+#
+#
+# Good luck hacking the SCAP Security Guide build system!
+
+
 set(SSG_SHARED "${CMAKE_SOURCE_DIR}/shared")
 set(SSG_SHARED_REFS "${SSG_SHARED}/references")
 set(SSG_SHARED_TRANSFORMS "${SSG_SHARED}/transforms")
