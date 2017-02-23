067eaa97b40b8d8c73c69057fd560e7bddecf7b7
Shawn Wells <shawn@redhat.com>
2017-02-23 00:58:06 -0500

    adding unique remediations for login events

---
+++ shared/templates/static/bash/audit_rules_login_events_tallylog.sh
@@ -0,0 +1,9 @@
+# platform = Red Hat Enterprise Linux 7, multi_platform_fedora
+
+# Include source function library.
+. /usr/share/scap-security-guide/remediation_functions
+
+# Perform the remediation for both possible tools: 'auditctl' and 'augenrules'
+
+fix_audit_watch_rule "auditctl" "/var/log/tallylog" "wa" "logins"
+fix_audit_watch_rule "augenrules" "/var/log/tallylog" "wa" "logins"
