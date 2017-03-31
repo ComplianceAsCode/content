7897e5b92b4960a41bedff4d5ae38768f6850721
Shawn Wells <shawn@redhat.com>
2017-03-30 21:40:37 -0400

    updates to audit kernel module remediations
    - moved kernel_module_loading from rhel7 to shared, updated macro
    - updated loading_delete and loading_init to reflect only b32 or b64 kernel mods can be installed
    
    resolves https://github.com/OpenSCAP/scap-security-guide/issues/1790

--- shared/templates/static/bash/audit_rules_kernel_module_loading_delete.sh
+++ shared/templates/static/bash/audit_rules_kernel_module_loading_delete.sh
@@ -5,12 +5,16 @@
 
 # First perform the remediation of the syscall rule
 # Retrieve hardware architecture of the underlying system
-[ "$(getconf LONG_BIT)" = "32" ] && RULE_ARCHS=("b32") || RULE_ARCHS=("b32" "b64")
+# Note: 32-bit kernel modules can't be loaded / unloaded on 64-bit kernel =>
+#       it's not required on a 64-bit system to check also for the presence
+#       of 32-bit's equivalent of the corresponding rule. Therefore for
+#       each system it's enought to check presence of system's native rule form.
+[ "$(getconf LONG_BIT)" = "32" ] && RULE_ARCHS=("b32") || RULE_ARCHS=("b64")
 
 for ARCH in "${RULE_ARCHS[@]}"
 do
 	PATTERN="-a always,exit -F arch=$ARCH -S delete_module \(-F key=\|-k \).*"
-	GROUP="delete_module"
+	GROUP="modules"
 	FULL_RULE="-a always,exit -F arch=$ARCH -S delete_module -k modules"
 	# Perform the remediation for both possible tools: 'auditctl' and 'augenrules'
 	fix_audit_syscall_rule "auditctl" "$PATTERN" "$GROUP" "$ARCH" "$FULL_RULE"
