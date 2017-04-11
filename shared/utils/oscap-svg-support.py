787810ef321ad6a1f6b5d5c9e754ed98b932a2d5
Martin Preisler <mpreisle@redhat.com>
2017-04-11 16:43:59 -0400

    Put functionality in the main() function in oscap-svg-support.py

--- shared/utils/oscap-svg-support.py
+++ shared/utils/oscap-svg-support.py
@@ -5,8 +5,6 @@ from tempfile import mkstemp
 import os
 import sys
 
-# Default exit with failure
-EXIT_CODE = 1
 
 svg_benchmark = """<?xml version="1.0"?>
 <Benchmark xmlns="http://checklists.nist.gov/xccdf/1.1"
@@ -31,38 +29,46 @@ svg_benchmark = """<?xml version="1.0"?>
 </Benchmark>
 """
 
-# Create temporary file with the content of svg_benchmark variable above
-fd, filename = mkstemp(prefix='svg_', suffix='.xml')
-xccdf = os.fdopen(fd, 'wt')
-xccdf.write(svg_benchmark)
-xccdf.close()
 
-# Call oscap process to generate guide
-command = "oscap xccdf generate guide %s" % filename
-child = Popen(command.split(), stdout=PIPE, stderr=PIPE)
-out, err = child.communicate()
-out = out.decode("utf-8")
-
-# Child run sanity check
-if child.returncode != 0:
-    # Set exit value to failure
+def main():
+    # Default exit with failure
     EXIT_CODE = 1
 
-# Delete the temporary file
-try:
-    os.remove(filename)
-except OSError as e:
-    sys.stderr.write(
-        "Error removing file: %s - %s\n" % (e.filename, e.strerror)
-    )
+    # Create temporary file with the content of svg_benchmark variable above
+    fd, filename = mkstemp(prefix='svg_', suffix='.xml')
+    xccdf = os.fdopen(fd, 'wt')
+    xccdf.write(svg_benchmark)
+    xccdf.close()
 
-# Check if generated guide contains desired SVG element
-if "circle" in out:
-    # If so, set exit value to success
-    EXIT_CODE = 0
-else:
-    # Otherwise to failure
-    EXIT_CODE = 1
+    # Call oscap process to generate guide
+    command = "oscap xccdf generate guide %s" % filename
+    child = Popen(command.split(), stdout=PIPE, stderr=PIPE)
+    out, err = child.communicate()
+    out = out.decode("utf-8")
 
-# Call exit with appropriate value
-sys.exit(EXIT_CODE)
+    # Child run sanity check
+    if child.returncode != 0:
+        # Set exit value to failure
+        EXIT_CODE = 1
+
+    # Delete the temporary file
+    try:
+        os.remove(filename)
+    except OSError as e:
+        sys.stderr.write(
+            "Error removing file: %s - %s\n" % (e.filename, e.strerror)
+        )
+
+    # Check if generated guide contains desired SVG element
+    if "circle" in out:
+        # If so, set exit value to success
+        EXIT_CODE = 0
+    else:
+        # Otherwise to failure
+        EXIT_CODE = 1
+
+    # Call exit with appropriate value
+    sys.exit(EXIT_CODE)
+
+if __name__ == "__main__":
+    main()
