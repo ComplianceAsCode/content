af59da9c1bf7b9f0857305541d1b3d319d67a9fc
Martin Preisler <mpreisle@redhat.com>
2017-04-24 13:38:56 -0400

    build-all-remediation-roles.py to use xccdf_utils to avoid duplication

--- shared/utils/build-all-remediation-roles.py
+++ shared/utils/build-all-remediation-roles.py
@@ -18,113 +18,17 @@ import subprocess
 
 import threading
 import Queue
-import re
 import sys
 import multiprocessing
 
+
+# Put shared python modules in path
+sys.path.insert(0, os.path.join(
+        os.path.dirname(os.path.dirname(os.path.realpath(__file__))),
+        "modules"))
+import xccdf_utils
+
 OSCAP_PATH = "oscap"
-
-XCCDF11_NS = "http://checklists.nist.gov/xccdf/1.1"
-XCCDF12_NS = "http://checklists.nist.gov/xccdf/1.2"
-
-# if a profile ID ends with a string listed here we skip it
-PROFILE_ID_BLACKLIST = ["test", "index", "default"]
-# filler XCCDF 1.2 prefix which we will strip to avoid very long filenames
-PROFILE_ID_PREFIX = ("^xccdf_org.*content_profile_")
-
-
-def get_benchmark_ids_titles_for_input(input_tree):
-    ret = {}
-
-    def scrape_benchmarks(root_element, namespace, dest):
-        candidates = \
-            list(root_element.findall(".//{%s}Benchmark" % (namespace)))
-        if root_element.tag == "{%s}Benchmark" % (namespace):
-            candidates.append(root_element)
-
-        for elem in candidates:
-            id_ = elem.get("id")
-            if id_ is None:
-                continue
-
-            title = "<unknown>"
-            for element in elem.findall("{%s}title" % (namespace)):
-                title = element.text
-                break
-
-            dest[id_] = title
-
-    input_root = input_tree.getroot()
-
-    scrape_benchmarks(
-        input_root, XCCDF11_NS, ret
-    )
-    scrape_benchmarks(
-        input_root, XCCDF12_NS, ret
-    )
-
-    return ret
-
-
-def get_profile_choices_for_input(input_tree, benchmark_id, tailoring_tree):
-    """Returns a dictionary that maps profile_ids to their respective titles.
-    """
-
-    # Ideally oscap would have a command line to do this, but as of now it
-    # doesn't so we have to implement it ourselves. Importing openscap Python
-    # bindings is nasty and overkill for this.
-
-    ret = {}
-
-    def scrape_profiles(root_element, namespace, dest):
-        for benchmark in root_element.findall(
-            ".//{%s}Benchmark" % (namespace)
-        ):
-            if benchmark.get("id") != benchmark_id:
-                continue
-
-            for elem in benchmark.findall(".//{%s}Profile" % (namespace)):
-                id_ = elem.get("id")
-                if id_ is None:
-                    continue
-
-                title = "<unknown>"
-                for element in elem.findall("{%s}title" % (namespace)):
-                    title = element.text
-                    break
-
-                dest[id_] = title
-
-    input_root = input_tree.getroot()
-
-    scrape_profiles(
-        input_root, XCCDF11_NS, ret
-    )
-    scrape_profiles(
-        input_root, XCCDF12_NS, ret
-    )
-
-    if tailoring_tree is not None:
-        tailoring_root = tailoring_tree.getroot()
-
-        scrape_profiles(
-            tailoring_root, XCCDF11_NS, ret
-        )
-        scrape_profiles(
-            tailoring_root, XCCDF12_NS, ret
-        )
-
-    return ret
-
-
-def get_profile_short_id(long_id):
-    """If given profile ID is the XCCDF 1.2 long ID this function shortens it
-    """
-
-    if re.search(PROFILE_ID_PREFIX, long_id):
-        return long_id[re.search(PROFILE_ID_PREFIX, long_id).end():]
-
-    return long_id
 
 
 def subprocess_check_output(*popenargs, **kwargs):
@@ -229,7 +133,7 @@ def main():
         path_base = path_base[:-6]
 
     input_tree = ElementTree.parse(input_path)
-    benchmarks = get_benchmark_ids_titles_for_input(input_tree)
+    benchmarks = xccdf_utils.get_benchmark_ids_titles_for_input(input_tree)
     if len(benchmarks) == 0:
         raise RuntimeError(
             "Expected input file '%s' to contain at least 1 xccdf:Benchmark. "
@@ -242,7 +146,9 @@ def main():
     # benchmarks. This needs to be removed once
     # https://github.com/OpenSCAP/openscap/issues/722 is fixed
     for benchmark_id in benchmarks.keys()[:1]:
-        profiles = get_profile_choices_for_input(input_tree, benchmark_id, None)
+        profiles = xccdf_utils.get_profile_choices_for_input(
+            input_tree, benchmark_id, None
+        )
         # add the default profile
         profiles[""] = "(default)"
 
@@ -276,7 +182,7 @@ def main():
 
     for benchmark_id, profile_id, profile_title in benchmark_profile_pairs:
         skip = False
-        for blacklisted_id in PROFILE_ID_BLACKLIST:
+        for blacklisted_id in xccdf_utils.PROFILE_ID_BLACKLIST:
             if profile_id.endswith(blacklisted_id):
                 skip = True
                 break
@@ -301,13 +207,13 @@ def main():
             role_filename = \
                 "%s-role-%s.%s" % \
                 (path_base,
-                 get_profile_short_id(profile_id_for_path),
+                 xccdf_utils.get_profile_short_id(profile_id_for_path),
                  args.extension)
         else:
             role_filename = \
                 "%s-%s-role-%s.%s" % \
                 (path_base, benchmark_id_for_path,
-                 get_profile_short_id(profile_id_for_path),
+                 xccdf_utils.get_profile_short_id(profile_id_for_path),
                  args.extension)
         role_path = os.path.join(output_dir, role_filename)
 
