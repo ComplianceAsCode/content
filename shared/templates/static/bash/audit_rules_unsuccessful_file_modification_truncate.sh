1ce7cfa887040c77ac8b899f6bdd52ebc0b7a9ba
Frank J Cameron (CAM1244) <cameron@ctc.com>
2017-06-02 16:02:15 -0400

    Fix typo EACCESS -> EACCES, EPRM -> EPERM in audit rules

--- shared/templates/static/bash/audit_rules_unsuccessful_file_modification_truncate.sh
+++ shared/templates/static/bash/audit_rules_unsuccessful_file_modification_truncate.sh
@@ -9,9 +9,9 @@
 
 for ARCH in "${RULE_ARCHS[@]}"
 do
-	PATTERN="-a always,exit -F arch=$ARCH -S truncate -F exit=-EACCESS.*"
+	PATTERN="-a always,exit -F arch=$ARCH -S truncate -F exit=-EACCES.*"
 	GROUP="access"
-	FULL_RULE="-a always,exit -F arch=$ARCH -S truncate -F exit=-EACCESS -F auid>=1000 -F auid!=4294967295 -F key=access"
+	FULL_RULE="-a always,exit -F arch=$ARCH -S truncate -F exit=-EACCES -F auid>=1000 -F auid!=4294967295 -F key=access"
 	# Perform the remediation for both possible tools: 'auditctl' and 'augenrules'
 	fix_audit_syscall_rule "auditctl" "$PATTERN" "$GROUP" "$ARCH" "$FULL_RULE"
 	fix_audit_syscall_rule "augenrules" "$PATTERN" "$GROUP" "$ARCH" "$FULL_RULE"
@@ -21,7 +21,7 @@ for ARCH in "${RULE_ARCHS[@]}"
 do
         PATTERN="-a always,exit -F arch=$ARCH -S truncate -F exit=-EPERM.*" 
         GROUP="access"
-        FULL_RULE="-a always,exit -F arch=$ARCH -S truncate -F exit=-EPRM -F auid>=1000 -F auid!=4294967295 -F key=access"
+        FULL_RULE="-a always,exit -F arch=$ARCH -S truncate -F exit=-EPERM -F auid>=1000 -F auid!=4294967295 -F key=access"
         # Perform the remediation for both possible tools: 'auditctl' and 'augenrules'
         fix_audit_syscall_rule "auditctl" "$PATTERN" "$GROUP" "$ARCH" "$FULL_RULE"
         fix_audit_syscall_rule "augenrules" "$PATTERN" "$GROUP" "$ARCH" "$FULL_RULE"
