0fe24781a265a6366a62cdc62ead45a883bea1fd
Martin Preisler <mpreisle@redhat.com>
2017-06-15 13:42:28 -0400

    Make the aide_periodic_cron_checking bash remediation idempotent

--- shared/templates/static/bash/aide_periodic_cron_checking.sh
+++ shared/templates/static/bash/aide_periodic_cron_checking.sh
@@ -1,2 +1,4 @@
 # platform = multi_platform_rhel
-echo "05 4 * * * root /usr/sbin/aide --check" >> /etc/crontab
+if ! grep -q "/usr/sbin/aide --check"; then
+    echo "05 4 * * * root /usr/sbin/aide --check" >> /etc/crontab
+fi
