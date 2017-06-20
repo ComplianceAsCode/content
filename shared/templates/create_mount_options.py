95f8a463f46b83a11dec2684b481f426fe535aaf
Gabe <redhatrises@gmail.com>
2017-06-20 13:56:05 -0600

    Remove multi-mount option capabilities in mount templates

--- shared/templates/create_mount_options.py
+++ shared/templates/create_mount_options.py
@@ -12,19 +12,9 @@ from template_common import FilesGenerator, UnknownTargetError
 class MountOptionsGenerator(FilesGenerator):
 
     def generate(self, target, path_info):
-        # the csv file contains lines that match the following layout:
-        #    mount_point,option,[option]+
-        # first col is the mount point, others are options, at least one
-        mount_infos = list()
-        mount_infos = path_info
-        mount_point = mount_infos[0]
-
-        # build a string out of the path that is suitable for use in id tags
-        # example:	/var/log --> _var_log
+        mount_point, option = path_info
         point_id = re.sub('[-\./]', '_', mount_point)
-        for option in mount_infos[1:]:
-            option_id = re.sub('[-\./ ]', '_', option)
-
+        if mount_point:
             if target == "ansible":
                 self.file_from_template(
                     "./template_ANSIBLE_mount_options",
@@ -32,7 +22,7 @@ class MountOptionsGenerator(FilesGenerator):
                         "MOUNTPOINT":       mount_point,
                         "MOUNTOPTION":       re.sub(' ', ',', option),
                     },
-                    "./ansible/mount_option{0}.yml", point_id + '_' + option_id
+                    "./ansible/mount_option{0}.yml", point_id + '_' + option
                 )
 
             elif target == "anaconda":
@@ -42,19 +32,14 @@ class MountOptionsGenerator(FilesGenerator):
                         "MOUNTPOINT":       mount_point,
                         "MOUNTOPTION":       re.sub(' ', ',', option),
                     },
-                    "./anaconda/mount_option{0}.anaconda", point_id + '_' + option_id
+                    "./anaconda/mount_option{0}.anaconda", point_id + '_' + option
                 )
 
             elif target == "oval":
-                # we are ready to create the check
-                # open the template and perform the conversions
-                option_str = ""
-                state_str = ""
-                #for option in mount_options.split():
-                state_str = "    <linux:state state_ref=\"state" + point_id + "_" +  option_id + "\" />\n"
-                option_str = "  <linux:partition_state id=\"state" + point_id + "_" + option_id + "\" version=\"1\">\n\
-        <linux:mount_options datatype=\"string\" entity_check=\"at least one\" operation=\"equals\">" + option + "</linux:mount_options>\n\
-      </linux:partition_state>\n"
+                state_str = "    <linux:state state_ref=\"state" + point_id + "_" +  option + "\" />\n"
+                option_str = "  <linux:partition_state id=\"state" + point_id + "_" + option + "\" version=\"1\">\n\
+    <linux:mount_options datatype=\"string\" entity_check=\"at least one\" operation=\"equals\">" + option + "</linux:mount_options>\n\
+  </linux:partition_state>\n"
 
                 self.file_from_template(
                     "./template_OVAL_mount_options",
@@ -64,9 +49,9 @@ class MountOptionsGenerator(FilesGenerator):
                         "OPTIONLIST":       option,
                         "MOUNTSTATES":	state_str,
                         "POINTID":     point_id,
-                        "OPTIONID":      option_id,
+                        "OPTIONID":      option,
                     },
-                    "./oval/mount_option{0}.xml", point_id + "_" + option_id
+                    "./oval/mount_option{0}.xml", point_id + "_" + option
                 )
             else:
                 raise UnknownTargetError(target)
