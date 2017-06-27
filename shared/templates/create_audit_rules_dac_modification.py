67a208f7191278712667e3ded138af8dd1ba1eb0
Raphael Sanchez Prudencio <rsprudencio@redhat.com>
2017-06-27 13:57:32 +0200

    Simplified audit_rules_dac_modification template

--- shared/templates/create_audit_rules_dac_modification.py
+++ shared/templates/create_audit_rules_dac_modification.py
@@ -17,21 +17,17 @@ class AuditRulesDacModificationGenerator(FilesGenerator):
 
     def generate(self, target, audit_info):
         # the csv file contains lines that match the following layout:
-        #    attr,pattern_32,pattern_64
+        #    attr
 
         if target == "oval":
             # we are ready to create the check
             # open the template and perform the conversions
             attr = audit_info[0]
-            pattern_32 = audit_info[1]
-            pattern_64 = audit_info[2]
 
             self.file_from_template(
                 "./template_OVAL_audit_rules_dac_modification",
                 {
                     "%ATTR%":           attr,
-                    "%PATTERN_32%":     pattern_32,
-                    "%PATTERN_64%":     pattern_64,
                 },
                 "./oval/audit_rules_dac_modification_{0}.xml", attr
             )
@@ -40,7 +36,7 @@ class AuditRulesDacModificationGenerator(FilesGenerator):
 
     def csv_format(self):
         return("CSV should contains lines of the format: "
-               "attr,pattern_32,pattern_64")
+               "attr")
 
 if __name__ == "__main__":
     AuditRulesDacModificationGenerator().main()
