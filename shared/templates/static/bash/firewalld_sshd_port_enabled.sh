cceab3407e0bc64f28781db53a431681f30beabc
Martin Preisler <mpreisle@redhat.com>
2017-07-11 18:22:09 -0400

    Added more flags to sshd_port_enabled bash and ansible fixes

--- shared/templates/static/bash/firewalld_sshd_port_enabled.sh
+++ shared/templates/static/bash/firewalld_sshd_port_enabled.sh
@@ -1,5 +1,8 @@
 # platform = Red Hat Enterprise Linux 7, multi_platform_fedora
 # reboot = false
+# complexity = low
+# strategy = configure
+# disruption = low
 
 . /usr/share/scap-security-guide/remediation_functions
 populate sshd_listening_port
