2b9f0d4bd9606bcf7a399f8bb4bd18c840e84bb4
Martin Preisler <mpreisle@redhat.com>
2017-07-14 15:16:35 -0400

    Follow the %VAR% convention in package_removed templates

--- shared/templates/create_package_removed.py
+++ shared/templates/create_package_removed.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python2
-
 #
 # create_package_removed.py
 #   automatically generate checks for removed packages
@@ -12,38 +10,37 @@ class PackageRemovedGenerator(FilesGenerator):
         pkgname = package_info[0]
         if pkgname:
             if target == "oval":
-
                 self.file_from_template(
                     "./template_OVAL_package_removed",
-                    { "PKGNAME" : pkgname},
+                    {"%PKGNAME%": pkgname},
                     "./oval/package_{0}_removed.xml", pkgname
                 )
 
             elif target == "bash":
                 self.file_from_template(
                     "./template_BASH_package_removed",
-                    { "PKGNAME" : pkgname },
+                    {"%PKGNAME%": pkgname},
                     "./bash/package_{0}_removed.sh", pkgname
                 )
 
             elif target == "ansible":
                 self.file_from_template(
                     "./template_ANSIBLE_package_removed",
-                    { "PKGNAME" : pkgname },
+                    {"%PKGNAME%": pkgname},
                     "./ansible/package_{0}_removed.yml", pkgname
                 )
 
             elif target == "anaconda":
                 self.file_from_template(
                     "./template_ANACONDA_package_removed",
-                    { "PKGNAME" : pkgname },
+                    {"%PKGNAME%": pkgname},
                     "./anaconda/package_{0}_removed.anaconda", pkgname
                 )
 
             elif target == "puppet":
                 self.file_from_template(
                     "./template_PUPPET_package_removed",
-                    { "PKGNAME" : pkgname },
+                    {"%PKGNAME%": pkgname},
                     "./puppet/package_{0}_removed.pp", pkgname
                 )
 
