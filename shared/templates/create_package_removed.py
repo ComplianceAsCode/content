f17289b443a5d6540e460cc21dca7c3e976f2a8d
Martin Preisler <mpreisle@redhat.com>
2017-07-14 15:17:35 -0400

    Make create_package_removed less matryoshka doll-like

--- shared/templates/create_package_removed.py
+++ shared/templates/create_package_removed.py
@@ -8,46 +8,47 @@ from template_common import FilesGenerator, UnknownTargetError
 class PackageRemovedGenerator(FilesGenerator):
     def generate(self, target, package_info):
         pkgname = package_info[0]
-        if pkgname:
-            if target == "oval":
-                self.file_from_template(
-                    "./template_OVAL_package_removed",
-                    {"%PKGNAME%": pkgname},
-                    "./oval/package_{0}_removed.xml", pkgname
-                )
+        if not pkgname:
+            raise RuntimeError(
+                "ERROR: input violation: the package name must be defined")
 
-            elif target == "bash":
-                self.file_from_template(
-                    "./template_BASH_package_removed",
-                    {"%PKGNAME%": pkgname},
-                    "./bash/package_{0}_removed.sh", pkgname
-                )
+        if target == "oval":
+            self.file_from_template(
+                "./template_OVAL_package_removed",
+                {"%PKGNAME%": pkgname},
+                "./oval/package_{0}_removed.xml", pkgname
+            )
 
-            elif target == "ansible":
-                self.file_from_template(
-                    "./template_ANSIBLE_package_removed",
-                    {"%PKGNAME%": pkgname},
-                    "./ansible/package_{0}_removed.yml", pkgname
-                )
+        elif target == "bash":
+            self.file_from_template(
+                "./template_BASH_package_removed",
+                {"%PKGNAME%": pkgname},
+                "./bash/package_{0}_removed.sh", pkgname
+            )
 
-            elif target == "anaconda":
-                self.file_from_template(
-                    "./template_ANACONDA_package_removed",
-                    {"%PKGNAME%": pkgname},
-                    "./anaconda/package_{0}_removed.anaconda", pkgname
-                )
+        elif target == "ansible":
+            self.file_from_template(
+                "./template_ANSIBLE_package_removed",
+                {"%PKGNAME%": pkgname},
+                "./ansible/package_{0}_removed.yml", pkgname
+            )
 
-            elif target == "puppet":
-                self.file_from_template(
-                    "./template_PUPPET_package_removed",
-                    {"%PKGNAME%": pkgname},
-                    "./puppet/package_{0}_removed.pp", pkgname
-                )
+        elif target == "anaconda":
+            self.file_from_template(
+                "./template_ANACONDA_package_removed",
+                {"%PKGNAME%": pkgname},
+                "./anaconda/package_{0}_removed.anaconda", pkgname
+            )
 
-            else:
-                raise UnknownTargetError(target)
+        elif target == "puppet":
+            self.file_from_template(
+                "./template_PUPPET_package_removed",
+                {"%PKGNAME%": pkgname},
+                "./puppet/package_{0}_removed.pp", pkgname
+            )
+
         else:
-            print "ERROR: input violation: the package name must be defined"
+            raise UnknownTargetError(target)
 
     def csv_format(self):
         return("CSV should contains lines of the format: " +
