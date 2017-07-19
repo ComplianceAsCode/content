ee135818cd7ccf5256c13f10b3626082d10af935
root <3958336+OnceUponALoop@users.noreply.github.com>
2017-07-19 13:10:37 -0500

    Verify that TMOUT is set to readonly

--- RHEL/7/templates/static/bash/accounts_tmout.sh
+++ RHEL/7/templates/static/bash/accounts_tmout.sh
@@ -8,3 +8,12 @@ else
         echo -e "\n# Set TMOUT to $var_accounts_tmout per security requirements" >> /etc/profile
         echo "TMOUT=$var_accounts_tmout" >> /etc/profile
 fi
+
+# Check if 'readonly TMOUT' is not set in /etc/profile
+# Add it if it's missing
+if ! grep --silent '^\s*readonly\s\+TMOUT\s*$' /etc/profile; then
+    echo -e "\n# Set TMOUT to readonly per security requirements" >> /etc/profile
+    echo "readonly TMOUT" >> /etc/profile
+fi
+
+
