78b148137ba7d0b6a766783020c078ccc6a9d18b
Molly Jo Bault <Molly.Jo.Bault@ballardtech.com>
2017-07-31 16:40:15 -0700

    Checking to see if /etc/ssh/*.pub files exist before changing

--- shared/templates/static/bash/file_permissions_sshd_pub_key.sh
+++ shared/templates/static/bash/file_permissions_sshd_pub_key.sh
@@ -1,3 +1,8 @@
 # platform = multi_platform_rhel
 
-chmod 0644 /etc/ssh/*.pub
+#check if files exist before attempting 
+#to change them
+if ls /etc/ssh/*.pub &> /dev/null
+then
+    chmod 0644 /etc/ssh/*.pub
+fi
