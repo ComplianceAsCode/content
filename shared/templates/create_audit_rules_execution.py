4980f4d1be10ee3a864956cfc5d168068ece2b2f
Wesley Ceraso Prudencio <wcerasop@redhat.com>
2017-08-04 12:42:10 +0200

    Creates templates for audit_rules_execution OVAL checks, BASH and ANSIBLE remediations
    Signed-off-by: Wesley Ceraso Prudencio <wcerasop@redhat.com>

--- shared/templates/create_audit_rules_execution.py
+++ shared/templates/create_audit_rules_execution.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python2
 
 #
-# create_audit_rules_privileged_commands.py
+# create_audit_rules_execution.py
 #        generate template-based checks for privileged commands rules
+# WARNING: this class uses the templates from audit_rules_privileged_commands as they are pretty much the same
 
 
 from template_common import FilesGenerator, UnknownTargetError
@@ -11,20 +12,20 @@ import os
 import re
 
 
-class AuditRulesPrivilegedCommandsGenerator(FilesGenerator):
+class AuditRulesExecutionGenerator(FilesGenerator):
     def generate(self, target, args):
         path = args[0]
-        name = re.sub('[-\./]', '_', os.path.basename(path))
+        name = os.path.basename(path).replace('[-\./]', '_')
         if target == "oval":
             self.file_from_template(
                 "./template_OVAL_audit_rules_privileged_commands",
                 {
-                    "%ID%": "audit_rules_privileged_commands_" + name,
-                    "%TITLE%": "Ensure auditd Collects Information on the Use of Privileged Commands - " + name,
+                    "%ID%": "audit_rules_execution_" + name,
+                    "%TITLE%": "Record Any Attempts to Run " + name,
                     "%NAME%":	name,
                     "%PATH%":	path.replace("/", "\\/")
                 },
-                "./oval/audit_rules_privileged_commands_{0}.xml", name
+                "./oval/audit_rules_execution_{0}.xml", name
             )
 
         elif target == "bash":
@@ -33,7 +34,7 @@ class AuditRulesPrivilegedCommandsGenerator(FilesGenerator):
                 {
                     "%PATH%":	path
                 },
-                "./bash/audit_rules_privileged_commands_{0}.sh", name
+                "./bash/audit_rules_execution_{0}.sh", name
             )
 
         elif target == "ansible":
@@ -43,7 +44,7 @@ class AuditRulesPrivilegedCommandsGenerator(FilesGenerator):
                     "%NAME%":	name,
                     "%PATH%":	path
                 },
-                "./ansible/audit_rules_privileged_commands_{0}.yml", name
+                "./ansible/audit_rules_execution_{0}.yml", name
             )
 
         else:
@@ -52,3 +53,4 @@ class AuditRulesPrivilegedCommandsGenerator(FilesGenerator):
     def csv_format(self):
         return("CSV should contains lines of the format: " +
                "PATH")
+               
