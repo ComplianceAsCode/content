1db26d5aec3697a11855724caabb3299ea7734f6
Watson Sato <wsato@redhat.com>
2017-08-21 08:59:17 +0200

    Use ElementTree parse and fix PEP8 issues

--- shared/utils/filter-machine-rules.py
+++ shared/utils/filter-machine-rules.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python2
 
 """
-Takes shorthand file and removes selects not applicable for containers from container profiles.
+Takes shorthand file and removes selects not applicable for containers from
+container profiles.
 """
-
 
 try:
     from xml.etree import cElementTree as ElementTree
@@ -12,17 +12,12 @@ except ImportError:
 
 import sys
 
-def parse_xml_file(xmlfile):
-    with open(xmlfile, 'r') as xml_file:
-        files_tring = xml_file.read()
-        tree = ElementTree.fromstring(files_tring)
-    return tree
 
 def main():
     xccdf_file = sys.argv[1]
     xccdf_out_file = sys.argv[2]
 
-    xccdf_tree = parse_xml_file(xccdf_file)
+    xccdf_tree = ElementTree.parse(xccdf_file)
 
     indexed_rules = {}
     for rule in xccdf_tree.findall(".//Rule"):
@@ -37,7 +32,8 @@ def main():
     for profile in all_profiles:
         profile_id = profile.get('id')
         if profile_id is None:
-            raise RuntimeError("Profile without id attribute, there is something very wrong!")
+            raise RuntimeError("Profile without id attribute, "
+                               "there is something very wrong!")
 
         # Only filter Rules in profiles for containers
         if not profile_id.endswith("-container"):
@@ -47,20 +43,21 @@ def main():
         for select in all_selects:
             select_idref = select.get('idref')
             if select_idref is None:
-                raise RuntimeError("Profile without id attribute, there is something very wrong!")
+                raise RuntimeError("Select without idref attribute, "
+                                   "there is something very wrong!")
 
             rule = indexed_rules.get(select_idref)
             if rule is None:
                 print("Dangling reference! Profile \"%s\" selects Rule \"%s\" "
-                        "which cannot be found in benchmark!"
-                        % (profile_id, select_idref))
+                      "which cannot be found in benchmark!"
+                      % (profile_id, select_idref))
                 continue
 
             x = rule.find('./machine')
             if x is not None:
                 profile.remove(select)
 
-    ElementTree.ElementTree(xccdf_tree).write(xccdf_out_file)
+    xccdf_tree.write(xccdf_out_file)
 
 
 if __name__ == "__main__":
