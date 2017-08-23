f4cc5b08cc7bbd2d8feb52f5664f42870e2920ea
Thomas Sjögren <konstruktoid@users.noreply.github.com>
2017-08-23 15:37:59 +0200

    CtrlAltDelBurstAction=none
    Signed-off-by: Thomas Sjögren <konstruktoid@users.noreply.github.com>

--- shared/templates/static/bash/disable_ctrlaltdel_reboot.sh
+++ shared/templates/static/bash/disable_ctrlaltdel_reboot.sh
@@ -1,2 +1,6 @@
 # platform = Red Hat Enterprise Linux 7, multi_platform_fedora
+
+SYSTEMCONF='/etc/systemd/system.conf'
+
 ln -sf /dev/null /etc/systemd/system/ctrl-alt-del.target
+sed -i 's/^#CtrlAltDelBurstAction=.*/CtrlAltDelBurstAction=none/' "$SYSTEMCONF"
