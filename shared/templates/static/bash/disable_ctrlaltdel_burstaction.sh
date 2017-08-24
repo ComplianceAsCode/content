1e8eb48078d3f588c11a01d0a438d2f15c64b949
Thomas Sjögren <konstruktoid@users.noreply.github.com>
2017-08-24 09:16:21 +0200

    rename _burstaction, update instructions
    Signed-off-by: Thomas Sjögren <konstruktoid@users.noreply.github.com>

--- shared/templates/static/bash/disable_ctrlaltdel_burstaction.sh
+++ shared/templates/static/bash/disable_ctrlaltdel_burstaction.sh
@@ -1,3 +1,5 @@
 # platform = Red Hat Enterprise Linux 7, multi_platform_fedora
 
-ln -sf /dev/null /etc/systemd/system/ctrl-alt-del.target
+SYSTEMCONF='/etc/systemd/system.conf'
+
+sed -i 's/^#CtrlAltDelBurstAction=.*/CtrlAltDelBurstAction=none/' "$SYSTEMCONF"
--- shared/templates/static/bash/disable_ctrlaltdel_ctrlaltdelburstaction.sh
+++
@@ -1,5 +0,0 @@
-# platform = Red Hat Enterprise Linux 7, multi_platform_fedora
-
-SYSTEMCONF='/etc/systemd/system.conf'
-
-sed -i 's/^#CtrlAltDelBurstAction=.*/CtrlAltDelBurstAction=none/' "$SYSTEMCONF"
