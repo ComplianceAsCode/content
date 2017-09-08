44721c7ac9990d8e9001458ddc53d0eae93af706
Watson Sato <wsato@redhat.com>
2017-09-08 10:49:45 +0200

    Allow firewalld SSH enabled zone to be customized
    Add iteractively tailorable value to specify which firewalld zone will
    have SSH service enabled by remediation.

--- shared/templates/static/bash/firewalld_sshd_port_enabled.sh
+++ shared/templates/static/bash/firewalld_sshd_port_enabled.sh
@@ -8,10 +8,13 @@
 
 package_command install firewalld
 populate sshd_listening_port
+populate firewalld_sshd_zone
 
 if [ $sshd_listening_port -ne 22 ] ; then
   firewall-cmd --permanent --service=ssh --add-port=$sshd_listening_port/tcp
   firewall-cmd --permanent --service=ssh --remove-port=22/tcp
 fi
 
-firewall-cmd --permanent --zone=public --add-service=ssh
+# Return code is 0 even if zone already enables SSH service,
+# so we just try to add SSH to the zone fearlessly
+firewall-cmd --permanent --zone=$firewalld_sshd_zone --add-service=ssh
