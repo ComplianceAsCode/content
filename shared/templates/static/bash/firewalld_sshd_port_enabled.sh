3c1d44d6b67a6ec701f71fa24122e7946bff4c66
Watson Sato <wsato@redhat.com>
2017-09-08 11:18:38 +0200

    Be sure there is only one port in SSH service
    Be sure only port in SSH service is the one configured through Value
    sshd_listening_port.

--- shared/templates/static/bash/firewalld_sshd_port_enabled.sh
+++ shared/templates/static/bash/firewalld_sshd_port_enabled.sh
@@ -11,8 +11,13 @@ populate sshd_listening_port
 populate firewalld_sshd_zone
 
 if [ $sshd_listening_port -ne 22 ] ; then
+  # Remove all ports from SSH service
+  ssh_ports=$(firewall-cmd --permanent --service=ssh --get-ports)
+  for port in $ssh_ports; do
+      firewall-cmd --permanent --service=ssh --remove-port=$port
+  done
+
   firewall-cmd --permanent --service=ssh --add-port=$sshd_listening_port/tcp
-  firewall-cmd --permanent --service=ssh --remove-port=22/tcp
 fi
 
 # Return code is 0 even if zone already enables SSH service,
