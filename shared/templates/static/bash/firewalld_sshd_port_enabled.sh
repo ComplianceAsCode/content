b18235ff16ce9b16a38fddf80ea7628fc2ecbf22
Watson Sato <wsato@redhat.com>
2017-09-08 10:18:06 +0200

    Decouple SSH listening port and enabled zone fixes
    Decouple fix for listening port from fix of SSH enabled zone.
    They are not mutualy exclusive.
    
    Plus, do not add SSH service to current firewalld default zone, add it
    to public zone.

--- shared/templates/static/bash/firewalld_sshd_port_enabled.sh
+++ shared/templates/static/bash/firewalld_sshd_port_enabled.sh
@@ -10,7 +10,8 @@ package_command install firewalld
 populate sshd_listening_port
 
 if [ $sshd_listening_port -ne 22 ] ; then
-  firewall-cmd --permanent --add-port=$sshd_listening_port/tcp
-else
-  firewall-cmd --permanent --add-service=ssh
+  firewall-cmd --permanent --service=ssh --add-port=$sshd_listening_port/tcp
+  firewall-cmd --permanent --service=ssh --remove-port=22/tcp
 fi
+
+firewall-cmd --permanent --zone=public --add-service=ssh
