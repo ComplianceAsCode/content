b8650f70f2a684658e65e2036bd97fc741831a06
Watson Sato <wsato@redhat.com>
2017-09-08 11:25:03 +0200

    Check if there is NIC witch can do SSH
    Check if there is a ethernet NIC bounded to a SSH enabled zone.
    If not, add first ethernet NIC to the SSH enabled zone.

--- shared/templates/static/bash/firewalld_sshd_port_enabled.sh
+++ shared/templates/static/bash/firewalld_sshd_port_enabled.sh
@@ -23,3 +23,19 @@ fi
 # Return code is 0 even if zone already enables SSH service,
 # so we just try to add SSH to the zone fearlessly
 firewall-cmd --permanent --zone=$firewalld_sshd_zone --add-service=ssh
+
+# Check if any eth interface is bounded to the zone with SSH service enabled
+nic_bound=false
+eth_interface_list=$(ip link show up | cut -d ' ' -f2 | cut -d ':' -s -f1 | grep '^en')
+for interface in $eth_interface_list; do
+    zone_of_interface=$(firewall-cmd --get-zone-of-interface=$interface)
+    if [ "$zone_of_interface" == "$ssh_zone" ]; then
+        nic_bound=true
+        break;
+    fi
+done
+
+if [ nic_bound = false ];then
+    # Add first NIC to SSH enabled zone
+    firewall-cmd --zone=$firewalld_sshd_zone --add-interface=${eth_interface_list[0]}
+fi
