acab6ba0612e05c1b73b27e24bd2da5789a1d29a
Gabe <redhatrises@gmail.com>
2017-09-21 14:21:09 -0600

    Add realpath to testoval.py
    - This should fix the occasional cannot find content error message

--- shared/utils/testoval.py
+++ shared/utils/testoval.py
@@ -4,8 +4,9 @@ import sys
 import os
 
 # always use shared/testoval_module.py version
-script_directory = os.path.dirname(__file__)
+script_directory = os.path.dirname(os.path.realpath(__file__))
 SHARED_MODULE_PATH = os.path.join(script_directory, "..", "modules")
+
 sys.path.insert(0, SHARED_MODULE_PATH)
 import testoval_module
 
