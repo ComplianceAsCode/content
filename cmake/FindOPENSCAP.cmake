012f6cfa33b53aa19d0ff87f15505988ed4575b7
Gabe <redhatrises@gmail.com>
2017-10-10 15:09:27 -0600

    Move OSCAP_V_OUTPUT into new FindOPENSCAP script

--- cmake/FindOPENSCAP.cmake
+++ cmake/FindOPENSCAP.cmake
@@ -2,12 +2,12 @@ set(OPENSCAP_POSSIBLE_ROOT_DIRS
     "${OPENSCAP_ROOT_DIR}"
     "$ENV{OPENSCAP_ROOT_DIR}"
     "$ENV{ProgramFiles}"
-    "/usr/local/share"
-    "/usr/share/"
-    "/usr/local"
     "/usr"
     "/usr/bin"
     "/usr/sbin"
+    "/usr/local"
+    "/usr/share/"
+    "/usr/local/share"
     "/opt"
     "/opt/local"
 )
@@ -34,4 +34,15 @@ endif()
 if (NOT EXISTS "${OSCAP_EXECUTABLE}")
     MESSAGE(SEND_ERROR
             "ERROR: The OPENSCAP executable was not found. Please specify the OPENSCAP ROOT DIR with the OPENSCAP_ROOT_DIR environment variable.")
+endif()
+
+execute_process(
+    COMMAND "${OSCAP_EXECUTABLE}" --v
+    OUTPUT_VARIABLE OSCAP_V_OUTPUT
+)
+
+if("${OSCAP_V_OUTPUT}" MATCHES "^OpenSCAP command line tool \\(oscap\\) ([0-9\\.]+)")
+    set(OSCAP_VERSION "${CMAKE_MATCH_1}")
+else()
+    set(OSCAP_VERSION "unknown")
 endif()
