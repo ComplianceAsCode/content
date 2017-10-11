f0ffa1ec5140a196d93524ce6549b6f38cc65efb
Watson Sato <wsato@redhat.com>
2017-10-11 15:48:04 +0200

    Add tests for Rule sshd_allow_only_protocol2

---
+++ tests/data/group_services/group_ssh/group_ssh_server/rule_sshd_allow_only_protocol2/openssh-6.6-configured.pass.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+# profiles = xccdf_org.ssgproject.content_profile_stig-rhel7-disa
+
+# Test targeted to RHEL 7.4
+yum downgrade -y openssh-6.6.1p1 openssh-clients-6.6.1p1 openssh-server-6.6.1p1
+
+echo "Protocol 2" >> /etc/ssh/sshd_config
