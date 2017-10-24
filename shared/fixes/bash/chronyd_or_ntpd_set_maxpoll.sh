8c8c405254dc9fc8b6e96f50eed3dbfaf69f7048
Watson Sato <wsato@redhat.com>
2017-10-24 15:21:59 +0200

    Set correct maxpoll value for chrony and ntp
    Set correct maxpoll value for server entries in config file for ntp and
    chrony.

--- shared/fixes/bash/chronyd_or_ntpd_set_maxpoll.sh
+++ shared/fixes/bash/chronyd_or_ntpd_set_maxpoll.sh
@@ -8,6 +8,9 @@ else
     config_file="/etc/ntp.conf"
 fi
 
+# Set maxpoll values to var_time_service_set_maxpoll
+sed -i "s/^\(server.*maxpoll\) [0-9][0-9]*\(.*\)$/\1 $var_time_service_set_maxpoll \2/" "$config_file"
+
 # Add maxpoll to server entries without maxpoll
 grep -P "^server((?!maxpoll).)*$" $config_file | while read -r line ; do
         sed -i "s/$line/&1 maxpoll $var_time_service_set_maxpoll/" "$config_file"
