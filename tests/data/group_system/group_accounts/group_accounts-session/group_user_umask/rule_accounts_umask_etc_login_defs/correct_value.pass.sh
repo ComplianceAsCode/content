11879f5bcb6962db0790f46f8dd9008c9f1e0034
Watson Sato <wsato@redhat.com>
2017-12-12 12:17:30 +0100

    Add test for accounts_umask_etc_login_defs
    Adds tests for correct and wrong UMASK for OSPP-RHEL7 profile.

---
+++ tests/data/group_system/group_accounts/group_accounts-session/group_user_umask/rule_accounts_umask_etc_login_defs/correct_value.pass.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# profiles = xccdf_org.ssgproject.content_profile_ospp-rhel7
+
+if grep -q "^UMASK" /etc/login.defs; then
+	sed -i "s/^UMASK.*/UMASK 077/" /etc/login.defs
+else
+	echo "UMASK 077" >> /etc/login.defs
+fi
