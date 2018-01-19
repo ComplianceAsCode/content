27363bc92905f8b7a5271480c64051869f2565d8
Nathan Peters <petna01@ca.com>
2018-01-18 17:18:07 -0800

    fixed syntax issue with sed expression in auditd_data_retention_space_left.sh

--- shared/fixes/bash/auditd_data_retention_space_left.sh
+++ shared/fixes/bash/auditd_data_retention_space_left.sh
@@ -3,7 +3,7 @@
 populate var_auditd_space_left
 
 grep -q ^space_left /etc/audit/auditd.conf && \
-  sed -i "s/space_left.*/space_left = $var_auditd_space_left/g" /etc/audit/auditd.conf
+  sed -i "s/^space_left[[:space:]]*=.*/space_left = $var_auditd_space_left/g" /etc/audit/auditd.conf
 if ! [ $? -eq 0 ]; then
     echo "space_left = $var_auditd_space_left" >> /etc/audit/auditd.conf
 fi
