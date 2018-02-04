72d53577cf1b14417478fb158acc382e185642d5
Olivier Bonhomme <ptitoliv@ptitoliv.net>
2018-02-04 02:31:09 +0100

    Merge the two banners into a common one used by the both rules.
    Regexp had to be updated and also the sed substitutions in remediations scripts
    in order to handle correctly that new regexp.

--- rhel7/fixes/bash/dconf_gnome_login_banner_text.sh
+++ rhel7/fixes/bash/dconf_gnome_login_banner_text.sh
@@ -1,9 +1,8 @@
 # platform = Red Hat Enterprise Linux 7
-#. /usr/share/scap-security-guide/remediation_functions
-populate dconf_banner_text
-# There was a regular-expression matching various banners, needs to be expanded
-# When there are multiple banners in dconf_banner_text, the first banner should be the one for RHEL7
-expanded=$(echo "$dconf_banner_text" | sed 's/\^(\(.*\)|.*$/\1/g;s/\[\\s\\n\][+*]/ /g;s/\\//g;s/nn\*-/\\n\\n-/g')
+. /usr/share/scap-security-guide/remediation_functions
+populate login_banner_text
+
+expanded=$(echo "$login_banner_text" | sed 's/\^(\(.*\)|.*$/\1/g;s/\[\\s\\n\][+*]/ /g;s/\\//g;s/(n)\*/\\n/g')
 
 cat <<EOF >/etc/dconf/db/gdm.d/00-security-settings
 [org/gnome/login-screen]
