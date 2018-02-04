0174ed76f8ff65efeb2cecd856ff619a6f505ee3
Olivier Bonhomme <ptitoliv@ptitoliv.net>
2018-02-04 02:31:09 +0100

    Merge the two banners into a common one used by the both rules.
    Regexp had to be updated and also the sed substitutions in remediations scripts
    in order to handle correctly that new regexp.

--- rhel7/fixes/bash/banner_etc_issue.sh
+++ rhel7/fixes/bash/banner_etc_issue.sh
@@ -4,7 +4,7 @@ populate login_banner_text
 
 # There was a regular-expression matching various banners, needs to be expanded
 # When there are multiple banners in login_banner_text, the first banner should be the one for RHEL7
-expanded=$(echo "$login_banner_text" | sed 's/\^(\(.*\)|.*$/\1/g;s/\[\\s\\n\][+*]/ /g;s/\\//g;s/[^-]- /\n\n-/g')
+expanded=$(echo "$login_banner_text" | sed 's/\^(\(.*\)|.*$/\1/g;s/\[\\s\\n\][+*]/ /g;s/\\//g;s/[^-]- /\n\n-/g;s/(n)\**//g')
 formatted=$(echo "$expanded" | fold -sw 80)
 
 cat <<EOF >/etc/issue
