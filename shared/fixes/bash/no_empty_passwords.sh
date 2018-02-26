64412e0f92010e243a6c3f66bfaf8ed02ef6e364
Ilya Okomin <ilya.okomin@oracle.com>
2018-02-26 19:36:08 +0300

    Enable additional rules in ol7 standard profile
    Added more rules to ol7 standard profile. Updated
    corresponding xccdf content and affected platform.

--- shared/fixes/bash/no_empty_passwords.sh
+++ shared/fixes/bash/no_empty_passwords.sh
@@ -1,3 +1,3 @@
-# platform = multi_platform_rhel, multi_platform_fedora
+# platform = multi_platform_rhel, multi_platform_fedora, multi_platform_ol
 sed --follow-symlinks -i 's/\<nullok\>//g' /etc/pam.d/system-auth
 sed --follow-symlinks -i 's/\<nullok\>//g' /etc/pam.d/password-auth
