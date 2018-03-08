423ab0ea1a34d9021d115a7e932f04983f783319
Olivier Bonhomme <ptitoliv@ptitoliv.net>
2018-03-08 01:13:24 +0100

    Improvments for banner checks and remediations (/etc/issue and GDM)
    This commit optimizes the regular expressions for the different available
    banners and also the sed transformations used in order to produce the
    final configuration files for /etc/issue and GDM banner.
    
    The transformations are written in order to handle the specific cases
    which are transforming \n and ' characters. A specific regular
    expression has been writeen for each case (/etc/issue and GDM)

--- rhel7/fixes/bash/dconf_gnome_login_banner_text.sh
+++ rhel7/fixes/bash/dconf_gnome_login_banner_text.sh
@@ -2,7 +2,7 @@
 . /usr/share/scap-security-guide/remediation_functions
 populate login_banner_text
 
-expanded=$(echo "$login_banner_text" | sed 's/\^(\(.*\)|.*$/\1/g;s/\[\\s\\n\][+*]/ /g;s/\\//g;s/(n)\*/\\n/g')
+expanded=$(echo "$login_banner_text" | sed 's/(\\\\\x27)\*/\\\x27/g;s/(\\\x27)\*//g;s/(\\\\\x27)/tamere/g;s/\^(\(.*\)|.*$/\1/g;s/\[\\s\\n\][+*]/ /g;s/\\//g;s/(n)\*/\\n/g;s/\x27/\\\x27/g;')
 
 cat <<EOF >/etc/dconf/db/gdm.d/00-security-settings
 [org/gnome/login-screen]
