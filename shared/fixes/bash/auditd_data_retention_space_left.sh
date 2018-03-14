269ab3e0422b97e6c3593850e4fe93020436047b
Watson Sato <wsato@redhat.com>
2018-03-14 16:30:20 +0100

    Fix append scenario in remediation for audit_data_retention_space_left
    Remediation for auditd_data_retention_space_left could not add
    configuration line when it was missing.
    
    grep command would return 0, sed command would not find the
    configuration line to replace and return 0.
    Subsequent if would consume $? equal 0 and not append config.
    
    Use of function replace_or_append is not easy here, [[:space:]] is not
    handled correctly.

--- shared/fixes/bash/auditd_data_retention_space_left.sh
+++ shared/fixes/bash/auditd_data_retention_space_left.sh
@@ -2,7 +2,7 @@
 . /usr/share/scap-security-guide/remediation_functions
 populate var_auditd_space_left
 
-grep -q ^space_left /etc/audit/auditd.conf && \
+grep -q "^space_left[[:space:]]*=" /etc/audit/auditd.conf && \
   sed -i "s/^space_left[[:space:]]*=.*/space_left = $var_auditd_space_left/g" /etc/audit/auditd.conf
 if ! [ $? -eq 0 ]; then
     echo "space_left = $var_auditd_space_left" >> /etc/audit/auditd.conf
