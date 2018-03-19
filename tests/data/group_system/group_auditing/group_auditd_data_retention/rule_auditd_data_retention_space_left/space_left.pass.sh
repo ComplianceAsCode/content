dbaa817ed675a3e182b1990a34bf9025accc9dfd
Watson Sato <wsato@redhat.com>
2018-03-19 18:20:05 +0100

    Make regexes for space_left tests consistent

--- tests/data/group_system/group_auditing/group_auditd_data_retention/rule_auditd_data_retention_space_left/space_left.pass.sh
+++ tests/data/group_system/group_auditing/group_auditd_data_retention/rule_auditd_data_retention_space_left/space_left.pass.sh
@@ -2,8 +2,9 @@
 # profiles = xccdf_org.ssgproject.content_profile_stig-rhel7-disa
 # remediation = bash
 
-if grep -q "^space_left[[:space:]]*= " /etc/audit/auditd.conf; then
-	sed -i "s/^space_left = .*$/space_left = 100/" /etc/audit/auditd.conf
+SPACE_LEFT_REGEX="^space_left[[:space:]]*=.*$"
+if grep -q "$SPACE_LEFT_REGEX" /etc/audit/auditd.conf; then
+	sed -i "s/$SPACE_LEFT_REGEX/space_left = 100/" /etc/audit/auditd.conf
 else
 	echo "space_left = 100" >> /etc/audit/auditd.conf
 fi
--- tests/data/group_system/group_auditing/group_auditd_data_retention/rule_auditd_data_retention_space_left/space_left_not_enough.fail.sh
+++ tests/data/group_system/group_auditing/group_auditd_data_retention/rule_auditd_data_retention_space_left/space_left_not_enough.fail.sh
@@ -2,4 +2,4 @@
 # profiles = xccdf_org.ssgproject.content_profile_stig-rhel7-disa
 # remediation = bash
 
-sed -i "s/^space_left = .*$/space_left = 15/" /etc/audit/auditd.conf
+sed -i "s/^space_left[[:space:]]*=.*$/space_left = 15/" /etc/audit/auditd.conf
