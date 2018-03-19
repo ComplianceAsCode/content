bbdbd508e3c2d60100abe533f50929748f375a2d
Watson Sato <wsato@redhat.com>
2018-03-19 18:20:05 +0100

    Make regexes for space_left tests consistent

--- shared/fixes/bash/auditd_data_retention_space_left.sh
+++ shared/fixes/bash/auditd_data_retention_space_left.sh
@@ -2,8 +2,8 @@
 . /usr/share/scap-security-guide/remediation_functions
 populate var_auditd_space_left
 
-grep -q "^space_left[[:space:]]*=" /etc/audit/auditd.conf && \
-  sed -i "s/^space_left[[:space:]]*=.*/space_left = $var_auditd_space_left/g" /etc/audit/auditd.conf
+grep -q "^space_left[[:space:]]*=.*$" /etc/audit/auditd.conf && \
+  sed -i "s/^space_left[[:space:]]*=.*$/space_left = $var_auditd_space_left/g" /etc/audit/auditd.conf
 if ! [ $? -eq 0 ]; then
     echo "space_left = $var_auditd_space_left" >> /etc/audit/auditd.conf
 fi
