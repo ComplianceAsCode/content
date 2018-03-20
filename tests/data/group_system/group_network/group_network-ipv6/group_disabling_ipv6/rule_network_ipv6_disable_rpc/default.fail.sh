c50538db2819bd78340ce5e1f470c4edc5d776ef
Watson Sato <wsato@redhat.com>
2018-03-20 17:27:19 +0100

    Remove redundant profiles in tests for ipv6_disable_rpc

--- tests/data/group_system/group_network/group_network-ipv6/group_disabling_ipv6/rule_network_ipv6_disable_rpc/default.fail.sh
+++ tests/data/group_system/group_network/group_network-ipv6/group_disabling_ipv6/rule_network_ipv6_disable_rpc/default.fail.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# profiles = xccdf_org.ssgproject.content_profile_ospp-rhel7, xccdf_org.ssgproject.content_profile_ospp-rhel7
+# profiles = xccdf_org.ssgproject.content_profile_ospp-rhel7
 # remediation = bash
 
 # default config has rpc enabled
