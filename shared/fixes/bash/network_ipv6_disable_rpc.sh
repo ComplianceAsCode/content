27fb8ba4ac762f027ac354c0f0157c68d429c370
Watson Sato <wsato@redhat.com>
2018-03-20 16:52:12 +0100

    Bash improvements for remdiation and tests

--- shared/fixes/bash/network_ipv6_disable_rpc.sh
+++ shared/fixes/bash/network_ipv6_disable_rpc.sh
@@ -2,9 +2,8 @@
 
 # Drop 'tcp6' and 'udp6' entries from /etc/netconfig to prevent RPC
 # services for NFSv4 from attempting to start IPv6 network listeners
-declare -a IPV6_RPC_ENTRIES=("tcp6" "udp6")
 
-for rpc_entry in ${IPV6_RPC_ENTRIES[@]}
+for rpc_entry in "tcp6" "udp6"
 do
 	sed -i "/^$rpc_entry[[:space:]]\+tpi\_.*inet6.*/d" /etc/netconfig
 done
--- tests/data/group_system/group_network/group_network-ipv6/group_configuring_ipv6/rule_network_ipv6_privacy_extensions/ipv6_privacy_enabled.pass.sh
+++ tests/data/group_system/group_network/group_network-ipv6/group_configuring_ipv6/rule_network_ipv6_privacy_extensions/ipv6_privacy_enabled.pass.sh
@@ -2,7 +2,7 @@
 #
 # profiles = xccdf_org.ssgproject.content_profile_ospp-rhel7
 
-for file in $(ls /etc/sysconfig/network-scripts/ifcfg-*)
+for interface in /etc/sysconfig/network-scripts/ifcfg-*
 do
-    echo "IPV6_PRIVACY=rfc3041" >> $file
+    echo "IPV6_PRIVACY=rfc3041" >> $interface
 done
