c09b5a38d62c733260c1c29227ec3032ea26e9b1
Watson Sato <wsato@redhat.com>
2018-03-20 16:52:12 +0100

    Bash improvements for remdiation and tests

--- shared/fixes/bash/network_ipv6_privacy_extensions.sh
+++ shared/fixes/bash/network_ipv6_privacy_extensions.sh
@@ -1,7 +1,7 @@
 # platform = multi_platform_rhel
 
 # enable randomness in ipv6 address generation
-for interface in $(ls /etc/sysconfig/network-scripts/ifcfg-*)
+for interface in /etc/sysconfig/network-scripts/ifcfg-*
 do
     echo "IPV6_PRIVACY=rfc3041" >> $interface
 done
