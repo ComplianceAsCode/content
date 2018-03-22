c48e1f0086b39540255d1086ac9e66711d76be4d
Watson Sato <wsato@redhat.com>
2018-03-22 15:51:21 +0100

    Add remediation for ldap_client_start_tls
    Configure USELDAPAUTH and start_tls

---
+++ shared/fixes/bash/ldap_client_start_tls.sh
@@ -0,0 +1,10 @@
+# platform = multi_platform_rhel
+
+# Include source function library.
+. /usr/share/scap-security-guide/remediation_functions
+
+# Use LDAP for authentication
+replace_or_append '/etc/sysconfig/authconfig' 'USELDAPAUTH' 'yes' '@CCENUM@' '%s=%s'
+
+# Configure client to use TLS for all authentications
+replace_or_append '/etc/nslcd.conf' 'ssl' 'start_tls' '@CCENUM@' '%s %s'
