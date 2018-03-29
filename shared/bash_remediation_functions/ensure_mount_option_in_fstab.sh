53e8a161cb656d1daf8c3fb9eaa088347f844b5e
Matěj Týč <matyc@redhat.com>
2018-03-29 13:33:15 +0200

    Minor mount_options fixes.
    * Deduplicated template files.
    * Fixed OVAL syntax.
    * Added comments to respective .csv files.

--- shared/bash_remediation_functions/ensure_mount_option_in_fstab.sh
+++ shared/bash_remediation_functions/ensure_mount_option_in_fstab.sh
@@ -2,19 +2,32 @@
 # $2: new mount point option
 function ensure_mount_option_in_fstab {
 	local _mount_point="$1" _new_opt="$2" _mount_point_match_regexp="" _previous_mount_opts=""
-	_mount_point_match_regexp="[[:space:]]$_mount_point[[:space:]]"
+	_mount_point_match_regexp="$(get_mount_point_regexp "$_mount_point")"
 
 	if [ $(grep "$_mount_point_match_regexp" /etc/fstab | grep -c "$_new_opt" ) -eq 0 ]; then
 		_previous_mount_opts=$(grep "$_mount_point_match_regexp" /etc/fstab | awk '{print $4}')
-		sed -i "s/\(${_mount_point_match_regexp}.*${_previous_mount_opts}\)/\1,${_new_opt}/" /etc/fstab
+		sed -i "s|\(${_mount_point_match_regexp}.*${_previous_mount_opts}\)|\1,${_new_opt}|" /etc/fstab
 	fi
 }
 
+# $1: mount point
+function get_mount_point_regexp {
+		printf "[[:space:]]%s[[:space:]]" "$1"
+}
+
+# $1: mount point
+function assert_mount_point_in_fstab {
+	local _mount_point_match_regexp
+	_mount_point_match_regexp="$(get_mount_point_regexp "$1")"
+	grep "$_mount_point_match_regexp" -q /etc/fstab \
+		|| { echo "The mount point '$1' is not even in /etc/fstab, so we can't set up mount options" >&2; return 1; }
+}
 
 function remove_defaults_from_fstab_if_overriden {
-	_mount_point_match_regexp="[[:space:]]$1[[:space:]]"
+	local _mount_point_match_regexp
+	_mount_point_match_regexp="$(get_mount_point_regexp "$1")"
 	if [ $(grep "$_mount_point_match_regexp" /etc/fstab | grep -q "defaults,") -gt 0 ]
 	then
-		sed -i "s/\(${_mount_point_match_regexp}.*\)defaults,/\1/" /etc/fstab
+		sed -i "s|\(${_mount_point_match_regexp}.*\)defaults,|\1|" /etc/fstab
 	fi
 }
