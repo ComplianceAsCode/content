d06430741e6316499ee1495a99de0e9a673761cb
Matěj Týč <matyc@redhat.com>
2018-04-12 16:46:20 +0200

    Cleanup templates, improved mount options tests.

---
+++ tests/data/group_system/group_permissions/group_partitions/rule_mount_option_var_tmp_noexec/fstab.fail.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# profiles = xccdf_org.ssgproject.content_profile_C2S
+
+. partition.sh
+
+create_partition
+
+make_fstab_given_partition_line /var/tmp nodev
+
+cat /etc/fstab
+
+mount_partition /var/tmp
