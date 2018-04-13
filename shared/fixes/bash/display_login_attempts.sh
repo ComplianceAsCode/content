6465cb57725b520a5f1195a79b27c0caf5f533f0
Gabe <redhatrises@gmail.com>
2018-04-13 16:41:57 -0600

    Simplify display_login_attempts remediation script logic
    - Fixes #2483

--- shared/fixes/bash/display_login_attempts.sh
+++ shared/fixes/bash/display_login_attempts.sh
@@ -1,14 +1,10 @@
 # platform = Red Hat Enterprise Linux 7
 
-if ! `grep -q ^[^#].*pam_succeed_if.*showfailed /etc/pam.d/postlogin` ; then
-  if ! grep `^session.*pam_succeed_if.so /etc/pam.d/postlogin` ; then
-    echo "session     [default=1]   pam_lastlog.so nowtmp showfailed" >> /etc/pam.d/postlogin
-    echo "session     optional      pam_lastlog.so silent noupdate showfailed" >> /etc/pam.d/postlogin
-  else
-    sed -i --follow-symlinks '/^session.*pam_succeed_if.so/a session\t    optional\t  pam_lastlog.so silent noupdate showfailed' /etc/pam.d/postlogin
-    sed -i --follow-symlinks '/^session.*pam_succeed_if.so/a session\t    [default=1]\t  pam_lastlog.so nowtmp showfailed' /etc/pam.d/postlogin
-  fi
+if ! `grep -q "^session.*pam_lastlog.so" /etc/pam.d/postlogin` ; then
+	echo "session     [default=1]   pam_lastlog.so nowtmp showfailed" >> /etc/pam.d/postlogin
+	echo "session     optional      pam_lastlog.so silent noupdate showfailed" >> /etc/pam.d/postlogin
 else
-  sed -i --follow-symlinks "s/session[ ]*\[default=1][ ]*pam_lastlog.so.*/session     [default=1]   pam_lastlog.so nowtmp showfailed/g" /etc/pam.d/postlogin
-  sed -i --follow-symlinks "s/session[ ]*optional[ ]*pam_lastlog.so.*/session     optional      pam_lastlog.so silent noupdate showfailed/g" /etc/pam.d/postlogin
+	sed -i --follow-symlinks "/pam_lastlog.so/d" /etc/pam.d/postlogin
+	echo "session     [default=1]   pam_lastlog.so nowtmp showfailed" >> /etc/pam.d/postlogin
+        echo "session     optional      pam_lastlog.so silent noupdate showfailed" >> /etc/pam.d/postlogin
 fi
