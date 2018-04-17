b240c65bef3966f2b64e1b73638b925ce8ca3e81
Gabe <redhatrises@gmail.com>
2018-04-17 17:27:38 -0600

    Simplify display_login_attempts.sh logic even more

--- shared/fixes/bash/display_login_attempts.sh
+++ shared/fixes/bash/display_login_attempts.sh
@@ -1,10 +1,8 @@
 # platform = Red Hat Enterprise Linux 7
 
-if ! `grep -q "^session.*pam_lastlog.so" /etc/pam.d/postlogin` ; then
-	echo "session     [default=1]   pam_lastlog.so nowtmp showfailed" >> /etc/pam.d/postlogin
-	echo "session     optional      pam_lastlog.so silent noupdate showfailed" >> /etc/pam.d/postlogin
-else
+if $(grep -q "^session.*pam_lastlog.so" /etc/pam.d/postlogin) ; then
 	sed -i --follow-symlinks "/pam_lastlog.so/d" /etc/pam.d/postlogin
-	echo "session     [default=1]   pam_lastlog.so nowtmp showfailed" >> /etc/pam.d/postlogin
-        echo "session     optional      pam_lastlog.so silent noupdate showfailed" >> /etc/pam.d/postlogin
 fi
+
+echo "session     [default=1]   pam_lastlog.so nowtmp showfailed" >> /etc/pam.d/postlogin
+echo "session     optional      pam_lastlog.so silent noupdate showfailed" >> /etc/pam.d/postlogin
