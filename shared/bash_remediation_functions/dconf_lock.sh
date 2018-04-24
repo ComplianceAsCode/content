9c558a7790f85539efb961642a7eaa4784d03189
Gabe <redhatrises@gmail.com>
2018-04-24 11:32:14 -0600

    Make sure that dconf dirs exist

--- shared/bash_remediation_functions/dconf_lock.sh
+++ shared/bash_remediation_functions/dconf_lock.sh
@@ -17,6 +17,12 @@ function dconf_lock {
 
 	# Check for setting in any of the DConf db directories
 	LOCKFILES=$(grep -r "^/${_key}/${_setting}$" "/etc/dconf/db/" | grep -v "distro\|ibus" | cut -d":" -f1)
+	LOCKSFOLDER="/etc/dconf/db/${_db}/locks"
+
+	if [ ! -d ${LOCKSFOLDER} ]
+	then
+		mkdir -p ${LOCKSFOLDER}
+	fi
 
 	if [[ -z "${LOCKFILES}" ]]
 	then
--- shared/bash_remediation_functions/dconf_settings.sh
+++ shared/bash_remediation_functions/dconf_settings.sh
@@ -18,6 +18,12 @@ function dconf_settings {
 	# Check for setting in any of the DConf db directories
 	SETTINGSFILES=($(grep -r "\[${_path}]" "/etc/dconf/db/" | grep -v "distro\|ibus" | cut -d":" -f1))
 	DCONFFILE="/etc/dconf/db/${_db}/${_settingFile}"
+	DBDIR="/etc/dconf/db/${_db}"
+
+	if [ ! -d ${DBDIR} ]
+	then
+		mkdir -p ${DBDIR}
+	fi
 
 	if [[ -z "${SETTINGSFILES[@]}" ]]
 	then
