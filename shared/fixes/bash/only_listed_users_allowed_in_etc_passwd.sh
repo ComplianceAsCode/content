493204edc5f4730afd13bc652d1fc783a18f09d0
xiruiyang <xirui.yang@oracle.com>
2018-05-25 12:04:42 +0200

    Fix small issues of the rule only_listed_users_allowed_in_etc_passwd
    	modified:   shared/checks/oval/only_listed_users_allowed_in_etc_passwd.xml
    	modified:   shared/fixes/bash/only_listed_users_allowed_in_etc_passwd.sh
    	modified:   shared/guide/system/software/sap/var_allowed_users_in_etc_passwd.var

--- shared/fixes/bash/only_listed_users_allowed_in_etc_passwd.sh
+++ shared/fixes/bash/only_listed_users_allowed_in_etc_passwd.sh
@@ -8,8 +8,8 @@ default_os_user="root"
 # delete users that is in /etc/passwd but neither in the var_allowed_users_in_etc_passwd
 # nor in default_os_user
 for username in $( sed 's/:.*//' /etc/passwd ) ; do
-    if [[ ! "$username" =~ ($default_os_user|$var_allowed_users_in_etc_passwd) ]]; then
-        userdel $username ; 
-    fi
+	if [[ ! "$username" =~ ($default_os_user|$var_allowed_users_in_etc_passwd) ]]; then
+		userdel $username ; 
+	fi
 done
 
