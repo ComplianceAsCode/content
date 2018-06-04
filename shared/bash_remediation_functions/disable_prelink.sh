0e9b4f19abcac6e0be9f671b8c59e006cb1082c9
Matěj Týč <matyc@redhat.com>
2018-06-04 14:12:46 +0200

    Improved remediation logic.

--- shared/bash_remediation_functions/disable_prelink.sh
+++ shared/bash_remediation_functions/disable_prelink.sh
@@ -3,10 +3,10 @@ function disable_prelink {
 	# whether it is installed.
 	if grep -q ^PRELINKING /etc/sysconfig/prelink
 	then
-		sed -i 's/PRELINKING.*/PRELINKING=no/g' /etc/sysconfig/prelink
+		sed -i 's/^PRELINKING[:blank:]*=[:blank:]*[:alpha:]*/PRELINKING=no/' /etc/sysconfig/prelink
 	else
-		printf '\n%s\n' '# Set PRELINKING=no per security requirements' >> /etc/sysconfig/prelink
-		echo 'PRELINKING=no' >> /etc/sysconfig/prelink
+		printf '\n' >> /etc/sysconfig/prelink
+		printf '%s\n' '# Set PRELINKING=no per security requirements' 'PRELINKING=no' >> /etc/sysconfig/prelink
 	fi
 
 	# Undo previous prelink changes to binaries if prelink is available.
