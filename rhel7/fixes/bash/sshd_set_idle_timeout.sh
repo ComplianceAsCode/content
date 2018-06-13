7b0d0f96e6265f5c6cf5d77bf9a1bcb55fc9a57d
Martin Preisler <mpreisle@redhat.com>
2018-06-13 14:02:04 -0400

    Modernize the sshd_set_idle_timeout.sh bash fix with the RHEL7 version

--- rhel7/fixes/bash/sshd_set_idle_timeout.sh
+++
@@ -1,5 +0,0 @@
-# platform = Red Hat Enterprise Linux 7
-. /usr/share/scap-security-guide/remediation_functions
-populate sshd_idle_timeout_value
-
-replace_or_append '/etc/ssh/sshd_config' '^ClientAliveInterval' $sshd_idle_timeout_value '@CCENUM@' '%s %s'
