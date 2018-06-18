b0ed5d20519f5fa3d7499ebba4c9c117620b4287
xiruiyang <xirui.yang@oracle.com>
2018-06-18 14:52:40 +0200

    small change according to matejak's suggestion
    	modified:   shared/fixes/bash/accounts_authorized_local_users_sidadm_orasid.sh

--- shared/fixes/bash/accounts_authorized_local_users_sidadm_orasid.sh
+++ shared/fixes/bash/accounts_authorized_local_users_sidadm_orasid.sh
@@ -14,8 +14,8 @@ sapmnt_SID_stem="/sapmnt/[A-Z][A-Z0-9][A-Z0-9]"
 if [ -d "/sapmnt" ] ; then 
 	# if /sapmnt/SID exists, add sidadm to the userlist
 	path_sapmnt_SID_list=$(find /sapmnt/ -regex "^$sapmnt_SID_stem$")
-	for i in $path_sapmnt_SID_list ; do
-		SID=${i:8:3}
+	for path_sapmnt_SID in $path_sapmnt_SID_list ; do
+		SID=${path_sapmnt_SID:8:3}
 		userlist="$userlist|$(echo "$SID" | sed -e 's/\(.*\)/\L\1/')adm"
 	done
 
@@ -24,15 +24,15 @@ if [ -d "/sapmnt" ] ; then
 	path_to_brspace_list=$(find /sapmnt/ -regex "^$sapmnt_SID_stem/exe/\(\|\(\|n\)uc/[a-z0-9_]+/\)brspace$")
 
 	# if brspace exist in any of the above directory of a SID, add orasid to the userlist 
-	for i in $path_to_brspace_list ; do
-        	SID=${i:8:3}
+	for path_to_brspace in $path_to_brspace_list ; do
+        	SID=${path_to_brspace:8:3}
         	userlist="$userlist|ora$(echo "$SID" | sed -e 's/\(.*\)/\L\1/')"
 	done
 
 	# if owner of any brspace file is oracle, add oracle to the userlist
 	oracle=false
-	for i in $path_to_brspace_list ; do
-        	if [ $(ls -ld $i | awk '{print $3}') = "oracle" ]; then
+	for path_to_brspace in $path_to_brspace_list ; do
+        	if [ $(ls -ld $path_to_brspace | awk '{print $3}') = "oracle" ]; then
                 	oracle=true
         	fi
 	done
