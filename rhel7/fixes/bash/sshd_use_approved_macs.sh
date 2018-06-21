e918514443b0efcfa24dbf8be261c1ed461a0958
Martin Preisler <mpreisle@redhat.com>
2018-06-21 15:07:51 -0400

    RHEL6 guide can now also use a variable to change sshd MACs

--- rhel7/fixes/bash/sshd_use_approved_macs.sh
+++
@@ -1,8 +0,0 @@
-# platform = Red Hat Enterprise Linux 7
-
-# Include source function library.
-. /usr/share/scap-security-guide/remediation_functions
-
-populate sshd_approved_macs
-
-replace_or_append '/etc/ssh/sshd_config' '^MACs' "$sshd_approved_macs" '@CCENUM@' '%s %s'
