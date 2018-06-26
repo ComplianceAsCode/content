b0eb3b7f7baa1a57dac3e373209d20bd55b3f215
Matěj Týč <matyc@redhat.com>
2018-06-26 11:42:49 +0200

    Added implicit double quoting of substituted vars.
    Variables in Ansible may be wrongly interpreted if they are not quoted
    (i.e. yes, 077 will be converted to bool and octal respectively).
    Unlike single quotes, double quotes may be escaped.
    
    Fixes: #2989

--- ssg/build_remediations.py
+++ ssg/build_remediations.py
@@ -194,7 +194,7 @@ def expand_xccdf_subs(fix, remediation_type, remediation_functions):
             r"- \(xccdf-var\s+(\S+)\)",
             r"- name: XCCDF Value \1 # promote to variable\n"
             r"  set_fact:\n"
-            r"    \1: (ansible-populate \1)\n"
+            r'    \1: "(ansible-populate \1)"\n'
             r"  tags:\n"
             r"    - always",
             fix_text
