dd06f70fae61d311ad5251c8dcd51f2822739a16
xiruiyang <xirui.yang@oracle.com>
2018-06-26 16:33:56 +0200

    change according to matejak's suggestion
    	modified:   shared/fixes/bash/accounts_authorized_local_users_sidadm_orasid.sh

--- shared/fixes/bash/accounts_authorized_local_users_sidadm_orasid.sh
+++ shared/fixes/bash/accounts_authorized_local_users_sidadm_orasid.sh
@@ -12,10 +12,10 @@ oracle_SID_stem="/oracle/[A-Z][A-Z0-9][A-Z0-9]"
 
 # if owner of any directory or file in the given list is the user oracle,
 # add the user oracle to the variable userlist. 
-# Usage: verify_oracle_user_to_userlist "${array[@]}"
+# Usage: verify_oracle_user_to_userlist "$path_list"
 # Note: this function might modify the value of the global variable userlist
 function verify_oracle_user_to_userlist {
-	local path_list=("$@")
+	local path_list="$1"
 	local is_oracle=no
 	for path in $path_list ; do
 		if [ $(ls -ld "$path" | awk '{print $3}') = "oracle" ]; then
@@ -48,14 +48,14 @@ if [ -d "/sapmnt" ] ; then
 	done
 
 	# if owner of any brspace file is oracle, add oracle to the userlist
-	verify_oracle_user_to_userlist "${path_to_brspace_list[@]}"
+	verify_oracle_user_to_userlist "$path_to_brspace_list"
 fi
 
 # if owner of any /oracle/SID directory is oracle, add oracle to the userlist
 # the user oracle could be added twice in the userlist, but it is harmlos to the final result
 if [ -d "/oracle" ] ; then
 	path_oracle_SID_list=$(find /oracle/ -regex "^$oracle_SID_stem$")
-	verify_oracle_user_to_userlist "${path_oracle_SID_list[@]}"
+	verify_oracle_user_to_userlist "$path_oracle_SID_list"
 fi
 
 # if /usr/sap/hostctrl is a directory or a symbolic link to a directory, add sapadm to the list
