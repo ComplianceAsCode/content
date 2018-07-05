b4618966a349c53ae93e14e452070894d64918c2
Martin Preisler <martin@preisler.me>
2018-07-05 13:14:32 -0400

    Unshadow sshd_set_keepalive on RHEL6

--- rhel6/fixes/bash/sshd_set_keepalive.sh
+++
@@ -1,6 +0,0 @@
-# platform = Red Hat Enterprise Linux 6
-grep -q ^ClientAliveCountMax /etc/ssh/sshd_config && \
-  sed -i "s/ClientAliveCountMax.*/ClientAliveCountMax 0/g" /etc/ssh/sshd_config
-if ! [ $? -eq 0 ]; then
-    echo "ClientAliveCountMax 0" >> /etc/ssh/sshd_config
-fi
