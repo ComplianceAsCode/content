07a471db882e2540a24b2a01280602785de26048
Martin Preisler <martin@preisler.me>
2018-07-05 13:12:49 -0400

    Unshadow bash fix display_login_attempts on RHEL6

--- rhel6/fixes/bash/display_login_attempts.sh
+++
@@ -1,2 +0,0 @@
-# platform = Red Hat Enterprise Linux 6
-sed -i --follow-symlinks '/pam_limits.so/a session\t    required\t  pam_lastlog.so showfailed' /etc/pam.d/system-auth
--- shared/fixes/bash/display_login_attempts.sh
+++ shared/fixes/bash/display_login_attempts.sh
@@ -1,8 +1,12 @@
-# platform = Red Hat Enterprise Linux 7
+# platform = multi_platform_rhel
 
+{{%- if product == "rhel6" -%}}
+sed -i --follow-symlinks '/pam_limits.so/a session\t    required\t  pam_lastlog.so showfailed' /etc/pam.d/system-auth
+{{%- else -%}}
 if $(grep -q "^session.*pam_lastlog.so" /etc/pam.d/postlogin) ; then
 	sed -i --follow-symlinks "/pam_lastlog.so/d" /etc/pam.d/postlogin
 fi
 
 echo "session     [default=1]   pam_lastlog.so nowtmp showfailed" >> /etc/pam.d/postlogin
 echo "session     optional      pam_lastlog.so silent noupdate showfailed" >> /etc/pam.d/postlogin
+{{%- endif -%}}
