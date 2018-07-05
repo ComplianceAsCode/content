b249f340f242cefce2f7a7a5ce314803bf809566
Martin Preisler <martin@preisler.me>
2018-07-05 13:34:45 -0400

    Unshadow enable_selinux_bootloader bash fix on RHEL6

--- shared/fixes/bash/enable_selinux_bootloader.sh
+++ shared/fixes/bash/enable_selinux_bootloader.sh
@@ -1,3 +1,8 @@
-# platform = Red Hat Enterprise Linux 7
+# platform = multi_platform_rhel
+{{%- if product == "rhel7" -%}}
 sed -i --follow-symlinks "s/selinux=0//gI" /etc/default/grub /etc/grub2.cfg /etc/grub.d/*
 sed -i --follow-symlinks "s/enforcing=0//gI" /etc/default/grub /etc/grub2.cfg /etc/grub.d/*
+{{%- elif product == "rhel6" -%}}
+sed -i --follow-symlinks "s/selinux=0//gI" /etc/grub.conf
+sed -i --follow-symlinks "s/enforcing=0//gI" /etc/grub.conf
+{{%- endif -%}}
