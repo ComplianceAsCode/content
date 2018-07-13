722d17b1e7d1aa1feffbf18e8f8dcef52462c8f7
Marek Haicman <mhaicman@redhat.com>
2018-07-13 15:11:15 +0200

    Ansible remediations are supported.

--- tests/data/group_services/group_sssd/group_sssd-ldap/rule_sssd_ldap_configure_tls_ca_dir/ldap_tls_cacertdir.pass.sh
+++ tests/data/group_services/group_sssd/group_sssd-ldap/rule_sssd_ldap_configure_tls_ca_dir/ldap_tls_cacertdir.pass.sh
@@ -3,5 +3,3 @@
 
 . ../setup_config_files.sh
 setup_correct_sssd_config
-
-sed -i '/\[domain/d' /etc/sssd/sssd.conf
--- tests/data/group_services/group_sssd/group_sssd-ldap/rule_sssd_ldap_start_tls/useldapauth_and_start_tls.pass.sh
+++ tests/data/group_services/group_sssd/group_sssd-ldap/rule_sssd_ldap_start_tls/useldapauth_and_start_tls.pass.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # profiles = xccdf_org.ssgproject.content_profile_stig-rhel7-disa
-# remediation = bash
 
 . ../setup_config_files.sh
 setup_correct_auth_and_sssd_configs
