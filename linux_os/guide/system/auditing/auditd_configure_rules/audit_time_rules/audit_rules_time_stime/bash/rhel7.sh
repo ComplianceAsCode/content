64e2db868b66f023c9940c441c618a78e6af07d7
Ilya Okomin <ilya.okomin@oracle.com>
2018-08-16 18:13:17 +0300

    Make audit_rules_time_stime shared
    Added ol7 support to audit_rules_time_stime.xml OVAL.
    Custom implementations of audit_rules_time_stime.sh in rhel6 and rhel7 merged to shared,
    added fedora and ol7 supported platforms.
    
    Signed-off-by: Ilya Okomin <ilya.okomin@oracle.com>

--- linux_os/guide/system/auditing/auditd_configure_rules/audit_time_rules/audit_rules_time_stime/bash/rhel7.sh
+++
@@ -1,6 +0,0 @@
-# platform = Red Hat Enterprise Linux 7
-
-# Include source function library.
-. /usr/share/scap-security-guide/remediation_functions
-
-rhel7_fedora_perform_audit_adjtimex_settimeofday_stime_remediation
