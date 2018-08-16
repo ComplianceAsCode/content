0ca43493b123fa46d099c0177a6acb703af7b9fb
Alexander Scheel <ascheel@redhat.com>
2018-08-16 13:14:44 -0400

    Add test for ssg.utils.required_key
    Signed-off-by: Alexander Scheel <ascheel@redhat.com>

--- tests/unit/ssg/test_utils.py
+++ tests/unit/ssg/test_utils.py
@@ -14,3 +14,15 @@ def test_merge_dicts():
     assert "red fish" not in left
     assert "red fish" in right
     assert 1 not in right
+
+
+def test_required_key():
+    rk = ssg.utils.required_key
+    _dict = {'something': 'some_value',
+             'something_else': 'other_value'}
+
+    assert rk(_dict, 'something') == 'some_value'
+    assert rk(_dict, 'something_else') == 'other_value'
+
+    with pytest.raises(ValueError):
+        rk(_dict, 'not-in-dict')
