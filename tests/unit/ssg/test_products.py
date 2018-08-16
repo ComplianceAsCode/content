1eb0c467fcf4610dc8e5666e52bc3cf9977fd3d3
Alexander Scheel <ascheel@redhat.com>
2018-08-16 12:55:14 -0400

    Add test for ssg.products.parse_name
    Signed-off-by: Alexander Scheel <ascheel@redhat.com>

--- tests/unit/ssg/test_products.py
+++ tests/unit/ssg/test_products.py
@@ -5,6 +5,22 @@ import pytest
 import ssg.products
 
 
+def test_parse_name():
+    pn = ssg.products.parse_name
+
+    n, v = pn("rhel7")
+    assert n == "rhel"
+    assert v == "7"
+
+    n, v = pn("rhel")
+    assert n == "rhel"
+    assert not v
+
+    n, v = pn("rhel-osp7")
+    assert n == "rhel-osp"
+    assert v == "7"
+
+
 def test_get_all():
     ssg_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../.."))
     products = ssg.products.get_all(ssg_root)
