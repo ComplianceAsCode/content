4fc167dfcb64c209676063a28f343a7f74c06980
Alexander Scheel <ascheel@redhat.com>
2018-08-16 13:11:44 -0400

    Remove parenthesis around assert
    Signed-off-by: Alexander Scheel <ascheel@redhat.com>

--- tests/unit/ssg/test_utils.py
+++ tests/unit/ssg/test_utils.py
@@ -9,8 +9,8 @@ def test_merge_dicts():
     merged_expected = {1: 2, "red fish": "blue fish"}
     merged_actual = ssg.utils.merge_dicts(left, right)
 
-    assert(merged_actual == merged_expected)
-    assert(1 in left)
-    assert("red fish" not in left)
-    assert("red fish" in right)
-    assert(1 not in right)
+    assert merged_actual == merged_expected
+    assert 1 in left
+    assert "red fish" not in left
+    assert "red fish" in right
+    assert 1 not in right
