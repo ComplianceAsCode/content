65efee4a33dfebb585819f99c92f0d993905dad9
Alexander Scheel <ascheel@redhat.com>
2018-08-21 08:15:09 -0400

    Make add_minimum_version unit testable
    Signed-off-by: Alexander Scheel <ascheel@redhat.com>

--- ssg/ansible.py
+++ ssg/ansible.py
@@ -1,5 +1,5 @@
 """
-Common ansible functions for SSG
+Common functions for processing Ansible in SSG
 """
 
 from __future__ import absolute_import
@@ -7,6 +7,7 @@ from __future__ import print_function
 
 from .constants import ansible_version_requirement_pre_task_name
 from .constants import min_ansible_version
+
 
 def add_minimum_version(ansible_src):
     """
@@ -21,5 +22,15 @@ def add_minimum_version(ansible_src):
            "You must update Ansible to at least version %s to use this role."
           """ % (ansible_version_requirement_pre_task_name,
                  min_ansible_version, min_ansible_version))
+
+    if ' - hosts: all' not in ansible_src:
+        return ansible_src
+
+    if 'pre_task' in ansible_src:
+        if 'ansible_version.full is version_compare' in ansible_src:
+            return ansible_src
+
+        raise ValueError("A pre_task already exists in ansible_src; failing to process: %s" %
+                         ansible_src)
 
     return ansible_src.replace(" - hosts: all", pre_task, 1)
