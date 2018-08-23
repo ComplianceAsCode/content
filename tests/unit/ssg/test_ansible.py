d4ab19b11d5aa9b6e43f9caa006fee2a484a1744
Alexander Scheel <ascheel@redhat.com>
2018-08-23 09:25:59 -0400

    Rename no_whitespace_compare -> strings_equal_except_whitespaces, use dedent
    Signed-off-by: Alexander Scheel <ascheel@redhat.com>

--- tests/unit/ssg/test_ansible.py
+++ tests/unit/ssg/test_ansible.py
@@ -2,50 +2,57 @@ import pytest
 
 import os
 import re
+import textwrap
 import ssg.ansible
 
 
-def no_whitespace_compare(left, right):
+def strings_equal_except_whitespaces(left, right):
     return re.sub(r'\s', '', left) == re.sub(r'\s', '', right)
 
 
 def test_add_minimum_version():
     good_snippet = """
- - hosts: all
-   vars:
-   tasks:
-"""
+    # a comment
+     - hosts: all
+       vars:
+       tasks:
+    """
+    good_snippet = textwrap.dedent(good_snippet)
 
     bad_snippet = """
- - hosts: all
-   pre_tasks:
-     - name: something_else
-       assert:
-         that: "2 > 3"
-         msg: "two is less than three!"
-   vars:
-   tasks:
-"""
+    # a comment
+     - hosts: all
+       pre_tasks:
+         - name: something_else
+           assert:
+             that: "2 > 3"
+             msg: "two is less than three!"
+       vars:
+       tasks:
+    """
+    bad_snippet = textwrap.dedent(bad_snippet)
 
     unknown_snippet = """
     I don't think this is YAML
-"""
+    """
 
     processed_snippet = """
- - hosts: all
-   pre_tasks:
-     - name: Verify Ansible meets SCAP-Security-Guide version requirements.
-       assert:
-         that: "ansible_version.full is version_compare('2.3', '>=')"
-         msg: >
-           "You must update Ansible to at least version 2.3 to use this role."
+    # a comment
+     - hosts: all
+       pre_tasks:
+         - name: Verify Ansible meets SCAP-Security-Guide version requirements.
+           assert:
+             that: "ansible_version.full is version_compare('2.3', '>=')"
+             msg: >
+               "You must update Ansible to at least version 2.3 to use this role."
 
-   vars:
-   tasks:
-"""
+       vars:
+       tasks:
+    """
+    processed_snippet = textwrap.dedent(processed_snippet)
 
     output = ssg.ansible.add_minimum_version(good_snippet)
-    assert no_whitespace_compare(output, processed_snippet)
+    assert strings_equal_except_whitespaces(output, processed_snippet)
 
     with pytest.raises(ValueError):
         ssg.ansible.add_minimum_version(bad_snippet)
