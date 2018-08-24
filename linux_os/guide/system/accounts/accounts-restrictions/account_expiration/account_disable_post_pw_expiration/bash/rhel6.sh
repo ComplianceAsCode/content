9bc2979bf068fc7d79f2478896216f37fce73a79
Watson Sato <wsato@redhat.com>
2018-08-24 12:42:48 +0200

    Merge Bash for account_disable_post_pw_expiration
    Merge rhel6 and rhel7 Bash remediation for
    account_disable_post_pw_expiration into shared, and also enable Fedora.

--- linux_os/guide/system/accounts/accounts-restrictions/account_expiration/account_disable_post_pw_expiration/bash/rhel6.sh
+++
@@ -1,9 +0,0 @@
-# platform = Red Hat Enterprise Linux 6
-. /usr/share/scap-security-guide/remediation_functions
-populate var_account_disable_post_pw_expiration
-
-grep -q ^INACTIVE /etc/default/useradd && \
-  sed -i "s/INACTIVE.*/INACTIVE=$var_account_disable_post_pw_expiration/g" /etc/default/useradd
-if ! [ $? -eq 0 ]; then
-    echo "INACTIVE=$var_account_disable_post_pw_expiration" >> /etc/default/useradd
-fi
