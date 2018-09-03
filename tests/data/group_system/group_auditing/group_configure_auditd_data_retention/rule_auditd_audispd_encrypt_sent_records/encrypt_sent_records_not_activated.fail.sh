c5e01b0bd2d375d35b7f26df409963f1bb63febd
Jan Černý <jcerny@redhat.com>
2018-09-03 11:34:38 +0200

    Add OSPP profile to test cases for rule auditd_audispd_encrypt_sent_records

--- tests/data/group_system/group_auditing/group_configure_auditd_data_retention/rule_auditd_audispd_encrypt_sent_records/encrypt_sent_records_not_activated.fail.sh
+++ tests/data/group_system/group_auditing/group_configure_auditd_data_retention/rule_auditd_audispd_encrypt_sent_records/encrypt_sent_records_not_activated.fail.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# profiles = xccdf_org.ssgproject.content_profile_stig-rhel7-disa
+# profiles = xccdf_org.ssgproject.content_profile_stig-rhel7-disa, xccdf_org.ssgproject.content_profile_ospp
 
 . ../../auditd_utils.sh
 prepare_auditd_test_enviroment
