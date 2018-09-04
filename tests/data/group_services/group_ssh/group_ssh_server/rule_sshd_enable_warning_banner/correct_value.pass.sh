cfc205e09bac1df6b8a97a8045cb4feef5566e73
Jan Černý <jcerny@redhat.com>
2018-09-04 16:36:11 +0200

    Add tests for sshd_enable_warning_banner

---
+++ tests/data/group_services/group_ssh/group_ssh_server/rule_sshd_enable_warning_banner/correct_value.pass.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# profiles = xccdf_org.ssgproject.content_profile_ospp
+
+if grep -q "^Banner" /etc/ssh/sshd_config; then
+	sed -i "s|^Banner.*|Banner /etc/issue|" /etc/ssh/sshd_config
+else
+	echo "Banner /etc/issue" >> /etc/ssh/sshd_config
+fi
