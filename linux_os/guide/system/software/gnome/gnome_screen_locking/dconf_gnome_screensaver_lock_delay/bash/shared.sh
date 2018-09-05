e5f2cb0ecaeea3cf23b7a85c412d5cccb5849899
Martin Preisler <mpreisle@redhat.com>
2018-09-05 14:12:40 -0400

    Enabled dconf_gnome_screensaver_lock_delay for Fedora
    Ansible fix is already there.

--- linux_os/guide/system/software/gnome/gnome_screen_locking/dconf_gnome_screensaver_lock_delay/bash/shared.sh
+++ linux_os/guide/system/software/gnome/gnome_screen_locking/dconf_gnome_screensaver_lock_delay/bash/shared.sh
@@ -1,4 +1,4 @@
-# platform = Red Hat Enterprise Linux 7
+# platform = Red Hat Enterprise Linux 7, multi_platform_fedora
 . /usr/share/scap-security-guide/remediation_functions
 populate var_screensaver_lock_delay
 
