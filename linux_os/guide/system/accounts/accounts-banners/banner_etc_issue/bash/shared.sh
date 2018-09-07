5ced2b09f365e32f027fda0f3d6e097572a1ea38
Jan Černý <jcerny@redhat.com>
2018-09-07 12:14:28 +0200

    Merge remediations for banner_etc_issue
    Also add multi_platform_fedora to the list

--- linux_os/guide/system/accounts/accounts-banners/banner_etc_issue/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-banners/banner_etc_issue/bash/shared.sh
@@ -1,9 +1,8 @@
-# platform = Red Hat Enterprise Linux 7
+# platform = multi_platform_rhel, multi_platform_fedora
 . /usr/share/scap-security-guide/remediation_functions
 populate login_banner_text
 
 # There was a regular-expression matching various banners, needs to be expanded
-# When there are multiple banners in login_banner_text, the first banner should be the one for RHEL7
 expanded=$(echo "$login_banner_text" | sed 's/(\\\\\x27)\*/\\\x27/g;s/(\\\x27)\*//g;s/\^(\(.*\)|.*$/\1/g;s/\[\\s\\n\][+*]/ /g;s/\\//g;s/[^-]- /\n\n-/g;s/(n)\**//g')
 formatted=$(echo "$expanded" | fold -sw 80)
 
