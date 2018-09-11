b0cf50b476bd626b7cbf60e18726e02a6282f977
Jan Černý <jcerny@redhat.com>
2018-09-11 13:10:16 +0200

    Improve comments

--- tests/data/group_system/group_auditing/rule_grub2_audit_argument/arg_not_there.fail.sh
+++ tests/data/group_system/group_auditing/rule_grub2_audit_argument/arg_not_there.fail.sh
@@ -2,8 +2,7 @@
 
 # profiles = xccdf_org.ssgproject.content_profile_ospp
 
-# Correct the form of default kernel command line in GRUB
+# Removes audit argument from kernel command line
 if grep -q '^GRUB_CMDLINE_LINUX=.*audit=.*"'  '/etc/default/grub' ; then
-	# Remove the audit arg from the GRUB command-line if an audit arg already exists
 	sed -i 's/\(^GRUB_CMDLINE_LINUX=".*\)audit=[^[:space:]]*\(.*"\)/\1 \2/'  '/etc/default/grub'
 fi
