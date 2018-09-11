4ce0cbe60b25bf1098cda79179e0e8b5abdec39f
Watson Sato <wsato@redhat.com>
2018-09-11 16:52:50 +0200

    Drop hardcoded audit rules, generate them during scenario setup
    Adds a script that looks for privileged commands, and creates an audit
    rule for each binary found. The rule is added to specified output file.

--- tests/data/group_system/group_auditing/group_auditd_configure_rules/group_audit_privileged_commands/rule_audit_rules_privileged_commands/rhel7_auditctl_rules_configured.pass.sh
+++ tests/data/group_system/group_auditing/group_auditd_configure_rules/group_audit_privileged_commands/rule_audit_rules_privileged_commands/rhel7_auditctl_rules_configured.pass.sh
@@ -2,6 +2,5 @@
 # profiles = xccdf_org.ssgproject.content_profile_pci-dss
 # remediation = bash
 
-cp rhel7_privileged.rules /etc/audit/rules.d/privileged.rules
-cp rhel7_privileged.rules /etc/audit/audit.rules
+./generate_privileged_commands_rule.sh 1000 privileged /etc/audit/audit.rules
 sed -i "s%^ExecStartPost=.*%ExecStartPost=-/sbin/auditctl%" /usr/lib/systemd/system/auditd.service
