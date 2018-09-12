88d6c80a9a4e50acfc4d607d60c813db1e4cf00c
Milan Lysonek <mlysonek@redhat.com>
2018-09-12 13:26:47 +0200

    Add accounts_passwords_pam_faillock_deny_root rule for Fedora and fix its bash remediation.

--- linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_passwords_pam_faillock_deny_root/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_passwords_pam_faillock_deny_root/bash/shared.sh
@@ -1,10 +1,10 @@
-# platform = multi_platform_rhel
+# platform = multi_platform_rhel,multi_platform_fedora
 
 AUTH_FILES[0]="/etc/pam.d/system-auth"
 AUTH_FILES[1]="/etc/pam.d/password-auth"
 
 # This script fixes absence of pam_faillock.so in PAM stack or the
-# absense of even_deny_root and deny=[0-9]+ in pam_faillock.so arguments
+# absense of even_deny_root in pam_faillock.so arguments
 # When inserting auth pam_faillock.so entries,
 # the entry with preauth argument will be added before pam_unix.so module
 # and entry with authfail argument will be added before pam_deny.so module.
@@ -12,30 +12,44 @@ AUTH_FILES[1]="/etc/pam.d/password-auth"
 # The placement of pam_faillock.so entries will not be changed
 # if they are already present
 
+# ensure, that pam.d folder exists
+mkdir -p "/etc/pam.d"
+
 for pamFile in "${AUTH_FILES[@]}"
 do
-	# pam_faillock.so already present?
-	if grep -q "^auth.*pam_faillock.so.*" $pamFile; then
+	# if auth file is missing, create it and add what this rule needs
+	if [ ! -f $pamFile ]; then
+		touch $pamFile
+		echo "
+auth required pam_faillock.so preauth silent even_deny_root deny=3 unlock_time=never fail_interval=900
+auth sufficient pam_unix.so
+auth [default=die] pam_faillock.so authfail silent even_deny_root deny=3 unlock_time=never fail_interval=900
+" >> $pamFile
+		# everything is set, don't check it again
+		continue
+	fi
 
-		# pam_faillock.so present, preauth even_deny_root directive present?
+	# is 'auth required' here?
+	if grep -q "^auth.*required.*pam_faillock.so.*" $pamFile; then
+		# has 'auth required' even_deny_root option?
 		if ! grep -q "^auth.*required.*pam_faillock.so.*preauth.*even_deny_root" $pamFile; then
 			# even_deny_root is not present
 			sed -i --follow-symlinks "s/\(^auth.*required.*pam_faillock.so.*preauth.*\).*/\1 even_deny_root/" $pamFile
 		fi
+	else
+		# no 'auth required', add it
+		sed -i --follow-symlinks "/^auth.*pam_unix.so.*/i auth required pam_faillock.so preauth silent even_deny_root" $pamFile
+	fi
 
-		# pam_faillock.so present, authfail even_deny_root directive present?
+	# is 'auth [default=die]' here?
+	if grep -q "^auth.*\[default=die\].*pam_faillock.so.*" $pamFile; then
+		# has 'auth [default=die]' even_deny_root option?
 		if ! grep -q "^auth.*\[default=die\].*pam_faillock.so.*authfail.*even_deny_root" $pamFile; then
 			# even_deny_root is not present
 			sed -i --follow-symlinks "s/\(^auth.*\[default=die\].*pam_faillock.so.*authfail.*\).*/\1 even_deny_root/" $pamFile
 		fi
-
-	# pam_faillock.so not present yet
 	else
-
-		# insert pam_faillock.so preauth row with proper value of the 'deny' option before pam_unix.so
-		sed -i --follow-symlinks "/^auth.*pam_unix.so.*/i auth        required      pam_faillock.so preauth silent even_deny_root" $pamFile
-		# insert pam_faillock.so authfail row with proper value of the 'deny' option before pam_deny.so, after all modules which determine authentication outcome.
-		sed -i --follow-symlinks "/^auth.*pam_deny.so.*/i auth        [default=die] pam_faillock.so authfail silent even_deny_root" $pamFile
+		# no 'auth [default=die]', add it
+		sed -i --follow-symlinks "/^auth.*pam_unix.so.*/a auth [default=die] pam_faillock.so authfail silent even_deny_root" $pamFile
 	fi
-
 done
