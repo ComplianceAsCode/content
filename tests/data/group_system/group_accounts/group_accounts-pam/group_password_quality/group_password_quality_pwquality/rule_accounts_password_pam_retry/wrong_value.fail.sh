f9c124f86f0195ccae330045039ebe694c9869bf
Jan Černý <jcerny@redhat.com>
2018-09-14 09:36:03 +0200

    Add tests for accounts_password_pam_retry

---
+++ tests/data/group_system/group_accounts/group_accounts-pam/group_password_quality/group_password_quality_pwquality/rule_accounts_password_pam_retry/wrong_value.fail.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# profiles = xccdf_org.ssgproject.content_profile_ospp
+
+retry_cnt=7
+config_file="/etc/pam.d/system-auth"
+
+if grep -q "pam_pwquality\.so.*retry=" "$config_file" ; then
+	sed -i --follow-symlinks "/pam_pwquality\.so/ s/\(retry *= *\).*/\1$retry_cnt/" $config_file
+else
+	sed -i --follow-symlinks "/pam_pwquality\.so/ s/$/ retry=$retry_cnt/" $config_file
+fi
