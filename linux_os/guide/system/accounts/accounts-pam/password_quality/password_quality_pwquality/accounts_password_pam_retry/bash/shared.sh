e7312c51f94cff3054fb9be8eb3789ba74003957
Jan Černý <jcerny@redhat.com>
2018-09-14 09:35:48 +0200

    Add multi_platform_fedora to Bash remediation

--- linux_os/guide/system/accounts/accounts-pam/password_quality/password_quality_pwquality/accounts_password_pam_retry/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-pam/password_quality/password_quality_pwquality/accounts_password_pam_retry/bash/shared.sh
@@ -1,8 +1,8 @@
-# platform = Red Hat Enterprise Linux 7
+# platform = Red Hat Enterprise Linux 7, multi_platform_fedora
 . /usr/share/scap-security-guide/remediation_functions
 populate var_password_pam_retry
 
-if grep -q "retry=" /etc/pam.d/system-auth; then   
+if grep -q "retry=" /etc/pam.d/system-auth ; then
 	sed -i --follow-symlinks "s/\(retry *= *\).*/\1$var_password_pam_retry/" /etc/pam.d/system-auth
 else
 	sed -i --follow-symlinks "/pam_pwquality.so/ s/$/ retry=$var_password_pam_retry/" /etc/pam.d/system-auth
