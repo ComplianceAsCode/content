e1ad92ff544eec36b6d03d25450a37f1e64fcef6
Watson Sato <wsato@redhat.com>
2018-09-20 21:28:52 +0200

    Remove default scenario that don't provide value

--- tests/data/group_system/group_auditing/group_auditd_configure_rules/group_audit_privileged_commands/rule_audit_rules_privileged_commands_sudo/rhel7_auditctl_default.fail.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# profiles = xccdf_org.ssgproject.content_profile_ospp
-# remediation = bash
-
-sed -i "s%^ExecStartPost=.*%ExecStartPost=-/sbin/auditctl%" /usr/lib/systemd/system/auditd.service
--- tests/data/group_system/group_auditing/group_auditd_configure_rules/group_audit_privileged_commands/rule_audit_rules_privileged_commands_sudo/rhel7_augenrules_default.fail.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# profiles = xccdf_org.ssgproject.content_profile_ospp
-# remediation = bash
-
-# augenrules is default for rhel7
-# This is a trick to fail setup of this test in rhel6 systems
-ls  /usr/lib/systemd/system/auditd.service
