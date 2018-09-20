cb2ca84970c783660c03464a55295243841baaa1
Watson Sato <wsato@redhat.com>
2018-09-20 21:34:58 +0200

    Fix indents in dconf_test_functions.sh

--- tests/data/group_system/group_software/group_gnome/dconf_test_functions.sh
+++ tests/data/group_system/group_software/group_gnome/dconf_test_functions.sh
@@ -1,7 +1,7 @@
 
 # Wipes out dconf db settings directory
 clean_dconf_settings(){
-    rm -rf /etc/dconf/db/*
+	rm -rf /etc/dconf/db/*
 }
 
 # Adds a new dconf setting
@@ -12,7 +12,7 @@ clean_dconf_settings(){
 # $5 _settingFile
 add_dconf_setting() {
 	local _path=$1 _setting=$2 _value=$3 _db=$4 _settingFile=$5
-    mkdir /etc/dconf/db/${_db}
+	mkdir /etc/dconf/db/${_db}
 	echo "[${_path}]" > /etc/dconf/db/${_db}/${_settingFile}
 	echo "${_setting}=${_value}" >> /etc/dconf/db/${_db}/${_settingFile}
 }
@@ -24,6 +24,6 @@ add_dconf_setting() {
 # $4 _settingFile
 add_dconf_lock(){
 	local _path=$1 _setting=$2 _db=$3 _settingFile=$4
-    mkdir -p /etc/dconf/db/${_db}/locks
+	mkdir -p /etc/dconf/db/${_db}/locks
 	echo "/${_path}/${_setting}" >> /etc/dconf/db/${_db}/locks/${_settingFile}
 }
