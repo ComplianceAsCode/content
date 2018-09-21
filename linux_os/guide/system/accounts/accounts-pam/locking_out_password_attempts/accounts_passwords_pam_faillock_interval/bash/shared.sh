c08b12b3d79cfc8f41ef2d9390f10e827995bff9
Jan Černý <jcerny@redhat.com>
2018-09-21 14:48:36 +0200

    Make the remediation functions more generic
    This function is now used in accounts_passwords_pam_faillock_interval,
    but in future it can be extended to other similar rules.

--- linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_passwords_pam_faillock_interval/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_passwords_pam_faillock_interval/bash/shared.sh
@@ -10,41 +10,43 @@ AUTH_FILES[1]="/etc/pam.d/password-auth"
 
 function insert_preauth {
 	local pam_file="$1"
-	local fail_interval="$2"
+	local option="$2"
+	local value="$3"
 	# is auth required pam_faillock.so preauth present?
 	if grep -qE "^\s*auth\s+required\s+pam_faillock\.so\s+preauth.*$" "$pam_file" ; then
-		# is the fail_interval option set?
-		if grep -qE "^\s*auth\s+required\s+pam_faillock\.so\s+preauth.*fail_interval=([0-9]*).*$" "$pam_file" ; then
-			# just change the value of fail_interval option to a correct value
-			sed -i --follow-symlinks "s/\(^auth.*required.*pam_faillock.so.*preauth.*silent.*\)\(fail_interval *= *\).*/\1\2$fail_interval/" "$pam_file"
-		# fail_interval option is not set.
+		# is the option set?
+		if grep -qE "^\s*auth\s+required\s+pam_faillock\.so\s+preauth.*$option=([0-9]*).*$" "$pam_file" ; then
+			# just change the value of option to a correct value
+			sed -i --follow-symlinks "s/\(^auth.*required.*pam_faillock.so.*preauth.*silent.*\)\($option *= *\).*/\1\2$value/" "$pam_file"
+		# the option is not set.
 		else
-			# append a fail_interval option
-			sed -i --follow-symlinks "/^auth.*required.*pam_faillock.so.*preauth.*silent.*/ s/$/ fail_interval=$fail_interval/" "$_pamFile"
+			# append the option
+			sed -i --follow-symlinks "/^auth.*required.*pam_faillock.so.*preauth.*silent.*/ s/$/ $option=$value/" "$_pamFile"
 		fi
 	# auth required pam_faillock.so preauth is not present, insert the whole line
 	else
-		sed -i --follow-symlinks "/^auth.*sufficient.*pam_unix.so.*/i auth        required      pam_faillock.so preauth silent fail_interval=$fail_interval" "$pam_file"
+		sed -i --follow-symlinks "/^auth.*sufficient.*pam_unix.so.*/i auth        required      pam_faillock.so preauth silent $option=$value" "$pam_file"
 	fi
 }
 
 function insert_authfail {
 	local pam_file="$1"
-	local fail_interval="$2"
+	local option="$2"
+	local value="$3"
 	# is auth default pam_faillock.so authfail present?
 	if grep -qE "^\s*auth\s+(\[default=die\])\s+pam_faillock\.so\s+authfail.*$" "$pam_file" ; then
-		# is the fail_interval option set?
-		if grep -qE "^\s*auth\s+(\[default=die\])\s+pam_faillock\.so\s+authfail.*fail_interval=([0-9]*).*$" "$pam_file" ; then
-			# just change the value of fail_interval option to a correct value
-			sed -i --follow-symlinks "s/\(^auth.*[default=die].*pam_faillock.so.*authfail.*\)\(fail_interval *= *\).*/\1\2$fail_interval/" "$pam_file"
-		# fail_interval option is not set.
+		# is the option set?
+		if grep -qE "^\s*auth\s+(\[default=die\])\s+pam_faillock\.so\s+authfail.*$option=([0-9]*).*$" "$pam_file" ; then
+			# just change the value of option to a correct value
+			sed -i --follow-symlinks "s/\(^auth.*[default=die].*pam_faillock.so.*authfail.*\)\($option *= *\).*/\1\2$value/" "$pam_file"
+		# the option is not set.
 		else
-			# append a fail_interval option
-			sed -i --follow-symlinks "/^auth.*[default=die].*pam_faillock.so.*authfail.*/ s/$/ fail_interval=$fail_interval/" "$_pamFile"
+			# append the option
+			sed -i --follow-symlinks "/^auth.*[default=die].*pam_faillock.so.*authfail.*/ s/$/ $option=$value/" "$_pamFile"
 		fi
 	# auth default pam_faillock.so authfail is not present, insert the whole line
 	else
-		sed -i --follow-symlinks "/^auth.*sufficient.*pam_unix.so.*/a auth        [default=die] pam_faillock.so authfail fail_interval=$fail_interval" "$pam_file"
+		sed -i --follow-symlinks "/^auth.*sufficient.*pam_unix.so.*/a auth        [default=die] pam_faillock.so authfail $option=$value" "$pam_file"
 	fi
 }
 
@@ -55,15 +57,16 @@ function insert_account {
 	fi
 }
 
-function set_faillock_in_pam_file {
+function set_faillock_option_in_pam_file {
 	local pam_file="$1"
-	local fail_interval="$2"
-	insert_preauth "$pam_file" "$fail_interval"
-	insert_authfail "$pam_file" "$fail_interval"
+	local option="$2"
+	local value="$3"
+	insert_preauth "$pam_file" "$option" "$value"
+	insert_authfail "$pam_file" "$option" "$value"
 	insert_account "$pam_file"
 }
 
 for pam_file in "${AUTH_FILES[@]}"
 do
-	set_faillock_in_pam_file "$pam_file" "$var_accounts_passwords_pam_faillock_fail_interval"
+	set_faillock_option_in_pam_file "$pam_file" "fail_interval" "$var_accounts_passwords_pam_faillock_fail_interval"
 done
