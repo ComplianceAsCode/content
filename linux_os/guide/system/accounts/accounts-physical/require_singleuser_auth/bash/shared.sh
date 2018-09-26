7485aba23c348348cda67a79866ca270088c0615
Jan Černý <jcerny@redhat.com>
2018-09-26 09:20:16 +0200

    Reuse variables in Fedora and RHEL7 remediation

--- linux_os/guide/system/accounts/accounts-physical/require_singleuser_auth/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-physical/require_singleuser_auth/bash/shared.sh
@@ -2,20 +2,19 @@
 
 {{% if init_system == "systemd" -%}}
 
-{{% if product == "fedora" -%}}
 service_file="/usr/lib/systemd/system/rescue.service"
+
+{{% if product == "fedora" -%}}
 sulogin="/usr/lib/systemd/systemd-sulogin-shell"
+{{%- else -%}}
+sulogin="/sbin/sulogin"
+{{%- endif %}}
+
 if grep "^ExecStart=.*" "$service_file" ; then
     sed -i "s%^ExecStart=.*%ExecStart=-$sulogin rescue%" "$service_file"
 else
     echo "ExecStart=-$sulogin rescue" >> "$service_file"
 fi
-{{%- else -%}}
-grep -q "^ExecStart=\-.*/sbin/sulogin" /usr/lib/systemd/system/rescue.service
-if ! [ $? -eq 0 ]; then
-    sed -i "s/ExecStart=-.*-c \"/&\/sbin\/sulogin; /g" /usr/lib/systemd/system/rescue.service
-fi
-{{%- endif -%}}
 
 {{%- else -%}}
 grep -q ^SINGLE /etc/sysconfig/init && \
