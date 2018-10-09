80299e87231e1fbfdc7b77fcc1a842a7adfad905
Watson Sato <wsato@redhat.com>
2018-10-09 15:54:29 -0300

    Improve robustness of ensure_gpgcheck_never_disabled
    Match spaces between "gpgcheck" and "="

--- linux_os/guide/system/software/updating/ensure_gpgcheck_never_disabled/bash/shared.sh
+++ linux_os/guide/system/software/updating/ensure_gpgcheck_never_disabled/bash/shared.sh
@@ -1,2 +1,2 @@
 # platform = multi_platform_rhel,multi_platform_ol,multi_platform_fedora
-sed -i 's/gpgcheck=.*/gpgcheck=1/g' /etc/yum.repos.d/*
+sed -i 's/gpgcheck\s*=.*/gpgcheck=1/g' /etc/yum.repos.d/*
