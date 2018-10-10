54473da38b698c49569fcf1c063b48346208c4f6
Shawn Wells <shawn@redhat.com>
2018-10-10 17:11:03 -0400

    Ansible/bash/oval for etcd_peer_auto_tls

---
+++ applications/openshift/etcd/etcd_peer_auto_tls/bash/shared.sh
@@ -0,0 +1,7 @@
+# platform = multi_platform_rhel, multi_platform_ocp
+#
+# Include source function library
+. /usr/share/scap-security-guide/remediation_functions
+populate var_etcd_peer_auto_tls
+
+replace_or_append '/etc/etcd/etcd.conf' '^ETCD_PEER_AUTO_TLS=' $var_etcd_peer_auto_tls '@CCENUM@' '%s=%s'
