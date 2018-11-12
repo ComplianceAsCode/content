e3818ba00f5fe2324b8ce0689dea4979c5792c0a
Watson Sato <wsato@redhat.com>
2018-11-12 15:02:26 +0100

    Fix and improve metadata of remediations scripts
    Fix metada for Ansible and Bash remediations for rule
    configure_kerberos_crypto_policy.

--- linux_os/guide/system/software/integrity/crypto/configure_kerberos_crypto_policy/bash/fedora.sh
+++ linux_os/guide/system/software/integrity/crypto/configure_kerberos_crypto_policy/bash/fedora.sh
@@ -1,4 +1,8 @@
 # platform = multi_platform_fedora
+# reboot = true
+# strategy = configure
+# complexity = low
+# disruption = low
 
 rm -f /etc/krb5.conf.d/crypto-policies
 ln -s /etc/crypto-policies/back-ends/krb5.config /etc/krb5.conf.d/crypto-policies
