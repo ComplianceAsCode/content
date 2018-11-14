e305dcb1f9910989734f68e3c9d98d0e9213fe6e
Jan Černý <jcerny@redhat.com>
2018-11-14 10:57:04 +0100

    Fix auditd_audispd_syslog_plugin_activated on Fedora and RHEL8
    In Audit 3.0 which is present in Fedora >= 29 and RHEL8, the
    `syslog.conf` moved to `/etc/audit/plugins.d/`.

--- linux_os/guide/system/auditing/configure_auditd_data_retention/auditd_audispd_syslog_plugin_activated/bash/shared.sh
+++ linux_os/guide/system/auditing/configure_auditd_data_retention/auditd_audispd_syslog_plugin_activated/bash/shared.sh
@@ -2,6 +2,10 @@
 . /usr/share/scap-security-guide/remediation_functions
 var_syslog_active="yes"
 
+{{% if product in ["rhel8", "fedora"] %}}
+AUDISP_SYSLOGCONFIG=/etc/audit/plugins.d/syslog.conf
+{{% else %}}
 AUDISP_SYSLOGCONFIG=/etc/audisp/plugins.d/syslog.conf
+{{% endif %}}
 
 replace_or_append $AUDISP_SYSLOGCONFIG '^active' "$var_syslog_active" "@CCENUM@"
