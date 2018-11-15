7b11fee089dd37686c685bcc70b3a74f4f19d3c0
Milan Lysonek <mlysonek@redhat.com>
2018-11-15 14:48:34 +0100

    Update Configure crypto policy rule to check symlinks and add tests for it

---
+++ tests/data/group_system/group_software/group_integrity/group_crypto/rule_configure_crypto_policy/policy_set.pass.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# profiles = xccdf_org.ssgproject.content_profile_standard
+
+update-crypto-policies --set "DEFAULT"
---
+++ tests/data/group_system/group_software/group_integrity/group_crypto/rule_configure_crypto_policy/wrong_policy.fail.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# profiles = xccdf_org.ssgproject.content_profile_standard
+
+sed -i "1s/.*/LEGACY/" /etc/crypto-policies/config
