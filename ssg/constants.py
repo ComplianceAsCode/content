76a730649b58fc824ec972b626875a94c45380c5
Gabe <redhatrises@gmail.com>
2018-11-19 11:18:39 -0700

    Add rhel8 derivatives

--- ssg/constants.py
+++ ssg/constants.py
@@ -80,11 +80,13 @@ PKG_MANAGER_TO_CONFIG_FILE = {
 RHEL_CENTOS_CPE_MAPPING = {
     "cpe:/o:redhat:enterprise_linux:6": "cpe:/o:centos:centos:6",
     "cpe:/o:redhat:enterprise_linux:7": "cpe:/o:centos:centos:7",
+    "cpe:/o:redhat:enterprise_linux:8": "cpe:/o:centos:centos:8",
 }
 
 RHEL_SL_CPE_MAPPING = {
     "cpe:/o:redhat:enterprise_linux:6": "cpe:/o:scientificlinux:scientificlinux:6",
     "cpe:/o:redhat:enterprise_linux:7": "cpe:/o:scientificlinux:scientificlinux:7",
+    "cpe:/o:redhat:enterprise_linux:8": "cpe:/o:scientificlinux:scientificlinux:8",
 }
 
 CENTOS_NOTICE = \
