479b32839e4b62122f8b91759411a6e10def3981
Watson Sato <wsato@redhat.com>
2018-11-27 10:24:39 +0100

    De-duplicate RHEL8 platform

--- linux_os/guide/system/accounts/accounts-session/accounts_tmout/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-session/accounts_tmout/bash/shared.sh
@@ -1,4 +1,4 @@
-# platform = Red Hat Enterprise Linux 7,Red Hat Enterprise Linux 8,Red Hat Enterprise Linux 8,multi_platform_fedora
+# platform = Red Hat Enterprise Linux 7,Red Hat Enterprise Linux 8,multi_platform_fedora
 . /usr/share/scap-security-guide/remediation_functions
 populate var_accounts_tmout
 
