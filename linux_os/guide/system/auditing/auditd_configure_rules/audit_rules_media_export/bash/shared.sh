7456559daac9d42a349f3132963b84df600125a5
Watson Sato <wsato@redhat.com>
2018-12-03 16:16:08 +0100

    Use unset instead of 4294967295 in audit rules

--- linux_os/guide/system/auditing/auditd_configure_rules/audit_rules_media_export/bash/shared.sh
+++ linux_os/guide/system/auditing/auditd_configure_rules/audit_rules_media_export/bash/shared.sh
@@ -9,9 +9,9 @@
 
 for ARCH in "${RULE_ARCHS[@]}"
 do
-	PATTERN="-a always,exit -F arch=$ARCH -S .* -F auid>={{{ auid }}} -F auid!=4294967295 -k *"
+	PATTERN="-a always,exit -F arch=$ARCH -S .* -F auid>={{{ auid }}} -F auid!=unset -k *"
 	GROUP="mount"
-	FULL_RULE="-a always,exit -F arch=$ARCH -S mount -F auid>={{{ auid }}} -F auid!=4294967295 -k export"
+	FULL_RULE="-a always,exit -F arch=$ARCH -S mount -F auid>={{{ auid }}} -F auid!=unset -k export"
 	# Perform the remediation for both possible tools: 'auditctl' and 'augenrules'
 	fix_audit_syscall_rule "auditctl" "$PATTERN" "$GROUP" "$ARCH" "$FULL_RULE"
 	fix_audit_syscall_rule "augenrules" "$PATTERN" "$GROUP" "$ARCH" "$FULL_RULE"
