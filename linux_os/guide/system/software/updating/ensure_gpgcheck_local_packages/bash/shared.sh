1fa57a820269b4a5119820ca16fef624bf209b4e
Ilya Okomin <ilya.okomin@oracle.com>
2018-12-21 16:51:01 +0300

    Add OL support to ensure_gpgcheck_local_packages rule
    Signed-off-by: Ilya Okomin <ilya.okomin@oracle.com>

--- linux_os/guide/system/software/updating/ensure_gpgcheck_local_packages/bash/shared.sh
+++ linux_os/guide/system/software/updating/ensure_gpgcheck_local_packages/bash/shared.sh
@@ -1,4 +1,4 @@
-# platform = Red Hat Enterprise Linux 7,Red Hat Enterprise Linux 8,multi_platform_fedora
+# platform = Red Hat Enterprise Linux 7,Red Hat Enterprise Linux 8,multi_platform_fedora,multi_platform_ol
 . /usr/share/scap-security-guide/remediation_functions
 
 replace_or_append '{{{ pkg_manager_config_file }}}' '^localpkg_gpgcheck' '1' '@CCENUM@'
