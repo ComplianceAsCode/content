e43c26bbcbedf32607a5c997b786b48973df2bcf
Watson Sato <wsato@redhat.com>
2019-02-11 17:47:51 +0100

    Add negative test for crypto-policy nss.config

---
+++ tests/data/group_system/group_software/group_integrity/group_crypto/rule_configure_crypto_policy/missing_nss_config.fail.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# platform = multi_platform_fedora, Red Hat Enterprise Linux 8
+# profiles = xccdf_org.ssgproject.content_profile_ospp
+
+update-crypto-policies --set "FIPS"
+
+rm -f "/etc/crypto-policies/back-ends/nss.config"
