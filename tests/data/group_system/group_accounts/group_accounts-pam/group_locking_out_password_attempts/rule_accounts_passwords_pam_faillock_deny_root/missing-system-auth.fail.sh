36b3f0a7f88886789eb65f3b550eafd64869caf9
Jan Černý <jcerny@redhat.com>
2019-02-18 10:02:45 +0100

    Remove test scenario missing-system-auth.fail.sh
    This scenario is completely invalid. We have some rules, where missing
    config file is finding that is fixable. Usually it's to override some
    default values. PAM configurations are not for overriding defaults, but
    to define behaviour. So in case system-auth is missing, either machine
    is completely broken and in very unsupported configuration, or not using
    PAM. We shouldn't try to fix it in such case.

--- tests/data/group_system/group_accounts/group_accounts-pam/group_locking_out_password_attempts/rule_accounts_passwords_pam_faillock_deny_root/missing-system-auth.fail.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# profiles = xccdf_org.ssgproject.content_profile_nist-800-171-cui, xccdf_org.ssgproject.content_profile_ospp
-# remediation = bash
-
-. set-up-pamd.sh
-
-set-up-pamd
-rm -f /etc/pam.d/system-auth
