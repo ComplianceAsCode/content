b32032da87d7eef694d04080320c3395cd0d5495
Matus Marhefka <mmarhefk@redhat.com>
2019-03-08 18:41:10 +0100

    Added remediations for rhel8 audit rules which need ordering
    * Remediations added by this commit need to configure audit
      rules in the /etc/audit/rules.d/ and preserve their order.
    * The remediations are cherrypicking lines from the
      /usr/share/doc/audit/rules/30-ospp-v42.rules master file which
      belongs to the audit package and add them into a file in the
      /etc/audit/rules.d/ folder preserving the specific order from
      the master file.

---
+++ linux_os/guide/system/auditing/auditd_configure_rules/audit_unsuccessful_file_modification/audit_rules_unsuccessful_file_modification_openat_rule_order/bash/rhel8.sh
@@ -0,0 +1,18 @@
+# platform = Red Hat Enterprise Linux 8
+
+# Include source function library.
+. /usr/share/scap-security-guide/remediation_functions
+include_merge_files_by_lines
+
+MASTER_FILE="/usr/share/doc/audit/rules/30-ospp-v42.rules"
+SAMPLE_FILE="/etc/audit/rules.d/30-ospp-v42-remediation.rules"
+
+# First perform the remediation of the syscall rule
+# Retrieve hardware architecture of the underlying system
+if [ "$(getconf LONG_BIT)" = "32" ]; then
+	# 32 bit rules
+	merge_first_lines_to_second_on_indices $MASTER_FILE $SAMPLE_FILE "6 10 20 24"
+else
+	# 32 bit and 64 bit rules
+	merge_first_lines_to_second_on_indices $MASTER_FILE $SAMPLE_FILE "6 7 10 11 20 21 24 25"
+fi
