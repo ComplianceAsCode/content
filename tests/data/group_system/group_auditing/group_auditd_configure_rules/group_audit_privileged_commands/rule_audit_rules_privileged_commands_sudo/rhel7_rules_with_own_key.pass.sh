2476a35d0ad4055d52c33c03bb82031f6f19c794
Watson Sato <wsato@redhat.com>
2019-03-15 17:11:37 +0100

    Enable privileged_commands test to run on Fedora
    Also create audit rules directory, to ensure scenario setup always
    works.

--- tests/data/group_system/group_auditing/group_auditd_configure_rules/group_audit_privileged_commands/rule_audit_rules_privileged_commands_sudo/rhel7_rules_with_own_key.pass.sh
+++ tests/data/group_system/group_auditing/group_auditd_configure_rules/group_audit_privileged_commands/rule_audit_rules_privileged_commands_sudo/rhel7_rules_with_own_key.pass.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # profiles = xccdf_org.ssgproject.content_profile_ospp
 # remediation = bash
+# platform = Red Hat Enterprise Linux 7,Fedora
 
 echo "-a always,exit -F path=/usr/bin/sudo -F perm=x -F auid>=1000 -F auid!=4294967295 -k own_key" >> /etc/audit/rules.d/privileged.rules
-# This is a trick to fail setup of this test in rhel6 systems
-ls /usr/lib/systemd/system/auditd.service
