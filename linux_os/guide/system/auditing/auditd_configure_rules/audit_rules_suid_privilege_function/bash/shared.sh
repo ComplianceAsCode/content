bdf0ea4fc10201f92a3a0bc2d03e336de3460402
Malte Kraus <malte.kraus@suse.com>
2019-03-22 15:32:26 +0100

    fix SLE12 audit rules for augenrules

--- linux_os/guide/system/auditing/auditd_configure_rules/audit_rules_suid_privilege_function/bash/shared.sh
+++ linux_os/guide/system/auditing/auditd_configure_rules/audit_rules_suid_privilege_function/bash/shared.sh
@@ -3,12 +3,13 @@
 IFS='
 '
 
-set -x
-
 for fs in $(df --local --output=target | tail -n +2) ; do
     for f in $(find "$fs" -xdev -type f \( -perm -4000 -o -perm -2000 \) \( -perm -100 -o -perm -10 -o -perm -1 \) ) ; do
-        if ! grep -qF -e "-w $f -p xwa -k privilege_function" /etc/audit/audit.rules ; then
+        if ! grep -qP -e '^\s*(?:-w |-F path=)'"$f"' (?:-p |-F perm=)(?:xwa|wxa|wax|xaw|axw|awx) (?:-k |-F key=)\w+\s*$' /etc/audit/audit.rules ; then
             printf "\n%s" "-w $f -p xwa -k privilege_function" >> /etc/audit/audit.rules
+        fi
+        if ! grep -RqP -e '^\s*(?:-w |-F path=)'"$f"' (?:-p |-F perm=)(?:xwa|wxa|wax|xaw|axw|awx) (?:-k |-F key=)\w+\s*$' /etc/audit/rules.d ; then
+            printf "\n%s" "-w $f -p xwa -k privilege_function" >> /etc/audit/rules.d/setxid.rules
         fi
     done
 done
