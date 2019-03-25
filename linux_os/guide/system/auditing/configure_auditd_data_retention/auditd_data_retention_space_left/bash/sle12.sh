ed40a7b7efb88a6a2f9761cbcefa1dafb4cbbf75
Malte Kraus <malte.kraus@suse.com>
2019-03-25 15:28:40 +0100

    auditd_data_retention_space_left fix SLE12 remediation

--- linux_os/guide/system/auditing/configure_auditd_data_retention/auditd_data_retention_space_left/bash/sle12.sh
+++ linux_os/guide/system/auditing/configure_auditd_data_retention/auditd_data_retention_space_left/bash/sle12.sh
@@ -1,15 +1,16 @@
 # platform = SUSE Linux Enterprise 12
 
+set -x
 
 divide_round_up() {
-    dividend="$0"
-    divisor="$1"
+    dividend="$1"
+    divisor="$2"
 
     # to round up instead of truncate the result, we add (divisor - 1) to the dividend
-    echo $((("$dividend" + "$divisor" - 1) / "$divisor"))
+    echo $((($dividend + $divisor - 1) / $divisor))
 }
 
-partition_size=$(df -B1M --output=size /var/log/audit | tail -n1)
+partition_size=$(df -B1M --output=size /var/log/audit | awk 'NR==2 {print $1}')
 # threshold is 1 quarter of the partition size
 space_left=$(divide_round_up "$partition_size" 4)
 
