e4c67a13a125cb7957985249d27df48c727464c9
Malte Kraus <malte.kraus@suse.com>
2019-04-18 10:26:36 +0200

    introduce var_sshd_priv_separation

--- linux_os/guide/services/ssh/ssh_server/sshd_use_priv_separation/bash/sle12.sh
+++
@@ -1,6 +0,0 @@
-# platform = SUSE Linux Enterprise 12
-
-# Include source function library.
-. /usr/share/scap-security-guide/remediation_functions
-
-replace_or_append '/etc/ssh/sshd_config' '^UsePrivilegeSeparation' 'yes' '@CCENUM@' '%s %s'
