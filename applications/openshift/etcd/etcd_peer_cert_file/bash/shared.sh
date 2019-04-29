aeee64110c763f33e4ad411345d1fa7549236ac9
Shawn Wells <shawn@redhat.com>
2019-04-29 15:41:49 -0400

    ETCD_PEER_CERT_FILE uses '=' as separator, not space

--- applications/openshift/etcd/etcd_peer_cert_file/bash/shared.sh
+++ applications/openshift/etcd/etcd_peer_cert_file/bash/shared.sh
@@ -3,4 +3,4 @@
 # Include source function library
 . /usr/share/scap-security-guide/remediation_functions
 
-replace_or_append '/etc/etcd/etcd.conf' '^ETCD_PEER_CERT_FILE' /etc/etcd/peer.crt '@CCENUM@' '%s=%s'
+replace_or_append '/etc/etcd/etcd.conf' '^ETCD_PEER_CERT_FILE=' /etc/etcd/peer.crt '@CCENUM@' '%s=%s'
