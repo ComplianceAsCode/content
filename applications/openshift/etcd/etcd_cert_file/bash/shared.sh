50a314c163d50365186e40438e04232721b31b4a
Shawn Wells <shawn@redhat.com>
2019-04-29 15:47:20 -0400

    update etcd_cert_file to use equal sign

--- applications/openshift/etcd/etcd_cert_file/bash/shared.sh
+++ applications/openshift/etcd/etcd_cert_file/bash/shared.sh
@@ -3,4 +3,4 @@
 # Include source function library
 . /usr/share/scap-security-guide/remediation_functions
 
-replace_or_append '/etc/etcd/etcd.conf' '^ETCD_CERT_FILE' /etc/etcd/server.crt '@CCENUM@' '%s=%s'
+replace_or_append '/etc/etcd/etcd.conf' '^ETCD_CERT_FILE=' /etc/etcd/server.crt '@CCENUM@' '%s=%s'
