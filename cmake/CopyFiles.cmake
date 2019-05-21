2aaab29d23267c2babd0f10d741d7ffb754c0d8b
Watson Sato <wsato@redhat.com>
2019-05-21 09:55:09 +0200

    Add comment to CopyFiles script

--- cmake/CopyFiles.cmake
+++ cmake/CopyFiles.cmake
@@ -1,3 +1,4 @@
+# This script is used to overcome copy limitations of cmake 2.8.12
 file(GLOB CSOURCE ${SOURCE})
 FOREACH(file_to_copy ${CSOURCE})
         execute_process(COMMAND "${CMAKE_COMMAND}" -E copy ${file_to_copy} "${DEST}")
