574e3baad07232b2c858af2445cc4442c2a33e3f
Keith Jackson <keithkjackson@gmail.com>
2019-05-21 09:52:42 -0400

    Nova checks for service and containerized service created.

--- applications/openstack/nova_docker/nova_file_perms/bash/rhel-osp7.sh
+++ applications/openstack/nova_docker/nova_file_perms/bash/rhel-osp7.sh
@@ -1,5 +1,5 @@
 # platform = Red Hat Enterprise Linux 7
-chmod 640 /etc/nova/nova.conf
-chmod 640 /etc/nova/api-paste.ini
-chmod 640 /etc/nova/policy.json
-chmod 640 /etc/nova/rootwrap.conf
+chmod 640 /var/lib/config-data/puppet-generated/nova/etc/nova/nova.conf
+chmod 640 /var/lib/config-data/puppet-generated/nova/etc/nova/api-paste.ini
+chmod 640 /var/lib/config-data/puppet-generated/nova/etc/nova/policy.json
+chmod 640 /var/lib/config-data/puppet-generated/nova/etc/nova/rootwrap.conf
--- applications/openstack/nova_docker/nova_use_keystone/bash/rhel-osp7.sh
+++ applications/openstack/nova_docker/nova_use_keystone/bash/rhel-osp7.sh
@@ -1,2 +1,2 @@
 # platform = Red Hat Enterprise Linux 7
-openstack-config --set /etc/nova/nova.conf DEFAULT auth_strategy keystone
+openstack-config --set /var/lib/config-data/puppet-generated/nova/etc/nova/nova.conf DEFAULT auth_strategy keystone
