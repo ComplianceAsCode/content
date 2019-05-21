7bc3f0f923713a0e0be707d24659cede84edc391
Ilya Okomin <ilya.okomin@oracle.com>
2019-05-21 13:49:19 +0300

    Update ensure_oracle_gpgkey_installed for OL8
    - add new Oracle Linux 8 GPG key support
    - fix gpg fingerprint handling for OL8 in bash script
    
    Signed-off-by: Ilya Okomin <ilya.okomin@oracle.com>

--- linux_os/guide/system/software/updating/ensure_oracle_gpgkey_installed/bash/shared.sh
+++ linux_os/guide/system/software/updating/ensure_oracle_gpgkey_installed/bash/shared.sh
@@ -1,7 +1,9 @@
 # platform = multi_platform_ol
 # OL fingerprints below retrieved from Oracle Linux Yum Server "Frequently Asked Questions"
 # https://yum.oracle.com/faq.html#a10
-readonly OL_FINGERPRINT="4214 4123 FECF C55B 9086 313D 72F9 7B74 EC55 1F03"
+readonly OL_FINGERPRINT="42144123FECFC55B9086313D72F97B74EC551F03"
+readonly OL8_FINGERPRINT="76FD3DB13AB67410B89DB10E82562EA9AD986DA3"
+
 # Location of the key we would like to import (once it's integrity verified)
 readonly OL_RELEASE_KEY="/etc/pki/rpm-gpg/RPM-GPG-KEY-oracle"
 
@@ -12,21 +14,16 @@ if [ "${RPM_GPG_DIR_PERMS}" -le "755" ]
 then
   # If they are safe, try to obtain fingerprints from the key file
   # (to ensure there won't be e.g. CRC error)
-  IFS=$'\n' GPG_OUT=($(gpg --with-fingerprint "${OL_RELEASE_KEY}"))
+  IFS=$'\n' GPG_OUT=($(gpg --with-fingerprint --with-colons "$OL_RELEASE_KEY" | grep "^fpr" | cut -d ":" -f 10))
   GPG_RESULT=$?
   # No CRC error, safe to proceed
   if [ "${GPG_RESULT}" -eq "0" ]
   then
-    for ITEM in "${GPG_OUT[@]}"
-    do
-      # Filter just hexadecimal fingerprints from gpg's output from
-      # processing of a key file
-      RESULT=$(echo ${ITEM} | sed -n "s/[[:space:]]*Key fingerprint = \(.*\)/\1/p" | tr -s '[:space:]')
-      # If fingerprint matches Oracle Linux 6 and 7 key import the key
-      if [[ ${RESULT} ]] && [[ ${RESULT} = "${OL_FINGERPRINT}" ]] 
-      then
-        rpm --import "${OL_RELEASE_KEY}"
-      fi
-    done
+    # Filter just hexadecimal fingerprints from gpg's output from
+    # processing of a key file
+    echo "${GPG_OUT[*]}" | grep -vE "${OL_FINGERPRINT}|${OL8_FINGERPRINT}" || {
+      # If $ OL_RELEASE_KEY file doesn't contain any keys with unknown fingerprint, import it
+      rpm --import "${OL_RELEASE_KEY}"
+    }
   fi
 fi
