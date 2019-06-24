27e0b9ba449f0bd78040d45b409513f8c90632ec
Watson Sato <wsato@redhat.com>
2019-06-24 14:14:29 +0200

    Add rule and profile content prefixes to constants

--- ssg/constants.py
+++ ssg/constants.py
@@ -38,7 +38,9 @@ ssg_version_uri = \
     "https://github.com/OpenSCAP/scap-security-guide/releases/latest"
 OSCAP_VENDOR = "org.ssgproject"
 OSCAP_DS_STRING = "xccdf_%s.content_benchmark_" % OSCAP_VENDOR
+OSCAP_PROFILE = "xccdf_%s.content_profile_" % OSCAP_VENDOR
 OSCAP_GROUP = "xccdf_%s.content_group_" % OSCAP_VENDOR
+OSCAP_RULE = "xccdf_%s.content_rule_" % OSCAP_VENDOR
 OSCAP_GROUP_PCIDSS = "xccdf_%s.content_group_pcidss-req" % OSCAP_VENDOR
 OSCAP_GROUP_VAL = "xccdf_%s.content_group_values" % OSCAP_VENDOR
 OSCAP_GROUP_NON_PCI = "xccdf_%s.content_group_non-pci-dss" % OSCAP_VENDOR
