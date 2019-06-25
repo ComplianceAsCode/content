aacab44e7d6a314aa0bdcec9aada8c0c9dc99903
Gabriel Becker <ggasparb@redhat.com>
2019-06-25 15:10:07 +0200

    Change profile in minlen test scenarios.

--- tests/data/group_system/group_accounts/group_accounts-restrictions/group_password_expiration/rule_accounts_password_minlen_login_defs/password_minlen_commented.fail.sh
+++ tests/data/group_system/group_accounts/group_accounts-restrictions/group_password_expiration/rule_accounts_password_minlen_login_defs/password_minlen_commented.fail.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# profiles = xccdf_org.ssgproject.content_profile_standard
+# profiles = xccdf_org.ssgproject.content_profile_ospp
 # platform = multi_platform_fedora
 
 sed -i "s/.*PASS_MIN_LEN.*/#PASS_MIN_LEN 12/" /etc/login.defs
--- tests/data/group_system/group_accounts/group_accounts-restrictions/group_password_expiration/rule_accounts_password_minlen_login_defs/password_minlen_not_there.fail.sh
+++ tests/data/group_system/group_accounts/group_accounts-restrictions/group_password_expiration/rule_accounts_password_minlen_login_defs/password_minlen_not_there.fail.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# profiles = xccdf_org.ssgproject.content_profile_standard
+# profiles = xccdf_org.ssgproject.content_profile_ospp
 # platform = multi_platform_fedora
 
 sed -i "/^PASS_MIN_LEN.*/d" /etc/login.defs
