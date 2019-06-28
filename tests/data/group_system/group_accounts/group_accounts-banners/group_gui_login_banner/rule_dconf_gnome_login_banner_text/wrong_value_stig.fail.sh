668be85d86747d0adc3af45047bc51161cd97dfe
Gabriel Becker <ggasparb@redhat.com>
2019-06-28 14:49:25 +0200

    Add test scenarios for dconf_gnome_login_banner_text in RHEL7 stig profile.

---
+++ tests/data/group_system/group_accounts/group_accounts-banners/group_gui_login_banner/rule_dconf_gnome_login_banner_text/wrong_value_stig.fail.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# profiles = xccdf_org.ssgproject.content_profile_stig
+
+. ../../../../group_software/group_gnome/dconf_test_functions.sh
+
+install_dconf_and_gdm_if_needed
+
+login_banner_text="Wrong Banner Text"
+expanded=$(echo "$login_banner_text" | sed 's/(\\\\\x27)\*/\\\x27/g;s/(\\\x27)\*//g;s/(\\\\\x27)/tamere/g;s/(\^\(.*\)\$|.*$/\1/g;s/\[\\s\\n\][+*]/ /g;s/\\//g;s/(n)\*/\\n/g;s/\x27/\\\x27/g;')
+
+clean_dconf_settings
+add_dconf_setting "org/gnome/login-screen" "banner-message-text" "'${expanded}'" "gdm.d" "00-security-settings"
+add_dconf_lock "org/gnome/login-screen" "banner-message-text" "gdm.d" "00-security-settings-lock"
+
+dconf update
