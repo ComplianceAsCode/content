b687a68f5ca8c328263dfef0bb5ce9f67a60c8b4
Watson Sato <wsato@redhat.com>
2019-07-11 16:51:38 +0200

    Make message for rules and vars independent

--- build-scripts/profile_tool.py
+++ build-scripts/profile_tool.py
@@ -123,10 +123,12 @@ def main():
 
         exclusive_rules = len(subtracted_profile.get_rule_selectors())
         exclusive_vars = len(subtracted_profile.get_variable_selectors())
-        if exclusive_rules > 0 or exclusive_vars > 0:
+        if exclusive_rules > 0:
             print("{} rules were left after subtraction.".format(exclusive_rules))
+        if  exclusive_vars > 0:
             print("{} variables were left after subtraction.".format(exclusive_vars))
 
+        if exclusive_rules > 0 or exclusive_vars > 0:
             profile1_basename = os.path.splitext(
                 os.path.basename(args.profile1))[0]
             profile2_basename = os.path.splitext(
