b457ba1cf5ea6043a501ecc45f7a54c4de61b372
Jan Černý <jcerny@redhat.com>
2019-07-22 15:26:48 +0200

    Compare suid/sgid files with the RPM database
    It is difficult to maintain the list to list paths of all possible suid
    and sgid binaries in a Linux distribution. Instead, we can check if the
    suid or sgid file is owned by an RPM package by consulting the RPM
    database. Another advantage of this solution is that we can have a
    single OVAL for all RPM-related Linux distributions.  The patch modifies
    OVAL for rules file_permissions_unauthorized_suid and
    file_permissions_unauthorized_sgid and also adds test scenarios for
    these rules.
    Fixes: https://bugzilla.redhat.com/show_bug.cgi?id=1693026

---
+++ linux_os/guide/system/permissions/files/file_permissions_unauthorized_suid/tests/unpackaged_suid.fail.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# profiles = xccdf_org.ssgproject.content_profile_standard
+# remediation = none
+
+for x in $(find / -perm /u=s) ; do
+	if ! rpm -qf $x ; then
+		rm -rf $x
+	fi
+done
+
+touch /usr/bin/suid_binary
+chmod u+xs /usr/bin/suid_binary
