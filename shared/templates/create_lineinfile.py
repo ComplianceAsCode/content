c7153f8052145ab64e754677963f5651a1a51f28
Gabriel Becker <ggasparb@redhat.com>
2019-07-29 11:35:49 +0200

    Fix PEP8 issues.

--- shared/templates/create_lineinfile.py
+++ shared/templates/create_lineinfile.py
@@ -7,13 +7,14 @@ import sys
 
 from template_common import FilesGenerator, UnknownTargetError, CSVLineError
 
+
 class LineinfileGenerator(FilesGenerator):
     def __init__(self, type=None):
         super(LineinfileGenerator, self).__init__()
         self.type = 'generic' if type is None else type
 
     def generate(self, target, lineinfileinfo):
-        try:            
+        try:
             # missing parameter pass is false by default
             missing_parameter_pass = False
 
@@ -31,11 +32,14 @@ class LineinfileGenerator(FilesGenerator):
                 elif missing_parameter_pass == "false":
                     missing_parameter_pass = False
                 else:
-                    raise ValueError("Unrecognized option for missing_parameter_pass parameter ({}). ".format(missing_parameter_pass) +
-                                     "Possible values are: true or false.")
+                    raise ValueError(
+                        "Unrecognized value for missing_parameter_pass (%s). "
+                        "Possible values are: true or false." % missing_parameter_pass)
+            else:
+                raise CSVLineError()
         except ValueError as e:
-            print("\tEntry: %s\n" % lineinfileinfo)
-            print("\tError unpacking rule_id, parameter, value and missing_parameter_pass: " + str(e))
+            sys.stderr.write("Error unpacking rule_id, parameter, "
+                             "value and missing_parameter_pass: %s\n" % str(e))
             raise CSVLineError()
 
         if target == "oval":
@@ -43,7 +47,7 @@ class LineinfileGenerator(FilesGenerator):
                 "./template_OVAL_%s_lineinfile" % self.type,
                 {
                     "products": "all",
-                    "rule_title" : "Rule title of " + rule_id,
+                    "rule_title": "Rule title of " + rule_id,
                     "rule_id": rule_id,
                     "PARAMETER": parameter,
                     "VALUE": value,
@@ -65,7 +69,7 @@ class LineinfileGenerator(FilesGenerator):
             self.file_from_template(
                 "./template_ANSIBLE_%s_lineinfile" % self.type,
                 {
-                    "rule_title" : "Rule title of " + rule_id,
+                    "rule_title": "Rule title of " + rule_id,
                     "PARAMETER": parameter,
                     "VALUE": value
                 },
