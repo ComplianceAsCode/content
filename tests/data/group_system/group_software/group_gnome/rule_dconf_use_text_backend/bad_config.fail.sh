d9645825afb9ea38ae749e0c5ba078dd6dd3c4a4
Gabriel Becker <ggasparb@redhat.com>
2019-08-07 09:47:16 +0200

    Delete unused/unwanted dconf_use_text_backend rule.

--- tests/data/group_system/group_software/group_gnome/rule_dconf_use_text_backend/bad_config.fail.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# profiles = xccdf_org.ssgproject.content_profile_ospp
-
-yum -y install dconf
-
-mkdir -p /etc/dconf/profile
-
-echo "Something stupid" > /etc/dconf/profile/user
-echo "service-db:keyfile/user" >> /etc/dconf/profile/user
