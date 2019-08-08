5a42d46d365c3b36ab3cf55106a7b28527c4cd8d
Milan Lysonek <mlysonek@redhat.com>
2019-08-08 09:25:36 +0200

    Change logging prefix for missing profile in test suite from "ERROR" to "WARNING".

--- tests/ssg_test_suite/rule.py
+++ tests/ssg_test_suite/rule.py
@@ -47,7 +47,7 @@ def get_viable_profiles(selected_profiles, datastream, benchmark):
                 valid_profiles += [ds_profile]
 
     if not valid_profiles:
-        logging.error('No profile ends with "{0}"'
+        logging.warning('No profile ends with "{0}"'
                       .format(", ".join(selected_profiles)))
     return valid_profiles
 
