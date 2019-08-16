cb665064d0f8b8e0b9d9f325bfbaf2ab09b1654e
Jan Černý <jcerny@redhat.com>
2019-08-16 09:42:33 +0200

    Get rid of negation and simplify the code
    Also remove redundant backticks.

--- linux_os/guide/services/ntp/service_chronyd_or_ntpd_enabled/bash/shared.sh
+++ linux_os/guide/services/ntp/service_chronyd_or_ntpd_enabled/bash/shared.sh
@@ -1,13 +1,13 @@
 # platform = Red Hat Enterprise Linux 7,Red Hat Enterprise Linux 8,multi_platform_fedora,multi_platform_ol
 . /usr/share/scap-security-guide/remediation_functions
 
-if ! `rpm -q --quiet chrony` && ! `rpm -q --quiet ntp-`; then
-  {{{ bash_package_install("chrony") | indent(2) }}}
-  service_command enable chronyd
-elif `rpm -q --quiet chrony`; then
-  if ! [ `/usr/sbin/pidof ntpd` ] ; then
-    service_command enable chronyd
-  fi
+if rpm -q --quiet chrony ; then
+    if ! /usr/sbin/pidof ntpd ; then
+        service_command enable chronyd
+    fi
+elif rpm -q --quiet ntp- ; then
+    service_command enable ntpd
 else
-  service_command enable ntpd
+    {{{ bash_package_install("chrony") | indent(4) }}}
+    service_command enable chronyd
 fi
