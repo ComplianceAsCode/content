90f9e1518066f5e8e92f430b0687ed00998a3382
Ilya Okomin <ilya.okomin@oracle.com>
2019-08-19 12:02:17 +0300

    Add tests for rule_rsyslog_files_groupownership
    Signed-off-by: Ilya Okomin <ilya.okomin@oracle.com>

---
+++ tests/data/group_system/group_logging/group_ensure_rsyslog_log_file_configuration/rule_rsyslog_files_groupownership/include_is_other.fail.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# profiles = xccdf_org.ssgproject.content_profile_pci-dss
+# platform = Red Hat Enterprise Linux 8,multi_platform_fedora,Oracle Linux 8
+
+# Check rsyslog.conf with root group-owner log from rules and
+# non root group-owner log from include() fails.
+
+source ../rsyslog_log_utils.sh
+
+GROUP_TEST=testssg
+groupadd $GROUP_TEST
+
+GROUP_ROOT=root
+
+# setup test data
+create_rsyslog_test_logs 2
+
+# setup test log files ownership
+chgrp $GROUP_ROOT ${RSYSLOG_TEST_LOGS[0]}
+chgrp $GROUP_TEST ${RSYSLOG_TEST_LOGS[1]}
+
+# create test configuration file
+test_conf=${RSYSLOG_TEST_DIR}/test1.conf
+cat << EOF > ${test_conf}
+# rsyslog configuration file
+
+#### RULES ####
+
+*.*     ${RSYSLOG_TEST_LOGS[1]}
+EOF
+
+# create rsyslog.conf configuration file
+cat << EOF > $RSYSLOG_CONF
+# rsyslog configuration file
+
+#### RULES ####
+
+*.*      ${RSYSLOG_TEST_LOGS[0]}
+
+#### MODULES ####
+
+include(file="${test_conf}")
+EOF
---
+++ tests/data/group_system/group_logging/group_ensure_rsyslog_log_file_configuration/rule_rsyslog_files_groupownership/include_is_root_IncludeConfig_is_root.pass.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+# profiles = xccdf_org.ssgproject.content_profile_pci-dss
+# platform = Red Hat Enterprise Linux 8,multi_platform_fedora,Oracle Linux 8
+
+# Check rsyslog.conf with root group-owner log from rules and
+# root group-owner log from include() passes.
+
+source ../rsyslog_log_utils.sh
+
+GROUP=root
+
+# setup test data
+create_rsyslog_test_logs 3
+
+# setup test log files ownership
+chgrp $GROUP ${RSYSLOG_TEST_LOGS[0]}
+chgrp $GROUP ${RSYSLOG_TEST_LOGS[1]}
+chgrp $GROUP ${RSYSLOG_TEST_LOGS[2]}
+
+# create test configuration file
+test_conf=${RSYSLOG_TEST_DIR}/test1.conf
+cat << EOF > ${test_conf}
+# rsyslog configuration file
+
+#### RULES ####
+
+*.*     ${RSYSLOG_TEST_LOGS[1]}
+EOF
+
+# create test2 configuration file
+test_conf2=${RSYSLOG_TEST_DIR}/test2.conf
+cat << EOF > ${test_conf2}
+# rsyslog configuration file
+
+#### RULES ####
+
+*.*     ${RSYSLOG_TEST_LOGS[2]}
+EOF
+
+# create rsyslog.conf configuration file
+cat << EOF > $RSYSLOG_CONF
+# rsyslog configuration file
+
+#### RULES ####
+
+*.*     ${RSYSLOG_TEST_LOGS[0]}
+
+#### MODULES ####
+
+include(file="${test_conf}")
+
+\$IncludeConfig ${test_conf2}
+EOF
