affd1e2981f1ea5bb07d4c2a495fba60003bdfca
Malte Kraus <malte.kraus@suse.com>
2019-08-19 11:26:55 +0200

    sshd_use_priv_separation: use bash/OVAL macros

--- linux_os/guide/services/ssh/ssh_server/sshd_use_priv_separation/bash/shared.sh
+++ linux_os/guide/services/ssh/ssh_server/sshd_use_priv_separation/bash/shared.sh
@@ -3,26 +3,9 @@
 # strategy = restrict
 # complexity = low
 # disruption = low
-if [ -e "/etc/ssh/sshd_config" ] ; then
-    LC_ALL=C sed -i "/^\s*UsePrivilegeSeparation\s\+/Id" "/etc/ssh/sshd_config"
-else
-    touch "/etc/ssh/sshd_config"
-fi
+
 # Include source function library.
 . /usr/share/scap-security-guide/remediation_functions
 populate var_sshd_priv_separation
 
-cp "/etc/ssh/sshd_config" "/etc/ssh/sshd_config.bak"
-# Insert before the line matching the regex '^Match'.
-local line_number="$(LC_ALL=C grep -n "^Match" "/etc/ssh/sshd_config.bak" | LC_ALL=C sed 's/:.*//g')"
-if [ -z "$line_number" ]; then
-    # There was no match of '^Match', insert at
-    # the end of the file.
-    printf '%s\n' "UsePrivilegeSeparation $var_sshd_priv_separation" >> "/etc/ssh/sshd_config"
-else
-    head -n "$(( line_number - 1 ))" "/etc/ssh/sshd_config.bak" > "/etc/ssh/sshd_config"
-    printf '%s\n' "UsePrivilegeSeparation $var_sshd_priv_separation" >> "/etc/ssh/sshd_config"
-    tail -n "+$(( line_number ))" "/etc/ssh/sshd_config.bak" >> "/etc/ssh/sshd_config"
-fi
-# Clean up after ourselves.
-rm "/etc/ssh/sshd_config.bak"
+{{{ bash_sshd_config_set(parameter="UsePrivilegeSeparation", value="$var_sshd_priv_separation") }}}
