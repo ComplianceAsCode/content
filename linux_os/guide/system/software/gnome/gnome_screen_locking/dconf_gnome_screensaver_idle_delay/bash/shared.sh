303141e3a68a3565ce813190e8ea122e627fd9dd
Jan Černý <jcerny@redhat.com>
2019-08-20 14:06:56 +0200

    Fix missing commas

--- linux_os/guide/system/software/gnome/gnome_screen_locking/dconf_gnome_screensaver_idle_delay/bash/shared.sh
+++ linux_os/guide/system/software/gnome/gnome_screen_locking/dconf_gnome_screensaver_idle_delay/bash/shared.sh
@@ -2,5 +2,5 @@
 . /usr/share/scap-security-guide/remediation_functions
 populate inactivity_timeout_value
 
-{{{ dconf_settings("org/gnome/desktop/session", "idle-delay" "uint32 ${inactivity_timeout_value}" "local.d", "00-security-settings") }}}
+{{{ dconf_settings("org/gnome/desktop/session", "idle-delay", "uint32 ${inactivity_timeout_value}", "local.d", "00-security-settings") }}}
 {{{ dconf_lock("org/gnome/desktop/session", "idle-delay", "local.d", "00-security-settings-lock") }}}
