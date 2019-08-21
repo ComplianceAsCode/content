072b5e361f27152d981081203615a4e6867cd04a
Vojtech Polasek <vpolasek@redhat.com>
2019-08-21 15:40:50 +0200

    added test cases for the case where the CP is customized

---
+++ tests/data/group_system/group_software/group_integrity/group_crypto/rule_configure_crypto_policy/override_policy.pass.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# platform = multi_platform_fedora,Red Hat Enterprise Linux 8
+# profiles = xccdf_org.ssgproject.content_profile_ospp
+
+#using openssh server as example
+CRYPTO_POLICY_OVERRIDE_FILE="/etc/crypto-policies/local.d/opensshserver-test.config"
+
+update-crypto-policies --set "FIPS"
+
+cat "CRYPTO_POLICY=test" > "$CRYPTO_POLICY_OVERRIDE_FILE"
+
+update-crypto-policies --set "FIPS"
