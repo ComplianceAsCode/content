23f93efbb21f63a2dfac989afb562fd47e536f02
Gabriel Becker <ggasparb@redhat.com>
2019-08-30 10:58:29 +0200

    Remove rule configure_opensc_nss_db from RHEL8 product.
    The rule is not applicable to RHEL8. OpenSC is set by default on RHEL8.

--- linux_os/guide/system/accounts/accounts-physical/screen_locking/smart_card_login/configure_opensc_nss_db/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-physical/screen_locking/smart_card_login/configure_opensc_nss_db/bash/shared.sh
@@ -1,4 +1,4 @@
-# platform = Red Hat Enterprise Linux 7,Red Hat Enterprise Linux 8,multi_platform_fedora,multi_platform_rhv,Oracle Linux 7
+# platform = Red Hat Enterprise Linux 7,multi_platform_fedora,multi_platform_rhv,Oracle Linux 7
 # reboot = false
 # strategy = configure
 # complexity = low
