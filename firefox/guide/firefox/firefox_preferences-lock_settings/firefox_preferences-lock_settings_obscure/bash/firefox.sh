e4c95a7af5691f53c6a8bef885694ac54296fdb4
Alexander Scheel <ascheel@redhat.com>
2019-09-09 13:04:14 -0400

    Add bash_firefox_js_setting Jinja macro
    This Jinja macro handles setting a JavaScript based configuration value
    in Firefox. We've migrated it from bash_remediation_functions to being a
    macro under macros-bash.jinja.
    
    Signed-off-by: Alexander Scheel <ascheel@redhat.com>

--- firefox/guide/firefox/firefox_preferences-lock_settings/firefox_preferences-lock_settings_obscure/bash/firefox.sh
+++ firefox/guide/firefox/firefox_preferences-lock_settings/firefox_preferences-lock_settings_obscure/bash/firefox.sh
@@ -1,4 +1,3 @@
 # platform = Mozilla Firefox
-. /usr/share/scap-security-guide/remediation_functions
 
-firefox_js_setting "stig_settings.js" "general.config.obscure_value" "0"
+{{{ bash_firefox_js_setting("stig_settings.js", "general.config.obscure_value", "0") }}}
