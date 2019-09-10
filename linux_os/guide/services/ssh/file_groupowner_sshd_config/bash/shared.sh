dfa668053c6a2b2be4d72be5330594bf2c237e90
Jayson Cofell <1051437+70k10@users.noreply.github.com>
2019-09-10 12:03:46 -0500

    Convert sshd_config check to use template

--- linux_os/guide/services/ssh/file_groupowner_sshd_config/bash/shared.sh
+++
@@ -1,2 +0,0 @@
-# platform=multi_platform_rhel,multi_platform_ol,multi_platform_fedora
-chgrp root /etc/ssh/sshd_config
--- linux_os/guide/services/ssh/file_permissions_sshd_config/bash/shared.sh
+++
@@ -1,2 +0,0 @@
-# platform=multi_platform_rhel,multi_platform_ol,multi_platform_fedora
-chmod 0600 /etc/ssh/sshd_config
