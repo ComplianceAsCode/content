2d73c6d8d97f04d12b71523dae0b916ed5776621
Watson Sato <wsato@redhat.com>
2019-09-12 13:55:34 +0200

    Add template_backend key to template data

--- utils/migrate_template_csv_to_rule.py
+++ utils/migrate_template_csv_to_rule.py
@@ -656,7 +656,7 @@ class ProductCSVData(object):
                     line_data_dict = csv_parser(line, csv_data)
 
                     if except_for_language:
-                        line_data_dict[except_for_language] = "off"
+                        line_data_dict["template_backends"] = {except_for_language: "off"}
                 except IndexError as e:
                     print(f"line:{line} in file: {csv_f}")
                     raise e
