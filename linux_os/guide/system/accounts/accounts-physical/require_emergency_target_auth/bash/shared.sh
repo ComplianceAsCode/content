c51cfcdb3bfe5ee1f277fffb19db0a759444d036
Jayson Cofell <1051437+70k10@users.noreply.github.com>
2019-09-18 08:16:14 -0500

    Add emergency service single user mode check

---
+++ linux_os/guide/system/accounts/accounts-physical/require_emergency_target_auth/bash/shared.sh
@@ -0,0 +1,15 @@
+# platform = multi_platform_wrlinux,multi_platform_rhel,multi_platform_fedora,multi_platform_ol,multi_platform_rhv
+
+service_file="/usr/lib/systemd/system/emergency.service"
+
+{{% if product in ["fedora", "rhel8"] -%}}
+sulogin="/usr/lib/systemd/systemd-sulogin-shell emergency"
+{{%- else -%}}
+sulogin='/bin/sh -c "/sbin/sulogin; /usr/bin/systemctl --fail --no-block default"'
+{{%- endif %}}
+
+if grep "^ExecStart=.*" "$service_file" ; then
+    sed -i "s%^ExecStart=.*%ExecStart=-$sulogin%" "$service_file"
+else
+    echo "ExecStart=-$sulogin" >> "$service_file"
+fi
