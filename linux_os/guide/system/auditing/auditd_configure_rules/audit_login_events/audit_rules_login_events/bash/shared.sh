10c50d294f61c3638abd4a8dfa0e6870c4d4f10f
Watson Sato <wsato@redhat.com>
2019-10-02 15:10:42 +0200

    Remove slash from audit rules login failock
    Follows 0e83474ea75d762c77f78630448ad5a72b58d211
    There shoudn't be slash.

--- linux_os/guide/system/auditing/auditd_configure_rules/audit_login_events/audit_rules_login_events/bash/shared.sh
+++ linux_os/guide/system/auditing/auditd_configure_rules/audit_login_events/audit_rules_login_events/bash/shared.sh
@@ -8,8 +8,8 @@
 fix_audit_watch_rule "auditctl" "/var/log/tallylog" "wa" "logins"
 fix_audit_watch_rule "augenrules" "/var/log/tallylog" "wa" "logins"
 
-fix_audit_watch_rule "auditctl" "/var/run/faillock/" "wa" "logins"
-fix_audit_watch_rule "augenrules" "/var/run/faillock/" "wa" "logins"
+fix_audit_watch_rule "auditctl" "/var/run/faillock" "wa" "logins"
+fix_audit_watch_rule "augenrules" "/var/run/faillock" "wa" "logins"
 
 fix_audit_watch_rule "auditctl" "/var/log/lastlog" "wa" "logins"
 fix_audit_watch_rule "augenrules" "/var/log/lastlog" "wa" "logins"
