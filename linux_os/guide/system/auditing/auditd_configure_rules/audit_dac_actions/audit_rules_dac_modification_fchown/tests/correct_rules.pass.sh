f8df0e4117af9276c2952c3c5e489fbb934b214e
Watson Sato <wsato@redhat.com>
2019-10-07 15:41:30 +0200

    Add basic test scenarios for audit rules
    Basic test scenarios for following rules:
    - audit_rules_dac_modification_fchown
    - audit_rules_file_deletion_events_rename
    - audit_rules_kernel_module_loading_init
    - audit_rules_media_export
    - audit_rules_networkconfig_modification
    
    Basic test scenario means, no rules configured, and correct rules
    are there (in single -S arg)

---
+++ linux_os/guide/system/auditing/auditd_configure_rules/audit_dac_actions/audit_rules_dac_modification_fchown/tests/correct_rules.pass.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# profiles = xccdf_org.ssgproject.content_profile_pci-dss
+
+echo "-a always,exit -F arch=b32 -S fchown -F auid>=1000 -F auid!=unset -F key=perm_mod" >> /etc/audit/rules.d/perm_mod.rules
+echo "-a always,exit -F arch=b64 -S fchown -F auid>=1000 -F auid!=unset -F key=perm_mod" >> /etc/audit/rules.d/perm_mod.rules
+
