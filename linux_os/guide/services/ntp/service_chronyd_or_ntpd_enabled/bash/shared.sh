1e398f0c834390875f17d1a298d8436b8e92f506
Matěj Týč <matyc@redhat.com>
2019-10-07 15:52:34 +0200

    Fix NTP-Chrony remediation + check.
    - Remove the trailing dash when checking for the ntp package in the remediation.
    - Add the correct package name to the Chrony(d) OVAL.

--- linux_os/guide/services/ntp/service_chronyd_or_ntpd_enabled/bash/shared.sh
+++ linux_os/guide/services/ntp/service_chronyd_or_ntpd_enabled/bash/shared.sh
@@ -4,7 +4,7 @@ if rpm -q --quiet chrony ; then
     if ! /usr/sbin/pidof ntpd ; then
         {{{ bash_service_command("enable", "chronyd") | indent(8) }}}
     fi
-elif rpm -q --quiet ntp- ; then
+elif rpm -q --quiet ntp ; then
     {{{ bash_service_command("enable", "ntpd") | indent(4) }}}
 else
     {{{ bash_package_install("chrony") | indent(4) }}}
