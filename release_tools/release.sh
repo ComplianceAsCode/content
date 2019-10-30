c57186c93311f919da9a68db88d561dc2888c3b8
Watson Sato <wsato@redhat.com>
2019-10-30 18:28:24 +0100

    Load and use the env file from python

--- release_tools/release.sh
+++ release_tools/release.sh
@@ -3,7 +3,6 @@
 . release-utils.sh
 echo == Create Release ==
 
-check_github_credentials
 move_on_to_next_milestone
 download_release_assets
 create_new_release
--- release_tools/release_content.py
+++ release_tools/release_content.py
@@ -1,8 +1,26 @@
 import argparse
+import yaml
+
+def load_env(env_path):
+    with open(env_path, 'r') as env_stream:
+        env = yaml.safe_load(env_stream)
+    return env
+
+def validate_env(env, env_path):
+    env_ok = True
+    mandatory_fields = ['github_token', 'jenkins_user', 'jenkins_token']
+    for field in mandatory_fields:
+        if field not in env:
+            print(f"Add your {field} in {env_path} file. Check the README.md for how to obtain it.")
+            env_ok = True
+    return env_ok
 
 
 def create_parser():
     parser = argparse.ArgumentParser()
+    parser.add_argument('--env', default='.env.yml', dest='env_path',
+                        help="Environment file with GitHub token and Jenkins username and token. "
+                             "Check the README.md file to know what to put in the file")
     parser.add_argument("--owner", default="ComplianceAsCode")
     parser.add_argument("--repo", default="content")
 
@@ -11,3 +29,10 @@ def create_parser():
 
 if __name__ == "__main__":
     parser = create_parser()
+
+    try:
+        env = load_env(parser.env_path)
+        if not validate_env(env, parser.env_path):
+            sys.exit(1)
+    except FileNotFoundError as e:
+        print("Create a .env.yaml file and populate it. Check the README.md file for details.")
