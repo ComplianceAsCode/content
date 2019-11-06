29a7d5b0b1fb1447697411ead743de8a88c7758f
Robert McAllister <rmcallis@redhat.com>
2019-11-05 21:36:11 -0700

    removed weird merge code

--- applications/openstack/cinder/cinder_conf_file_perms/bash/shared.sh
+++ applications/openstack/cinder/cinder_conf_file_perms/bash/shared.sh
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
 # platform = Red Hat OpenStack Platform 10
-=======
->>>>>>> 9c329e7d95f2ebe891551fc5e51e82838caf741d
 for file in /etc/cinder/cinder.conf \
 		/etc/cinder/rootwrap.conf; do
 	chown root $file
 	chgrp cinder $file
 done
-<<<<<<< HEAD
-=======
-
->>>>>>> 9c329e7d95f2ebe891551fc5e51e82838caf741d
