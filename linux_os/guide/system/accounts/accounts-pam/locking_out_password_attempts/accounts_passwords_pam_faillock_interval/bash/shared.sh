1cfd223721148cd070ba6ac8309ee202d7000cb4
Jan Černý <jcerny@redhat.com>
2019-11-11 14:19:40 +0100

    Refactor
    Include the common code for all 3 remediations to the macro.
    Merge the low-level macros to the parent high-level macro.

--- linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_passwords_pam_faillock_interval/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_passwords_pam_faillock_interval/bash/shared.sh
@@ -5,10 +5,4 @@
 
 populate var_accounts_passwords_pam_faillock_fail_interval
 
-AUTH_FILES[0]="/etc/pam.d/system-auth"
-AUTH_FILES[1]="/etc/pam.d/password-auth"
-
-for pam_file in "${AUTH_FILES[@]}"
-do
-	{{{ bash_set_faillock_option("$pam_file", "fail_interval", "$var_accounts_passwords_pam_faillock_fail_interval") }}}
-done
+{{{ bash_set_faillock_option("fail_interval", "$var_accounts_passwords_pam_faillock_fail_interval") }}}
--- linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_passwords_pam_faillock_unlock_time/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_passwords_pam_faillock_unlock_time/bash/shared.sh
@@ -2,10 +2,4 @@
 . /usr/share/scap-security-guide/remediation_functions
 populate var_accounts_passwords_pam_faillock_unlock_time
 
-AUTH_FILES[0]="/etc/pam.d/system-auth"
-AUTH_FILES[1]="/etc/pam.d/password-auth"
-
-for pam_file in "${AUTH_FILES[@]}"
-do
-	{{{ bash_set_faillock_option("$pam_file", "unlock_time", "$var_accounts_passwords_pam_faillock_unlock_time") }}}
-done
+{{{ bash_set_faillock_option("unlock_time", "$var_accounts_passwords_pam_faillock_unlock_time") }}}
