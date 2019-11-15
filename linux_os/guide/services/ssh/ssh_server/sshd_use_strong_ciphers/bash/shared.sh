32c5bdbfc532d36bae5aaf0e0510b8516373598e
Matěj Týč <matyc@redhat.com>
2019-11-15 14:44:25 +0100

    Fixed sshd_use_strong_ciphers.
    - Fixed ciphers rule description metadata and bash remediation - removed duplicate ciphers.
    - Fixed ciphers rule OVAL.
    - Added test cases.

--- linux_os/guide/services/ssh/ssh_server/sshd_use_strong_ciphers/bash/shared.sh
+++ linux_os/guide/services/ssh/ssh_server/sshd_use_strong_ciphers/bash/shared.sh
@@ -1,3 +1,3 @@
 # platform = multi_platform_all
 
-{{{ bash_sshd_config_set(parameter="Ciphers", value="aes128-ctr,aes192-ctr,aes256-ctr,chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes128-gcm@openssh.com,aes256-ctr,aes128-ctr") }}}
+{{{ bash_sshd_config_set(parameter="Ciphers", value="aes128-ctr,aes192-ctr,aes256-ctr,chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes128-gcm@openssh.com") }}}
