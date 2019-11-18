38022641e40d4e5dc043291412aa4f7f7f562d1f
miguel <miguel@localhost.localdomain>
2019-11-18 16:14:55 +0100

    Creacion utilidades para modificacion de plataformas afectadas for las reglas

---
+++ utils/add_profile_from_list.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+ 
+import sys
+import os
+import argparse
+import json
+ 
+import ssg.rules
+import ssg.utils
+import ssg.rule_yaml
+
+from pathlib import Path
+ 
+SSG_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+
+#script en sucio de prueba
+#Este script crea un perfil para el producto deseado con el nombre especificado a partir de una lista de reglas
+#Ejemplo de uso: utils/add_profile_from_list.py 'archivo_con_lista_de_reglas' 'sle11' 'nombre_perfil'
+ 
+def main():
+ 
+    with open(sys.argv[1], 'r') as file:
+        product = sys.argv[2]
+        name = sys.argv[3]
+        ruleArray = file.readlines()
+        ruleArray = [x.strip() for x in ruleArray]
+
+    path = Path("./{}/profiles/{}.profile".format(product,name))
+
+    with open(path, 'w+') as f:
+        f.write("documentation_complete: true\n"+"\n"+"title: '{}'\n".format(name)+"\n"+"description: |- \n"+"    Write the profile description here\n"+"\n"+"selections:\n") 	
+        for rule in ruleArray:
+            if rule != "":
+                f.write("    - {}\n".format(rule))
+
+ 
+if __name__ == "__main__":
+        main()
+
---
+++ utils/add_rules_prodtype_from_list.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+ 
+import sys
+import os
+import argparse
+import json
+ 
+import ssg.rules
+import ssg.utils
+import ssg.rule_yaml
+ 
+import mod_prodtype
+ 
+SSG_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+
+#script en sucio de prueba
+#Este script añade el producto deseado(ej. sle) al campo de productos afectados de cada regla de una lista de reglas dada
+#Ejemplo de uso: utils/add_rules_prodtype_from_list.py 'list/add' 'sle11'
+ 
+def main():
+ 
+    if len(sys.argv) > 4:
+        print("Demasiados argumentos")
+    else:
+        file = open(sys.argv[1], 'r')
+        action = sys.argv[2]
+        ruleArray = file.readlines()
+        ruleArray = [x.strip() for x in ruleArray]
+        file.close()
+ 
+        if action=="list" or (action=="add" and len(sys.argv) > 2):
+            os.system('~/content/utils/rule_dir_json.py')
+            for rule in ruleArray:
+                if action=="list":
+                    os.system('~/content/utils/mod_prodtype.py {} {}'.format(rule, action))
+                elif action=="add":
+                    product = sys.argv[3]
+                    os.system('~/content/utils/mod_prodtype.py {} {} {}'.format(rule, action, product))
+        else:
+            print("Especifique la opcion correcta deseada(list or add)")
+ 
+ 
+if __name__ == "__main__":
+    main()
+
