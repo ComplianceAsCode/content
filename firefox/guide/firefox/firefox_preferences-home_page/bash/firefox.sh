29c5ccd48216f99195ca420d5c1782538c3cac8b
Matěj Týč <matyc@redhat.com>
2019-11-20 17:28:49 +0100

    Fixed Jinja Bash Firefox macros

--- firefox/guide/firefox/firefox_preferences-home_page/bash/firefox.sh
+++ firefox/guide/firefox/firefox_preferences-home_page/bash/firefox.sh
@@ -1,4 +1,8 @@
 # platform = Mozilla Firefox
+
+# Include source function library.
+. /usr/share/scap-security-guide/remediation_functions
+
 populate var_default_home_page
 
-{{{ bash_firefox_cfg_setting("stig.cfg", "browser.startup.homepage", "\"${var_default_home_page}\"") }}}
+{{{ bash_firefox_cfg_setting("stig.cfg", "browser.startup.homepage", quoted_value="${var_default_home_page}") }}}
