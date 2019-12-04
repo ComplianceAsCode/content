fb5fe8c7dea9c83558b9e4fd7d2235caff6bd4db
Marek Haicman <mhaicman@redhat.com>
2019-12-04 15:11:39 +0100

    Create macro to translate text to banner text.
    With banner texts having every whitespace replaced with more complex regular
    expression, it's not really readable in that form. This macro should provide
    way to write human readable text in source, and get machine readable text
    as the output.

--- linux_os/guide/system/accounts/accounts-banners/banner_etc_issue/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-banners/banner_etc_issue/bash/shared.sh
@@ -3,7 +3,7 @@
 populate login_banner_text
 
 # There was a regular-expression matching various banners, needs to be expanded
-expanded=$(echo "$login_banner_text" | sed 's/(\\\\\x27)\*/\\\x27/g;s/(\\\x27)\*//g;s/(\^\(.*\)\$|.*$/\1/g;s/\[\\s\\n\][+*]/ /g;s/\\//g;s/[^-]- /\n\n-/g;s/(n)\**//g')
+expanded=$(echo "$login_banner_text" | sed 's/(\\\\\x27)\*/\\\x27/g;s/(\\\x27)\*//g;s/(\^\(.*\)\$|.*$/\1/g;s/\[\\s\\n\]+/ /g;s/\\//g;s/\[n\]+/\n/g')
 formatted=$(echo "$expanded" | fold -sw 80)
 
 cat <<EOF >/etc/issue
