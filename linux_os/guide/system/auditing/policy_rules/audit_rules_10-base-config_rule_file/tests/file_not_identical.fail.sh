aa86c98a00192a13a7d249f124c14e1fcc99d39d
Vojtech Polasek <vpolasek@redhat.com>
2019-12-10 16:28:53 +0100

    added tests

---
+++ linux_os/guide/system/auditing/policy_rules/audit_rules_10-base-config_rule_file/tests/file_not_identical.fail.sh
@@ -0,0 +1,4 @@
+# profiles = xccdf_org.ssgproject.content_profile_ospp
+
+cp /usr/share/audit/sample-rulesaú10-base-config.rules /etc/audit/rules.d/
+echo "some additional text" >> /etc/audit/rules.d/10-base-config.rules
