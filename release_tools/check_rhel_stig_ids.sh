30ddbe66bd23d215b9b58104de4c3780c58a990b
Watson Sato <wsato@redhat.com>
2020-01-14 14:34:47 +0100

    Fix check_rhel_stig script to build products
    CMake now defaults to building nothing, we need to say products we want
    to build.
    Also added die() function.

--- release_tools/check_rhel_stig_ids.sh
+++ release_tools/check_rhel_stig_ids.sh
@@ -3,10 +3,16 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 CONTENT_REPO_ROOT="$(cd "$SCRIPT_DIR" && cd .. && pwd)"
 BUILDDIR="$CONTENT_REPO_ROOT/build"
 
+die()
+{
+    echo "$1"
+    exit 1
+}
+
 echo Checking missing STIG IDS...
 echo Building RHEL6 and RHEL7 content
 ncpu=$(nproc) # This won't return number of physical core, but it shouldn't be problem
-(cd "$BUILDDIR" && cmake .. && make -j $(ncpu) rhel7 rhel6) &> rhel_build.log || die "Error building RHEL7 content, check rhel_build.log file for errors"
+(cd "$BUILDDIR" && cmake -D SSG_PRODUCT_RHEL6:BOOLEAN=ON -D SSG_PRODUCT_RHEL7:BOOLEAN=ON .. && make -j $($ncpu) rhel6 rhel7) &> rhel_build.log || die "Error building RHEL7 content, check rhel_build.log file for errors"
 (PYTHONPATH=.. python3 ../build-scripts/profile_tool.py stats -b ../build/ssg-rhel6-xccdf.xml --missing-stig-ids --profile stig) > rhel6-stig-ids.log
 echo :: Check rhel6-stig-ids.log for rules missing STIG IDs
 (PYTHONPATH=.. python3 ../build-scripts/profile_tool.py stats -b ../build/ssg-rhel7-xccdf.xml --missing-stig-ids --profile stig) > rhel7-stig-ids.log
