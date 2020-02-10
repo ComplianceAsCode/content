6ec7b01ea80f3dca5e6b33478c8d204f6f913826
Pat Riehecky <riehecky@fnal.gov>
2020-02-10 09:55:18 -0600

    Permit setting sshd GSSAPI to yes

---
+++ linux_os/guide/services/ssh/ssh_server/sshd_enable_gssapi_auth/tests/wrong_value.fail.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if grep -q "^GSSAPIAuthentication" /etc/ssh/sshd_config; then
+	sed -i "s/^GSSAPIAuthentication.*/GSSAPIAuthentication no/" /etc/ssh/sshd_config
+else
+	echo "GSSAPIAuthentication no" >> /etc/ssh/sshd_config
+fi
