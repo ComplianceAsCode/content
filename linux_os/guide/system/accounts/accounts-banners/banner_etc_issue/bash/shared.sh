f94f4ba5a5d650c5ae50f83d59b7464e7f785b9d
Watson Sato <wsato@redhat.com>
2020-03-03 12:48:10 +0100

    Document what the regexes do in the banner

--- linux_os/guide/system/accounts/accounts-banners/banner_etc_issue/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-banners/banner_etc_issue/bash/shared.sh
@@ -2,7 +2,12 @@
 . /usr/share/scap-security-guide/remediation_functions
 populate login_banner_text
 
-# There was a regular-expression matching various banners, needs to be expanded
+# Multiple regexes transform the banner regex into a usable banner
+# 1 - Keep only the first banners if there are multiple, and remove wrapping regex syntax.
+#    (dod_banners contains the long and shor banner)
+# 2- Add spaces ' '. (Transforms regex for "space or newline" into a " ")
+# 3- Adds newlines. (Transforms "(?:\[\\n\]+|(?:\\n)+)" into "\n")
+# 4- Remove any leftover backslash. (From any parethesis in the banner, for example).
 expanded=$(echo "$login_banner_text" | sed 's/\^(\(.*\)|.*$/\1/g;s/\[\\s\\n\]+/ /g;s/(?:\[\\n\]+|(?:\\n)+)/\n/g;s/\\//g;')
 formatted=$(echo "$expanded" | fold -sw 80)
 
--- linux_os/guide/system/accounts/accounts-banners/gui_login_banner/dconf_gnome_login_banner_text/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-banners/gui_login_banner/dconf_gnome_login_banner_text/bash/shared.sh
@@ -2,6 +2,14 @@
 . /usr/share/scap-security-guide/remediation_functions
 populate login_banner_text
 
+# Multiple regexes transform the banner regex into a usable banner
+# 1 - Keep only the first banners if there are multiple, and remove wrapping regex syntax.
+#    (dod_banners contains the long and shor banner)
+# 2- Add spaces ' '. (Transforms regex for "space or newline" into a " ")
+# 3- Adds newline "tokens". (Transforms "(?:\[\\n\]+|(?:\\n)+)" into "(n)*")
+# 4- Remove any leftover backslash. (From any parethesis in the banner, for example).
+# 5- Removes the newline "token." (Transforms them into newline escape sequences "\n").
+#    ( Needs to be done after 4, otherwise the escapce sequence will become just "n".
 expanded=$(echo "$login_banner_text" | sed 's/\^(\(.*\)|.*$/\1/g;s/\[\\s\\n\]+/ /g;s/(?:\[\\n\]+|(?:\\n)+)/(n)\*/g;s/\\//g;s/(n)\*/\\n/g;')
 
 {{{ bash_dconf_settings("org/gnome/login-screen", "banner-message-text", "'${expanded}'", "gdm.d", "00-security-settings") }}}
