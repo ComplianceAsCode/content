6b27221e857cefe7efaa04f4491c506ea0cb096c
Watson Sato <wsato@redhat.com>
2020-03-07 13:12:28 +0100

    Move bash banner deregexification to macros
    This aims to increase maintenability and readability.
    Every step in the deregexification is a separate macro.
    The macros 'bash_deregexify_banner_etc_issue' and
    'bash_deregexify_banner_dconf_gnome' build upon the basic steps.

--- linux_os/guide/system/accounts/accounts-banners/banner_etc_issue/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-banners/banner_etc_issue/bash/shared.sh
@@ -4,12 +4,15 @@ populate login_banner_text
 
 # Multiple regexes transform the banner regex into a usable banner
 # 1 - Keep only the first banners if there are multiple, and remove wrapping regex syntax.
-#    (dod_banners contains the long and shor banner)
+#    (dod_banners contains the long and short banner)
+{{{ bash_deregexify_multiple_banners("login_banner_text") }}}
 # 2- Add spaces ' '. (Transforms regex for "space or newline" into a " ")
+{{{ bash_deregexify_banner_space("login_banner_text") }}}
 # 3- Adds newlines. (Transforms "(?:\[\\n\]+|(?:\\n)+)" into "\n")
+{{{ bash_deregexify_banner_newline("login_banner_text", "\\n") }}}
 # 4- Remove any leftover backslash. (From any parethesis in the banner, for example).
-expanded=$(echo "$login_banner_text" | sed 's/\^(\(.*\)|.*$/\1/g;s/\[\\s\\n\]+/ /g;s/(?:\[\\n\]+|(?:\\n)+)/\n/g;s/\\//g;')
-formatted=$(echo "$expanded" | fold -sw 80)
+{{{ bash_deregexify_banner_backslash("login_banner_text") }}}
+formatted=$(echo "$login_banner_text" | fold -sw 80)
 
 cat <<EOF >/etc/issue
 $formatted
--- linux_os/guide/system/accounts/accounts-banners/gui_login_banner/dconf_gnome_login_banner_text/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-banners/gui_login_banner/dconf_gnome_login_banner_text/bash/shared.sh
@@ -4,13 +4,17 @@ populate login_banner_text
 
 # Multiple regexes transform the banner regex into a usable banner
 # 1 - Keep only the first banners if there are multiple, and remove wrapping regex syntax.
-#    (dod_banners contains the long and shor banner)
+#    (dod_banners contains the long and short banner)
+{{{ bash_deregexify_multiple_banners("login_banner_text") }}}
 # 2- Add spaces ' '. (Transforms regex for "space or newline" into a " ")
+{{{ bash_deregexify_banner_space("login_banner_text") }}}
 # 3- Adds newline "tokens". (Transforms "(?:\[\\n\]+|(?:\\n)+)" into "(n)*")
+{{{ bash_deregexify_banner_newline("login_banner_text", "(n)*") }}}
 # 4- Remove any leftover backslash. (From any parethesis in the banner, for example).
+{{{ bash_deregexify_banner_backslash("login_banner_text") }}}
 # 5- Removes the newline "token." (Transforms them into newline escape sequences "\n").
 #    ( Needs to be done after 4, otherwise the escapce sequence will become just "n".
-expanded=$(echo "$login_banner_text" | sed 's/\^(\(.*\)|.*$/\1/g;s/\[\\s\\n\]+/ /g;s/(?:\[\\n\]+|(?:\\n)+)/(n)\*/g;s/\\//g;s/(n)\*/\\n/g;')
+{{{ bash_deregexify_banner_newline_token("login_banner_text")}}}
 
-{{{ bash_dconf_settings("org/gnome/login-screen", "banner-message-text", "'${expanded}'", "gdm.d", "00-security-settings") }}}
+{{{ bash_dconf_settings("org/gnome/login-screen", "banner-message-text", "'${login_banner_text}'", "gdm.d", "00-security-settings") }}}
 {{{ bash_dconf_lock("org/gnome/login-screen", "banner-message-text", "gdm.d", "00-security-settings-lock") }}}
