b5379d0850f2ee366c7259512c74355d86babf2f
Vojtech Polasek <vpolasek@redhat.com>
2020-03-10 19:05:57 +0100

    create new chrony rules for CIS
    add package_chrony_installed
    add service_chronyd_enabled
    add chrony_specify_remote_server
    add default value to chrony_multiple_servers variable

---
+++ linux_os/guide/services/ntp/chronyd_specify_remote_server/bash/shared.sh
@@ -0,0 +1,9 @@
+# platform = Red Hat Enterprise Linux 7,Red Hat Enterprise Linux 8,Red Hat Virtualization 4,multi_platform_fedora,multi_platform_ol
+. /usr/share/scap-security-guide/remediation_functions
+populate var_multiple_time_servers
+
+config_file="/etc/chrony.conf"
+
+if ! grep -q ^server "$config_file" ; then
+  {{{ bash_ensure_there_are_servers_in_ntp_compatible_config_file("$config_file", "$var_multiple_time_servers") | indent(2) }}}
+fi
