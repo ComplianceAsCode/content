e6145398300fae26e9765dc2798d7eec602be70c
Vojtech Polasek <vpolasek@redhat.com>
2020-03-11 11:05:17 +0100

    add tests and ansible remediation
    remove shared oval for checking chronyd_specify_remote_server

--- linux_os/guide/services/ntp/chronyd_specify_remote_server/bash/shared.sh
+++ linux_os/guide/services/ntp/chronyd_specify_remote_server/bash/shared.sh
@@ -4,6 +4,6 @@ populate var_multiple_time_servers
 
 config_file="/etc/chrony.conf"
 
-if ! grep -q ^server "$config_file" ; then
+if ! grep -q '^[\s]*server[\s]+[\w]+' "$config_file" ; then
   {{{ bash_ensure_there_are_servers_in_ntp_compatible_config_file("$config_file", "$var_multiple_time_servers") | indent(2) }}}
 fi
