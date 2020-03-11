9bab3df80a12504b862f2d0ee032e462799aab0f
Jan Černý <jcerny@redhat.com>
2020-03-11 14:47:15 +0100

    Fix template mount_option_removable_partitions
    There are multiple problems that we try to address by this patch.
    
    1. The test scenario dvd_bad_opts.fail.sh for rule
    mount_option_noexec_removable_partitions was failing because scanner
    returned pass instead of expected fail. That is because the rule checks
    if path specified by var_removable_partition exists and if it doesn't
    exist the rule fail. This variable is always set to /dev/cdrom in all
    profiles, as the variable definition has only this value. See
    /linux_os/guide/system/permissions/partitions/var_removable_partition.var
    Unless tailoring is used, the rule always passes if /dev/cdrom doesn't
    exist. As the test scenario didn't create /dev/cdrom, but /dev/dvd,
    the rule passed.
    
    2. The test scenario dvd_good_opts.pass.sh for rule
    mount_option_noexec_removable_partitions was also broken even if it
    returned OK. The test scenario was creating /dev/dvd. So again, the
    OVAL check passed because /dev/cdrom didn't exist.  Therefore the test
    scenario was not testing the actual regular expression matching
    in /etc/fstab.
    
    3. The Bash and Ansible remediation weren't functional, because they
    operated with a mount point (second column of /etc/fstab) but they
    were passed values of the block device (which is in first column of
    /etc/fstab). I think that people can mount the removable media to
    arbitrary directories so we should base our checks on the block device
    path instead of mount points. Actually, in all 3 rules we passed the
    device path (/dev/cdrom) as a mount point.  That was probably a wrong
    understanding of the remediation code. The remediations are reworked
    to find entry starting with var_removable_partition value.
    
    4. The OVAL check wasn't checking only /etc/fstab configuration, but was
    also checking runtime state. That didn't conform to the rule
    description. My understanding of the rule description is that the rule
    is only about configuration, there is no mention of runtime.  Moreover,
    the removable device doesn't have to be mounted at the moment of system
    evaluation.
    
    5. The test scenarios didn't cover multiple situation, so apart of
    fixing the broken scenarios, new test scenarios have been added.
    
    We still have a problem that /dev/cdrom is not the only possible
    removable device that could exist. Think of USB flash drives, memory
    cards, etc. The rule descriptions speak about removable media and
    mention USB keys, but the checks don't check them. The challenge
    will be recognizing removable media.
    
    Resolves: RHBZ#1691579

--- linux_os/guide/system/permissions/partitions/mount_option_noexec_removable_partitions/tests/no_partitions.pass.sh
+++ linux_os/guide/system/permissions/partitions/mount_option_noexec_removable_partitions/tests/no_partitions.pass.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-#
-# profiles = xccdf_org.ssgproject.content_profile_C2S
+
+# Regression test for rhbz#1403905
+# The rule should pass if there is no removable media entry in /etc/fstab
 
 touch /dev/cdrom
 echo "" > /etc/fstab
