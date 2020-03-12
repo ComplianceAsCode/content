3b27ac637ae22a85bc62418bea6d31c1e4239277
Matěj Týč <matyc@redhat.com>
2020-03-12 16:50:40 +0100

    Converted sshd_use_approved_ciphers to use the template for bash and ansible.

--- linux_os/guide/services/ssh/ssh_server/sshd_use_approved_ciphers/bash/shared.sh
+++
@@ -1,6 +0,0 @@
-# platform = multi_platform_wrlinux,Red Hat Enterprise Linux 7,Oracle Linux 7,multi_platform_rhv
-
-# Include source function library.
-. /usr/share/scap-security-guide/remediation_functions
-
-replace_or_append '/etc/ssh/sshd_config' '^Ciphers' 'aes128-ctr,aes192-ctr,aes256-ctr,aes128-cbc,3des-cbc,aes192-cbc,aes256-cbc' '@CCENUM@' '%s %s'
