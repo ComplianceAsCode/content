dcefd47e94095cbb39059f5d0ec9ef42593ae595
Gabriel Becker <ggasparb@redhat.com>
2020-04-15 17:15:39 +0200

    Add ansible and bash remediation for rule sshd_set_max_auth_tries.

---
+++ linux_os/guide/services/ssh/ssh_server/sshd_set_max_auth_tries/tests/comment.fail.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+SSHD_CONFIG="/etc/ssh/sshd_config"
+
+if grep -q "^MaxAuthTries" $SSHD_CONFIG; then
+	sed -i "s/^MaxAuthTries.*/# MaxAuthTries 4/" $SSHD_CONFIG
+else
+	echo "# MaxAuthTries 4" >> $SSHD_CONFIG
+fi
