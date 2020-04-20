3e1381a89b54591b7ca6a6b54cf56c6594cb87c0
Gabriel Becker <ggasparb@redhat.com>
2020-04-20 17:46:08 +0200

    Simplify xwindows_runlevel_target artifacts.

--- linux_os/guide/services/xwindows/disabling_xwindows/xwindows_runlevel_target/tests/correct_target.pass.sh
+++ linux_os/guide/services/xwindows/disabling_xwindows/xwindows_runlevel_target/tests/correct_target.pass.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 # platform = multi_platform_sle,multi_platform_rhv,multi_platform_fedora,Red Hat Enterprise Linux 7,Red Hat Enterprise Linux 8
 
-rm -f /etc/systemd/system/default.target
-ln -s /usr/lib/systemd/system/multi-user.target /etc/systemd/system/default.target
+systemctl set-default multi-user.target
