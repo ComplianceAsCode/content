fb9475d77cb0c301a9207cdc5987fc17bcd9036d
Juan Antonio Osorio Robles <jaosorior@redhat.com>
2020-04-21 12:38:29 +0300

    ocp4: Expand unit tests to validate profile selections
    Some checks are not valid for OCP. Let's start checking those and
    erroring out with a reasonable message so folks don't try to enable
    them.

--- tests/unit/kubernetes/unit_test.go
+++ tests/unit/kubernetes/unit_test.go
@@ -7,12 +7,29 @@ import (
 func TestUnit(t *testing.T) {
 	ctx := newUnitTestContext(t)
 	t.Run("Verify kubernetes files are correct", func(t *testing.T) {
-		ctx.assertWithRelevantFiles(func(path string) {
+		ctx.assertWithRelevantContentFiles(func(path string) {
 			obj := ctx.assertObjectIsValid(path)
 
 			if isMachineConfig(obj) {
 				ctx.assertMachineConfigIsValid(obj, path)
 			}
+		})
+	})
+
+	t.Run("Verify OCP profiles don't contain invalid checks", func(t *testing.T) {
+		ctx.asssertWithOCPProfiles(func(path string) {
+			obj := ctx.assertProfileIsValid(path)
+			ctx.asssertSelectionsNotInOCPProfile(obj.Selections, path,
+				[]undesiredSelection{
+					{
+						Name:   "sysctl_net_ipv4_ip_forward",
+						Reason: "Disabling IP forwarding breaks the SDN.",
+					},
+					{
+						Name:   "sysctl_user_max_user_namespaces",
+						Reason: "Limiting user namespaces affects the usage of rootless containers and limits general container capabilities.",
+					},
+				})
 		})
 	})
 }
