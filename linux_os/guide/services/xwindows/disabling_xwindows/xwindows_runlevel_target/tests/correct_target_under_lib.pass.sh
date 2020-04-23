568ea36774cd41778c5ffcb004c11b538697f39b
Gabriel Becker <ggasparb@redhat.com>
2020-04-23 17:13:58 +0200

    OVAL Check for xwindows_runlevel_target consider files from both /usr and /lib directory prefix.

---
+++ linux_os/guide/services/xwindows/disabling_xwindows/xwindows_runlevel_target/tests/correct_target_under_lib.pass.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# platform = multi_platform_sle,multi_platform_rhv,multi_platform_fedora,Red Hat Enterprise Linux 7,Red Hat Enterprise Linux 8
+
+systemctl set-default multi-user.target
