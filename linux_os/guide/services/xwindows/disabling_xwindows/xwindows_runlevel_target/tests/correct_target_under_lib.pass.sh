2965265fcaf9b14b53866e33d18eeb89f50902c1
Gabriel Becker <ggasparb@redhat.com>
2020-04-23 17:32:21 +0200

    Fix location of symlink created by test scenario for xwindows_runlevel_target.

--- linux_os/guide/services/xwindows/disabling_xwindows/xwindows_runlevel_target/tests/correct_target_under_lib.pass.sh
+++ linux_os/guide/services/xwindows/disabling_xwindows/xwindows_runlevel_target/tests/correct_target_under_lib.pass.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # platform = multi_platform_sle,multi_platform_rhv,multi_platform_fedora,Red Hat Enterprise Linux 7,Red Hat Enterprise Linux 8
 
-systemctl set-default multi-user.target
+ln -sf /lib/systemd/system/multi-user.target /etc/systemd/system/default.target
