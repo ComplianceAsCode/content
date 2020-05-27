24c3c92007e6d3f8a684282b1351703523441389
Gabriel Becker <ggasparb@redhat.com>
2020-05-27 18:48:57 +0200

    Remediation requires reboot.
    Update OVAL check to disallow spaces.
    Removed selinuxtype_minimum test scenario since breaks the system.

--- linux_os/guide/system/selinux/selinux_policytype/bash/shared.sh
+++ linux_os/guide/system/selinux/selinux_policytype/bash/shared.sh
@@ -1,4 +1,8 @@
 # platform = multi_platform_wrlinux,multi_platform_rhel,multi_platform_fedora,multi_platform_ol,multi_platform_rhv
+# reboot = true
+# strategy = restrict
+# complexity = low
+# disruption = low
 
 # Include source function library.
 . /usr/share/scap-security-guide/remediation_functions
