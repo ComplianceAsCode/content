8c7ac4cab8deb7de9e53d60add4f879bdf3be4a9
Gabriel Becker <ggasparb@redhat.com>
2020-05-27 13:12:20 +0200

    Fix ansible/bash remediation for rule grub2_enable_selinux.

--- linux_os/guide/system/selinux/grub2_enable_selinux/bash/shared.sh
+++ linux_os/guide/system/selinux/grub2_enable_selinux/bash/shared.sh
@@ -1,9 +1,9 @@
-# platform = multi_platform_rhel,multi_platform_rhv
+# platform = multi_platform_rhel,multi_platform_rhv,multi_platform_fedora,multi_platform_ol,SUSE Linux Enterprise 15
 
-{{% if product == "rhel7" -%}}
-sed -i --follow-symlinks "s/selinux=0//gI" /etc/default/grub /etc/grub2.cfg /etc/grub.d/*
-sed -i --follow-symlinks "s/enforcing=0//gI" /etc/default/grub /etc/grub2.cfg /etc/grub.d/*
-{{%- elif product == "rhel6" -%}}
+{{% if product == "rhel6" -%}}
 sed -i --follow-symlinks "s/selinux=0//gI" /etc/grub.conf
 sed -i --follow-symlinks "s/enforcing=0//gI" /etc/grub.conf
+{{%- else -%}}
+sed -i --follow-symlinks "s/selinux=0//gI" /etc/default/grub /etc/grub2.cfg /etc/grub.d/*
+sed -i --follow-symlinks "s/enforcing=0//gI" /etc/default/grub /etc/grub2.cfg /etc/grub.d/*
 {{%- endif -%}}
