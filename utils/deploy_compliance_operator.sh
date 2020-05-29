32879b466f9c65f51132077447185e6023f83ed9
Juan Antonio Osorio Robles <jaosorior@redhat.com>
2020-05-29 10:39:27 +0300

    Enhance output of utils/deploy_compliance_operator.sh
    This also adds a friendly message on how to get into the appropriate
    namespace

--- utils/deploy_compliance_operator.sh
+++ utils/deploy_compliance_operator.sh
@@ -2,7 +2,7 @@
 
 root_dir=$(git rev-parse --show-toplevel)
 
-echo "Ensuring openshift-compliance namespace exists."
+echo "* Ensuring openshift-compliance namespace exists."
 # If it already exists, this is not a problem.
 oc apply -f "$root_dir/ocp-resources/compliance-operator-ns.yaml"
 
@@ -10,17 +10,21 @@ oc apply -f "$root_dir/ocp-resources/compliance-operator-ns.yaml"
 # namespace
 sleep 5
 
-echo "Creating OperatorSource"
+echo "* Creating OperatorSource"
 # Create operator source so we can install the latest available release which
 # is available from an "external datastore"; meaning, it's our upstream release
 # which is not yet in OperatorHub
 oc apply -f "$root_dir/ocp-resources/compliance-operator-source.yaml"
 
-echo "Creating OperatorGroup"
+echo "* Creating OperatorGroup"
 # Create operator group (defines which namespaces are targetted by the
 # operator)
 oc apply -f "$root_dir/ocp-resources/compliance-operator-operator-group.yaml"
 
-echo "Creating Subscription"
+echo "* Creating Subscription"
 # Create subscription (which installs the operator)
 oc apply -f "$root_dir/ocp-resources/compliance-operator-alpha-subscription.yaml"
+
+echo "* The compliance-operator is now installing."
+echo "* To take it into use, do:"
+echo -e "\t$ oc project openshift-compliance"
