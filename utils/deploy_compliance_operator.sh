c425a677bb484595789fe6036c2d9b9d6274e8e4
Juan Antonio Osorio Robles <jaosorior@redhat.com>
2020-05-29 10:28:42 +0300

    Quick fixes to utils/deploy_compliance_operator.sh script
    This:
    
    * Makes the script idempotent by using the `oc apply` command instead of
      `oc create`
    
    * Removes the deprecated resource `csc`.

--- utils/deploy_compliance_operator.sh
+++ utils/deploy_compliance_operator.sh
@@ -4,23 +4,19 @@ root_dir=$(git rev-parse --show-toplevel)
 
 echo "Ensuring openshift-compliance namespace exists."
 # If it already exists, this is not a problem.
-oc create -f "$root_dir/ocp-resources/compliance-operator-ns.yaml" || true
+oc apply -f "$root_dir/ocp-resources/compliance-operator-ns.yaml"
 
 echo "Creating OperatorSource"
 # Create operator source so we can install the latest available release which
 # is available from an "external datastore"; meaning, it's our upstream release
 # which is not yet in OperatorHub
-oc create -f "$root_dir/ocp-resources/compliance-operator-source.yaml"
-
-echo "Creating CatalogSourceConfig"
-# This allows us to create subscriptions
-oc create -f "$root_dir/ocp-resources/compliance-operator-csc.yaml"
+oc apply -f "$root_dir/ocp-resources/compliance-operator-source.yaml"
 
 echo "Creating OperatorGroup"
 # Create operator group (defines which namespaces are targetted by the
 # operator)
-oc create -f "$root_dir/ocp-resources/compliance-operator-operator-group.yaml"
+oc apply -f "$root_dir/ocp-resources/compliance-operator-operator-group.yaml"
 
 echo "Creating Subscription"
 # Create subscription (which installs the operator)
-oc create -f "$root_dir/ocp-resources/compliance-operator-alpha-subscription.yaml"
+oc apply -f "$root_dir/ocp-resources/compliance-operator-alpha-subscription.yaml"
