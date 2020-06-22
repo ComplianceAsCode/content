c07481ea627f8b7acb6fe2c6a711069d9b5111f8
cyarbrough76 <42849651+cyarbrough76@users.noreply.github.com>
2020-06-22 15:41:26 -0500

    Update shared.sh
    Edited remediation for chronyd or ntpd set maxpoll. Previous code resulted in sed error when applied.

--- linux_os/guide/services/ntp/chronyd_or_ntpd_set_maxpoll/bash/shared.sh
+++ linux_os/guide/services/ntp/chronyd_or_ntpd_set_maxpoll/bash/shared.sh
@@ -11,6 +11,4 @@ config_file="/etc/ntp.conf"
 sed -i "s/^\(server.*maxpoll\) [0-9][0-9]*\(.*\)$/\1 $var_time_service_set_maxpoll \2/" "$config_file"
 
 # Add maxpoll to server entries without maxpoll
-grep "^server" "$config_file" | grep -v maxpoll | while read -r line ; do
-        sed -i "s/$line/& maxpoll $var_time_service_set_maxpoll/" "$config_file"
-done
+sed -i "/maxpoll/! s/^\(server [0-9.]\+\)/\1 maxpoll $var_time_service_set_maxpoll/g" "$config_file"
