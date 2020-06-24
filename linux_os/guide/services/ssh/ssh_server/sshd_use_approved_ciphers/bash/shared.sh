a200faecfe6f818c29c33c85ca344ac447fef981
cyarbrough76 <42849651+cyarbrough76@users.noreply.github.com>
2020-06-24 09:16:22 -0500

    Edit remediation for use_approved_ciphers
    added RHEL6 remediation to shared.sh rather than having separate rhel6.sh

--- linux_os/guide/services/ssh/ssh_server/sshd_use_approved_ciphers/bash/shared.sh
+++ linux_os/guide/services/ssh/ssh_server/sshd_use_approved_ciphers/bash/shared.sh
@@ -1,4 +1,4 @@
-# platform = multi_platform_wrlinux,Red Hat Enterprise Linux 7,Oracle Linux 7
+# platform = multi_platform_wrlinux,Red Hat Enterprise Linux 7,Red Hat Enterprise Linux 6,Oracle Linux 7
 
 # Include source function library.
 . /usr/share/scap-security-guide/remediation_functions
