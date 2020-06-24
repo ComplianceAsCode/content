724043a8d167879f4e94b2eb9ef73b78be35777f
cyarbrough76 <42849651+cyarbrough76@users.noreply.github.com>
2020-06-24 17:12:55 -0500

    edit bash for smartcard_configure_cert_checking
    add condition to check for and install pcsc-lite

--- linux_os/guide/system/accounts/accounts-physical/screen_locking/smart_card_login/smartcard_configure_cert_checking/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-physical/screen_locking/smart_card_login/smartcard_configure_cert_checking/bash/shared.sh
@@ -2,6 +2,7 @@
 
 # Install required packages
 if ! rpm --quiet -q pam_pkcs11; then yum -y -d 1 install pam_pkcs11; fi
+if ! rpm --quiet -q pcsc-lite; then yum -y -d 1 install pcsc-lite; fi
 
 if grep "^\s*cert_policy" /etc/pam_pkcs11/pam_pkcs11.conf | grep -qv "ocsp_on"; then
 	sed -i "/^\s*#/! s/cert_policy.*/cert_policy = ca, ocsp_on, signature;/g" /etc/pam_pkcs11/pam_pkcs11.conf
