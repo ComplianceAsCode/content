e715fc2950d3e43512a2cbc75e88000603508c56
cyarbrough76 <42849651+cyarbrough76@users.noreply.github.com>
2020-06-24 11:27:54 -0500

    Added condition for rhel6 specific info in rule, and formatting in oval and bash

--- linux_os/guide/system/accounts/accounts-pam/display_login_attempts/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-pam/display_login_attempts/bash/shared.sh
@@ -1,12 +1,9 @@
 # platform = Red Hat Virtualization 4,multi_platform_fedora,multi_platform_ol,multi_platform_rhel,multi_platform_wrlinux
 
-{{%- if product == "rhel6" -%}}
+{{% if product == "rhel6" %}}
+sed -i --follow-symlinks "/pam_lastlog.so/d" /etc/pam.d/system-auth
 sed -i --follow-symlinks '/pam_limits.so/a session\t    required\t  pam_lastlog.so showfailed' /etc/pam.d/system-auth
 {{% else %}}
-if grep -q "^session.*pam_lastlog.so" /etc/pam.d/postlogin; then
-	sed -i --follow-symlinks "/pam_lastlog.so/d" /etc/pam.d/postlogin
-fi
-
-echo "session     [default=1]   pam_lastlog.so nowtmp showfailed" >> /etc/pam.d/postlogin
-echo "session     optional      pam_lastlog.so silent noupdate showfailed" >> /etc/pam.d/postlogin
-{{%- endif -%}}
+sed -i --follow-symlinks "/pam_lastlog.so/d" /etc/pam.d/postlogin
+echo "session     required      pam_lastlog.so showfailed" >> /etc/pam.d/postlogin
+{{% endif %}}
