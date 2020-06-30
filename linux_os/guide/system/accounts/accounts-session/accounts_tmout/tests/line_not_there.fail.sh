ca48a54eb0230d7408c60843c956a132d8aeb289
cyarbrough76 <42849651+cyarbrough76@users.noreply.github.com>
2020-06-30 09:45:50 -0500

    edited test per feedback

--- linux_os/guide/system/accounts/accounts-session/accounts_tmout/tests/line_not_there.fail.sh
+++ linux_os/guide/system/accounts/accounts-session/accounts_tmout/tests/line_not_there.fail.sh
@@ -2,4 +2,6 @@
 #
 # profiles = xccdf_org.ssgproject.content_profile_ospp
 
-sed -i "/^TMOUT.*/d" /etc/profile.d/tmout.sh
+if grep -q "TMOUT" /etc/profile.d/tmout.sh; then
+	sed -i "/^TMOUT.*/d" /etc/profile.d/tmout.sh
+fi
