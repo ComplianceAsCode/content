4f6682ba02da46151f65ab0c65145b1e2c7e3fe5
Gabriel Becker <ggasparb@redhat.com>
2020-07-30 15:23:00 +0200

    Use helper functions to install dconf and gdm.

--- linux_os/guide/system/software/gnome/gnome_screen_locking/dconf_gnome_screensaver_lock_locked/tests/correct_value.pass.sh
+++ linux_os/guide/system/software/gnome/gnome_screen_locking/dconf_gnome_screensaver_lock_locked/tests/correct_value.pass.sh
@@ -3,6 +3,6 @@
 
 . $SHARED/dconf_test_functions.sh
 
-yum -y install dconf
+install_dconf_and_gdm_if_needed
 clean_dconf_settings
 add_dconf_lock "org/gnome/desktop/screensaver" "lock-enabled" "local.d" "00-security-settings"
