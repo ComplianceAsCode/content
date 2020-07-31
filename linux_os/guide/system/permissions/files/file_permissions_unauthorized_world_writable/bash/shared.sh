c716119a5403951955b19950477774193aa2115f
Matěj Týč <matej.tyc@gmail.com>
2020-07-31 17:26:14 +0200

    Simplified find invocation
    - use -xdev instead of manual exclusion of procedural filesystems
    - drop -v from chmod
    - use \; instead of + so the max command-line length is not an issue.

--- linux_os/guide/system/permissions/files/file_permissions_unauthorized_world_writable/bash/shared.sh
+++ linux_os/guide/system/permissions/files/file_permissions_unauthorized_world_writable/bash/shared.sh
@@ -1,3 +1,3 @@
-# platform = multi_platform_rhel
+# platform = multi_platform_all
 
-(cd / && find . -type f -perm -002 -not -path "./selinux/*" -not -path "./proc/*" -not -path "./sys/*" -not -path "./cgroup/*" -exec chmod -v o-w {} +)
+(cd / && find / -xdev -type f -perm -002 -exec chmod o-w {} \;)
