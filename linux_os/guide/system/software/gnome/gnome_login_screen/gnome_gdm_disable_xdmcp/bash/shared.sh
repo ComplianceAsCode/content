b8a5bb00fe038a8ae030f54d5ea2f951a96bae9f
Matěj Týč <matyc@redhat.com>
2020-08-12 16:35:00 +0200

    Add Bash and Ansible remediations as Jinja macros.

--- linux_os/guide/system/software/gnome/gnome_login_screen/gnome_gdm_disable_xdmcp/bash/shared.sh
+++ linux_os/guide/system/software/gnome/gnome_login_screen/gnome_gdm_disable_xdmcp/bash/shared.sh
@@ -1,17 +1,3 @@
 # platform = multi_platform_all
 
-CONF="/etc/gdm/custom.conf"
-DISABL_XDMCP_REGEX="[[:space:]]*\[xdmcp]([^\n\[]*\n+)+?[[:space:]]*Enable"
-XDMCP_REGEX="[[:space:]]*\[xdmcp]"
-
-# This is a candidate of bash ini Jinja macro
-# Try find [xdmcp] and Enable in custom.conf, if it exists, set
-# to false, if it isn't here, add it, if [xdmcp] doesn't exist, add it there
-if grep -qzosP "$DISABL_XDMCP_REGEX" $CONF; then
-    sed -i "s/Enable[^(\n)]*/Enable=false/" $CONF
-elif grep -qs "$XDMCP_REGEX" $CONF; then
-    sed -i "/$XDMCP_REGEX/a Enable=false" $CONF
-else
-    mkdir -p /etc/gdm
-    printf '%s\n' "[xdmcp]" "Enable=false" >> $CONF
-fi
+{{{ bash_ini_file_set("/etc/gdm/custom.conf", "xdmcp", "Enable", "false") }}}
