169e26c88c60b7036163475b890c3599a9576692
Gabriel Becker <ggasparb@redhat.com>
2020-08-19 18:32:00 +0200

    Check sssd conf.d files and fix bash remediation.

--- linux_os/guide/services/sssd/sssd_enable_pam_services/tests/services_pam_missing.pass.sh
+++ linux_os/guide/services/sssd/sssd_enable_pam_services/tests/services_pam_missing.pass.sh
@@ -2,6 +2,7 @@
 #
 # profiles = xccdf_org.ssgproject.content_profile_stig
 
+SSSD_SERVICES_REGEX_SHORT="^[[:space:]]*services.*$"
 SSSD_CONF="/etc/sssd/sssd.conf"
 
 yum -y install /usr/lib/systemd/system/sssd.service
@@ -9,7 +10,8 @@ rm -rf /etc/sssd/conf.d/
 rm -f SSSD_CONF
 cat <<EOF > $SSSD_CONF
 [sssd]
-services = nss,pam
+section1 = key
+section2 = nss
 [pam]
 example1 = abc
 EOF
