5f2d44019ad0db6170b79e32f27e26549298d48c
Matěj Týč <matyc@redhat.com>
2020-08-20 13:55:15 +0200

    Corrected naming and default remote directories.
    Moved from "non-local" to "remote".
    Provided more accurate defaults for remote dirs.

---
+++ linux_os/guide/system/permissions/files/file_permissions_unauthorized_world_writable/tests/wwritable_in_mnt.fail.sh
@@ -0,0 +1,4 @@
+# platform: multi_platform_all
+
+touch /mnt/world-writable
+chmod a+w /mnt/world-writable
--- linux_os/guide/system/permissions/files/file_permissions_unauthorized_world_writable/tests/wwritable_in_tmp.pass.sh
+++
@@ -1,4 +0,0 @@
-# platform: multi_platform_all
-
-touch /tmp/world-writable
-chmod a+w /tmp/world-writable
