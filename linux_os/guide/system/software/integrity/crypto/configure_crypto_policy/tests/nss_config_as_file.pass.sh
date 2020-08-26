756da8a3d03f0696d94cd4dcd2a067afdaa0d2bd
Milan Lysonek <mlysonek@redhat.com>
2020-08-26 17:25:10 +0200

    Remove Fedora platform from tests working with FIPS:OSPP crypto policy

--- linux_os/guide/system/software/integrity/crypto/configure_crypto_policy/tests/nss_config_as_file.pass.sh
+++ linux_os/guide/system/software/integrity/crypto/configure_crypto_policy/tests/nss_config_as_file.pass.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# platform = multi_platform_fedora,Red Hat Enterprise Linux 8
+# platform = Red Hat Enterprise Linux 8
 # profiles = xccdf_org.ssgproject.content_profile_ospp
 
 update-crypto-policies --set "FIPS:OSPP"
