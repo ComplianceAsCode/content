50e93d95f61c1167990834f5d05652ad12b05b09
Gabriel Becker <ggasparb@redhat.com>
2020-09-01 19:03:58 +0200

    Create bash and ansible jinja macros for sssd ldap rules.

--- linux_os/guide/services/sssd/sssd-ldap/sssd_ldap_start_tls/bash/shared.sh
+++ linux_os/guide/services/sssd/sssd-ldap/sssd_ldap_start_tls/bash/shared.sh
@@ -3,23 +3,5 @@
 # Include source function library.
 . /usr/share/scap-security-guide/remediation_functions
 
-SSSD_CONF="/etc/sssd/sssd.conf"
-LDAP_REGEX='[[:space:]]*\[domain\/[^]]*]([^(\n)]*(\n)+)+?[[:space:]]*ldap_id_use_start_tls'
-AD_REGEX='[[:space:]]*\[domain\/[^]]*]([^(\n)]*(\n)+)+?[[:space:]]*id_provider[[:space:]]*=[[:space:]]*((?i)ad)[[:space:]]*$'
-DOMAIN_REGEX="[[:space:]]*\[domain\/[^]]*]"
+{{{ bash_sssd_ldap_config(parameter="ldap_id_use_start_tls", value="True") }}}
 
-# Check if id_provider is not set to ad (Active Directory) which makes start_tls not applicable, note the -v option to invert the grep.
-# Try to find [domain/..] and ldap_id_use_start_tls in sssd.conf, if it exists, set to 'True'
-# if ldap_id_use_start_tls isn't here, add it
-# if [domain/..] doesn't exist, add it here for default domain
-if grep -qvzosP $AD_REGEX $SSSD_CONF; then
-        if grep -qzosP $LDAP_REGEX $SSSD_CONF; then
-                sed -i 's/ldap_id_use_start_tls[^(\n)]*/ldap_id_use_start_tls = True/' $SSSD_CONF
-        elif grep -qs $DOMAIN_REGEX $SSSD_CONF; then
-                sed -i "/$DOMAIN_REGEX/a ldap_id_use_start_tls = True" $SSSD_CONF
-        else
-                mkdir -p /etc/sssd
-                touch $SSSD_CONF
-                echo -e "[domain/default]\nldap_id_use_start_tls = True" >> $SSSD_CONF
-        fi
-fi
