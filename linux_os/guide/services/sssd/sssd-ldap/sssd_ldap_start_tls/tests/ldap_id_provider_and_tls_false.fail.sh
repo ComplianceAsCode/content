7778f247ed6cf2787235cd03b7c5428a14846c27
Gabriel Becker <ggasparb@redhat.com>
2020-09-04 16:10:07 +0200

    Install required sssd package in sssd ldap test scenarios.

--- linux_os/guide/services/sssd/sssd-ldap/sssd_ldap_start_tls/tests/ldap_id_provider_and_tls_false.fail.sh
+++ linux_os/guide/services/sssd/sssd-ldap/sssd_ldap_start_tls/tests/ldap_id_provider_and_tls_false.fail.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # profiles = xccdf_org.ssgproject.content_profile_stig
 
+yum -y install /usr/lib/systemd/system/sssd.service
+systemctl enable sssd
+
 . $SHARED/setup_config_files.sh
 setup_correct_sssd_config
 sed -i 's/ldap_id_use_start_tls = True/ldap_id_use_start_tls = False/' /etc/sssd/sssd.conf
