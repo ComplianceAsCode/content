09810912211e7f29643d47868b36acd2ae59e58d
Milan Lysonek <milan.lysonek@gmail.com>
2020-09-16 16:26:21 +0200

    Merge pull request #6082 from ggbecker/fix-sudo-line-in-file-macro
    Fix regex pattern in jinja macros for sudo related rules.
    
    (cherry picked from commit 87badebaabe3c75794383009492c42dab69e106b)

---
+++ linux_os/guide/system/software/sudo/sudo_require_authentication/bash/shared.sh
@@ -0,0 +1,9 @@
+# platform = multi_platform_all
+# reboot = false
+# strategy = restrict
+# complexity = low
+# disruption = low
+
+{{{ bash_sudo_remove_config("NOPASSWD", "NOPASSWD[\s]*\:") }}}
+
+{{{ bash_sudo_remove_config("!authenticate", "\!authenticate") }}}
