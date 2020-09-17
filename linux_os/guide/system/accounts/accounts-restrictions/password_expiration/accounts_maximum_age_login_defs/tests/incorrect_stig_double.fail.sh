e5060290c75d694497a99fc82b69ac75805ba42a
Watson Sato <wsato@redhat.com>
2020-09-17 22:01:32 +0200

    Fix PCRE_ERROR_MATCHLIMIT in PASS_MAX_DAYS
    Fix memory limit match error when scanning an actual /etc/login.defs
    file, which is much larger than the test files.
    
    Removed the single line behavior and added tests to check whether it is
    mathing the last PASS_MAX_DAYS.

---
+++ linux_os/guide/system/accounts/accounts-restrictions/password_expiration/accounts_maximum_age_login_defs/tests/incorrect_stig_double.fail.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# profiles = xccdf_org.ssgproject.content_profile_stig
+
+rm -f /etc/login.defs
+echo "PASS_MAX_DAYS        60" > /etc/login.defs
+echo "PASS_MAX_DAYS        120" >> /etc/login.defs
