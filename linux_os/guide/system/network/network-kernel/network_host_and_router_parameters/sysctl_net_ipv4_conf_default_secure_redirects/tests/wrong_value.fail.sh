be239e23ae02be6d6f6f9bc355c2b6ced79b76d8
Matus Marhefka <mmarhefk@redhat.com>
2020-09-30 17:01:10 +0200

    Update test scenarios for sysctl_net_ipv4_conf_default_secure_redirects rule
    Remove profile metadata as they are not needed and the rule can be
    tested using 'all' profile.

--- linux_os/guide/system/network/network-kernel/network_host_and_router_parameters/sysctl_net_ipv4_conf_default_secure_redirects/tests/wrong_value.fail.sh
+++ linux_os/guide/system/network/network-kernel/network_host_and_router_parameters/sysctl_net_ipv4_conf_default_secure_redirects/tests/wrong_value.fail.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-#
-# profiles = xccdf_org.ssgproject.content_profile_ospp
 
 if grep -q "^net.ipv4.conf.default.secure_redirects" /etc/sysctl.conf; then
 	sed -i "s/^net.ipv4.conf.default.secure_redirects.*/net.ipv4.conf.default.secure_redirects = 1/" /etc/sysctl.conf
