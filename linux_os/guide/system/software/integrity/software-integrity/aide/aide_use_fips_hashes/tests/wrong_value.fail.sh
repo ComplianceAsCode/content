78b70a215233846bb1590b2c9fa436372e8cdf18
Gabriel Becker <ggasparb@redhat.com>
2020-10-05 13:34:22 +0200

    Fix regex in aide rules to consider first letter as uppercase.

--- linux_os/guide/system/software/integrity/software-integrity/aide/aide_use_fips_hashes/tests/wrong_value.fail.sh
+++ linux_os/guide/system/software/integrity/software-integrity/aide/aide_use_fips_hashes/tests/wrong_value.fail.sh
@@ -5,6 +5,8 @@ yum install -y aide
 
 cat >/etc/aide.conf <<EOL
 All = p+i+n+u+g+s+m+S+acl+xattrs+selinux
+option = yes
+Group = selinux
 /bin All # apply the custom rule to the files in bin 
 /sbin All # apply the same custom rule to the files in sbin 
 EOL
