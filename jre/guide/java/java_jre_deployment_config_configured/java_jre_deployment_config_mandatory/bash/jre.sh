ab4ef0b8366b26a2db366b6c28481632fc517f1e
Carlos Matos <cmatos@redhat.com>
2020-10-22 14:45:06 -0400

    Adding more changes to Java JRE content

--- jre/guide/java/java_jre_deployment_config_configured/java_jre_deployment_config_mandatory/bash/jre.sh
+++ jre/guide/java/java_jre_deployment_config_configured/java_jre_deployment_config_mandatory/bash/jre.sh
@@ -1,8 +1,8 @@
 # platform = Java Runtime Environment
 JAVA_CONFIG="/etc/.java/deployment/deployment.config"
 
-grep -q "^deployment.system.config.mandatory=false$" ${JAVA_CONFIG} && \
-sed -i "s/deployment.system.config.mandatory=.*/deployment.system.config.mandatory=false/g" ${JAVA_CONFIG}
+grep -q "^deployment.system.config.mandatory=.*" ${JAVA_CONFIG} && \
+sed -i "s/deployment.system.config.mandatory=.*/deployment.system.config.mandatory=true/g" ${JAVA_CONFIG}
 if ! [ $? -eq 0 ] ; then
-  echo "deployment.system.config.mandatory=false" >> ${JAVA_CONFIG}
+  echo "deployment.system.config.mandatory=true" >> ${JAVA_CONFIG}
 fi
