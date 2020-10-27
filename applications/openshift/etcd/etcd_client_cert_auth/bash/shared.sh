ecd95fd43281d7c4e5dcac24efbe97365c99d70d
Jakub Hrozek <jhrozek@redhat.com>
2020-10-27 13:20:09 +0100

    Remove bash remediations from applications/openshift
    The proper way of configuring OCP4 is through machine config. Moreover,
    these remediations are not tested and any code that is not tested should
    be considered bitrot and removed.

--- applications/openshift/etcd/etcd_client_cert_auth/bash/shared.sh
+++
@@ -1,6 +0,0 @@
-# platform = multi_platform_ocp
-#
-# Include source function library
-. /usr/share/scap-security-guide/remediation_functions
-
-replace_or_append '/etc/etcd/etcd.conf' '^ETCD_CLIENT_CERT_AUTH=' 'true' '@CCENUM@' '%s=%s'
