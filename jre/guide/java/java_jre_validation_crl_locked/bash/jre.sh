4ebe80c6446a30817a305208d9c66343239821cc
Carlos Matos <cmatos@redhat.com>
2020-11-10 13:50:18 -0500

    Part 1 of remediation updates/additions for Java JRE

--- jre/guide/java/java_jre_validation_crl_locked/bash/jre.sh
+++ jre/guide/java/java_jre_validation_crl_locked/bash/jre.sh
@@ -1,5 +1,14 @@
 # platform = Java Runtime Environment
 JAVA_PROPERTIES="/etc/.java/deployment/deployment.properties"
+JAVA_DIR="/etc/.java/deployment"
+
+if [ ! -d ${JAVA_DIR} ] ; then
+  mkdir -p -m 755 ${JAVA_DIR}
+fi
+
+if [ ! -e ${JAVA_PROPERTIES} ]; then
+  touch ${JAVA_PROPERTIES} && chmod 644 ${JAVA_PROPERTIES}
+fi
 
 grep -q "^deployment.security.validation.crl.locked$" ${JAVA_PROPERTIES}
 if ! [ $? -eq 0 ] ; then
