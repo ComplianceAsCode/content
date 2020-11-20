8315bfbcdc2892ecf584ec902ff397a6dd8f35f6
Juan Antonio Osorio Robles <jaosorior@redhat.com>
2020-11-20 15:16:32 +0200

    ocp4/e2e: Only apply manual remediations once
    If a manual remediation appears more than one (a role evaluates the same
    rule as another role), let's only execute it once.

--- tests/ocp4e2e/helpers.go
+++ tests/ocp4e2e/helpers.go
@@ -622,7 +622,7 @@ func (ctx *e2econtext) getInvalidResultsFromSuite(s string) int {
 }
 
 func (ctx *e2econtext) verifyCheckResultsForSuite(s string, afterRemediations bool) (int, []string) {
-	manualRemediations := []string{}
+	manualRemediationSet := map[string]bool{}
 	resList := &cmpv1alpha1.ComplianceCheckResultList{}
 	matchLabels := dynclient.MatchingLabels{
 		cmpv1alpha1.SuiteLabel: s,
@@ -643,8 +643,13 @@ func (ctx *e2econtext) verifyCheckResultsForSuite(s string, afterRemediations bo
 			ctx.t.Error(err)
 		}
 		if manualRem != "" {
-			manualRemediations = append(manualRemediations, manualRem)
+			manualRemediationSet[manualRem] = true
 		}
+	}
+
+	manualRemediations := []string{}
+	for key := range manualRemediationSet {
+		manualRemediations = append(manualRemediations, key)
 	}
 
 	return len(resList.Items), manualRemediations
