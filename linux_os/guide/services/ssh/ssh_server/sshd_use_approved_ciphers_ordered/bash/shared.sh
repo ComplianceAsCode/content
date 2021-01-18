fd95afe448b8fee2ed35baad929eed3470aad51e
Vojtech Polasek <vpolasek@redhat.com>
2021-01-18 14:33:45 +0100

    add rule

---
+++ linux_os/guide/services/ssh/ssh_server/sshd_use_approved_ciphers_ordered/bash/shared.sh
@@ -0,0 +1,7 @@
+# platform = multi_platform_wrlinux,Red Hat Enterprise Linux 7,Oracle Linux 7
+
+if grep -q -P '^[[:space:]]*[Cc]iphers[[:space:]]+' /etc/ssh/sshd_config; then
+  sed -i 's/^[\s]*[Cc]iphers.*/Ciphers aes256-ctr,aes192-ctr,aes128-ctr/' /etc/ssh/sshd_config
+else
+  echo "Ciphers aes256-ctr,aes192-ctr,aes128-ctr" >> /etc/ssh/sshd_config
+fi
