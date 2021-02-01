bbc64ba95f6edbb8a36ca0028d30c18b24581038
Gabriel Becker <ggasparb@redhat.com>
2021-02-01 19:48:57 +0100

    Improve yamlfile_value template.
    Remove option to use multiple value when using xccdf_variable option
    which retrieves a not embedded value. Also improves error handling of
    input data.

--- shared/templates/yamlfile_value/template.py
+++ shared/templates/yamlfile_value/template.py
@@ -1,10 +1,23 @@
 def preprocess(data, lang):
 
     if data.get("xccdf_variable") and data.get("embedded_data") == "true":
-        if not data.get("values"):
+        values = data.get("values", [{}])
+        if len(values) > 1:
+            raise ValueError(
+                "Only a single value can be checked when querying "
+                "for a 'xccdf_value' that returns an embedded value. "
+                "Rule ID: {}".format(data["_rule_id"]))
+        elif not values[0].get("value"):
             raise ValueError(
                 "You should specify a capture regex in the 'value' field "
                 "when querying for a 'xccdf_value' that returns an embedded value. "
+                "Rule ID: {}".format(data["_rule_id"]))
+
+    if data.get("xccdf_variable") and data.get("embedded_data") != "true":
+        if data.get("values"):
+            raise ValueError(
+                "You cannot specify the 'value' field when querying "
+                "for a 'xccdf_value' that doesn't return an embedded value. "
                 "Rule ID: {}".format(data["_rule_id"]))
 
     data["embedded_data"] = data.get("embedded_data", "false") == "true"
