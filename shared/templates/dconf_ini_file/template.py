6541ed76a4fe16cd883d143805b5a77603e1d076
Samuel Warren <swarren@redhat.com>
2021-02-16 14:44:18 -0800

    adds remediation for 1.2.25 api_server_audit_log_maxsize

--- shared/templates/dconf_ini_file/template.py
+++ shared/templates/dconf_ini_file/template.py
@@ -1,6 +1,7 @@
 
 import os
 
+
 def preprocess(data, lang):
     data["lock_path"] = os.path.join(data["path"], "locks")
     data["dconf_database_directory"] = os.path.basename(os.path.normpath(data["path"]))
