6162883860ff57238dc2eafb9ca6a92506a69b0f
Vojtech Polasek <vpolasek@redhat.com>
2021-03-09 17:03:01 +0100

    change remediation to prefer pwhistory

--- linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_password_pam_unix_remember/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_password_pam_unix_remember/bash/shared.sh
@@ -7,9 +7,15 @@ AUTH_FILES[1]="/etc/pam.d/password-auth"
 
 for pamFile in "${AUTH_FILES[@]}"
 do
-	if grep -q "remember=" $pamFile; then
-		sed -i --follow-symlinks "s/\(^password.*sufficient.*pam_unix.so.*\)\(\(remember *= *\)[^ $]*\)/\1remember=$var_password_pam_unix_remember/" $pamFile
+	if grep -q ".*pwhistory\.so.*remember=" $pamFile; then
+		sed -i -E --follow-symlinks "s/(^password.*pam_pwhistory\.so.*)(remember\s*=\s*[0-9]+)(.*$)/\1remember=$var_password_pam_unix_remember \3/" $pamFile
+	elif grep -q ".*pwhistory\.so.*" $pamFile; then
+		sed -i --follow-symlinks "/^password[[:space:]]\+.*[[:space:]]\+pam_pwhistory\.so/ s/$/ remember=$var_password_pam_unix_remember/" $pamFile
+	elif grep -q ".*unix\.so.*remember=" $pamFile; then
+		sed -i -E --follow-symlinks "s/(^password.*pam_unix\.so.*)(remember\s*=\s*[0-9]+)(.*$)/\1remember=$var_password_pam_unix_remember \3/" $pamFile
+	elif grep -q ".*unix\.so.*" $pamFile; then
+		sed -i --follow-symlinks "/^password[[:space:]]\+.*[[:space:]]\+pam_unix\.so/ s/$/ remember=$var_password_pam_unix_remember/" $pamFile
 	else
-		sed -i --follow-symlinks "/^password[[:space:]]\+sufficient[[:space:]]\+pam_unix.so/ s/$/ remember=$var_password_pam_unix_remember/" $pamFile
+		sed -i -E --follow-symlinks '0,/^password/s/(^password.*)/\1\npassword requisite pam_pwhistory.so remember=$var_password_pam_unix_remember/' $pamFile
 	fi
 done
