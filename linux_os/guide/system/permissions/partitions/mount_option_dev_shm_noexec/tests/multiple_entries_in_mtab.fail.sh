8223da60b3516a949bf8cc239a56ede6426c9ead
Milan Lysonek <mlysonek@redhat.com>
2021-03-10 16:29:52 +0100

    Change mount_option_dev_shm_noexec test scenarios to scenarios that actually adjust a machine instead of testing default state

--- linux_os/guide/system/permissions/partitions/mount_option_dev_shm_noexec/tests/multiple_entries_in_mtab.fail.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cat /etc/mtab > /etc/mtab.old
-# destroy symlink
-rm -f /etc/mtab
-cp /etc/mtab.old /etc/mtab
-echo "tmpfs /dev/shm tmpfs rw,seclabel,relatime 0 0" >> /etc/mtab
-echo "tmpfs /dev/shm tmpfs rw,seclabel,relatime 0 0" >> /etc/mtab
