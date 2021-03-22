bf3d2259ef7a9aabf897b1bb04e7cb0c446c6ccd
Matus Marhefka <mmarhefk@redhat.com>
2021-03-22 16:43:39 +0100

    Add tests for rule accounts_minimum_age_login_defs

---
+++ linux_os/guide/system/accounts/accounts-restrictions/password_expiration/accounts_minimum_age_login_defs/tests/disabled.fail.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+grep -q "^PASS_MIN_DAYS" /etc/login.defs && \
+  sed -i "s/PASS_MIN_DAYS.*/PASS_MIN_DAYS\t-1/g" /etc/login.defs
+if ! [ $? -eq 0 ]; then
+    echo -e "PASS_MIN_DAYS\t-1" >> /etc/login.defs
+fi
