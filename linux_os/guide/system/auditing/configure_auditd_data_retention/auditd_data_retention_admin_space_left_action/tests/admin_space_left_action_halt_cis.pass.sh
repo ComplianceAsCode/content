3ec3d5454ad6231f576d946e64e464ca54d3060f
Matus Marhefka <mmarhefk@redhat.com>
2021-03-30 13:08:48 +0200

    Add test for auditd_data_retention_admin_space_left_action and CIS profile

---
+++ linux_os/guide/system/auditing/configure_auditd_data_retention/auditd_data_retention_admin_space_left_action/tests/admin_space_left_action_halt_cis.pass.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+# profiles = xccdf_org.ssgproject.content_profile_cis
+# remediation = bash
+
+. $SHARED/auditd_utils.sh
+prepare_auditd_test_enviroment
+set_parameters_value /etc/audit/auditd.conf "admin_space_left_action" "halt"
