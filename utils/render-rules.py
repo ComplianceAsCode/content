e036e5123fb6ebcc2744bd90123d320a43f39159
Matěj Týč <matyc@redhat.com>
2021-04-12 21:32:05 +0200

    Render rules related to a component.

---
+++ utils/render-rules.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+
+from glob import glob
+import collections
+import os
+import pathlib
+
+import argparse
+
+import ssg.build_yaml
+import ssg.controls
+import ssg.yaml
+import ssg.jinja
+import template_renderer
+
+
+class HtmlOutput(template_renderer.Renderer):
+    TEMPLATE_NAME = "rules-template.html"
+
+    def _get_all_compiled_profiles(self):
+        compiled_profiles = glob(str(self.built_content_path / "profiles" / "*.profile"))
+        profiles = []
+        for p in compiled_profiles:
+            profiles.append(ssg.build_yaml.Profile.from_yaml(p))
+        return profiles
+
+    def _set_rule_profiles_membership(self, rule, profiles):
+        rule.in_profiles = []
+        for p in profiles:
+            if rule.id_ in p.selected:
+                rule.in_profiles.append(p)
+
+    def process_rules(self, rule_files, component):
+        rules = []
+        profiles = self._get_all_compiled_profiles()
+
+        for r_file in rule_files:
+            rule = ssg.build_yaml.Rule.from_yaml(r_file, self.env_yaml)
+            self._set_rule_relative_definition_location(rule)
+            self._set_rule_profiles_membership(rule, profiles)
+
+            rules.append(rule)
+
+        self.template_data["rules"] = rules
+        self.template_data["component"] = component
+
+
+def parse_args():
+    parser = HtmlOutput.create_parser(
+        "Pass a list of rule YAMLs (that are related to a component), and the script will "
+        "render their summary as an HTML along with links to the usage of these rules in profiles "
+        "and with links to the upstream rule source.")
+    parser.add_argument(
+        "--component", help="Name of the component.")
+    parser.add_argument(
+        "rule", metavar="FILENAME", nargs="+", help="The rule YAML files")
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    renderer = HtmlOutput(args.product, args.build_dir)
+    renderer.process_rules(args.rule, args.component)
+    renderer.output_results(args)
