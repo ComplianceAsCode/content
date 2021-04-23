7a657939b4fc7b7960d912bb86edc003afd46e68
yarunachalam <yarunachalam@suse.com>
2021-04-23 17:16:40 +0000

    Fix remediation to match stig
    /var/run/utmp -> /run/utmp

--- linux_os/guide/system/auditing/auditd_configure_rules/audit_rules_session_events_utmp/bash/shared.sh
+++ linux_os/guide/system/auditing/auditd_configure_rules/audit_rules_session_events_utmp/bash/shared.sh
@@ -5,5 +5,5 @@
 
 # Perform the remediation
 # Perform the remediation for both possible tools: 'auditctl' and 'augenrules'
-fix_audit_watch_rule "auditctl" "/var/run/utmp" "wa" "session"
-fix_audit_watch_rule "augenrules" "/var/run/utmp" "wa" "session"
+fix_audit_watch_rule "auditctl" "/run/utmp" "wa" "session"
+fix_audit_watch_rule "augenrules" "/run/utmp" "wa" "session"
