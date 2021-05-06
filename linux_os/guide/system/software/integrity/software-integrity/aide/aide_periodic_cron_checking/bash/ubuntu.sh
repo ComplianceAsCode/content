424afd9d4a5641216763cf309b47461a75c0c863
Alexander Scheel <alex.scheel@canonical.com>
2021-05-06 13:42:51 -0400

    Remove exit 0 from aide_periodic_cron_checking bash
    Signed-off-by: Alexander Scheel <alex.scheel@canonical.com>

--- linux_os/guide/system/software/integrity/software-integrity/aide/aide_periodic_cron_checking/bash/ubuntu.sh
+++ linux_os/guide/system/software/integrity/software-integrity/aide/aide_periodic_cron_checking/bash/ubuntu.sh
@@ -4,10 +4,10 @@
 
 # AiDE usually adds its own cron jobs to /etc/cron.daily. If script is there, this rule is
 # compliant.
-egrep -q '^(/usr/bin/)?aide\.wrapper\s+' /etc/cron.*/* && exit 0
+if ! egrep -q '^(/usr/bin/)?aide\.wrapper\s+' /etc/cron.*/*; then
+    ctab_line="0 5 * * * root /usr/bin/aide.wrapper --config /etc/aide/aide.conf --check"
 
-ctab_line="0 5 * * * root /usr/bin/aide.wrapper --config /etc/aide/aide.conf --check"
-
-if ! grep -q "$ctab_line" /etc/crontab; then
-    echo -e "# Line added by CIS hardening scripts\n""$ctab_line" >> /etc/crontab
+    if ! grep -q "$ctab_line" /etc/crontab; then
+        echo -e "# Line added by CIS hardening scripts\n""$ctab_line" >> /etc/crontab
+    fi
 fi
