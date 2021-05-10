775e410736d629592f75b8f33d6dba9404e9c4ec
Carlos Matos <cmatos@redhat.com>
2021-05-10 10:24:36 -0400

    ensuring accounts_tmout tests actually create profile.d file

--- linux_os/guide/system/accounts/accounts-session/accounts_tmout/tests/comment_profile_d.fail.sh
+++ linux_os/guide/system/accounts/accounts-session/accounts_tmout/tests/comment_profile_d.fail.sh
@@ -2,10 +2,14 @@
 
 # variables = var_accounts_tmout=600
 
+TEST_FILE=/etc/profile.d/tmout.sh
+
 sed -i "/.*TMOUT.*/d" /etc/profile
 
-if grep -q "^TMOUT" /etc/profile.d/tmout.sh; then
-	sed -i "s/^TMOUT.*/# TMOUT=600/" /etc/profile.d/tmout.sh
+test -f $TEST_FILE || touch $TEST_FILE
+
+if grep -q "^TMOUT" $TEST_FILE; then
+	sed -i "s/^TMOUT.*/# TMOUT=600/" $TEST_FILE
 else
-	echo "# TMOUT=600" >> /etc/profile.d/tmout.sh
+	echo "# TMOUT=600" >> $TEST_FILE
 fi
