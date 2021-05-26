1fb0409c4f80f31a807ed6a8e2bf0c63ed5cfa8a
Carlos Matos <cmatos@redhat.com>
2021-05-26 10:18:05 -0400

    made changes to the remediation to handle test cases better

--- linux_os/guide/system/software/integrity/crypto/configure_gnutls_tls_crypto_policy/bash/shared.sh
+++ linux_os/guide/system/software/integrity/crypto/configure_gnutls_tls_crypto_policy/bash/shared.sh
@@ -4,17 +4,10 @@
 # complexity = low
 # disruption = low
 
-RULE_LINE='+VERS-ALL:-VERS-DTLS0.9:-VERS-SSL3.0:-VERS-TLS1.0:-VERS-TLS1.1:-VERS-DTLS1.0'
 CONF_FILE=/etc/crypto-policies/back-ends/gnutls.config
 correct_value='+VERS-ALL:-VERS-DTLS0.9:-VERS-SSL3.0:-VERS-TLS1.0:-VERS-TLS1.1:-VERS-DTLS1.0'
 
-# Make sure file is present
-if [[ -f ${CONF_FILE} ]]; then
-    echo "Aborting remediation as '${CONF_FILE}' was not even found." >&2
-    exit 1
-fi
-
-grep -q ${RULE_LINE} ${CONF_FILE}
+grep -q ${correct_value} ${CONF_FILE}
 
 if [[ $? -ne 0 ]]; then
     # We need to get the existing value, using PCRE to maintain same regex
