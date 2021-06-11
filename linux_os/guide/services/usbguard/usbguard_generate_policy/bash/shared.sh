e2c77e189de61c322c05ead270fba34c38e55b28
Gabriel Becker <ggasparb@redhat.com>
2021-06-11 16:28:42 +0200

    Make sure remediation of usbguard_generate_policy check for the package.
    Only perform actions if package usbguard is actually installed in the
    system.

--- linux_os/guide/services/usbguard/usbguard_generate_policy/bash/shared.sh
+++ linux_os/guide/services/usbguard/usbguard_generate_policy/bash/shared.sh
@@ -4,14 +4,20 @@
 # complexity = low
 # disruption = low
 
-USBGUARD_CONF=/etc/usbguard/rules.conf
-if [ ! -f "$USBGUARD_CONF" ] || [ ! -s "$USBGUARD_CONF" ]; then
-    usbguard generate-policy > $USBGUARD_CONF
-    # make sure it has correct permissions
-    chmod 600 $USBGUARD_CONF
 
-    SYSTEMCTL_EXEC='/usr/bin/systemctl'
-    "$SYSTEMCTL_EXEC" unmask 'usbguard.service'
-    "$SYSTEMCTL_EXEC" restart 'usbguard.service'
-    "$SYSTEMCTL_EXEC" enable 'usbguard.service'
+if rpm --quiet -q usbguard
+then
+    USBGUARD_CONF=/etc/usbguard/rules.conf
+    if [ ! -f "$USBGUARD_CONF" ] || [ ! -s "$USBGUARD_CONF" ]; then
+        usbguard generate-policy > $USBGUARD_CONF
+        # make sure it has correct permissions
+        chmod 600 $USBGUARD_CONF
+
+        SYSTEMCTL_EXEC='/usr/bin/systemctl'
+        "$SYSTEMCTL_EXEC" unmask 'usbguard.service'
+        "$SYSTEMCTL_EXEC" restart 'usbguard.service'
+        "$SYSTEMCTL_EXEC" enable 'usbguard.service'
+    fi
+else
+    echo "USBGuard is not installed. No remediation was applied!"
 fi
