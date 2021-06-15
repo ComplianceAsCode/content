6ba9648b9c514a431be0bc677a0a8b842ef4ed35
Carlos Matos <cmatos@redhat.com>
2021-06-15 16:16:20 -0400

    Adding package pam meta to test scenarios

--- linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_password_pam_pwhistory_remember_password_auth/tests/rhel8_correct_value.pass.sh
+++ linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_password_pam_pwhistory_remember_password_auth/tests/rhel8_correct_value.pass.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # platform = Red Hat Enterprise Linux 8,Red Hat Virtualization 4,multi_platform_fedora,multi_platform_ol,multi_platform_wrlinux
 # variables = var_password_pam_remember_control_flag=required
+# packages = pam
 
 remember_cnt=5
 control_flag='required'
