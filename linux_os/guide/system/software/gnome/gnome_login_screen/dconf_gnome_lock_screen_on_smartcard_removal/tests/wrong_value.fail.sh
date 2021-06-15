25261933c16637d4fcfd92fa002f273dc1f94b43
Gabriel Becker <ggasparb@redhat.com>
2021-06-15 14:54:47 +0200

    Add single quotes to dconf value "lock-screen".

--- linux_os/guide/system/software/gnome/gnome_login_screen/dconf_gnome_lock_screen_on_smartcard_removal/tests/wrong_value.fail.sh
+++ linux_os/guide/system/software/gnome/gnome_login_screen/dconf_gnome_lock_screen_on_smartcard_removal/tests/wrong_value.fail.sh
@@ -5,7 +5,7 @@
 install_dconf_and_gdm_if_needed
 
 clean_dconf_settings
-add_dconf_setting "org/gnome/settings-daemon/peripherals/smartcard" "removal-action" "wrong_value" "local.d" "00-security-settings"
+add_dconf_setting "org/gnome/settings-daemon/peripherals/smartcard" "removal-action" "'wrong_value'" "local.d" "00-security-settings"
 add_dconf_lock "org/gnome/settings-daemon/peripherals/smartcard" "removal-action" "local.d" "00-security-settings-lock"
 
 dconf update
