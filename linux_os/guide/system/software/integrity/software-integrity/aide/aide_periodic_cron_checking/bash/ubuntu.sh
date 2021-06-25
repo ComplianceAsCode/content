7fcf24f780d03142eb3a8664834e68205cff78a3
Richard Maciel Costa <richard.maciel.costa@canonical.com>
2021-06-25 18:16:13 -0300

    Changed fix script to adequate for Ubuntu style
    - ubuntu.sh uses the script shipped with aide package to fulfill the
      requirement
    - removed platform ubuntu from shared.sh
    
    Signed-off-by: Richard Maciel Costa <richard.maciel.costa@canonical.com>

--- linux_os/guide/system/software/integrity/software-integrity/aide/aide_periodic_cron_checking/bash/ubuntu.sh
+++ linux_os/guide/system/software/integrity/software-integrity/aide/aide_periodic_cron_checking/bash/ubuntu.sh
@@ -3,11 +3,7 @@
 {{{ bash_package_install("aide") }}}
 
 # AiDE usually adds its own cron jobs to /etc/cron.daily. If script is there, this rule is
-# compliant.
+# compliant. Otherwise, we copy the script to the /etc/cron.weekly
 if ! egrep -q '^(/usr/bin/)?aide\.wrapper\s+' /etc/cron.*/*; then
-    ctab_line="0 5 * * * root /usr/bin/aide.wrapper --config /etc/aide/aide.conf --check"
-
-    if ! grep -q "$ctab_line" /etc/crontab; then
-        echo -e "# Line added by CIS hardening scripts\n""$ctab_line" >> /etc/crontab
-    fi
+    cp -f /usr/share/aide/config/cron.daily/aide /etc/cron.weekly/
 fi
