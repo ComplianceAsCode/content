6fc37f03f4f4c2d10f916b4248ff13c26ab8e171
Gabriel Becker <ggasparb@redhat.com>
2021-07-02 16:18:13 +0200

    Unify display_login_attempts artifacts among different products.
    The silent option absence is desired in all OSes.
    Added test scenarios and improved remediations.

--- linux_os/guide/system/accounts/accounts-pam/display_login_attempts/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-pam/display_login_attempts/bash/shared.sh
@@ -1,7 +1,12 @@
-# platform = Red Hat Virtualization 4,multi_platform_fedora,multi_platform_ol,multi_platform_rhel,multi_platform_wrlinux
+{{% if product in ["sle12", "sle15"] %}}
+{{% set pam_lastlog_path = "/etc/pam.d/login" %}}
+{{% else %}}
+{{% set pam_lastlog_path = "/etc/pam.d/postlogin" %}}
+{{% endif %}}
+# platform = multi_platform_sle,Red Hat Virtualization 4,multi_platform_fedora,multi_platform_ol,multi_platform_rhel,multi_platform_wrlinux
+. /usr/share/scap-security-guide/remediation_functions
 
-if grep -q "^session.*pam_lastlog.so" /etc/pam.d/postlogin; then
-	sed -i --follow-symlinks "/pam_lastlog.so/d" /etc/pam.d/postlogin
-fi
+ensure_pam_module_options '{{{ pam_lastlog_path }}}' 'session' 'required' 'pam_lastlog.so' 'showfailed' "" ""
 
-echo "session     required      pam_lastlog.so showfailed" >> /etc/pam.d/postlogin
+# remove 'silent' option
+sed -i --follow-symlinks -E -e 's/^([^#]+pam_lastlog\.so[^#]*)\ssilent/\1/' '{{{ pam_lastlog_path }}}'
--- linux_os/guide/system/accounts/accounts-pam/display_login_attempts/bash/sle12.sh
+++
@@ -1,7 +0,0 @@
-# platform = SUSE Linux Enterprise 12
-. /usr/share/scap-security-guide/remediation_functions
-
-ensure_pam_module_options '/etc/pam.d/login' 'session' 'required' 'pam_lastlog.so' 'showfailed' "" ""
-
-# remove 'silent' option
-sed -i --follow-symlinks -E -e 's/^([^#]+pam_lastlog\.so[^#]*)\ssilent/\1/' '/etc/pam.d/login'
