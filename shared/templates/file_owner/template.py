6b36a890660c807a1703ede48b4532e1bef24ac0
Eduardo Barretto <eduardo.barretto@canonical.com>
2021-07-12 13:14:44 +0200

    Adapt file_owner to deal with a list of filepaths
    Signed-off-by: Eduardo Barretto <eduardo.barretto@canonical.com>

--- shared/templates/file_owner/template.py
+++ shared/templates/file_owner/template.py
@@ -1,12 +1,21 @@
 from ssg.utils import parse_template_boolean_value
 
 def _file_owner_groupowner_permissions_regex(data):
-    data["is_directory"] = data["filepath"].endswith("/")
-    if "file_regex" in data and not data["is_directory"]:
-        raise ValueError(
-            "Used 'file_regex' key in rule '{0}' but filepath '{1}' does not "
-            "specify a directory. Append '/' to the filepath or remove the "
-            "'file_regex' key.".format(data["_rule_id"], data["filepath"]))
+    if isinstance(data["filepath"], list):
+        for f in data["filepath"]:
+            data["is_directory"] = f.endswith("/")
+            if "file_regex" in data and not data["is_directory"]:
+                raise ValueError(
+                    "Used 'file_regex' key in rule '{0}' but filepath '{1}' does not "
+                    "specify a directory. Append '/' to the filepath or remove the "
+                    "'file_regex' key.".format(data["_rule_id"], data["filepath"]))
+    else:
+        data["is_directory"] = data["filepath"].endswith("/")
+        if "file_regex" in data and not data["is_directory"]:
+            raise ValueError(
+                "Used 'file_regex' key in rule '{0}' but filepath '{1}' does not "
+                "specify a directory. Append '/' to the filepath or remove the "
+                "'file_regex' key.".format(data["_rule_id"], data["filepath"]))
 
 
 def preprocess(data, lang):
