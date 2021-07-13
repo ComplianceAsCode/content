71deac482753a13a9f98d6d7382b13e9031a2ce4
Carlos Matos <cmatos@redhat.com>
2021-07-13 13:40:25 -0400

    Updated test for nobody_group_owned_dir rule

--- linux_os/guide/system/permissions/files/permissions_within_important_dirs/dir_group_ownership_library_dirs/tests/nobody_group_owned_dir_on_lib.fail.sh
+++ linux_os/guide/system/permissions/files/permissions_within_important_dirs/dir_group_ownership_library_dirs/tests/nobody_group_owned_dir_on_lib.fail.sh
@@ -1,6 +1,6 @@
 # platform = multi_platform_sle,Red Hat Enterprise Linux 8,multi_platform_fedora
 
-DIRS="/lib /lib64"
+DIRS="/lib /lib64 /usr/lib /usr/lib64"
 for dirPath in $DIRS; do
-	mkdir -p "$dirPath/testme" && chown root:nogroup "$dirPath/testme"
+	mkdir -p "$dirPath/testme" && chgrp nobody "$dirPath/testme"
 done
