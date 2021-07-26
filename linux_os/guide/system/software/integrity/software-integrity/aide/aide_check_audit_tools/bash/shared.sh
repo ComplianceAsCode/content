065b6e540a2aa437ddf5239c97ed4e1fddf43b50
Matthew Burket <mburket@redhat.com>
2021-07-26 09:00:49 -0500

    Update rule aide_check_audit_tools for RHEL-08-030650

---
+++ linux_os/guide/system/software/integrity/software-integrity/aide/aide_check_audit_tools/bash/shared.sh
@@ -0,0 +1,25 @@
+# platform = multi_platform_rhel
+# reboot = false
+# strategy = restrict
+# complexity = low
+# disruption = low
+. /usr/share/scap-security-guide/remediation_functions
+
+{{{ bash_package_install("aide") }}}
+
+{{% set configString = "p+i+n+u+g+s+b+acl+xattrs+sha512" %}}
+{{% set configFile = "/etc/aide.conf" %}}
+{{% for file in (
+      "/usr/sbin/auditctl",
+      "/usr/sbin/auditd",
+      "/usr/sbin/ausearch",
+      "/usr/sbin/aureport",
+      "/usr/sbin/autrace",
+      "/usr/sbin/augenrules" ) %}}
+
+if grep -i '^.*{{{file}}}.*$' {{{ configFile }}}; then
+sed -i "s#.*{{{file}}}.*#{{{file}}} {{{ configString }}}#" {{{ configFile }}}
+else
+echo "{{{ file }}} {{{ configString }}}" >> {{{ configFile }}}
+fi
+{{% endfor %}}
