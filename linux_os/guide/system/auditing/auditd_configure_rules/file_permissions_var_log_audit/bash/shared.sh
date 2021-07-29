ae8af6cf8dd169bf5fac150badaf97da640f5ff2
Eduardo Barretto <eduardo.barretto@canonical.com>
2021-07-29 12:46:02 +0200

    Make file_permissions_var_log_audit bash script address log_file
    parameter in auditd.conf

--- linux_os/guide/system/auditing/auditd_configure_rules/file_permissions_var_log_audit/bash/shared.sh
+++ linux_os/guide/system/auditing/auditd_configure_rules/file_permissions_var_log_audit/bash/shared.sh
@@ -1,15 +1,21 @@
 # platform = multi_platform_rhel,multi_platform_ol,multi_platform_fedora,multi_platform_rhv,multi_platform_sle
 
+if LC_ALL=C grep -iw log_file /etc/audit/auditd.conf; then
+    FILE=$(awk -F "=" '/^log_file/ {print $2}' /etc/audit/auditd.conf | tr -d ' ')
+else
+    FILE="/var/log/audit/audit.log"
+fi
+
 if LC_ALL=C grep -m 1 -q ^log_group /etc/audit/auditd.conf; then
   GROUP=$(awk -F "=" '/log_group/ {print $2}' /etc/audit/auditd.conf | tr -d ' ')
   if ! [ "${GROUP}" == 'root' ] ; then
-    chmod 0640 /var/log/audit/audit.log
-    chmod 0440 /var/log/audit/audit.log.*
+    chmod 0640 $FILE
+    chmod 0440 $FILE.*
   else
-    chmod 0600 /var/log/audit/audit.log
-    chmod 0400 /var/log/audit/audit.log.*
+    chmod 0600 $FILE
+    chmod 0400 $FILE.*
   fi
 else
-  chmod 0600 /var/log/audit/audit.log
-  chmod 0400 /var/log/audit/audit.log.*
+  chmod 0600 $FILE
+  chmod 0400 $FILE.*
 fi
