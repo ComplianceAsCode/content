1216eda0621bedfd60f189bbfd60e79f3b6f5411
Gabriel Becker <ggasparb@redhat.com>
2021-08-03 12:30:11 +0200

    Add two new rules to cover STIG req based on existing rule.
    The rule used as basis is audit_rules_sysadmin_actions. This rules is
    used by many profiles and it didn't make sense to change its behavior,
    so two new rules were created to be used only by RHEL8 STIG.

---
+++ linux_os/guide/system/auditing/auditd_configure_rules/audit_rules_sudoers/tests/empty.fail.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+rm -rf /etc/audit/rules.d/
+mkdir -p /etc/audit/rules.d/
+touch /etc/audit/audit.rules
---
+++ linux_os/guide/system/auditing/auditd_configure_rules/audit_rules_sudoers_d/tests/correct.pass.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+mkdir -p /etc/audit/rules.d/
+echo "-w /etc/sudoers.d/ -p wa -k actions" >> /etc/audit/rules.d/actions.rules
