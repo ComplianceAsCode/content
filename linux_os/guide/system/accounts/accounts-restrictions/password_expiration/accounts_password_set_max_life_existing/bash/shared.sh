3075d947693b52207e9100273b26ea02077c4e7d
rchikov <rumen.chikov@suse.com>
2021-08-09 17:27:23 +0200

    Rule SLES-12-010290 - added ansible,bash and tests

---
+++ linux_os/guide/system/accounts/accounts-restrictions/password_expiration/accounts_password_set_max_life_existing/bash/shared.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# platform = multi_platform_sle
+
+SECURE_MAX_PASS_AGE=60
+
+usrs_max_pass_age=( $(awk -F: '$5 > SECURE_MAX_PASS_AGE || $5 == "" {print $1}' /etc/shadow) )
+for i in ${usrs_max_pass_age[@]};
+do
+  passwd -x $SECURE_MAX_PASS_AGE $i
+done
