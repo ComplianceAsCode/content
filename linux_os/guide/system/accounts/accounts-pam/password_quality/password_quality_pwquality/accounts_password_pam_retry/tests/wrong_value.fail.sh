67a679ce6eff69b2694d224a8d4aecb938e00545
Eduardo Barretto <eduardo.barretto@canonical.com>
2021-08-13 10:02:39 +0200

    Add ubuntu platform to accounts_password_pam_retry tests

--- linux_os/guide/system/accounts/accounts-pam/password_quality/password_quality_pwquality/accounts_password_pam_retry/tests/wrong_value.fail.sh
+++ linux_os/guide/system/accounts/accounts-pam/password_quality/password_quality_pwquality/accounts_password_pam_retry/tests/wrong_value.fail.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-# platform = Red Hat Enterprise Linux 7,Red Hat Virtualization 4,multi_platform_fedora,multi_platform_ol,multi_platform_wrlinux
+# platform = Red Hat Enterprise Linux 7,Red Hat Virtualization 4,multi_platform_fedora,multi_platform_ol,multi_platform_wrlinux,multi_platform_ubuntu
 
 retry_cnt=7
+{{% if 'ubuntu' not in product %}}
 config_file="/etc/pam.d/system-auth"
+{{% else %}}
+config_file="/etc/pam.d/common-password"
+{{% endif %}}
 
 if grep -q "pam_pwquality\.so.*retry=" "$config_file" ; then
 	sed -i --follow-symlinks "/pam_pwquality\.so/ s/\(retry *= *\).*/\1$retry_cnt/" $config_file
