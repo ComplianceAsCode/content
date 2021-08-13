7cd51856c071ef470958df6ea31d884b730b8434
Lenox <jdlenox@labnet.rockwellcollins.lab>
2021-08-13 08:16:33 -0500

    Clean up tests for min/max password life to trash all noninteractive users before adding entries to make test.

--- linux_os/guide/system/accounts/accounts-restrictions/password_expiration/accounts_password_set_max_life_existing/tests/correct_stig_noninteractive.pass.sh
+++ linux_os/guide/system/accounts/accounts-restrictions/password_expiration/accounts_password_set_max_life_existing/tests/correct_stig_noninteractive.pass.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Remove noninteractive users
-sed -i '/^\S\+:x:[0-9]\{4,9\}/d' /etc/passwd
+sed -i '/:\(\*\|!!\):/!d' /etc/shadow
 # Noninteractive users are a pass
 echo "max-test-user:x:540:540:Test User for Password Expiration:/:/sbin/nologin" >> /etc/passwd
 echo "max-test-user:!!:18648:1::::" >> /etc/shadow
--- linux_os/guide/system/accounts/accounts-restrictions/password_expiration/accounts_password_set_max_life_existing/tests/incorrect_stig.fail.sh
+++ linux_os/guide/system/accounts/accounts-restrictions/password_expiration/accounts_password_set_max_life_existing/tests/incorrect_stig.fail.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo "max-test-user:x:10020:10020:Test User for Password Expiration:/:/sbin/nologin" >> /etc/passwd
-echo "max-test-user:!!:18648:0:90:::" >> /etc/shadow
+sed -i '/:\(\*\|!!\):/!d' /etc/shadow
+echo 'max-test-user:$1$q.YkdxU1$ADmXcU4xwPrM.Pc.dclK81:18648:1:90::::' >> /etc/shadow
