27d64329d2d9d3cdac03f0a46866f99c299b430d
Watson Sato <wsato@redhat.com>
2021-08-17 16:37:12 +0200

    Move template audit_rules_dac_modification to Ansible macro
    Use Ansible macro ansible_audit_augenrules_add_syscall_rule and
    ansible_audit_auditctl_add_syscall_rule that group the syscalls
    according to defined grouping.

--- shared/templates/audit_rules_dac_modification/template.py
+++ shared/templates/audit_rules_dac_modification/template.py
@@ -7,5 +7,12 @@ def preprocess(data, lang):
         if "syscall_grouping" in data:
             # Make it easier to tranform the syscall_grouping into a Bash array
             data["syscall_grouping"] = " ".join(data["syscall_grouping"])
+    elif lang == "ansible":
+        if "attr" in data:
+            # Tranform the syscall into a Ansible list
+            data["attr"] = [ data["attr"] ]
+        if "syscall_grouping" not in data:
+            # Ensure that syscall_grouping is a list
+            data["syscall_grouping"] = []
 
     return data
