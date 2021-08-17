a355d5b5578477a4464023dccccdb474ff571768
Watson Sato <wsato@redhat.com>
2021-08-17 14:35:17 +0200

    Move template audit_rules_path_syscall to Ansible macro

--- shared/templates/audit_rules_path_syscall/template.py
+++ shared/templates/audit_rules_path_syscall/template.py
@@ -11,4 +11,11 @@ def preprocess(data, lang):
         if "syscall_grouping" in data:
             # Make it easier to tranform the syscall_grouping into a Bash array
             data["syscall_grouping"] = " ".join(data["syscall_grouping"])
+    elif lang == "ansible":
+        if "syscall" in data:
+            # Tranform the syscall into a Ansible list
+            data["syscall"] = [ data["syscall"] ]
+        if "syscall_grouping" not in data:
+            # Ensure that syscall_grouping is a list
+            data["syscall_grouping"] = []
     return data
