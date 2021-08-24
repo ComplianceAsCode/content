aad7d195640b1a221b24e1ba2e219605e2ed4669
Jiri Odehnal <jodehnal@redhat.com>
2021-08-24 10:27:41 +0200

    Fixed the rest of macro invocations

--- linux_os/guide/system/permissions/partitions/mount_option_nodev_nonroot_local_partitions/bash/shared.sh
+++ linux_os/guide/system/permissions/partitions/mount_option_nodev_nonroot_local_partitions/bash/shared.sh
@@ -1,7 +1,5 @@
 # platform = multi_platform_all
 
-{{{ bash_include_mount_options_functions() }}}
-
 MOUNT_OPTION="nodev"
 # Create array of local non-root partitions
 readarray -t partitions_records < <(findmnt --mtab --raw --evaluate | grep "^/\w" | grep "\s/dev/\w")
@@ -12,6 +10,6 @@ for partition_record in "${partitions_records[@]}"; do
     device="$(echo ${partition_record} | cut -d " " -f2)"
     device_type="$(echo ${partition_record} | cut -d " " -f3)"
     # device and device_type will be used only in case when the device doesn't have fstab record
-    ensure_mount_option_in_fstab "$mount_point" "$MOUNT_OPTION" "$device" "$device_type"
-    ensure_partition_is_mounted "$mount_point"
+    {{{ bash_ensure_mount_option_in_fstab("$mount_point", "$MOUNT_OPTION", "$device", "$device_type") }}}
+    {{{ bash_ensure_partition_is_mounted("$mount_point") }}}
 done
