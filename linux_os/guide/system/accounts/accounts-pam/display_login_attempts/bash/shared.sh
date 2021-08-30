11135fec23a983f314fddd6cbc5097e634eda0a1
Jiri Odehnal <jodehnal@redhat.com>
2021-08-30 15:11:13 +0200

    Changed function calls to macro invocations; deleted the function file

--- linux_os/guide/system/accounts/accounts-pam/display_login_attempts/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-pam/display_login_attempts/bash/shared.sh
@@ -4,9 +4,8 @@
 {{% set pam_lastlog_path = "/etc/pam.d/postlogin" %}}
 {{% endif %}}
 # platform = multi_platform_sle,Red Hat Virtualization 4,multi_platform_fedora,multi_platform_ol,multi_platform_rhel,multi_platform_wrlinux
-. /usr/share/scap-security-guide/remediation_functions
 
-ensure_pam_module_options '{{{ pam_lastlog_path }}}' 'session' 'required' 'pam_lastlog.so' 'showfailed' "" ""
+{{{ bash_ensure_pam_module_options(pam_lastlog_path, 'session', 'required', 'pam_lastlog.so', 'showfailed', "", "") }}}
 
 # remove 'silent' option
 sed -i --follow-symlinks -E -e 's/^([^#]+pam_lastlog\.so[^#]*)\ssilent/\1/' '{{{ pam_lastlog_path }}}'
--- linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_passwords_pam_tally2/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_passwords_pam_tally2/bash/shared.sh
@@ -1,6 +1,5 @@
 # platform = multi_platform_sle
-. /usr/share/scap-security-guide/remediation_functions
 
-ensure_pam_module_options '/etc/pam.d/common-auth' 'auth' 'required' 'pam_tally2.so' 'deny' '[123]' '3'
-ensure_pam_module_options '/etc/pam.d/common-auth' 'auth' 'required' 'pam_tally2.so' 'onerr' '(fail)' 'fail'
-ensure_pam_module_options '/etc/pam.d/common-account' 'account' 'required' 'pam_tally2.so' '' '' ''
+{{{ bash_ensure_pam_module_options('/etc/pam.d/common-auth', 'auth', 'required', 'pam_tally2.so', 'deny', '[123]', '3') }}}
+{{{ bash_ensure_pam_module_options('/etc/pam.d/common-auth', 'auth', 'required', 'pam_tally2.so', 'onerr', '(fail)', 'fail') }}}
+{{{ bash_ensure_pam_module_options('/etc/pam.d/common-account', 'account', 'required', 'pam_tally2.so', '', '', '') }}}
