30e1ed76700f11f061ccc9f038279cb6d7977f41
Jiri Odehnal <jodehnal@redhat.com>
2021-08-31 14:02:31 +0200

    Converted create_audit_remediation_unsuccessful_file_modification_detailed and its calls to a jinja macro; removed the old function

--- linux_os/guide/system/auditing/auditd_configure_rules/audit_file_modification/audit_rules_unsuccessful_file_modification_openat_rule_order/bash/shared.sh
+++ linux_os/guide/system/auditing/auditd_configure_rules/audit_file_modification/audit_rules_unsuccessful_file_modification_openat_rule_order/bash/shared.sh
@@ -1,6 +1,3 @@
 # platform = multi_platform_rhel,multi_platform_ol
-#
-# Include source function library.
-. /usr/share/scap-security-guide/remediation_functions
 
-create_audit_remediation_unsuccessful_file_modification_detailed /etc/audit/rules.d/30-ospp-v42-remediation.rules
+{{{ bash_create_audit_remediation_unsuccessful_file_modification_detailed("/etc/audit/rules.d/30-ospp-v42-remediation.rules") }}}
