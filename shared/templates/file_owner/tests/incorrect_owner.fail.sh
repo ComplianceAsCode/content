7aa0b7988c3293e904a758501d82f7ffa97d3911
Eduardo Barretto <eduardo.barretto@canonical.com>
2021-09-02 09:26:32 +0200

    Improve file_owner tests

--- shared/templates/file_owner/tests/incorrect_owner.fail.sh
+++ shared/templates/file_owner/tests/incorrect_owner.fail.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 #
 
-touch {{{ FILEPATH }}}
-chown 1 {{{ FILEPATH }}}
+useradd testuser_123
+
+{{% for path in FILEPATH %}}
+{{% if IS_DIRECTORY and FILE_REGEX %}}
+echo "Create specific tests for this rule because of regex"
+{{% else %}}
+if [ ! -f {{{ path }}} ]; then
+    touch {{{ path }}}
+fi
+chown testuser_123 {{{ path }}}
+{{% endif %}}
+{{% endfor %}}
