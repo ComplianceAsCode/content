2632911efb8d729c3f90f005f0aa8ae5b0041cb5
rumch-se <77793453+rumch-se@users.noreply.github.com>
2021-09-03 10:45:59 +0200

    Update correct_max_pas_age.pass.sh
    Implementation of proposed change

--- linux_os/guide/system/accounts/accounts-restrictions/password_expiration/accounts_password_set_max_life_existing/tests/correct_max_pas_age.pass.sh
+++ linux_os/guide/system/accounts/accounts-restrictions/password_expiration/accounts_password_set_max_life_existing/tests/correct_max_pas_age.pass.sh
@@ -6,5 +6,5 @@ SECURE_MAX_PASS_AGE=60
 usrs_max_pass_age=( $(awk -F: '$5 > SECURE_MAX_PASS_AGE || $5 == "" {print $1}' /etc/shadow) )
 for i in ${usrs_max_pass_age[@]};
 do
-  passwd -x $SECURE_MAX_PASS_AGE $i
+  chage -M $SECURE_MAX_PASS_AGE $i
 done
