9ebe371931b6f82e0623817612248836ddc8607b
Lenox, Joseph D (Collins) <Joseph.Lenox@collins.com>
2021-09-03 13:05:44 -0500

    Fix typos in test scripts.

--- linux_os/guide/system/accounts/accounts-restrictions/password_expiration/accounts_password_set_max_life_existing/tests/incorrect_stig.fail.sh
+++ linux_os/guide/system/accounts/accounts-restrictions/password_expiration/accounts_password_set_max_life_existing/tests/incorrect_stig.fail.sh
@@ -5,4 +5,4 @@ for acct in $(grep -ve ':\(!!\|\*\):' /etc/shadow | awk -F: '{print $1}'); do
 done
 # Add a failing test.
 echo 'max-test-user:$1$q.YkdxU1$ADmXcU4xwPrM.Pc.dclK81:18648:1:90::::' >> /etc/shadow
-echo "max-test-user:x:50000:1000::/:/usr/bin/bash' >> /etc/passwd
+echo "max-test-user:x:50000:1000::/:/usr/bin/bash" >> /etc/passwd
--- linux_os/guide/system/accounts/accounts-restrictions/password_expiration/accounts_password_set_min_life_existing/tests/correct_stig.pass.sh
+++ linux_os/guide/system/accounts/accounts-restrictions/password_expiration/accounts_password_set_min_life_existing/tests/correct_stig.pass.sh
@@ -4,4 +4,4 @@ for acct in $(grep -ve ':\(!!\|\*\):' /etc/shadow | awk -F: '{print $1}'); do
     chage -m 1 -d $(date +%Y-%m-%d) $acct
 done
 echo 'max-test-user:$1$q.YkdxU1$ADmXcU4xwPrM.Pc.dclK81:18648:1:60::::' >> /etc/shadow
-echo "max-test-user:x:50000:1000::/:/usr/bin/bash' >> /etc/passwd
+echo "max-test-user:x:50000:1000::/:/usr/bin/bash" >> /etc/passwd
