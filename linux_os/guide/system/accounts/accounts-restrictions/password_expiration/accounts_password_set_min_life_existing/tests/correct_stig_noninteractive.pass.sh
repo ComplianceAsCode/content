65726a2bf764f3dc4d312c3e7e40f2e182851860
Lenox, Joseph D (Collins) <Joseph.Lenox@collins.com>
2021-09-03 14:05:11 -0500

    Set tests for min_life_existing to "pre-remediate" the existing accounts with large numbers.

--- linux_os/guide/system/accounts/accounts-restrictions/password_expiration/accounts_password_set_min_life_existing/tests/correct_stig_noninteractive.pass.sh
+++ linux_os/guide/system/accounts/accounts-restrictions/password_expiration/accounts_password_set_min_life_existing/tests/correct_stig_noninteractive.pass.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+
 # make existing entries pass
+# assumption: use a nominally large value for var_accounts_minimum_age_login_defs
+#             so as to not interfere.
 for acct in $(grep -ve ':\(!!\|\*\):' /etc/shadow | awk -F: '{print $1}'); do
-    chage -m 1 -d $(date +%Y-%m-%d) $acct
+    chage -m 300 -d $(date +%Y-%m-%d) $acct
 done
 # Noninteractive users are a pass
 echo "max-test-user:x:540:540:Test User for Password Expiration:/:/sbin/nologin" >> /etc/passwd
--- linux_os/guide/system/accounts/accounts-restrictions/password_expiration/accounts_password_set_min_life_existing/tests/incorrect_stig.fail.sh
+++ linux_os/guide/system/accounts/accounts-restrictions/password_expiration/accounts_password_set_min_life_existing/tests/incorrect_stig.fail.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # make existing entries pass
+# assumption: use a nominally large value for var_accounts_minimum_age_login_defs
+#             so as to not interfere.
 for acct in $(grep -ve ':\(!!\|\*\):' /etc/shadow | awk -F: '{print $1}'); do
-    chage -m 1 -d $(date +%Y-%m-%d) $acct
+    chage -m 300 -d $(date +%Y-%m-%d) $acct
 done
 # add a failing entry
 echo 'max-test-user:$1$q.YkdxU1$ADmXcU4xwPrM.Pc.dclK81:18648:0:60::::' >> /etc/shadow
