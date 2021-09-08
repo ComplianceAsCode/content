32461b63b3be9246d2d3de07dc6a9b6f0a0827d9
Eduardo Barretto <eduardo.barretto@canonical.com>
2021-09-08 18:18:46 +0200

    Improve template file_groupowner tests

--- shared/templates/file_groupowner/tests/correct_groupowner.pass.sh
+++ shared/templates/file_groupowner/tests/correct_groupowner.pass.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 #
 
-touch {{{ FILEPATH }}}
-chgrp {{{ FILEGID }}} {{{ FILEPATH }}}
-
+{{% for path in FILEPATH %}}
+{{% if IS_DIRECTORY and FILE_REGEX %}}
+echo "Create specific tests for this rule because of regex"
+{{% else %}}
+if [ ! -f {{{ path }}} ]; then
+    touch {{{ path }}}
+fi
+chgrp {{{ FILEGID }}} {{{ path }}}
+{{% endif %}}
+{{% endfor %}}
--- shared/templates/file_groupowner/tests/incorrect_groupowner.fail.sh
+++ shared/templates/file_groupowner/tests/incorrect_groupowner.fail.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 #
 
-touch {{{ FILEPATH }}}
-chgrp 1 {{{ FILEPATH }}}
+groupadd group_test
+
+{{% for path in FILEPATH %}}
+{{% if IS_DIRECTORY and FILE_REGEX %}}
+echo "Create specific tests for this rule because of regex"
+{{% else %}}
+if [ ! -f {{{ path }}} ]; then
+    touch {{{ path }}}
+fi
+chgrp group_test {{{ path }}}
+{{% endif %}}
+{{% endfor %}}
