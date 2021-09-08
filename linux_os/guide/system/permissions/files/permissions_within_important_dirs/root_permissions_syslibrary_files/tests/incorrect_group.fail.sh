8d5e5d0f04806c12361cd61bc9a37510bbf1e1a9
Eduardo Barretto <eduardo.barretto@canonical.com>
2021-09-08 19:16:32 +0200

    Make root_permissions_syslibrary_files tests work in ubuntu

--- linux_os/guide/system/permissions/files/permissions_within_important_dirs/root_permissions_syslibrary_files/tests/incorrect_group.fail.sh
+++ linux_os/guide/system/permissions/files/permissions_within_important_dirs/root_permissions_syslibrary_files/tests/incorrect_group.fail.sh
@@ -1,10 +1,11 @@
-# platform = multi_platform_sle,Red Hat Enterprise Linux 8,multi_platform_fedora
+# platform = multi_platform_sle,Red Hat Enterprise Linux 8,multi_platform_fedora,multi_platform_ubuntu
 
+groupadd group_test
 for TESTFILE in /lib/test_me /lib64/test_me /usr/lib/test_me /usr/lib64/test_me
 do
    if [[ ! -f $TESTFILE ]]
    then
      touch $TESTFILE
    fi
-   chown nobody.nobody $TESTFILE
+   chgrp group_test $TESTFILE
 done
