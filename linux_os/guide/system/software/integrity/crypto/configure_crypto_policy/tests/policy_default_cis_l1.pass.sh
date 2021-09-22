37da2cde3ae45a8576cefb775734b16485e30de9
Matus Marhefka <mmarhefk@redhat.com>
2021-09-22 14:10:50 +0200

    Add test scenarios for configure_crypto_policy and CIS profiles

---
+++ linux_os/guide/system/software/integrity/crypto/configure_crypto_policy/tests/policy_default_cis_l1.pass.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# platform = Red Hat Enterprise Linux 8,Red Hat Enterprise Linux 9
+# profiles = xccdf_org.ssgproject.content_profile_cis_server_l1,xccdf_org.ssgproject.content_profile_cis_workstation_l1
+
+update-crypto-policies --set "DEFAULT"
---
+++ linux_os/guide/system/software/integrity/crypto/configure_crypto_policy/tests/policy_future_cis_l2.pass.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# platform = Red Hat Enterprise Linux 8,Red Hat Enterprise Linux 9
+# profiles = xccdf_org.ssgproject.content_profile_cis,xccdf_org.ssgproject.content_profile_cis_workstation_l2
+
+update-crypto-policies --set "FUTURE"
