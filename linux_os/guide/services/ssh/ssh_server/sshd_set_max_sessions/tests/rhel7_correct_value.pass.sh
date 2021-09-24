9f0d5109a28c37e416e0719e1791aa124fc9498f
ayfantis <ayfantis@localhost.localdomain>
2021-09-24 17:53:41 +0200

    Making the fail test multi_platform, fixing the value check in the correct test

--- linux_os/guide/services/ssh/ssh_server/sshd_set_max_sessions/tests/rhel7_correct_value.pass.sh
+++ linux_os/guide/services/ssh/ssh_server/sshd_set_max_sessions/tests/rhel7_correct_value.pass.sh
@@ -1,5 +1,3 @@
 # platform = Red Hat Enterprise Linux 7
 
-{{{ bash_instantiate_variables("var_sshd_max_sessions") }}}
-
-{{{ bash_sshd_config_set(parameter="MaxSessions", value="$var_sshd_max_sessions") }}}
+{{{ bash_sshd_config_set(parameter="MaxSessions", value="$10") }}}
