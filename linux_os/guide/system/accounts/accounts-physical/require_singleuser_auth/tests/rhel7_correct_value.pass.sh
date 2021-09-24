fac2e5ae10646e7510251298d2630ab995cb658d
ayfantis <ayfantis@localhost.localdomain>
2021-09-24 10:06:24 +0200

    removing unecessary part of code

--- linux_os/guide/system/accounts/accounts-physical/require_singleuser_auth/tests/rhel7_correct_value.pass.sh
+++ linux_os/guide/system/accounts/accounts-physical/require_singleuser_auth/tests/rhel7_correct_value.pass.sh
@@ -7,9 +7,6 @@ service_file="/usr/lib/systemd/system/rescue.service"
 sulogin='/bin/sh -c "/usr/sbin/sulogin; /usr/bin/systemctl --fail --no-block default"'
 if grep "^ExecStart=.*" "$service_file" ; then
     sed -i "s%^ExecStart=.*%ExecStart=-$sulogin%" "$service_file"
-else
-    echo "ExecStart=-$sulogin" >> "$service_file"
-fi
 {{%- else -%}}
 grep -q ^SINGLE /etc/sysconfig/init && \
   sed -i "s/SINGLE.*/SINGLE=\/sbin\/sulogin/g" /etc/sysconfig/init
