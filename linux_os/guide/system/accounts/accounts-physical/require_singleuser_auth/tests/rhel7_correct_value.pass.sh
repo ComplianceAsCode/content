f2d53ca42720cf0303401992819db8db7423f48a
ayfantis <ayfantis@localhost.localdomain>
2021-09-24 10:24:43 +0200

    Added if removed by mistake, removed correct one

--- linux_os/guide/system/accounts/accounts-physical/require_singleuser_auth/tests/rhel7_correct_value.pass.sh
+++ linux_os/guide/system/accounts/accounts-physical/require_singleuser_auth/tests/rhel7_correct_value.pass.sh
@@ -2,15 +2,10 @@
 #
 # platform = Red Hat Enterprise Linux 7
 
-{{% if init_system == "systemd" -%}}
 service_file="/usr/lib/systemd/system/rescue.service"
 sulogin='/bin/sh -c "/usr/sbin/sulogin; /usr/bin/systemctl --fail --no-block default"'
 if grep "^ExecStart=.*" "$service_file" ; then
     sed -i "s%^ExecStart=.*%ExecStart=-$sulogin%" "$service_file"
-{{%- else -%}}
-grep -q ^SINGLE /etc/sysconfig/init && \
-  sed -i "s/SINGLE.*/SINGLE=\/sbin\/sulogin/g" /etc/sysconfig/init
-if ! [ $? -eq 0 ]; then
-    echo "SINGLE=/sbin/sulogin" >> /etc/sysconfig/init
+else
+    echo "ExecStart=-$sulogin" >> "$service_file"
 fi
-{{%- endif -%}}
