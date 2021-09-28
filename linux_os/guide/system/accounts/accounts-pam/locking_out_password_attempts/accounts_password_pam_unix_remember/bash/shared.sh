1943e0ee99499bb6ac4bb1edf18e32d0817ca7e7
Yavor Georgiev <strandjata@gmail.com>
2021-09-28 08:07:19 +0300

    Rework bash remediation for accounts_password_pam_unix_remember rule to support the case when pam config file does not exist

--- linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_password_pam_unix_remember/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_password_pam_unix_remember/bash/shared.sh
@@ -1,20 +1,17 @@
 # platform = multi_platform_wrlinux,multi_platform_rhel,multi_platform_fedora,multi_platform_ol,multi_platform_rhv,multi_platform_sle
+. /usr/share/scap-security-guide/remediation_functions
 
 {{{ bash_instantiate_variables("var_password_pam_unix_remember") }}}
 
+{{% if product in [ "sle15" ] %}}
+AUTH_FILES[0]="/etc/pam.d/common-password"
+{{% else %}}
 AUTH_FILES[0]="/etc/pam.d/system-auth"
+{{% endif %}}
 AUTH_FILES[1]="/etc/pam.d/password-auth"
 
 for pamFile in "${AUTH_FILES[@]}"
 do
-	if grep -q "pam_unix.so.*remember=" $pamFile; then
-		sed -i --follow-symlinks "s/\(^password.*sufficient.*pam_unix.so.*\)\(\(remember *= *\)[^ $]*\)/\1remember=$var_password_pam_unix_remember/" $pamFile
-	else
-		sed -i --follow-symlinks "/^password[[:space:]]\+sufficient[[:space:]]\+pam_unix.so/ s/$/ remember=$var_password_pam_unix_remember/" $pamFile
-	fi
-	if grep -q "pam_pwhistory.so.*remember=" $pamFile; then
-		sed -i --follow-symlinks "s/\(^password.*\)\(\(remember *= *\)[^ $]*\)/\1remember=$var_password_pam_unix_remember/" $pamFile
-	else
-		sed -i --follow-symlinks "/^password[[:space:]]\+\(requisite\|required\)[[:space:]]\+pam_pwhistory.so/ s/$/ remember=$var_password_pam_unix_remember/" $pamFile
-	fi
+	{{{ bash_ensure_pam_module_options("$pamFile", 'password', 'sufficient', 'pam_unix.so', 'remember', "$var_password_pam_unix_remember", "$var_password_pam_unix_remember") }}}
+	{{{ bash_ensure_pam_module_options("$pamFile", 'password', 'required', 'pam_pwhistory.so', 'remember', "$var_password_pam_unix_remember", "$var_password_pam_unix_remember") }}}
 done
