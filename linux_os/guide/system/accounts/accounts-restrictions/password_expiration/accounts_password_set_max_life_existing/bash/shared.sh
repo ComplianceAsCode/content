9acf79b0d4a66bfd7b9a518446dbe3cf4833c1fe
Watson Sato <wsato@redhat.com>
2021-09-28 21:02:59 +0200

    Fix variable expansion in awk expression
    The remediation for max_life_existing was returning more users than it
    should be (maybe not an actual problem).
    But the remediation for min_life_existing was returning less users than
    it should (no users actually).

--- linux_os/guide/system/accounts/accounts-restrictions/password_expiration/accounts_password_set_max_life_existing/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-restrictions/password_expiration/accounts_password_set_max_life_existing/bash/shared.sh
@@ -8,7 +8,7 @@
 
 {{{ bash_instantiate_variables("var_accounts_maximum_age_login_defs") }}}
 
-usrs_max_pass_age=( $(awk -F: '$5 > var_accounts_maximum_age_login_defs || $5 == "" {print $1}' /etc/shadow) )
+usrs_max_pass_age=( $(awk -v var="$var_accounts_maximum_age_login_defs" -F: '$5 > var || $5 == "" {print $1}' /etc/shadow) )
 for i in ${usrs_max_pass_age[@]};
 do
   passwd -x $var_accounts_maximum_age_login_defs $i
--- linux_os/guide/system/accounts/accounts-restrictions/password_expiration/accounts_password_set_min_life_existing/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-restrictions/password_expiration/accounts_password_set_min_life_existing/bash/shared.sh
@@ -8,7 +8,7 @@
 
 {{{ bash_instantiate_variables("var_accounts_minimum_age_login_defs") }}}
 
-usrs_min_pass_age=( $(awk -F: '$4 < var_accounts_minimum_age_login_defs || $4 == "" {print $1}' /etc/shadow) )
+usrs_min_pass_age=( $(awk -v var="$var_accounts_minimum_age_login_defs" -F: '$4 < var || $4 == "" {print $1}' /etc/shadow) )
 for i in ${usrs_min_pass_age[@]};
 do
   passwd -n $var_accounts_minimum_age_login_defs $i
