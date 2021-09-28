9e447ddcbcb417dc305b480ec7f1fca4b71b332a
ayfantis <ayfantis@localhost.localdomain>
2021-09-29 01:19:23 +0200

    added profile metadata to pass case

--- linux_os/guide/services/ssh/ssh_server/sshd_use_approved_macs/tests/rhel7_correct_value.pass.sh
+++ linux_os/guide/services/ssh/ssh_server/sshd_use_approved_macs/tests/rhel7_correct_value.pass.sh
@@ -1,3 +1,5 @@
 # platform = Red Hat Enterprise Linux 7
+#
+# profiles = xccdf_org.ssgproject.content_profile_cis
 
 {{{ bash_replace_or_append('/etc/ssh/sshd_config', '^MACs', "umac-64-etm@openssh.com,umac-128-etm@openssh.com,hmac-sha2-256-etm@openssh.com,hmac-sha2-512-etm@openssh.com,hmac-sha1-etm@openssh.com,umac-64@openssh.com,umac-128@openssh.com,hmac-sha2-256,hmac-sha2-512,hmac-sha1,hmac-sha1-etm@openssh.com", '@CCENUM@', '%s %s') }}}
