d746d4db0540623fd29c71fea0b070d65087cd4c
Eduardo Barretto <eduardo.barretto@canonical.com>
2021-09-28 10:16:23 +0200

    Adapt file_group_ownership_var_log_audit tests to check log_file

--- linux_os/guide/system/auditing/auditd_configure_rules/file_group_ownership_var_log_audit/tests/wrong_value.fail.sh
+++ linux_os/guide/system/auditing/auditd_configure_rules/file_group_ownership_var_log_audit/tests/wrong_value.fail.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+if grep -iwq "log_file" /etc/audit/auditd.conf; then
+    FILE=$(awk -F "=" '/^log_file/ {print $2}' /etc/audit/auditd.conf | tr -d ' ')
+else
+    FILE="/var/log/audit/audit.log"
+fi
+
 sed -i "/\s*log_group.*/d" /etc/audit/auditd.conf
 echo "log_group = root" >> /etc/audit/auditd.conf
-touch /var/log/audit/audit.log.1
+touch $FILE.1
 groupadd group_test
-chgrp group_test /var/log/audit/audit.log.1
+chgrp group_test $FILE.1
