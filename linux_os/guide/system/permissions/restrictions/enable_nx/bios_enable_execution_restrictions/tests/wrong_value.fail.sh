9b92f9d3459dce41ae4dcfc24b7f6f74faefd631
ayfantis <ayfantis@localhost.localdomain>
2021-09-29 23:30:57 +0200

    updated cpuinfo test to include all possible outcomes

--- linux_os/guide/system/permissions/restrictions/enable_nx/bios_enable_execution_restrictions/tests/wrong_value.fail.sh
+++ linux_os/guide/system/permissions/restrictions/enable_nx/bios_enable_execution_restrictions/tests/wrong_value.fail.sh
@@ -4,25 +4,25 @@
 #
 # remediation = none
 
-
-if [ -e /etc/cpuinfo ] ; then
 > /tmp/cpuinfo
+> /tmp/cmdline
+
+if [ -e /tmp/cpuinfo ] && [ -e /proc/cpuinfo ]; then
 cp /proc/cpuinfo /tmp/cpuinfo
-if grep -Fxq 's/^flags.*:.*/& nx/g' /tmp/cpuinfo ; then
+if grep -Fxq '^flags.*:.*/& nx' /tmp/cpuinfo ; then
 sed -i 's/^flags.*:.*/& nx/g' /tmp/cpuinfo
 else
-echo '' >> /tmp/cpuinfo
+echo 'flags = nx' >> /tmp/cpuinfo
 fi
-mount --bind /etc/cpuinfo /proc/cpuinfo
+mount --bind /tmp/cpuinfo /proc/cpuinfo
 fi
 
-if [ -e /etc/cmdline ] ; then
-> /tmp/cmdline
+if [ -e /tmp/cmdline ] && [ -e /proc/cmdline ]; then
 cp /proc/cmdline /tmp/cmdline
-if grep -Fxq sed -r 's/\s+noexec[0-9]*=off[\s]*//g' /tmp/cmdline ; then
+if grep -Fxq sed -r '\s+noexec[0-9]*=off[\s]*' /tmp/cmdline ; then
 sed -r 's/\s+noexec[0-9]*=off[\s]*//g' /tmp/cmdline
 else
-echo '' >> /tmp/cmdline
+echo 'noexec100=off' >> /tmp/cmdline
 fi
-mount --bind /etc/cmdline /proc/cmdline
+mount --bind /tmp/cmdline /proc/cmdline
 fi
