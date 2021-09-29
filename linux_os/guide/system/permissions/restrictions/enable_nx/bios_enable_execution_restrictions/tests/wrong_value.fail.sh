ff17118fe524b855e870ad2692c171df76c1e2d2
ayfantis <ayfantis@localhost.localdomain>
2021-09-29 13:59:08 +0200

    updating script to not completely alter the desired files

--- linux_os/guide/system/permissions/restrictions/enable_nx/bios_enable_execution_restrictions/tests/wrong_value.fail.sh
+++ linux_os/guide/system/permissions/restrictions/enable_nx/bios_enable_execution_restrictions/tests/wrong_value.fail.sh
@@ -4,8 +4,25 @@
 #
 # remediation = none
 
-> /etc/cpuinfo
 
 if [ -e /etc/cpuinfo ] ; then
+> /tmp/cpuinfo
+cp /proc/cpuinfo /tmp/cpuinfo
+if grep -Fxq 's/^flags.*:.*/& nx/g' /tmp/cpuinfo ; then
+sed -i 's/^flags.*:.*/& nx/g' /tmp/cpuinfo
+else
+echo '' >> /tmp/cpuinfo
+fi
 mount --bind /etc/cpuinfo /proc/cpuinfo
+fi
+
+if [ -e /etc/cmdline ] ; then
+> /tmp/cmdline
+cp /proc/cmdline /tmp/cmdline
+if grep -Fxq sed -r 's/\s+noexec[0-9]*=off[\s]*//g' /tmp/cmdline ; then
+sed -r 's/\s+noexec[0-9]*=off[\s]*//g' /tmp/cmdline
+else
+echo '' >> /tmp/cmdline
+fi
+mount --bind /etc/cmdline /proc/cmdline
 fi
