eb676a00177976592fd5fd64e9322b82416111c2
ayfantis <ayfantis@localhost.localdomain>
2021-09-29 10:00:45 +0200

    fixed platform

--- linux_os/guide/services/ssh/ssh_server/sshd_use_approved_macs/tests/wrong_value.fail.sh
+++ linux_os/guide/services/ssh/ssh_server/sshd_use_approved_macs/tests/wrong_value.fail.sh
@@ -1,3 +1,3 @@
-# platform = multi_platform_rhel, multi_platform_fedora
+# platform = multi_platform_rhel
 
 {{{ bash_replace_or_append('/etc/ssh/sshd_config', '^MACs', "wrong_value_expected_to_fail.com", '@CCENUM@', '%s %s') }}}
