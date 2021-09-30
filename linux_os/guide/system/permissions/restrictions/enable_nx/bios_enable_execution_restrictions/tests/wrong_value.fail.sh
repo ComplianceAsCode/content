012661d2d18a901248cca97e3b532fbd7f7704d5
ayfantis <ayfantis@localhost.localdomain>
2021-09-30 12:14:31 +0200

    Simplified script due to the nature of the test execution

--- linux_os/guide/system/permissions/restrictions/enable_nx/bios_enable_execution_restrictions/tests/wrong_value.fail.sh
+++ linux_os/guide/system/permissions/restrictions/enable_nx/bios_enable_execution_restrictions/tests/wrong_value.fail.sh
@@ -2,23 +2,10 @@
 # platform = multi_platform_rhel,multi_platform_fedora
 # remediation = none
 
-> /tmp/cpuinfo
-> /tmp/cmdline
-
-if [ cp /proc/cpuinfo /tmp/cpuinfo ]; then
-if grep -q '^flags.*:.*/& nx' /tmp/cpuinfo ; then
+cp /proc/cpuinfo /tmp/cpuinfo
 sed -i 's/^flags.*:.*/& nx/g' /tmp/cpuinfo
-else
-echo 'flags = nx' >> /tmp/cpuinfo
-fi
 mount --bind /tmp/cpuinfo /proc/cpuinfo
-fi
 
-if [ cp /proc/cmdline /tmp/cmdline ]; then
-if grep -q '\s+noexec[0-9]*=off[\s]*' /tmp/cmdline ; then
+cp /proc/cmdline /tmp/cmdline
 sed -r 's/\s+noexec[0-9]*=off[\s]*//g' /tmp/cmdline
-else
-echo 'noexec100=off' >> /tmp/cmdline
-fi
 mount --bind /tmp/cmdline /proc/cmdline
-fi
