d253f6919b90af81194946a8c43c8b2e77f86f0b
teacup-on-rockingchair <strandjata@gmail.com>
2021-10-12 22:04:25 +0300

    Update linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_password_pam_unix_remember/bash/shared.sh
    Co-authored-by: Watson Yuuma Sato <wsato@redhat.com>

--- linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_password_pam_unix_remember/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_password_pam_unix_remember/bash/shared.sh
@@ -11,7 +11,7 @@ AUTH_FILES[1]="/etc/pam.d/password-auth"
 
 for pamFile in "${AUTH_FILES[@]}"
 do
-    if grep -q "pam_unix.so.*remember=" $pamFile; then
+    if grep -q "pam_unix.so.*" $pamFile; then
         {{{ bash_provide_pam_module_options("$pamFile", 'password', 'sufficient', 'pam_unix.so', 'remember', "$var_password_pam_unix_remember", "$var_password_pam_unix_remember") }}}
     fi
     if grep -q "pam_pwhistory.so.*remember=" $pamFile; then
