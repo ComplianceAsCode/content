a5cce64337e8b8617f3bf3ee1311e80d652754ea
Gabriel Becker <ggasparb@redhat.com>
2021-10-14 12:12:16 +0200

    Set sshd priv keys permissions 600 for all products.

--- linux_os/guide/services/ssh/file_permissions_sshd_private_key/tests/missing_file_test.pass.sh
+++ linux_os/guide/services/ssh/file_permissions_sshd_private_key/tests/missing_file_test.pass.sh
@@ -3,5 +3,6 @@
 
 mkdir -p /etc/ssh/keys
 mv /etc/ssh/*_key /etc/ssh/keys
-sed -i "s#HostKey /etc/ssh/#HostKey /etc/ssh/keys/#g" sshd_config
+sed -i "s#HostKey /etc/ssh/#HostKey /etc/ssh/keys/#g" /etc/ssh/sshd_config
 systemctl restart sshd
+rm -f /etc/ssh/*_key # systemctl restart sshd always regenerate keys in the /etc/ssh if they are not there
