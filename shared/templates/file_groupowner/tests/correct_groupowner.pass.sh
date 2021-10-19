9d51c22aad402739b9eb0b8ad55c18bbb38c25b3
Eduardo Barretto <eduardo.barretto@canonical.com>
2021-10-19 15:12:49 +0200

    Add recursive parameter to file_groupowner template

--- shared/templates/file_groupowner/tests/correct_groupowner.pass.sh
+++ shared/templates/file_groupowner/tests/correct_groupowner.pass.sh
@@ -4,6 +4,8 @@
 {{% for path in FILEPATH %}}
 {{% if IS_DIRECTORY and FILE_REGEX %}}
 echo "Create specific tests for this rule because of regex"
+{{% elif IS_DIRECTORY and RECURSIVE %}}
+find -L {{{ path }}} -type d -exec chgrp {{{ FILEGID }}} {} \;
 {{% else %}}
 if [ ! -f {{{ path }}} ]; then
     touch {{{ path }}}
--- shared/templates/file_groupowner/tests/incorrect_groupowner.fail.sh
+++ shared/templates/file_groupowner/tests/incorrect_groupowner.fail.sh
@@ -6,6 +6,8 @@ groupadd group_test
 {{% for path in FILEPATH %}}
 {{% if IS_DIRECTORY and FILE_REGEX %}}
 echo "Create specific tests for this rule because of regex"
+{{% elif IS_DIRECTORY and RECURSIVE %}}
+find -L {{{ path }}} -type d -exec chgrp group_test {} \;
 {{% else %}}
 if [ ! -f {{{ path }}} ]; then
     touch {{{ path }}}
