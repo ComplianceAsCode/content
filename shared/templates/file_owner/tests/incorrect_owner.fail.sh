ad9f03e0015a64bdf8ad5b08968b32f6ee5627da
Eduardo Barretto <eduardo.barretto@canonical.com>
2021-10-20 09:37:09 +0200

    Add recursive parameter to file_owner template

--- shared/templates/file_owner/tests/incorrect_owner.fail.sh
+++ shared/templates/file_owner/tests/incorrect_owner.fail.sh
@@ -6,6 +6,8 @@ useradd testuser_123
 {{% for path in FILEPATH %}}
 {{% if IS_DIRECTORY and FILE_REGEX %}}
 echo "Create specific tests for this rule because of regex"
+{{% elif IS_DIRECTORY and RECURSIVE %}}
+find -L {{{ path }}} -type d -exec chown testuser_123 {} \;
 {{% else %}}
 if [ ! -f {{{ path }}} ]; then
     touch {{{ path }}}
