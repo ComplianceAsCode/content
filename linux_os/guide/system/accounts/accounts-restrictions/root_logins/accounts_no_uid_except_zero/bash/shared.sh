0a8db304cc23017bc698d1cc9666d2aa0cb5b738
Matthew Burket <mburket@redhat.com>
2021-10-21 14:47:20 -0500

    accounts_no_uid_except_zero: Don't run passwd if awk returns nothing
    Fixes #7457

--- linux_os/guide/system/accounts/accounts-restrictions/root_logins/accounts_no_uid_except_zero/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-restrictions/root_logins/accounts_no_uid_except_zero/bash/shared.sh
@@ -1,2 +1,2 @@
 # platform = multi_platform_all
-awk -F: '$3 == 0 && $1 != "root" { print $1 }' /etc/passwd | xargs --max-lines=1 passwd -l
+awk -F: '$3 == 0 && $1 != "root" { print $1 }' /etc/passwd | xargs --no-run-if-empty --max-lines=1 passwd -l
