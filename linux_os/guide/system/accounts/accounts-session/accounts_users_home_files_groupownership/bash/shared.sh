289734148252e6177df901fd2afefbe04acdc20e
Marcus Burghardt <maburgha@redhat.com>
2021-11-03 12:44:58 +0100

    OVAL, tests and remediation for the rule:
    accounts_users_home_files_groupownership

---
+++ linux_os/guide/system/accounts/accounts-session/accounts_users_home_files_groupownership/bash/shared.sh
@@ -0,0 +1,7 @@
+# platform = multi_platform_all
+# reboot = false
+# strategy = restrict
+# complexity = low
+# disruption = low
+
+awk -F':' '{ if ($4 >= {{{ gid_min }}} && $4 != 65534) system("chgrp -Rf " $4" "$6) }' /etc/passwd
