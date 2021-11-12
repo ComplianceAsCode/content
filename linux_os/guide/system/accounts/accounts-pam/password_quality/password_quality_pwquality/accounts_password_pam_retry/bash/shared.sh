66cce6a1117547b86e1189530623eef88e8f3eb3
Gabriel Becker <ggasparb@redhat.com>
2021-11-12 14:00:24 +0100

    Add RHEL9 to jinja inline if statements when applicable.

--- linux_os/guide/system/accounts/accounts-pam/password_quality/password_quality_pwquality/accounts_password_pam_retry/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-pam/password_quality/password_quality_pwquality/accounts_password_pam_retry/bash/shared.sh
@@ -8,7 +8,7 @@ else
 	sed -i --follow-symlinks "/pam_pwquality.so/ s/$/ retry=$var_password_pam_retry/" /etc/pam.d/system-auth
 fi
 
-{{% if product == "rhel8" -%}}
+{{% if product in ['rhel8', 'rhel9'] -%}}
 if grep -q "retry=" /etc/pam.d/password-auth ; then
 	sed -i --follow-symlinks "s/\(retry *= *\).*/\1$var_password_pam_retry/" /etc/pam.d/password-auth
 else
