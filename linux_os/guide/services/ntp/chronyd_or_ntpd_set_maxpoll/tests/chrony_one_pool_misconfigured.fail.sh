1652c04338fd8705b074e5467f916a1377c681e8
Watson Sato <wsato@redhat.com>
2021-11-22 16:32:12 +0100

    Add the maxpoll option to chrony pool directives
    The same options that apply to server also applies to pool.
    This updates the rule to recognize and remediate pool entries with
    the appropriate maxpoll value.

---
+++ linux_os/guide/services/ntp/chronyd_or_ntpd_set_maxpoll/tests/chrony_one_pool_misconfigured.fail.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# packages = chrony
+#
+# profiles = xccdf_org.ssgproject.content_profile_stig
+
+yum remove -y ntp
+
+# Remove all server or pool options
+sed -i "/^\(server\|pool\).*/d" /etc/chrony.conf
+
+echo "pool pool.ntp.org iburst maxpoll 18" >> /etc/chrony.conf
+
+systemctl enable chronyd.service
+
