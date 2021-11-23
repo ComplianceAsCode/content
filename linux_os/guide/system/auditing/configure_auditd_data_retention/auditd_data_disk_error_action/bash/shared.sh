c2a1bf0053795f7eef9f0a79a4f5022319a12ec5
Federico Ramirez <federico.r.ramirez@oracle.com>
2021-11-23 14:16:16 -0600

    Add OL8 platform to remediation content
    Add OL8 platform to remediation content for rules
    part of the OL8 stig profile
    
    Signed-off-by: Federico Ramirez <federico.r.ramirez@oracle.com>

--- linux_os/guide/system/auditing/configure_auditd_data_retention/auditd_data_disk_error_action/bash/shared.sh
+++ linux_os/guide/system/auditing/configure_auditd_data_retention/auditd_data_disk_error_action/bash/shared.sh
@@ -1,4 +1,4 @@
-# platform = Red Hat Virtualization 4,multi_platform_rhel
+# platform = Red Hat Virtualization 4,multi_platform_ol,multi_platform_rhel
 
 {{{ bash_instantiate_variables("var_auditd_disk_error_action") }}}
 
