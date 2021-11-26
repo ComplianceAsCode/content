bc750040c1cdb7f84d78431a62b3a42db7257142
Marcus Burghardt <maburgha@redhat.com>
2021-11-26 16:13:13 +0100

    Update the accounts_passwords_pam_faillock_unlock_time rule
    Redesigned the OVAL and remediation logic aligning this rule
    with the other rules related to pam_faillock.so.

---
+++ linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_passwords_pam_faillock_unlock_time/tests/pam_faillock_expected_pam_files.pass.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# variables = var_accounts_passwords_pam_faillock_unlock_time=600
+
+if [ -f /usr/sbin/authconfig ]; then
+    authconfig --enablefaillock --faillockargs="unlock_time=600" --update
+else
+    authselect enable-feature with-faillock
+    sed -i --follow-symlinks 's/\(pam_faillock.so \(preauth silent\|authfail\)\).*$/\1 unlock_time=600/g' /etc/pam.d/system-auth /etc/pam.d/password-auth
+fi
+if [ -f /etc/security/faillock.conf ]; then
+    > /etc/security/faillock.conf
+fi
