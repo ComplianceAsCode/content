280c0febcd922bffcbe5a74b981b3374524463c1
Marcus Burghardt <maburgha@redhat.com>
2021-11-26 15:28:58 +0100

    Update the accounts_passwords_pam_faillock_interval rule
    Redesign the OVAL and remediation logic aligning this rule
    with the other rules related to pam_faillock.so.

--- linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_passwords_pam_faillock_interval/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_passwords_pam_faillock_interval/bash/shared.sh
@@ -1,5 +1,30 @@
-# platform = multi_platform_rhel,multi_platform_fedora,multi_platform_ol,multi_platform_rhv
+# platform = multi_platform_wrlinux,multi_platform_rhel,multi_platform_fedora,multi_platform_ol,multi_platform_rhv,multi_platform_sle
 
 {{{ bash_instantiate_variables("var_accounts_passwords_pam_faillock_fail_interval") }}}
 
-{{{ bash_set_faillock_option("fail_interval", "$var_accounts_passwords_pam_faillock_fail_interval") }}}
+if [ $(grep -c "^\s*auth.*pam_unix.so" /etc/pam.d/system-auth) > 1 ] || \
+   [ $(grep -c "^\s*auth.*pam_unix.so" /etc/pam.d/password-auth) > 1 ]; then
+   echo "Skipping remediation because there are more pam_unix.so entries than expected."
+   false
+fi
+
+if [ -f /etc/security/faillock.conf ]; then
+    if $(grep -q '^\s*fail_interval\s*=' /etc/security/faillock.conf); then
+        sed -i --follow-symlinks "s/^\s*\(fail_interval\s*\)=.*$/\1 = $var_accounts_passwords_pam_faillock_fail_interval/g" /etc/security/faillock.conf
+    else
+        echo "fail_interval = $var_accounts_passwords_pam_faillock_fail_interval" >> /etc/security/faillock.conf
+    fi
+    # If the faillock.conf file is present, but for any reason, like an OS upgrade, the
+    # pam_faillock.so parameters are still defined in pam files, this make them compatible with
+    # the newer versions of authselect tool and ensure the parameters are only in faillock.conf.
+    sed -i --follow-symlinks 's/\(pam_faillock.so preauth\).*$/\1 silent/g' /etc/pam.d/system-auth /etc/pam.d/password-auth
+    sed -i --follow-symlinks 's/\(pam_faillock.so authfail\).*$/\1/g' /etc/pam.d/system-auth /etc/pam.d/password-auth
+    authselect enable-feature with-faillock
+else
+    if [ -f /usr/sbin/authconfig ]; then
+        authconfig --enablefaillock --update
+    else
+        authselect enable-feature with-faillock
+    fi
+    {{{ bash_set_faillock_option("fail_interval", "$var_accounts_passwords_pam_faillock_fail_interval") }}}
+fi
---
+++ linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_passwords_pam_faillock_interval/tests/pam_faillock_multiple_pam_unix_pam_files.fail.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# remediation = none
+# variables = var_accounts_passwords_pam_faillock_fail_interval=900
+
+if [ -f /usr/sbin/authconfig ]; then
+    authconfig --enablefaillock --faillockargs="fail_interval=900" --update
+else
+    authselect enable-feature with-faillock
+    sed -i --follow-symlinks 's/\(pam_faillock.so \(preauth silent\|authfail\)\).*$/\1 fail_interval=900/g' /etc/pam.d/system-auth /etc/pam.d/password-auth
+fi
+if [ -f /etc/security/faillock.conf ]; then
+    > /etc/security/faillock.conf
+fi
+
+# Multiple instances of pam_unix.so in auth section may, intentionally or not, interfere
+# in the expected bevaviour of pam_faillock.so.
+echo "auth        sufficient       pam_unix.so" >> /etc/pam.d/password-auth
