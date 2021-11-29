dca020344599c03549402ece7f8b44b35cda1760
Marcus Burghardt <maburgha@redhat.com>
2021-11-29 11:37:53 +0100

    Update the accounts_passwords_pam_faillock_enforce_local rule
    Aligned the OVAL and remediation logic of this rule with the
    other rules related to pam_faillock.so.

---
+++ linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_passwords_pam_faillock_enforce_local/bash/shared.sh
@@ -0,0 +1,15 @@
+# platform = multi_platform_fedora,Red Hat Enterprise Linux 8,Red Hat Enterprise Linux 9
+
+SYSTEM_AUTH="/etc/pam.d/system-auth"
+PASSWORD_AUTH="/etc/pam.d/password-auth"
+
+if [ $(grep -c "^\s*auth.*pam_unix.so" $SYSTEM_AUTH) > 1 ] || \
+   [ $(grep -c "^\s*auth.*pam_unix.so" $PASSWORD_AUTH) > 1 ]; then
+   echo "Skipping remediation because there are more pam_unix.so entries than expected."
+   false
+fi
+
+if [ ! $(grep -q '^\s*local_users_only' /etc/security/faillock.conf) ]; then
+    echo "local_users_only" >> /etc/security/faillock.conf
+fi
+authselect enable-feature with-faillock
