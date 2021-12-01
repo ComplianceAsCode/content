ff2d97245e16b808c4f54bc7366a8cda5dc5df7e
Jan Černý <jcerny@redhat.com>
2021-12-01 15:01:13 +0100

    Make sure /etc/selinux/config exists
    There are containers that don't contain this file.
    
    If it exists and we touch it it won't change its contents.

--- linux_os/guide/system/selinux/selinux_state/tests/selinux_enforcing.pass.sh
+++ linux_os/guide/system/selinux/selinux_state/tests/selinux_enforcing.pass.sh
@@ -2,6 +2,7 @@
 # profiles = xccdf_org.ssgproject.content_profile_C2S, xccdf_org.ssgproject.content_profile_ospp
 
 SELINUX_FILE='/etc/selinux/config'
+touch "$SELINUX_FILE"
 
 if grep -s '^[[:space:]]*SELINUX' $SELINUX_FILE; then
 	sed -i 's/^\([[:space:]]*SELINUX[[:space:]]*=[[:space:]]*\).*/\1enforcing/' $SELINUX_FILE
