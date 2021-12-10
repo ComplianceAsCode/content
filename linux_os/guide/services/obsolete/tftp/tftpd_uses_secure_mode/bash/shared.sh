c1ecd82b61beea3166c070b3e3a51e7eb146dbc9
Matej Tyc <matyc@redhat.com>
2021-12-10 17:54:42 +0100

    Remove shebangs from source
    We have snippets, not scripts, shebangs should be added by scanners

--- linux_os/guide/services/obsolete/tftp/tftpd_uses_secure_mode/bash/shared.sh
+++ linux_os/guide/services/obsolete/tftp/tftpd_uses_secure_mode/bash/shared.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 # platform = Oracle Linux 7,Oracle Linux 8,Red Hat Enterprise Linux 7,Red Hat Enterprise Linux 8,Red Hat Virtualization 4,WRLinux 1019
 
 {{{ bash_instantiate_variables ("var_tftpd_secure_directory") }}}
