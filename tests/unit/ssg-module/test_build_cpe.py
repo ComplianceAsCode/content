f98a1427c88839f94cb34a8131501091a8b876d3
Jan Černý <jcerny@redhat.com>
2022-01-03 17:40:02 +0100

    Improve test for ProductCPEs.get_cpe()
    * add comments to explain things
    * merge into a single test function
    * compare that CPE obtained by CPE name is the same as CPE obtained
      by CPE ID

--- tests/unit/ssg-module/test_build_cpe.py
+++ tests/unit/ssg-module/test_build_cpe.py
@@ -94,13 +94,30 @@ def test_extract_referred_nodes():
     assert results[0].text == 'Source Code'
 
 
+#############################################
+# Unit tests for ProductCPEs.get_cpe() method
+#############################################
+#
+# Note that there are 2 types of CPE definitions that differ by the source they
+# come from:
+# * Product CPEs, loaded from product YAML
+# * Content CPEs, loaded from directory specified by the `cpes_root` key in
+#   product YML, usually from the `/applicability` directory
+#
+# This test case test that both types are used by the ProductCPEs class and
+# that both CPE types are handled equally.
 def test_product_cpes():
+
+    # CPEs are loaded from `DATADIR/product.yml` but also from
+    # `DATADIR/applicability` because `DATADIR/product.yml` references the
+    # `DATADIR/applicability` directory in the `cpes_root` key
     product_yaml_path = os.path.join(DATADIR, "product.yml")
     product_yaml = open_raw(product_yaml_path)
     product_yaml["product_dir"] = os.path.dirname(product_yaml_path)
     product_cpes = ssg.build_cpe.ProductCPEs(product_yaml)
 
-    # get CPE by name and verify it's loaded
+    # get a product CPE by name and verify it's loaded
+    # this CPE is defined in `DATADIR/product.yml`
     rhel7_cpe = product_cpes.get_cpe("rhel7")
     assert(rhel7_cpe.name == "cpe:/o:redhat:enterprise_linux:7")
     assert(rhel7_cpe.title == "Red Hat Enterprise Linux 7")
@@ -108,22 +125,17 @@ def test_product_cpes():
     assert(rhel7_cpe.bash_conditional == "")
     assert(rhel7_cpe.ansible_conditional == "")
 
-    # get CPE by ID and verify it's loaded
+    # get CPE by ID and verify it's loaded, the get_cpe method should return
+    # the same object as when CPE name was used above
     rhel7_cpe_2 = product_cpes.get_cpe("cpe:/o:redhat:enterprise_linux:7")
-    assert(rhel7_cpe_2.name == "cpe:/o:redhat:enterprise_linux:7")
-    assert(rhel7_cpe_2.title == "Red Hat Enterprise Linux 7")
-    assert(rhel7_cpe_2.check_id == "installed_OS_is_rhel7")
-    assert(rhel7_cpe.bash_conditional == "")
-    assert(rhel7_cpe.ansible_conditional == "")
+    assert(rhel7_cpe_2.name == rhel7_cpe.name)
+    assert(rhel7_cpe_2.title == rhel7_cpe_2.title)
+    assert(rhel7_cpe_2.check_id == rhel7_cpe.check_id)
+    assert(rhel7_cpe_2.bash_conditional == rhel7_cpe.bash_conditional)
+    assert(rhel7_cpe_2.ansible_conditional == rhel7_cpe.ansible_conditional)
 
-
-def test_content_cpes():
-    product_yaml_path = os.path.join(DATADIR, "product.yml")
-    product_yaml = open_raw(product_yaml_path)
-    product_yaml["product_dir"] = os.path.dirname(product_yaml_path)
-    product_cpes = ssg.build_cpe.ProductCPEs(product_yaml)
-
-    # get CPE by name and verify it's loaded
+    # get a content CPE by name and verify it's loaded
+    # this CPE is defined in `DATADIR/applicability/virtualization.yml`
     cpe1 = product_cpes.get_cpe("machine")
     assert(cpe1.name == "cpe:/a:machine")
     assert(cpe1.title == "Bare-metal or Virtual Machine")
@@ -131,10 +143,11 @@ def test_content_cpes():
     assert(cpe1.ansible_conditional == "ansible_virtualization_type not in [\"docker\", \"lxc\", \"openvz\", \"podman\", \"container\"]")
     assert(cpe1.bash_conditional == "[ ! -f /.dockerenv ] && [ ! -f /run/.containerenv ]")
 
-    # get CPE by ID and verify it's loaded
+    # get CPE by ID and verify it's loaded, the get_cpe method should return
+    # the same object as when CPE name was used above
     cpe2 = product_cpes.get_cpe("cpe:/a:machine")
-    assert(cpe2.name == "cpe:/a:machine")
-    assert(cpe2.title == "Bare-metal or Virtual Machine")
-    assert(cpe2.check_id == "installed_env_is_a_machine")
-    assert(cpe2.ansible_conditional == "ansible_virtualization_type not in [\"docker\", \"lxc\", \"openvz\", \"podman\", \"container\"]")
-    assert(cpe2.bash_conditional == "[ ! -f /.dockerenv ] && [ ! -f /run/.containerenv ]")
+    assert(cpe2.name == cpe1.name)
+    assert(cpe2.title == cpe1.title)
+    assert(cpe2.check_id == cpe1.check_id)
+    assert(cpe2.ansible_conditional == cpe1.ansible_conditional)
+    assert(cpe2.bash_conditional == cpe1.bash_conditional)
