29ec4722c34a3e41e3a7e9468a4e7332f5282928
Marcus Burghardt <maburgha@redhat.com>
2022-01-06 22:27:06 +0100

    Change the remediation approach for authselect
    For systems which rely on authselect to manage pam settings, the
    remediation will be aborted if the authselect integrity check fails.
    This way, any manual and probably intentional change in pam files will
    be preserved and an informative message will be shown in the
    remediation report.

--- linux_os/guide/system/accounts/accounts-restrictions/password_storage/no_empty_passwords/tests/nullok_present.fail.sh
+++ linux_os/guide/system/accounts/accounts-restrictions/password_storage/no_empty_passwords/tests/nullok_present.fail.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+
 SYSTEM_AUTH_FILE="/etc/pam.d/system-auth"
 
 if ! $(grep -q "^[^#].*pam_unix.so.*nullok" $SYSTEM_AUTH_FILE); then
-    sed -i 's/\([\s].*pam_unix.so.*\)\s\(try_first_pass.*\)/\1nullok \2/' $SYSTEM_AUTH_FILE
+    if [ -f /usr/bin/authselect ]; then
+        authselect disable-feature without-nullok
+        authselect apply-changes
+    else
+        sed -i 's/\([\s].*pam_unix.so.*\)\s\(try_first_pass.*\)/\1nullok \2/' $SYSTEM_AUTH_FILE
+    fi
 fi
