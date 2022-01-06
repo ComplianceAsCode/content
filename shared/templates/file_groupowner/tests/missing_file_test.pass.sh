b09c8f1963969e2db1dcf0c9e41f3eb208bd3e13
Eduardo Barretto <eduardo.barretto@canonical.com>
2022-01-06 18:45:24 +0100

    Improve missing_file_test_pass test in template file_groupowner

--- shared/templates/file_groupowner/tests/missing_file_test.pass.sh
+++ shared/templates/file_groupowner/tests/missing_file_test.pass.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 #
 
-{{% if MISSING_FILE_PASS %}}
-    rm -f {{{ FILEPATH }}}
-{{% else %}}
-    true
-{{% endif %}}
+{{% for path in FILEPATH %}}
+    {{% if MISSING_FILE_PASS %}}
+        rm -f {{{ path }}}
+    {{% else %}}
+        {{% if IS_DIRECTORY and FILE_REGEX %}}
+        echo "Create specific tests for this rule because of regex"
+        {{% elif IS_DIRECTORY and RECURSIVE %}}
+        find -L {{{ path }}} -type d -exec chgrp {{{ FILEGID }}} {} \;
+        {{% else %}}
+        if [ ! -f {{{ path }}} ]; then
+            mkdir -p "$(dirname '{{{ path }}}')"
+            touch {{{ path }}}
+        fi
+        chgrp {{{ FILEGID }}} {{{ path }}}
+        {{% endif %}}
+    {{% endif %}}
+{{% endfor %}}
