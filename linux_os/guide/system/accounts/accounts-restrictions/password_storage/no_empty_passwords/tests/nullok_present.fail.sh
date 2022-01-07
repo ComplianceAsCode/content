bc45611be00d6273a8ba83597a8340fe5d6d4066
Marcus Burghardt <maburgha@redhat.com>
2022-01-07 12:22:21 +0100

    Improve informative message and test scenarios

--- linux_os/guide/system/accounts/accounts-restrictions/password_storage/no_empty_passwords/tests/nullok_present.fail.sh
+++ linux_os/guide/system/accounts/accounts-restrictions/password_storage/no_empty_passwords/tests/nullok_present.fail.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
+# packages = authselect
+# platform = Red Hat Enterprise Linux 7,Red Hat Virtualization 4,multi_platform_fedora,multi_platform_ol,multi_platform_wrlinux
 
 SYSTEM_AUTH_FILE="/etc/pam.d/system-auth"
 
 if ! $(grep -q "^[^#].*pam_unix.so.*nullok" $SYSTEM_AUTH_FILE); then
-    if [ -f /usr/bin/authselect ]; then
-        authselect disable-feature without-nullok
-        authselect apply-changes
-    else
-        sed -i 's/\([\s].*pam_unix.so.*\)\s\(try_first_pass.*\)/\1nullok \2/' $SYSTEM_AUTH_FILE
-    fi
+    sed -i 's/\([\s].*pam_unix.so.*\)\s\(try_first_pass.*\)/\1nullok \2/' $SYSTEM_AUTH_FILE
 fi
