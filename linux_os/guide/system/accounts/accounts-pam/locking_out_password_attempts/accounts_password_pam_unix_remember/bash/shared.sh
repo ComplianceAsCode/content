f0cbb0b3a883ffbc40dc095619ebb9614f93b5cd
Marcus Burghardt <maburgha@redhat.com>
2022-01-07 11:20:36 +0100

    Change the remediation approach for authselect
    For systems which rely on authselect to manage pam settings, the
    remediation will be aborted if the authselect integrity check fails.
    This way, any manual and probably intentional change in pam files will
    be preserved and an informative message will be shown in the
    remediation report.

--- linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_password_pam_unix_remember/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_password_pam_unix_remember/bash/shared.sh
@@ -3,30 +3,40 @@
 {{{ bash_instantiate_variables("var_password_pam_unix_remember") }}}
 
 if [ -f /usr/bin/authselect ]; then
-    # Ensure a backup before any modification
-    authselect apply-changes -b --backup=before-hardening.backup
-    # Existing profiles should not be modified, so we create a custom profile if not already in use.
-    CURRENT_PROFILE=$(authselect current -r | awk '{ print $1 }')
-    if [[ ! $CURRENT_PROFILE == custom/* ]]; then
-        ENABLED_FEATURES=$(authselect current | tail -n+3 | awk '{ print $2 }')
-        authselect create-profile hardening -b sssd
-        CURRENT_PROFILE="custom/hardening"
-    fi
-    # Include the desired configuration in the custom profile
-    CUSTOM_SYSTEM_AUTH="/etc/authselect/$CURRENT_PROFILE/system-auth"
-    CUSTOM_PASSWORD_AUTH="/etc/authselect/$CURRENT_PROFILE/password-auth"
-    for custom_pam_file in $CUSTOM_SYSTEM_AUTH $CUSTOM_PASSWORD_AUTH; do
-        if ! $(grep -q "^[^#].*pam_pwhistory.so.*remember=" $custom_pam_file); then
-            sed -i "/^password.*requisite.*pam_pwquality.so/a password    requisite     pam_pwhistory.so remember=$var_password_pam_unix_remember use_authtok" $custom_pam_file
-        else
-            sed -i "s/\(.*pam_pwhistory.so.*remember=\)[[:digit:]]\+\s\(.*\)/\1$var_password_pam_unix_remember \2/g" $custom_pam_file
+    if authselect check; then
+        CURRENT_PROFILE=$(authselect current -r | awk '{ print $1 }')
+        # Standard profiles delivered with authselect should not be modified.
+        # If not already in use, a custom profile is created preserving the enabled features.
+        if [[ ! $CURRENT_PROFILE == custom/* ]]; then
+            ENABLED_FEATURES=$(authselect current | tail -n+3 | awk '{ print $2 }')
+            authselect create-profile hardening -b $CURRENT_PROFILE
+            CURRENT_PROFILE="custom/hardening"
+            # Ensure a backup before changing the profile
+            authselect apply-changes -b --backup=before-pwhistory-hardening.backup
+            authselect select $CURRENT_PROFILE
+            for feature in $ENABLED_FEATURES; do
+                authselect enable-feature $feature;
+            done
         fi
-    done
-    authselect select $CURRENT_PROFILE --force
-    for feature in $ENABLED_FEATURES; do
-        authselect enable-feature $feature;
-    done
-    authselect apply-changes
+        # Include the desired configuration in the custom profile
+        CUSTOM_SYSTEM_AUTH="/etc/authselect/$CURRENT_PROFILE/system-auth"
+        CUSTOM_PASSWORD_AUTH="/etc/authselect/$CURRENT_PROFILE/password-auth"
+        for custom_pam_file in $CUSTOM_SYSTEM_AUTH $CUSTOM_PASSWORD_AUTH; do
+            if ! $(grep -q "^[^#].*pam_pwhistory.so.*remember=" $custom_pam_file); then
+                sed -i --follow-symlinks "/^password.*requisite.*pam_pwquality.so/a password    requisite     pam_pwhistory.so remember=$var_password_pam_unix_remember use_authtok" $custom_pam_file
+            else
+                sed -i --follow-symlinks "s/\(.*pam_pwhistory.so.*remember=\)[[:digit:]]\+\s\(.*\)/\1$var_password_pam_unix_remember \2/g" $custom_pam_file
+            fi
+        done
+        authselect apply-changes -b --backup=after-pwhistory-hardening.backup
+    else
+        echo "
+authselect integrity check failed. Remediation aborted!
+This remediation could not be applied because the authselect profile is not integer, probably due to manual edition.
+In cases where the default authselect profile does not cover a specific demand, a custom authselect profile is recommended.
+Where authselect is in place, it is not recommended to manually edit pam files."
+    false
+    fi
 else
     {{% if product in [ "sle15" ] %}}
     AUTH_FILES[0]="/etc/pam.d/common-password"
--- linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_password_pam_unix_remember/tests/authselect_modified_pam.fail.sh
+++ linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_password_pam_unix_remember/tests/authselect_modified_pam.fail.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
+# packages = authselect
 # platform = Red Hat Enterprise Linux 8,Red Hat Enterprise Linux 9,multi_platform_fedora
+# remediation = none
 
 SYSTEM_AUTH_FILE="/etc/pam.d/system-auth"
 
 # This modification will break the integrity checks done by authselect. The remediation
 # should be robust enough to deal with possible manual and unvalidated modification.
 if ! $(grep -q "^[^#].*pam_pwhistory.so.*remember=" $SYSTEM_AUTH_FILE); then
-	sed -i "/^password.*requisite.*pam_pwquality.so/a password    requisite     pam_pwhistory.so" $SYSTEM_AUTH_FILE
+	sed -i --follow-symlinks "/^password.*requisite.*pam_pwquality.so/a password    requisite     pam_pwhistory.so" $SYSTEM_AUTH_FILE
 else
-   sed -i "s/\(.*pam_pwhistory.so.*remember=\)[[:digit:]]\+\s\(.*\)/\1/g" $SYSTEM_AUTH_FILE
+   sed -i --follow-symlinks "s/\(.*pam_pwhistory.so.*remember=\)[[:digit:]]\+\s\(.*\)/\1/g" $SYSTEM_AUTH_FILE
 fi
