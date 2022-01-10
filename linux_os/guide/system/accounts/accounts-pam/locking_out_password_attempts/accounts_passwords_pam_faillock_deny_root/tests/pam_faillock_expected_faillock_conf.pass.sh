8e9c359ae98ddbc15014aab62fccc72b439accb4
Marcus Burghardt <maburgha@redhat.com>
2022-01-10 10:37:24 +0100

    Adjust test scenarios for pam_faillock rules
    Previosly, test scenarios for authconfig and authselect shared the same
    script. Now it was included the "platform" header and simplified each
    script for its platform. This also saves time during the test scenarios
    for a specific platform.

--- linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_passwords_pam_faillock_deny_root/tests/pam_faillock_expected_faillock_conf.pass.sh
+++ linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_passwords_pam_faillock_deny_root/tests/pam_faillock_expected_faillock_conf.pass.sh
@@ -4,8 +4,6 @@
 
 authselect select sssd --force
 authselect enable-feature with-faillock
-# Ensure the parameters only in /etc/security/faillock.conf
-sed -i --follow-symlinks 's/\(pam_faillock.so \(preauth silent\|authfail\)\).*$/\1/g' /etc/pam.d/system-auth /etc/pam.d/password-auth
 > /etc/security/faillock.conf
 echo "even_deny_root" >> /etc/security/faillock.conf
 echo "silent" >> /etc/security/faillock.conf
