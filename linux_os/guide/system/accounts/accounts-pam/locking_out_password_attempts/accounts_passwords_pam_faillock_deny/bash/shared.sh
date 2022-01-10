e4b2d17a5474fe8de43cb68aaac5a859aa5c8325
Marcus Burghardt <maburgha@redhat.com>
2022-01-10 10:44:07 +0100

    Adjust remediation for pam_faillock rules
    Previously, pam_faillock rules were updated to use authselect and
    authconfig. This improvement, however, conflicted with other pam rules
    not yet updated and caused authselect to fail its integrity check. The
    pam rules remediation were updated to firstly check the integrity of
    the current authselect profile before any change. If the integrity check
    fails, the remediation is aborted in order to preserve intentional
    changes not made throught authselect profiles.

--- linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_passwords_pam_faillock_deny/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_passwords_pam_faillock_deny/bash/shared.sh
@@ -5,31 +5,26 @@
 if [ -f /usr/sbin/authconfig ]; then
     authconfig --enablefaillock --update
 elif [ -f /usr/bin/authselect ]; then
-    if [ $(authselect enable-feature with-faillock) ]; then
+    if authselect check; then
+        authselect enable-feature with-faillock
         authselect apply-changes
     else
-        # If authselect can't manage the pam files, mainly due to manual modifications, the current
-        # profile will be turned back to a inter state, preserving previously enabled features.
-        ENABLED_FEATURES=$(authselect current | tail -n+3 | awk '{ print $2 }')
-        CURRENT_PROFILE=$(authselect current -r | awk '{ print $1 }')
-        authselect select $CURRENT_PROFILE --force
-        for feature in $ENABLED_FEATURES with-faillock; do
-            authselect enable-feature $feature;
-        done
-        authselect apply-changes
+        echo "
+authselect integrity check failed. Remediation aborted!
+This remediation could not be applied because the authselect profile is not integer, probably due to manual edition.
+In cases where the default authselect profile does not cover a specific demand, a custom authselect profile is recommended.
+Where authselect is in place, it is not recommended to manually edit pam files."
+        false
     fi
-else
-    {{{ bash_set_faillock_option("deny", "$var_accounts_passwords_pam_faillock_deny") }}}
 fi
 
-# If the faillock.conf file is present, but for any reason, like an OS upgrade, the pam_faillock.so
-# parameters were not previously defined in faillock.conf, they are ensured now.
 FAILLOCK_CONF="/etc/security/faillock.conf"
-
 if [ -f $FAILLOCK_CONF ]; then
     if $(grep -q '^\s*deny\s*=' $FAILLOCK_CONF); then
         sed -i --follow-symlinks "s/^\s*\(deny\s*\)=.*$/\1 = $var_accounts_passwords_pam_faillock_deny/g" $FAILLOCK_CONF
     else
         echo "deny = $var_accounts_passwords_pam_faillock_deny" >> $FAILLOCK_CONF
     fi
+else
+    {{{ bash_set_faillock_option("deny", "$var_accounts_passwords_pam_faillock_deny") }}}
 fi
