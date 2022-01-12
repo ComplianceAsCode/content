7ef02758042252ff97918df6c297ef2ef02d4b3b
Marcus Burghardt <maburgha@redhat.com>
2022-01-12 05:03:57 +0100

    Update accounts_password_pam_retry rule to use pwquality.conf file
    The retry parameter from pam_pwquality was recently introduced in
    pwquality.conf. Previously, it was only possible to set it directly in
    PAM files. The OVAL, remediation and test scenarios were updated to be
    in alignment with this new option to set the retry parameter.

--- linux_os/guide/system/accounts/accounts-pam/password_quality/password_quality_pwquality/accounts_password_pam_retry/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-pam/password_quality/password_quality_pwquality/accounts_password_pam_retry/bash/shared.sh
@@ -2,16 +2,12 @@
 
 {{{ bash_instantiate_variables("var_password_pam_retry") }}}
 
-if grep -q "retry=" /etc/pam.d/system-auth ; then
-	sed -i --follow-symlinks "s/\(retry *= *\).*/\1$var_password_pam_retry/" /etc/pam.d/system-auth
-else
-	sed -i --follow-symlinks "/pam_pwquality.so/ s/$/ retry=$var_password_pam_retry/" /etc/pam.d/system-auth
-fi
-
 {{% if product in ['rhel8', 'rhel9'] -%}}
-if grep -q "retry=" /etc/pam.d/password-auth ; then
-	sed -i --follow-symlinks "s/\(retry *= *\).*/\1$var_password_pam_retry/" /etc/pam.d/password-auth
-else
-	sed -i --follow-symlinks "/pam_pwquality.so/ s/$/ retry=$var_password_pam_retry/" /etc/pam.d/password-auth
-fi
+	{{{ bash_replace_or_append('/etc/security/pwquality.conf', '^retry', '$var_password_pam_retry', '%s = %s') }}}
+{{% else %}}
+	if grep -q "retry=" /etc/pam.d/system-auth ; then
+		sed -i --follow-symlinks "s/\(retry *= *\).*/\1$var_password_pam_retry/" /etc/pam.d/system-auth
+	else
+		sed -i --follow-symlinks "/pam_pwquality.so/ s/$/ retry=$var_password_pam_retry/" /etc/pam.d/system-auth
+	fi
 {{%- endif %}}
