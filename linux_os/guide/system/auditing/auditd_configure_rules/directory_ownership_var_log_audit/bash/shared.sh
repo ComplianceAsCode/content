5f654d30b8a583109f952b1da220fbce4cb9c1bc
Edgar Aguilar <edgar.aguilar@oracle.com>
2022-01-21 12:15:11 -0600

    Update directory_ownership_var_log_audit for ol8
    Now OVAL passes only if (log_file is set and root owns its directory)
    or (log_file is not set and root owns default directory). Update bash
    accordingly
    
    Signed-off-by: Edgar Aguilar <edgar.aguilar@oracle.com>

--- linux_os/guide/system/auditing/auditd_configure_rules/directory_ownership_var_log_audit/bash/shared.sh
+++ linux_os/guide/system/auditing/auditd_configure_rules/directory_ownership_var_log_audit/bash/shared.sh
@@ -1,3 +1,13 @@
 # platform = multi_platform_all
 
+{{% if product == "ol8" %}}
+if LC_ALL=C grep -iw ^log_file /etc/audit/auditd.conf; then
+    FILE=$(awk -F "=" '/^log_file/ {print $2}' /etc/audit/auditd.conf | tr -d ' ')
+    LOGPATH="$(dirname "$FILE")"
+    chown root $LOGPATH
+else
+    chown root /var/log/audit
+fi
+{{% else %}}
 chown root /var/log/audit
+{{% endif %}}
