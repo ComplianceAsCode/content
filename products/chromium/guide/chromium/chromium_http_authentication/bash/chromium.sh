6106114d1bf8b9086c47dfcdec5021ba9cd5e8c8
Jiri Odehnal <jodehnal@redhat.com>
2022-01-31 17:25:10 +0100

    Fix variable name

--- products/chromium/guide/chromium/chromium_http_authentication/bash/chromium.sh
+++ products/chromium/guide/chromium/chromium_http_authentication/bash/chromium.sh
@@ -3,4 +3,4 @@
 {{{ bash_instantiate_variables("var_auth_schema") }}}
 
 
-{{{ bash_chromium_pol_setting("chrome_stig_policy.json", "/etc/chromium/policies/managed/", "AuthSchemes", '${var_auth_schemes}') }}}
+{{{ bash_chromium_pol_setting("chrome_stig_policy.json", "/etc/chromium/policies/managed/", "AuthSchemes", '${var_auth_schema}') }}}
