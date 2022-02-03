607798a47107b0a09a82aa0f7c934c6789f51a64
Gabriel Becker <ggasparb@redhat.com>
2022-02-03 17:57:18 +0100

    Update configure_bashrc_exec_tmux to consider .d directory.
    This is part of RHEL-08-020041 STIG item and configurations under
    /etc/profile.d/ should be considered as well.

---
+++ linux_os/guide/system/accounts/accounts-physical/screen_locking/console_screen_locking/configure_bashrc_exec_tmux/tests/duplicate_value_multiple_files.fail.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cat >> /etc/profile.d/00-complianceascode.conf <<'EOF'
+if [ "$PS1" ]; then
+  parent=$(ps -o ppid= -p $$)
+  name=$(ps -o comm= -p $parent)
+  case "$name" in sshd|login) exec tmux ;; esac
+fi
+EOF
+
+cat >> /etc/bashrc <<'EOF'
+if [ "$PS1" ]; then
+  parent=$(ps -o ppid= -p $$)
+  name=$(ps -o comm= -p $parent)
+  case "$name" in sshd|login) exec tmux ;; esac
+fi
+EOF
