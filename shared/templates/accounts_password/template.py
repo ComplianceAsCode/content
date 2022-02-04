aae3558d4acf77154e49b37b1bd43ea5f438a08c
Edgar Aguilar <edgar.aguilar@oracle.com>
2022-02-04 12:51:41 -0600

    Introduce nonzero_check to accounts_password
    For OL7 the STIG id OL07-00-010190 needs to check both if
    maxclassrepeat is less than 4 and different to 0. For this reason
    included the variable nonzero_check with default value False this way
    the rule keeps using the template accounts_password without modifying
    other rules behaviour
    
    Signed-off-by: Edgar Aguilar <edgar.aguilar@oracle.com>

--- shared/templates/accounts_password/template.py
+++ shared/templates/accounts_password/template.py
@@ -1,4 +1,7 @@
+from ssg.utils import parse_template_boolean_value
+
 def preprocess(data, lang):
     if lang == "oval":
         data["sign"] = "-?" if data["variable"].endswith("credit") else ""
+    data["nonzero_check"] = parse_template_boolean_value(data, parameter="nonzero_check", default_value=False)
     return data
