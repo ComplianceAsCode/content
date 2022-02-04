40d5b8b5dc0f9a9d7d518210448d9c7e66c993d0
Watson Sato <wsato@redhat.com>
2022-02-04 12:06:45 +0100

    Update rule to handle distributed config
    Based on the template sshd_lineinfile, updated rule sshd_rekey_limit to
    check and remediate SSHD configuration in products that support
    /etc/sshd/sshd_config.d/
    
    The rule cannot use the template as it relies on two external variables.

--- linux_os/guide/services/ssh/ssh_server/sshd_rekey_limit/bash/shared.sh
+++ linux_os/guide/services/ssh/ssh_server/sshd_rekey_limit/bash/shared.sh
@@ -2,4 +2,4 @@
 
 {{{ bash_instantiate_variables("var_rekey_limit_size", "var_rekey_limit_time") }}}
 
-{{{ bash_sshd_config_set(parameter='RekeyLimit', value="$var_rekey_limit_size $var_rekey_limit_time") }}}
+{{{ bash_sshd_remediation(parameter='RekeyLimit', value="$var_rekey_limit_size $var_rekey_limit_time", config_is_distributed=sshd_distributed_config) -}}}
