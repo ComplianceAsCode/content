117bdb6017ea58e7b044d450e1b7d712e6736d03
vincent <wenshen@redhat.com>
2022-02-07 14:29:29 -0800

    OCP fix insecureEdgeTerminationPolicy check
    We should check route insecureEdgeTerminationPolicy is None/Redirect instead of Disabled/Redirect according to: https://github.com/openshift/api/blob/31ffd77a8f025c73b5d19b390af353f9775e7ef1/route/v1/types.go#L270
    
    Related BZ link: https://bugzilla.redhat.com/show_bug.cgi?id=2002695

---
+++ applications/openshift/networking/routes_protected_by_tls/tests/all_route.pass.sh
@@ -0,0 +1,116 @@
+#!/bin/bash
+# remediation = none
+
+mkdir -p /kubernetes-api-resources/apis/route.openshift.io/v1/
+
+cat <<EOF > /kubernetes-api-resources/apis/route.openshift.io/v1/routes?limit=500
+{
+    "apiVersion": "v1",
+    "items": [
+        {
+            "apiVersion": "route.openshift.io/v1",
+            "kind": "Route",
+            "metadata": {
+                "creationTimestamp": "2022-02-07T15:44:41Z",
+                "labels": {
+                    "app": "oauth-openshift"
+                },
+                "name": "oauth-openshift",
+                "namespace": "openshift-authentication",
+                "resourceVersion": "20112",
+                "uid": "f90675f0-6090-4222-8b41-bf32d522d9ac"
+            },
+            "spec": {
+                "host": "oauth-openshift.apps.wenshen-51.devcluster.openshift.com",
+                "port": {
+                    "targetPort": 6443
+                },
+                "tls": {
+                    "insecureEdgeTerminationPolicy": "Redirect",
+                    "termination": "passthrough"
+                },
+                "to": {
+                    "kind": "Service",
+                    "name": "oauth-openshift",
+                    "weight": 100
+                },
+                "wildcardPolicy": "None"
+            },
+            "status": {
+                "ingress": [
+                    {
+                        "conditions": [
+                            {
+                                "lastTransitionTime": "2022-02-07T15:47:15Z",
+                                "status": "True",
+                                "type": "Admitted"
+                            }
+                        ],
+                        "host": "oauth-openshift.apps.wenshen-51.devcluster.openshift.com",
+                        "routerCanonicalHostname": "router-default.apps.wenshen-51.devcluster.openshift.com",
+                        "routerName": "default",
+                        "wildcardPolicy": "None"
+                    }
+                ]
+            }
+        },
+        {
+            "apiVersion": "route.openshift.io/v1",
+            "kind": "Route",
+            "metadata": {
+                "annotations": {
+                    "openshift.io/host.generated": "true"
+                },
+                "creationTimestamp": "2022-02-07T15:48:36Z",
+                "labels": {
+                    "app.kubernetes.io/component": "query-layer",
+                    "app.kubernetes.io/instance": "thanos-querier",
+                    "app.kubernetes.io/name": "thanos-query",
+                    "app.kubernetes.io/version": "0.22.0"
+                },
+                "name": "thanos-querier",
+                "namespace": "openshift-monitoring",
+                "resourceVersion": "22723",
+                "uid": "a9aee465-0939-466a-a187-5d1c3e97d8e9"
+            },
+            "spec": {
+                "host": "thanos-querier-openshift-monitoring.apps.wenshen-51.devcluster.openshift.com",
+                "port": {
+                    "targetPort": "web"
+                },
+                "tls": {
+                    "insecureEdgeTerminationPolicy": "Redirect",
+                    "termination": "reencrypt"
+                },
+                "to": {
+                    "kind": "Service",
+                    "name": "thanos-querier",
+                    "weight": 100
+                },
+                "wildcardPolicy": "None"
+            },
+            "status": {
+                "ingress": [
+                    {
+                        "conditions": [
+                            {
+                                "lastTransitionTime": "2022-02-07T15:48:36Z",
+                                "status": "True",
+                                "type": "Admitted"
+                            }
+                        ],
+                        "host": "thanos-querier-openshift-monitoring.apps.wenshen-51.devcluster.openshift.com",
+                        "routerCanonicalHostname": "router-default.apps.wenshen-51.devcluster.openshift.com",
+                        "routerName": "default",
+                        "wildcardPolicy": "None"
+                    }
+                ]
+            }
+        }
+    ],
+    "kind": "List",
+    "metadata": {
+        "resourceVersion": "",
+        "selfLink": ""
+    }
+}
