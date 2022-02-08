1acfe8c256c171a44eb4395904a9b3dd78483302
Jiri Odehnal <jodehnal@redhat.com>
2022-02-08 13:55:11 +0100

    Use new jinja macro instead of repetitive remediation scripts

--- products/chromium/guide/chromium/chromium_enable_approved_plugins/bash/chromium.sh
+++ products/chromium/guide/chromium/chromium_enable_approved_plugins/bash/chromium.sh
@@ -2,15 +2,5 @@
 
 {{{ bash_instantiate_variables("var_enable_approved_plugins") }}}
 
-CHROME_POL_FILE="chrome_stig_policy.json"
-CHROME_POL_DIR="/etc/chromium/policies/managed/"
-POL_SETTING="EnabledPlugins"
-POL_SETTING_VAL=$(echo ${var_enable_approved_plugins} | sed 's/\//\\\/\\/')
 
-grep -q ${POL_SETTING} ${CHROME_POL_DIR}/${CHROME_POL_FILE}
-
-if ! [ $? -eq 0 ] ; then
-   sed -i -e '/{/a \  "'${POL_SETTING}'": \['${var_enable_approved_plugins}'\],' ${CHROME_POL_DIR}/${CHROME_POL_FILE}
-else
-   sed -i -e 's/\"'${POL_SETTING}'\".*/\"'${POL_SETTING}'\": \['${POL_SETTING_VAL}'\],/g' ${CHROME_POL_DIR}/${CHROME_POL_FILE}
-fi
+{{{ bash_chromium_pol_setting("chrome_stig_policy.json", "/etc/chromium/policies/managed/", "EnabledPlugins", "$(echo ${var_enable_approved_plugins} | sed 's/\//\\\/\\/')", "${var_enable_approved_plugins}") }}}
--- products/chromium/guide/chromium/chromium_http_authentication/bash/chromium.sh
+++ products/chromium/guide/chromium/chromium_http_authentication/bash/chromium.sh
@@ -2,14 +2,5 @@
 
 {{{ bash_instantiate_variables("var_auth_schema") }}}
 
-CHROME_POL_FILE="chrome_stig_policy.json"
-CHROME_POL_DIR="/etc/chromium/policies/managed/"
-POL_SETTING="AuthSchemes"
 
-grep -q ${POL_SETTING} ${CHROME_POL_DIR}/${CHROME_POL_FILE}
-
-if ! [ $? -eq 0 ] ; then
-   sed -i -e '/{/a \  "'${POL_SETTING}'": "'${var_auth_schemes}'",' ${CHROME_POL_DIR}/${CHROME_POL_FILE}
-else
-   sed -i -e 's/\"'${POL_SETTING}'.*/\"'${POL_SETTING}'\": \"'${var_auth_schemes}'\",/g' ${CHROME_POL_DIR}/${CHROME_POL_FILE}
-fi
+{{{ bash_chromium_pol_setting("chrome_stig_policy.json", "/etc/chromium/policies/managed/", "AuthSchemes", '${var_auth_schemes}') }}}
