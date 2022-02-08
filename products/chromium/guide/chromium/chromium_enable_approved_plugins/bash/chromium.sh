1aa9ffa318cb0d6bbc82b76a08fed9483a4682d5
Jiri Odehnal <jodehnal@redhat.com>
2022-02-08 18:38:12 +0100

    Add brackets to remediations, solve CI error by changing macro

--- products/chromium/guide/chromium/chromium_enable_approved_plugins/bash/chromium.sh
+++ products/chromium/guide/chromium/chromium_enable_approved_plugins/bash/chromium.sh
@@ -2,6 +2,11 @@
 
 {{{ bash_instantiate_variables("var_enable_approved_plugins") }}}
 
+# set var to empty string if None
+if [ "$var_enable_approved_plugins" = "None" ]; then
+    var_enable_approved_plugins=""
+fi
+
 var_enable_approved_plugins_modified="$(echo ${var_enable_approved_plugins} | sed 's/\//\\\/\\/')"
 
-{{{ bash_chromium_pol_setting("chrome_stig_policy.json", "/etc/chromium/policies/managed/", "EnabledPlugins", "${var_enable_approved_plugins_modified}", "${var_enable_approved_plugins}") }}}
+{{{ bash_chromium_pol_setting("chrome_stig_policy.json", "/etc/chromium/policies/managed/", "EnabledPlugins", "\[${var_enable_approved_plugins_modified}\]", "\[${var_enable_approved_plugins}\]") }}}
