652c80b0f537d8dde1d77cf5b8df0a5f330a7b06
Jiri Odehnal <jodehnal@redhat.com>
2022-02-08 13:55:11 +0100

    Use new jinja macro instead of repetitive remediation scripts

--- products/chromium/guide/chromium/chromium_disable_protocol_schemas/bash/chromium.sh
+++ products/chromium/guide/chromium/chromium_disable_protocol_schemas/bash/chromium.sh
@@ -2,15 +2,5 @@
 
 {{{ bash_instantiate_variables("var_url_blacklist") }}}
 
-CHROME_POL_FILE="chrome_stig_policy.json"
-CHROME_POL_DIR="/etc/chromium/policies/managed/"
-POL_SETTING="URLBlacklist"
-POL_SETTING_VAL=$(echo ${var_url_blacklist}| sed 's/\//\\\/\\/')
 
-grep -q ${POL_SETTING} ${CHROME_POL_DIR}/${CHROME_POL_FILE}
-
-if ! [ $? -eq 0 ] ; then
-   sed -i -e '/{/a \  "'${POL_SETTING}'": \["'${var_url_blacklist}'"\],' ${CHROME_POL_DIR}/${CHROME_POL_FILE}
-else
-   sed -i -e 's/\"'${POL_SETTING}'.*/\"'${POL_SETTING}'\": \[\"'${POL_SETTING_VAL}'\"\],/g' ${CHROME_POL_DIR}/${CHROME_POL_FILE}
-fi
+{{{ bash_chromium_pol_setting("chrome_stig_policy.json", "/etc/chromium/policies/managed/", "URLBlacklist", "$(echo ${var_url_blacklist}| sed 's/\//\\\/\\/')", "${var_url_blacklist}") }}}
