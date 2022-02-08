4073f172d782f7bb04ca3f08030a4f61f83697ba
Matthew Burket <mburket@redhat.com>
2022-02-08 11:39:53 -0600

    Refactor arg_audit

--- utils/srg_audit.py
+++ utils/srg_audit.py
@@ -43,16 +43,7 @@ def parse_args() -> argparse.Namespace:
     return parser.parse_args()
 
 
-def main():
-    args = parse_args()
-    if not os.path.exists(args.file):
-        sys.stderr.write(f"Unable to perform audit.\n")
-        sys.stderr.write(f"File not found: {args.file}\n")
-        exit(1)
-    if not os.path.isfile(args.file):
-        sys.stderr.write(f"Unable to perform audit.\n")
-        sys.stderr.write(f"Input must be a file: {args.file}\n")
-        exit(2)
+def create_reduction_dicts() -> (dict, dict, dict, dict):
     # The goal these dictionaries is to reduce all values the dictionary to zero,
     # save optional values.
     #
@@ -90,7 +81,6 @@ def main():
                      'Status Justification': 1,
                      'CCI': 1,
                      'SRGID': 1}
-
     not_applicable = {'Requirement': 1,
                       'VulDiscussion': 0,
                       'Status': 1,
@@ -102,48 +92,21 @@ def main():
                       'Status Justification': 1,
                       'CCI': 1,
                       'SRGID': 1}
-    with open(args.file, 'r') as f:
-        reader = csv.DictReader(f)
-        errors = dict()
-        for row in reader:
-            if row['SRGID'] not in errors:
-                errors[row['SRGID']] = list()
-            if not row['Requirement']:
-                errors[row['SRGID']].append(Problems.MISSING_REQUIREMENT.value)
-            req_bad_chars = ['\"', "&", "'", "<", ">"]
-            if has_element(req_bad_chars, row['Requirement']):
-                errors[row['SRGID']].append(Problems.REQUIREMENT_HAS_INVALID_CHARS.value)
-            if row['Status'] == DisaStatus.AUTOMATED:
-                results = config_dict.copy()
-                validate_check_fix(errors, row, row['SRGID'])
-            elif row['Status'] == DisaStatus.INHERENTLY_MET:
-                results = meets_dict.copy()
-            elif row['Status'] == DisaStatus.DOES_NOT_MEET:
-                results = does_not_meet.copy()
-            elif row['Status'] == DisaStatus.NOT_APPLICABLE:
-                results = not_applicable.copy()
-            else:
-                errors[row['SRGID']].append(Problems.INVALID_STATUS.value)
-                continue
-            for col in row:
-                col_value = row[col].strip()
-                if col_value != '' and col in results:
-                    results[col] -= 1
-            for result in results:
-                if results[result] == 1:
-                    errors[row["SRGID"]].append(f'Field {result} is not defined.')
-                elif results[result] == -1:
-                    errors[row["SRGID"]].append(f'Field {result} is defined and should not be.')
-                elif results[result] not in [0, 2, 3]:
-                    raise AttributeError(f"Config data is not defined correctly. "
-                                         f"Result = {result}, {results[result]}")
-        for result in errors:
-            print(result)
-            for error in errors[str(result)]:
-                print(f'\t{error}')
+    return config_dict, does_not_meet, meets_dict, not_applicable
 
 
-def validate_check_fix(results, row, row_id):
+def check_paths(file: str) -> None:
+    if not os.path.exists(file):
+        sys.stderr.write(f"Unable to perform audit.\n")
+        sys.stderr.write(f"File not found: {file}\n")
+        exit(1)
+    if not os.path.isfile(file):
+        sys.stderr.write(f"Unable to perform audit.\n")
+        sys.stderr.write(f"Input must be a file: {file}\n")
+        exit(2)
+
+
+def validate_check_fix(results: dict, row: dict, row_id: str) -> None:
     check_invalid = ['shall', 'should', 'please']
     check_invalid_start = ['ensure', 'interview']
     if has_element(check_invalid, row['Fix']):
@@ -156,6 +119,76 @@ def validate_check_fix(results, row, row_id):
         results[row_id].append(Problems.FIX_BAD_START.value)
     if 'this is a finding.' not in row['Check']:
         results[row_id].append(Problems.CHECK_IS_A_FIND.value)
+
+
+def get_results_dict(row: dict) -> dict:
+    config_dict, does_not_meet, meets_dict, not_applicable = create_reduction_dicts()
+    if row['Status'] == DisaStatus.AUTOMATED:
+        results = config_dict.copy()
+    elif row['Status'] == DisaStatus.INHERENTLY_MET:
+        results = meets_dict.copy()
+    elif row['Status'] == DisaStatus.DOES_NOT_MEET:
+        results = does_not_meet.copy()
+    elif row['Status'] == DisaStatus.NOT_APPLICABLE:
+        results = not_applicable.copy()
+    else:
+        results = None
+    return results
+
+
+def process_row_results(errors: dict, results: dict, row: dict) -> None:
+    for result in results:
+        if results[result] == 1:
+            errors[row["SRGID"]].append(f'Field {result} is not defined.')
+        elif results[result] == -1:
+            errors[row["SRGID"]].append(f'Field {result} is defined and should not be.')
+        elif results[result] not in [0, 2, 3]:
+            raise AttributeError(f"Config data is not defined correctly. "
+                                 f"Result = {result}, {results[result]}")
+
+
+def validate_base_rows(errors: dict, row: dict) -> None:
+    if row['SRGID'] not in errors:
+        errors[row['SRGID']] = list()
+    if not row['Requirement']:
+        errors[row['SRGID']].append(Problems.MISSING_REQUIREMENT.value)
+    req_bad_chars = ['\"', "&", "'", "<", ">"]
+    if has_element(req_bad_chars, row['Requirement']):
+        errors[row['SRGID']].append(Problems.REQUIREMENT_HAS_INVALID_CHARS.value)
+
+
+def set_blank_rows_results(results: dict, row: dict) -> None:
+    for col in row:
+        col_value = row[col].strip()
+        if col_value != '' and col in results:
+            results[col] -= 1
+
+
+def print_results(errors: dict) -> None:
+    for result in errors:
+        print(result)
+        for error in errors[str(result)]:
+            print(f'\t{error}')
+
+
+def main():
+    args = parse_args()
+    check_paths(args.file)
+
+    with open(args.file, 'r') as f:
+        reader = csv.DictReader(f)
+        errors = dict()
+        for row in reader:
+            validate_base_rows(errors, row)
+            results = get_results_dict(row)
+            if row['Status'] == DisaStatus.AUTOMATED:
+                validate_check_fix(errors, row, row['SRGID'])
+            if row['Status'] not in DisaStatus.STATUSES:
+                errors[row['SRGID']].append(Problems.INVALID_STATUS.value)
+                continue
+            set_blank_rows_results(results, row)
+            process_row_results(errors, results, row)
+        print_results(errors)
 
 
 if __name__ == '__main__':
