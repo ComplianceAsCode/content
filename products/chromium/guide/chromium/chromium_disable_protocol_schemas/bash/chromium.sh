61bd14f046e42cf09c65ce95afa0fe9485c8a7aa
Jiri Odehnal <jodehnal@redhat.com>
2022-02-08 13:52:05 +0100

    Fix formatting, readability and add changes from #8032

--- products/chromium/guide/chromium/chromium_disable_protocol_schemas/bash/chromium.sh
+++ products/chromium/guide/chromium/chromium_disable_protocol_schemas/bash/chromium.sh
@@ -2,5 +2,6 @@
 
 {{{ bash_instantiate_variables("var_url_blacklist") }}}
 
+var_url_blacklist_modified="$(echo ${var_url_blacklist}| sed 's/\//\\\/\\/')"
 
-{{{ bash_chromium_pol_setting("chrome_stig_policy.json", "/etc/chromium/policies/managed/", "URLBlacklist", "$(echo ${var_url_blacklist}| sed 's/\//\\\/\\/')", "${var_url_blacklist}") }}}
+{{{ bash_chromium_pol_setting("chrome_stig_policy.json", "/etc/chromium/policies/managed/", "URLBlacklist", "${var_url_blacklist_modified}", "${var_url_blacklist}") }}}
--- products/chromium/guide/chromium/chromium_extension_whitelist/bash/chromium.sh
+++ products/chromium/guide/chromium/chromium_extension_whitelist/bash/chromium.sh
@@ -2,5 +2,6 @@
 
 {{{ bash_instantiate_variables("var_extension_whitelist") }}}
 
+var_extension_whitelist_modified="$(echo ${var_extension_whitelist} | sed 's/\//\\\/\\/')"
 
-{{{ bash_chromium_pol_setting("chrome_stig_policy.json", "/etc/chromium/policies/managed/", "ExtensionInstallWhitelist", "$(echo ${var_extension_whitelist} | sed 's/\//\\\/\\/')", "${var_extension_whitelist}") }}}
+{{{ bash_chromium_pol_setting("chrome_stig_policy.json", "/etc/chromium/policies/managed/", "ExtensionInstallWhitelist", "${var_extension_whitelist_modified}", "${var_extension_whitelist}") }}}
