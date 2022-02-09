45e33030ee5117bd2bf2f6a1324ada953f1d3b76
Gabriel Becker <ggasparb@redhat.com>
2022-02-09 17:09:27 +0100

    Update template accounts_password with single parameter for zero cmp.

--- shared/templates/accounts_password/template.py
+++ shared/templates/accounts_password/template.py
@@ -3,6 +3,5 @@ from ssg.utils import parse_template_boolean_value
 def preprocess(data, lang):
     if lang == "oval":
         data["sign"] = "-?" if data["variable"].endswith("credit") else ""
-    data["check_zero_boundary"] = parse_template_boolean_value(data, parameter="check_zero_boundary", default_value=False)
-    data["zero_boundary_operator"] = data.get("zero_boundary_operator", "greater than")
+    data["zero_comparison_operation"] = data.get("zero_comparison_operation", None)
     return data
