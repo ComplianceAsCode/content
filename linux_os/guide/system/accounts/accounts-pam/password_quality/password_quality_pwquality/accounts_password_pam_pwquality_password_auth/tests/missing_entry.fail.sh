5f45892e45418d8e7b737eb72275008fe84ca709
Marcus Burghardt <maburgha@redhat.com>
2022-02-09 17:22:19 +0100

    Add rule accounts_password_pam_pwquality_password_auth
    STIG ID RHEL-08-020100

---
+++ linux_os/guide/system/accounts/accounts-pam/password_quality/password_quality_pwquality/accounts_password_pam_pwquality_password_auth/tests/missing_entry.fail.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# platform = Red Hat Enterprise Linux 7,Red Hat Virtualization 4,multi_platform_fedora
+# packages = pam
+
+config_file=/etc/pam.d/password-auth
+
+sed -i --follow-symlinks '/^password\s*requisite\s*pam_pwquality\.so/d' $config_file
