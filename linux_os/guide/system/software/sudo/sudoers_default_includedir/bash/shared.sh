3686fe72a6e27049f1c46d0a4efa07e1b42b6a20
Watson Sato <wsato@redhat.com>
2022-02-10 17:26:59 +0100

    Add test and fix for case when the single includedir is wrong

--- linux_os/guide/system/software/sudo/sudoers_default_includedir/bash/shared.sh
+++ linux_os/guide/system/software/sudo/sudoers_default_includedir/bash/shared.sh
@@ -5,7 +5,10 @@ sudoers_includedir_count=$(grep -c "#includedir" "$sudoers_config_file")
 if [ "$sudoers_includedir_count" -gt 1 ]; then
     sed -i "/#includedir.*/d" "$sudoers_config_file"
     echo "#includedir /etc/sudoers.d" >> "$sudoers_config_file"
-fi
-if [ "$sudoers_includedir_count" -eq 0 ]; then
+elif [ "$sudoers_includedir_count" -eq 0 ]; then
     echo "#includedir /etc/sudoers.d" >> "$sudoers_config_file"
+else
+    if ! grep -q "^#includedir /etc/sudoers.d" /etc/sudoers; then
+        sed -i "s|^#includedir.*|#includedir /etc/sudoers.d|g" /etc/sudoers
+    fi
 fi
