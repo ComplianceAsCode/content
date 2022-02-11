ac142511952ec8926df53c88a345ae1ba690bbb5
Jiri Odehnal <jodehnal@redhat.com>
2022-02-11 16:45:20 +0100

    Add new sssd config macro and use it in remediations; fix comments

--- linux_os/guide/services/sssd/sssd_run_as_sssd_user/bash/shared.sh
+++ linux_os/guide/services/sssd/sssd_run_as_sssd_user/bash/shared.sh
@@ -1,24 +1,3 @@
 # platform = multi_platform_rhel,multi_platform_fedora,multi_platform_ol
 
-found=false
-for f in /etc/sssd/sssd.conf /etc/sssd/conf.d/*.conf; do
-	if [ ! -e "$f" ]; then
-		continue
-	fi
-	user=$(awk '/^\s*\[/{f=0} /^\s*\[sssd\]/{f=1} f{nu=gensub("^\\s*user\\s*=\\s*(\\S+).*","\\1",1); if($0!=nu){user=nu}} END{print user}' "$f")
-	if [ -n "$user" ] ; then
-		if [ "$user" != sssd ] ; then
-			sed -i 's/^\s*user\s*=.*/user = sssd/' "$f"
-		fi
-		found=true
-	fi
-done
-
-if ! $found ; then
-	SSSD_CONF="/etc/sssd/conf.d/ospp.conf"
-	mkdir -p "$(dirname "$SSSD_CONF")"
-	touch "$SSSD_CONF"
-	chown root:root "$SSSD_CONF"
-	chmod 600 "$SSSD_CONF"
-	echo -e "[sssd]\nuser = sssd" >> "$SSSD_CONF"
-fi
+{{{ bash_sssd_set_option_new("sssd", "ospp", "user", "sssd") }}}
