8f4a8eff21a60ccb049b873d4220ab8aebb7b655
Watson Sato <wsato@redhat.com>
2022-02-14 14:50:11 +0100

    Add tests for sudo file and dir includes in /etc/sudoers.d

---
+++ linux_os/guide/system/software/sudo/sudoers_default_includedir/tests/sudoers.d_with_include.fail.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# platform = multi_platform_all
+
+# Ensure default config is there
+if ! grep -q "#includedir /etc/sudoers.d" /etc/sudoers; then
+    echo "#includedir /etc/sudoers.d" >> /etc/sudoers
+fi
+
+echo "#include /etc/my-sudoers" > /etc/sudoers.d/my-sudoers
