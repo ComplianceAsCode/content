d91e3eefe6c265c27634cb15b0f276a298f81645
Watson Sato <wsato@redhat.com>
2022-02-14 14:59:18 +0100

    Update rule catch and remove other sudo includes
    Any other #include or #includedir besides:
    "/etc/sudoers: #includedir /etc/sudoers.d" should be removed.

--- linux_os/guide/system/software/sudo/sudoers_default_includedir/bash/shared.sh
+++ linux_os/guide/system/software/sudo/sudoers_default_includedir/bash/shared.sh
@@ -1,6 +1,7 @@
 # platform = multi_platform_all
 
 sudoers_config_file="/etc/sudoers"
+sudoers_config_dir="/etc/sudoers.d"
 sudoers_includedir_count=$(grep -c "#includedir" "$sudoers_config_file")
 if [ "$sudoers_includedir_count" -gt 1 ]; then
     sed -i "/#includedir.*/d" "$sudoers_config_file"
@@ -8,7 +9,9 @@ if [ "$sudoers_includedir_count" -gt 1 ]; then
 elif [ "$sudoers_includedir_count" -eq 0 ]; then
     echo "#includedir /etc/sudoers.d" >> "$sudoers_config_file"
 else
-    if ! grep -q "^#includedir /etc/sudoers.d" /etc/sudoers; then
-        sed -i "s|^#includedir.*|#includedir /etc/sudoers.d|g" /etc/sudoers
+    if ! grep -q "^#includedir /etc/sudoers.d" "$sudoers_config_file"; then
+        sed -i "s|^#includedir.*|#includedir /etc/sudoers.d|g" "$sudoers_config_file"
     fi
 fi
+sed -i "/^#include\s\+.*/d" "$sudoers_config_file" "${sudoers_config_dir}"/*
+sed -i "/^#includedir\s\+.*/d" "${sudoers_config_dir}"/*
