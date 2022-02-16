b5439aec6425ed809fca2204360829c0ba91b96c
anivan-suse <anastasija.ivanovic@suse.com>
2022-02-16 23:41:36 +0100

    Update fix

--- linux_os/guide/system/accounts/accounts-pam/set_password_hashing_algorithm/set_password_hashing_algorithm_systemauth/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-pam/set_password_hashing_algorithm/set_password_hashing_algorithm_systemauth/bash/shared.sh
@@ -1,13 +1,20 @@
-# platform = multi_platform_wrlinux,multi_platform_rhel,multi_platform_fedora,multi_platform_ol,multi_platform_rhv
+# platform = multi_platform_all
 
 AUTH_FILES[0]="/etc/pam.d/system-auth"
 {{%- if product == "rhel7" %}}
 AUTH_FILES[1]="/etc/pam.d/password-auth"
 {{%- endif %}}
 
+{{% if product in ["sle15", "sle12"] -%}}
+if ! grep -q "^password.*required.*pam_unix.so.*sha512" /etc/pam.d/common-password ; then
+   sed -i --follow-symlinks "/^password.*required.*pam_unix.so/ s/$/ sha512/" /etc/pam.d/common-password
+fi
+{{%- else -%}}
 for pamFile in "${AUTH_FILES[@]}"
 do
 	if ! grep -q "^password.*sufficient.*pam_unix.so.*sha512" $pamFile; then
 		sed -i --follow-symlinks "/^password.*sufficient.*pam_unix.so/ s/$/ sha512/" $pamFile
 	fi
 done
+{{%- endif %}}
+
