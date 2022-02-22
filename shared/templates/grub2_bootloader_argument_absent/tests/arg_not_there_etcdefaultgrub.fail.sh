49c835ae37b95bf0646a6c7aa3935f71927deba8
Watson Sato <wsato@redhat.com>
2022-02-22 17:52:45 +0100

    copy grub2_template_argument as grub2_template_argument_absent

---
+++ shared/templates/grub2_bootloader_argument_absent/tests/arg_not_there_etcdefaultgrub.fail.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# platform = Red Hat Enterprise Linux 7,Red Hat Enterprise Linux 8,Red Hat Enterprise Linux 9
+
+# Removes argument from kernel command line in /etc/default/grub
+if grep -q '^GRUB_CMDLINE_LINUX=.*{{{ARG_NAME}}}=.*"'  '/etc/default/grub' ; then
+	sed -i 's/\(^GRUB_CMDLINE_LINUX=".*\){{{ARG_NAME}}}=[^[:space:]]*\(.*"\)/\1 \2/'  '/etc/default/grub'
+fi
+
