bb4080924ac6934c499218082d27d5c9ee915173
Matěj Týč <matyc@redhat.com>
2022-02-23 10:33:30 +0100

    Add quoting
    and improve formatting

--- linux_os/guide/system/network/network-firewalld/ruleset_modifications/configure_firewalld_ports/bash/shared.sh
+++ linux_os/guide/system/network/network-firewalld/ruleset_modifications/configure_firewalld_ports/bash/shared.sh
@@ -4,9 +4,7 @@
 # strategy = configure
 # disruption = low
 
-
 {{{ bash_package_install("firewalld") }}}
-
 
 {{{ bash_instantiate_variables("firewalld_sshd_zone") }}}
 
@@ -17,19 +15,19 @@
 {{% endif %}}
 
 # This assumes that firewalld_sshd_zone is one of the pre-defined zones
-if [ ! -f /etc/firewalld/zones/${firewalld_sshd_zone}.xml ]; then
-    cp /usr/lib/firewalld/zones/${firewalld_sshd_zone}.xml /etc/firewalld/zones/${firewalld_sshd_zone}.xml
+if [ ! -f "/etc/firewalld/zones/${firewalld_sshd_zone}.xml" ]; then
+    cp "/usr/lib/firewalld/zones/${firewalld_sshd_zone}.xml" "/etc/firewalld/zones/${firewalld_sshd_zone}.xml"
 fi
-if ! grep -q 'service name="ssh"' /etc/firewalld/zones/${firewalld_sshd_zone}.xml; then
+if ! grep -q 'service name="ssh"' "/etc/firewalld/zones/${firewalld_sshd_zone}.xml"; then
     sed -i '/<\/description>/a \
-  <service name="ssh"/>' /etc/firewalld/zones/${firewalld_sshd_zone}.xml
+  <service name="ssh"/>' "/etc/firewalld/zones/${firewalld_sshd_zone}.xml"
 fi
 
 # Check if any eth interface is bounded to the zone with SSH service enabled
 nic_bound=false
 readarray -t eth_interface_list < <(ip link show up | cut -d ' ' -f2 | cut -d ':' -s -f1 | grep -E '^(en|eth)')
 for interface in "${eth_interface_list[@]}"; do
-    if grep -qi "ZONE=$firewalld_sshd_zone" {{{ network_config_path }}}; then
+    if grep -qi "ZONE=$firewalld_sshd_zone" "{{{ network_config_path }}}"; then
         nic_bound=true
         break;
     fi
@@ -49,7 +47,7 @@ if [ $nic_bound = false ];then
         # If firewalld service is running, we need to do this step with firewall-cmd
         # Otherwise firewalld will communicate with NetworkManage and will revert assigned zone
         # of NetworkManager managed interfaces upon reload
-        firewall-cmd --permanent --zone=$firewalld_sshd_zone --add-interface=${eth_interface_list[0]}
+        firewall-cmd --permanent --zone="$firewalld_sshd_zone" --add-interface="${eth_interface_list[0]}"
         firewall-cmd --reload
     fi
 fi
