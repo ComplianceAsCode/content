8491f2c57d4dcdcf1041c18d23a884c71148a5d0
Watson Sato <wsato@redhat.com>
2022-02-23 13:49:58 +0100

    Ensure test scenarios work on containers too
    Ensure that the test scenarios setup the necessary assets, like
    /etc/default/grub and the grubby utility.

--- shared/templates/grub2_bootloader_argument_absent/tests/mising_arg_invalid_rescue.pass.sh
+++ shared/templates/grub2_bootloader_argument_absent/tests/mising_arg_invalid_rescue.pass.sh
@@ -4,4 +4,5 @@
 # Ensure the kernel command line for each installed kernel in the bootloader
 grubby --update-kernel=ALL --remove-args="{{{ ARG_NAME }}}"
 
+mkdir -p /boot/loader/entries
 echo "I am an invalid boot entry, but nobody should care, because I am rescue" > /boot/loader/entries/trololol-rescue.conf
