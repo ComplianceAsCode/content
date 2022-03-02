86d8b563d8cbfdfe1149ac35734bedb7882f06c2
anivan-suse <anastasija.ivanovic@suse.com>
2022-03-02 12:50:07 +0100

    Remove sle files

--- linux_os/guide/system/accounts/accounts-pam/set_password_hashing_algorithm/set_password_hashing_algorithm_systemauth/bash/sle12.sh
+++
@@ -1,5 +0,0 @@
-# platform = multi_platform_sle
-
-if ! grep -q "^password.*required.*pam_unix.so.*sha512" /etc/pam.d/common-password ; then
-   sed -i --follow-symlinks "/^password.*required.*pam_unix.so/ s/$/ sha512/" /etc/pam.d/common-password
-fi
