3cf860ad1560a215de6a7b432542747216f2a5c6
sampsone <esampson@suse.com>
2022-03-02 15:47:27 -0600

    fix aide_build_database rule and remediation to work with sles 12 and 15
    There were a number of issues:
    oval/shared.xml
    assumed aide.conf had:
    @@define DBDIR /path
    database_out=file:@@{DBDIR}/filename
    database=file:@@{DBDIR}/filename
    SLE 12 and 15 does not use: :@@{DBDIR}
    
    in remediation bash was not enabled for sle
    and sle uses /var/lib/aide/aide.db.new and /var/lib/aide/aide.db not
    /var/lib/aide/aide.db.new.gz and /var/lib/aide/aide.db.gz
    
    and path to aide is /usr/bin/aide as oppsed to /usr/sbin/aide --init
    
    in the rule.yml
    Need to make sle aware for file name and path differences.

--- linux_os/guide/system/software/integrity/software-integrity/aide/aide_build_database/tests/db_malformed.fail.sh
+++ linux_os/guide/system/software/integrity/software-integrity/aide/aide_build_database/tests/db_malformed.fail.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 # packages = aide
 
+{{% if 'ubuntu' in product or 'sle' in product %}}
+DB=/var/lib/aide/aide.db
+{{% else %}}
 DB=/var/lib/aide/aide.db.gz
+{{% endif %}}
 
 rm -rf $DB
 echo "not really a database" > $DB
+
--- linux_os/guide/system/software/integrity/software-integrity/aide/aide_build_database/tests/db_not_present.fail.sh
+++ linux_os/guide/system/software/integrity/software-integrity/aide/aide_build_database/tests/db_not_present.fail.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # packages = aide
 
+{{% if 'ubuntu' in product or 'sle' in product %}}
+DB=/var/lib/aide/aide.db
+{{% else %}}
 DB=/var/lib/aide/aide.db.gz
+{{% endif %}}
 
 rm -rf $DB
