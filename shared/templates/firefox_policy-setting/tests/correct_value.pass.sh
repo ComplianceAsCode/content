443f963058c1cf0d51b35b0d2b02164c2d1f03d6
Lenox, Joseph D (Collins) <Joseph.Lenox@collins.com>
2022-03-03 13:51:05 -0600

    Add tests for the policy set

---
+++ shared/templates/firefox_policy-setting/tests/correct_value.pass.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+firefox_cfg="distribution/policies.json"
+firefox_dirs="/usr/lib/firefox /usr/lib64/firefox /usr/local/lib/firefox /usr/local/lib64/firefox"
+
+# Iterate over the possible Firefox install directories
+for firefox_dir in ${firefox_dirs}; do
+    # write our bad config in every location to ensure remediation fixes it.
+    mkdir -p ${firefox_dir}
+    touch ${firefox_dir}/${firefox_cfg}
+    echo """
+import json
+_file=open('${firefox_dir}/${firefox_cfg}', 'rb')
+_tree=json.load(_file)
+_file.close()
+{{% for policy_item in POLICIES %}}
+{{% for p in policy_item.subpath %}}
+if '{{{ p }}}' in _tree{{{ policy_item.path_python[loop.revindex] }}}:
+   pass
+else:
+   _tree{{{ policy_item.path_python[loop.revindex0] }}} = dict()
+{{% endfor %}}
+# Set the new value to the correct one
+_tree{{{ policy_item.path_python[0] }}}['{{{ policy_item.parameter }}}'] = {{{ policy_item.value_escaped }}}
+{{% endfor %}}
+_file=open('${firefox_dir}/${firefox_cfg}', 'wb')
+json.dump(_tree, _file, indent=4, sort_keys=True)
+_file.close()
+""" | python
+done
