f7ea9774cba6ff20abf9edd5c31934777ab64399
Watson Sato <wsato@redhat.com>
2022-03-03 16:32:12 +0100

    Add variable handling in templated test scenario
    The the rule uses a variable, the `correct_value.pass.sh` will overwrite
    the varaible's default value to "correct_value" and also setup the test
    using "correct_value".

--- shared/templates/grub2_bootloader_argument/tests/correct_value.pass.sh
+++ shared/templates/grub2_bootloader_argument/tests/correct_value.pass.sh
@@ -1,3 +1,7 @@
 # platform = multi_platform_all
+{{%- if ARG_VARIABLE %}}
+# variables = {{{ ARG_VARIABLE }}}=correct_value
+{{%- set ARG_NAME_VALUE= ARG_NAME ~ "=correct_value" %}}
+{{%- endif %}}
 
 {{{ grub2_bootloader_argument_remediation(ARG_NAME, ARG_NAME_VALUE) }}}
