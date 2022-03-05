04f330df9b85a9488e080186c3acc9b07c831a34
Jiri Odehnal <jodehnal@redhat.com>
2022-03-05 18:41:36 +0100

    Add bash_ensure_ini_config macro I realized that having two different macros for sssd was unnecessary, and so I replaced both of them with a general-purpose macro bash_ensure_ini_config, which works with any ini-style config and combines the functionality of both macros that it replaces. I moved chown and chmod commands that were used on the output config file from the macro as it is an edge case and causes issues with CI and BATS tests. If the resulting config file must have different owner or permissions, this must be addressed in the specific remediation script where the macro is invoked.

--- linux_os/guide/services/sssd/sssd_certificate_verification/bash/shared.sh
+++ linux_os/guide/services/sssd/sssd_certificate_verification/bash/shared.sh
@@ -6,4 +6,11 @@
 
 {{{ bash_instantiate_variables("var_sssd_certificate_verification_digest_function") }}}
 
-{{{ bash_sssd_set_option_new("[sssd]", "certificate_verification", "certificate_verification", "ocsp_dgst = $var_sssd_certificate_verification_digest_function") }}}
+MAIN_CONF="/etc/sssd/conf.d/certificate_verification.conf"
+
+{{{ bash_ensure_ini_config("$MAIN_CONF /etc/sssd/sssd.conf /etc/sssd/conf.d/*.conf", "sssd", "certificate_verification", "ocsp_dgst = $var_sssd_certificate_verification_digest_function") }}}
+
+if [ ! -e "$MAIN_CONF" ]; then
+    chown root:root "$MAIN_CONF"
+	chmod 600 "$MAIN_CONF"
+fi
