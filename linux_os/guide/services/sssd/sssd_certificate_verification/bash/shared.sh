98f565c7b2caceeab3f46a662468a2a9f7e3c67d
Jiri Odehnal <jodehnal@redhat.com>
2022-03-05 19:37:48 +0100

    Fix condition logic, CI shellcheck issue and add BATS tests

--- linux_os/guide/services/sssd/sssd_certificate_verification/bash/shared.sh
+++ linux_os/guide/services/sssd/sssd_certificate_verification/bash/shared.sh
@@ -10,7 +10,7 @@ MAIN_CONF="/etc/sssd/conf.d/certificate_verification.conf"
 
 {{{ bash_ensure_ini_config("$MAIN_CONF /etc/sssd/sssd.conf /etc/sssd/conf.d/*.conf", "sssd", "certificate_verification", "ocsp_dgst = $var_sssd_certificate_verification_digest_function") }}}
 
-if [ ! -e "$MAIN_CONF" ]; then
+if [ -e "$MAIN_CONF" ]; then
     chown root:root "$MAIN_CONF"
 	chmod 600 "$MAIN_CONF"
 fi
