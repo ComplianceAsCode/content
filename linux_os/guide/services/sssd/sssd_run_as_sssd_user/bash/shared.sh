d071b1f53b3c6c54801be5a700c0948d534465b8
Jiri Odehnal <jodehnal@redhat.com>
2022-03-05 19:37:48 +0100

    Fix condition logic, CI shellcheck issue and add BATS tests

--- linux_os/guide/services/sssd/sssd_run_as_sssd_user/bash/shared.sh
+++ linux_os/guide/services/sssd/sssd_run_as_sssd_user/bash/shared.sh
@@ -4,7 +4,7 @@ MAIN_CONF="/etc/sssd/conf.d/ospp.conf"
 
 {{{ bash_ensure_ini_config("$MAIN_CONF /etc/sssd/sssd.conf /etc/sssd/conf.d/*.conf", "sssd", "user", "sssd") }}}
 
-if [ ! -e "$MAIN_CONF" ]; then
+if [ -e "$MAIN_CONF" ]; then
     chown root:root "$MAIN_CONF"
 	chmod 600 "$MAIN_CONF"
 fi
