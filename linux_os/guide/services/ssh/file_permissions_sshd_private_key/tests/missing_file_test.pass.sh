6ae1a9e5b6ea9c5d6a08e6fd52a8ef511441d997
Milan Lysonek <mlysonek@redhat.com>
2022-03-08 10:48:41 +0100

    Delete test scenario that removes SSH keys from machine

--- linux_os/guide/services/ssh/file_permissions_sshd_private_key/tests/missing_file_test.pass.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-
-mkdir -p /etc/ssh/keys
-mv /etc/ssh/*_key /etc/ssh/keys
-sed -i "s#HostKey /etc/ssh/#HostKey /etc/ssh/keys/#g" /etc/ssh/sshd_config
-systemctl restart sshd
-rm -f /etc/ssh/*_key # systemctl restart sshd always regenerate keys in the /etc/ssh if they are not there
