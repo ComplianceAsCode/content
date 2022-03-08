d2c7c568dc4d5719f42acf46e852ac1fbe3ae6c5
Edgar Aguilar <edgar.aguilar@oracle.com>
2022-03-08 14:47:38 -0600

    Partially migrate rule to template
    Migrate OVAL to template so it is easier to introduce this change, also
    this allows an easier future maintenance. DISA's Ol8 STIG allows a few
    different confs for disk_full_action. So this makes possible the use of
    regex for that, while keeping the setup for other profiles. Introduce
    the var value_regex, so a future complete migration is also easier
    
    Signed-off-by: Edgar Aguilar <edgar.aguilar@oracle.com>

--- shared/templates/auditd_lineinfile/template.py
+++ shared/templates/auditd_lineinfile/template.py
@@ -3,4 +3,6 @@ from ssg.utils import parse_template_boolean_value
 
 def preprocess(data, lang):
     data["missing_parameter_pass"] = parse_template_boolean_value(data, parameter="missing_parameter_pass", default_value=False)
+    if "value_regex" not in data:
+        data["value_regex"] = data["value"]
     return data
