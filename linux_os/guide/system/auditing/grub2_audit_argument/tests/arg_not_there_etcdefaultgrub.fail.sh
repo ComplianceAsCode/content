be5a6c0eee59f6065a3eb0644c5013189d2fd5d7
Milan Lysonek <mlysonek@redhat.com>
2022-03-10 10:09:30 +0100

    grub2_audit_argument test scenarios cleanup

--- linux_os/guide/system/auditing/grub2_audit_argument/tests/arg_not_there_etcdefaultgrub.fail.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# platform = Red Hat Enterprise Linux 7
-
-# Removes audit argument from kernel command line in /etc/default/grub
-if grep -q '^GRUB_CMDLINE_LINUX=.*audit=.*"'  '/etc/default/grub' ; then
-	sed -i 's/\(^GRUB_CMDLINE_LINUX=".*\)audit=[^[:space:]]*\(.*"\)/\1 \2/'  '/etc/default/grub'
-fi
