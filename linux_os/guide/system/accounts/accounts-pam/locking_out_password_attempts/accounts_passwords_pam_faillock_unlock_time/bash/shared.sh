4693188a0a62f92587ae8b2182524808f911a5c7
Marcus Burghardt <maburgha@redhat.com>
2022-03-15 16:51:34 -0300

    Refactor Bash macros for pam_faillock rules
    Unify the Bash remediation that sets options in faillock.conf
    on macro bash_set_faillock_option().
    The macro will handle plain options and options with variables.

--- linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_passwords_pam_faillock_unlock_time/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_passwords_pam_faillock_unlock_time/bash/shared.sh
@@ -1,18 +1,8 @@
 # platform = multi_platform_wrlinux,multi_platform_rhel,multi_platform_fedora,multi_platform_ol,multi_platform_rhv,multi_platform_sle
 
-{{{ bash_instantiate_variables("var_accounts_passwords_pam_faillock_unlock_time") }}}
-
 if [ -f /usr/bin/authselect ]; then
     {{{ bash_enable_pam_faillock_with_authselect() }}}
 fi
 
-FAILLOCK_CONF="/etc/security/faillock.conf"
-if [ -f $FAILLOCK_CONF ]; then
-    if grep -q '^\s*unlock_time\s*=' $FAILLOCK_CONF; then
-        sed -i --follow-symlinks "s/^\s*\(unlock_time\s*\)=.*$/\1 = $var_accounts_passwords_pam_faillock_unlock_time/g" $FAILLOCK_CONF
-    else
-        echo "unlock_time = $var_accounts_passwords_pam_faillock_unlock_time" >> $FAILLOCK_CONF
-    fi
-else
-    {{{ bash_set_faillock_option("unlock_time", "$var_accounts_passwords_pam_faillock_unlock_time") }}}
-fi
+{{{ bash_instantiate_variables("var_accounts_passwords_pam_faillock_unlock_time") }}}
+{{{ bash_set_faillock_option("unlock_time", "$var_accounts_passwords_pam_faillock_unlock_time") }}}
