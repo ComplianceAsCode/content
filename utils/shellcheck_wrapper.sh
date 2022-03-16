4f98e72e47523f97b1ade71b858372dd0e51b907
Markus Linnala <Markus.Linnala@knowit.fi>
2022-03-16 16:26:17 +0200

    change: shellcheck_wrapper.sh: use bash nullglob
    This avoid execs etc.

--- utils/shellcheck_wrapper.sh
+++ utils/shellcheck_wrapper.sh
@@ -6,9 +6,9 @@ scripts_directory="$1"
 shift
 shellcheck_options=("$@")
 
-scripts_count=$(ls "$scripts_directory" | wc -l)
-if [ "$scripts_count" -eq 0 ]; then
+shopt -s nullglob
+set -- "$scripts_directory"/*
+if [ "$#" -eq 0 ]; then
     exit 0
-else
-    "$shellcheck_executable" "${shellcheck_options[@]}" "$scripts_directory"/*
 fi
+"$shellcheck_executable" "${shellcheck_options[@]}" "$@"
