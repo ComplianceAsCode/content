d30ef91f8704b01262f250fffa50a42db51c0841
Markus Linnala <Markus.Linnala@knowit.fi>
2022-03-16 16:24:12 +0200

    products/jre: silence shellcheck SC2174
    Example of the same issue:
    
    In build/jre/fixes/bash/java_jre_unsigned_applications.sh line 8:
      mkdir -p -m 755 ${JAVA_DIR}
               ^-- SC2174: When used with -p, -m only applies to the deepest directory.
    
    For more information:
      https://www.shellcheck.net/wiki/SC2174 -- When used with -p, -m only applie...
    
    When using mkdir -p there is no need to check if directory exists.
    
    Other possible solution is to drop -m option.

--- products/jre/guide/java/java_jre_accepted_sites_configured/java_jre_accepted_sites_exists/bash/jre.sh
+++ products/jre/guide/java/java_jre_accepted_sites_configured/java_jre_accepted_sites_exists/bash/jre.sh
@@ -2,9 +2,8 @@
 JAVA_FILE="/etc/.java/deployment/exception.sites"
 JAVA_DIR="/etc/.java/deployment"
 
-if [ ! -d ${JAVA_DIR} ] ; then
-  mkdir -p -m 755 ${JAVA_DIR}
-fi
+# shellcheck disable=SC2174
+mkdir -p -m 755 ${JAVA_DIR}
 
 if [ ! -e ${JAVA_FILE} ]; then
   touch ${JAVA_FILE} && chmod 644 ${JAVA_FILE}
