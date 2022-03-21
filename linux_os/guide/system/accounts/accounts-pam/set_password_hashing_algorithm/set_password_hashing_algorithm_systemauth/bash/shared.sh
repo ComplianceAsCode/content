ec47ce6e85f7d8dda507b141cccc93a1414caaee
anivan-suse <anastasija.ivanovic@suse.com>
2022-03-21 15:26:44 +0100

    Move code

--- linux_os/guide/system/accounts/accounts-pam/set_password_hashing_algorithm/set_password_hashing_algorithm_systemauth/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-pam/set_password_hashing_algorithm/set_password_hashing_algorithm_systemauth/bash/shared.sh
@@ -1,15 +1,15 @@
 # platform = multi_platform_all
 
-AUTH_FILES[0]="/etc/pam.d/system-auth"
-{{%- if product == "rhel7" %}}
-AUTH_FILES[1]="/etc/pam.d/password-auth"
-{{%- endif %}}
 
 {{% if product in ["sle15", "sle12"] -%}}
 if ! grep -q "^password.*required.*pam_unix.so.*sha512" /etc/pam.d/common-password ; then
    sed -i --follow-symlinks "/^password.*required.*pam_unix.so/ s/$/ sha512/" /etc/pam.d/common-password
 fi
 {{%- else -%}}
+AUTH_FILES[0]="/etc/pam.d/system-auth"
+{{%- if product == "rhel7" %}}
+AUTH_FILES[1]="/etc/pam.d/password-auth"
+{{%- endif %}}
 for pamFile in "${AUTH_FILES[@]}"
 do
 	if ! grep -q "^password.*sufficient.*pam_unix.so.*sha512" $pamFile; then
