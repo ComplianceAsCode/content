92415b83c2241bb9c879a20868254fcff30f935d
Gabriel Becker <ggasparb@redhat.com>
2022-03-28 12:47:07 +0200

    Add option in SSH rules to mark rule as default value is compliant.
    This new option enables the content to remediate any default compliant
    value into a different file to keep default values separate from
    non-default values. This helps to identify which settings are being
    reinforced even though they are default values.

--- shared/templates/sshd_lineinfile/template.py
+++ shared/templates/sshd_lineinfile/template.py
@@ -3,4 +3,10 @@ from ssg.utils import parse_template_boolean_value
 
 def preprocess(data, lang):
     data["missing_parameter_pass"] = parse_template_boolean_value(data, parameter="missing_parameter_pass", default_value=False)
+
+    is_default_value = parse_template_boolean_value(data, parameter="is_default_value", default_value=False)
+    if is_default_value:
+        data["config_basename"] = "01-complianceascode-reinforce-os-defaults.conf"
+    else:
+        data["config_basename"] = "00-complianceascode-hardening.conf"
     return data
