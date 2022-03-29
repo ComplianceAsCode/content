fa9f5ba311c53eceaf2264c1e5ab009ca707d348
Watson Sato <wsato@redhat.com>
2022-03-29 09:49:20 +0200

    Reduce test time of test scenario
    Remediation whole directory permissions takes a long time, lets just
    change a subset of the files in the library dirs.

--- linux_os/guide/system/permissions/files/permissions_within_important_dirs/file_permissions_library_dirs/tests/lenient_permissions.fail.sh
+++ linux_os/guide/system/permissions/files/permissions_within_important_dirs/file_permissions_library_dirs/tests/lenient_permissions.fail.sh
@@ -2,5 +2,7 @@
 
 DIRS="/lib /lib64 /usr/lib /usr/lib64"
 for dirPath in $DIRS; do
-    find "$dirPath" -type f -exec chmod go+w '{}' \;
+    # Limit the test changes to a subset of file in the directory
+    # Remediation the whole library dirs is very time consuming
+    find "$dirPath" -type f -regex ".*\.txt" -exec chmod go+w '{}' \;
 done
