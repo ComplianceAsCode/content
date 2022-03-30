b3452b8d6fdefc9c39addff327301840e4bb3899
Watson Sato <wsato@redhat.com>
2022-03-30 09:48:56 +0200

    Update OVAL to also check for negative values
    I haven't seen '=n' in any config file in /boot/config-*, but there
    should be no harm in checking  for the value.

--- shared/templates/kernel_build_config/template.py
+++ shared/templates/kernel_build_config/template.py
@@ -1,7 +1,6 @@
 def preprocess(data, lang):
-    data['existence'] = "at_least_one_exists"
     if data['value'] == 'n':
         # A kernel configuration that is not set is not present in the config file
         # or is commented out
-        data['existence'] = "none_exist"
+        data['should_check_absence'] = True
     return data
