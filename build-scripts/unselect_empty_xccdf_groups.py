6b3192911a7c3edcfacff90a96bef5c1d7ebb2d0
Markus Linnala <Markus.Linnala@knowit.fi>
2022-03-31 17:32:55 +0300

    *.py: always use shebang /usb/bin/python3
    Rationale: Reduce variations
    
         15 #!/usr/bin/env python3
         20 #!/usr/bin/python3
         42 #!/usr/bin/env python

--- build-scripts/unselect_empty_xccdf_groups.py
+++ build-scripts/unselect_empty_xccdf_groups.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/python3
 
 """
 Takes given *resolved* XCCDF, goes through every profile. For every profile,
--- utils/xccdf2csv-stig.py
+++ utils/xccdf2csv-stig.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/python3
 
 """
 This script creates a CSV file from an XCCDF file formatted in the
