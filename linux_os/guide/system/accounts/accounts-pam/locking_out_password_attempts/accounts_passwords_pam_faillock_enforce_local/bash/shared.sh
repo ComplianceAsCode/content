b036c12a56b1186386135dadd92259b63eccd8c8
Marcus Burghardt <maburgha@redhat.com>
2022-03-31 12:59:16 -0300

    Align pam_faillock bash macros to ansible macros
    Recently pam_faillock.so remediation were refactored to remove code
    duplication with macros. That time Ansible macros were developed and
    bash macros were slightly changed. Now the bash macros were also
    refactored to be more aligned to Ansible macros using similar functions.
    It is now less prone to discrepancies between Ansible and Bash
    remediation.

--- linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_passwords_pam_faillock_enforce_local/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_passwords_pam_faillock_enforce_local/bash/shared.sh
@@ -1,4 +1,4 @@
 # platform = multi_platform_fedora,Red Hat Enterprise Linux 8,Red Hat Enterprise Linux 9
 
-{{{ bash_enable_pam_faillock_with_authselect() }}}
-{{{ bash_set_faillock_option("local_users_only", "") }}}
+{{{ bash_pam_faillock_enable() }}}
+{{{ bash_pam_faillock_parameter_value("local_users_only", "") }}}
