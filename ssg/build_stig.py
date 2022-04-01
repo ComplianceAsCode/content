fdf4118f2a15e968ffe5f78e303081402fe3dc04
Jan Černý <jcerny@redhat.com>
2022-04-01 16:07:06 +0200

    Remove unused code

--- ssg/build_stig.py
+++ ssg/build_stig.py
@@ -25,31 +25,3 @@ def get_versions(reference_file_name):
         if version is not None and version.text:
             dictionary[version.text] = rule.get('id')
     return dictionary
-
-
-def add_references(input_file_name, reference_file_name):
-    """
-    For a given input XCCDF file and DISA STIG reference XCCDF file, process
-    all STIG references in the rules from input XCCDF file and correctly link
-    them to the corresponding reference rule.
-
-    Returns the updated ElementTree containing updated reference elements.
-    """
-    dictionary = get_versions(reference_file_name)
-
-    target_root = ET.parse(input_file_name)
-    target_rules = target_root.findall('.//{%s}Rule' % XCCDF11_NS)
-
-    for rule in target_rules:
-        refs = rule.findall('.//{%s}reference' % XCCDF11_NS)
-        for ref in refs:
-            if (ref.get('href').startswith(stig_refs) and
-                    ref.text in dictionary):
-                index = list(rule).index(ref)
-                new_ref = ET.Element(
-                    '{%s}reference' % XCCDF11_NS, {'href': stig_ns})
-                new_ref.text = dictionary[ref.text]
-                new_ref.tail = ref.tail
-                rule.insert(index + 1, new_ref)
-
-    return target_root
