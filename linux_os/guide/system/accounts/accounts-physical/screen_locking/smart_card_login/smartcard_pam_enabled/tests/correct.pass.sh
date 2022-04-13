9f45fc8362a82d50517f06dc89a555fd87855ee3
sampsone <esampson@suse.com>
2022-04-13 15:20:05 -0500

    Enable bash remediation for SUSE smartcard_pam_enabled
    create smartcard_pam_enabled/bash/shared.sh
    which includes both sle and ubuntu remediations
    use jinja ifs and remove
    smartcard_pam_enabled/bash/ubuntu.sh
    since that is now in shared.sh
    
    Update test code to support sle.

--- linux_os/guide/system/accounts/accounts-physical/screen_locking/smart_card_login/smartcard_pam_enabled/tests/correct.pass.sh
+++ linux_os/guide/system/accounts/accounts-physical/screen_locking/smart_card_login/smartcard_pam_enabled/tests/correct.pass.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-# platform = multi_platform_ubuntu
+# platform = multi_platform_ubuntu,multi_platform_sle
 # packages = libpam-pkcs11
 
+{{% if product in ["ubuntu1604", "ubuntu1804", "ubuntu2004"] %}}
 echo 'auth [success=2 default=ignore] pam_pkcs11.so' > /etc/pam.d/common-auth
+{{% else %}}
+echo 'auth sufficient pam_pkcs11.so' > /etc/pam.d/common-auth
+{{% endif %}}
--- linux_os/guide/system/accounts/accounts-physical/screen_locking/smart_card_login/smartcard_pam_enabled/tests/substring.fail.sh
+++ linux_os/guide/system/accounts/accounts-physical/screen_locking/smart_card_login/smartcard_pam_enabled/tests/substring.fail.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-# platform = multi_platform_ubuntu
+# platform = multi_platform_ubuntu,multi_platform_sle
 # packages = libpam-pkcs11
 
+{{% if product in ["ubuntu1604", "ubuntu1804", "ubuntu2004"] %}}
 echo 'aauth [success=2 default=ignore] pam_pkcs11.so' > /etc/pam.d/common-auth
+{{% else %}}
+echo 'aauth sufficient pam_pkcs11.so' > /etc/pam.d/common-auth
+{{% endif %}}
