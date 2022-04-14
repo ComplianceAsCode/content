c6b161d440add9263eb8abd80fe1e1883d3224d5
YiLin.Li <YiLin.Li@linux.alibaba.com>
2022-04-14 14:14:29 +0800

    products/alinux2: supports Alibaba Cloud Linux 2
    Signed-off-by: YiLin.Li <YiLin.Li@linux.alibaba.com>
    Signed-off-by: YuQing.Yang <yyq0391@163.com>

--- tests/unit/ssg-module/test_utils.py
+++ tests/unit/ssg-module/test_utils.py
@@ -12,7 +12,7 @@ def test_is_applicable():
 
     assert not ssg.utils.is_applicable('fedora,multi_platform_ubuntu', 'rhel7')
     assert not ssg.utils.is_applicable('ol7', 'rhel7')
-    assert not ssg.utils.is_applicable('fedora,debian9,debian10,debian11', 'rhel7')
+    assert not ssg.utils.is_applicable('alinux2,fedora,debian9,debian10,debian11', 'rhel7')
 
 
 def test_is_applicable_for_product():
