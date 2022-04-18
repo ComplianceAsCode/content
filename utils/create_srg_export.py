d01cca2dd88470a248327ef53cfa901f7dff2ffb
Matthew Burket <mburket@redhat.com>
2022-04-18 13:04:57 -0500

    Refactor handle_output in create_srg_export.py

--- utils/create_srg_export.py
+++ utils/create_srg_export.py
@@ -481,26 +481,36 @@ def get_policy(args, env_yaml) -> ssg.controls.Policy:
     return policy
 
 
+def handle_csv_output(output, results):
+    with open(output, 'w') as csv_file:
+        csv_writer = setup_csv_writer(csv_file)
+        for row in results:
+            csv_writer.writerow(row)
+
+
+def handle_xlsx_output(output, product, results):
+    output = output.replace('.csv', '.xlsx')
+    for row in results:
+        row['IA Control'] = get_iacontrol(row['SRGID'])
+    convert_srg_export_to_xlsx.handle_dict(results, output, f'{product} SRG Mapping')
+    return output
+
+
+def handle_html_output(output, product, results):
+    for row in results:
+        row['IA Control'] = get_iacontrol(row['SRGID'])
+    output = output.replace('.csv', '.html')
+    convert_srg_export_to_html.handle_dict(results, output, f'{product} SRG Mapping')
+    return output
+
+
 def handle_output(output: str, results: list, format_type: str, product: str) -> None:
     if format_type == 'csv':
-        with open(output, 'w') as csv_file:
-            csv_writer = setup_csv_writer(csv_file)
-            for row in results:
-                csv_writer.writerow(row)
-
+        handle_csv_output(output, results)
     elif format_type == 'xlsx':
-        output = output.replace('.csv', '.xlsx')
-        for row in results:
-            row['STIGID'] = ""
-            row['IA Control'] = get_iacontrol(row['SRGID'])
-        convert_srg_export_to_xlsx.handle_dict(results, output, f'{product} SRG Mapping')
+        output = handle_xlsx_output(output, product, results)
     elif format_type == 'html':
-        for row in results:
-            row['STIGID'] = ""
-            row['IA Control'] = get_iacontrol(row['SRGID'])
-        output = output.replace('.csv', '.html')
-        convert_srg_export_to_html.handle_dict(results, output, f'{product} SRG Mapping')
-
+        output = handle_html_output(output, product, results)
 
     print(f'Wrote output to {output}')
 
