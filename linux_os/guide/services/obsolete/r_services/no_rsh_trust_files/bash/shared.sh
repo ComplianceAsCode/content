79924f462697466bfced8cff21a5c0b7bb6a09f5
sampsone <esampson@suse.com>
2022-04-20 16:15:51 -0500

    SUSE enable no_rsh_trust_files remediation bash and ansible

--- linux_os/guide/services/obsolete/r_services/no_rsh_trust_files/bash/shared.sh
+++ linux_os/guide/services/obsolete/r_services/no_rsh_trust_files/bash/shared.sh
@@ -1,4 +1,4 @@
-# platform = Red Hat Virtualization 4,multi_platform_ol,multi_platform_rhel
+# platform = Red Hat Virtualization 4,multi_platform_ol,multi_platform_rhel,multi_platform_sle
 
 find /root -xdev -type f -name ".rhosts" -exec rm -f {} \;
 find /home -maxdepth 2 -xdev -type f -name ".rhosts" -exec rm -f {} \;
