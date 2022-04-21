f8ef8ab9b552cdda0b134ca96628eb83e890ab69
sampsone <esampson@suse.com>
2022-04-21 16:54:36 -0500

    Simplify ubuntu product checks.

--- linux_os/guide/system/accounts/accounts-physical/screen_locking/smart_card_login/smartcard_pam_enabled/tests/correct.pass.sh
+++ linux_os/guide/system/accounts/accounts-physical/screen_locking/smart_card_login/smartcard_pam_enabled/tests/correct.pass.sh
@@ -2,7 +2,7 @@
 # platform = multi_platform_ubuntu,multi_platform_sle
 # packages = libpam-pkcs11
 
-{{% if product in ["ubuntu1604", "ubuntu1804", "ubuntu2004"] %}}
+{{% if 'ubuntu' in product %}}
 echo 'auth [success=2 default=ignore] pam_pkcs11.so' > /etc/pam.d/common-auth
 {{% else %}}
 echo 'auth sufficient pam_pkcs11.so' > /etc/pam.d/common-auth
--- linux_os/guide/system/accounts/accounts-physical/screen_locking/smart_card_login/smartcard_pam_enabled/tests/substring.fail.sh
+++ linux_os/guide/system/accounts/accounts-physical/screen_locking/smart_card_login/smartcard_pam_enabled/tests/substring.fail.sh
@@ -2,7 +2,7 @@
 # platform = multi_platform_ubuntu,multi_platform_sle
 # packages = libpam-pkcs11
 
-{{% if product in ["ubuntu1604", "ubuntu1804", "ubuntu2004"] %}}
+{{% if 'ubuntu' in product %}}
 echo 'aauth [success=2 default=ignore] pam_pkcs11.so' > /etc/pam.d/common-auth
 {{% else %}}
 echo 'aauth sufficient pam_pkcs11.so' > /etc/pam.d/common-auth
