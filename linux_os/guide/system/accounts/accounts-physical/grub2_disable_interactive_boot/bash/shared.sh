fa614f0e1fe56f1f9676b871940c36d6435c9470
sampsone <esampson@suse.com>
2022-04-21 12:25:58 -0500

    SUSE enable remediiation for grub2_disable_interactive_boot
    Two issues:
    1: oval requires:
       GRUB_DISABLE_RECOVERY=true
       in /etc/default/grub
    
       But this was not being remediated in either ansible or bash.
    
    2: bash remediation was not updated, so it was still trying to use grubby
       which is not part of sle12/15 destribution

--- linux_os/guide/system/accounts/accounts-physical/grub2_disable_interactive_boot/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-physical/grub2_disable_interactive_boot/bash/shared.sh
@@ -11,5 +11,22 @@ if grep -q "\(GRUB_CMDLINE_LINUX\|GRUB_CMDLINE_LINUX_DEFAULT\)" /etc/default/gru
 then
 	sed -i "s/${CONFIRM_SPAWN_YES}/${CONFIRM_SPAWN_NO}/" /etc/default/grub
 fi
+{{% if 'sle' in product %}}
+#Verify that Interactive Boot is Disabled (runtime)
+/usr/bin/grub2-editenv - unset systemd.confirm_spawn
+
+# make sure GRUB_DISABLE_RECOVERY=true
+if grep -q '^GRUB_DISABLE_RECOVERY=.*'  '/etc/default/grub' ; then
+       # modify the GRUB command-line if an GRUB_DISABLE_RECOVERY= arg already exists
+       sed -i 's/GRUB_DISABLE_RECOVERY=.*/GRUB_DISABLE_RECOVERY=true/' /etc/default/grub
+else
+       # no GRUB_DISABLE_RECOVERY=arg is present, append it to file
+       echo "GRUB_DISABLE_RECOVERY=true"  >> '/etc/default/grub'
+fi
+grub2-mkconfig -o /boot/grub2/grub.cfg
+
+{{% else %}}
+
 # Remove 'systemd.confirm_spawn' kernel argument also from runtime settings
 /sbin/grubby --update-kernel=ALL --remove-args="systemd.confirm_spawn"
+{{% endif %}}
