e3094ac9befcc44e2b6715bc915cd5ba7f58ccb1
Vojtech Polasek <vpolasek@redhat.com>
2022-05-02 14:34:11 +0200

    modify parsing of bash conditionals
    remove the to_bash_conditional function
    add get_bash_conditional_line function and get_bash_insert_before_remediation function

--- ssg/build_yaml.py
+++ ssg/build_yaml.py
@@ -1856,7 +1856,8 @@ class Platform(XCCDFEntity):
         name=lambda: "",
         original_expression=lambda: "",
         xml_content=lambda: "",
-        bash_conditional=lambda: "",
+        bash_conditional_line=lambda: "",
+        bash_inserted_before_remediation=lambda: "",
         ansible_conditional=lambda: "",
         ** XCCDFEntity.KEYS
     )
@@ -1865,7 +1866,7 @@ class Platform(XCCDFEntity):
         "name",
         "xml_content",
         "original_expression",
-        "bash_conditional",
+        "bash_conditional_line",
         "ansible_conditional"
     ]
 
@@ -1885,7 +1886,8 @@ class Platform(XCCDFEntity):
         platform.name = id
         platform.original_expression = expression
         platform.xml_content = platform.get_xml()
-        platform.bash_conditional = platform.test.to_bash_conditional()
+        platform.bash_conditional_line = platform.test.get_bash_conditional_line()
+        platform.bash_inserted_before_remediation = platform.test.get_bash_inserted_before_remediation()
         platform.ansible_conditional = platform.test.to_ansible_conditional()
         return platform
 
@@ -1908,8 +1910,11 @@ class Platform(XCCDFEntity):
     def to_xml_element(self):
         return self.xml_content
 
-    def to_bash_conditional(self):
-        return self.bash_conditional
+    def get_bash_conditional_line(self):
+        return self.bash_conditional_line
+
+    def get_bash_inserted_before_remediation(self):
+        return self.bash_inserted_before_remediation
 
     def to_ansible_conditional(self):
         return self.ansible_conditional
