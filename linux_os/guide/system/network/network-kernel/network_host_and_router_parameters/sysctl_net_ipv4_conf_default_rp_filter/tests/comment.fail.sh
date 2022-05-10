c6bbf80b8cf1fbd8d911b543fba70d8518ee4da2
Watson Sato <wsato@redhat.com>
2022-05-10 10:42:47 +0200

    sysctl template: Add test for commented option
    Remove identical test scenarios from the rules.

--- linux_os/guide/system/network/network-kernel/network_host_and_router_parameters/sysctl_net_ipv4_conf_default_rp_filter/tests/comment.fail.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Clean sysctl config directories
-rm -rf /usr/lib/sysctl.d/* /run/sysctl.d/* /etc/sysctl.d/*
-
-sed -i "/net.ipv4.conf.default.rp_filter/d" /etc/sysctl.conf
-echo "# net.ipv4.conf.default.rp_filter = 1" >> /etc/sysctl.conf
-
-sysctl -w net.ipv4.conf.default.rp_filter=1
