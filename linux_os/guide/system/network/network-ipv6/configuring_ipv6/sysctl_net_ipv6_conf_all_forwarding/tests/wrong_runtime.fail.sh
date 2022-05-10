677824df4a40cf04752b5b0462ca14c5ce380410
Watson Sato <wsato@redhat.com>
2022-05-10 18:59:32 +0200

    sysctl template: Add test for wrong runtime value
    Remove duplicate test scenarios in the rules.

--- linux_os/guide/system/network/network-ipv6/configuring_ipv6/sysctl_net_ipv6_conf_all_forwarding/tests/wrong_runtime.fail.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Clean sysctl config directories
-rm -rf /usr/lib/sysctl.d/* /run/sysctl.d/* /etc/sysctl.d/*
-
-sed -i "/net.ipv6.conf.all.forwarding/d" /etc/sysctl.conf
-echo "net.ipv6.conf.all.forwarding = 0" >> /etc/sysctl.conf
-# Setting wrong runtime value
-sysctl -w net.ipv6.conf.all.forwarding=1
--- linux_os/guide/system/network/network-kernel/network_host_parameters/sysctl_net_ipv4_conf_all_send_redirects/tests/wrong_runtime.fail.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Clean sysctl config directories
-rm -rf /usr/lib/sysctl.d/* /run/sysctl.d/* /etc/sysctl.d/*
-
-sed -i "/net.ipv4.conf.all.send_redirects/d" /etc/sysctl.conf
-echo "net.ipv4.conf.all.send_redirects = 0" >> /etc/sysctl.conf
-# Setting wrong runtime value
-sysctl -w net.ipv4.conf.all.send_redirects=1
