7ec3541d4c336a7594a23664f38a2863f7137e87
Watson Sato <wsato@redhat.com>
2022-05-10 17:32:43 +0200

    sysctl template: Add test for correct value
    Remove identical test scenarios from the rules.

--- shared/templates/sysctl/tests/correct_value.pass.sh
+++ shared/templates/sysctl/tests/correct_value.pass.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
+{{% if SYSCTLVAL == "" %}}
+{{% if DATATYPE=="int" %}}
+{{% set VALUE="0" %}}
+{{% elif DATATYPE=="string" %}}
+{{% set VALUE="correct_value" %}}
+{{% endif %}}
+# variables = sysctl_{{{ SYSCTLID }}}_value={{{ VALUE }}}
+
+{{% else %}}
+{{% set VALUE=SYSCTLVAL %}}
+{{% endif %}}
 
 # Clean sysctl config directories
 rm -rf /usr/lib/sysctl.d/* /run/sysctl.d/* /etc/sysctl.d/*
 
-sed -i "/net.ipv4.conf.default.accept_source_route/d" /etc/sysctl.conf
-echo "net.ipv4.conf.default.accept_source_route = 0" >> /etc/sysctl.conf
+sed -i "/{{{ SYSCTLVAR }}}/d" /etc/sysctl.conf
+echo "{{{ SYSCTLVAR }}} = {{{ VALUE }}}" >> /etc/sysctl.conf
 
-sysctl -w net.ipv4.conf.default.accept_source_route=0
+# set correct runtime value to check if the filesystem configuration is evaluated properly
+sysctl -w {{{ SYSCTLVAR }}}="{{{ VALUE }}}"
