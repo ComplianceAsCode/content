d41ad7d6fd7b7d5129cfc8b629f4666151c0be1e
Evgeny Kolesnikov <ekolesni@redhat.com>
2022-05-11 14:33:12 +0200

    CPE OVAL templates cleanup

--- ssg/boolean_expression.py
+++ ssg/boolean_expression.py
@@ -4,6 +4,7 @@ from .ext.boolean import boolean
 # Setuptools recognize the issue: https://github.com/pypa/setuptools/issues/2522
 import pkg_resources
 import re
+import uuid
 import ssg.utils
 
 pkg_resources.safe_name = lambda name: re.sub('[^A-Za-z0-9_.]+', '-', name)
@@ -11,7 +12,7 @@ pkg_resources.safe_name = lambda name: re.sub('[^A-Za-z0-9_.]+', '-', name)
 # We don't support ~= to avoid confusion with boolean operator NOT (~)
 SPEC_SYMBOLS = ['<', '>', '=', '!', ',', '[', ']']
 
-VERSION_SYMBOLS = ['.', '-', '_', '*']
+VERSION_SYMBOLS = ['.', '-', '_', ':']
 
 SPEC_OP_ID_TRANSLATION = {
     '==': 'eq',
@@ -65,12 +66,15 @@ class Function(boolean.Function):
             op = 'or'
         return '_{0}_'.format(op).join([arg.as_id() for arg in self.args])
 
+    def as_uuid(self):
+        return str(uuid.uuid5(uuid.NAMESPACE_X500, self.as_id()))
+
 
 class Symbol(boolean.Symbol):
     """
     Base class for boolean symbols
 
-    Sub-class it and pass to the `Algebra` as `symbol_cls` to enrich
+    Subclass it and pass to the `Algebra` as `symbol_cls` to enrich
     expression elements with domain-specific methods.
 
     The `as_id` method will generate an unique string identifier usable as
@@ -79,7 +83,7 @@ class Symbol(boolean.Symbol):
 
     def __init__(self, obj):
         super(Symbol, self).__init__(obj)
-        self.spec = pkg_resources.Requirement.parse(obj)
+        self.spec = pkg_resources.Requirement.parse(obj.replace(':', '!'))
         self.obj = self.spec
 
     def __call__(self, **kwargs):
@@ -109,12 +113,17 @@ class Symbol(boolean.Symbol):
             id_str += '_' + self._spec_id(spec)
         return id_str
 
+    def as_uuid(self):
+        return str(uuid.uuid5(uuid.NAMESPACE_X500, self.as_id()))
+
     def as_dict(self):
         res = {'id': self.as_id(), 'name': self.name, 'arg': '', 'ver_str': '', 'ver_cpe': '', 'specs': []}
 
         if self.spec.specs:
-            res['ver_str'] = ' and '.join([op+ver for op, ver in self.spec.specs])
-            res['ver_cpe'] = ':'.join([ver for op, ver in self.spec.specs])
+            res['ver_str'] = ' and '.join([
+                '{0} {1}'.format(SPEC_OP_OVAL_EVR_STRING_TRANSLATION.get(op, 'equals'), ver.replace('!', ':'))
+                for op, ver in self.spec.specs])
+            res['ver_cpe'] = ':'.join([ver.replace('!', ':') for op, ver in self.spec.specs])
 
         if self.spec.extras:
             res['arg'] = self.spec.extras[0]
@@ -125,24 +134,21 @@ class Symbol(boolean.Symbol):
             res['specs'].append({
                 'id': self._spec_id(spec),
                 'op': op,
-                'ver': ver,
+                'ver': ver.replace('!', ':'),
                 'evr_op': SPEC_OP_OVAL_EVR_STRING_TRANSLATION.get(op, 'equals'),
                 'evr_ver': str(version.epoch) + ':'
-                    + '.'.join(str(x) for x in version.release) + '-'
-                    + (str(version.post) if version.post else '0')
+                           + '.'.join(str(x) for x in version.release) + '-'
+                           + (str(version.post) if version.post else '0')
             })
         return res
 
-    def get_arg(self):
+    @property
+    def arg(self):
         return self.spec.extras[0] if self.spec.extras else None
 
     @property
     def name(self):
         return self.spec.project_name
-
-    @property
-    def specs(self):
-        return self.spec.specs
 
 
 class Algebra(boolean.BooleanAlgebra):
@@ -156,9 +162,9 @@ class Algebra(boolean.BooleanAlgebra):
 
     Limitations:
     - no white space is allowed inside specifier expressions;
-    - ~= specifier operator is not supported.
+    - ~= specifier operator is not supported (along with '*' as a part of the version).
 
-    For example: "(oranges>=2.0.8,<=5 | banana) and ~apple + !pie"
+    For example: "(oranges>=2.0.8,<=5 | banana) and ~apple + !pie[beef]"
     """
 
     def __init__(self, symbol_cls, function_cls):
--- tests/unit/ssg-module/test_boolean_expressions.py
+++ tests/unit/ssg-module/test_boolean_expressions.py
@@ -1,35 +1,11 @@
 import pytest
 
 from ssg import boolean_expression
-from xml.dom import expatbuilder
-
-
-class PlatformFunction(boolean_expression.Function):
-    def as_cpe_lang_xml(self):
-        return '<cpe-lang:logical-test negate="' + ('true' if self.is_not() else 'false') + \
-               '" operator="' + ('OR' if self.is_or() else 'AND') + '">' + \
-               ''.join([arg.as_cpe_lang_xml() for arg in self.args]) + "</cpe-lang:logical-test>"
-
-
-class PlatformSymbol(boolean_expression.Symbol):
-    def as_cpe_lang_xml(self):
-        return '<cpe-lang:fact-ref name="cpe:/a:' + self.name + ':' + ':'.join([v for (op, v) in self.specs]) + '"/>'
-
-
-class PlatformAlgebra(boolean_expression.Algebra):
-    def __init__(self):
-        super(PlatformAlgebra, self).__init__(symbol_cls=PlatformSymbol, function_cls=PlatformFunction)
-
-    @staticmethod
-    def as_cpe_lang_xml(expr):
-        s = '<cpe-lang:platform id="' + expr.as_id() + '">' + expr.as_cpe_lang_xml() + '</cpe-lang:platform>'
-        # A primitive but simple way to pretty-print an XML string
-        return expatbuilder.parseString(s, False).toprettyxml()
 
 
 @pytest.fixture
 def algebra():
-    return PlatformAlgebra()
+    return boolean_expression.Algebra(symbol_cls=boolean_expression.Symbol, function_cls=boolean_expression.Function)
 
 
 @pytest.fixture
@@ -37,44 +13,22 @@ def expression1(algebra):
     return algebra.parse(u'(oranges==2.0 | banana) and not ~apple + !pie', simplify=True)
 
 
-def test_dyn():
-    alg = boolean_expression.Algebra(symbol_cls=PlatformSymbol, function_cls=PlatformFunction)
-    exp = alg.parse('not banana and not apple or anything')
-    assert str(exp) == '(~banana&~apple)|anything'
-    assert str(exp.simplify()) == 'anything|(~apple&~banana)'
-
-
-def test_id(expression1):
+def test_expression1_id(expression1):
     assert str(expression1.as_id()) == 'apple_and_banana_or_oranges_eq_2_0_or_not_pie'
+    assert str(expression1.as_uuid()) == '314ed18f-90e5-5d59-9842-cc64fdd2e520'
+    assert str(expression1.simplify()) == '(apple&(banana|oranges==2.0))|~pie'
 
 
-def test_cnf(algebra, expression1):
+def test_expression1_cnf_dnf(algebra, expression1):
     assert str(algebra.cnf(expression1)) == '(apple|~pie)&(banana|oranges==2.0|~pie)'
-
-
-def test_dnf(algebra, expression1):
     assert str(algebra.dnf(expression1)) == '(apple&banana)|(apple&oranges==2.0)|~pie'
 
 
-def test_as_cpe_xml(algebra, expression1):
-    xml = algebra.as_cpe_lang_xml(algebra.dnf(expression1))
-    assert xml == """<?xml version="1.0" ?>
-<cpe-lang:platform id="apple_and_banana_or_apple_and_oranges_eq_2_0_or_not_pie">
-\t<cpe-lang:logical-test negate="false" operator="OR">
-\t\t<cpe-lang:logical-test negate="false" operator="AND">
-\t\t\t<cpe-lang:fact-ref name="cpe:/a:apple:"/>
-\t\t\t<cpe-lang:fact-ref name="cpe:/a:banana:"/>
-\t\t</cpe-lang:logical-test>
-\t\t<cpe-lang:logical-test negate="false" operator="AND">
-\t\t\t<cpe-lang:fact-ref name="cpe:/a:apple:"/>
-\t\t\t<cpe-lang:fact-ref name="cpe:/a:oranges:2.0"/>
-\t\t</cpe-lang:logical-test>
-\t\t<cpe-lang:logical-test negate="true" operator="AND">
-\t\t\t<cpe-lang:fact-ref name="cpe:/a:pie:"/>
-\t\t</cpe-lang:logical-test>
-\t</cpe-lang:logical-test>
-</cpe-lang:platform>
-"""
+def test_dynamic_algebra():
+    alg = boolean_expression.Algebra(symbol_cls=boolean_expression.Symbol, function_cls=boolean_expression.Function)
+    exp = alg.parse('not banana and not apple or anything')
+    assert str(exp) == '(~banana&~apple)|anything'
+    assert str(exp.simplify()) == 'anything|(~apple&~banana)'
 
 
 def test_underscores_and_dashes_in_name(algebra):
