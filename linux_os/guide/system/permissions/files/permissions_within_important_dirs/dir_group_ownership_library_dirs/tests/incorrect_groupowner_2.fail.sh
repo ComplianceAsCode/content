380045139c6303ab6217e2ff0e18473de2ebf47e
Marcus Burghardt <maburgha@redhat.com>
2022-05-11 14:54:05 +0200

    Remove similar test scenarios on rules templated by file_groupownership

--- linux_os/guide/system/permissions/files/permissions_within_important_dirs/dir_group_ownership_library_dirs/tests/incorrect_groupowner_2.fail.sh
+++
@@ -1,6 +0,0 @@
-# platform = multi_platform_sle,multi_platform_rhel,multi_platform_fedora
-
-DIRS="/lib /lib64 /usr/lib /usr/lib64"
-for dirPath in $DIRS; do
-	mkdir -p "$dirPath/testme/test2" && chgrp nobody "$dirPath/testme/test2"
-done
--- linux_os/guide/system/permissions/files/permissions_within_important_dirs/dir_group_ownership_library_dirs/tests/nobody_group_owned_dir_on_lib.fail.sh
+++
@@ -1,6 +0,0 @@
-# platform = multi_platform_sle,multi_platform_rhel,multi_platform_fedora
-
-DIRS="/lib /lib64 /usr/lib /usr/lib64"
-for dirPath in $DIRS; do
-	mkdir -p "$dirPath/testme" && chgrp nobody "$dirPath/testme"
-done
