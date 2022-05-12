bcc8729ce60a5c0f72c8fb001f05fbcb5f382da6
sampsone <esampson@suse.com>
2022-05-12 18:57:37 -0500

    clean_components_post_updating, enabe bash remediation for sle15
    Sle12 ansible and bash remediation was by sle os specific files.
    But sle15 was never done for bash. Since the sl12 and sle15 remediations
    are the same, handle both in shared files under jinga2 conditionals.

--- linux_os/guide/system/software/updating/clean_components_post_updating/bash/shared.sh
+++ linux_os/guide/system/software/updating/clean_components_post_updating/bash/shared.sh
@@ -1,8 +1,12 @@
 # platform = multi_platform_fedora,multi_platform_ol,multi_platform_rhel,multi_platform_rhv,multi_platform_sle
 
+{{% if 'sle' in product %}}
+{{{ bash_replace_or_append('/etc/zypp/zypp.conf', '^solver.upgradeRemoveDroppedPackages', 'true', '%s=%s') }}}
+{{% else %}}
 if grep --silent ^clean_requirements_on_remove {{{ pkg_manager_config_file }}} ; then
         sed -i "s/^clean_requirements_on_remove.*/clean_requirements_on_remove=1/g" {{{ pkg_manager_config_file }}}
 else
         echo -e "\n# Set clean_requirements_on_remove to 1 per security requirements" >> {{{ pkg_manager_config_file }}}
         echo "clean_requirements_on_remove=1" >> {{{ pkg_manager_config_file }}}
 fi
+{{% endif %}}
