aebfd85f4301b6a11d9b8ef44e3b48da1ea787bd
Vojtech Polasek <vpolasek@redhat.com>
2022-05-12 15:21:40 +0200

    make test scenarios modify all relevant directories

--- shared/templates/kernel_module_disabled/tests/comment.fail.sh
+++ shared/templates/kernel_module_disabled/tests/comment.fail.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-if [ -f /etc/modprobe.d/{{{ KERNMODULE }}}.conf ]; then
-    sed -i '/install cramfs/d' /etc/modprobe.d/{{{ KERNMODULE }}}.conf
-fi
-echo "# install {{{ KERNMODULE }}} /bin/true" > /etc/modprobe.d/{{{ KERNMODULE }}}.conf
+# Paths to configuration files and directories for modprobe
+kernel_mod_files=( "/etc/modprobe.d/*.conf" "/etc/modprobe.conf" "/etc/modules-load.d/*.conf" "/run/modules-load.d/*.conf" "/usr/lib/modules-load.d/*.conf" "/run/modprobe.d/*.conf" "/usr/lib/modprobe.d/*.conf" )
+for filename in "${kernel_mod_files[@]}"; do
+    for file in $filename; do
+        if [[ -f $file ]]; then
+            sed -i '/install {{{ KERNMODULE }}}/d' $file
+            echo "# install {{{ KERNMODULE }}} /bin/true" > $file
+        fi
+    done
+done
