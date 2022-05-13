38f1b533da7555a500da31151b43aa7283885721
Watson Sato <wsato@redhat.com>
2022-05-13 17:25:54 +0200

    Address review feedback
    Remove unused imports and fix typos

--- tests/ssg_test_suite/template.py
+++ tests/ssg_test_suite/template.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 from __future__ import print_function
 
-import logging
-import re
-
 from ssg_test_suite import rule
 
 
@@ -14,7 +11,7 @@ class TemplateChecker(rule.RuleChecker):
 
     If the target template doesn't have tests, only the tests
     from the rules are executed.
-    If there are templated test scenarios they are exeuted together
+    If there are templated test scenarios they are executed together
     with any extra tests available for the rule.
     """
 
@@ -36,7 +33,7 @@ def perform_template_check(options):
     checker.remediate_using = options.remediate_using
     checker.dont_clean = options.dont_clean
     checker.no_reports = options.no_reports
-    # No debug option is provided for combined mode
+    # No debug option is provided for template mode
     checker.manual_debug = False
     checker.benchmark_cpes = options.benchmark_cpes
     checker.scenarios_regex = options.scenarios_regex
