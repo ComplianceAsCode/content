e200fd79077e784afb333ca766cae7392a7a79cc
Lenox, Joseph D (Joseph) <joseph.lenox@collins.com>
2022-05-17 11:25:35 -0500

    Use here-documents with JSON generated in template preprocess so that there is no assumption of an existing file. Remove unnecessary postprocess file as the Python code already does the pretty-print.

--- shared/templates/firefox_policy-setting/tests/wrong_value.fail.sh
+++ shared/templates/firefox_policy-setting/tests/wrong_value.fail.sh
@@ -3,30 +3,13 @@
 firefox_cfg="policies.json"
 firefox_dirs="/usr/lib/firefox/distribution /usr/lib64/firefox/distribution /usr/local/lib/firefox/distribution /usr/local/lib64/firefox/distribution"
 
-{{{ find_python() }}}
-
 # Iterate over the possible Firefox install directories
 for firefox_dir in ${firefox_dirs}; do
-    # write our bad config in every location to ensure remediation fixes it.
+    # write our test config in every location to ensure remediation fixes it.
     mkdir -p ${firefox_dir}
-    echo "{ \"policies\" :{ } }" > ${firefox_dir}/${firefox_cfg}
-    echo """
-import json
-_file=open('${firefox_dir}/${firefox_cfg}', 'r')
-_tree=json.load(_file)
-_file.close()
-{{% for policy_item in POLICIES %}}
-{{% for p in policy_item.subpath %}}
-if \"{{{ p }}}\" in _tree{{{ policy_item.path_python[loop.revindex] }}}:
-   pass
-else:
-   _tree{{{ policy_item.path_python[loop.revindex0] }}} = dict()
-{{% endfor %}}
-# Set the new value to the correct one
-_tree{{{ policy_item.path_python[0] }}}[\"{{{ policy_item.parameter }}}\"] = \"bad_value\"
-{{% endfor %}}
-_file=open('${firefox_dir}/${firefox_cfg}', 'w')
-json.dump(_tree, _file, indent=4, sort_keys=True)
-_file.close()
-""" | ${__REMEDIATE_PYTHON}
+    (
+cat <<'__EOF__'
+{{{ _BAD_WRONG }}}
+__EOF__
+    ) > ${firefox_dir}/${firefox_cfg}
 done
