ad28d426b3d31436fabf9473f44db248a0449bcd
Jan Černý <jcerny@redhat.com>
2022-05-25 14:34:08 +0200

    Use a Jinja macro istead of custom code

--- linux_os/guide/system/auditing/configure_auditd_data_retention/auditd_data_disk_error_action_stig/bash/shared.sh
+++ linux_os/guide/system/auditing/configure_auditd_data_retention/auditd_data_disk_error_action_stig/bash/shared.sh
@@ -2,15 +2,4 @@
 
 {{{ bash_instantiate_variables("var_auditd_disk_error_action") }}}
 
-#
-# If disk_error_action present in /etc/audit/auditd.conf, change value
-# to var_auditd_disk_error_action, else
-# add "disk_error_action = $var_auditd_disk_error_action" to /etc/audit/auditd.conf
-#
-
-if grep --silent ^disk_error_action /etc/audit/auditd.conf ; then
-        sed -i 's/^disk_error_action.*/disk_error_action = '"$var_auditd_disk_error_action"'/g' /etc/audit/auditd.conf
-else
-        echo -e "\n# Set disk_error_action to $var_auditd_disk_error_action per security requirements" >> /etc/audit/auditd.conf
-        echo "disk_error_action = $var_auditd_disk_error_action" >> /etc/audit/auditd.conf
-fi
+{{{ bash_replace_or_append("/etc/audit/auditd.conf", '^disk_error_action', "$var_auditd_disk_error_action") }}}
