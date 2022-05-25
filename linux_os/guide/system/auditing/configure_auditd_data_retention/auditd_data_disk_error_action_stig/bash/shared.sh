62c328c65bc7b85e6e8dc0893bffa116295121d8
Jan Černý <jcerny@redhat.com>
2022-05-25 11:34:32 +0200

    Add a new rule for audit disk_error_action
    The new rule auditd_data_disk_error_action_stig is very similar to
    auditd_data_disk_error_action, but allows 3 different values -
    single, halt and syslog whereas the auditd_data_disk_error_action
    ollows just a single value. The reason for creating this rule is
    that STIG allows the 3 values, so if we would allow only a single
    value the users could get false positive results.

---
+++ linux_os/guide/system/auditing/configure_auditd_data_retention/auditd_data_disk_error_action_stig/bash/shared.sh
@@ -0,0 +1,16 @@
+# platform = Red Hat Virtualization 4,multi_platform_ol,multi_platform_rhel
+
+{{{ bash_instantiate_variables("var_auditd_disk_error_action") }}}
+
+#
+# If disk_error_action present in /etc/audit/auditd.conf, change value
+# to var_auditd_disk_error_action, else
+# add "disk_error_action = $var_auditd_disk_error_action" to /etc/audit/auditd.conf
+#
+
+if grep --silent ^disk_error_action /etc/audit/auditd.conf ; then
+        sed -i 's/^disk_error_action.*/disk_error_action = '"$var_auditd_disk_error_action"'/g' /etc/audit/auditd.conf
+else
+        echo -e "\n# Set disk_error_action to $var_auditd_disk_error_action per security requirements" >> /etc/audit/auditd.conf
+        echo "disk_error_action = $var_auditd_disk_error_action" >> /etc/audit/auditd.conf
+fi
