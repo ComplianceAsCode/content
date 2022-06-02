1c51f6cd5422c1e6e3dda73209de6c65ca1673c0
Vojtech Polasek <vpolasek@redhat.com>
2022-06-02 16:36:16 +0200

    add ansible template to package conditional

--- shared/templates/cond_package/template.py
+++ shared/templates/cond_package/template.py
@@ -1,5 +1,14 @@
 import re
 
+OP_TO_ANSIBLE = {
+    'equals': '==',
+    'not equal': '!=',
+    'greater than': '>',
+    'less than': '<',
+    'greater than or equal': '>=',
+    'less than or equal': '<='
+}
+
 
 def preprocess(data, lang):
     if "evr" in data:
@@ -9,4 +18,8 @@ def preprocess(data, lang):
                 "ERROR: input violation: evr key should be in "
                 "epoch:version-release format, but package {0} has set "
                 "evr to {1}".format(data["pkgname"], evr))
+    if lang == "ansible":
+        if "specs" in data:
+            for spec in data["specs"]:
+                spec["evr_op"] = OP_TO_ANSIBLE[spec["evr_op"]]
     return data
