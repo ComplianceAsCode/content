32f0fdc472ff3f5bb5dbc2513169cbeb496d83fb
Vojtech Polasek <vpolasek@redhat.com>
2022-06-13 15:54:55 +0200

    add tests

---
+++ linux_os/guide/system/software/gnome/gnome_screen_locking/dconf_gnome_screensaver_mode_blank/tests/correct_value_not_locked.fail.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# packages = dconf,gdm
+# variables = inactivity_timeout_value=900
+
+. $SHARED/dconf_test_functions.sh
+
+clean_dconf_settings
+add_dconf_setting "org/gnome/desktop/screensaver" "picture-uri" "string ''" "local.d" "00-security-settings"
