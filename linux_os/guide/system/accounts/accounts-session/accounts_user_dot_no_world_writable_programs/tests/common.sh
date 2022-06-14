2f1fe424ae68368a0315f20ceadb77882bc10459
Federico Ramirez <federico.r.ramirez@oracle.com>
2022-06-14 14:39:15 -0500

    Introduce variable for user init files
    Introduce a varibale which defines a regex used to
    identify interactive users' initialization files.
    
    This helps to avoid trying to perform a pattern match
    on binary dotfiles, which makes evaluations result
    in error.
    
    Also use this new var in OVAL checks for
    accounts_user_dot_no_world_writable_programs rule.
    
    Signed-off-by: Federico Ramirez <federico.r.ramirez@oracle.com>

--- linux_os/guide/system/accounts/accounts-session/accounts_user_dot_no_world_writable_programs/tests/common.sh
+++ linux_os/guide/system/accounts/accounts-session/accounts_user_dot_no_world_writable_programs/tests/common.sh
@@ -2,8 +2,8 @@
 
 USER="cac_user"
 world_writable_file="/wwf.exec"
-dot_file="/home/$USER/.initfile"
-not_dot_file="/home/$USER/notinitfile.txt"
+dot_file="/home/$USER/.profile"
+not_dot_file="/home/$USER/.notinitfile"
 
 useradd -m $USER
 
--- linux_os/guide/system/accounts/accounts-session/accounts_user_dot_no_world_writable_programs/tests/no_world_writable_in_dot_file.pass.sh
+++ linux_os/guide/system/accounts/accounts-session/accounts_user_dot_no_world_writable_programs/tests/no_world_writable_in_dot_file.pass.sh
@@ -2,5 +2,5 @@
 
 source common.sh
 
-echo "# sudo bash $world_writable_file" > $dot_file
-echo "sudo bash $world_writable_file" > $not_dot_file
+echo "# sudo bash $world_writable_file" >> $dot_file
+echo "sudo bash $world_writable_file" >> $not_dot_file
