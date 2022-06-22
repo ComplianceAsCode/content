38fcccf7c1f7f4632f93d35593c5452d9ebfbd51
Federico Ramirez <federico.r.ramirez@oracle.com>
2022-06-22 17:23:37 -0500

    Add OL9 to jinja conditionals
    Add missing OL9 product to jinja conditionals
    across several rules to generate content
    closely aligned with RHEL one.
    
    Signed-off-by: Federico Ramirez <federico.r.ramirez@oracle.com>

--- linux_os/guide/system/accounts/accounts-pam/password_quality/password_quality_pwquality/accounts_password_pam_retry/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-pam/password_quality/password_quality_pwquality/accounts_password_pam_retry/bash/shared.sh
@@ -2,7 +2,7 @@
 
 {{{ bash_instantiate_variables("var_password_pam_retry") }}}
 
-{{% if product in ['rhel8', 'rhel9'] -%}}
+{{% if product in ['ol8', 'ol9', 'rhel8', 'rhel9'] -%}}
 	{{{ bash_replace_or_append('/etc/security/pwquality.conf', '^retry', '$var_password_pam_retry', '%s = %s') }}}
 {{% else %}}
 	{{{ bash_ensure_pam_module_option('/etc/pam.d/system-auth', 'password', 'requisite', 'pam_pwquality.so', 'retry', "$var_password_pam_retry", '^account.*required.*pam_permit.so') }}}
