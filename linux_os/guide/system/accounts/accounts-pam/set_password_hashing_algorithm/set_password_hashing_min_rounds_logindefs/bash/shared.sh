a721aa9ff15d968c5d4cbd30ea3f242947fc4f04
Federico Ramirez <federico.r.ramirez@oracle.com>
2022-06-22 15:02:26 -0500

    Exclude OL9 from remediations
    Changed the multi_platform_ol entry in some remediations
    to OL7 and OL8 to prevent them from being generated for OL9
    as well.
    
    Signed-off-by: Federico Ramirez <federico.r.ramirez@oracle.com>

--- linux_os/guide/system/accounts/accounts-pam/set_password_hashing_algorithm/set_password_hashing_min_rounds_logindefs/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-pam/set_password_hashing_algorithm/set_password_hashing_min_rounds_logindefs/bash/shared.sh
@@ -1,8 +1,7 @@
-# platform = multi_platform_ol
+# platform = Oracle Linux 7,Oracle Linux 8
 
 {{{ set_config_file(path="/etc/login.defs",
                     parameter="SHA_CRYPT_MIN_ROUNDS",
                     value="5000",
                     separator=" ",
                     separator_regex="\s*") }}}
-
--- linux_os/guide/system/accounts/accounts-session/accounts_have_homedir_login_defs/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-session/accounts_have_homedir_login_defs/bash/shared.sh
@@ -1,3 +1,3 @@
-# platform = Red Hat Enterprise Linux 7,Red Hat Enterprise Linux 8,Red Hat Virtualization 4,multi_platform_ol,multi_platform_sle,multi_platform_fedora
+# platform = Red Hat Enterprise Linux 7,Red Hat Enterprise Linux 8,Red Hat Virtualization 4,Oracle Linux 7,Oracle Linux 8,multi_platform_sle,multi_platform_fedora
 
 {{{ set_config_file("/etc/login.defs", "CREATE_HOME", "yes", create=true, insert_after="", insert_before="^\s*CREATE_HOME", insensitive=true) }}}
