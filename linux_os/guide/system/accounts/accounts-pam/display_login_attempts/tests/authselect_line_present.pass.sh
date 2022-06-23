4fc989197f683589ca8b538c872883d29cb34b0c
Watson Sato <wsato@redhat.com>
2022-06-23 09:57:24 +0200

    Correct display_login_attempts pass scenarios
    Make sure the pass scenarios don't have the silent keyword.

--- linux_os/guide/system/accounts/accounts-pam/display_login_attempts/tests/authselect_line_present.pass.sh
+++ linux_os/guide/system/accounts/accounts-pam/display_login_attempts/tests/authselect_line_present.pass.sh
@@ -10,4 +10,7 @@ CUSTOM_POSTLOGIN="/etc/authselect/$CUSTOM_PROFILE/postlogin"
 if [ $(grep -c "^\s*session.*required.*pam_lastlog.so\s\+showfailed\s*$" $CUSTOM_POSTLOGIN) -eq 0 ]; then
     sed -i --follow-symlinks '0,/^session.*/s/^session.*/session     required                   pam_lastlog.so showfailed\n&/' $CUSTOM_POSTLOGIN
 fi
+if [ $(grep -c "^\s*session.*pam_lastlog.so.*silent$" $CUSTOM_POSTLOGIN) -eq 0 ]; then
+    sed -i --follow-symlinks 's/^\(session.*pam_lastlog.so.*\) silent\( .*\)/\1\2/g' $CUSTOM_POSTLOGIN
+fi
 authselect apply-changes -b
