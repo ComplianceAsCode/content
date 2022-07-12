d790a441c268f2ce1b053ab83de994cab25e8255
anivan-suse <anastasija.ivanovic@suse.com>
2022-07-12 18:31:11 +0200

    SUSE dconf_gnome_screensaver_lock_enabled fix bash and ansible remediation
    SLE 12 and 15 uses disable-lock-screen not lock-enabled to control
    GNOME3 Screensaver Lock After Idle Period and disable-lock-screen is what is check for in the oval.
    Update the bash and ansible remediation to set disable-lock-screen for SUSE and update test code to reflect this.

--- linux_os/guide/system/software/gnome/gnome_screen_locking/dconf_gnome_screensaver_lock_enabled/tests/comment.fail.sh
+++ linux_os/guide/system/software/gnome/gnome_screen_locking/dconf_gnome_screensaver_lock_enabled/tests/comment.fail.sh
@@ -4,5 +4,10 @@
 . $SHARED/dconf_test_functions.sh
 
 clean_dconf_settings
-add_dconf_setting "org/gnome/desktop/screensaver" "#lock-enabled" "true" "local.d" "00-security-settings"
-add_dconf_lock "org/gnome/desktop/screensaver" "lock-enabled" "local.d" "00-security-settings"
+{{% if 'sle' in product %}}
+{{{ add_dconf_setting("org/gnome/desktop/lockdown", "disable-lock-screen", "false", "local.d", "00-security-settings") }}}
+{{{ add_dconf_lock("org/gnome/desktop/lockdown", "disable-lock-screen", "local.d", "00-security-settings-lock") }}}
+{{% else %}}
+{{{ add_dconf_setting "org/gnome/desktop/screensaver" "#lock-enabled" "true" "local.d" "00-security-settings"}}}
+{{{ add_dconf_lock "org/gnome/desktop/screensaver" "lock-enabled" "local.d" "00-security-settings"}}}
+{{% endif %}}
--- linux_os/guide/system/software/gnome/gnome_screen_locking/dconf_gnome_screensaver_lock_enabled/tests/wrong_value.fail.sh
+++ linux_os/guide/system/software/gnome/gnome_screen_locking/dconf_gnome_screensaver_lock_enabled/tests/wrong_value.fail.sh
@@ -4,5 +4,9 @@
 . $SHARED/dconf_test_functions.sh
 
 clean_dconf_settings
-add_dconf_setting "org/gnome/desktop/screensaver" "lock-enabled" "false" "local.d" "00-security-settings"
-add_dconf_lock "org/gnome/desktop/screensaver" "lock-enabled" "local.d" "00-security-settings"
+{{% if 'sle' in product }}
+{{{ add_dconf_setting("org/gnome/desktop/lockdown", "disable-lock-screen", "false", "local.d", "00-security-settings") }}}
+{{% else %}}
+{{{ add_dconf_setting "org/gnome/desktop/screensaver" "lock-enabled" "false" "local.d" "00-security-settings" }}}
+{{{ add_dconf_lock "org/gnome/desktop/screensaver" "lock-enabled" "local.d" "00-security-settings" }}}
+{{% endif %}}
