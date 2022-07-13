c50304234dfac1dcd74b3056c978eec2c097216d
Jan Černý <jcerny@redhat.com>
2022-07-13 10:47:51 +0200

    Move check unrelated to the test scenarios
    The check for an mepty list is unrelated to the test scenarios,
    rather is a generic check to avoid problems during the build.
    Therefore, it shouldn't be inside code block that is handling
    data for test scenarios, but can be extracted to a sooner position.

--- shared/templates/sysctl/template.py
+++ shared/templates/sysctl/template.py
@@ -11,7 +11,12 @@ def preprocess(data, lang):
     data["flags"] = "SR" + ipv6_flag
     if "operation" not in data:
         data["operation"] = "equals"
+    if isinstance(data["sysctlval"], list) and len(data["sysctlval"]) == 0:
+        raise ValueError(
+            "The sysctlval parameter of {0} is an empty list".format(
+                data["_rule_id"]))
 
+    # Configure data for test scenarios
     if data["datatype"] not in ["string", "int"]:
         raise ValueError(
             "Test scenarios for data type '{0}' are not implemented yet.\n"
@@ -19,7 +24,6 @@ def preprocess(data, lang):
             "{2} to add tests for it.".format(
                 data["datatype"], data["_rule_id"], __file__))
 
-    # Configure data for test scenarios
     if data["sysctlval"] == "":
         if data["datatype"] == "int":
             data["sysctl_correct_value"] = "0"
@@ -28,9 +32,6 @@ def preprocess(data, lang):
             data["sysctl_correct_value"] = "correct_value"
             data["sysctl_wrong_value"] = "wrong_value"
     elif isinstance(data["sysctlval"], list):
-        if len(data["sysctlval"]) == 0:
-            raise ValueError(
-                "The sysctlval parameter of {0} is an empty list".format(data["_rule_id"]))
         data["sysctl_correct_value"] = data["sysctlval"][0]
         if data["datatype"] == "int":
             data["sysctl_wrong_value"] = "1" + data["sysctlval"][0]
