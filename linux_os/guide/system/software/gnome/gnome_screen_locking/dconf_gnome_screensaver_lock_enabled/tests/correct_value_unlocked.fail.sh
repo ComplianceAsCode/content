9f96581586a086b4476fee15a98b7676f25a3992
anivan-suse <anastasija.ivanovic@suse.com>
2022-07-13 15:15:57 +0200

    Fix tests

--- linux_os/guide/system/software/gnome/gnome_screen_locking/dconf_gnome_screensaver_lock_enabled/tests/correct_value_unlocked.fail.sh
+++ linux_os/guide/system/software/gnome/gnome_screen_locking/dconf_gnome_screensaver_lock_enabled/tests/correct_value_unlocked.fail.sh
@@ -7,5 +7,5 @@ clean_dconf_settings
 {{% if 'sle' in product %}}
 {{{ add_dconf_settings("org/gnome/desktop/lockdown", "disable-lock-screen", "false", "local.d", "00-security-settings") }}}
 {{% else %}}
-{{{ add_dconf_setting "org/gnome/desktop/screensaver" "lock-enabled" "true" "local.d" "00-security-settings" }}}
+add_dconf_setting "org/gnome/desktop/screensaver" "lock-enabled" "true" "local.d" "00-security-settings"
 {{% endif %}}
