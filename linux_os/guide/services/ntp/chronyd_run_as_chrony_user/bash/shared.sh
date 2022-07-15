cf086ed5b4dca8506e0d01ee6c039404c81a91b9
Matej Tyc <matyc@redhat.com>
2022-07-15 16:54:04 +0200

    Fix check and remediation wrt POSIX CLI standard
    Specifying '-u chrony' is not the only way - '-uchrony' is equally valid.
    I have verified this functionally.

--- linux_os/guide/services/ntp/chronyd_run_as_chrony_user/bash/shared.sh
+++ linux_os/guide/services/ntp/chronyd_run_as_chrony_user/bash/shared.sh
@@ -8,14 +8,14 @@
 if grep -q 'OPTIONS=.*' /etc/sysconfig/chronyd; then
 	# trying to solve cases where the parameter after OPTIONS
 	#may or may not be enclosed in quotes
-	sed -i -E -e 's/\s*-u\s+\w+\s*/ /' -e 's/^([\s]*OPTIONS=["]?[^"]*)("?)/\1\2/' /etc/sysconfig/chronyd
+	sed -i -E -e 's/\s*-u\s*\w+\s*/ /' -e 's/^([\s]*OPTIONS=["]?[^"]*)("?)/\1\2/' /etc/sysconfig/chronyd
 fi
 {{% else -%}}
 {{%- endif %}}
 if grep -q 'OPTIONS=.*' /etc/sysconfig/chronyd; then
 	# trying to solve cases where the parameter after OPTIONS
 	#may or may not be enclosed in quotes
-	sed -i -E -e 's/\s*-u\s+\w+\s*/ /' -e 's/^([\s]*OPTIONS=["]?[^"]*)("?)/\1 -u chrony\2/' /etc/sysconfig/chronyd
+	sed -i -E -e 's/\s*-u\s*\w+\s*/ /' -e 's/^([\s]*OPTIONS=["]?[^"]*)("?)/\1 -u chrony\2/' /etc/sysconfig/chronyd
 else
 	echo 'OPTIONS="-u chrony"' >> /etc/sysconfig/chronyd
 fi
