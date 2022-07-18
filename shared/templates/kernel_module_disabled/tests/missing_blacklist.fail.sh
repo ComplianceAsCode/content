7c9d2a84d8d3aa359c3ef81073e6474a01f7b628
Edgar Aguilar <xeicker@gmail.com>
2022-07-18 14:34:07 -0500

    Update platform in test
    Set more concise platform definition in test missing_blacklist for
    kernel_module_disabled template
    
    Co-authored-by: Matthew Burket <m@tthewburket.com>

--- shared/templates/kernel_module_disabled/tests/missing_blacklist.fail.sh
+++ shared/templates/kernel_module_disabled/tests/missing_blacklist.fail.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# platform Oracle Linux 7, Oracle Linux 8, Red Hat Enterprise Linux 7, Red Hat Enterprise Linux 8
+# platform multi_platform_rhel,multi_platform_ol
 
 echo "install {{{ KERNMODULE }}} /bin/true" > /etc/modprobe.d/{{{ KERNMODULE }}}.conf
