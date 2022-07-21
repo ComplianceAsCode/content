35351ef6cc976d027d842fa1e0a8f5a7cbe4c9b4
Federico Ramirez <federico.r.ramirez@oracle.com>
2022-07-21 15:54:39 -0500

    Add OL platform to tests
    Signed-off-by: Federico Ramirez <federico.r.ramirez@oracle.com>

--- linux_os/guide/system/software/integrity/crypto/configure_openssl_crypto_policy/tests/nothing.fail.sh
+++ linux_os/guide/system/software/integrity/crypto/configure_openssl_crypto_policy/tests/nothing.fail.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# platform = multi_platform_fedora,Red Hat Enterprise Linux 8,Red Hat Enterprise Linux 9,multi_platform_sle
+# platform = multi_platform_fedora,Oracle Linux 8,Oracle Linux 9,Red Hat Enterprise Linux 8,Red Hat Enterprise Linux 9,multi_platform_sle
 
 . common.sh
 
