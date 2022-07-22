e453bc805a5c06d22499696c79cb0c7e0f8eb6b7
Matthew Burket <mburket@redhat.com>
2022-07-22 16:02:27 -0500

    Remove CentOS 6 and SL 6 from the project

--- ssg/constants.py
+++ ssg/constants.py
@@ -287,14 +287,12 @@ MULTI_PLATFORM_MAPPING = {
 }
 
 RHEL_CENTOS_CPE_MAPPING = {
-    "cpe:/o:redhat:enterprise_linux:6": "cpe:/o:centos:centos:6",
     "cpe:/o:redhat:enterprise_linux:7": "cpe:/o:centos:centos:7",
     "cpe:/o:redhat:enterprise_linux:8": "cpe:/o:centos:centos:8",
     "cpe:/o:redhat:enterprise_linux:9": "cpe:/o:centos:centos:9",
 }
 
 RHEL_SL_CPE_MAPPING = {
-    "cpe:/o:redhat:enterprise_linux:6": "cpe:/o:scientificlinux:scientificlinux:6",
     "cpe:/o:redhat:enterprise_linux:7": "cpe:/o:scientificlinux:scientificlinux:7",
 }
 
