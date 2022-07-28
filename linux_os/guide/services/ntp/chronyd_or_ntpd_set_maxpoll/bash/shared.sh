5d11a48b992c8035138f1f7bd3081408c0b0184b
sampsone <esampson@suse.com>
2022-07-28 08:33:12 -0500

    SUSE update chronyd max poll to support chrony.d
    For chrony ntp servers may be listed in either chrony.conf or
    in conf files in the chrony.d directory.
    This is correctly handled in the oval, but the ansible
    and bash remediation will only handles chrony.conf
    
    To fix, find all chrony conf file and loop over those.

--- linux_os/guide/services/ntp/chronyd_or_ntpd_set_maxpoll/bash/shared.sh
+++ linux_os/guide/services/ntp/chronyd_or_ntpd_set_maxpoll/bash/shared.sh
@@ -3,20 +3,32 @@
 {{{ bash_instantiate_variables("var_time_service_set_maxpoll") }}}
 
 
-{{% if 'ubuntu' not in product %}}
-pof="/usr/sbin/pidof"
-{{% else %}}
+{{% if 'sle' in product or 'ubuntu' in product %}}
 pof="/bin/pidof"
+{{% else %}}
+pof="/usr/sbin/pidof"
 {{% endif %}}
 
-config_file="/etc/ntp.conf"
-$pof ntpd || config_file="{{{ chrony_conf_path }}}"
+CONFIG_FILES="/etc/ntp.conf"
+$pof ntpd || {
+    CHRONY_NAME={{{ chrony_conf_path }}}
+    CHRONY_PATH=${CHRONY_NAME%%.*}
+    CONFIG_FILES=$(find ${CHRONY_PATH}.* -type f -name '*.conf')
+}
+
+# get list of ntp files
+
+for config_file in $CONFIG_FILES; do
+    # Set maxpoll values to var_time_service_set_maxpoll
+    sed -i "s/^\(\(server\|pool\|peer\).*maxpoll\) [0-9][0-9]*\(.*\)$/\1 $var_time_service_set_maxpoll \3/" "$config_file"
+done
 
 
-# Set maxpoll values to var_time_service_set_maxpoll
-sed -i "s/^\(\(server\|pool\|peer\).*maxpoll\) [0-9][0-9]*\(.*\)$/\1 $var_time_service_set_maxpoll \3/" "$config_file"
 
-# Add maxpoll to server, pool or peer entries without maxpoll
-grep "^\(server\|pool\|peer\)" "$config_file" | grep -v maxpoll | while read -r line ; do
+
+for config_file in $CONFIG_FILES; do
+    # Add maxpoll to server, pool or peer entries without maxpoll
+    grep "^\(server\|pool\|peer\)" "$config_file" | grep -v maxpoll | while read -r line ; do
         sed -i "s/$line/& maxpoll $var_time_service_set_maxpoll/" "$config_file"
+    done
 done
