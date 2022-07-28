30886aaab5089d7e7c5e8c188fc81ac9ce0b029f
Jan Černý <jcerny@redhat.com>
2022-07-28 14:03:53 +0200

    Rename variables

--- tests/ssg_test_suite/common.py
+++ tests/ssg_test_suite/common.py
@@ -440,15 +440,15 @@ def fetch_templated_test_scenarios(
         rule, template_builder, tests_dir, local_env_yaml, product_yaml):
     if not rule.template or not rule.template['vars']:
         return dict()
-    templated_tests = template_builder.get_all_tests(rule.template)
+    tests_paths = template_builder.get_all_tests(rule.template)
     test_config = get_test_dir_config(tests_dir, product_yaml)
-    allowed_templated_tests = select_templated_tests(
-        test_config, templated_tests.keys())
-    all_tests = {
+    allowed_tests_paths = select_templated_tests(
+        test_config, tests_paths.keys())
+    templated_test_scenarios = {
         name: template_builder.get_test(
-            templated_tests[name], rule.template, local_env_yaml)
-        for name in allowed_templated_tests}
-    return all_tests
+            tests_paths[name], rule.template, local_env_yaml)
+        for name in allowed_tests_paths}
+    return templated_test_scenarios
 
 
 def file_known_as_useless(file_name):
