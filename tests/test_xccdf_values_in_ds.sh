e57a3625d54234b867a6ec538e4ac497434e5396
Jan Černý <jcerny@redhat.com>
2022-08-05 13:51:40 +0200

    Fix CodeClimate problem

--- tests/test_xccdf_values_in_ds.sh
+++ tests/test_xccdf_values_in_ds.sh
@@ -2,10 +2,10 @@
 
 DS="$1"
 if grep -iq "dangling reference to" "$DS" ; then
-    printf "Found dangling reference in $DS, ensure correct usage of the xccdf_value Jinja macro!" >&2
+    printf "Found dangling reference in %s, ensure correct usage of the xccdf_value Jinja macro!" "$DS" >&2
     exit 1
 fi
 if grep -iq "xccdf_value" "$DS" ; then
-    printf "Found obtrusive xccdf_value in $DS, ensure correct usage of the xccdf_value Jinja macro!" >&2
+    printf "Found obtrusive xccdf_value in %s, ensure correct usage of the xccdf_value Jinja macro!" "$DS" >&2
     exit 1
 fi
