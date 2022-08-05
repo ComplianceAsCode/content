a07b3d4004f672a5607a510cd2075d3e080dd67f
Watson Sato <wsato@redhat.com>
2022-08-05 16:47:52 +0200

    Separate XML structure from and Diff logic
    Separate compare_ds.py into 2 parts, one part that understands the
    structure of the XML content, and another that knows how to compare the
    contents.
    
    This should modularize and facilitate improvemtnes to the modes of comparison.

---
+++ ssg/content_diff.py
@@ -0,0 +1,369 @@
+#!/usr/bin/python3
+
+import difflib
+import os
+import re
+
+import ssg.xml
+from ssg.constants import FIX_TYPE_TO_SYSTEM
+
+
+check_system_to_uri = {
+    "OVAL": ssg.constants.oval_namespace,
+    "OCIL": ssg.constants.ocil_cs
+}
+
+
+class StandardContentDiffer(object):
+
+    def __init__(self, old_content, new_content, rule_id, show_diffs, only_rules):
+        self.old_content = old_content
+        self.new_content = new_content
+
+        self.rule_id = rule_id
+        self.show_diffs = show_diffs
+        self.only_rules = only_rules
+
+    def _get_rules_to_compare(self, benchmark, rule_id):
+        if rule_id:
+            if not rule_id.startswith(ssg.constants.OSCAP_RULE):
+                rule_id = ssg.constants.OSCAP_RULE + rule_id
+        rules = benchmark.find_rules(rule_id)
+        return rules
+
+    def compare_rules(self, old_benchmark, new_benchmark):
+        missing_rules = []
+        try:
+            rules_in_old_benchmark = self._get_rules_to_compare(old_benchmark, self.rule_id)
+        except ValueError as e:
+            print(str(e))
+            return
+
+        for old_rule in rules_in_old_benchmark:
+            rule_id = old_rule.get_attr("id")
+            new_rule = new_benchmark.find_rule(rule_id)
+            if new_rule is None:
+                missing_rules.append(rule_id)
+                print("%s is missing in new datastream." % (rule_id))
+                continue
+            if self.only_rules:
+                continue
+            self.compare_rule(old_rule, new_rule, self.old_content, self.new_content)
+            self.compare_platforms(old_rule, new_rule,
+                                   old_benchmark, new_benchmark)
+
+    def compare_rule(
+            self, old_rule, new_rule, old_xml_content, new_xml_content):
+        self.compare_rule_texts(old_rule, new_rule)
+        self.compare_checks(
+            old_rule, new_rule, old_xml_content.components.get("oval"), new_xml_content.components.get("oval"), "OVAL")
+        self.compare_checks(
+            old_rule, new_rule, old_xml_content.components.get("ocil"), new_xml_content.components.get("ocil"), "OCIL")
+        for remediation_type in FIX_TYPE_TO_SYSTEM.keys():
+            self.compare_remediations(old_rule, new_rule, remediation_type, self.show_diffs)
+
+    def compare_platforms(self, old_rule, new_rule, old_benchmark, new_benchmark):
+        entries = [{
+                "benchmark": old_benchmark,
+                "rule": old_rule,
+                "cpe": []
+            }, {
+                "benchmark": new_benchmark,
+                "rule": new_rule,
+                "cpe": []
+            }]
+
+        for entry in entries:
+            for platform in entry["rule"].get_all_platform_elements():
+                idref = platform.get("idref")
+                if idref.startswith("#"):
+                    cpe_platforms = entry["benchmark"].find_all_cpe_platforms(idref)
+                    if len(cpe_platforms) > 1:
+                        print("Platform {0} defined more than once".format(idref))
+                        for cpe_p in cpe_platforms:
+                            ET.dump(cpe_p)
+                    if len(cpe_platforms) == 0:
+                        print("Platform {0} not defined in platform specification".format(idref))
+                        break
+                    for cpe_platform in cpe_platforms:
+                        fact_refs = cpe_platform.find_all_fact_ref_elements()
+                        for fact_ref in fact_refs:
+                            entry["cpe"].append(fact_ref.get("name"))
+                else:
+                    entry["cpe"].append(idref)
+
+        if entries[0]["cpe"] != entries[1]["cpe"]:
+            print("Platform has been changed for rule '{0}'".format(entries[0]["rule"].get_attr("id")))
+            print("--- old datastream")
+            print("+++ new datastream")
+            print("-{}".format(repr(entries[0]["cpe"])))
+            print("+{}".format(repr(entries[1]["cpe"])))
+
+    def compare_rule_texts(self, old_rule, new_rule):
+        old_rule_text = self.join_text_elements(old_rule)
+        new_rule_text = self.join_text_elements(new_rule)
+
+        if old_rule_text == new_rule_text:
+            return
+
+        rule_id = old_rule.get_attr("id")
+        print(
+            "New content has different text for rule '%s':" % (rule_id))
+
+        if self.show_diffs:
+            diff = self.compare_fix_texts(old_rule_text, new_rule_text)
+            print(
+                "New content has different text for rule '%s':" % (rule_id))
+            print(diff)
+
+    def join_text_elements(self, xml_rule):
+        """
+        This function collects the text of almost all subelements.
+        Similar to what itertext() would do, except that this function skips some elements that
+        are not relevant for comparison.
+
+        This function also injects a line for each element whose text was collected, to
+        facilitate tracking of where in the rule the text came from.
+        """
+        text = ""
+        for el in xml_rule.root:
+            el_tag = ssg.xml.get_element_tag_without_ns(el.tag)
+            if el_tag == "fix":
+                # We ignore the fix xml_rule because it has its own dedicated differ
+                continue
+            if el_tag == "reference":
+                # We ignore references to DISA Benchmark Rules, they have a format of SV-\d+r\d+_rule
+                # and can change for non-text related changes
+                if ssg.constants.stig_ns == el.get("href"):
+                    continue
+            text += "\n[%s]:\n" % el_tag
+            text += "".join(el.itertext()) + "\n"
+        return text
+
+    def compare_checks(self, old_rule, new_rule, old_checks, new_checks, system):
+        check_system_uri = check_system_to_uri[system]
+        old_check = old_rule.get_check_element(check_system_uri)
+        new_check = new_rule.get_check_element(check_system_uri)
+        rule_id = old_rule.get_attr("id")
+        if (old_check is None and new_check is not None):
+            print("New datastream adds %s for rule '%s'." % (system, rule_id))
+        elif (old_check is not None and new_check is None):
+            print(
+                "New datastream is missing %s for rule '%s'." % (system, rule_id))
+        elif (old_check is not None and new_check is not None):
+            old_check_content_ref = old_rule.get_check_content_ref_element(old_check)
+            new_check_content_ref = new_rule.get_check_content_ref_element(new_check)
+            old_check_id = old_check_content_ref.get("name")
+            new_check_id = new_check_content_ref.get("name")
+            old_check_file_name = old_check_content_ref.get("href")
+            new_check_file_name = new_check_content_ref.get("href")
+            if old_check_file_name != new_check_file_name:
+                print(
+                    "%s definition file for rule '%s' has changed from "
+                    "'%s' to '%s'." % (
+                        system, rule_id, old_check_file_name, new_check_file_name)
+                )
+            if old_check_id != new_check_id:
+                print(
+                    "%s definition ID for rule '%s' has changed from "
+                    "'%s' to '%s'." % (
+                        system, rule_id, old_check_id, new_check_id)
+                )
+            if self.show_diffs and rule_id != "xccdf_org.ssgproject.content_rule_security_patches_up_to_date":
+                try:
+                    old_check_doc = old_checks[old_check_file_name]
+                except KeyError:
+                    print(
+                        "Rule '%s' points to '%s' which isn't a part of the "
+                        "old datastream" % (rule_id, old_check_file_name))
+                    return
+                try:
+                    new_check_doc = new_checks[new_check_file_name]
+                except KeyError:
+                    print(
+                        "Rule '%s' points to '%s' which isn't a part of the "
+                        "new datastream" % (rule_id, new_check_file_name))
+                    return
+                if system == "OVAL":
+                    self.compare_oval_definitions(
+                        old_check_doc, old_check_id, new_check_doc,
+                        new_check_id)
+                elif system == "OCIL":
+                    self.compare_ocils(
+                        old_check_doc, old_check_id, new_check_doc,
+                        new_check_id)
+                else:
+                    raise RuntimeError("Unknown check system '%s'" % system)
+
+    def compare_oval_definitions(self, old_oval_def_doc, old_oval_def_id, new_oval_def_doc, new_oval_def_id):
+        old_def = old_oval_def_doc.find_oval_definition(old_oval_def_id)
+        new_def = new_oval_def_doc.find_oval_definition(new_oval_def_id)
+        old_els = self._oval_definition_to_elements(old_def)
+        new_els = self._oval_definition_to_elements(new_def)
+        for x in old_els.copy():
+            for y in new_els.copy():
+                if x[0] == y[0] and x[1] == y[1]:
+                    old_els.remove(x)
+                    new_els.remove(y)
+                    break
+        if old_els or new_els:
+            print("OVAL definition %s differs:" % (old_oval_def_id))
+            print("--- old datastream")
+            print("+++ new datastream")
+            self.print_offending_elements(old_els, "-")
+            self.print_offending_elements(new_els, "+")
+
+    def _oval_definition_to_elements(self, definition):
+        criteria = definition.get_criteria_element()
+        elements = []
+        for child in criteria.iter():  # iter recurses
+            el_tag = ssg.xml.get_element_tag_without_ns(child.tag)
+            if el_tag == "criteria":
+                operator = child.get("operator")
+                elements.append(("criteria", operator))
+            elif el_tag == "criterion":
+                test_id = child.get("test_ref")
+                elements.append(("criterion", test_id))
+            elif el_tag == "extend_definition":
+                extend_def_id = child.get("definition_ref")
+                elements.append(("extend_definition", extend_def_id))
+        return elements
+
+    def compare_ocils(self, old_ocil_doc, old_ocil_id, new_ocil_doc, new_ocil_id):
+        try:
+            old_question = self._find_boolean_question(old_ocil_doc, old_ocil_id)
+            new_question = self._find_boolean_question(new_ocil_doc, new_ocil_id)
+        except ValueError as e:
+            print("Rule '%s' OCIL can't be found: %s" % (self.rule_id, str(e)))
+            return
+        diff = self.compare_fix_texts(old_question, new_question)
+        if diff:
+            print("OCIL for rule '%s' differs:\n%s" % (self.rule_id, diff))
+
+    def _find_boolean_question(self, doc, ocil_id):
+        questionnaire = doc.find_ocil_questionnaire(ocil_id)
+        if questionnaire is None:
+            raise ValueError("OCIL questionnaire %s doesn't exist" % ocil_id)
+        test_action_ref = questionnaire.get_test_action_ref_element().text
+        test_action = doc.find_ocil_test_action(test_action_ref)
+        if test_action is None:
+            raise ValueError(
+                "OCIL boolean_question_test_action %s doesn't exist" % (
+                    test_action_ref))
+        question_id = test_action.get_attr("question_ref")
+        question = doc.find_ocil_boolean_question(question_id)
+        if question is None:
+            raise ValueError(
+                "OCIL boolean_question %s doesn't exist" % question_id)
+        question_text = question.get_question_test_element()
+        return question_text.text
+
+    def compare_remediations(self, old_rule, new_rule, remediation_type, show_diffs):
+        system = FIX_TYPE_TO_SYSTEM[remediation_type]
+        old_fix = old_rule.get_fix_element(system)
+        new_fix = new_rule.get_fix_element(system)
+        rule_id = old_rule.get_attr("id")
+        if (old_fix is None and new_fix is not None):
+            print("New datastream adds %s remediation for rule '%s'." % (
+                remediation_type, rule_id))
+        elif (old_fix is not None and new_fix is None):
+            print("New datastream is missing %s remediation for rule '%s'." % (
+                remediation_type, rule_id))
+        elif (old_fix is not None and new_fix is not None):
+            self._compare_fix_elements(
+                old_fix, new_fix, remediation_type, rule_id, show_diffs)
+
+    def _compare_fix_elements(self, old_fix, new_fix, remediation_type, rule_id, show_diffs):
+        old_fix_id = old_fix.get("id")
+        new_fix_id = new_fix.get("id")
+        if old_fix_id != new_fix_id:
+            print(
+                "%s remediation ID for rule '%s' has changed from "
+                "'%s' to '%s'." % (
+                    remediation_type, rule_id, old_fix_id, new_fix_id)
+            )
+        if show_diffs:
+            old_fix_text = "".join(old_fix.itertext())
+            new_fix_text = "".join(new_fix.itertext())
+            diff = self.compare_fix_texts(old_fix_text, new_fix_text)
+            if diff:
+                print("%s remediation for rule '%s' differs:\n%s" % (
+                    remediation_type, rule_id, diff))
+
+    def compare_fix_texts(self, old_r, new_r, fromfile="old datastream", tofile="new datastream", n=3):
+        if old_r != new_r:
+            diff = "".join(difflib.unified_diff(
+                old_r.splitlines(keepends=True), new_r.splitlines(keepends=True),
+                fromfile=fromfile, tofile=tofile, n=n))
+            return diff
+        return None
+
+    def print_offending_elements(self, elements, sign):
+        for thing, atrribute in elements:
+            print("%s %s %s" % (sign, thing, atrribute))
+
+
+class StigContentDiffer(StandardContentDiffer):
+
+    def __init__(self, old_content, new_content, rule_id, show_diffs, only_rules, output_dir):
+        super(StigContentDiffer, self).__init__(old_content, new_content, rule_id, show_diffs, only_rules)
+        self.output_dir = output_dir
+
+        self._ensure_output_dir_exists()
+
+    def _ensure_output_dir_exists(self):
+        if os.path.exists(self.output_dir):
+            if not os.path.isdir(self.output_dir):
+                print("Output path '%s' exists and it is not a directory." % self.output_dir)
+                sys.exit(1)
+        else:
+            os.mkdir(self.output_dir)
+
+    def get_stig_rule_SV(self, rule_id):
+        return re.search(r'(SV-\d+)r\d+_rule', rule_id).group(1)
+
+    def compare_rules(self, old_benchmark, new_benchmark):
+        missing_rules = []
+        try:
+            rules_in_old_benchmark = self._get_rules_to_compare(old_benchmark, self.rule_id)
+            rules_in_new_benchmark = self._get_rules_to_compare(new_benchmark, self.rule_id)
+        except ValueError as e:
+            print(str(e))
+            return
+
+        # The rules in STIG Benchmarks will have their IDS changed whenever there is an update.
+        # However, only the release number changes, the SV number stays the same.
+        # This creates map the SV numbers to their equivalent full IDs in the new Benchmark.
+        new_rule_mapping = {self.get_stig_rule_SV(rule.get_attr("id")): rule.get_attr("id")
+                            for rule in rules_in_new_benchmark}
+
+        for old_rule in rules_in_old_benchmark:
+            rule_id = new_rule_mapping[self.get_stig_rule_SV(old_rule.get_attr("id"))]
+            new_rule = new_benchmark.find_rule(rule_id)
+            if new_rule is None:
+                missing_rules.append(rule_id)
+                print("%s is missing in new datastream." % (rule_id))
+                continue
+            if self.only_rules:
+                continue
+            self.compare_rule(old_rule, new_rule, self.old_content, self.new_content)
+            self.compare_platforms(old_rule, new_rule,
+                                   old_benchmark, new_benchmark)
+
+    def compare_rule_texts(self, old_rule, new_rule):
+        old_rule_text = self.join_text_elements(old_rule)
+        new_rule_text = self.join_text_elements(new_rule)
+
+        if old_rule_text == new_rule_text:
+            return
+
+        stig_id = new_rule.get_version_element()
+        print(
+            "New content has different text for '%s'." % (stig_id.text))
+
+        if self.show_diffs:
+            diff = self.compare_fix_texts(old_rule_text, new_rule_text,
+                                          fromfile=stig_id.text, tofile=stig_id.text, n=200)
+
+            with open("%s/%s" % (self.output_dir, stig_id.text), "w") as f:
+                f.write(diff)
--- ssg/xml.py
+++ ssg/xml.py
@@ -3,9 +3,18 @@ from __future__ import print_function
 
 import platform
 import re
+import xml.etree.ElementTree as ET
 
 from .constants import (
     xml_version, oval_header, timestamp, PREFIX_TO_NS, XCCDF11_NS, XCCDF12_NS)
+from .constants import (
+    datastream_namespace,
+    oval_namespace,
+    cat_namespace,
+    xlink_namespace,
+    ocil_namespace,
+    cpe_language_namespace,
+)
 
 
 try:
@@ -105,3 +114,260 @@ def determine_xccdf_tree_namespace(tree):
     else:
         raise ValueError("Unknown root element '%s'" % root.tag)
     return xccdf_ns
+
+
+def get_element_tag_without_ns(xml_tag):
+    return re.search(r'^{.*}(.*)', xml_tag).group(1)
+
+
+def get_element_namespace(self):
+    return re.search(r'^{(.*)}.*', self.root.tag).group(1)
+
+
+class XMLElement(object):
+    '''
+    Represents an generic element read from an XML file.
+    '''
+    ns = {
+        "ds": datastream_namespace,
+        "xccdf-1.1": XCCDF11_NS,
+        "xccdf-1.2": XCCDF12_NS,
+        "oval": oval_namespace,
+        "catalog": cat_namespace,
+        "xlink": xlink_namespace,
+        "ocil": ocil_namespace,
+        "cpe-lang": cpe_language_namespace,
+    }
+
+    def __init__(self, root):
+        self.root = root
+        self._determine_xccdf_version()
+
+    def get_attr(self, attr):
+        return self.root.get(attr)
+
+    def get_namespace(self):
+        return re.search(r'^{(.*)}.*', self.root.tag).group(1)
+
+    def _determine_xccdf_version(self):
+        if self.get_namespace() == self.ns["xccdf-1.1"]:
+            self.content_xccdf_ns = "xccdf-1.1"
+        else:
+            self.content_xccdf_ns = "xccdf-1.2"
+
+
+class XMLContent(XMLElement):
+    '''
+    Can represent a Data Stream or an XCCDF Benchmark read from an XML file.
+    '''
+
+    check_engines = [("oval", "oval:oval_definitions"), ("ocil", "ocil:ocil")]
+
+    def __init__(self, root):
+        super(XMLContent, self).__init__(root)
+        self.component_refs = self.get_component_refs()
+        self.uris = self.get_uris()
+        self.components = self._find_all_component_contents()
+
+    def get_component_refs(self):
+        component_refs = dict()
+        for ds in self.root.findall("ds:data-stream", self.ns):
+            checks = ds.find("ds:checks", self.ns)
+            for component_ref in checks.findall("ds:component-ref", self.ns):
+                component_ref_href = component_ref.get("{%s}href" % (self.ns["xlink"]))
+                component_ref_id = component_ref.get("id")
+                component_refs[component_ref_href] = component_ref_id
+        return component_refs
+
+    def get_uris(self):
+        uris = dict()
+        for ds in self.root.findall("ds:data-stream", self.ns):
+            checklists = ds.find("ds:checklists", self.ns)
+            catalog = checklists.find(".//catalog:catalog", self.ns)
+            for uri in catalog.findall("catalog:uri", self.ns):
+                uri_uri = uri.get("uri")
+                uri_name = uri.get("name")
+                uris[uri_uri] = uri_name
+        return uris
+
+    def is_benchmark(self):
+        if self.root.tag == "{%s}Benchmark" % (self.ns["xccdf-1.2"]):
+            return True
+        elif self.root.tag == "{%s}Benchmark" % (self.ns["xccdf-1.1"]):
+            self.content_xccdf_ns = "xccdf-1.1"
+            return True
+
+    def get_benchmarks(self):
+        ds_components = self.root.findall("ds:component", self.ns)
+        if not ds_components:
+            # The content is not a DS, maybe it is just an XCCDF Benchmark
+            if self.is_benchmark():
+                yield XMLBenchmark(self.root)
+        for component in ds_components:
+            for benchmark in component.findall("%s:Benchmark" % self.content_xccdf_ns, self.ns):
+                yield XMLBenchmark(benchmark)
+
+    def find_benchmark(self, id_):
+        ds_components = self.root.findall("ds:component", self.ns)
+        if not ds_components:
+            # The content is not a DS, maybe it is just an XCCDF Benchmark
+            if self.is_benchmark():
+                return XMLBenchmark(self.root)
+        for component in ds_components:
+            benchmark = component.find("%s:Benchmark[@id='%s']" % (self.content_xccdf_ns, id_), self.ns)
+            if benchmark is not None:
+                return XMLBenchmark(benchmark)
+        return None
+
+    def _find_all_component_contents(self):
+        component_doc_dict = dict()
+        for component in self.root.findall("ds:component", self.ns):
+            for check_spec in self.check_engines:
+                def_doc = component.find(check_spec[1], self.ns)
+                if def_doc is not None:
+                    def_doc_dict = dict()
+                    comp_id = component.get("id")
+                    comp_href = "#" + comp_id
+                    try:
+                        filename = self.uris["#" + self.component_refs[comp_href]]
+                    except KeyError:
+                        continue
+                    def_doc_dict[filename] = XMLComponent(def_doc)
+                    component_doc_dict[check_spec[0]] = def_doc_dict
+                    # This component matched one of the checking engines,
+                    # thre is no need to continue further
+                    break
+        return component_doc_dict
+
+
+class XMLBenchmark(XMLElement):
+    '''
+    Represents an XCCDF Benchmark read from an XML file.
+    '''
+
+    def __init__(self, root):
+        super(XMLBenchmark, self).__init__(root)
+        self.root = root
+
+    def find_rules(self, rule_id):
+        if rule_id:
+            rules = [XMLRule(r) for r in self.root.iterfind(
+                ".//%s:Rule[@id='%s']" % (self.content_xccdf_ns, rule_id), self.ns)]
+            if len(rules) == 0:
+                raise ValueError("Can't find rule %s" % (rule_id))
+        else:
+            rules = [XMLRule(r) for r in self.root.iterfind(
+                ".//%s:Rule" % (self.content_xccdf_ns), self.ns)]
+        return rules
+
+    def find_rule(self, rule_id):
+        rule = self.root.find(
+                ".//%s:Rule[@id='%s']" % (self.content_xccdf_ns, rule_id), self.ns)
+        return XMLRule(rule) if rule else None
+
+    def find_all_cpe_platforms(self, idref):
+        cpes = [XMLCPEPlatform(p) for p in self.root.iterfind(
+            ".//cpe-lang:platform[@id='{0}']".format(idref.replace("#", "")), self.ns)]
+        return cpes
+
+
+class XMLRule(XMLElement):
+    '''
+    Represents an XCCDF Rule read from an XML file.
+    '''
+
+    def __init__(self, root):
+        super(XMLRule, self).__init__(root)
+        self.root = root
+
+    def get_check_element(self, check_system_uri):
+        return self.root.find(
+            "%s:check[@system='%s']" % (self.content_xccdf_ns, check_system_uri), self.ns)
+
+    def get_check_content_ref_element(self, check_element):
+        return check_element.find(
+            "%s:check-content-ref" % (self.content_xccdf_ns), self.ns)
+
+    def get_fix_element(self, remediation_type_uri):
+        return self.root.find("%s:fix[@system='%s']" % (self.content_xccdf_ns, remediation_type_uri), self.ns)
+
+    def get_version_element(self):
+        return self.root.find("%s:version" % (self.content_xccdf_ns), self.ns)
+
+    def get_all_platform_elements(self):
+        return self.root.findall(".//%s:platform" % (self.content_xccdf_ns), self.ns)
+
+
+class XMLComponent(XMLElement):
+    '''
+    Represents the element of the Data stream component that has relevant content.
+
+    This make it easier to access contents pertaining to a SCAP component.
+    '''
+    def __init__(self, root):
+        super(XMLComponent, self).__init__(root)
+
+    def find_oval_definition(self, def_id):
+        definitions = self.root.find("oval:definitions", self.ns)
+        definition = definitions.find("oval:definition[@id='%s']" % (def_id), self.ns)
+        return XMLOvalDefinition(definition)
+
+    def find_ocil_questionnaire(self, def_id):
+        questionnaires = self.root.find("ocil:questionnaires", self.ns)
+        questionnaire = questionnaires.find(
+            "ocil:questionnaire[@id='%s']" % def_id, self.ns)
+        return XMLOcilQuestionnaire(questionnaire)
+
+    def find_ocil_test_action(self, test_action_ref):
+        test_actions = self.root.find("ocil:test_actions", self.ns)
+        test_action = test_actions.find(
+            "ocil:boolean_question_test_action[@id='%s']" % test_action_ref, self.ns)
+        return XMLOcilTestAction(test_action)
+
+    def find_ocil_boolean_question(self, question_id):
+        questions = self.root.find("ocil:questions", self.ns)
+        question = questions.find(
+            "ocil:boolean_question[@id='%s']" % question_id, self.ns)
+        return XMLOcilQuestion(question)
+
+
+class XMLOvalDefinition(XMLComponent):
+    def __init__(self, root):
+        super(XMLOvalDefinition, self).__init__(root)
+
+    def get_criteria_element(self):
+        return self.root.find("oval:criteria", self.ns)
+
+
+class XMLOcilQuestionnaire(XMLComponent):
+    def __init__(self, root):
+        super(XMLOcilQuestionnaire, self).__init__(root)
+
+    def get_test_action_ref_element(self):
+        return self.root.find(
+            "ocil:actions/ocil:test_action_ref", self.ns)
+
+    def get_test_action_ref_element(self):
+        return self.root.find(
+            "ocil:actions/ocil:test_action_ref", self.ns)
+
+
+class XMLOcilTestAction(XMLComponent):
+    def __init__(self, root):
+        super(XMLOcilTestAction, self).__init__(root)
+
+
+class XMLOcilQuestion(XMLComponent):
+    def __init__(self, root):
+        super(XMLOcilQuestion, self).__init__(root)
+
+    def get_question_test_element(self):
+        return self.root.find("ocil:question_text", self.ns)
+
+
+class XMLCPEPlatform(XMLElement):
+    def __init__(self, root):
+        super(XMLCPEPlatform, self).__init__(root)
+
+    def find_all_fact_ref_elements(self):
+        return self.root.findall(".//cpe-lang:fact-ref", self.ns)
