14253bb602ac98bd0117a5c1d1bb21f3b943218e
Edgar Aguilar <edgar.aguilar@oracle.com>
2022-08-11 12:23:12 -0500

    Add tests to rule about pam_faillock_silent
    Signed-off-by: Edgar Aguilar <edgar.aguilar@oracle.com>

---
+++ linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_passwords_pam_faillock_silent/tests/missing_parameter_password_auth.fail.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# packages = authselect
+# platform = Oracle Linux 8,Oracle Linux 9,Red Hat Enterprise Linux 8,Red Hat Enterprise Linux 9
+
+source common.sh
+
+if ! grep -q "pam_faillock\.so.*silent" "$CUSTOM_PROFILE_DIR/system-auth" ; then
+    sed -i --follow-symlinks "/pam_faillock\.so/s/preauth/preauth silent/" \
+    "$CUSTOM_PROFILE_DIR/system-auth"
+fi
+
+authselect apply-changes
