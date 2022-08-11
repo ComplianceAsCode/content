1e7ecad302a4684d92799f5c68c030b576e6f832
Federico Ramirez <federico.r.ramirez@oracle.com>
2022-08-11 11:26:00 -0500

    Allow sudoers_validate_passwd in multiple files
    The DISA STIG requirement that this rule implements changed
    to allow several non-conflicting instances of the required
    config entries
    
    Update OVAL checks, bash and ansible remediations, rule's
    yaml file, and tests accordingly
    
    Signed-off-by: Federico Ramirez <federico.r.ramirez@oracle.com>

--- linux_os/guide/system/software/sudo/sudoers_validate_passwd/bash/shared.sh
+++ linux_os/guide/system/software/sudo/sudoers_validate_passwd/bash/shared.sh
@@ -1,17 +1,18 @@
 # platform = multi_platform_all
 
 {{%- macro delete_line_in_sudoers_d(line) %}}
+if grep -x '^{{{line}}}$' /etc/sudoers; then
+    sed -i "/{{{line}}}/d" /etc/sudoers \;
+fi
 if grep -x '^{{{line}}}$' /etc/sudoers.d/*; then
     find /etc/sudoers.d/ -type f -exec sed -i "/{{{line}}}/d" {} \;
 fi
 {{%- endmacro %}}
 
-{{{- delete_line_in_sudoers_d("Defaults !targetpw") }}}
-{{{- delete_line_in_sudoers_d("Defaults !rootpw") }}}
-{{{- delete_line_in_sudoers_d("Defaults !runaspw") }}}
+{{{- delete_line_in_sudoers_d("Defaults targetpw") }}}
+{{{- delete_line_in_sudoers_d("Defaults rootpw") }}}
+{{{- delete_line_in_sudoers_d("Defaults runaspw") }}}
 
 {{{ set_config_file(path="/etc/sudoers", parameter="Defaults !targetpw", value="", create=true, insensitive=false, separator="", separator_regex="", prefix_regex="") }}}
 {{{ set_config_file(path="/etc/sudoers", parameter="Defaults !rootpw", value="", create=true, insensitive=false, separator="", separator_regex="", prefix_regex="") }}}
 {{{ set_config_file(path="/etc/sudoers", parameter="Defaults !runaspw", value="", create=true, insensitive=false, separator="", separator_regex="", prefix_regex="") }}}
-
-
