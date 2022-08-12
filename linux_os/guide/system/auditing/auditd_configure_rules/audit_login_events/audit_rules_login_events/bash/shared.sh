dd3738cc4b0028996f921a9c669b85842305de1e
Markus Linnala <Markus.Linnala@knowit.fi>
2022-08-12 10:19:04 +0300

    WIP: feature: add faillock_path
    Fedora default is /var/run/faillock
    
    man faillock(8)
    ...
    OPTIONS
           --dir /path/to/tally-directory
               The directory where the user files with the failure records are kept. The default is /var/run/faillock.
    ...
    
    FILES
           /var/run/faillock/*
               the files logging the authentication failures for users
    
    Purposefully ignore rules under: locking_out_password_attempts
    for now.
    Those rules focus main on situation where setting is in config file or
    pam.
    
    There is variable: var_accounts_passwords_pam_faillock_dir

--- linux_os/guide/system/auditing/auditd_configure_rules/audit_login_events/audit_rules_login_events/bash/shared.sh
+++ linux_os/guide/system/auditing/auditd_configure_rules/audit_login_events/audit_rules_login_events/bash/shared.sh
@@ -2,12 +2,6 @@
 
 # Perform the remediation for both possible tools: 'auditctl' and 'augenrules'
 
-{{% if product in ["ol8", "ol9", "rhel8", "rhel9"] %}}
-{{% set faillock_path = "/var/log/faillock" %}}
-{{% else %}}
-{{% set faillock_path = "/var/run/faillock" %}}
-{{% endif %}}
-
 {{{ bash_fix_audit_watch_rule("auditctl", "/var/log/tallylog", "wa", "logins") }}}
 {{{ bash_fix_audit_watch_rule("augenrules", "/var/log/tallylog", "wa", "logins") }}}
 
