e70b9d6d4ffcbe18bbe8a37c1722f650285c51fa
Markus Linnala <Markus.Linnala@knowit.fi>
2022-08-15 15:29:28 +0300

    change: sshd_set_max_sessions: use complete_ocil_entry_sshd_option and template/sshd_lineinfile

--- linux_os/guide/services/ssh/ssh_server/sshd_set_max_sessions/bash/shared.sh
+++
@@ -1,9 +0,0 @@
-# platform = multi_platform_all
-# reboot = false
-# strategy = configure
-# complexity = low
-# disruption = low
-
-{{{ bash_instantiate_variables("var_sshd_max_sessions") }}}
-
-{{{ bash_sshd_config_set(parameter="MaxSessions", value="$var_sshd_max_sessions") }}}
--- linux_os/guide/services/ssh/ssh_server/sshd_set_max_sessions/tests/rhel7_correct_value.pass.sh
+++
@@ -1,3 +0,0 @@
-# platform = Red Hat Enterprise Linux 7
-
-{{{ bash_sshd_config_set(parameter="MaxSessions", value="10") }}}
