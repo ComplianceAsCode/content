0ffe222c03310e7d9656de1ac35851494afe44ef
Milan Lysonek <mlysonek@redhat.com>
2022-08-18 17:23:07 +0200

    Remove auditd_data_retention_max_log_file_action STIG specific test scenario

--- linux_os/guide/system/auditing/configure_auditd_data_retention/auditd_data_retention_max_log_file_action/tests/max_log_file_action_stig.pass.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# packages = audit
-# profiles = xccdf_org.ssgproject.content_profile_stig
-# platform = Red Hat Enterprise Linux 8, Red Hat Enterprise Linux 9, multi_platform_fedora
-
-. $SHARED/auditd_utils.sh
-prepare_auditd_test_enviroment
-set_parameters_value /etc/audit/auditd.conf "max_log_file_action" "syslog"
