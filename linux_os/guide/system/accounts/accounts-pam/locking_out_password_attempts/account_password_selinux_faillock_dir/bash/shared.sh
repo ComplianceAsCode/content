861a0b3e9090595d5d8b0bd7354b119617c3d561
Marcus Burghardt <maburgha@redhat.com>
2022-08-22 08:24:52 +0200

    Fix issues with bash remediation
    The bash remediation was not prepared for a situation where more than
    one directory is defined, for example, in faillock.conf and in PAM
    files. In situations like this, the remediation would fail. Also, the
    remediation was not considering an existing configuration for SELinux
    context. For example, if the directory was already defined via semanage,
    an error is returned if tried to insert it again. In this case, the
    SELinux context should be updated with appropriate command.

--- linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/account_password_selinux_faillock_dir/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/account_password_selinux_faillock_dir/bash/shared.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 # platform = multi_platform_all
 
-FAILLOCK_DIR=$(grep -oP "^\s*(?:auth.*pam_faillock.so.*)?dir\s*=\s*(\S+)" \
-                    /etc/security/faillock.conf \
-                    /etc/pam.d/system-auth \
-                    /etc/pam.d/password-auth \
-                    | sed -r 's/.*=\s*(\S+)/\1/')
+FAILLOCK_CONF_FILES="/etc/security/faillock.conf /etc/pam.d/system-auth /etc/pam.d/password-auth"
 
-mkdir -p $FAILLOCK_DIR
-
-semanage fcontext -a -t faillog_t "$FAILLOCK_DIR(/.*)?"
-
-restorecon -R -v "$FAILLOCK_DIR"
+for dir in $(grep -oP "^\s*(?:auth.*pam_faillock.so.*)?dir\s*=\s*(\S+)" $FAILLOCK_CONF_FILES \
+            | sed -r 's/.*=\s*(\S+)/\1/'); do
+    if ! `semanage fcontext -a -t faillog_t "$dir(/.*)?"`; then
+        semanage fcontext -m -t faillog_t "$dir(/.*)?"
+    fi
+    if [ ! -e $dir ]; then
+        mkdir -p $dir
+    fi
+    restorecon -R -v $dir
+done
