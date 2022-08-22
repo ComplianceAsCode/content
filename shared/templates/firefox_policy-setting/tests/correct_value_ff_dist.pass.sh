d8b20ea5a035c46fa68f2c8873a7fbc78d625e5b
Lenox, Joseph D (Joseph) <joseph.lenox@collins.com>
2022-08-22 15:03:48 -0500

    fix tests to account for both possible locations.

--- shared/templates/firefox_policy-setting/tests/correct_value_ff_dist.pass.sh
+++ shared/templates/firefox_policy-setting/tests/correct_value_ff_dist.pass.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 firefox_cfg="policies.json"
-firefox_dirs="/etc/firefox/policies"
+firefox_dirs="/usr/lib64/firefox/distribution"
 
-# Iterate over the possible Firefox install directories
+# Write the prerequisite to /usr/lib64/firefox so that the remediation script uses it.
+mkdir -p /usr/lib64/firefox
+touch /usr/lib64/firefox/firefox-bin
 for firefox_dir in ${firefox_dirs}; do
     # write our test config in every location to ensure remediation fixes it.
     mkdir -p ${firefox_dir}
