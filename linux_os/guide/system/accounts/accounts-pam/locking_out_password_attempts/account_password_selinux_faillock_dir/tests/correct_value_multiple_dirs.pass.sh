4bb9ef6a4c9e7b219bd8bfde2c11c33cb22ec970
Marcus Burghardt <maburgha@redhat.com>
2022-08-26 15:05:56 +0200

    Fix missing files in echo commands

--- linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/account_password_selinux_faillock_dir/tests/correct_value_multiple_dirs.pass.sh
+++ linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/account_password_selinux_faillock_dir/tests/correct_value_multiple_dirs.pass.sh
@@ -4,7 +4,7 @@
 
 truncate -s 0 /etc/security/faillock.conf
 echo "dir=/var/log/faillock" > /etc/security/faillock.conf
-echo "auth  required pam_faillock.so dir=/var/log/faillock_admins"
+echo "auth  required pam_faillock.so dir=/var/log/faillock_admins" >> /etc/pam.d/system-auth
 
 mkdir /var/log/faillock /var/log/faillock_admins
 semanage fcontext -a -t faillog_t "/var/log/faillock(/.*)?"
