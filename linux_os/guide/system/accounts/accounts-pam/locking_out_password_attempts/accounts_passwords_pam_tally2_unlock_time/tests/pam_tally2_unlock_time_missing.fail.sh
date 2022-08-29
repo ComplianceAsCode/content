5136a3689847021456da7fb94ef3d4d8a6ba5927
teacup-on-rockingchair <strandjata@gmail.com>
2022-08-29 08:54:53 +0300

    Replace accounts_passwords_pam_faillock_unlock_time with accounts_passwords_pam_tally2_unlock_time for SLE platforms

---
+++ linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_passwords_pam_tally2_unlock_time/tests/pam_tally2_unlock_time_missing.fail.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# platform = multi_platform_sle,Ubuntu 20.04
+
+cp -f common-account_pam_tally2_configured /etc/pam.d/common-account
+cp -f common-auth_pam_tally2_unlock_time_absent /etc/pam.d/common-auth
