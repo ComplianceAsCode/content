33cf2f94f12769ef1b8191b3f5264b32cb620cef
Markus Linnala <Markus.Linnala@knowit.fi>
2022-08-31 10:01:31 +0300

    fix: sshd_rekey_limit/tests: drop unnecessary .*
    `.*` at the end of match in sed does nothing
    
    Match is now:
    - from beginning
    - accept whitespace without length limits
    - require RekeyLimit caseinsensitive
    
    And remove part would add:
    - accept any string of any length
    
    And that is just pointless. sed match does not need to match whole line.

--- linux_os/guide/services/ssh/ssh_server/sshd_rekey_limit/tests/no_line_directory.fail.sh
+++ linux_os/guide/services/ssh/ssh_server/sshd_rekey_limit/tests/no_line_directory.fail.sh
@@ -4,5 +4,5 @@ mkdir -p /etc/ssh/sshd_config.d
 touch /etc/ssh/sshd_config.d/nothing
 
 if grep -iq "^\s*RekeyLimit" /etc/ssh/sshd_config /etc/ssh/sshd_config.d/* ; then
-	sed -i "/^\s*RekeyLimit.*/Id" /etc/ssh/sshd_config /etc/ssh/sshd_config.d/*
+	sed -i "/^\s*RekeyLimit/Id" /etc/ssh/sshd_config /etc/ssh/sshd_config.d/*
 fi
