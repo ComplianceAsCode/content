d4a68a22e28c7d62065ca980f66ec564ee416d06
Marcus Burghardt <maburgha@redhat.com>
2022-09-01 21:14:32 +0200

    Use the proper variable in bash remediation
    The bash remediation was using a static dir as value for the dir option
    in pam_faillock.so settings while there is a proper variable for this.

--- linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_passwords_pam_faillock_dir/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_passwords_pam_faillock_dir/bash/shared.sh
@@ -4,6 +4,7 @@
 # complexity = low
 # disruption = low
 
+{{{ bash_instantiate_variables("var_accounts_passwords_pam_faillock_dir") }}}
+
 {{{ bash_pam_faillock_enable() }}}
- 
-{{{ bash_pam_faillock_parameter_value("dir","/var/log/faillock") }}}
+{{{ bash_pam_faillock_parameter_value("dir", "$var_accounts_passwords_pam_faillock_dir") }}}
