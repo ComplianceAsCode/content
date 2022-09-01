a651aa7f3be0a05ef387fe739c09d811702158fe
Markus Linnala <Markus.Linnala@knowit.fi>
2022-09-01 19:51:14 +0300

    fix: account_password_selinux_faillock_dir: skip testing under podman
    Tests run restorecon and as far as selinux does not work in containers
    it is not reasonable to test.

--- linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/account_password_selinux_faillock_dir/tests/wrong_value_multiple_dirs.fail.sh
+++ linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/account_password_selinux_faillock_dir/tests/wrong_value_multiple_dirs.fail.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # packages = policycoreutils-python-utils
 # platform = multi_platform_all
+#   selinux / restorecon does not work under podman
+# skip_test_env = podman-based
 
 truncate -s 0 /etc/security/faillock.conf
 echo "dir=/var/log/faillock" > /etc/security/faillock.conf
