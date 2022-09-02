466e143cc664644bf1babd5c7fe5fc3192f7f5cc
Markus Linnala <Markus.Linnala@knowit.fi>
2022-09-02 14:54:48 +0300

    bugfix: accounts_polyinstantiated_*: fix bash remediation
    And use basedir to sync /tmp and /var/tmp together

--- linux_os/guide/system/accounts/accounts-session/accounts_polyinstantiated_tmp/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-session/accounts_polyinstantiated_tmp/bash/shared.sh
@@ -1,13 +1,14 @@
+{{%- set basedir="/tmp" -%}}
 # platform = multi_platform_all
-if ! [ -d /tmp/tmp-inst ] ; then
-    mkdir --mode 000 /tmp/tmp-inst
+if ! [ -d {{{ basedir }}}/tmp-inst ] ; then
+    mkdir --mode 000 {{{ basedir }}}/tmp-inst
 fi
-chmod 000 /tmp/tmp-inst
-chcon --reference=/tmp /tmp/tmp-inst
+chmod 000 {{{ basedir }}}/tmp-inst
+chcon --reference={{{ basedir }}}/ {{{ basedir }}}/tmp-inst
 
-if ! grep -Eq '^\s*/tmp\s+/tmp/tmp-inst/\s+level\s+root,adm$' /etc/security/namespace.conf ; then
-    if grep -Eq '^\s*/tmp\s+' /etc/security/namespace.conf ; then
-        sed -i '/^\s*\/tmp/d' /etc/security/namespace.conf
+if ! grep -Eq '^\s*{{{ basedir }}}\s+{{{ basedir }}}/tmp-inst/\s+level\s+root,adm\s*$' /etc/security/namespace.conf ; then
+    if grep -Eq '^\s*{{{ basedir }}}\s+' /etc/security/namespace.conf ; then
+        sed -i '/^\s*\{{{ basedir }}}/d' /etc/security/namespace.conf
     fi
-    echo "/tmp     /tmp/tmp-inst/        level      root,adm" >> /etc/security/namespace.conf
+    echo "{{{ basedir }}} {{{ basedir }}}/tmp-inst/    level      root,adm" >> /etc/security/namespace.conf
 fi
--- linux_os/guide/system/accounts/accounts-session/accounts_polyinstantiated_var_tmp/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-session/accounts_polyinstantiated_var_tmp/bash/shared.sh
@@ -1,13 +1,14 @@
+{{%- set basedir="/var/tmp" -%}}
 # platform = multi_platform_all
-if ! [ -d /tmp-inst ] ; then
-    mkdir --mode 000 /var/tmp/tmp-inst
+if ! [ -d {{{ basedir }}}/tmp-inst ] ; then
+    mkdir --mode 000 {{{ basedir }}}/tmp-inst
 fi
-chmod 000 /var/tmp/tmp-inst
-chcon --reference=/var/tmp/ /var/tmp/tmp-inst
+chmod 000 {{{ basedir }}}/tmp-inst
+chcon --reference={{{ basedir }}}/ {{{ basedir }}}/tmp-inst
 
-if ! grep -Eq '^\s*/var/tmp\s+/var/tmp/tmp-inst/\s+level\s+root,adm$' /etc/security/namespace.conf ; then
-    if grep -Eq '^\s*/var/tmp\s+' /etc/security/namespace.conf ; then
-        sed -i '/^\s*\/var\/tmp/d' /etc/security/namespace.conf
+if ! grep -Eq '^\s*{{{ basedir }}}\s+{{{ basedir }}}/tmp-inst/\s+level\s+root,adm\s*$' /etc/security/namespace.conf ; then
+    if grep -Eq '^\s*{{{ basedir }}}\s+' /etc/security/namespace.conf ; then
+        sed -i '/^\s*\{{{ basedir }}}/d' /etc/security/namespace.conf
     fi
-    echo "/var/tmp /var/tmp/tmp-inst/    level      root,adm" >> /etc/security/namespace.conf
+    echo "{{{ basedir }}} {{{ basedir }}}/tmp-inst/    level      root,adm" >> /etc/security/namespace.conf
 fi
