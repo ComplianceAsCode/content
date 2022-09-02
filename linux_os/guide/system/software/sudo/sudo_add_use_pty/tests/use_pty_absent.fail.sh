428d68321b513d6685d93c31799d17ab94d820bf
Markus Linnala <Markus.Linnala@knowit.fi>
2022-09-02 17:56:28 +0300

    bugfix: sudo_add_use_pty: Handle situation where /etc/sudoers.d is empty

--- linux_os/guide/system/software/sudo/sudo_add_use_pty/tests/use_pty_absent.fail.sh
+++ linux_os/guide/system/software/sudo/sudo_add_use_pty/tests/use_pty_absent.fail.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # platform = multi_platform_all
 
+touch /etc/sudoers.d/empty
 # Code taken from macro bash_sudo_remove_config()
 for f in $( ls /etc/sudoers /etc/sudoers.d/* 2> /dev/null ) ; do
   matching_list=$(grep -P '^(?!#).*[\s]+use_pty.*$' $f | uniq )
