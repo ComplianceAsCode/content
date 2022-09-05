6c8cfc97fa02495ea789912f8d0538517f290365
Jan Černý <jcerny@redhat.com>
2022-09-05 15:58:00 +0200

    Remove unused xccdf2table-byref.xslt
    This was mentioned in the docs but actually isn't used by any part of
    the build system and most of the products don't have it

--- products/example/new_product.sh
+++ products/example/new_product.sh
@@ -104,18 +104,6 @@ cat << EOF >> $NEW_PRODUCT/transforms/xccdf-apply-overlay-stig.xslt
 </xsl:stylesheet>
 EOF
 
-cat << EOF >> $NEW_PRODUCT/transforms/xccdf2table-byref.xslt 
-<?xml version="1.0" encoding="utf-8" standalone="yes"?>
-<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:cdf="http://checklists.nist.gov/xccdf/1.1" xmlns:xhtml="http://www.w3.org/1999/xhtml">
-
-<xsl:import href="../../../shared/transforms/shared_xccdf2table-byref.xslt"/>
-
-<xsl:include href="constants.xslt"/>
-<xsl:include href="table-style.xslt"/>
-
-</xsl:stylesheet>
-EOF
-
 cat << EOF >> $NEW_PRODUCT/transforms/xccdf2table-cce.xslt
 <?xml version="1.0" encoding="utf-8" standalone="yes"?>
 <xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform" xmlns:cce="http://cce.mitre.org" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:cdf="http://checklists.nist.gov/xccdf/1.1" xmlns:xhtml="http://www.w3.org/1999/xhtml">
