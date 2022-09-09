a181040631d444ff5639f03fa268858b6a8c5bd7
Vojtech Polasek <vpolasek@redhat.com>
2022-09-09 14:32:53 +0200

    fix one test scenario - putting content into wrong file

--- linux_os/guide/system/auditing/auditd_configure_rules/audit_rules_networkconfig_modification/tests/augen_correct_rules_watch_rules_without_key.pass.sh
+++ linux_os/guide/system/auditing/auditd_configure_rules/audit_rules_networkconfig_modification/tests/augen_correct_rules_watch_rules_without_key.pass.sh
@@ -3,7 +3,7 @@
 
 echo "-a always,exit -F arch=b32 -S sethostname,setdomainname -F key=audit_rules_networkconfig_modification" >> /etc/audit/rules.d/networkconfig.rules
 echo "-a always,exit -F arch=b64 -S sethostname,setdomainname -F key=audit_rules_networkconfig_modification" >> /etc/audit/rules.d/networkconfig.rules
-echo "-w /etc/issue -p wa" >> /etc/audit/audit.rules
-echo "-w /etc/issue.net -p wa" >> /etc/audit/audit.rules
-echo "-w /etc/hosts -p wa" >> /etc/audit/audit.rules
-echo "-w /etc/sysconfig/network -p wa" >> /etc/audit/audit.rules
+echo "-w /etc/issue -p wa" >> /etc/audit/rules.d/networkconfig.rules
+echo "-w /etc/issue.net -p wa" >> /etc/audit/rules.d/networkconfig.rules
+echo "-w /etc/hosts -p wa" >> /etc/audit/rules.d/networkconfig.rules
+echo "-w /etc/sysconfig/network -p wa" >> /etc/audit/rules.d/networkconfig.rules
