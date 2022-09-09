6c4acca5093c092ff655d1b93e80617a8b40308d
Edgar Aguilar <edgar.aguilar@oracle.com>
2022-09-09 14:01:06 -0500

    Add tests to dconf_gnome_session_idle_user_locks
    Signed-off-by: Edgar Aguilar <edgar.aguilar@oracle.com>

---
+++ linux_os/guide/system/software/gnome/gnome_screen_locking/dconf_gnome_session_idle_user_locks/tests/comented_value.fail.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# packages = dconf,gdm
+
+. $SHARED/dconf_test_functions.sh
+
+clean_dconf_settings
+add_dconf_lock "# org/gnome/desktop/session" "idle-delay" "local.d" "00-security-settings-lock"
+
+dconf update
