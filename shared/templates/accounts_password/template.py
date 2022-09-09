feaf1ef8543250466c8013881a85594f1c6f6bd9
Edgar Aguilar <edgar.aguilar@oracle.com>
2022-09-09 11:02:18 -0500

    Update accounts_password template's OVAL
    The ZERO_COMPARISON_OPERATION wasn't being used in objects looking into
    pwquality.conf.d/*.conf
    
    Signed-off-by: Edgar Aguilar <edgar.aguilar@oracle.com>

--- shared/templates/accounts_password/template.py
+++ shared/templates/accounts_password/template.py
@@ -1,7 +1,5 @@
 from ssg.utils import parse_template_boolean_value
 
 def preprocess(data, lang):
-    if lang == "oval":
-        data["sign"] = "-?" if data["variable"].endswith("credit") else ""
     data["zero_comparison_operation"] = data.get("zero_comparison_operation", None)
     return data
