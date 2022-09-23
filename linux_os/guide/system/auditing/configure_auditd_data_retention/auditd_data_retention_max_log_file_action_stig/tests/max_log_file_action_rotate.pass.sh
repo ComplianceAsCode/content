f7cbda5d8fa887af9182d7dd84681123eb4dbc4b
Gabriel Becker <ggasparb@redhat.com>
2022-09-23 13:25:33 +0200

    Update OVAL to accept expected values from RHEL9 STIG profile.
    Both rotate and single should be accepted.

--- linux_os/guide/system/auditing/configure_auditd_data_retention/auditd_data_retention_max_log_file_action_stig/tests/max_log_file_action_rotate.pass.sh
+++ linux_os/guide/system/auditing/configure_auditd_data_retention/auditd_data_retention_max_log_file_action_stig/tests/max_log_file_action_rotate.pass.sh
@@ -2,7 +2,6 @@
 # packages = audit
 # variables = var_auditd_max_log_file_action=rotate
 
-
 . $SHARED/auditd_utils.sh
 prepare_auditd_test_enviroment
-set_parameters_value /etc/audit/auditd.conf "max_log_file_action" "suspend"
+set_parameters_value /etc/audit/auditd.conf "max_log_file_action" "rotate"
