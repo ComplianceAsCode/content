32a41b09b0b963e3fb681a5ea617e96383e2277c
Marcus Burghardt <maburgha@redhat.com>
2022-10-19 08:39:04 +0200

    Reinvent the test scenarios for firewalld_sshd_port_enabled
    The test scenarios were aligned to the old remediation approach, making
    them also incomplete and incapable to catch real cases. Once the
    remediation was robust, test scenarios also need the same level of
    robustness in order to ensure the rules is as much realistic as
    possible. They are now covering cases with multiple interfaces and
    multiple active zones. It is also covered custom SSH port.

--- linux_os/guide/services/ssh/ssh_server/firewalld_sshd_port_enabled/tests/ssh_port_enabled.pass.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# packages = firewalld
-
-firewall-cmd --add-port=22/tcp
-firewall-cmd --add-port=22/tcp --permanent
--- linux_os/guide/services/ssh/ssh_server/firewalld_sshd_port_enabled/tests/ssh_zone_nic_bounded.pass.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# packages = firewalld
-
-firewall-cmd --permanent --zone=public --add-service=ssh
-
-eth_interface=$(ip link show up | cut -d ' ' -f2 | cut -d ':' -s -f1 | grep -E '^(en|eth)')
-
-firewall-cmd --permanent --zone=public --add-interface=${eth_interface[0]}
