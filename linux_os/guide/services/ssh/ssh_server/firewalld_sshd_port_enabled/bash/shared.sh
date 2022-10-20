df8cd2df8661a3fe9fb7d5b5b493a93e1f977654
Marcus Burghardt <maburgha@redhat.com>
2022-10-20 11:03:56 +0200

    Simplify the Bash remediation in alignment to Ansible

--- linux_os/guide/services/ssh/ssh_server/firewalld_sshd_port_enabled/bash/shared.sh
+++ linux_os/guide/services/ssh/ssh_server/firewalld_sshd_port_enabled/bash/shared.sh
@@ -8,31 +8,22 @@
 {{{ bash_package_install("NetworkManager") }}}
 {{{ bash_instantiate_variables("firewalld_sshd_zone") }}}
 
-if firewall-cmd --state -q; then
+if systemctl is-active NetworkManager && systemctl is-active firewalld; then
     # First make sure the SSH service is enabled in run-time for the proper zone.
     # This is to avoid connection issues when new interfaces are addeded to this zone.
     firewall-cmd --zone="$firewalld_sshd_zone" --add-service=ssh
 
-    if systemctl is-active NetworkManager; then
-        # This will collect all NetworkManager connections names
-        readarray -t nm_connections < <(nmcli --fields CONNECTION device status | grep -v "\-\-" | tail -n+2 | sed 's/ *$//g')
-
-        # If the connection is not yet assigned to a firewalld zone, assign it to the proper zone.
-        # This will not change connections which are already assigned to any firewalld zone.
-        for connection in "${nm_connections[@]}"; do
-            current_zone=$(nmcli -f connection.zone connection show "$connection" | awk '{ print $2}')
-            if [ $current_zone = "--" ]; then
-                nmcli connection modify "$connection" connection.zone $firewalld_sshd_zone
-            fi
-        done
-        systemctl restart NetworkManager
-    else
-        echo "
-        NetworkManager service is not active. Remediation aborted!
-        This remediation could not be applied because it depends on NetworkManager service running.
-        The service is not started by this remediation in order to prevent connection issues."
-        exit 1
-    fi
+    # This will collect all NetworkManager connections names
+    readarray -t nm_connections < <(nmcli --fields CONNECTION device status | grep -v "\-\-" | tail -n+2 | sed 's/ *$//g')
+    # If the connection is not yet assigned to a firewalld zone, assign it to the proper zone.
+    # This will not change connections which are already assigned to any firewalld zone.
+    for connection in "${nm_connections[@]}"; do
+        current_zone=$(nmcli -f connection.zone connection show "$connection" | awk '{ print $2}')
+        if [ $current_zone = "--" ]; then
+            nmcli connection modify "$connection" connection.zone $firewalld_sshd_zone
+        fi
+    done
+    systemctl restart NetworkManager
 
     # Active zones are zones with at least one interface assigned to it.
     # It is possible that traffic is comming by any active interface and consequently any
@@ -44,8 +35,8 @@ if firewall-cmd --state -q; then
     firewall-cmd --reload
 else
     echo "
-    firewalld service is not active. Remediation aborted!
-    This remediation could not be applied because it depends on firewalld service running.
+    firewalld and NetworkManager services are not active. Remediation aborted!
+    This remediation could not be applied because it depends on firewalld and NetworkManager services running.
     The service is not started by this remediation in order to prevent connection issues."
     exit 1
 fi
