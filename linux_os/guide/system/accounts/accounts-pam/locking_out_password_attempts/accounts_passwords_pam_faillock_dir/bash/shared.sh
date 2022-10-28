c1773d532fa145630e3c50160dc6deb38d4be1cc
Marcus Burghardt <maburgha@redhat.com>
2022-10-28 16:32:51 +0200

    Increment remediation to ensure correct SELinux fcontext
    This remediation was setting the permanent dir for tally logs, but was
    not ensuring this new directory was properly configured with the
    faillog_t SELinux context. As a consequence, in a second assessment of
    relevant profiles would fail in the account_password_selinux_faillock_dir
    rule. The Ansible and Bash remediation were updated to ensure the proper
    SELinux file context.

--- linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_passwords_pam_faillock_dir/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_passwords_pam_faillock_dir/bash/shared.sh
@@ -8,3 +8,11 @@
 
 {{{ bash_pam_faillock_enable() }}}
 {{{ bash_pam_faillock_parameter_value("dir", "$var_accounts_passwords_pam_faillock_dir") }}}
+
+{{{ bash_package_install("python3-libselinux") }}}
+{{{ bash_package_install("python3-policycoreutils") }}}
+{{{ bash_package_install("policycoreutils-python-utils") }}}
+
+mkdir -p "$var_accounts_passwords_pam_faillock_dir"
+semanage fcontext -a -t faillog_t "$var_accounts_passwords_pam_faillock_dir(/.*)?"
+restorecon -R -v "$var_accounts_passwords_pam_faillock_dir"
