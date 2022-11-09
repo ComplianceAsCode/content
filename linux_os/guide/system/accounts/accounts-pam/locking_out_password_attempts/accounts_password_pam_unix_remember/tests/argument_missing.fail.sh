741a4daf96aec3f5d37c17a4c05926f4f0c95d0a
teacup-on-rockingchair <315160+teacup-on-rockingchair@users.noreply.github.com>
2022-11-09 18:43:53 +0200

    Add sle platform for relevant accounts_password_pam_unix_remember test scripts
    Do not enable authselect-based tests since they are irrelevant for SLE-based platforms

--- linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_password_pam_unix_remember/tests/argument_missing.fail.sh
+++ linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_password_pam_unix_remember/tests/argument_missing.fail.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-# platform = Oracle Linux 7,Red Hat Enterprise Linux 7,Red Hat Virtualization 4,multi_platform_fedora
+# platform = Oracle Linux 7,Red Hat Enterprise Linux 7,Red Hat Virtualization 4,multi_platform_fedora,multi_platform_sle
 
+{{% if product in [ "sle12", "sle15" ] %}}
+for auth_file in common-password password-auth; do
+{{% else %}}
 for auth_file in system-auth password-auth; do
+{{% endif %}}
     config_file=/etc/pam.d/${auth_file}
     if grep -q "pam_pwhistory\.so.*remember=" "$config_file" ; then
         sed -i --follow-symlinks "/pam_pwhistory\.so/ s/\(remember *= *\).*//" $config_file
