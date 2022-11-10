8c6d618070476bd81edd0524c895a3497fc902a6
Watson Sato <wsato@redhat.com>
2022-11-10 17:48:55 +0100

    accounts_password_pam_retry: Add test for dupes and conflicts
    Add test scenarios to ensure that conflicting values are failing
    and that duplicated rule are passing.

---
+++ linux_os/guide/system/accounts/accounts-pam/password_quality/password_quality_pwquality/accounts_password_pam_retry/tests/pwquality_conf_conflicting_values.fail.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# variables = var_password_pam_retry=3
+
+source common.sh
+
+CONF_FILE="/etc/security/pwquality.conf"
+retry_cnt=3
+
+truncate -s 0 $CONF_FILE
+
+echo "retry = 3" >> $CONF_FILE
+echo "retry = 4" >> $CONF_FILE
