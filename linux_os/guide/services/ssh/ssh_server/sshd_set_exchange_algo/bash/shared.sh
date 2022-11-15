5a9b11cb1a8c444e7bc830c0152d24db72334e5b
anivan-suse <anastasija.ivanovic@suse.com>
2022-11-15 15:50:04 +0100

    Add bash dir

---
+++ linux_os/guide/services/ssh/ssh_server/sshd_set_exchange_algo/bash/shared.sh
@@ -0,0 +1,11 @@
+# platform = multi_platform_sle
+
+# Edit the /etc/ssh/sshd_config file add/modify the KexAlgorithms line to contain a comma
+# separated list of the site approved key exchange algorithms
+
+# Ensure there are no weak kex algs:
+# diffie-hellman-group1-sha1
+# diffie-hellman-group14-sha1
+# diffie-hellman-group-exchange-sha1
+
+
