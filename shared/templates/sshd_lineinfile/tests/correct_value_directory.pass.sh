9c84a764ad810d9b10861a82fd79eb7e47ebc7bd
Edgar Aguilar <edgar.aguilar@oracle.com>
2022-11-24 11:43:35 -0600

    Update sshd_lineinfile template tests
    Signed-off-by: Edgar Aguilar <edgar.aguilar@oracle.com>

--- shared/templates/sshd_lineinfile/tests/correct_value_directory.pass.sh
+++ shared/templates/sshd_lineinfile/tests/correct_value_directory.pass.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-# platform = multi_platform_fedora,Red Hat Enterprise Linux 9
+# platform = multi_platform_fedora,Oracle Linux 8,Oracle Linux 9,Red Hat Enterprise Linux 9
 
 mkdir -p /etc/ssh/sshd_config.d
 touch /etc/ssh/sshd_config.d/nothing
+
+{{% if product in ["ol8", "ol9"] %}}
+{{{ bash_replace_or_append("/etc/ssh/sshd_config", "Include", "/etc/ssh/sshd_config.d/*.conf", "%s %s") }}}
+{{% endif %}}
 
 {{{ bash_sshd_remediation(parameter=PARAMETER, value=VALUE, config_is_distributed=true) -}}}
--- shared/templates/sshd_lineinfile/tests/duplicated_param_directory.pass.sh
+++ shared/templates/sshd_lineinfile/tests/duplicated_param_directory.pass.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-# platform = multi_platform_fedora,Red Hat Enterprise Linux 9
+# platform = multi_platform_fedora,Oracle Linux 8,Oracle Linux 9,Red Hat Enterprise Linux 9
 
 mkdir -p /etc/ssh/sshd_config.d
 touch /etc/ssh/sshd_config.d/nothing
+
+{{% if product in ["ol8", "ol9"] %}}
+{{{ bash_replace_or_append("/etc/ssh/sshd_config", "Include", "/etc/ssh/sshd_config.d/*.conf", "%s %s") }}}
+{{% endif %}}
 
 if grep -q "^\s*{{{ PARAMETER }}}" /etc/ssh/sshd_config /etc/ssh/sshd_config.d/* ; then
     sed -i "/^\s*{{{ PARAMETER }}}.*/Id" /etc/ssh/sshd_config /etc/ssh/sshd_config.d/*
