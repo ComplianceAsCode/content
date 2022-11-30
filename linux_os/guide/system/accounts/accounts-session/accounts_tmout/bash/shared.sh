8fd54ea2693cbadc9c6ce8a8fdbdcfd3b3a30eb8
Watson Sato <wsato@redhat.com>
2022-11-30 15:23:29 +0100

    Ensure the remediation updates occurences of TMOUT
    The Ansible remediation is not removing conflicting settings in other
    files.
    The bash remediation was not recognizing more complex occurrences of
    TMOUT, like: declare -xr TMOUT=

--- linux_os/guide/system/accounts/accounts-session/accounts_tmout/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-session/accounts_tmout/bash/shared.sh
@@ -6,8 +6,8 @@
 tmout_found=0
 
 for f in /etc/profile /etc/profile.d/*.sh; do
-    if grep --silent '^\s*TMOUT' $f; then
-        sed -i -E "s/^(\s*)TMOUT\s*=\s*(\w|\$)*(.*)$/declare -xr TMOUT=$var_accounts_tmout\3/g" $f
+    if grep --silent '^[^#].*TMOUT' $f; then
+        sed -i -E "s/^(.*)TMOUT\s*=\s*(\w|\$)*(.*)$/declare -xr TMOUT=$var_accounts_tmout\3/g" $f
         tmout_found=1
     fi
 done
