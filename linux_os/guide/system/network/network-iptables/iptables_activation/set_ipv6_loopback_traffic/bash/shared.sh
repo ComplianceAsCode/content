816e30b68950e7a9487f857d28fa66eb21452411
Christian Hagenest <christian.hagenest@suse.com>
2022-12-07 16:55:09 +0100

    fix bash syntax for set_ipv6_loopback_traffic

--- linux_os/guide/system/network/network-iptables/iptables_activation/set_ipv6_loopback_traffic/bash/shared.sh
+++ linux_os/guide/system/network/network-iptables/iptables_activation/set_ipv6_loopback_traffic/bash/shared.sh
@@ -1,6 +1,6 @@
 # platform = multi_platform_sle
 
-if [ $(sysctl -n net.ipv6.conf.all.disable_ipv6) -eq 0 ]; then
+if [ "$(sysctl -n net.ipv6.conf.all.disable_ipv6)" -eq 0 ]; then
   # IPv6 is not disabled, so run the script
   ip6tables -A INPUT -i lo -j ACCEPT
   ip6tables -A OUTPUT -o lo -j ACCEPT
