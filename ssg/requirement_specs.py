23d4114eb6499891f1ee4591790ec5d37aca4690
Evgeny Kolesnikov <ekolesni@redhat.com>
2022-12-12 13:22:27 +0100

    Add support for intermediary version of setuptools to requirements_specs.py
    At around v40.8 the `post` attribute of `Version` wasn't available.

--- ssg/requirement_specs.py
+++ ssg/requirement_specs.py
@@ -10,15 +10,15 @@ pkg_resources.safe_name = lambda name: re.sub('[^A-Za-z0-9_.]+', '-', name)
 
 def _evr_from_tuple(version_tuple):
     # This is a version tuple from setuptools 0.9.8
-    # 1.22.33-444 -> '00000011', '00000022', '00000033', '*final-', '00000444', '*final')
+    # 1.22.33-444 -> ('00000011', '00000022', '00000033', '*final-', '00000444', '*final')
     if isinstance(version_tuple, tuple):
-        # TODO: We do not support `epoch` at this moment, it is always 0
+        # TODO: We do not support `epoch` at this moment, it is always None
         evr = {'epoch': None, 'version': None, 'release': None}
         component = 'version'
         elements = []
         for el in version_tuple:
             if el.startswith('*'):
-                evr[component] = '.'.join(elements if elements else '0')
+                evr[component] = '.'.join(elements if elements else ['0'])
                 elements = []
                 if el.endswith('final-'):
                     component = 'release'
@@ -31,10 +31,22 @@ def _evr_from_tuple(version_tuple):
 
 def _evr_from_version_object(version_object):
     if isinstance(version_object, pkg_resources.packaging.version.Version):
-        # TODO: We do not support `epoch` at this moment, it is always 0
+        # TODO: We do not support `epoch` at this moment, it is always None
+        if hasattr(version_object, 'post'):
+            post = version_object.post
+        elif hasattr(version_object, '_version') and hasattr(version_object._version, 'post'):
+            # There was a range of versions around 40.8 of setuptools
+            # where `post` hasn't been a public property of the Version class.
+            # In _version it has structure of ('post', 12) for version 1.2.3-12.
+            post = version_object._version.post[1] if version_object._version.post else None
+        else:
+            raise ValueError('Invalid pkg_resources.packaging.version.Version object: '
+                             'no Version.post or Version._version.post attributes.'
+                             'Current version of setuptools is not supported.')
+
         return {'epoch': None,
                 'version': version_object.base_version,
-                'release': str(version_object.post) if version_object.post else None}
+                'release': str(post) if post else None}
     raise ValueError('Invalid version object: %s, '
                      'expected: pkg_resources.packaging.version.Version' % repr(version_object))
 
