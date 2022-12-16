d94f5b637473933300e8ddd7a33481be9bd4a953
Marcus Burghardt <maburgha@redhat.com>
2022-12-16 13:26:36 +0100

    Update the test scenario scripts to cover new scenarios
    The existing and relevant test scenarios were udpated to consider the
    /etc/security/pwhistory.conf file. It was included a test scenario for
    conflicting settings.

--- linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_password_pam_pwhistory_remember_system_auth/tests/authselect_missing_line.fail.sh
+++ linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_password_pam_pwhistory_remember_system_auth/tests/authselect_missing_line.fail.sh
@@ -3,9 +3,15 @@
 # platform = Oracle Linux 8,Oracle Linux 9,Red Hat Enterprise Linux 8,Red Hat Enterprise Linux 9,multi_platform_fedora
 # variables = var_password_pam_remember=5,var_password_pam_remember_control_flag=requisite
 
-authselect create-profile hardening -b sssd
-CUSTOM_PROFILE="custom/hardening"
-authselect select $CUSTOM_PROFILE --force
-CUSTOM_SYSTEM_AUTH="/etc/authselect/$CUSTOM_PROFILE/system-auth"
-sed -i --follow-symlinks '/.*pam_pwhistory\.so/d' $CUSTOM_SYSTEM_AUTH
+if authselect list-features minimal | grep -q with-pwhistory; then
+    authselect select sssd --force
+    authselect disable-feature with-pwhistory
+else
+    authselect create-profile hardening -b sssd
+    CUSTOM_PROFILE="custom/hardening"
+    authselect select $CUSTOM_PROFILE --force
+    CUSTOM_SYSTEM_AUTH="/etc/authselect/$CUSTOM_PROFILE/system-auth"
+    sed -i --follow-symlinks '/.*pam_pwhistory\.so/d' $CUSTOM_SYSTEM_AUTH
+fi
 authselect apply-changes -b
+> /etc/security/pwhistory.conf
