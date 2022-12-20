8a53f4a87e20165b7002aa0f8051fe20f4f57956
teacup-on-rockingchair <315160+teacup-on-rockingchair@users.noreply.github.com>
2022-12-20 06:26:25 +0200

    Drop dead code from bash remediation and some double quote to prevent globbing and word splitting.

--- linux_os/guide/system/accounts/accounts-session/accounts_users_netrc_file_permissions/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-session/accounts_users_netrc_file_permissions/bash/shared.sh
@@ -5,10 +5,6 @@
 # disruption = low
 
 for user in $(awk -F':' '{ if ($4 >= {{{ gid_min }}} && $4 != 65534) print $1 }' /etc/passwd); do
-    home_dir=$(getent passwd $user | cut -d: -f6)
-    group=$(getent passwd $user | cut -d: -f4)
-    # Only update the group-ownership when necessary. This will avoid changing the inode timestamp
-    # when the group is already defined as expected, therefore not impacting in possible integrity
-    # check systems that also check inodes timestamps.
-    find $home_dir/.netrc -exec chmod 0600 {} \;
+    home_dir=$(getent passwd "$user" | cut -d: -f6)
+    find "${home_dir}/.netrc" -exec chmod 0600 {} \;
 done
