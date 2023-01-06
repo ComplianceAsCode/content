5eacd51c6fd049598b9a0e4b7426d149dd7eb9ba
Eduardo Barretto <eduardo.barretto@canonical.com>
2023-01-06 15:55:36 +0100

    accounts_password_set_max_life_existing: tests: Attempt to unify the
    tests independent of the benchmark

--- linux_os/guide/system/accounts/accounts-restrictions/password_expiration/accounts_password_set_max_life_existing/tests/incorrect_max_pass_age.fail.sh
+++ linux_os/guide/system/accounts/accounts-restrictions/password_expiration/accounts_password_set_max_life_existing/tests/incorrect_max_pass_age.fail.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-MAX_PASS_AGE=99999
-
-USERNAME="testuser_123"
-useradd $USERNAME
-echo "cac_test_pass" | passwd --stdin $USERNAME
-
-chage -M $MAX_PASS_AGE $USERNAME
+# variables = var_accounts_minimum_age_login_defs=1,var_accounts_maximum_age_login_defs=60
+# make existing entries pass
+for acct in $(awk -F: '(/^[^:]+:[^!*]/ && ($5 > var || $5 == "")) {print $1}' /etc/shadow ); do
+    chage -M 60 -d $(date +%Y-%m-%d) $acct
+done
+# Add a failing test.
+# 9000 is picked as an arbitrarily large number.
+echo 'max-test-user:$1$q.YkdxU1$ADmXcU4xwPrM.Pc.dclK81:18648:1:9000::::' >> /etc/shadow
+echo "max-test-user:x:50000:1000::/:/usr/bin/bash" >> /etc/passwd
