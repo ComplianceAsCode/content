f10188ecbff84f20bc372b02fd12b640d8c243ff
teacup-on-rockingchair <315160+teacup-on-rockingchair@users.noreply.github.com>
2023-01-08 14:15:52 +0200

    Add basic tests for accounts_password_last_change_is_in_past

---
+++ linux_os/guide/system/accounts/accounts-restrictions/password_storage/accounts_password_last_change_is_in_past/tests/all_password_change_time_in_past.pass.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+TODAY="$(($(date +%s)/86400))"
+DAY_AGO="$(( TODAY - 1 ))"
+
+awk -v newdate="$DAY_AGO" 'BEGIN { FS=":"; OFS = ":"} {$3=newdate; print}' /etc/shadow > /etc/shadow_new
+
+mv /etc/shadow_new /etc/shadow
