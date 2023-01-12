adc79a95909f78e345139cb520400efeb139c40f
teacup-on-rockingchair <315160+teacup-on-rockingchair@users.noreply.github.com>
2023-01-12 07:37:24 +0200

    Add ansible remediation for sshd_use_strong_kex rule

--- linux_os/guide/services/ssh/ssh_server/sshd_use_strong_kex/bash/shared.sh
+++ linux_os/guide/services/ssh/ssh_server/sshd_use_strong_kex/bash/shared.sh
@@ -1,4 +1,8 @@
 # platform = multi_platform_all
+# reboot = false
+# strategy = restrict
+# complexity = low
+# disruption = low
 
 {{{ bash_sshd_config_set(parameter="KexAlgorithms", value="ecdh-sha2-nistp256,ecdh-sha2-nistp384,ecdh-sha2-nistp521,diffie-hellman-group-exchange-sha256,diffie-hellman-group16-sha512,diffie-hellman-group18-sha512,diffie-hellman-group14-sha256") }}}
 
