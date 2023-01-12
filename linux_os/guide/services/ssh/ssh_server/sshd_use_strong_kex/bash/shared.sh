59fc150b06db95c18aec32b82c9457d008dc500d
teacup-on-rockingchair <315160+teacup-on-rockingchair@users.noreply.github.com>
2023-01-12 07:45:34 +0200

    Use variable containing strong KEX algorithms for remediations

--- linux_os/guide/services/ssh/ssh_server/sshd_use_strong_kex/bash/shared.sh
+++ linux_os/guide/services/ssh/ssh_server/sshd_use_strong_kex/bash/shared.sh
@@ -3,6 +3,7 @@
 # strategy = restrict
 # complexity = low
 # disruption = low
+{{{ bash_instantiate_variables("sshd_strong_kex") }}}
 
-{{{ bash_sshd_config_set(parameter="KexAlgorithms", value="ecdh-sha2-nistp256,ecdh-sha2-nistp384,ecdh-sha2-nistp521,diffie-hellman-group-exchange-sha256,diffie-hellman-group16-sha512,diffie-hellman-group18-sha512,diffie-hellman-group14-sha256") }}}
+{{{ bash_sshd_config_set(parameter="KexAlgorithms", value="$sshd_strong_kex") }}}
 
