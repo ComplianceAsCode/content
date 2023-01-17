2b8bf7e7df11bfc0eaf4370ab8a77f0fac47d942
Vojtech Polasek <vpolasek@redhat.com>
2023-01-17 14:13:14 +0100

    fix bash remediation
    fix indentation
    replace file loop with something more robust, taking advice of Shellcheck
    quoting the variable name

--- linux_os/guide/system/auditing/auditd_configure_rules/audit_rules_immutable_login_uids/bash/shared.sh
+++ linux_os/guide/system/auditing/auditd_configure_rules/audit_rules_immutable_login_uids/bash/shared.sh
@@ -7,11 +7,11 @@ if grep -q '^\s*ExecStartPost=-/sbin/auditctl' /usr/lib/systemd/system/auditd.se
   fi
 else
   immutable_found=0
-  for f in /etc/audit/rules.d/*.rules; do
-    if grep -q '^\s*--loginuid-immutable\s*$' $f; then
-  immutable_found=1
+  while IFS= read -r -d '' f; do
+    if grep -q '^\s*--loginuid-immutable\s*$' "$f"; then
+      immutable_found=1
     fi
-  done
+  done <    <(find /etc/audit/rules.d -maxdepth 1 -name '*.rules' -print0)
   if [ $immutable_found -eq 0 ]; then
     echo "--loginuid-immutable" >> /etc/audit/rules.d/immutable.rules
   fi
