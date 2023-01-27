1fb9537b4374e9b276dec05a2dde90b0bd7b6efa
rchikov <rumen.chikov@suse.com>
2023-01-27 10:02:43 +0100

    Some corrections

--- linux_os/guide/system/network/network-nftables/set_nftables_table/bash/shared.sh
+++ linux_os/guide/system/network/network-nftables/set_nftables_table/bash/shared.sh
@@ -2,14 +2,12 @@
 
 #Set nftables family name
 {{{ bash_instantiate_variables("var_nftables_family") }}}
-NETWORK_LEVEL=$var_nftables_family
 
 #Set nftables table name
 {{{ bash_instantiate_variables("var_nftables_table") }}} 
-TABLE_NAME=$var_nftables_table
 
 IS_TABLE=$(nft list tables)
 if [ -z "$IS_TABLE" ]
 then
-  nft create table "$NETWORK_LEVEL" "$TABLE_NAME"
+  nft create table "$var_nftables_family" "$var_nftables_table"
 fi
