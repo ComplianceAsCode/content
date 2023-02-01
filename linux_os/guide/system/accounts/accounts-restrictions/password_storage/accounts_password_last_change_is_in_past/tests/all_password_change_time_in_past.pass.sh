ed1bfeb0c8c9cbbde817adba9f32fae7fb6b41de
teacup-on-rockingchair <315160+teacup-on-rockingchair@users.noreply.github.com>
2023-02-01 07:50:18 +0200

    Style fixes
    - Fix line wrap to 99 chars in OVAL and tests
    - Fix a grammar error in the description in rule and some new&empty line fixes
    - Drop unused var_check param in OVAL var state section
    
    Thanks @marcusburghardt for the feedback on this

--- linux_os/guide/system/accounts/accounts-restrictions/password_storage/accounts_password_last_change_is_in_past/tests/all_password_change_time_in_past.pass.sh
+++ linux_os/guide/system/accounts/accounts-restrictions/password_storage/accounts_password_last_change_is_in_past/tests/all_password_change_time_in_past.pass.sh
@@ -3,6 +3,7 @@
 TODAY="$(($(date +%s)/86400))"
 DAY_AGO="$(( TODAY - 1 ))"
 
-awk -v newdate="$DAY_AGO" 'BEGIN { FS=":"; OFS = ":"} {$3=newdate; print}' /etc/shadow > /etc/shadow_new
+awk -v newdate="$DAY_AGO" 'BEGIN { FS=":"; OFS = ":"} 
+    {$3=newdate; print}' /etc/shadow > /etc/shadow_new
 
 mv /etc/shadow_new /etc/shadow
