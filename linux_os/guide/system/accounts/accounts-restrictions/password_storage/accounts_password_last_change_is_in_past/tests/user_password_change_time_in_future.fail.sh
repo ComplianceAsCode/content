d5b29aab36c8eec08e84f848181d2f925a211376
teacup-on-rockingchair <315160+teacup-on-rockingchair@users.noreply.github.com>
2023-02-01 07:50:18 +0200

    Style fixes
    - Fix line wrap to 99 chars in OVAL and tests
    - Fix a grammar error in the description in rule and some new&empty line fixes
    - Drop unused var_check param in OVAL var state section
    
    Thanks @marcusburghardt for the feedback on this

--- linux_os/guide/system/accounts/accounts-restrictions/password_storage/accounts_password_last_change_is_in_past/tests/user_password_change_time_in_future.fail.sh
+++ linux_os/guide/system/accounts/accounts-restrictions/password_storage/accounts_password_last_change_is_in_past/tests/user_password_change_time_in_future.fail.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-useradd testUserChage1
-echo -e "testpass\ntestpass" | passwd testUserChage1
+TEST_USER="testUserChage1"
+
+useradd $TEST_USER
+echo -e "testpass\ntestpass" | passwd $TEST_USER
 
 TODAY="$(($(date +%s)/86400))"
 TOMORROW="$(( TODAY + 1 ))"
 
-awk -v newdate="$TOMORROW" 'BEGIN { FS=":"; OFS = ":"} {  if($1 =="testUserChage1") ($3=newdate); print}' /etc/shadow > /etc/shadow_new
-
+awk -v newdate="$TOMORROW" -v test_user="$TEST_USER" 'BEGIN { FS=":"; OFS = ":"} \
+    {  if($1 ==test_user) ($3=newdate); print}' /etc/shadow > /etc/shadow_new
 mv /etc/shadow_new /etc/shadow
