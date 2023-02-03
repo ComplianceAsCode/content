564945f8ee95d0a2514d679369df499c7fe18329
Nico Truzzolino <nico.truzzolino@gmx.de>
2023-02-03 11:23:53 +0100

    Separate variables for banners This allows a separate regex for each file.

--- linux_os/guide/system/accounts/accounts-banners/banner_etc_motd/tests/banner_etc_motd_default.pass.sh
+++ linux_os/guide/system/accounts/accounts-banners/banner_etc_motd/tests/banner_etc_motd_default.pass.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-# default banner from login_banner_text.var
+# default banner from motd_banner_text.var
 echo "Authorized uses only. All activity may be monitored and reported." > /etc/motd
