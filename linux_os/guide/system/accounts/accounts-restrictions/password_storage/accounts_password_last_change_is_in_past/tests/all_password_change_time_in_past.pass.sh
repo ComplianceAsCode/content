413f10b1096cc9f4f59cf7459142e49cad372f25
teacup-on-rockingchair <315160+teacup-on-rockingchair@users.noreply.github.com>
2023-02-07 17:23:50 +0200

    Make running tests in special container env pass

--- linux_os/guide/system/accounts/accounts-restrictions/password_storage/accounts_password_last_change_is_in_past/tests/all_password_change_time_in_past.pass.sh
+++ linux_os/guide/system/accounts/accounts-restrictions/password_storage/accounts_password_last_change_is_in_past/tests/all_password_change_time_in_past.pass.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+# packages = passwd
 
 TODAY="$(($(date +%s)/86400))"
 DAY_AGO="$(( TODAY - 1 ))"
 
-awk -v newdate="$DAY_AGO" 'BEGIN { FS=":"; OFS = ":"} 
+awk -v newdate="$DAY_AGO" 'BEGIN { FS=":"; OFS = ":"}
     {$3=newdate; print}' /etc/shadow > /etc/shadow_new
 
 mv /etc/shadow_new /etc/shadow
