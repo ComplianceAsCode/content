06f97c13c34bde87bd1b018dffeaf5b871f38bb4
Jan Černý <jcerny@redhat.com>
2023-02-08 14:37:54 +0100

    Use distributed configuration in tests
    Use distributed configuration in templated test scenarios for the
    `sshd_lineinfile` template. Some products (fedora and rhel9 at this
    moment) account for distributed configuration of sshd options. That
    means that the test scenarios also should work with distributed
    configuration, the test scenarios should make sure that there is no
    offending within `/etc/ssh/sshd_config.d` setting if they expect a
    `pass` result.
    
    Fixes: https://github.com/ComplianceAsCode/content/issues/10169

--- shared/templates/sshd_lineinfile/tests/correct_value.pass.sh
+++ shared/templates/sshd_lineinfile/tests/correct_value.pass.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-{{{ bash_sshd_remediation(parameter=PARAMETER, value=VALUE, config_is_distributed=false) -}}}
+{{{ bash_sshd_remediation(parameter=PARAMETER, value=VALUE, config_is_distributed=sshd_distributed_config) -}}}
