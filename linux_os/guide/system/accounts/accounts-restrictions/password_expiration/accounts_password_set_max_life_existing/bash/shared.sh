c2dfe8afc900bdc832ec35270d797fb239ad750e
rchikov <rumen.chikov@suse.com>
2023-02-09 16:57:55 +0100

    Fixes of errors

--- linux_os/guide/system/accounts/accounts-restrictions/password_expiration/accounts_password_set_max_life_existing/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-restrictions/password_expiration/accounts_password_set_max_life_existing/bash/shared.sh
@@ -7,8 +7,8 @@
 {{{ bash_instantiate_variables("var_accounts_maximum_age_login_defs") }}}
 
 {{% if product in ["sle12", "sle15"] %}}
-usrs_max_pass_age=( $(awk -F: '$5 > $var_accounts_maximum_age_login_defs || $5 == "" {print $1}' /etc/shadow) )
-for i in ${usrs_max_pass_age[@]}
+usrs_max_pass_age=( "$(awk -F: '$5 > $var_accounts_maximum_age_login_defs || $5 == "" {print $1}' /etc/shadow)" )
+for i in "${usrs_max_pass_age[@]}"
 do
   passwd -q -x $((var_accounts_maximum_age_login_defs)) $i
 done
