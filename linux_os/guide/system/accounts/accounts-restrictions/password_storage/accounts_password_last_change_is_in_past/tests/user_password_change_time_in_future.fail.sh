7ebdcd74b65e770bdb88b47d5a623279637aff3e
teacup-on-rockingchair <315160+teacup-on-rockingchair@users.noreply.github.com>
2023-02-10 10:20:33 +0200

    Update linux_os/guide/system/accounts/accounts-restrictions/password_storage/accounts_password_last_change_is_in_past/tests/user_password_change_time_in_future.fail.sh
    Co-authored-by: Marcus Burghardt <2074099+marcusburghardt@users.noreply.github.com>

--- linux_os/guide/system/accounts/accounts-restrictions/password_storage/accounts_password_last_change_is_in_past/tests/user_password_change_time_in_future.fail.sh
+++ linux_os/guide/system/accounts/accounts-restrictions/password_storage/accounts_password_last_change_is_in_past/tests/user_password_change_time_in_future.fail.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # remediation = none
-# packages = passwd
 
 # create valid testuser
 echo 'testuservalid:$6$rounds=65536$exIFis0tobKRcGBk$b.UR.Z8h96FdxJ1bgA/vhdnp0Lsm488swdILNguQX/5qH5hdmClyYb5xk3TpELXWzr4JOiTlHfRkPsXSjMPjv0:19396:1:60:7:35::' >> /etc/shadow
