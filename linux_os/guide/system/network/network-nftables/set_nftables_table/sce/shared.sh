d862443e2dae62052da4c0f1d23b1acdc622fc4a
Marcus Burghardt <maburgha@redhat.com>
2023-02-21 13:07:25 +0100

    Enable SCE check in set_nftables_table for RHEL

--- linux_os/guide/system/network/network-nftables/set_nftables_table/sce/shared.sh
+++ linux_os/guide/system/network/network-nftables/set_nftables_table/sce/shared.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-#
-# platform = multi_platform_ubuntu
+# platform = multi_platform_rhel,multi_platform_ubuntu
 # check-import = stdout
-#
 
-tbl_output=$(nft list tables)
+tbl_output=$(nft list tables | grep inet)
 if [ -z "${tbl_output}" ]; then
     exit ${XCCDF_RESULT_FAIL}
 fi
