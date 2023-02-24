bd8ea9911e9c591c3f8a33eb61c09a8dbf074244
rchikov <rumen.chikov@suse.com>
2023-02-24 10:55:24 +0100

    New tests added

---
+++ linux_os/guide/system/software/sudo/sudo_require_reauthentication/tests/wrong_value_3.fail.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# packages = sudo
+
+# Remove Defaults timestamp_timeout from sudoers
+if grep -q 'timestamp_timeout' /etc/sudoers; then
+	sed -i '/.*timestamp_timeout.*/d' /etc/sudoers
+fi
+
+# Set Defaults timestamp_timeout in /etc/sudoers.d/00-complianceascode-test.conf
+if grep -q 'timestamp_timeout' /etc/sudoers.d/00-complianceascode-test.conf; then
+	sed -i 's/.*timestamp_timeout.*/Defaults timestamp_timeout=+0/' /etc/sudoers.d/00-complianceascode-test.conf
+else
+	echo "Defaults timestamp_timeout=+0" >> /etc/sudoers.d/00-complianceascode-test.conf
+fi
