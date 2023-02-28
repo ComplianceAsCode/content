d4996722369485f3f680d83a6eae242438cd5ca3
Marcus Burghardt <maburgha@redhat.com>
2023-02-28 13:38:47 +0100

    Improve test scenatio scripts
    The scripts were reviewed and aligned to the OVAL and remediation.
    New relevant scenarios were included to cover partially compliant
    systems and also systems without user passwords.

--- linux_os/guide/system/accounts/accounts-restrictions/password_expiration/accounts_set_post_pw_existing/tests/correct_set_post_pw.pass.sh
+++ linux_os/guide/system/accounts/accounts-restrictions/password_expiration/accounts_set_post_pw_existing/tests/correct_set_post_pw.pass.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
+# variables = var_account_disable_post_pw_expiration=30
 
-SECURE_INACTIVE=30
+INACTIVE_VALUE=30
+TEST_USER="cac_testuser"
 
-users_to_set=( $(awk -v var=\"$SECURE_INACTIVE\" -F: '$7 > var || $7 == "" {print $1}' /etc/shadow) )
-for i in ${users_to_set[@]};
+# create valid testuser entry in /etc/shadow
+useradd $TEST_USER
+sed -E -i "s/($TEST_USER:)(.*)/\1\$6\$exIFis0tobKRcGBk\$b.UR.Z8h96FdxJ1bgA\/vhdnp0Lsm488swdILNguQX\/5qH5hdmClyYb5xk3TpELXWzr4JOiTlHfRkPsXSjMPjv0:19396:1:60:7:$INACTIVE_VALUE::/" /etc/shadow
+
+# ensure existing users with a password defined also have the correct value defined
+users_to_set=($(awk -v var=\"$INACTIVE_VALUE\" -F: '(($7 > var || $7 == \"\") && $2 ~ /^\$/) {print $1}' /etc/shadow))
+for user in ${users_to_set[@]};
 do
-   chage --inactive $SECURE_INACTIVE $i
+   chage --inactive $INACTIVE_VALUE $user
 done
