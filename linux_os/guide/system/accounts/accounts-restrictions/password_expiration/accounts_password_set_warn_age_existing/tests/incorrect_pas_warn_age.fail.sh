0576070c5febd204a65e021da913cdbc2abe9e16
Marcus Burghardt <maburgha@redhat.com>
2023-03-03 10:29:54 +0100

    Align test scenarios to the OVAL updates
    The test scenarios were updated to ignore users without a password
    defined. A new test was included to test environments without any user
    with password. Finally, it was ensured at least a user exists for some
    tests in order to avoid failures caused by different test environments.

--- linux_os/guide/system/accounts/accounts-restrictions/password_expiration/accounts_password_set_warn_age_existing/tests/incorrect_pas_warn_age.fail.sh
+++ linux_os/guide/system/accounts/accounts-restrictions/password_expiration/accounts_password_set_warn_age_existing/tests/incorrect_pas_warn_age.fail.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
+# variables = var_accounts_password_warn_age_login_defs=7
 
-BAD_PAS_WARN_AGE=3
+WARN_AGE_VALUE=3
+TEST_USER="cac_testuser"
 
-# Configure the OS to enforce a PASS_WARN_AGE < 7 of each accout
-system_users=( $(awk -F: '{print $1}' /etc/shadow) )
-for i in ${system_users[@]};
+# create valid testuser entry in /etc/shadow
+useradd $TEST_USER
+sed -E -i "s/($TEST_USER:)(.*)/\1\$6\$exIFis0tobKRcGBk\$b.UR.Z8h96FdxJ1bgA\/vhdnp0Lsm488swdILNguQX\/5qH5hdmClyYb5xk3TpELXWzr4JOiTlHfRkPsXSjMPjv0:19396:1:60:$WARN_AGE_VALUE:::/" /etc/shadow
+
+# ensure existing users with a password defined also have the correct value defined
+users_to_set=($(awk -v var=\"$WARN_AGE_VALUE\" -F: '(($6 < var || $6 == \"\") && $2 ~ /^\$/) {print $1}' /etc/shadow))
+for user in ${users_to_set[@]};
 do
-  chage --warndays $BAD_PAS_WARN_AGE $i
+  chage --warndays $WARN_AGE_VALUE $user
 done
