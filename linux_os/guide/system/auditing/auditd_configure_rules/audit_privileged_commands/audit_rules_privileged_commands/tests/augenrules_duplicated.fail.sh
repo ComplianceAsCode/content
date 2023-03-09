5b8faa2b06b4217b9fecbf5a4c297e6661a75f66
Marcus Burghardt <maburgha@redhat.com>
2023-03-09 11:57:48 +0100

    Review and update test scenario scripts
    The header was updated to not restrict the remedition to bash and also
    to include multi_platform_rhel since they are also applicable for RHEL9,
    which was not listed in plaform header.
    The rules created in test scenarios are now including -F perm=x, in
    alignement to the OVAL.

--- linux_os/guide/system/auditing/auditd_configure_rules/audit_privileged_commands/audit_rules_privileged_commands/tests/augenrules_duplicated.fail.sh
+++ linux_os/guide/system/auditing/auditd_configure_rules/audit_privileged_commands/audit_rules_privileged_commands/tests/augenrules_duplicated.fail.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # packages = audit
-# Remediation for this rule cannot remove the duplicates
 # remediation = none
-# platform = Fedora,Oracle Linux 7,Oracle Linux 8,Red Hat Enterprise Linux 7,Red Hat Enterprise Linux 8
+# platform = multi_platform_fedora,multi_platform_rhel,Oracle Linux 7,Oracle Linux 8
 
 ./generate_privileged_commands_rule.sh 1000 privileged /tmp/privileged.rules
 
+# Remediation for this rule cannot remove the duplicates
 cp /tmp/privileged.rules /etc/audit/rules.d/privileged.rules
 sed 's/unset/4294967295/' /tmp/privileged.rules >> /etc/audit/rules.d/privileged.rules
