6c082b0fb7aca20cee14139ef19415064e229742
teacup-on-rockingchair <315160+teacup-on-rockingchair@users.noreply.github.com>
2023-03-14 06:19:49 +0200

    Update linux_os/guide/system/network/network-firewalld/ruleset_modifications/ensure_firewall_rules_for_open_ports/tests/correct.pass.sh
    Co-authored-by: Marcus Burghardt <2074099+marcusburghardt@users.noreply.github.com>

--- linux_os/guide/system/network/network-firewalld/ruleset_modifications/ensure_firewall_rules_for_open_ports/tests/correct.pass.sh
+++ linux_os/guide/system/network/network-firewalld/ruleset_modifications/ensure_firewall_rules_for_open_ports/tests/correct.pass.sh
@@ -6,7 +6,7 @@ source common.sh
 # check if ssh started
 is_sshd_active=$(systemctl status sshd|grep Active|awk '{print $2}')
 if [ "${is_sshd_active}" != "active" ]; then
-# start ssh if not - to have for sure at least one network service running
+    # start ssh if not - to have for sure at least one network service running
     systemctl start sshd
 fi
 
