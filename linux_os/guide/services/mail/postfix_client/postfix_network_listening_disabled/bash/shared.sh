2c956496c57e4907e8ee877c303cbb952a597917
Eduardo Barretto <eduardo.barretto@canonical.com>
2023-03-22 11:11:20 +0100

    postfix_network_listening_disabled: Restart postfix service

--- linux_os/guide/services/mail/postfix_client/postfix_network_listening_disabled/bash/shared.sh
+++ linux_os/guide/services/mail/postfix_client/postfix_network_listening_disabled/bash/shared.sh
@@ -3,3 +3,5 @@
 {{{ bash_instantiate_variables("var_postfix_inet_interfaces") }}}
 
 {{{ set_config_file(path="/etc/postfix/main.cf", parameter="inet_interfaces", value="$var_postfix_inet_interfaces", create=true, insensitive=true, separator="=", separator_regex="\s\+=\s\+", prefix_regex="^\s*") }}}
+
+systemctl restart postfix
