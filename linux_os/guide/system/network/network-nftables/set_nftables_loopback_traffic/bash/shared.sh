e262c8155f46e9e58f15835363242f5e7b927088
teacup-on-rockingchair <315160+teacup-on-rockingchair@users.noreply.github.com>
2023-03-26 09:41:03 +0300

    Replace non-existing command create with insert

--- linux_os/guide/system/network/network-nftables/set_nftables_loopback_traffic/bash/shared.sh
+++ linux_os/guide/system/network/network-nftables/set_nftables_loopback_traffic/bash/shared.sh
@@ -2,7 +2,7 @@
 
 # Implement the loopback rules:
 nft add rule inet filter input iif lo accept
-nft create rule inet filter input ip saddr 127.0.0.0/8 counter drop
+nft insert rule inet filter input ip saddr 127.0.0.0/8 counter drop
 
 # Check IPv6 is disabled, if false implement IPv6 loopback rules
 [ -n "$passing" ] && passing=""
