6a2b22cb702728a355fb71537cfc2a114db880b1
Eduardo Barretto <eduardo.barretto@canonical.com>
2023-03-28 11:04:27 +0200

    ensure_shadow_group_empty: bash: Improve remediation

--- linux_os/guide/system/accounts/accounts-restrictions/account_expiration/ensure_shadow_group_empty/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-restrictions/account_expiration/ensure_shadow_group_empty/bash/shared.sh
@@ -1,4 +1,4 @@
 # platform = multi_platform_sle,multi_platform_ubuntu
 
-grep '^shadow:[^:]*:[^:]*:[^:]+' /etc/group
-awk -F: '($4 == "<shadow-gid>") { print }' /etc/passwd
+sed -ri 's/(^shadow:[^:]*:[^:]*:)([^:]+$)/\1/' /etc/group
+awk -F: -v GID="$(awk -F: '($1=="shadow") {print $3}' /etc/group)" '($4==GID) {print $1}' /etc/passwd
