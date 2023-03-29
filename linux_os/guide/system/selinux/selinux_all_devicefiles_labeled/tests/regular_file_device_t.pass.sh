67b0c151effc505c1cea546df8eaa05681e696eb
Markus Linnala <Markus.Linnala@knowit.fi>
2023-03-29 07:38:19 +0300

    fix: selinux_all_devicefiles_labeled: skip testing under podman
    Tests run restorecon and as far as selinux does not work in containers
    it is not reasonable to test.

--- linux_os/guide/system/selinux/selinux_all_devicefiles_labeled/tests/regular_file_device_t.pass.sh
+++ linux_os/guide/system/selinux/selinux_all_devicefiles_labeled/tests/regular_file_device_t.pass.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#   selinux / restorecon does not work under podman
+# skip_test_env = podman-based
 
 touch /dev/foo
 restorecon -F /dev/foo
--- linux_os/guide/system/selinux/selinux_all_devicefiles_labeled/tests/symlink_with_wrong_label.pass.sh
+++ linux_os/guide/system/selinux/selinux_all_devicefiles_labeled/tests/symlink_with_wrong_label.pass.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#   selinux / restorecon does not work under podman
+# skip_test_env = podman-based
 
 ln -s /dev/cpu /dev/foo
 restorecon -F /dev/foo
