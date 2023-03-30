09582c02a01f84f841618a589f0c67dbe006ceba
rchikov <rumen.chikov@suse.com>
2023-03-30 11:41:35 +0200

    Corrections in bash parts

--- linux_os/guide/system/accounts/accounts-restrictions/password_expiration/accounts_password_set_max_life_existing/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-restrictions/password_expiration/accounts_password_set_max_life_existing/bash/shared.sh
@@ -6,17 +6,10 @@
 
 {{{ bash_instantiate_variables("var_accounts_maximum_age_login_defs") }}}
 
-{{% if product in ["sle12", "sle15"] %}}
-while IFS= read -r line; do
-    user=$(echo "$line" | cut -d ":" -f 1)
-    cur_max_age=$(echo "$line" | cut -d ":" -f 5)
-    if [ -z "$cur_max_age" ] || [ $((cur_max_age)) -gt  $((var_accounts_maximum_age_login_defs)) ]
-    then
-       passwd -q -x $var_accounts_maximum_age_login_defs $user
-    fi
-done < /etc/shadow
-{{% else %}}
 {{% call iterate_over_command_output("i", "awk -v var=\"$var_accounts_maximum_age_login_defs\" -F: '(/^[^:]+:[^!*]/ && ($5 > var || $5 == \"\")) {print $1}' /etc/shadow") -%}}
+{{% if product in ["sle12", "sle15"] %}}
+passwd -q -x $var_accounts_maximum_age_login_defs $i
+{{% else %}}
 chage -M $var_accounts_maximum_age_login_defs $i
-{{%- endcall %}}
 {{% endif %}}
+{{%- endcall %}}
--- linux_os/guide/system/accounts/accounts-restrictions/password_expiration/accounts_password_set_min_life_existing/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-restrictions/password_expiration/accounts_password_set_min_life_existing/bash/shared.sh
@@ -6,17 +6,10 @@
 
 {{{ bash_instantiate_variables("var_accounts_minimum_age_login_defs") }}}
 
-{{% if product in ["sle12", "sle15"] %}}
-while IFS= read -r line; do
-    user=$(echo "$line" | cut -d ":" -f 1)
-    cur_min_age=$(echo "$line" | cut -d ":" -f 4)
-    if [ -z "$cur_min_age" ] || [ $((var_accounts_minimum_age_login_defs)) -gt $((cur_min_age))  ]
-    then
-       passwd -q -n $var_accounts_minimum_age_login_defs $user
-    fi
-done < /etc/shadow
-{{% else %}}
 {{% call iterate_over_command_output("i", "awk -v var=\"$var_accounts_minimum_age_login_defs\" -F: '(/^[^:]+:[^!*]/ && ($4 < var || $4 == \"\")) {print $1}' /etc/shadow") -%}}
+{{% if product in ["sle12", "sle15"] %}}
+passwd -q -n $var_accounts_minimum_age_login_defs $i
+{{% else %}}
 chage -m $var_accounts_minimum_age_login_defs $i
-{{%- endcall %}}
 {{% endif %}}
+{{%- endcall %}}
