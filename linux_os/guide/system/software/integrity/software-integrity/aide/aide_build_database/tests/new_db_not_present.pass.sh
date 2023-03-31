354eec9e6872622c56f0ce3d93cbeec6f30c5b8d
Jan Černý <jcerny@redhat.com>
2023-03-31 11:23:50 +0200

    Remove check of the "new" AIDE database
    The "new" database isn't needed for AIDE to work, only the
    installed database matters, therefore, we shouldn't require
    existence of the "new" database in our check. Sometimes,
    the installed database is created by copying the "new" database,
    but often it's created by moving the "new" database, which
    is also suggested by the aide man page and the the "new" file
    doesn't exist anymore.
    
    Fixes: https://bugzilla.redhat.com/show_bug.cgi?id=2175684
    
    This commit also adds a small regression test for the RHBZ.

---
+++ linux_os/guide/system/software/integrity/software-integrity/aide/aide_build_database/tests/new_db_not_present.pass.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# packages = aide
+
+# This TS is a regression test for https://bugzilla.redhat.com/show_bug.cgi?id=2175684
+
+{{% if 'ubuntu' in product or 'sle' in product %}}
+DB_NEW=/var/lib/aide/aide.db.new
+DB=/var/lib/aide/aide.db
+{{% else %}}
+DB_NEW=/var/lib/aide/aide.db.new.gz
+DB=/var/lib/aide/aide.db.gz
+{{% endif %}}
+
+aide --init
+cp "$DB_NEW" "$DB"
+rm -rf "$DB_NEW"
