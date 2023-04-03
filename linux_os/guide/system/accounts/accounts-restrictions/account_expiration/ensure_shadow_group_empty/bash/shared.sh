cc16bda7b44325a448e007e8dc8a8d40a9ef51e0
Eduardo Barretto <eduardo.barretto@canonical.com>
2023-04-03 15:52:04 +0200

    ensure_shadow_group_empty: bash: Remove non-remediation line

--- linux_os/guide/system/accounts/accounts-restrictions/account_expiration/ensure_shadow_group_empty/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-restrictions/account_expiration/ensure_shadow_group_empty/bash/shared.sh
@@ -1,4 +1,3 @@
 # platform = multi_platform_sle,multi_platform_ubuntu
 
 sed -ri 's/(^shadow:[^:]*:[^:]*:)([^:]+$)/\1/' /etc/group
-awk -F: -v GID="$(awk -F: '($1=="shadow") {print $3}' /etc/group)" '($4==GID) {print $1}' /etc/passwd
