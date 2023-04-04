59aec41026c5000323f0b5b203d2998cf9572dee
Marcus Burghardt <maburgha@redhat.com>
2023-04-04 15:15:28 -0300

    Enable ensure_root_password_configured for RHEL9
    This rule complements the 5.6.6 CIS requirement for RHEL9.

--- linux_os/guide/system/accounts/accounts-restrictions/root_logins/ensure_root_password_configured/tests/correct.pass.sh
+++ linux_os/guide/system/accounts/accounts-restrictions/root_logins/ensure_root_password_configured/tests/correct.pass.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# platform = multi_platform_ubuntu
+# platform = multi_platform_all
 
 sed -i "s/^root:[^:]*/root:\$y\$AAAAAAAAAA/" /etc/shadow
--- linux_os/guide/system/accounts/accounts-restrictions/root_logins/ensure_root_password_configured/tests/empty.fail.sh
+++ linux_os/guide/system/accounts/accounts-restrictions/root_logins/ensure_root_password_configured/tests/empty.fail.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# platform = multi_platform_ubuntu
+# platform = multi_platform_all
 # remediation = None
 
 passwd -d root
