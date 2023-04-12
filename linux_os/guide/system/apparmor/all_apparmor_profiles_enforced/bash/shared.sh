1b2f177649a9211f2e832063f93a6c1f39ea3093
Eduardo Barretto <eduardo.barretto@canonical.com>
2023-04-12 22:09:34 +0200

    all_apparmor_profiles_enforced: bash: Add Ubuntu bits

--- linux_os/guide/system/apparmor/all_apparmor_profiles_enforced/bash/shared.sh
+++ linux_os/guide/system/apparmor/all_apparmor_profiles_enforced/bash/shared.sh
@@ -1,19 +1,27 @@
-# platform = multi_platform_sle
+# platform = multi_platform_sle,multi_platform_ubuntu
+
+# make sure apparmor-utils is installed for aa-complain and aa-enforce
+{{{ bash_package_install("apparmor-utils") }}}
 
 # Ensure all AppArmor Profiles are enforcing
 apparmor_parser -q -r /etc/apparmor.d/
 aa-enforce /etc/apparmor.d/*
 
+{{% if 'ubuntu' in product %}}
+UNCONFINED=$(aa-status | grep "processes are unconfined" | awk '{print $1;}')
+if [ $UNCONFINED -ne 0 ];
+{{% else %}}
 UNCONFINED=$(aa-unconfined)
 if [ ! -z "$UNCONFINED" ]
+{{% endif %}}
 then
   echo -e "***WARNING***: There are some unconfined processes:"
-  echo -e "----------------------------" 
+  echo -e "----------------------------"
   echo "The may need to have a profile created or activated for them and then be restarted."
   for PROCESS in "${UNCONFINED[@]}"
   do
-      echo "$PROCESS"  
+      echo "$PROCESS"
   done
   echo -e "----------------------------"
   echo "The may need to have a profile created or activated for them and then be restarted."
-fi 
+fi
