93911592dfb8c732d32b49f026075c07b1f62d35
Eduardo Barretto <eduardo.barretto@canonical.com>
2023-04-14 16:54:33 +0200

    sshd_limit_user_access: Add tests

---
+++ linux_os/guide/services/ssh/ssh_server/sshd_limit_user_access/tests/deny_users_groups.pass.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# remediation = None
+
+find /etc/ssh/ssh_config* -type f -print0 | xargs -0 sed -i '/^(Allow|Deny)(Users|Groups).*/d'
+echo "DenyUsers testuser1 testuser2 testuser3" >> /etc/ssh/sshd_config
+echo "DenyGroups testgroup1 testgroup2 testgroup3" >> /etc/ssh/sshd_config
