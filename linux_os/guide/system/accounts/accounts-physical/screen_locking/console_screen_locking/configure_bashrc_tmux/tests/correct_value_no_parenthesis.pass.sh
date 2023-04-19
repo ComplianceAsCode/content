97fa9047721a3e7651a975e7a80b6b2db75ee9b0
Gabriel Becker <ggasparb@redhat.com>
2023-04-19 11:29:04 +0200

    Use opening parenthesis in the switch case condition.
    This change is to align with DISA STIG since it's also a valid syntax
    and they have not changed their SCAP content. The OVAL file should still
    accept a configuration without the opening parenthesis by making it
    optional in the regex.

--- linux_os/guide/system/accounts/accounts-physical/screen_locking/console_screen_locking/configure_bashrc_tmux/tests/correct_value_no_parenthesis.pass.sh
+++ linux_os/guide/system/accounts/accounts-physical/screen_locking/console_screen_locking/configure_bashrc_tmux/tests/correct_value_no_parenthesis.pass.sh
@@ -5,6 +5,6 @@ cat >> /etc/bashrc <<'EOF'
 if [ "$PS1" ]; then
   parent=$(ps -o ppid= -p $$)
   name=$(ps -o comm= -p $parent)
-  case "$name" in (sshd|login) tmux ;; esac
+  case "$name" in sshd|login) tmux ;; esac
 fi
 EOF
