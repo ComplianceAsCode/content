8fe3315eac1fbfcc7ba3104a85b4f22274480aaa
Edgar Aguilar <edgar.aguilar@oracle.com>
2023-04-21 11:37:44 -0600

    Update jinja conditionals that apply to any ol
    There are many jinja conditionals in the repo that apply to all major
    releases of OL, but where included as a list of them, and didn't
    include OL9. Updated to be more generic in OL products
    
    Signed-off-by: Edgar Aguilar <edgar.aguilar@oracle.com>

--- shared/templates/kernel_module_disabled/tests/correct_value_modules_load_d.pass.sh
+++ shared/templates/kernel_module_disabled/tests/correct_value_modules_load_d.pass.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "install {{{ KERNMODULE }}} /bin/true" > /etc/modules-load.d/{{{ KERNMODULE }}}.conf
-{{% if product in ["ol7", "ol8"] or 'rhel' in product %}}
+{{% if "ol" in product or 'rhel' in product %}}
 echo "blacklist {{{ KERNMODULE }}}" >> /etc/modules-load.d/{{{ KERNMODULE }}}.conf
 {{% endif %}}
