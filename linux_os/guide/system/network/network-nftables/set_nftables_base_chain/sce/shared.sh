2d238152f1305e635cd7f9733769635092ca54c5
rchikov <rumen.chikov@suse.com>
2023-05-03 08:49:37 +0200

    Added SCE check

---
+++ linux_os/guide/system/network/network-nftables/set_nftables_base_chain/sce/shared.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# platform = multi_platform_sle
+# check-import = stdout
+
+chain_hook=$(nft list ruleset | grep 'hook input')
+if [ -z "${chain_hook}" ]; then
+    exit ${XCCDF_RESULT_FAIL}
+fi
+
+chain_hook=$(nft list ruleset | grep 'hook forward')
+if [ -z "${chain_hook}" ]; then
+    exit ${XCCDF_RESULT_FAIL}
+fi
+
+chain_hook=$(nft list ruleset | grep 'hook output')
+if [ -z "${chain_hook}" ]; then
+    exit ${XCCDF_RESULT_FAIL}
+fi
+
+exit ${XCCDF_RESULT_PASS}
