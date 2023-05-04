d01898c547a2d41d11a855608a8876faaa36f174
Edgar Aguilar <edgar.aguilar@oracle.com>
2023-05-03 18:09:53 -0600

    Fix tests in sshd_lineinfile template
    In some cases the testing environment had existing non compliant
    configurations which made some test fail when they were expected to
    pass. This ensures that doesn't happen
    
    Signed-off-by: Edgar Aguilar <edgar.aguilar@oracle.com>

--- shared/templates/sshd_lineinfile/tests/comment.fail.sh
+++ shared/templates/sshd_lineinfile/tests/comment.fail.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 
-SSHD_PARAM={{{ PARAMETER }}}
-SSHD_VAL={{{ VALUE }}}
-
-mkdir -p /etc/ssh/sshd_config.d
-touch /etc/ssh/sshd_config.d/nothing
-
-if grep -q "^\s*${SSHD_PARAM}" /etc/ssh/sshd_config /etc/ssh/sshd_config.d/* ; then
-	sed -i "s/^${SSHD_PARAM}.*/# ${SSHD_PARAM} ${SSHD_VAL}/g" /etc/ssh/sshd_config /etc/ssh/sshd_config.d/*
-else
-	echo "# ${SSHD_PARAM} ${SSHD_VAL}" >> /etc/ssh/sshd_config
-fi
+source common.sh
--- shared/templates/sshd_lineinfile/tests/correct_value_multiple_includes.pass.sh
+++ shared/templates/sshd_lineinfile/tests/correct_value_multiple_includes.pass.sh
@@ -2,8 +2,7 @@
 
 # platform = Oracle Linux 8,Oracle Linux 9
 
-mkdir -p /etc/ssh/sshd_config.d
-touch /etc/ssh/sshd_config.d/nothing
+source common.sh
 
 {{{ bash_replace_or_append("/etc/ssh/sshd_config", "Include", "sshd_config.d/*.conf", "%s %s") }}}
 echo "Include /etc/dummy" >> "/etc/ssh/sshd_config"
