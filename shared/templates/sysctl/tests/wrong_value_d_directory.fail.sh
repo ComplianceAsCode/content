82624ff46badabdfc67e2385af4ecfb4f6b566cf
Markus Linnala <Markus.Linnala@knowit.fi>
2023-05-05 18:48:16 +0300

    fix: sysctl/tests: change wrong_value_d_directory.fail.sh to test all directories
    And thus wrong_value_usr_local_lib.fail.sh is not needed any more.
    
    Now wrong_value_d_directory.fail.sh tests OVAL that it finds all
    relevant wrong values.
    
    During remediation test that all relevant wrong values are fixed.
    
    I believe testing one directory at a time is not benefical usage of
    testing resources. As we want to test one item, it is to test if list of
    directories implemented in OVAL or in remediation are not in sync.

--- shared/templates/sysctl/tests/wrong_value_d_directory.fail.sh
+++ shared/templates/sysctl/tests/wrong_value_d_directory.fail.sh
@@ -6,7 +6,10 @@
 {{{ bash_sysctl_test_clean() }}}
 
 sed -i "/{{{ SYSCTLVAR }}}/d" /etc/sysctl.conf
-echo "{{{ SYSCTLVAR }}} = {{{ SYSCTL_WRONG_VALUE }}}" >> /etc/sysctl.d/98-sysctl.conf
+{{{ bash_sysctl_set_config_directories('sysctl_directories', all_possible=true) }}}
+for d in "${sysctl_directories[@]}"; do
+echo "{{{ SYSCTLVAR }}} = {{{ SYSCTL_WRONG_VALUE }}}" >> "${d}"/98-sysctl.conf
+done
 
 # Setting correct runtime value
 sysctl -w {{{ SYSCTLVAR }}}="{{{ SYSCTL_CORRECT_VALUE }}}"
