2ef7d18ad36995dd2e54344f9dc7d4d33c175a0b
Markus Linnala <Markus.Linnala@knowit.fi>
2023-05-10 15:25:01 +0300

    fix: file_group_ownership_var_log_audit/tests: use mkdir -p

--- linux_os/guide/system/auditing/auditd_configure_rules/file_group_ownership_var_log_audit/tests/common.sh
+++ linux_os/guide/system/auditing/auditd_configure_rules/file_group_ownership_var_log_audit/tests/common.sh
@@ -5,7 +5,7 @@ sed -i "/^\s*log_group.*/d" /etc/audit/auditd.conf
 
 groupadd group_test
 rm -f /var/log/audit/*
-mkdir /var/log/audit2/
+mkdir -p /var/log/audit2
 
 FILE1=/var/log/audit/audit.log
 FILE2=/var/log/audit2/audit.log
