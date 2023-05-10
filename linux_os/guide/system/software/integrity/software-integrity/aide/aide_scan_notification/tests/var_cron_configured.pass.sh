7f58b563458d673edb6cf3c39fc78c570724516d
Markus Linnala <Markus.Linnala@knowit.fi>
2023-05-10 14:12:27 +0300

    bugfix: aide_scan_notificatio/tests: ensure crontabs/chronie
    When testing without relevant packages installed you get wrong results.

--- linux_os/guide/system/software/integrity/software-integrity/aide/aide_scan_notification/tests/var_cron_configured.pass.sh
+++ linux_os/guide/system/software/integrity/software-integrity/aide/aide_scan_notification/tests/var_cron_configured.pass.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# packages = aide
+# packages = aide,cronie
 
 # configured in crontab
 echo '0 5 * * * root /usr/sbin/aide  --check | /bin/mail -s "SSG Test Suite - AIDE Integrity Check" admin@ssgtestsuite' >> /var/spool/cron/root
