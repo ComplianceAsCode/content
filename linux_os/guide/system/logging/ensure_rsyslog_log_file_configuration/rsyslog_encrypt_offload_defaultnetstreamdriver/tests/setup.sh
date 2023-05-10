696628ca4515b72ddc9c39cbc1cd16abe8fe6bae
Markus Linnala <Markus.Linnala@knowit.fi>
2023-05-10 15:25:02 +0300

    fix: rsyslog_encrypt_offload_defaultnetstreamdriver/tests: use mkdir -p

--- linux_os/guide/system/logging/ensure_rsyslog_log_file_configuration/rsyslog_encrypt_offload_defaultnetstreamdriver/tests/setup.sh
+++ linux_os/guide/system/logging/ensure_rsyslog_log_file_configuration/rsyslog_encrypt_offload_defaultnetstreamdriver/tests/setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # Use this script to ensure the rsyslog directory structure and rsyslog conf file
 # exist in the test env.
 config_file=/etc/rsyslog.conf
@@ -6,4 +7,4 @@ config_file=/etc/rsyslog.conf
 # Ensure directory structure exists (useful for container based testing)
 test -f $config_file || touch $config_file
 
-test -d /etc/rsyslog.d/ || mkdir /etc/rsyslog.d/
+mkdir -p /etc/rsyslog.d
