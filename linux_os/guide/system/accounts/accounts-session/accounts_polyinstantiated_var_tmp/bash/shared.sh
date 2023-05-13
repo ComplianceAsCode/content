75552299b6a1dfe1a1c270c4544636ac1423f7c2
Markus Linnala <Markus.Linnala@knowit.fi>
2023-05-13 21:19:53 +0300

    fix: accounts_polyinstantiated_var_tmp: use mkdir -p

--- linux_os/guide/system/accounts/accounts-session/accounts_polyinstantiated_var_tmp/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-session/accounts_polyinstantiated_var_tmp/bash/shared.sh
@@ -1,9 +1,10 @@
+#!/bin/bash
 # platform = multi_platform_all
-if ! [ -d /tmp-inst ] ; then
-    mkdir --mode 000 /var/tmp/tmp-inst
-fi
+
+# shellcheck disable=SC2174
+mkdir -p --mode 000 /var/tmp/tmp-inst
 chmod 000 /var/tmp/tmp-inst
-chcon --reference=/var/tmp/ /var/tmp/tmp-inst
+chcon --reference=/var/tmp /var/tmp/tmp-inst
 
 if ! grep -Eq '^\s*/var/tmp\s+/var/tmp/tmp-inst/\s+level\s+root,adm$' /etc/security/namespace.conf ; then
     if grep -Eq '^\s*/var/tmp\s+' /etc/security/namespace.conf ; then
