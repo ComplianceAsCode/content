95cc939efbe9fb7a0318daab245a30330c35d9a2
Markus Linnala <Markus.Linnala@knowit.fi>
2023-05-22 17:58:21 +0300

    fix: ssg_test_suite/RuleResult: simplify run_cmd_local

--- tests/ssg_test_suite/common.py
+++ tests/ssg_test_suite/common.py
@@ -4,6 +4,7 @@ import functools
 import logging
 import os
 import re
+import shlex
 import shutil
 import subprocess
 import tarfile
@@ -168,23 +169,11 @@ class RuleResult(object):
 
 
 def run_cmd_local(command, verbose_path, env=None):
-    command_string = ' '.join(command)
-    logging.debug('Running {}'.format(command_string))
-    returncode, output = _run_cmd(command, verbose_path, env)
-    return returncode, output
-
-
-def _run_cmd(command_list, verbose_path, env=None):
-    returncode = 0
-    output = b""
-    try:
-        with open(verbose_path, 'w') as verbose_file:
-            output = subprocess.check_output(
-                command_list, stderr=verbose_file, env=env)
-    except subprocess.CalledProcessError as e:
-        returncode = e.returncode
-        output = e.output
-    return returncode, output.decode('utf-8')
+    logging.debug('Running local command: {}'.format(shlex.join(command)))
+    with open(verbose_path, 'w') as verbose_file:
+        output = subprocess.run(
+            command, stdout=subprocess.PIPE, stderr=verbose_file, env=env)
+    return output.returncode, output.stdout.decode('utf-8')
 
 
 def _get_platform_cpes(platform):
