821b4fc32b1ac960dcc6843820a94144b0400ac5
Markus Linnala <Markus.Linnala@knowit.fi>
2023-05-28 22:12:15 +0300

    fix: mount_option_nosuid_removable_partitions/tests: use bash_systemctl_daemon_reload after fstab changed

--- linux_os/guide/system/permissions/partitions/mount_option_nosuid_removable_partitions/tests/cd_bad_opts.fail.sh
+++ linux_os/guide/system/permissions/partitions/mount_option_nosuid_removable_partitions/tests/cd_bad_opts.fail.sh
@@ -2,3 +2,4 @@
 
 touch /dev/cdrom
 echo "/dev/cdrom /var/cdrom iso9660 ro 0 0" > /etc/fstab
+{{{ bash_systemctl_daemon_reload() }}}
--- linux_os/guide/system/permissions/partitions/mount_option_nosuid_removable_partitions/tests/cd_multiple_opts_first.pass.sh
+++ linux_os/guide/system/permissions/partitions/mount_option_nosuid_removable_partitions/tests/cd_multiple_opts_first.pass.sh
@@ -2,3 +2,4 @@
 
 touch /dev/cdrom
 echo "/dev/cdrom /media/cdrom iso9660 nosuid,ro,noauto,noexec,nodev 0 0" >> /etc/fstab
+{{{ bash_systemctl_daemon_reload() }}}
