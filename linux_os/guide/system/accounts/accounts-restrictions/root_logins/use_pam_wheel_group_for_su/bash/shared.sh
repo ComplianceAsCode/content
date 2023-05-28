cf449034dd43d7890787fdc8a2d34141c4b9331b
Markus Linnala <Markus.Linnala@knowit.fi>
2023-05-28 23:17:04 +0300

    fix: use grep -E instead of deprecated egrep
    From grep(1):
    
           In addition, the variant programs egrep and fgrep are the same as
           grep -E and grep -F, respectively. These variants are deprecated,
           but are provided for backward compatibility.
    
    Made using:
        git grep -l egrep docs .github linux_os|xargs -r \
            sed -Ei 's/egrep -/grep -E/g;s/egrep /grep -E /g'

--- linux_os/guide/system/accounts/accounts-restrictions/root_logins/use_pam_wheel_group_for_su/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-restrictions/root_logins/use_pam_wheel_group_for_su/bash/shared.sh
@@ -8,7 +8,7 @@ if [ -z "$pamstr" ]; then
     sed -Ei '/^auth\b.*\brequired\b.*\bpam_wheel\.so/d' ${PAM_CONF} # remove any remaining uncommented pam_wheel.so line
     sed -Ei "/^auth\s+sufficient\s+pam_rootok\.so.*$/a auth required pam_wheel.so use_uid group=${var_pam_wheel_group_for_su}" ${PAM_CONF}
 else
-    group_val=$(echo -n "$pamstr" | egrep -o '\bgroup=[_a-z][-0-9_a-z]*' | cut -d '=' -f 2)
+    group_val=$(echo -n "$pamstr" | grep -Eo '\bgroup=[_a-z][-0-9_a-z]*' | cut -d '=' -f 2)
     if [ -z "${group_val}" ] || [ ${group_val} != ${var_pam_wheel_group_for_su} ]; then
         sed -Ei "s/(^auth\s+required\s+pam_wheel.so\s+[^#]*group=)[_a-z][-0-9_a-z]*/\1${var_pam_wheel_group_for_su}/" ${PAM_CONF}
     fi
