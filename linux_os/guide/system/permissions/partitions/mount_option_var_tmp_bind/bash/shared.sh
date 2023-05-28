4b5f9159c207206bebc65296e57b05dcc30d9637
Markus Linnala <Markus.Linnala@knowit.fi>
2023-05-28 22:23:27 +0300

    fix: mount_option_var_tmp_bind: use bash_systemctl_daemon_reload after modifying fstab

--- linux_os/guide/system/permissions/partitions/mount_option_var_tmp_bind/bash/shared.sh
+++ linux_os/guide/system/permissions/partitions/mount_option_var_tmp_bind/bash/shared.sh
@@ -5,11 +5,13 @@
 if grep -q -P '.*\/var\/tmp.*' /etc/fstab
 then
   sed -i '/.*\/var\/tmp.*/d' /etc/fstab
+  {{{ bash_systemctl_daemon_reload() }}}
 fi
 umount /var/tmp
 
 # Bind-mount /var/tmp to /tmp via /etc/fstab (preserving the /etc/fstab form)
 printf "%-24s%-24s%-8s%-32s%-3s\n" "/tmp" "/var/tmp" "none" "rw,nodev,noexec,nosuid,bind" "0 0" >> /etc/fstab
+{{{ bash_systemctl_daemon_reload() }}}
 
 mkdir -p /var/tmp
 mount -B /tmp /var/tmp
