5f3757f7aeff99deab999191b1d2ed33d05326a3
Markus Linnala <Markus.Linnala@knowit.fi>
2023-05-28 20:34:44 +0300

    fix: no_password_auth_for_systemaccounts: use uid_min

--- linux_os/guide/system/accounts/accounts-restrictions/root_logins/no_password_auth_for_systemaccounts/tests/sysaccounts_without_password.pass.sh
+++ linux_os/guide/system/accounts/accounts-restrictions/root_logins/no_password_auth_for_systemaccounts/tests/sysaccounts_without_password.pass.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# lock all system accounts (ID < 1000) from /etc/passwd
+# lock all system accounts (ID < {{{ uid_min }}}) from /etc/passwd
 readarray -t systemaccounts < <(awk -F: \
   '($3 < {{{ uid_min }}} && $3 != root && $3 != halt && $3 != sync && $3 != shutdown \
   && $3 != nfsnobody) { print $1 }' /etc/passwd)
