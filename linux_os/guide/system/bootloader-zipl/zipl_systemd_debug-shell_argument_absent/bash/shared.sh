1f85974cf9ced1b563ab9c8db21012f9de6b219c
Matej Tyc <matyc@redhat.com>
2023-05-31 13:07:41 +0200

    Consolidate grub rules
    - Assign the grub helper exec to the product
    - Remove most of the references to product names from grub macros
    - Substitute literal invocations of e.g. grubby with macros

--- linux_os/guide/system/bootloader-zipl/zipl_systemd_debug-shell_argument_absent/bash/shared.sh
+++ linux_os/guide/system/bootloader-zipl/zipl_systemd_debug-shell_argument_absent/bash/shared.sh
@@ -1,9 +1,10 @@
 # platform = Red Hat Enterprise Linux 8,Red Hat Enterprise Linux 9
 
+{{% set option = "systemd.debug-shell" -%}}
 # Correct BLS option using grubby, which is a thin wrapper around BLS operations
-grubby --update-kernel=ALL --remove-args="systemd.debug-shell"
+{{{ grub_command("remove", option) }}}
 
 # Ensure new kernels and boot entries retain the boot option
-if grep -q '\bsystemd.debug-shell\b' /etc/kernel/cmdline; then
-    sed -Ei 's/^(.*)\s*systemd.debug-shell\b\S*(.*)/\1\2/' /etc/kernel/cmdline
+if grep -q '\b{{{ option }}}\b' /etc/kernel/cmdline; then
+    sed -Ei 's/^(.*)\s*{{{ option }}}\b\S*(.*)/\1\2/' /etc/kernel/cmdline
 fi
