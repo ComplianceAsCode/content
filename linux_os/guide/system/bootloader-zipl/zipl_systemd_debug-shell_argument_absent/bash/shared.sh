79b1fd8a637540566fdd9edaf571831d3316f18c
Matej Tyc <matyc@redhat.com>
2023-06-08 13:58:23 +0200

    Revert macro usage in zipl rules
    Grubby is not used in the context of grub,
    but in another context of BLS utility,
    and the macro doesn't understand this.

--- linux_os/guide/system/bootloader-zipl/zipl_systemd_debug-shell_argument_absent/bash/shared.sh
+++ linux_os/guide/system/bootloader-zipl/zipl_systemd_debug-shell_argument_absent/bash/shared.sh
@@ -1,10 +1,9 @@
 # platform = Red Hat Enterprise Linux 8,Red Hat Enterprise Linux 9
 
-{{% set option = "systemd.debug-shell" -%}}
 # Correct BLS option using grubby, which is a thin wrapper around BLS operations
-{{{ grub_command("remove", option) }}}
+grubby --update-kernel=ALL --remove-args="systemd.debug-shell"
 
 # Ensure new kernels and boot entries retain the boot option
-if grep -q '\b{{{ option }}}\b' /etc/kernel/cmdline; then
-    sed -Ei 's/^(.*)\s*{{{ option }}}\b\S*(.*)/\1\2/' /etc/kernel/cmdline
+if grep -q '\bsystemd.debug-shell\b' /etc/kernel/cmdline; then
+    sed -Ei 's/^(.*)\s*systemd.debug-shell\b\S*(.*)/\1\2/' /etc/kernel/cmdline
 fi
