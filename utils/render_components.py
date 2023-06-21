8f9a765d1ee97fc6c09501088cd4875ed1840781
Jan Černý <jcerny@redhat.com>
2023-06-21 10:25:07 +0200

    Add typing hints

--- utils/render_components.py
+++ utils/render_components.py
@@ -5,6 +5,7 @@ import collections
 import glob
 import os
 import sys
+from typing import Optional
 
 import ssg.build_yaml
 import ssg.components
@@ -28,7 +29,7 @@ ProductData = collections.namedtuple(
     "ProductData", ["product_id", "product_full_name", "components"])
 
 
-def parse_args():
+def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(
         description="Script that converts the component data and component "
         "-> rule mapping to a set of HTML files that can be published "
@@ -41,7 +42,8 @@ def parse_args():
     return args
 
 
-def create_component_html(output_dir, component_data):
+def create_component_html(
+        output_dir: str, component_data: ComponentData) -> None:
     component_name = component_data.component.name
     title = f"Rules related to '{component_name}'"
     data = dict(title=title, component_data=component_data)
@@ -50,7 +52,8 @@ def create_component_html(output_dir, component_data):
         data, COMPONENT_TEMPLATE, output_path)
 
 
-def create_index_html(output_dir, product_components):
+def create_index_html(
+        output_dir: str, product_components: list) -> None:
     index_path = os.path.join(output_dir, "index.html")
     title = "List of components"
     data = dict(title=title, product_components=product_components)
@@ -58,7 +61,7 @@ def create_index_html(output_dir, product_components):
         data, COMPONENT_INDEX_TEMPLATE, index_path)
 
 
-def load_rule_data(rule_path):
+def load_rule_data(rule_path: str) -> RuleData:
     rule = ssg.build_yaml.Rule.from_yaml(rule_path)
     description = resolve_var_substitutions(rule.description)
     rationale = resolve_var_substitutions(rule.rationale)
@@ -70,7 +73,7 @@ def load_rule_data(rule_path):
     return rule_data
 
 
-def find_rule_paths(component_rules, resolved_rules_dir):
+def find_rule_paths(component_rules: list, resolved_rules_dir: str) -> list:
     rule_paths = []
     for rule_id in component_rules:
         rule_path = os.path.join(resolved_rules_dir, rule_id + ".yml")
@@ -79,7 +82,7 @@ def find_rule_paths(component_rules, resolved_rules_dir):
     return rule_paths
 
 
-def collect_rules(rule_paths):
+def collect_rules(rule_paths: list) -> list:
     rules = []
     for rule_path in rule_paths:
         rule_data = load_rule_data(rule_path)
@@ -87,7 +90,9 @@ def collect_rules(rule_paths):
     return rules
 
 
-def process_component(component_path, resolved_rules_dir, output_product_dir):
+def process_component(
+        component_path: str, resolved_rules_dir: str,
+        output_product_dir: str) -> Optional[str]:
     component = ssg.components.Component(component_path)
     rule_paths = find_rule_paths(component.rules, resolved_rules_dir)
     if len(rule_paths) == 0:
@@ -98,7 +103,9 @@ def process_component(component_path, resolved_rules_dir, output_product_dir):
     return component.name
 
 
-def process_components(components_dir, resolved_rules_dir, output_product_dir):
+def process_components(
+        components_dir: str, resolved_rules_dir: str,
+        output_product_dir: str) -> list:
     component_names = []
     for component_file in os.listdir(components_dir):
         component_path = os.path.join(components_dir, component_file)
@@ -109,7 +116,9 @@ def process_components(components_dir, resolved_rules_dir, output_product_dir):
     return sorted(component_names)
 
 
-def process_product(product_id, ssg_root, output_dir):
+def process_product(
+        product_id: str, ssg_root: str,
+        output_dir: str) -> Optional[ProductData]:
     build_product_dir = os.path.join(ssg_root, "build", product_id)
     if not os.path.exists(build_product_dir):
         print(f"Product {product_id} isn't built.", file=sys.stderr)
@@ -135,7 +144,7 @@ def process_product(product_id, ssg_root, output_dir):
     return product_data
 
 
-def find_products(ssg_root):
+def find_products(ssg_root: str) -> list:
     product_ids = []
     build_product_dir = os.path.join(ssg_root, "build")
     for product_yaml_path in glob.glob(build_product_dir + "/**/product.yml"):
@@ -144,7 +153,7 @@ def find_products(ssg_root):
     return product_ids
 
 
-def process_products(ssg_root, output_dir):
+def process_products(ssg_root: str, output_dir: str) -> list:
     product_ids = find_products(ssg_root)
     product_components = []
     for product_id in product_ids:
