0aadc5a90bc0ae3c47e7ea1189b640288a827f5f
Mackemania <8738793+Mackemania@users.noreply.github.com>
2023-06-22 09:49:30 +0200

    Update name of rule to remove benchmark coupling

--- linux_os/guide/system/auditing/auditd_configure_rules/audit_rules_suid_privilege_function_cis/tests/correct_value.pass.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# packages = audit
-
-OTHER_FILTERS=" -C euid!=uid -F auid!=unset"
-
-echo "-a always,exit -F arch=b32${OTHER_FILTERS} -S execve -k user_emulation" >> /etc/audit/rules.d/privileged.rules
-echo "-a always,exit -F arch=b64${OTHER_FILTERS} -S execve -k user_emulation" >> /etc/audit/rules.d/privileged.rules
