cdd400df7df01961a1ab69c2f2a06eb0ccd18381
teacup-on-rockingchair <315160+teacup-on-rockingchair@users.noreply.github.com>
2023-06-26 18:46:08 +0300

    Implement bash remediation

--- linux_os/guide/services/ntp/service_timesyncd_configured/bash/shared.sh
+++ linux_os/guide/services/ntp/service_timesyncd_configured/bash/shared.sh
@@ -6,16 +6,23 @@
 
 {{{ bash_instantiate_variables("var_multiple_time_servers") }}}
 
+IFS=',' read -r -a time_servers_array <<< "$var_multiple_time_servers"
+preferred_ntp_servers_array="${time_servers_array[@]:0:2}"
+preferred_ntp_servers=$( echo ${preferred_ntp_servers_array[@]}|sed -e 's/\s\+/,/g' )
+fallback_ntp_servers_array="${time_servers_array[@]:2}"
+fallback_ntp_servers=$( echo ${fallback_ntp_servers_array[@]}|sed -e 's/\s\+/,/g' )
+
 config_file="/etc/systemd/timesyncd.d/oscap-remedy.conf"
-preferred_ntp_servers='{{ (var_multiple_time_servers.split(",") | slice(2)| first |\
-                           join(",") }}'
-fallback_ntp_servers='{{ var_multiple_time_servers.split(",") | slice(2)| last |\
-                         join(",") }}'
-
+current_cfg_arr=( "/etc/systemd/timesyncd.conf" )
+current_cfg_arr+=($(ls /etc/systemd/timesyncd.d/*))
 # Comment existing NTP FallbackNTP and RootDistance settings
-# TODO
-
+for current_cfg in "${current_cfg_arr[@]}"
+do
+    sed -i 's/^NTP/#&/g' "$current_cfg"
+    sed -i 's/^FallbackNTP/#&/g' "$current_cfg"
+    sed -i 's/^RootDistanceMax/#&/g' "$current_cfg"
+done
 # Set primary fallback NTP servers and RootDistance in drop-in configuration
-echo "NTP={{ preferred_ntp_servers }}" >> "$config_file"
-echo "FallbackNTP={{ fallback_ntp_servers }}" >> "$config_file"
+echo "NTP=$preferred_ntp_servers" >> "$config_file"
+echo "FallbackNTP=$fallback_ntp_servers" >> "$config_file"
 echo "RootDistanceMax=1" >> "$config_file"
