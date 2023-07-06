7dd492cd4d1d20c09ac519d237b0efdbc86cf448
Dexter Le <dexter.le@sap.com>
2023-07-06 19:03:55 +0000

    Add UBTU-20-010045 for enforcing proper kex exchange algorithms
    This commit will add UBTU-20-010045 for sshd_use_approved_kex_ordered_stig rule. Additionally, ubuntu2004 has been included in the tests

--- linux_os/guide/services/ssh/ssh_server/sshd_use_approved_kex_ordered_stig/tests/line_not_there.fail.sh
+++ linux_os/guide/services/ssh/ssh_server/sshd_use_approved_kex_ordered_stig/tests/line_not_there.fail.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# platform = multi_platform_ol,multi_platform_rhel,multi_platform_sle
+# platform = multi_platform_ol,multi_platform_rhel,multi_platform_sle,multi_platform_ubuntu
 
 source common.sh
