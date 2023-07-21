5bd1ead2f33f5dda932fb4095fd707f267b03d3c
Matthew Burket <mburket@redhat.com>
2023-07-21 13:15:06 -0500

    Update harden_sshd_macs_opensshserver_conf_crypto_policy for RHEL-08-010290

--- linux_os/guide/system/software/integrity/crypto/harden_sshd_macs_opensshserver_conf_crypto_policy/tests/rhel8_stig_correct.pass.sh
+++ linux_os/guide/system/software/integrity/crypto/harden_sshd_macs_opensshserver_conf_crypto_policy/tests/rhel8_stig_correct.pass.sh
@@ -2,7 +2,7 @@
 # platform = Oracle Linux 8,Red Hat Enterprise Linux 8
 # profiles = xccdf_org.ssgproject.content_profile_stig
 
-sshd_approved_macs=hmac-sha2-512,hmac-sha2-256
+sshd_approved_macs=hmac-sha2-512,hmac-sha2-256,hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com
 configfile=/etc/crypto-policies/back-ends/opensshserver.config
 correct_value="-oMACs=${sshd_approved_macs}"
 
