20042ab0aa4731b4cadf112a7c578a6afadf09d6
teacup-on-rockingchair <315160+teacup-on-rockingchair@users.noreply.github.com>
2023-07-25 08:06:50 +0300

    Drop irrelevant commit

--- linux_os/guide/services/ntp/service_timesyncd_configured/bash/shared.sh
+++ linux_os/guide/services/ntp/service_timesyncd_configured/bash/shared.sh
@@ -15,13 +15,13 @@ fallback_ntp_servers=$( echo "${fallback_ntp_servers_array[@]}"|sed -e 's/\s\+/,
 config_file="/etc/systemd/timesyncd.d/oscap-remedy.conf"
 current_cfg_arr=( "/etc/systemd/timesyncd.conf" )
 current_cfg_arr+=("$(ls /etc/systemd/timesyncd.d/*)")
-# Comment existing NTP FallbackNTP and RootDistance settings
+# Comment existing NTP FallbackNTP settings
 for current_cfg in "${current_cfg_arr[@]}"
 do
     sed -i 's/^NTP/#&/g' "$current_cfg"
     sed -i 's/^FallbackNTP/#&/g' "$current_cfg"
 done
-# Set primary fallback NTP servers and RootDistance in drop-in configuration
+# Set primary fallback NTP servers in drop-in configuration
 echo "NTP=$preferred_ntp_servers" >> "$config_file"
 echo "FallbackNTP=$fallback_ntp_servers" >> "$config_file"
 
