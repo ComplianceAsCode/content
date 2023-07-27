a409ccbdb85c28b41839d4c549caa4ed536fac50
teacup-on-rockingchair <315160+teacup-on-rockingchair@users.noreply.github.com>
2023-07-27 09:55:44 +0300

    Align comment with code

--- linux_os/guide/services/ntp/service_timesyncd_root_distance_configured/bash/shared.sh
+++ linux_os/guide/services/ntp/service_timesyncd_root_distance_configured/bash/shared.sh
@@ -7,10 +7,10 @@
 config_file="/etc/systemd/timesyncd.d/oscap-remedy.conf"
 current_cfg_arr=( "/etc/systemd/timesyncd.conf" )
 current_cfg_arr+=("$(ls /etc/systemd/timesyncd.d/*)")
-# Comment existing NTP FallbackNTP and RootDistance settings
+# Comment existing NTP RootDistance settings
 for current_cfg in "${current_cfg_arr[@]}"
 do
     sed -i 's/^RootDistanceMax/#&/g' "$current_cfg"
 done
-# Set primary fallback NTP servers and RootDistance in drop-in configuration
+# Set RootDistance in drop-in configuration
 echo "RootDistanceMax=1" >> "$config_file"
