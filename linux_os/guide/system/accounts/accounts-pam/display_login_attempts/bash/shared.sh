3a98bb410518b4ba21e7e2764342bb86ce6eb9d2
Marcus Burghardt <maburgha@redhat.com>
2023-08-10 16:10:35 +0200

    Update bash remediation in alignment to the check

--- linux_os/guide/system/accounts/accounts-pam/display_login_attempts/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-pam/display_login_attempts/bash/shared.sh
@@ -1,9 +1,10 @@
 # platform = multi_platform_sle,Red Hat Virtualization 4,multi_platform_fedora,multi_platform_ol,multi_platform_rhel,multi_platform_ubuntu
 
-{{% if product in ["sle12", "sle15"] or "ubuntu" in product %}}
-{{{ bash_ensure_pam_module_configuration('/etc/pam.d/login', 'session', 'required', 'pam_lastlog.so', 'showfailed', '', 'BOF') }}}
-{{{ bash_remove_pam_module_option_configuration('/etc/pam.d/login', 'session', '', 'pam_lastlog.so', 'silent') }}}
+{{% if product in ["sle12", "sle15"] or 'ubuntu' in product %}}
+{{% set pam_lastlog_path = "/etc/pam.d/login" %}}
 {{% else %}}
-{{{ bash_ensure_pam_module_configuration('/etc/pam.d/postlogin', 'session', 'required', 'pam_lastlog.so', 'showfailed', '', 'BOF') }}}
-{{{ bash_remove_pam_module_option_configuration('/etc/pam.d/postlogin', 'session', '', 'pam_lastlog.so', 'silent') }}}
+{{% set pam_lastlog_path = "/etc/pam.d/postlogin" %}}
 {{% endif %}}
+
+{{{ bash_ensure_pam_module_configuration(pam_lastlog_path, 'session', '\[default=1\]', 'pam_lastlog.so', 'showfailed', '', 'BOF') }}}
+{{{ bash_remove_pam_module_option_configuration(pam_lastlog_path, 'session', '\[default=1\]', 'pam_lastlog.so', 'silent') }}}
