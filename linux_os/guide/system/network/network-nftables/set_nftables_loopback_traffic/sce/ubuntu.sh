3d87ab84f4b6ff4439bd7ceb46614420fb51cfe0
Eduardo Barretto <eduardo.barretto@canonical.com>
2023-08-24 17:54:23 +0200

    nftables: sce: Improve scripts to avoid text being recognized
    as bash special characters

--- linux_os/guide/system/network/network-nftables/set_nftables_loopback_traffic/sce/ubuntu.sh
+++ linux_os/guide/system/network/network-nftables/set_nftables_loopback_traffic/sce/ubuntu.sh
@@ -3,16 +3,16 @@
 # check-import = stdout
 
 output=$(nft list ruleset | awk '/hook input/,/}/')
-if ! grep 'iif "lo" accept' "$output"; then
+if ! echo "$output" | grep -q 'iif "lo" accept'; then
     exit "${XCCDF_RESULT_FAIL}"
 fi
 
-if ! grep 'ip saddr' "$output"; then
+if ! echo "$output" | grep -q 'ip saddr'; then
     exit "${XCCDF_RESULT_FAIL}"
 fi
 
 if [ -e /proc/sys/net/ipv6/conf/all/disable_ipv6 ] && [ "$(cat /proc/sys/net/ipv6/conf/all/disable_ipv6)" -eq 0 ]; then
-    if ! grep 'ip6 saddr' "$output"; then
+    if ! echo "$output" | grep -q 'ip6 saddr'; then
         exit "${XCCDF_RESULT_FAIL}"
     fi
 fi
