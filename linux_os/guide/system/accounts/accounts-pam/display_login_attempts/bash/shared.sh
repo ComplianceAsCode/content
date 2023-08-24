5a983d0b0bc410feb7d00108fc0cfdcd4262b690
Marcus Burghardt <maburgha@redhat.com>
2023-08-24 09:22:35 +0200

    Remove extra blank lines from remediation

--- linux_os/guide/system/accounts/accounts-pam/display_login_attempts/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-pam/display_login_attempts/bash/shared.sh
@@ -1,10 +1,10 @@
 # platform = multi_platform_sle,Red Hat Virtualization 4,multi_platform_fedora,multi_platform_ol,multi_platform_rhel,multi_platform_ubuntu
 
-{{% if product in ["sle12", "sle15"] or 'ubuntu' in product %}}
-{{% set pam_lastlog_path = "/etc/pam.d/login" %}}
-{{% else %}}
-{{% set pam_lastlog_path = "/etc/pam.d/postlogin" %}}
-{{% endif %}}
+{{%- if product in ["sle12", "sle15"] or 'ubuntu' in product %}}
+{{%- set pam_lastlog_path = "/etc/pam.d/login" %}}
+{{%- else %}}
+{{%- set pam_lastlog_path = "/etc/pam.d/postlogin" %}}
+{{%- endif %}}
 
 {{{ bash_ensure_pam_module_configuration(pam_lastlog_path, 'session', '\[default=1\]', 'pam_lastlog.so', 'showfailed', '', 'BOF') }}}
 {{{ bash_remove_pam_module_option_configuration(pam_lastlog_path, 'session', '\[default=1\]', 'pam_lastlog.so', 'silent') }}}
