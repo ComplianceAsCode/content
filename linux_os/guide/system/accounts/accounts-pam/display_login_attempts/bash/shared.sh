933503d46567fbf3989918582b66bcac93d06077
Marcus Burghardt <maburgha@redhat.com>
2023-08-29 10:04:43 +0200

    Make control more flexible in remediation
    Although the control is not critical for the OVAL check, it is relevant
    for remediation so distros can use their own preferences.

--- linux_os/guide/system/accounts/accounts-pam/display_login_attempts/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-pam/display_login_attempts/bash/shared.sh
@@ -1,13 +1,19 @@
 # platform = multi_platform_sle,Red Hat Virtualization 4,multi_platform_fedora,multi_platform_ol,multi_platform_rhel,multi_platform_ubuntu
 
-{{%- if product in ["sle12", "sle15"] or 'ubuntu' in product %}}
+{{%- if "sle" in product or "ubuntu" in product %}}
 {{%- set pam_lastlog_path = "/etc/pam.d/login" %}}
-{{%- set control = "optional" %}}
 {{%- set after_match = "^\s*session.*include\s+common-session$" %}}
 {{%- else %}}
 {{%- set pam_lastlog_path = "/etc/pam.d/postlogin" %}}
-{{%- set control = "\[default=1\]" %}}
 {{%- set after_match = "^\s*session\s+.*pam_succeed_if\.so.*" %}}
+{{%- endif %}}
+
+{{%- if "ol" in product or "ubuntu" in product %}}
+{{%- set control = "required" %}}
+{{%- elif "sle" in product %}}
+{{%- set control = "optional" %}}
+{{%- else %}}
+{{%- set control = "\[default=1\]" %}}
 {{%- endif %}}
 
 {{{ bash_pam_lastlog_enable_showfailed(pam_lastlog_path, control, after_match) }}}
