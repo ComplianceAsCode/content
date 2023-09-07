3164759774d534c29a6eeccaf1ce3c03a0557ad7
Eduardo Barretto <eduardo.barretto@canonical.com>
2023-09-07 09:39:26 +0200

    sshd_use_approved_kex_ordered_stig: tests: Fix typo in correct_scrambled

--- linux_os/guide/services/ssh/ssh_server/sshd_use_approved_kex_ordered_stig/tests/correct_scrambled.fail.sh
+++ linux_os/guide/services/ssh/ssh_server/sshd_use_approved_kex_ordered_stig/tests/correct_scrambled.fail.sh
@@ -7,7 +7,7 @@ readarray -t KEX_ALGOS_ARR < <(echo $KEX_ALGOS | tr "," "\n")
 
 #swap first and second algorithms
 
-KEX_ALGOS_SCRAMBLED=$(echo ${KEY_ALGOS_ARR[0]},${KEY_ALGOS_ARR[1]},$(echo ${KEY_ALGOS_ARR[@]:2} | tr " " ","))
+KEX_ALGOS_SCRAMBLED=$(echo ${KEX_ALGOS_ARR[0]},${KEX_ALGOS_ARR[1]},$(echo ${KEX_ALGOS_ARR[@]:2} | tr " " ","))
 
 CONF="${CONF_PREFIX}${KEX_ALGOS_SCRAMBLED}${CONF_SUFIX}"
 
