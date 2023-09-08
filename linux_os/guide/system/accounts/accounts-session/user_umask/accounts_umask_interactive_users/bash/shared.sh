a3065847124f81c75a7b0a23c119a858c224aed4
Edgar Aguilar <edgar.aguilar@oracle.com>
2023-09-08 17:12:09 -0600

    Fix bash for accounts_umask_interactive_users
    The sed regex doesn't find correctly blanck spaces at the beginning
    of the configuration line
    
    Signed-off-by: Edgar Aguilar <edgar.aguilar@oracle.com>

--- linux_os/guide/system/accounts/accounts-session/user_umask/accounts_umask_interactive_users/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-session/user_umask/accounts_umask_interactive_users/bash/shared.sh
@@ -7,7 +7,7 @@
 {{% call iterate_over_command_output("dir", "awk -F':' '{ if ($3 >= " ~ uid_min ~ " && $3 != " ~ nobody_uid ~ ") print $6}' /etc/passwd") -%}}
 {{% call iterate_over_find_output("file", '$dir -maxdepth 1 -type f -name ".*"') -%}}
 if [ "$(basename $file)" != ".bash_history" ]; then
-    sed -i 's/^\([\s]*umask\s*\)/#\1/g' "$file"
+    sed -i 's/^\(\s*umask\s*\)/#\1/g' "$file"
 fi
 {{%- endcall %}}
 {{%- endcall %}}
