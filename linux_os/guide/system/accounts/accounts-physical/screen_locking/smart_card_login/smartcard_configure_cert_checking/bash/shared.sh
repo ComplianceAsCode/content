7cd441ee8b938bd28586cdb1fe8d8a7ffc823062
Dexter Le <dexter.le@sap.com>
2023-09-15 17:32:57 +0000

    Consolidate ubuntu bash remediation into shared
    This commit will consolidate the bash specific remediation of ubuntu into the shared bash remediation. This involves adding in a conditional check to ensure that the pam_pkcs11 equivalent package for ubuntu is added.

--- linux_os/guide/system/accounts/accounts-physical/screen_locking/smart_card_login/smartcard_configure_cert_checking/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-physical/screen_locking/smart_card_login/smartcard_configure_cert_checking/bash/shared.sh
@@ -1,6 +1,10 @@
-# platform = Red Hat Enterprise Linux 7,Red Hat Enterprise Linux 8,multi_platform_ol,multi_platform_sle
+# platform = Red Hat Enterprise Linux 7,Red Hat Enterprise Linux 8,multi_platform_ol,multi_platform_sle,multi_platform_ubuntu
 
+{{% if 'ubuntu' in product %}}
+{{{ bash_package_install("libpam-pkcs11") }}}
+{{% else %}}
 {{{ bash_package_install("pam_pkcs11") }}}
+{{% endif %}}
 
 if grep "^\s*cert_policy" /etc/pam_pkcs11/pam_pkcs11.conf | grep -qv "ocsp_on"; then
 	sed -i "/^\s*#/! s/cert_policy.*/cert_policy = ca, ocsp_on, signature;/g" /etc/pam_pkcs11/pam_pkcs11.conf
