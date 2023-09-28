b28df4832be9a4a736316f4fbeb4a60bbcb77e21
Dexter Le <dexter.le@sap.com>
2023-09-28 15:00:08 +0000

    Use shared remediations for non-template applicable OS's
    This commit will remove specific remediations for sle15 and sle12 to adopt shared remediations. This is necessary for the following OS's because the supplied template within rule.yml is not applicable.

--- linux_os/guide/system/auditing/auditd_configure_rules/audit_privileged_commands/audit_rules_privileged_commands_kmod/bash/ubuntu2004.sh
+++
@@ -1,5 +0,0 @@
-# platform = multi_platform_ubuntu
-
-# Perform the remediation for both possible tools: 'auditctl' and 'augenrules'
-{{{ bash_fix_audit_watch_rule("auditctl", "/bin/kmod", "x", "modules") }}}
-{{{ bash_fix_audit_watch_rule("augenrules", "/bin/kmod", "x", "modules") }}}
