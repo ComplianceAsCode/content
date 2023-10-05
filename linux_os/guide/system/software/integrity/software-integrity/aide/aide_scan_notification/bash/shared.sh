90c29e5a69a7a6cb89c771a7c9c993eb99f6e897
teacup-on-rockingchair <315160+teacup-on-rockingchair@users.noreply.github.com>
2023-10-05 09:56:57 +0300

    Add SLE15 specific remediation for aide check notification

--- linux_os/guide/system/software/integrity/software-integrity/aide/aide_scan_notification/bash/shared.sh
+++ linux_os/guide/system/software/integrity/software-integrity/aide/aide_scan_notification/bash/shared.sh
@@ -3,9 +3,28 @@
 {{{ bash_package_install("aide") }}}
 {{{ bash_instantiate_variables("var_aide_scan_notification_email") }}}
 
-CRONTAB=/etc/crontab
-CRONDIRS='/etc/cron.d /etc/cron.daily /etc/cron.weekly /etc/cron.monthly'
-
+{{% if product in ["sle15"] %}}
+# create unit file for periodic aide database check
+cat > /etc/systemd/system/aidecheck.service <<CHECKEOF
+[Unit]
+        Description=Aide Check
+        Before=aidecheck-notify.service
+        Wants=aidecheck-notify.service
+        [Service]
+        Type=forking
+        ExecStart={{{ aide_bin_path }}} --check -r file:/tmp/aide-report.log
+        [Install]
+        WantedBy=multi-user.target
+CHECKEOF
+cat > /etc/systemd/system/aidecheck-notify.service <<NOTIFYEOF
+[Unit]        
+        Description=Status email for AIDE check result
+        After=aidecheck.service
+        [Service]
+        Type=forking
+        ExecStart=/bin/sh -c 'cat /tmp/aide-report.log | /bin/mail -s "$(hostname) - AIDE Integrity Check"  {{ var_aide_scan_notification_email }}'
+NOTIFYEOF
+{{% else %}}
 # NOTE: on some platforms, /etc/crontab may not exist
 if [ -f /etc/crontab ]; then
 	CRONTAB_EXIST=/etc/crontab
@@ -18,4 +37,4 @@ fi
 if ! grep -qR '^.*{{{ aide_bin_path }}}\s*\-\-check.*|.*\/bin\/mail\s*-s\s*".*"\s*.*@.*$' $CRONTAB_EXIST $VARSPOOL $CRONDIRS; then
 	echo "0 5 * * * root {{{ aide_bin_path }}}  --check | /bin/mail -s \"\$(hostname) - AIDE Integrity Check\" $var_aide_scan_notification_email" >> $CRONTAB
 fi
-
+{{% endif %}}
