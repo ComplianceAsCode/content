31a135be0757eeb5be3b933ba55be40f43172dba
teacup-on-rockingchair <315160+teacup-on-rockingchair@users.noreply.github.com>
2023-11-05 15:47:42 +0200

    Improve stability of timesyncd based remediation
    Improve generating array of config files for timesyncd service
    Improve stability of ansible remediation jinja code to get last element. In some cases last element cannot be extracted directly and appears issue like https://github.com/pallets/jinja/issues/897

--- linux_os/guide/services/ntp/service_timesyncd_configured/bash/shared.sh
+++ linux_os/guide/services/ntp/service_timesyncd_configured/bash/shared.sh
@@ -5,22 +5,23 @@
 # disruption = low
 
 {{{ bash_instantiate_variables("var_multiple_time_servers") }}}
-
 IFS=',' read -r -a time_servers_array <<< "$var_multiple_time_servers"
 preferred_ntp_servers_array=("${time_servers_array[@]:0:2}")
 preferred_ntp_servers=$( echo "${preferred_ntp_servers_array[@]}"|sed -e 's/\s\+/,/g' )
 fallback_ntp_servers_array=("${time_servers_array[@]:2}")
 fallback_ntp_servers=$( echo "${fallback_ntp_servers_array[@]}"|sed -e 's/\s\+/,/g' )
 
+IFS=" " mapfile -t current_cfg_arr < <(ls -1 /etc/systemd/timesyncd.d/* 2>/dev/null)
 config_file="/etc/systemd/timesyncd.d/oscap-remedy.conf"
-current_cfg_arr=( "/etc/systemd/timesyncd.conf" )
-current_cfg_arr+=("$(ls /etc/systemd/timesyncd.d/*)")
+current_cfg_arr+=( "/etc/systemd/timesyncd.conf" )
 # Comment existing NTP FallbackNTP settings
-for current_cfg in "${current_cfg_arr[@]}"
-do
-    sed -i 's/^NTP/#&/g' "$current_cfg"
-    sed -i 's/^FallbackNTP/#&/g' "$current_cfg"
-done
+if [ ${#current_cfg_arr[@]} -ne 0 ]; then
+    for current_cfg in "${current_cfg_arr[@]}"
+    do
+        sed -i 's/^NTP/#&/g' "$current_cfg"
+        sed -i 's/^FallbackNTP/#&/g' "$current_cfg"
+    done
+fi
 # Set primary fallback NTP servers in drop-in configuration
 echo "NTP=$preferred_ntp_servers" >> "$config_file"
 echo "FallbackNTP=$fallback_ntp_servers" >> "$config_file"
