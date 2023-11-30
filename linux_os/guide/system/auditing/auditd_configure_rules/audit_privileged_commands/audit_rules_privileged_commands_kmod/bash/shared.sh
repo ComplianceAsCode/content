f89db6825dbf08cba3bc5d473e8edb8c077cbad6
Vojtech Polasek <vpolasek@redhat.com>
2023-11-30 17:19:30 +0100

    remove explicit remediations, check and tests from templated rule audit_rules_privileged_commands_kmod

--- linux_os/guide/system/auditing/auditd_configure_rules/audit_privileged_commands/audit_rules_privileged_commands_kmod/bash/shared.sh
+++
@@ -1,5 +0,0 @@
-# platform = multi_platform_all
-
-# Perform the remediation for both possible tools: 'auditctl' and 'augenrules'
-{{{ bash_fix_audit_watch_rule("auditctl", "/usr/bin/kmod", "x", "modules") }}}
-{{{ bash_fix_audit_watch_rule("augenrules", "/usr/bin/kmod", "x", "modules") }}}
--- linux_os/guide/system/auditing/auditd_configure_rules/audit_privileged_commands/audit_rules_privileged_commands_kmod/tests/auditctl_correct_value.pass.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# packages = audit
-
-echo "-w /usr/bin/kmod -p x -k modules" >> /etc/audit/audit.rules
-sed -i "s%^ExecStartPost=.*%ExecStartPost=-/sbin/auditctl%" /usr/lib/systemd/system/auditd.service
