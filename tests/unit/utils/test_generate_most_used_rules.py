ed3c530988c4b048f9a19b07be6862c2e12bb169
Jan Rodak <hony.com@seznam.cz>
2024-01-11 15:48:45 +0100

    Add test

---
+++ tests/unit/utils/test_generate_most_used_rules.py
@@ -0,0 +1,20 @@
+import os
+from argparse import Namespace
+from utils.profile_tool import command_most_used_rules
+
+DATA_DIR = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), "..", "ssg-module", "data")
+)
+DATA_STREAM_PATH = os.path.join(DATA_DIR, "simple_data_stream.xml")
+
+
+def get_fake_args():
+    return Namespace(
+        subcommand="most-used-rules", benchmarks=[str(DATA_STREAM_PATH)], format="plain"
+    )
+
+
+def test_command(capsys):
+    command_most_used_rules(get_fake_args())
+    captured = capsys.readouterr()
+    assert "xccdf_com.example.www_rule_test-pass: 1" in captured.out
