f68e1e8f0a42e9b7a9371eb85614da8eb5c18b5f
Jan Rodak <hony.com@seznam.cz>
2024-01-17 11:51:36 +0100

    Add processing of controlfiles

--- utils/profile_tool/most_used_rules.py
+++ utils/profile_tool/most_used_rules.py
@@ -1,10 +1,16 @@
 import json
 
 from ssg.build_profile import XCCDFBenchmark
+from .profile import get_profile
+from ..controleval import (
+    load_controls_manager,
+    get_available_products,
+    get_product_profiles_files,
+)
 
 
-def _count_rules_per_profile(profile, rules):
-    for rule in profile.get("rules", []):
+def _count_rules_per_rules_list(rules_list, rules):
+    for rule in rules_list:
         if rule in rules:
             rules[rule] += 1
         else:
@@ -14,7 +20,23 @@ def _count_rules_per_profile(profile, rules):
 def _count_rules_per_benchmark(benchmark, rules):
     benchmark = XCCDFBenchmark(benchmark)
     for profile in benchmark.get_all_profile_stats():
-        _count_rules_per_profile(profile, rules)
+        _count_rules_per_rules_list(profile.get("rules", []), rules)
+
+
+def _get_profiles_for_product(ctrls_mgr, product):
+    profiles_files = get_product_profiles_files(product)
+
+    profiles = []
+    for file in profiles_files:
+        profiles.append(get_profile(profiles_files, file, ctrls_mgr.policies))
+    return profiles
+
+
+def _process_all_products_from_controls(rules):
+    for product in get_available_products():
+        controls_manager = load_controls_manager("./controls/", product)
+        for profile in _get_profiles_for_product(controls_manager, product):
+            _count_rules_per_rules_list(profile.rules, rules)
 
 
 def _sorted_rules(rules):
@@ -27,8 +49,12 @@ def _sorted_rules(rules):
 
 def command_most_used_rules(args):
     rules = {}
-    for benchmark in args.benchmarks:
-        _count_rules_per_benchmark(benchmark, rules)
+
+    if not args.BENCHMARKS:
+        _process_all_products_from_controls(rules)
+    else:
+        for benchmark in args.BENCHMARKS:
+            _count_rules_per_benchmark(benchmark, rules)
 
     sorted_rules = _sorted_rules(rules)
 
