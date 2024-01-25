0acdf79c2032f5014737cc64c9ac8528d3879270
Matthew Burket <mburket@redhat.com>
2024-01-25 07:01:17 -0600

    Remove 'exec' from configure_bashrc_exec_tmux

--- linux_os/guide/system/accounts/accounts-physical/screen_locking/console_screen_locking/configure_bashrc_exec_tmux/tests/correct_value.pass.sh
+++ linux_os/guide/system/accounts/accounts-physical/screen_locking/console_screen_locking/configure_bashrc_exec_tmux/tests/correct_value.pass.sh
@@ -5,7 +5,7 @@ cat >> /etc/bashrc <<'EOF'
 if [ "$PS1" ]; then
   parent=$(ps -o ppid= -p $$)
   name=$(ps -o comm= -p $parent)
-  case "$name" in sshd|login) exec tmux ;; esac
+  case "$name" in sshd|login) tmux ;; esac
 fi
 EOF
 
