0259f77573f7661c65a3547844eba855125651ab
Vojtech Polasek <vpolasek@redhat.com>
2024-01-26 15:48:21 +0100

    auditd_data_retention_admin_space_left_action - allow multiple config values

--- linux_os/guide/system/auditing/configure_auditd_data_retention/auditd_data_retention_admin_space_left_action/bash/shared.sh
+++ linux_os/guide/system/auditing/configure_auditd_data_retention/auditd_data_retention_admin_space_left_action/bash/shared.sh
@@ -2,6 +2,8 @@
 
 {{{ bash_instantiate_variables("var_auditd_admin_space_left_action") }}}
 
+var_auditd_admin_space_left_action="$(echo $var_auditd_admin_space_left_action | cut -d \| -f 1)"
+
 AUDITCONFIG=/etc/audit/auditd.conf
 
 {{{ bash_replace_or_append("$AUDITCONFIG", '^admin_space_left_action', "$var_auditd_admin_space_left_action") }}}
