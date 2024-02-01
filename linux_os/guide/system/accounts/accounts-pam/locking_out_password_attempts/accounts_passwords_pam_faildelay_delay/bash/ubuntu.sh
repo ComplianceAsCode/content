7a376e1a200cf4ddd57d3307afe60bbd36d6f138
Miha Purg <miha.purg@canonical.com>
2024-02-01 18:22:23 +0100

    Fix ubuntu remediation for pam_faildelay
    pam_faildelay module was placed at end of /etc/pam.d/common-auth,
    after various modules with success=die, sufficient and requisite,
    and was thus never reached.
    
    The solution is to place the module at beginning of file.

--- linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_passwords_pam_faildelay_delay/bash/ubuntu.sh
+++ linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_passwords_pam_faildelay_delay/bash/ubuntu.sh
@@ -2,4 +2,4 @@
 
 {{{ bash_instantiate_variables("var_password_pam_delay") }}}
 
-{{{ bash_ensure_pam_module_options('/etc/pam.d/common-auth', 'auth', 'required', 'pam_faildelay.so', 'delay', "$var_password_pam_delay", "$var_password_pam_delay") }}}
+{{{ bash_ensure_pam_module_option('/etc/pam.d/common-auth', 'auth', 'required', 'pam_faildelay.so', 'delay', "$var_password_pam_delay", 'BOF') }}}
