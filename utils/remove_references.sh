33dd8ad8baf46f225ef776d60cb8b75ea42c6144
Jan Černý <jcerny@redhat.com>
2024-02-05 11:39:34 +0100

    Solve Code Climate problem

--- utils/remove_references.sh
+++ utils/remove_references.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-for x in $(find linux_os -name 'rule.yml') ; do
-	sed -i '/^\s*cis@rhel[7,8,9]:/d' $x
-done
+while IFS= read -r -d '' file
+do
+	sed -i '/^\s*cis@rhel[7,8,9]:/d' "$file"
+done < <(find linux_os -name 'rule.yml' -print0)
