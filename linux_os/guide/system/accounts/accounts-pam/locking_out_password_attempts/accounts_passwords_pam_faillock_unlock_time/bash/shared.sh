a438906f3ddba63a91751e43adfcfc88ec37ecff
Alexandre SKRZYNIARZ <alexandre.skrzyniarz@laposte.net>
2024-02-28 16:20:39 +0100

    Update linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_passwords_pam_faillock_unlock_time/bash/shared.sh
    Co-authored-by: Marcus Burghardt <2074099+marcusburghardt@users.noreply.github.com>

--- linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_passwords_pam_faillock_unlock_time/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_passwords_pam_faillock_unlock_time/bash/shared.sh
@@ -1,4 +1,4 @@
-# platform = multi_platform_rhel,multi_platform_fedora,multi_platform_ol,multi_platform_rhv,multi_platform_ubuntu,multi_platform_debian
+# platform = multi_platform_all
 
 {{{ bash_instantiate_variables("var_accounts_passwords_pam_faillock_unlock_time") }}}
 
