5ef97d15022fb88869bceb863f71c84394116f82
Jan Černý <jcerny@redhat.com>
2024-03-01 13:01:48 +0100

    Do not reformat if not needed
    If we have Python 3.9 or newer, we use the ElementTree.indent method
    which formats the output which means the output doesn't have to be
    reformatted.

--- cmake/SSGCommon.cmake
+++ cmake/SSGCommon.cmake
@@ -425,15 +425,26 @@ macro(ssg_build_xccdf_final PRODUCT)
 
 endmacro()
 
-# Apply a final xmllint pass over the OVAL document to pretty-format the output
-# OVAL document for the product.
+# If built with Python older than 3.9, apply a final xmllint pass over the
+# OVAL document to pretty-format the output OVAL document for the product.
+# If Python 3.9 or newer is used for the build, the output OVAL is already
+# generated pretty and doesn't need to be reformatted.
 macro(ssg_build_oval_final PRODUCT)
-    add_custom_command(
-        OUTPUT "${CMAKE_BINARY_DIR}/ssg-${PRODUCT}-oval.xml"
-        COMMAND "${XMLLINT_EXECUTABLE}" --nsclean --format --output "${CMAKE_BINARY_DIR}/ssg-${PRODUCT}-oval.xml" "${CMAKE_CURRENT_BINARY_DIR}/ssg-${PRODUCT}-oval.xml"
-        DEPENDS generate-${PRODUCT}-xccdf-oval-ocil "${CMAKE_CURRENT_BINARY_DIR}/ssg-${PRODUCT}-xccdf.xml" "${CMAKE_CURRENT_BINARY_DIR}/ssg-${PRODUCT}-oval.xml" "${CMAKE_CURRENT_BINARY_DIR}/ssg-${PRODUCT}-ocil.xml"
-        COMMENT "[${PRODUCT}-content] generating ssg-${PRODUCT}-oval.xml"
-    )
+    if (PYTHON_VERSION_MAJOR LESS 3 OR PYTHON_VERSION_MINOR LESS 9)
+        add_custom_command(
+            OUTPUT "${CMAKE_BINARY_DIR}/ssg-${PRODUCT}-oval.xml"
+            COMMAND "${XMLLINT_EXECUTABLE}" --nsclean --format --output "${CMAKE_BINARY_DIR}/ssg-${PRODUCT}-oval.xml" "${CMAKE_CURRENT_BINARY_DIR}/ssg-${PRODUCT}-oval.xml"
+            DEPENDS generate-${PRODUCT}-xccdf-oval-ocil "${CMAKE_CURRENT_BINARY_DIR}/ssg-${PRODUCT}-xccdf.xml" "${CMAKE_CURRENT_BINARY_DIR}/ssg-${PRODUCT}-oval.xml" "${CMAKE_CURRENT_BINARY_DIR}/ssg-${PRODUCT}-ocil.xml"
+            COMMENT "[${PRODUCT}-content] generating ssg-${PRODUCT}-oval.xml"
+        )
+    else()
+        add_custom_command(
+            OUTPUT "${CMAKE_BINARY_DIR}/ssg-${PRODUCT}-oval.xml"
+            COMMAND ${CMAKE_COMMAND} -E copy "${CMAKE_CURRENT_BINARY_DIR}/ssg-${PRODUCT}-oval.xml" "${CMAKE_BINARY_DIR}/ssg-${PRODUCT}-oval.xml"
+            DEPENDS generate-${PRODUCT}-xccdf-oval-ocil "${CMAKE_CURRENT_BINARY_DIR}/ssg-${PRODUCT}-xccdf.xml" "${CMAKE_CURRENT_BINARY_DIR}/ssg-${PRODUCT}-oval.xml" "${CMAKE_CURRENT_BINARY_DIR}/ssg-${PRODUCT}-ocil.xml"
+            COMMENT "[${PRODUCT}-content] generating ssg-${PRODUCT}-oval.xml"
+        )
+    endif()
     add_custom_target(
         generate-ssg-${PRODUCT}-oval.xml
         DEPENDS "${CMAKE_BINARY_DIR}/ssg-${PRODUCT}-oval.xml"
