e5cff1f751570c429859240396a56c6e8bda40d1
rchikov <rumen.chikov@suse.com>
2024-03-15 09:48:37 +0100

    Addes some new tests

---
+++ linux_os/guide/system/software/updating/ensure_gpgcheck_globally_activated/tests/wrong_value_off.fail.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+{{% if 'sle' in product %}}
+# packages = libselinux1
+{{% else %}} 
+# packages = python3-libselinux
+{{% endif %}}
+
+. $SHARED/group_updating_utils.sh
+
+config_file="$(find_config_file)"
+
+if grep -q "^gpgcheck" "$config_file"; then
+	sed -i "s/^gpgcheck.*/gpgcheck=off/" "$config_file"
+else
+	echo "gpgcheck=off" >> "$config_file"
+fi
