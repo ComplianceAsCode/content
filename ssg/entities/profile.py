ComplianceAsCode/content

commit 890ce49010529494bea0dabeedec0326fa885795

Fix broken profile resolution
First, rename the function `resolve_selections_with_rules` to
`apply_filter` to better express the purpose of this function.

Second, move the call of `apply_filter` to a correct place.
Previously, the function was checking if a rule ID is a rule that
exists and is applicable to the currently built product. It removed
the selections that selected the rules that don't exist. However,
this operation shadowed a check in `resolve` that served to raise
an exception if a rule isn't available. This exception
could never be raised because at the moment of the check guarding
the exception the selections already contained only existing rules.
This flaw is fixed by moving the `apply_filter` call after
the exception and removing the duplicate check.

ssg/entities/profile.py
@@ -18,11 +18,9 @@ class ResolvableProfile(Profile):
     def extend_by(self, extended_profile):
         self.update_with(extended_profile)
 
-    def resolve_selections_with_rules(self, rules_by_id):
+    def apply_filter(self, rules_by_id):
         selections = set()
         for rid in self.selected:
-            if rid not in rules_by_id:
-                continue
             rule = rules_by_id[rid]
             if not self.rule_filter(rule):
                 continue
@@ -36,7 +34,6 @@ class ResolvableProfile(Profile):
         if controls_manager:
             self.resolve_controls(controls_manager)
 
-        self.resolve_selections_with_rules(rules_by_id)
 
         if self.extends:
             if self.extends not in all_profiles:
@@ -65,6 +62,8 @@ class ResolvableProfile(Profile):
                     "This may be caused by a discrepancy of prodtypes."
                     .format(rid=rid, profile=self.id_))
                 raise ValueError(msg)
+
+        self.apply_filter(rules_by_id)
 
         self.resolved = True
 

commit 33e45637770ee68e53604dba395ae93f34bfc339

Stabilize resolved profiles
Currently, the resolved profiles have random order of items in the
selections list. This makes it less convenient to update the profile
stability test data, because copying a resolved profile causes shuffling
the git diff. This problem is a recent regression. Git bisect shows
that it has been broken by 890ce49010529494bea0dabeedec0326fa885795.
The proposed fix is to use a list instead of set so that the original
order of elements is preserved.

Related to conversation in #11719.

ssg/entities/profile.py
@@ -19,13 +19,13 @@ class ResolvableProfile(Profile):
         self.update_with(extended_profile)
 
     def apply_filter(self, rules_by_id):
-        selections = set()
+        selections = []
         for rid in self.selected:
             rule = rules_by_id[rid]
             if not self.rule_filter(rule):
                 continue
-            selections.add(rid)
-        self.selected = list(selections)
+            selections.append(rid)
+        self.selected = selections
 
     def resolve(self, all_profiles, rules_by_id, controls_manager=None):
         if self.resolved: