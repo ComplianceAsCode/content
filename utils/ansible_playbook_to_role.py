d49e4e752402276558dfba80af64e2cc73edc4c7
Matthew Burket <mburket@redhat.com>
2024-03-22 14:17:32 -0500

    Clean up utils/ansible_playbook_to_role.py

--- utils/ansible_playbook_to_role.py
+++ utils/ansible_playbook_to_role.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
 
 from __future__ import print_function
 
@@ -607,7 +606,7 @@ def main():
             elif repo.name not in potential_roles:
                 print("Repo '%s' is not managed by this script. "
                       "It may need to be deleted, please verify and do that "
-                      "manually!" % repo.name)
+                      "manually!" % repo.name, file=sys.stderr)
 
 
 if __name__ == "__main__":
