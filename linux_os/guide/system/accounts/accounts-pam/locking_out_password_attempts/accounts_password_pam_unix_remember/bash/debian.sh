f44e383effeaacd7997408ace5dd45ab61a286d8
Alexandre Skrzyniarz <alexandre.skrzyniarz@laposte.net>
2024-03-27 17:29:57 +0100

    factorise code in pam_account_password_unix_remember

--- linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_password_pam_unix_remember/bash/debian.sh
+++
@@ -1,5 +0,0 @@
-# platform = multi_platform_debian
-
-{{{ bash_instantiate_variables("var_password_pam_unix_remember") }}}
-
-{{{ bash_ensure_pam_module_options('/etc/pam.d/common-password', 'password', '\[success=[[:alnum:]].*\]', 'pam_unix.so', 'remember', "$var_password_pam_unix_remember", "$var_password_pam_unix_remember") }}}
