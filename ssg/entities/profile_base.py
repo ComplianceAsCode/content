f742f09ac7167d70f36a83b247b9fd1dd9dbf82b
Vojtech Polasek <vpolasek@redhat.com>
2024-04-11 15:57:57 +0200

    add function profile.remove_components_not_included
    this function takes the data structure containing components which were for some reason excluded from the datastream.
    The function ensures that data structures of the Profile class do not include those components (rules, groups).

--- ssg/entities/profile_base.py
+++ ssg/entities/profile_base.py
@@ -259,6 +259,13 @@ class Profile(XCCDFEntity, SelectionHandler):
             raise ValueError(msg)
         self.unselected_groups.extend(sorted(empty_groups))
 
+    def remove_components_not_included(self, components_to_not_include):
+        rules_to_not_include = components_to_not_include.get("rules", set())
+        groups_to_not_include = components_to_not_include.get("groups", set())
+        self.selected = list(set(self.selected) - rules_to_not_include)
+        self.unselected = list(set(self.unselected) - rules_to_not_include)
+        self.unselected_groups = list(set(self.unselected_groups) - groups_to_not_include)
+
     def __sub__(self, other):
         profile = Profile(self.id_)
         profile.title = self.title
