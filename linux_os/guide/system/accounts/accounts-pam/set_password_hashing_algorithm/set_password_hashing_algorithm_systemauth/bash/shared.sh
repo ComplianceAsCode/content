e427553cb69977e36763a5b641f2eb3389c86778
Miha Purg <miha.purg@canonical.com>
2024-04-22 17:12:42 +0200

    Fix rule set_password_hashing_algorithm_systemauth on Ubuntu
    Modified bash and OVAL of set_password_hashing_algorithm_systemauth
    to use the correct pathname (/etc/pam.d/common-password) and control
    ([success=... ]) on Ubuntu.

--- linux_os/guide/system/accounts/accounts-pam/set_password_hashing_algorithm/set_password_hashing_algorithm_systemauth/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-pam/set_password_hashing_algorithm/set_password_hashing_algorithm_systemauth/bash/shared.sh
@@ -2,6 +2,13 @@
 
 {{% if product in ["sle15", "sle12"] -%}}
 {{{ bash_ensure_pam_module_configuration('/etc/pam.d/common-password', 'password', 'required', 'pam_unix.so', 'sha512', '', '') }}}
+{{% elif 'ubuntu' in product -%}}
+# Can't use macro bash_ensure_pam_module_configuration because the control
+# contains special characters and is not static ([success=N default=ignore)
+PAM_FILE_PATH=/etc/pam.d/common-password
+if ! grep -qP '^\s*password\s+.*\s+pam_unix.so\s+.*\s+sha512\b' "$PAM_FILE_PATH"; then
+  sed -i -E --follow-symlinks '/\s*password\s+.*\s+pam_unix.so.*/ s/$/ sha512/' "$PAM_FILE_PATH"
+fi
 {{%- else -%}}
 {{{ bash_ensure_pam_module_configuration('/etc/pam.d/system-auth', 'password', 'sufficient', 'pam_unix.so', 'sha512', '', '') }}}
 {{%- endif %}}
