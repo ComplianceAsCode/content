e0800074e8aece49ba5597881b987669904573f6
Miha Purg <miha.purg@canonical.com>
2024-04-24 23:12:55 +0200

    Align chronyd_sync_clock to Ubuntu 22.04 STIG

--- linux_os/guide/services/ntp/chronyd_sync_clock/tests/correct_value.pass.sh
+++ linux_os/guide/services/ntp/chronyd_sync_clock/tests/correct_value.pass.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 # packages = chrony
 
+{{% if product == 'ubuntu2204' -%}}
+echo "makestep 1 1" >> {{{ chrony_conf_path }}}
+{{% else -%}}
 echo "makestep 1 -1" >> {{{ chrony_conf_path }}}
+{{% endif -%}}
