a99b885fc6728e8eee33ae4be6c86f9795dd5754
Miha Purg <miha.purg@canonical.com>
2024-04-25 11:20:55 +0200

    Align wireless_disable_interfaces with Ubuntu 22.04 STIG
    - Created Ubuntu-specific OVAL to check /proc/net/wireless for enabled interfaces.
      The existing implementation relies on the interface name starting with
      "wl", which could be overriden.
    - Modified remediation to disable the driver modules as suggested by CIS and STIG,
      instead of disabling wifi using nmcli.

--- linux_os/guide/system/network/network-wireless/wireless_software/wireless_disable_interfaces/bash/ubuntu.sh
+++ linux_os/guide/system/network/network-wireless/wireless_software/wireless_disable_interfaces/bash/ubuntu.sh
@@ -1,8 +1,6 @@
 # platform = multi_platform_ubuntu
 
-if command -v nmcli >/dev/null 2>&1 ; then
-    nmcli radio all off
-elif [ -n "$(find /sys/class/net/*/ -type d -name wireless)" ]; then
+if [ -n "$(find /sys/class/net/*/ -type d -name wireless)" ]; then
     interfaces=$(find /sys/class/net/*/wireless -type d -name wireless | xargs -0 dirname | xargs basename)
 
     for i in $interfaces; do
