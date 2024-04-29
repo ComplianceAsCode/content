ec6fd3915f8d5a69d74f8b449a6a5fa12cf970d4
Miha Purg <miha.purg@canonical.com>
2024-04-29 13:52:28 +0200

    Fix value syntax for rule dconf_gnome_disable_ctrlaltdel_reboot
    For the `logout` setting to work, the value has to be an array.

--- linux_os/guide/system/software/gnome/gnome_system_settings/dconf_gnome_disable_ctrlaltdel_reboot/tests/correct_value_unlocked.fail.sh
+++ linux_os/guide/system/software/gnome/gnome_system_settings/dconf_gnome_disable_ctrlaltdel_reboot/tests/correct_value_unlocked.fail.sh
@@ -4,4 +4,4 @@
 
 install_dconf_and_gdm_if_needed
 clean_dconf_settings
-add_dconf_setting "org/gnome/settings-daemon/plugins/media-keys" "logout" "''" "local.d" "00-security-settings"
+add_dconf_setting "org/gnome/settings-daemon/plugins/media-keys" "logout" "['']" "local.d" "00-security-settings"
