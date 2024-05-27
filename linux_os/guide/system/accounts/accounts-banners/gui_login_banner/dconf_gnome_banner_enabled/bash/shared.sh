09639ea4cba2fd75c17810aa090e59afcca70287
Miha Purg <miha.purg@canonical.com>
2024-05-27 14:05:54 +0200

    Modify ubuntu remediation for dconf_gnome_banner_enabled
    The remediation was modified to enable the banner also
    in `greeter.dconf-defaults` and not only in the dconf database.
    
    This is to be consistent with the remediation in related rule
    `dconf_gnome_login_banner_text` and be better aligned with STIG V1R1.

--- linux_os/guide/system/accounts/accounts-banners/gui_login_banner/dconf_gnome_banner_enabled/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-banners/gui_login_banner/dconf_gnome_banner_enabled/bash/shared.sh
@@ -3,6 +3,9 @@
 {{% if 'ubuntu' in product %}}
 {{{ bash_enable_dconf_user_profile(profile="user", database="local") }}}
 {{{ bash_enable_dconf_user_profile(profile="gdm", database="gdm") }}}
+# Duplicate the setting also in 'greeter.dconf-defaults' for consistency with
+# 'dconf_gnome_login_banner_text' and better alignment with STIG V1R1.
+{{{ set_config_file("/etc/gdm3/greeter.dconf-defaults", "banner-message-enable", value="true", create='no', insert_after="\[org/gnome/login-screen\]", insert_before="", separator="=", separator_regex="", prefix_regex="^\s*") }}}
 {{% endif %}}
 
 {{{ bash_dconf_settings("org/gnome/login-screen", "banner-message-enable", "true", dconf_gdm_dir, "00-security-settings") }}}
