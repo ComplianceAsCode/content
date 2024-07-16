bdb3abeabd4815fe8a46253723d01a64de4b6865
Marcus Burghardt <maburgha@redhat.com>
2024-07-16 15:06:09 +0200

    Update set_password_hashing_algorithm_passwordauth
    In alignment to set_password_hashing_algorithm_systemauth, oval, bash
    and ansible and test scenarios were updated to properly use the
    var_password_hashing_algorithm_pam variable.
    
    Signed-off-by: Marcus Burghardt <maburgha@redhat.com>

--- linux_os/guide/system/accounts/accounts-pam/set_password_hashing_algorithm/set_password_hashing_algorithm_passwordauth/tests/authselect_multiple_options.fail.sh
+++ linux_os/guide/system/accounts/accounts-pam/set_password_hashing_algorithm/set_password_hashing_algorithm_passwordauth/tests/authselect_multiple_options.fail.sh
@@ -6,8 +6,9 @@
 authselect create-profile hardening -b sssd
 CUSTOM_PROFILE="custom/hardening"
 authselect select $CUSTOM_PROFILE --force
-CUSTOM_PASSWORD_AUTH="/etc/authselect/$CUSTOM_PROFILE/password-auth"
-if ! $(grep -q "^password.*sufficient.*pam_unix\.so.*sha512" "$CUSTOM_PASSWORD_AUTH"); then
-    sed -i --follow-symlinks '/^password.*sufficient.*pam_unix\.so/ s/$/ sha512/' "$CUSTOM_PASSWORD_AUTH"
+CUSTOM_SYSTEM_AUTH="/etc/authselect/$CUSTOM_PROFILE/password-auth"
+if ! $(grep -q "^password.*sufficient.*pam_unix\.so.*sha512" "$CUSTOM_SYSTEM_AUTH"); then
+    sed -i --follow-symlinks '/^password.*sufficient.*pam_unix\.so/ s/$/ sha512/' "$CUSTOM_SYSTEM_AUTH"
 fi
+sed -i --follow-symlinks '/^password.*sufficient.*pam_unix\.so/ s/$/ yescrypt/' "$CUSTOM_SYSTEM_AUTH"
 authselect apply-changes -b
