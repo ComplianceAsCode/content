4c304c09228c9bbdd0a668df7fa4a2bd0b2dcf37
Marcus Burghardt <maburgha@redhat.com>
2024-07-17 12:55:50 +0200

    Improve test scenarios for hashing algorithm in PAM files
    The test scenarios were assuming that sha512 was already present in the
    system used for test, which is not the case, for example in Fedora.
    It was also included a new test scenario script for cases where multiple
    hashing algorithm options are used.
    
    Signed-off-by: Marcus Burghardt <maburgha@redhat.com>

--- linux_os/guide/system/accounts/accounts-pam/set_password_hashing_algorithm/set_password_hashing_algorithm_passwordauth/tests/authselect_multiple_options.fail.sh
+++ linux_os/guide/system/accounts/accounts-pam/set_password_hashing_algorithm/set_password_hashing_algorithm_passwordauth/tests/authselect_multiple_options.fail.sh
@@ -6,9 +6,12 @@
 authselect create-profile hardening -b sssd
 CUSTOM_PROFILE="custom/hardening"
 authselect select $CUSTOM_PROFILE --force
-CUSTOM_SYSTEM_AUTH="/etc/authselect/$CUSTOM_PROFILE/password-auth"
-if ! $(grep -q "^password.*sufficient.*pam_unix\.so.*sha512" "$CUSTOM_SYSTEM_AUTH"); then
-    sed -i --follow-symlinks '/^password.*sufficient.*pam_unix\.so/ s/$/ sha512/' "$CUSTOM_SYSTEM_AUTH"
-fi
-sed -i --follow-symlinks '/^password.*sufficient.*pam_unix\.so/ s/$/ yescrypt/' "$CUSTOM_SYSTEM_AUTH"
+CUSTOM_PASSWORD_AUTH="/etc/authselect/$CUSTOM_PROFILE/password-auth"
+
+declare -a HASHING_ALGORITHMS_OPTIONS=("sha512" "yescrypt")
+for hash_option in "${HASHING_ALGORITHMS_OPTIONS[@]}"; do
+    if ! $(grep -q "^password.*sufficient.*pam_unix\.so.*$hash_option" "$CUSTOM_PASSWORD_AUTH"); then
+        sed -i --follow-symlinks "/^password.*sufficient.*pam_unix\.so/ s/$/ $hash_option/" "$CUSTOM_PASSWORD_AUTH"
+    fi
+done
 authselect apply-changes -b
--- linux_os/guide/system/accounts/accounts-pam/set_password_hashing_algorithm/set_password_hashing_algorithm_systemauth/tests/authselect_multiple_options.fail.sh
+++ linux_os/guide/system/accounts/accounts-pam/set_password_hashing_algorithm/set_password_hashing_algorithm_systemauth/tests/authselect_multiple_options.fail.sh
@@ -7,8 +7,11 @@ authselect create-profile hardening -b sssd
 CUSTOM_PROFILE="custom/hardening"
 authselect select $CUSTOM_PROFILE --force
 CUSTOM_SYSTEM_AUTH="/etc/authselect/$CUSTOM_PROFILE/system-auth"
-if ! $(grep -q "^password.*sufficient.*pam_unix\.so.*sha512" "$CUSTOM_SYSTEM_AUTH"); then
-    sed -i --follow-symlinks '/^password.*sufficient.*pam_unix\.so/ s/$/ sha512/' "$CUSTOM_SYSTEM_AUTH"
-fi
-sed -i --follow-symlinks '/^password.*sufficient.*pam_unix\.so/ s/$/ yescrypt/' "$CUSTOM_SYSTEM_AUTH"
+
+declare -a HASHING_ALGORITHMS_OPTIONS=("sha512" "yescrypt")
+for hash_option in "${HASHING_ALGORITHMS_OPTIONS[@]}"; do
+    if ! $(grep -q "^password.*sufficient.*pam_unix\.so.*$hash_option" "$CUSTOM_SYSTEM_AUTH"); then
+        sed -i --follow-symlinks "/^password.*sufficient.*pam_unix\.so/ s/$/ $hash_option/" "$CUSTOM_SYSTEM_AUTH"
+    fi
+done
 authselect apply-changes -b
