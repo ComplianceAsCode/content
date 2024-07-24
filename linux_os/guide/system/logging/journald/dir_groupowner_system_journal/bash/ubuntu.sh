d89b68244014bf376458d93d3f96351fd61c6249
wqian <alan.moore@canonical.com>
2024-07-24 21:48:50 +0100

    Directly modify the configuration Align with STIG Avoid override the whole configuration

--- linux_os/guide/system/logging/journald/dir_groupowner_system_journal/bash/ubuntu.sh
+++ linux_os/guide/system/logging/journald/dir_groupowner_system_journal/bash/ubuntu.sh
@@ -1,17 +1,15 @@
 # platform = multi_platform_ubuntu
 
-TMPFILES_CONF="/etc/tmpfiles.d/systemd.conf"
+TMPFILES_CONF="/usr/lib/tmpfiles.d/systemd.conf"
 
-if grep -qP "^.[+]*\s+\/var\/log\/journal\s+" "$TMPFILES_CONF"; then
-    sed -i --follow-symlinks "s/\(^.[+]*\)\(\s\+\/var\/log\/journal\s\+[^ $]\+\s\+[^ $]\+\s\+\)\([^ $]\+\)/Z\2systemd-journal/" "$TMPFILES_CONF"
-else
-    echo "Z /var/log/journal ~2750 root systemd-journal - -" >> "$TMPFILES_CONF"
+if grep -qP "^[zZ][+]*\s+\/var\/log\/journal\s+" "$TMPFILES_CONF"; then
+    sed -i --follow-symlinks -E '/[zZ][+]*\s+\/var\/log\/journal\s*/d' "$TMPFILES_CONF"
 fi
+echo "Z /var/log/journal ~2750 root systemd-journal - -" >> "$TMPFILES_CONF"
 
-if grep -qP "^.[+]*\s+\/run\/log\/journal\s+" "$TMPFILES_CONF"; then
-    sed -i --follow-symlinks "s/\(^.[+]*\)\(\s\+\/run\/log\/journal\s\+[^ $]\+\s\+[^ $]\+\s\+\)\([^ $]\+\)/Z\2systemd-journal/" "$TMPFILES_CONF"
-else
-    echo "Z /run/log/journal ~2750 root systemd-journal - -" >> "$TMPFILES_CONF"
+if grep -qP "^[zZ][+]*\s+\/run\/log\/journal\s+" "$TMPFILES_CONF"; then
+    sed -i --follow-symlinks -E '/[zZ][+]*\s+\/run\/log\/journal\s*/d' "$TMPFILES_CONF"
 fi
+echo "Z /run/log/journal ~2750 root systemd-journal - -" >> "$TMPFILES_CONF"
 
 systemd-tmpfiles --create
