84a9e52b09b2c143cdc020aacc8880c6c7aa1632
wqian <alan.moore@canonical.com>
2024-07-27 10:59:03 +0100

    Avoid duplicated fix

--- linux_os/guide/system/logging/journald/dir_owner_system_journal/bash/ubuntu.sh
+++ linux_os/guide/system/logging/journald/dir_owner_system_journal/bash/ubuntu.sh
@@ -2,14 +2,18 @@
 
 TMPFILES_CONF="/usr/lib/tmpfiles.d/systemd.conf"
 
-if grep -qP "^[zZ][+]*\s+\/var\/log\/journal\s+" "$TMPFILES_CONF"; then
-    sed -i --follow-symlinks -E '/[zZ][+]*\s+\/var\/log\/journal\s*/d' "$TMPFILES_CONF"
+if ! grep -q 'Z /var/log/journal ~2750 root systemd-journal - -' "$TMPFILES_CONF"; then
+    if grep -qP "^[zZ][+]*\s+\/var\/log\/journal" "$TMPFILES_CONF"; then
+        sed -i --follow-symlinks "s/\(^[zZ][+]*\)\(\s\+\/var\/log\/journal.*\)/# \1\2/" "$TMPFILES_CONF"
+    fi
+    echo "Z /var/log/journal ~2750 root systemd-journal - -" >>"$TMPFILES_CONF"
 fi
-echo "Z /var/log/journal ~2750 root systemd-journal - -" >> "$TMPFILES_CONF"
 
-if grep -qP "^[zZ][+]*\s+\/run\/log\/journal\s+" "$TMPFILES_CONF"; then
-    sed -i --follow-symlinks -E '/[zZ][+]*\s+\/run\/log\/journal\s*/d' "$TMPFILES_CONF"
+if ! grep -q 'Z /run/log/journal ~2750 root systemd-journal - -' "$TMPFILES_CONF"; then
+    if grep -qP "^[zZ][+]*\s+\/run\/log\/journal" "$TMPFILES_CONF"; then
+        sed -i --follow-symlinks "s/\(^[zZ][+]*\)\(\s\+\/run\/log\/journal.*\)/# \1\2/" "$TMPFILES_CONF"
+    fi
+    echo "Z /run/log/journal ~2750 root systemd-journal - -" >>"$TMPFILES_CONF"
 fi
-echo "Z /run/log/journal ~2750 root systemd-journal - -" >> "$TMPFILES_CONF"
 
 systemd-tmpfiles --create
