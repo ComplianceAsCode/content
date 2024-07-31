40d7cdd994dd026040c64b1d940e29315a85b5ef
Matthew Burket <mburket@redhat.com>
2024-07-31 11:49:17 -0500

    Remove uos20
    Closes #12135

--- ssg/constants.py
+++ ssg/constants.py
@@ -60,7 +60,6 @@ product_directories = [
     'rhv4',
     'sle12', 'sle15', 'slmicro5',
     'ubuntu1604', 'ubuntu1804', 'ubuntu2004', 'ubuntu2204',
-    'uos20',
 ]
 
 JINJA_MACROS_DIRECTORY = os.path.abspath(os.path.join(os.path.dirname(os.path.dirname(
@@ -230,7 +229,6 @@ FULL_NAME_TO_PRODUCT_MAPPING = {
     "Ubuntu 18.04": "ubuntu1804",
     "Ubuntu 20.04": "ubuntu2004",
     "Ubuntu 22.04": "ubuntu2204",
-    "UnionTech OS Server 20": "uos20",
     "OpenEmbedded": "openembedded",
     "Not Applicable": "example",
 }
@@ -304,7 +302,6 @@ MULTI_PLATFORM_MAPPING = {
     "multi_platform_sle": ["sle12", "sle15"],
     "multi_platform_slmicro": ["slmicro5"],
     "multi_platform_ubuntu": ["ubuntu1604", "ubuntu1804", "ubuntu2004", "ubuntu2204"],
-    "multi_platform_uos": ["uos20"],
     "multi_platform_openembedded": ["openembedded"],
     "multi_platform_al": ["al2023"],
 }
