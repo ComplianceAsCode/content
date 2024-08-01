acf787b666de36249ab835e6e1b9420b7d6be0d7
Vojtech Polasek <vpolasek@redhat.com>
2024-08-01 10:06:33 +0200

    extend template.py of sshd_lineinfile
    implement notion of XCCDF variables
    also start using a new template parameter datatype.
    It is used to generate proper templated test scenarios, see the template.py of the sysctl template.

--- shared/templates/sshd_lineinfile/template.py
+++ shared/templates/sshd_lineinfile/template.py
@@ -2,6 +2,9 @@ from ssg.utils import parse_template_boolean_value
 
 
 def preprocess(data, lang):
+    if data["datatype"] not in ["string", "int"]:
+        errmsg = "The template instance of the rule {0} contains invalid datatype. It must be either 'string' or 'int'".format(data["_rule_id"])
+        raise ValueError(errmsg)
     data["missing_parameter_pass"] = parse_template_boolean_value(
         data, parameter="missing_parameter_pass", default_value=False)
 
@@ -11,4 +14,21 @@ def preprocess(data, lang):
         data["config_basename"] = "01-complianceascode-reinforce-os-defaults.conf"
     else:
         data["config_basename"] = "00-complianceascode-hardening.conf"
+
+    # set variables used in test scenarios
+    if data["datatype"] == "int":
+        if not data.get("value"):
+            data["wrong_value"] = 123456
+            data["correct_value"] = 0
+        else:
+            data["wrong_value"] = str(int(data["value"]) + 1)
+            data["correct_value"] = str(data["value"])
+    elif data["datatype"] == "string":
+        if not data.get("value"):
+            data["wrong_value"] = "wrong_value"
+            data["correct_value"] = "correct_value"
+        else:
+            data["wrong_value"] = "wrong_value"
+            data["correct_value"] = str(data["value"])
+
     return data
