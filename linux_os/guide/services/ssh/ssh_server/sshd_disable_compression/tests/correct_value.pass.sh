fafe78fa8680f0ff3a1db220aaf912931d55f29a
Vojtech Polasek <vpolasek@redhat.com>
2024-08-09 16:38:42 +0200

    convert sshd_disable_compression to templated rule

--- linux_os/guide/services/ssh/ssh_server/sshd_disable_compression/tests/correct_value.pass.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if grep -q "^Compression" /etc/ssh/sshd_config; then
-	sed -i "s/^Compression.*/Compression no/" /etc/ssh/sshd_config
-else
-	echo "Compression no" >> /etc/ssh/sshd_config
-fi
