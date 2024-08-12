a698789170c3fd58b90fab88239e5cee78cea57c
wqian <alan.moore@canonical.com>
2024-08-12 13:48:17 +0100

    Remove the example test file

--- linux_os/guide/system/accounts/accounts-restrictions/password_storage/no_empty_passwords/tests/example_no_nullok.pass.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# platform = Oracle Linux 7,Red Hat Virtualization 4,multi_platform_fedora,multi_platform_ubuntu
-
-{{% if 'ubuntu' in product %}}
-sed -i --follow-symlinks '/nullok/d' /etc/pam.d/common-password
-{{% else %}}
-sed -i --follow-symlinks '/nullok/d' /etc/pam.d/system-auth
-sed -i --follow-symlinks '/nullok/d' /etc/pam.d/password-auth
-{{% endif %}}
