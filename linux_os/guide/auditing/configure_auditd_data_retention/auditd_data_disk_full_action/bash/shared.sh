d57683617ea437624533ee5fefeb610e47057a29
svet-se <svetlin.boychev@suse.com>
2024-08-12 14:43:25 +0300

    Add rule auditd_data_disk_full_action to sle micro 5 stig profile

--- linux_os/guide/auditing/configure_auditd_data_retention/auditd_data_disk_full_action/bash/shared.sh
+++ linux_os/guide/auditing/configure_auditd_data_retention/auditd_data_disk_full_action/bash/shared.sh
@@ -1,4 +1,4 @@
-# platform = Red Hat Virtualization 4,multi_platform_ol,multi_platform_rhel,multi_platform_sle,multi_platform_ubuntu
+# platform = Red Hat Virtualization 4,multi_platform_ol,multi_platform_rhel,multi_platform_sle,multi_platform_slmicro,multi_platform_ubuntu
 
 {{{ bash_instantiate_variables("var_auditd_disk_full_action") }}}
 
