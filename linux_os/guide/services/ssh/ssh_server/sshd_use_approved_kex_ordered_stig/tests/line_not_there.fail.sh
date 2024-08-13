f908dbe2affc20ef7fe1979e40d0d5d8059c5ee0
svet-se <svetlin.boychev@suse.com>
2024-08-13 15:01:39 +0300

    Add rule sshd_use_approved_kex_ordered_stig to sle micro 5 stig profile

--- linux_os/guide/services/ssh/ssh_server/sshd_use_approved_kex_ordered_stig/tests/line_not_there.fail.sh
+++ linux_os/guide/services/ssh/ssh_server/sshd_use_approved_kex_ordered_stig/tests/line_not_there.fail.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# platform = multi_platform_ol,multi_platform_rhel,multi_platform_sle,multi_platform_ubuntu
+# platform = multi_platform_ol,multi_platform_rhel,multi_platform_sle,multi_platform_slmicro,multi_platform_ubuntu
 
 source common.sh
