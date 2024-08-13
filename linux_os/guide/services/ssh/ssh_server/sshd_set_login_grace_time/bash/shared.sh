7ccc03fa5e320f5af228a1e769df3a399b7dc0c3
Vojtech Polasek <vpolasek@redhat.com>
2024-08-13 11:31:02 +0200

    partially convert sshd_set_login_grace_timeout to templated rule
    The OVAL is kept for now with modification to support distributed sshd configuration.

--- linux_os/guide/services/ssh/ssh_server/sshd_set_login_grace_time/bash/shared.sh
+++
@@ -1,9 +0,0 @@
-# platform = multi_platform_all
-# reboot = false
-# strategy = configure
-# complexity = low
-# disruption = low
-
-{{{ bash_instantiate_variables("var_sshd_set_login_grace_time") }}}
-
-{{{ bash_sshd_config_set(parameter="LoginGraceTime", value="$var_sshd_set_login_grace_time") }}}
