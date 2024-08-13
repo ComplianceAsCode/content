24fbff33b22c57328d1cd354e97526a9e9035e26
Vojtech Polasek <vpolasek@redhat.com>
2024-08-13 17:34:53 +0200

    convert sshd_use_priv_separation to templated rule

--- linux_os/guide/services/ssh/ssh_server/sshd_use_priv_separation/bash/shared.sh
+++
@@ -1,9 +0,0 @@
-# platform = multi_platform_all
-# reboot = false
-# strategy = restrict
-# complexity = low
-# disruption = low
-
-{{{ bash_instantiate_variables("var_sshd_priv_separation") }}}
-
-{{{ bash_sshd_config_set(parameter="UsePrivilegeSeparation", value="$var_sshd_priv_separation") }}}
--- linux_os/guide/services/ssh/ssh_server/sshd_use_priv_separation/tests/correct_value.pass.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# variables = var_sshd_priv_separation=sandbox
-
-if grep -q "^UsePrivilegeSeparation" /etc/ssh/sshd_config; then
-	sed -i "s/^UsePrivilegeSeparation.*/UsePrivilegeSeparation sandbox/" /etc/ssh/sshd_config
-else
-	echo "UsePrivilegeSeparation sandbox" >> /etc/ssh/sshd_config
-fi
