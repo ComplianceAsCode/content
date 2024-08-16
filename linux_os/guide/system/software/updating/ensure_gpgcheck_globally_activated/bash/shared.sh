67ffe290f75f7484c56937b59ee6174fa507221d
svet-se <svetlin.boychev@suse.com>
2024-08-16 15:51:48 +0300

    Add rule ensure_gpgcheck_globally_activated to sle micro 5 stig profile

--- linux_os/guide/system/software/updating/ensure_gpgcheck_globally_activated/bash/shared.sh
+++ linux_os/guide/system/software/updating/ensure_gpgcheck_globally_activated/bash/shared.sh
@@ -1,3 +1,3 @@
-# platform = multi_platform_rhel,multi_platform_ol,multi_platform_fedora,multi_platform_rhv,multi_platform_sle
+# platform = multi_platform_rhel,multi_platform_ol,multi_platform_fedora,multi_platform_rhv,multi_platform_sle,multi_platform_slmicro
 
 {{{ bash_replace_or_append( pkg_manager_config_file , '^gpgcheck', '1') }}}
