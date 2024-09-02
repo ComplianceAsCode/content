7fe3562e3463d9d698737938735b003106518374
svet-se <svetlin.boychev@suse.com>
2024-09-02 16:33:08 +0300

    Add rule smartcard_pam_enabled to sle micro 5 stig profile

--- linux_os/guide/system/accounts/accounts-physical/screen_locking/smart_card_login/smartcard_pam_enabled/tests/commented.fail.sh
+++ linux_os/guide/system/accounts/accounts-physical/screen_locking/smart_card_login/smartcard_pam_enabled/tests/commented.fail.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# platform = multi_platform_ubuntu,multi_platform_sle
+# platform = multi_platform_sle,multi_platform_slmicro,multi_platform_ubuntu
 # packages = libpam-pkcs11
 {{% if 'ubuntu' in product %}}
 sed -i '/^auth.*pam_unix.so/i # auth [success=2 default=ignore] pam_pkcs11.so' /etc/pam.d/common-auth
--- linux_os/guide/system/accounts/accounts-physical/screen_locking/smart_card_login/smartcard_pam_enabled/tests/nothing.fail.sh
+++ linux_os/guide/system/accounts/accounts-physical/screen_locking/smart_card_login/smartcard_pam_enabled/tests/nothing.fail.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# platform = multi_platform_ubuntu,multi_platform_sle
+# platform = multi_platform_sle,multi_platform_slmicro,multi_platform_ubuntu
 # packages = libpam-pkcs11
 
 echo "auth	[success=1 default=ignore]	pam_unix.so nullok" > /etc/pam.d/common-auth
