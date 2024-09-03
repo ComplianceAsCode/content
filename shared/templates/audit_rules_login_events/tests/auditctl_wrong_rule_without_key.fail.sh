3c7c1c93a3010a194bfa56d040293e35a3217a9c
Vojtech Polasek <vpolasek@redhat.com>
2024-09-03 11:57:09 +0200

    modify template of audit_login_events

--- shared/templates/audit_rules_login_events/tests/auditctl_wrong_rule_without_key.fail.sh
+++ shared/templates/audit_rules_login_events/tests/auditctl_wrong_rule_without_key.fail.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # packages = audit
 
+{{{ setup_auditctl_environment() }}}
+
 path={{{ PATH }}}
 . $SHARED/audit_rules_login_events/auditctl_wrong_rule_without_key.fail.sh
--- tests/shared/audit_rules_login_events/auditctl_correct.pass.sh
+++ tests/shared/audit_rules_login_events/auditctl_correct.pass.sh
@@ -2,4 +2,3 @@
 # packages = audit
 
 echo "-w $path -p wa -k logins" >> /etc/audit/audit.rules
-sed -i "s%^ExecStartPost=.*%ExecStartPost=-/sbin/auditctl%" /usr/lib/systemd/system/auditd.service
