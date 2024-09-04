e3dd9be469f35bc1aa2b9d98463fd83001485214
Vojtech Polasek <vpolasek@redhat.com>
2024-09-04 11:21:09 +0200

    align test scenarios of audit_rules_session_events

--- linux_os/guide/auditing/auditd_configure_rules/audit_rules_session_events/tests/auditctl_rules_missing.fail.sh
+++ linux_os/guide/auditing/auditd_configure_rules/audit_rules_session_events/tests/auditctl_rules_missing.fail.sh
@@ -2,7 +2,7 @@
 # packages = audit
 
 # use auditctl
-sed -i "s%^ExecStartPost=.*%ExecStartPost=-/sbin/auditctl%" /usr/lib/systemd/system/auditd.service
+{{{ setup_auditctl_environment() }}}
 
 rm -f /etc/audit/rules.d/*
 > /etc/audit/audit.rules
