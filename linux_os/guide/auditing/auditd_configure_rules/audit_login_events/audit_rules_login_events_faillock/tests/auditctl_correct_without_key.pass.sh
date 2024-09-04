7d20a05664f786f4aea03bb95332ddf61b9d50c0
Vojtech Polasek <vpolasek@redhat.com>
2024-09-04 09:51:09 +0200

    align rule audit_rules_login_events_faillock

--- linux_os/guide/auditing/auditd_configure_rules/audit_login_events/audit_rules_login_events_faillock/tests/auditctl_correct_without_key.pass.sh
+++ linux_os/guide/auditing/auditd_configure_rules/audit_login_events/audit_rules_login_events_faillock/tests/auditctl_correct_without_key.pass.sh
@@ -2,5 +2,7 @@
 # packages = audit
 # platform = multi_platform_all
 
+{{{ setup_auditctl_environment() }}}
+
 path="/var/log/faillock"
 . $SHARED/audit_rules_login_events/auditctl_correct_without_key.pass.sh
--- linux_os/guide/auditing/auditd_configure_rules/audit_login_events/audit_rules_login_events_faillock/tests/auditctl_remove_all_rules.fail.sh
+++ linux_os/guide/auditing/auditd_configure_rules/audit_login_events/audit_rules_login_events_faillock/tests/auditctl_remove_all_rules.fail.sh
@@ -2,4 +2,6 @@
 # packages = audit
 # platform = multi_platform_all
 
+{{{ setup_auditctl_environment() }}}
+
 . $SHARED/audit_rules_login_events/auditctl_remove_all_rules.fail.sh
