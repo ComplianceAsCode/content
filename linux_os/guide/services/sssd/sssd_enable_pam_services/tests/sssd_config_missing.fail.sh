6e3a497286a7a0336bf6a40bc892e1da89853772
Marcus Burghardt <maburgha@redhat.com>
2024-09-16 16:31:32 +0200

    Use sssd_conf_files_present applicability in sssd_enable_pam_services
    This rule is only applicable when SSSD is already in use and
    consequently configured.
    
    Signed-off-by: Marcus Burghardt <maburgha@redhat.com>

--- linux_os/guide/services/sssd/sssd_enable_pam_services/tests/sssd_config_missing.fail.sh
+++ linux_os/guide/services/sssd/sssd_enable_pam_services/tests/sssd_config_missing.fail.sh
@@ -2,6 +2,9 @@
 # packages = sssd-common
 # remediation = none
 
+# SSSD configuration files are expected to be created manually since the configuration can
+# be different for each site. Therefore, if there is no configuration files previously created
+# in the system, this rule will report "not applicable".
 SSSD_CONF_FILE="/etc/sssd/sssd.conf"
 SSSD_CONF_DIR_FILES="/etc/sssd/conf.d/*.conf"
 
