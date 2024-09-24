b8d237d62c4edb5eee15e3b1f072d5add0529e09
Matthew Burket <mburket@redhat.com>
2024-09-24 14:23:30 -0500

    Add new rule chrony_set_nts

---
+++ linux_os/guide/services/ntp/chrony_set_nts/tests/chrony_one_pool_missing.fail.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# packages = chrony
+
+{{{ bash_package_remove("ntp") }}}
+
+# Remove all server or pool options
+sed -i "/^\(server\|pool\).*/d" {{{ chrony_conf_path }}}
+
+echo "pool pool.ntp.org iburst" >> {{{ chrony_conf_path }}}
+
+systemctl enable chronyd.service
+
