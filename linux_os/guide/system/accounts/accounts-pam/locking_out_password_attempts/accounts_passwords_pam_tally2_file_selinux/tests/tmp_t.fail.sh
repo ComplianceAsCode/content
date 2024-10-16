762e132991b6bc5d1ad49c08f59744efdf8cf2ee
Matthew Burket <mburket@redhat.com>
2024-10-16 14:15:15 -0500

    Fix invalid platforms in tests

--- linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_passwords_pam_tally2_file_selinux/tests/tmp_t.fail.sh
+++ linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_passwords_pam_tally2_file_selinux/tests/tmp_t.fail.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # packages = policycoreutils-python-utils
-# platform = multi_platform_slmicro5
+# platform = multi_platform_slmicro
 
 semanage fcontext -m -t tmp_t "/var/log/tallylog"
 restorecon -R -v "/var/log/tallylog"
