1b8df2c4be8bf74516eb71deba1127296e4a0d9e
Matthew Burket <mburket@redhat.com>
2024-10-18 15:14:23 -0500

    Add tests for drop ins to require_singleuser_auth and require_emergency_target_auth

---
+++ linux_os/guide/system/accounts/accounts-physical/require_emergency_target_auth/tests/correct_value_dropin.pass.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# platform = multi_platform_fedora,multi_platform_rhel,multi_platform_sle
+rm -f /etc/systemd/system/emergency.service
+mkdir -p /etc/systemd/system/emergency.service.d/
+cat << EOF > /etc/systemd/system/emergency.service.d/10-automatus.conf
+[Service]
+ExecStart=-/usr/lib/systemd/systemd-sulogin-shell emergency
+EOF
