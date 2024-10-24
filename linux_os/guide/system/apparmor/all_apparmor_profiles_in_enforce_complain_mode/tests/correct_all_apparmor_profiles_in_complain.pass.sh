7f33cf6931cbbde8a195a2513def62d662978cbf
Miha Purg <miha.purg@canonical.com>
2024-10-24 13:31:26 +0200

    Fix apparmor tests
    Tests for ensure_apparmor_enforce were failing because
    aa-complain and aa-enforce enabled the rsyslogd profile,
    which is disabled by default, thus marking it as an "unconfined
    process with a defined profile" and failing the SCE.
    
    Solution is to restart the service when loading the profiles.
    
    Tests for ensure_apparmor_enforce_or_complain were adapted
    to not load disabled profiles.

--- linux_os/guide/system/apparmor/all_apparmor_profiles_in_enforce_complain_mode/tests/correct_all_apparmor_profiles_in_complain.pass.sh
+++ linux_os/guide/system/apparmor/all_apparmor_profiles_in_enforce_complain_mode/tests/correct_all_apparmor_profiles_in_complain.pass.sh
@@ -3,5 +3,11 @@
 
 #Replace apparmor definitions
 apparmor_parser -q -r /etc/apparmor.d/
+{{% if 'ubuntu' in product %}}
+# Set all profiles to complain mode except disabled profiles
+find /etc/apparmor.d -maxdepth 1 ! -type d -exec bash -c '[[ -e "/etc/apparmor.d/disable/$(basename "{}")" ]] || aa-complain "{}"' \;
+{{% else %}}
 #Set all profiles in complain mode
 aa-complain /etc/apparmor.d/*
+{{% endif %}}
+
