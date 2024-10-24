34c3ecbc7a2d6b0ff0c514935b1699e237c513ff
Miha Purg <miha.purg@canonical.com>
2024-10-24 13:30:40 +0200

    Fix apparmor_complain rule to not load disabled profiles
    This is to avoid enforcing the rsyslogd profile which
    is disabled by default on Jammy.
    
    Issue introduced with the recent change of the default value
    var_apparmor_mode from complain to enforce.

--- linux_os/guide/system/apparmor/all_apparmor_profiles_in_enforce_complain_mode/bash/shared.sh
+++ linux_os/guide/system/apparmor/all_apparmor_profiles_in_enforce_complain_mode/bash/shared.sh
@@ -13,8 +13,13 @@ APPARMOR_MODE="$var_apparmor_mode"
 
 if [ "$APPARMOR_MODE" = "enforce" ]
 then
+  {{% if 'ubuntu' in product %}}
+  # Set all profiles to enforce mode except disabled profiles
+  find /etc/apparmor.d -maxdepth 1 ! -type d -exec bash -c '[[ -e "/etc/apparmor.d/disable/$(basename "{}")" ]] || aa-enforce "{}"' \;
+  {{% else %}}
   # Set all profiles to enforce mode
   aa-enforce /etc/apparmor.d/*
+  {{% endif %}}
 fi
 
 if [ "$APPARMOR_MODE" = "complain" ]
