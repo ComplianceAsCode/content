557c2197f476d367df4dbca8b6c7fd9509c63ef2
Alexandre Skrzyniarz <alexandre.skrzyniarz@laposte.net>
2024-10-24 11:46:24 +0200

    Fix file path inconsistencies in accounts_umask_etc_bashrc
    fixes #12409

--- linux_os/guide/system/accounts/accounts-session/user_umask/accounts_umask_etc_bashrc/tests/ospp_cis_correct.pass.sh
+++ linux_os/guide/system/accounts/accounts-session/user_umask/accounts_umask_etc_bashrc/tests/ospp_cis_correct.pass.sh
@@ -2,7 +2,7 @@
 # profiles = xccdf_org.ssgproject.content_profile_cis, xccdf_org.ssgproject.content_profile_ospp
 # packages = bash
 
-{{% if 'sle' in product or 'ubuntu' in product %}}
+{{% if 'sle' in product or 'ubuntu' in product or 'debian' in product %}}
 etc_bash_rc="/etc/bash.bashrc"
 {{% else %}}
 etc_bash_rc="/etc/bashrc"
--- linux_os/guide/system/accounts/accounts-session/user_umask/accounts_umask_etc_bashrc/tests/wrong_and_mangled_and_not_at_the_begining_of_line.fail.sh
+++ linux_os/guide/system/accounts/accounts-session/user_umask/accounts_umask_etc_bashrc/tests/wrong_and_mangled_and_not_at_the_begining_of_line.fail.sh
@@ -3,6 +3,12 @@
 
 # This TS is a regression test for https://github.com/ComplianceAsCode/content/issues/11937
 
+{{% if 'sle' in product or 'ubuntu' in product or 'debian' in product %}}
+etc_bash_rc="/etc/bash.bashrc"
+{{% else %}}
+etc_bash_rc="/etc/bashrc"
+{{% endif %}}
+
 sed -i '/umask/d' /etc/bashrc
-echo "    [ \`umask\` -eq 0 ] && umask 027022" >> /etc/bashrc
+echo "    [ \`umask\` -eq 0 ] && umask 027022" >> $etc_bash_rc
 umask 000
