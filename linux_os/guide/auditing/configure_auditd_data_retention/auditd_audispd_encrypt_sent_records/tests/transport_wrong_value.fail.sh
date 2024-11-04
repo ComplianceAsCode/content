f6f740030b648e9cad17db8147ec2fab1ebe9ed8
svet-se <svetlin.boychev@suse.com>
2024-11-04 13:57:40 +0200

    Fix tests

--- linux_os/guide/auditing/configure_auditd_data_retention/auditd_audispd_encrypt_sent_records/tests/transport_wrong_value.fail.sh
+++ linux_os/guide/auditing/configure_auditd_data_retention/auditd_audispd_encrypt_sent_records/tests/transport_wrong_value.fail.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # packages = audit
-# platform = Red Hat Enterprise Linux 8,multi_platform_fedora,SUSE Linux Enterprise 15
+# platform = Red Hat Enterprise Linux 8,multi_platform_fedora
 
 . $SHARED/auditd_utils.sh
 prepare_auditd_test_enviroment
