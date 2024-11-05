e079930f574cec7ed37c23ab014727fc637739b4
Matus Marhefka <mmarhefk@redhat.com>
2024-11-05 14:20:31 +0100

    Update networkmanager_dns_mode for bootable containers
    Remediation of the rule `networkmanager_dns_mode` calls
    `systemctl reload` which doesn't work in the bootable
    container build environment and so we use the
    `bash_not_bootc_build` macro to not run it there.

--- linux_os/guide/system/network/networkmanager/networkmanager_dns_mode/bash/shared.sh
+++ linux_os/guide/system/network/networkmanager/networkmanager_dns_mode/bash/shared.sh
@@ -8,4 +8,6 @@
 
 {{{ bash_ini_file_set("/etc/NetworkManager/NetworkManager.conf", "main", "dns", "$var_networkmanager_dns_mode") }}}
 
-systemctl reload NetworkManager
+if {{{ bash_not_bootc_build() }}} ; then
+    systemctl reload NetworkManager
+fi
