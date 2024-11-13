8d30f27c15dfb07f49a4d084339b5371283e59bb
Marcus Burghardt <maburgha@redhat.com>
2024-11-13 09:21:45 +0100

    Review and create docstrings for ssg/xccdf.py
    Signed-off-by: Marcus Burghardt <maburgha@redhat.com>

--- ssg/xccdf.py
+++ ssg/xccdf.py
@@ -19,6 +19,16 @@ PROFILE_ID_PREFIX = ("^xccdf_org.*content_profile_")
 
 
 def get_benchmark_id_title_map(input_tree):
+    """
+    Extracts a mapping of benchmark IDs to their titles from an XML tree.
+
+    Args:
+        input_tree (xml.etree.ElementTree.ElementTree): The XML tree containing benchmark data.
+
+    Returns:
+        dict: A dictionary where the keys are benchmark IDs (str) and the values are benchmark
+              titles (str).
+    """
     input_root = input_tree.getroot()
     ret = {}
     candidates = []
@@ -40,9 +50,17 @@ def get_benchmark_id_title_map(input_tree):
 
 
 def get_profile_choices_for_input(input_tree, benchmark_id, tailoring_tree):
-    """Returns a dictionary that maps profile_ids to their respective titles.
     """
+    Returns a dictionary that maps profile_ids to their respective titles.
 
+    Args:
+        input_tree (ElementTree): The XML tree containing the benchmark profiles.
+        benchmark_id (str): The ID of the benchmark to filter profiles.
+        tailoring_tree (ElementTree, optional): An optional XML tree containing tailored profiles.
+
+    Returns:
+        dict: A dictionary where keys are profile IDs and values are profile titles.
+    """
     # Ideally oscap would have a command line to do this, but as of now it
     # doesn't so we have to implement it ourselves. Importing openscap Python
     # bindings is nasty and overkill for this.
@@ -86,9 +104,16 @@ def get_profile_choices_for_input(input_tree, benchmark_id, tailoring_tree):
 
 
 def get_profile_short_id(long_id):
-    """If given profile ID is the XCCDF 1.2 long ID this function shortens it
     """
+    Shortens the given profile ID if it matches the XCCDF 1.2 long ID format.
 
+    Args:
+        long_id (str): The long profile ID to be shortened.
+
+    Returns:
+        str: The shortened profile ID if the long ID matches the XCCDF 1.2 format, otherwise
+             returns the original long ID.
+    """
     if re.search(PROFILE_ID_PREFIX, long_id):
         return long_id[re.search(PROFILE_ID_PREFIX, long_id).end():]
 
@@ -97,7 +122,20 @@ def get_profile_short_id(long_id):
 
 def scrape_benchmarks(root, namespace, dest):
     """
-    Add all benchmark elements in root to dest list
+    Add all benchmark elements in root to dest list.
+
+    This function searches for all elements with the tag 'Benchmark' within the given XML root
+    element, using the specified namespace. It then adds these elements to the destination list
+    'dest' along with their namespace. If the root element itself is a 'Benchmark', it is also
+    added to the list.
+
+    Args:
+        root (xml.etree.ElementTree.Element): The root XML element to search within.
+        namespace (str): The XML namespace to use when searching for 'Benchmark' elements.
+        dest (list): The list to which found 'Benchmark' elements and their namespace will be added.
+
+    Returns:
+        None
     """
     dest.extend([
         (namespace, elem)
