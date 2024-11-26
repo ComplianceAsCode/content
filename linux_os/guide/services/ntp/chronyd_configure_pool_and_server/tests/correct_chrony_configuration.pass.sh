de97dbbc3b9cbac74e3f7cc77b4d43f81e48cebb
Miha Purg <miha.purg@canonical.com>
2024-11-26 15:40:13 +0100

    Fix tests for chronyd_configure_pool_and_server
    - define variables var_multiple_time_servers and var_multiple_time_pools
      so that the tests work when the variables are not defined
      by the profile (e.g. when profile is 'all')

--- linux_os/guide/services/ntp/chronyd_configure_pool_and_server/tests/correct_chrony_configuration.pass.sh
+++ linux_os/guide/services/ntp/chronyd_configure_pool_and_server/tests/correct_chrony_configuration.pass.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # packages = chrony
+# variables = var_multiple_time_servers=0.suse.pool.ntp.org,1.suse.pool.ntp.org,2.suse.pool.ntp.org,3.suse.pool.ntp.org
+# variables = var_multiple_time_pools=0.suse.pool.ntp.org,1.suse.pool.ntp.org,2.suse.pool.ntp.org,3.suse.pool.ntp.org
 
-
-{{{ bash_instantiate_variables("var_multiple_time_servers") }}}
-{{{ bash_instantiate_variables("var_multiple_time_pools") }}}
+var_multiple_time_servers=0.suse.pool.ntp.org,1.suse.pool.ntp.org,2.suse.pool.ntp.org,3.suse.pool.ntp.org
+var_multiple_time_pools=0.suse.pool.ntp.org,1.suse.pool.ntp.org,2.suse.pool.ntp.org,3.suse.pool.ntp.org
 
 config_file="{{{ chrony_conf_path }}}"
 
