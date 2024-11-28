629da10684b62f7385837d55e3f6318a50be9867
teacup-on-rockingchair <315160+teacup-on-rockingchair@users.noreply.github.com>
2024-11-28 14:14:28 +0200

    Add changes to support CIS for slmicro5
    Added references and identifiers, where needed enabled slmicro5 platform or configured appropriate template variables

--- linux_os/guide/system/software/updating/ensure_gpgcheck_never_disabled/bash/shared.sh
+++ linux_os/guide/system/software/updating/ensure_gpgcheck_never_disabled/bash/shared.sh
@@ -1,5 +1,5 @@
-# platform = multi_platform_rhel,multi_platform_ol,multi_platform_fedora,multi_platform_rhv,multi_platform_sle
-{{% if product in ["sle12", "sle15"] %}}
+# platform = multi_platform_fedora,multi_platform_ol,multi_platform_rhel,multi_platform_rhv,multi_platform_sle,multi_platform_slmicro
+{{% if product in ["sle12", "sle15", "slmicro5"] %}}
 sed -i 's/gpgcheck\s*=.*/gpgcheck=1/g' /etc/zypp/repos.d/*
 {{% else %}}
 sed -i 's/gpgcheck\s*=.*/gpgcheck=1/g' /etc/yum.repos.d/*
