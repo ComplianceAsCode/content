54193761711d73b981a7e10455c9670e1e399c44
Eric Berry <eric.berry@canonical.com>
2024-12-03 08:20:35 -0800

    Testing additional tests

---
+++ linux_os/guide/system/accounts/accounts-pam/set_password_hashing_algorithm/set_password_hashing_algorithm_logindefs/tests/default_multivalue.pass.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# variables = var_password_hashing_algorithm=good_value1|good_value2
+
+if grep -q "^ENCRYPT_METHOD" /etc/login.defs; then
+	sed -i "s/^ENCRYPT_METHOD\b.*/ENCRYPT_METHOD good_value2/" /etc/login.defs
+else
+	echo "ENCRYPT_METHOD good_value2" >> /etc/login.defs
+fi
