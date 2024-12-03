f0b5fcde02e169f0de4c38fad57524093afd644f
Miha Purg <miha.purg@canonical.com>
2024-12-03 22:52:27 +0100

    Fix sce for ip6tables_rules_for_open_ports
    Fixed sce to also consider the protocol of the open port (tcp/udp).
    Before, the sce incorrectly passed when an ip6tables rule was defined
    for the open port but with the wrong protocol.
    
    The sce was also improved to match ip6tables rules where ports
    are defined with --multiport.

--- linux_os/guide/system/network/network-iptables/iptables_ruleset_modifications/ip6tables_rules_for_open_ports/sce/shared.sh
+++ linux_os/guide/system/network/network-iptables/iptables_ruleset_modifications/ip6tables_rules_for_open_ports/sce/shared.sh
@@ -3,14 +3,14 @@
 # check-import = stdout
 
 result=$XCCDF_RESULT_PASS
-iptables_status="$(ip6tables -L INPUT -v -n)"
 
-while read -r lpn;
+iptables_status="$(ip6tables -S INPUT -v)"
+while read -r proto port;
 do
-        if ! grep -Pq "dpt:$lpn" <<< "$iptables_status"; then
-                result=$XCCDF_RESULT_FAIL
-                break
-        fi
-    done < <(ss -6tulnH | awk '($5!~/::1/) {n=split($5, a, ":"); print a[n]}' | sort -u)
+    if ! grep -Piq " \-p $proto .* \-\-dport(s)? [0-9,]*\b$port\b" <<< "$iptables_status"; then
+        result=$XCCDF_RESULT_FAIL
+        break
+    fi
+done < <(ss -6tulnH | awk '($5!~/::1/) {n=split($5, a, ":"); print $1, a[n]}' | sort -u)
 
-exit "$result"
+exit $result
