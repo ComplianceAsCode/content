7082868cbab31b62e4387330f19925c843f8281f
Miha Purg <miha.purg@canonical.com>
2024-12-06 10:21:35 +0100

    Fix sshd_set_maxstartups tests

--- linux_os/guide/services/ssh/ssh_server/sshd_set_maxstartups/tests/stricter_value.pass.sh
+++ linux_os/guide/services/ssh/ssh_server/sshd_set_maxstartups/tests/stricter_value.pass.sh
@@ -2,7 +2,7 @@
 # variables = var_sshd_set_maxstartups=10:30:60
 
 if grep -q "^MaxStartups" /etc/ssh/sshd_config; then
-	sed -i "s/^MaxStartups.*/MaxStartups 10:30:60/" /etc/ssh/sshd_config
+	sed -i "s/^MaxStartups.*/MaxStartups 5:60:30/" /etc/ssh/sshd_config
 else
 	echo "MaxStartups 5:60:30" >> /etc/ssh/sshd_config
 fi
