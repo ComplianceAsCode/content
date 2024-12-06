97c3e7c058beb69030a7dfbcca4f02e017ff5eb1
Miha Purg <miha.purg@canonical.com>
2024-12-06 11:53:39 +0100

    Fix tests for template pam_account_password_faillock
    - tests were generalized and are no longer specific to `_deny` rule
    - tests check the different logic flows defined by template parameters
      and external variables
    - a new macro was created to initialize the variables
    - tests from the rules that use the template were removed

--- linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_passwords_pam_faillock_unlock_time/tests/pam_faillock_stricter_pam_files.pass.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# packages = authconfig
-# platform = Oracle Linux 7,multi_platform_fedora
-# variables = var_accounts_passwords_pam_faillock_unlock_time=600
-
-authconfig --enablefaillock --faillockargs="unlock_time=900" --update
