73b8561617f4b523bfc7538a7016b94466e3c899
Alan Moore <alan.moore@canonical.com>
2024-12-06 15:49:08 +0000

    Remove duplicated configuration

--- linux_os/guide/system/accounts/accounts-pam/password_quality/password_quality_pwquality/accounts_password_pam_retry/bash/ubuntu.sh
+++ linux_os/guide/system/accounts/accounts-pam/password_quality/password_quality_pwquality/accounts_password_pam_retry/bash/ubuntu.sh
@@ -3,8 +3,4 @@
 {{{ bash_instantiate_variables("var_password_pam_retry") }}}
 
 {{{ bash_pam_pwquality_enable() }}}
-PAM_FILE_PATH=/usr/share/pam-configs/cac_pwquality
-if grep -qE 'pam_pwquality\.so.*retry=[^[:space:]]' "$PAM_FILE_PATH"; then
-    sed -i -E '/pam_pwquality\.so/ s/\bretry=[^[:space:]]*\b ?//' "$PAM_FILE_PATH"
-fi
 {{{ bash_pam_pwquality_parameter_value('retry', "$var_password_pam_retry") }}}
