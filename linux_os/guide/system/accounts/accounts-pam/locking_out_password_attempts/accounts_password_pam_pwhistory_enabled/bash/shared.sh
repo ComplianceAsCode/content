dedc5a7a608b6802a6cb7cb08c163584841f63ec
Alan Moore <alan.moore@canonical.com>
2024-12-17 15:35:09 +0000

    Implement bash_pam_pwhistory_enable macro

--- linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_password_pam_pwhistory_enabled/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_password_pam_pwhistory_enabled/bash/shared.sh
@@ -1,22 +1,3 @@
 # platform = multi_platform_ubuntu
 
-conf_name=cac_pwhistory
-conf_path="/usr/share/pam-configs"
-
-if [ ! -f "$conf_path"/"$conf_name" ]; then
-    if [ -f "$conf_path"/pwhistory ]; then
-        cp "$conf_path"/pwhistory "$conf_path"/"$conf_name"
-        sed -i '/Default: yes/a Priority: 1025\
-Conflicts: pwhistory' "$conf_path"/"$conf_name"
-    else
-        cat << EOF > "$conf_path"/"$conf_name"
-Name: pwhistory password history checking
-Default: yes
-Priority: 1024
-Password-Type: Primary
-Password: requisite pam_pwhistory.so remember=24 enforce_for_root try_first_pass use_authtok
-EOF
-    fi
-fi
-
-DEBIAN_FRONTEND=noninteractive pam-auth-update
+{{{ bash_pam_pwhistory_enable() }}}
