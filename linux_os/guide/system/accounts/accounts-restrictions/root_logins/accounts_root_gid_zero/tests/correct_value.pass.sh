0317615da3eace45af227d612597201d0dd56f1a
Alan Moore <alan.moore@canonical.com>
2025-01-06 12:56:17 +0000

    Improve accounts_root_gid_zero test

--- linux_os/guide/system/accounts/accounts-restrictions/root_logins/accounts_root_gid_zero/tests/correct_value.pass.sh
+++ linux_os/guide/system/accounts/accounts-restrictions/root_logins/accounts_root_gid_zero/tests/correct_value.pass.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 # remediation = none
+
+{{% if 'ubuntu' in product %}}
+users_to_remove=$(awk -F: '$4 == 0 && $1 !~ /^(root|sync|shutdown|halt|operator)$/ {print $1}' /etc/passwd)
+for user in $users_to_remove; do
+    sudo userdel -rf "$user"
+done
+{{% endif %}}
