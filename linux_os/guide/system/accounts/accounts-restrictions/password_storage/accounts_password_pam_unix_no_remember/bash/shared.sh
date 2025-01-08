38a4cebde095c964a37cd85b10fdb7377c4026e4
Alan Moore <alan.moore@canonical.com>
2025-01-08 11:37:58 +0000

    Regex improvement in remediation

--- linux_os/guide/system/accounts/accounts-restrictions/password_storage/accounts_password_pam_unix_no_remember/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-restrictions/password_storage/accounts_password_pam_unix_no_remember/bash/shared.sh
@@ -6,22 +6,10 @@
 
 {{{ bash_pam_unix_enable() }}}
 config_file="/usr/share/pam-configs/cac_unix"
-sed -i -E '/^Password:/,/^[^[:space:]]/ {
+sed -i -E '/^Password(-Initial)?:/,/^[^[:space:]]/ {
     /pam_unix\.so/ {
         s/\s*\bremember=\d+\b//g
     }
 }' "$config_file"
-
-sed -i -E '/^Password-Initial:/,/^[^[:space:]]/ {
-    /pam_unix\.so/ {
-        s/\s*\bremember=\d+\b//g
-    }
-}' "$config_file"
-
-{{{ bash_remove_pam_module_option('/etc/pam.d/common-auth', 'auth', '', 'pam_unix.so', 'remember') }}}
-{{{ bash_remove_pam_module_option('/etc/pam.d/common-account', 'account', '', 'pam_unix.so', 'remember') }}}
-{{{ bash_remove_pam_module_option('/etc/pam.d/common-password', 'password', '', 'pam_unix.so', 'remember') }}}
-{{{ bash_remove_pam_module_option('/etc/pam.d/common-session', 'session', '', 'pam_unix.so', 'remember') }}}
-{{{ bash_remove_pam_module_option('/etc/pam.d/common-session-noninteractive', 'session', '', 'pam_unix.so', 'remember') }}}
 
 DEBIAN_FRONTEND=noninteractive pam-auth-update
