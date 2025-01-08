42f838c90f11c352baa674f25ed65e3f5371a450
Alan Moore <alan.moore@canonical.com>
2025-01-08 14:47:51 +0000

    use xargs in correct_value.pass.sh

--- linux_os/guide/system/accounts/accounts-restrictions/root_logins/accounts_root_gid_zero/tests/correct_value.pass.sh
+++ linux_os/guide/system/accounts/accounts-restrictions/root_logins/accounts_root_gid_zero/tests/correct_value.pass.sh
@@ -2,8 +2,5 @@
 # remediation = none
 
 {{% if 'ubuntu' in product %}}
-users_to_remove=$(awk -F: '$4 == 0 && $1 !~ /^(root|sync|shutdown|halt|operator)$/ {print $1}' /etc/passwd)
-for user in $users_to_remove; do
-    sudo userdel -rf "$user"
-done
+awk -F: '$4 == 0 && $1 !~ /^(root|sync|shutdown|halt|operator)$/ {print $1}' /etc/passwd | xargs --no-run-if-empty -I '{}' userdel -f '{}'
 {{% endif %}}
