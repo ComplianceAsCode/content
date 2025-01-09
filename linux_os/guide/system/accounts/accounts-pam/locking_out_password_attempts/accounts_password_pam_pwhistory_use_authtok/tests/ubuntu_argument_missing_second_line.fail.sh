e962ffe1b5da98a8d99d9c32d0c3d3b864a4f397
Alan Moore <alan.moore@canonical.com>
2025-01-09 17:31:35 +0000

    Implement rule 5.3.3.3.3 Ensure pam_pwhistory includes use_authtok

---
+++ linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_password_pam_pwhistory_use_authtok/tests/ubuntu_argument_missing_second_line.fail.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# platform = multi_platform_ubuntu
+# packages = pam
+
+config_file=/usr/share/pam-configs/tmp_pwhistory
+
+cat << EOF > "$config_file"
+Name: pwhistory password history checking
+Default: yes
+Priority: 1024
+Password-Type: Primary
+Password: 
+    requisite pam_pwhistory.so remember=24 enforce_for_root try_first_pass # use_authtok
+    [success=end default=ignore] pam_unix.so obscure use_authtok try_first_pass yescrypt
+    requisite pam_pwhistory.so remember=24 enforce_for_root try_first_pass
+EOF
+
+DEBIAN_FRONTEND=noninteractive pam-auth-update --enable tmp_pwhistory
+rm "$config_file"
