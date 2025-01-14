d153cfda59156f18fdff89a3f3ac2f01fe95767e
Matus Marhefka <mmarhefk@redhat.com>
2025-01-14 13:53:06 +0100

    Update enable_fips_mode for bootable containers
    This commit extends rule `enable_fips_mode` for bootable containers.
    
    To enable FIPS mode at bootable container build time we need to
    configure `fips=1` kernel argument through a configuration file in
    `/usr/lib/bootc/kargs.d/` directory and set the cryptographic policy
    to `FIPS`.
    
    The OVAL check of the rule has also been updated to distinguish between
    bootable container build environment and other environments.
    
    For more details see:
    https://docs.redhat.com/en/documentation/red_hat_enterprise_linux/10-beta/html/using_image_mode_for_rhel_to_build_deploy_and_manage_operating_systems/enabling-the-fips-mode-while-building-a-bootc-image#enabling-the-fips-mode-while-building-a-bootc-image

--- linux_os/guide/system/software/integrity/fips/enable_fips_mode/bash/shared.sh
+++ linux_os/guide/system/software/integrity/fips/enable_fips_mode/bash/shared.sh
@@ -1,9 +1,16 @@
 # platform = multi_platform_all
 {{{ bash_instantiate_variables("var_system_crypto_policy") }}}
 
-fips-mode-setup --enable
+if {{{ bash_bootc_build() }}}; then
+	crypto_policies_no_reload="--no-reload"
+	cat > /usr/lib/bootc/kargs.d/01-fips.toml << EOF
+kargs = ["fips=1"]
+EOF
+else
+	fips-mode-setup --enable
+fi
 
-stderr_of_call=$(update-crypto-policies --set ${var_system_crypto_policy} 2>&1 > /dev/null)
+stderr_of_call=$(update-crypto-policies $crypto_policies_no_reload --set ${var_system_crypto_policy} 2>&1 > /dev/null)
 rc=$?
 
 if test "$rc" = 127; then
