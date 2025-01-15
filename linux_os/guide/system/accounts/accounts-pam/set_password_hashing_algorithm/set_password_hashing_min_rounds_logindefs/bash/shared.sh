ec138ac1a79cf9566e521ac0e0fe5af80fc4635e
Vojtech Polasek <vpolasek@redhat.com>
2025-01-15 15:22:46 +0100

    change bash remediation to use the variable

--- linux_os/guide/system/accounts/accounts-pam/set_password_hashing_algorithm/set_password_hashing_min_rounds_logindefs/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-pam/set_password_hashing_algorithm/set_password_hashing_min_rounds_logindefs/bash/shared.sh
@@ -1,7 +1,9 @@
 # platform = multi_platform_all
 
+{{{ bash_instantiate_variables("var_password_hashing_min_rounds_login_defs") }}}
+
 {{{ set_config_file(path="/etc/login.defs",
                     parameter="SHA_CRYPT_MIN_ROUNDS",
-                    value="5000",
+                    value="$var_password_hashing_min_rounds_login_defs",
                     separator=" ",
                     separator_regex="\s*") }}}
