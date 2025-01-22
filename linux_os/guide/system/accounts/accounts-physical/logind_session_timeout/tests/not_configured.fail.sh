d8e527a86adfacfb7f35194a47fee9fe495f71bb
Matthew Burket <mburket@redhat.com>
2025-01-22 15:28:25 -0600

    Update logind_session_timeout/not_configured.fail to handle if the systemd config is not there

--- linux_os/guide/system/accounts/accounts-physical/logind_session_timeout/tests/not_configured.fail.sh
+++ linux_os/guide/system/accounts/accounts-physical/logind_session_timeout/tests/not_configured.fail.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 # variables = var_logind_session_timeout = 5_minutes
 
+mkdir -p /etc/systemd
+touch /etc/systemd/logind.conf
+
 sed -i '/^.*StopIdleSessionSec.*$/d' /etc/systemd/logind.conf
