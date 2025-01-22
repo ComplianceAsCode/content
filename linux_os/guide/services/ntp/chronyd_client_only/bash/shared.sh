7e24592ce0657006c59a4ec14ad35a5e44ef86f1
Simon John <sjohn@tuxcare.com>
2025-01-22 21:05:25 +0000

    Fixed some merge conflicts when rebasing to master again

--- linux_os/guide/services/ntp/chronyd_client_only/bash/shared.sh
+++ linux_os/guide/services/ntp/chronyd_client_only/bash/shared.sh
@@ -1,4 +1,4 @@
-# platform = multi_platform_rhel,multi_platform_fedora,multi_platform_ol
+# platform = multi_platform_rhel,multi_platform_fedora,multi_platform_ol,multi_platform_almalinux
 
 
 {{{ bash_replace_or_append(chrony_conf_path, '^port', '0', '%s %s') }}}
--- linux_os/guide/services/ssh/ssh_server/sshd_use_strong_ciphers/tests/no_ciphers.fail.sh
+++ linux_os/guide/services/ssh/ssh_server/sshd_use_strong_ciphers/tests/no_ciphers.fail.sh
@@ -1,3 +1,3 @@
-# platform = multi_platform_fedora,multi_platform_ol,multi_platform_rhel
+# platform = multi_platform_fedora,multi_platform_ol,multi_platform_rhel,multi_platform_almalinux
 
 sed -i 's/^\s*Ciphers\s/# &/i' /etc/ssh/sshd_config
