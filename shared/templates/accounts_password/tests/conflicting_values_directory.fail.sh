7afa32c1332fdff4ddc51dc281fe28c362511e49
Matthew Burket <mburket@redhat.com>
2025-01-22 12:31:09 -0600

    Update template accounts_password to use drop in files

--- shared/templates/accounts_password/tests/conflicting_values_directory.fail.sh
+++ shared/templates/accounts_password/tests/conflicting_values_directory.fail.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # This test only applies to platforms that check the pwquality.conf.d directory
-# platform = Oracle Linux 8
+# platform = Oracle Linux 8,multi_platform_rhel
 # variables = var_password_pam_{{{ VARIABLE }}}={{{ TEST_VAR_VALUE }}}
 
 truncate -s 0 /etc/security/pwquality.conf
