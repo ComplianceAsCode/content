b9af5e63fed97b5f501e4b2032457a24b52e5d65
Vojtech Polasek <vpolasek@redhat.com>
2025-01-23 16:16:03 +0100

    update the key_value_pair_in_file to be able to use XCCDF variable instead of hardcoded value

--- shared/templates/key_value_pair_in_file/template.py
+++ shared/templates/key_value_pair_in_file/template.py
@@ -1,4 +1,18 @@
+def set_variables_for_test_scenarios(data):
+    if not data.get("value"):
+        # this implies XCCDF variable is used
+        data["correct_value"] = "correct_value"
+    else:
+        data["correct_value"] = str(data["value"])
+
+    return data
+
+
 def preprocess(data, lang):
+    if data.get("value") is not None and data.get("xccdf_variable") is not None:
+        errmsg = ("The template definition of {0} specifies both value and xccdf_variable."
+                  "This is forbidden.".format(data["_rule_id"]))
+        raise ValueError(errmsg)
     if "sep" not in data:
         data["sep"] = " = "
 
@@ -10,4 +24,5 @@ def preprocess(data, lang):
 
     if "app" not in data:
         data["app"] = ""
+    data = set_variables_for_test_scenarios(data)
     return data
