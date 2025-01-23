b00bb342ce054b705efb1dd32f1056573e75274c
teacup-on-rockingchair <315160+teacup-on-rockingchair@users.noreply.github.com>
2025-01-23 17:37:12 +0200

    Drop code duplication in favour of product variable
    Thanks to @jan-cerny for the hint :bow:

--- linux_os/guide/services/xwindows/disabling_xwindows/xwindows_remove_packages/tests/packages_installed.fail.sh
+++ linux_os/guide/services/xwindows/disabling_xwindows/xwindows_remove_packages/tests/packages_installed.fail.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-{{% if product in ["sle12", "sle15"] %}}
-{{% set xwindows_packages = ['xorg-x11-server', 'xorg-x11-server-extra', 'xorg-x11-server-Xvfb', 'xwayland'] %}}
-{{% elif 'ol7' in product %}}
-{{% set xwindows_packages = ['xorg-x11-server-Xorg', 'xorg-x11-server-common', 'xorg-x11-server-utils'] %}}
-{{% else %}}
-{{% set xwindows_packages = ['xorg-x11-server-Xorg', 'xorg-x11-server-common', 'xorg-x11-server-utils', 'xorg-x11-server-Xwayland'] %}}
-{{% endif %}}
-
 # install packages
 {{% for package in xwindows_packages %}}
 {{{ bash_package_install(package) }}}
