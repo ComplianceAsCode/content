a33ad4fb5d1fdee9f332e5eec18ca1dbcff8b3f6
Simon John <sjohn@tuxcare.com>
2025-01-23 20:38:04 +0000

    Fixed some more merge conflicts when rebasing to master

--- linux_os/guide/auditing/auditd_configure_rules/directory_permissions_var_log_audit/tests/correct_value_0700.pass.sh
+++ linux_os/guide/auditing/auditd_configure_rules/directory_permissions_var_log_audit/tests/correct_value_0700.pass.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # packages = audit
-# platform = multi_platform_ol,multi_platform_rhel
+# platform = multi_platform_ol,multi_platform_rhel,multi_platform_almalinux
 
 source common_0700.sh
 
