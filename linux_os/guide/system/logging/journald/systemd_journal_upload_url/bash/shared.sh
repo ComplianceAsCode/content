55fdf224d1cd7021cb85842e36bae9ae482371dd
Eric Berry <eric.berry@canonical.com>
2025-01-28 10:55:08 -0800

    Ubuntu 24.04 6.1.2.1.2 Ensure systemd-journal-upload authentication is configured

--- linux_os/guide/system/logging/journald/systemd_journal_upload_url/bash/shared.sh
+++ linux_os/guide/system/logging/journald/systemd_journal_upload_url/bash/shared.sh
@@ -1,9 +1,15 @@
 # platform = multi_platform_slmicro,multi_platform_ubuntu
 
-{{{ bash_comment_config_line("/etc/systemd/journal-upload.conf(\.d/[^/]+\.conf)", '^URL') }}}
-
+{{% if 'ubuntu' in product %}}
+var_journal_upload_conf_file=/etc/systemd/journal-upload.conf.d/60-journald_upload.conf
 mkdir -p /etc/systemd/journal-upload.conf.d
 touch /etc/systemd/journal-upload.conf.d/60-journald_upload.conf
+
+{{{ bash_comment_config_line("/etc/systemd/journal-upload.conf(\.d/[^/]+\.conf)", '^URL') }}}
+{{% else %}}
+var_journal_upload_conf_file=/etc/systemd/journal-upload.conf
+{{% endif %}}
+
 {{{ bash_instantiate_variables("var_journal_upload_url") }}}
-{{{ bash_replace_or_append('/etc/systemd/journal-upload.conf.d/60-journald_upload.conf', '^URL', "$var_journal_upload_url", '%s=%s') }}}
+{{{ bash_replace_or_append('$var_journal_upload_conf_file', '^URL', "$var_journal_upload_url", '%s=%s') }}}
 
