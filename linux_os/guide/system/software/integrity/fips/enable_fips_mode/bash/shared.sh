2a9885b473657f823f1e0875d1156ff1cff42a77
Matthew Burket <mburket@redhat.com>
2025-01-31 07:28:12 -0600

    Address feedback in #12946

--- linux_os/guide/system/software/integrity/fips/enable_fips_mode/bash/shared.sh
+++ linux_os/guide/system/software/integrity/fips/enable_fips_mode/bash/shared.sh
@@ -1,8 +1,7 @@
 # platform = multi_platform_all
-{{{ bash_instantiate_variables("var_system_crypto_policy") }}}
 
 if {{{ bash_bootc_build() }}}; then
-	crypto_policies_no_reload="--no-reload"
 	cat > /usr/lib/bootc/kargs.d/01-fips.toml << EOF
 kargs = ["fips=1"]
 EOF
+fi
