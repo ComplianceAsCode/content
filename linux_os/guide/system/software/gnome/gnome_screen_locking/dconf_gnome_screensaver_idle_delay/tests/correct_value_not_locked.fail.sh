dce05b7be4590ce0552fc4c0c01ac15ce70cc6fd
Miha Purg <miha.purg@canonical.com>
2025-01-31 10:27:43 +0100

    Extend screensaver_idle_delay to check if setting is locked

--- linux_os/guide/system/software/gnome/gnome_screen_locking/dconf_gnome_screensaver_idle_delay/tests/correct_value_not_locked.fail.sh
+++ linux_os/guide/system/software/gnome/gnome_screen_locking/dconf_gnome_screensaver_idle_delay/tests/correct_value_not_locked.fail.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# platform = multi_platform_ubuntu
 # packages = dconf,gdm
 # variables = inactivity_timeout_value=900
 
@@ -8,7 +9,3 @@ clean_dconf_settings
 
 add_dconf_profiles
 add_dconf_setting "org/gnome/desktop/session" "idle-delay" "uint32 900" "local.d" "00-security-settings"
-
-{{% if 'ubuntu' in product %}}
-add_dconf_lock "org/gnome/desktop/screensaver" "idle-delay" "local.d" "00-security-settings"
-{{% endif %}}
--- linux_os/guide/system/software/gnome/gnome_screen_locking/dconf_gnome_screensaver_idle_delay/tests/wrong_value.fail.sh
+++ linux_os/guide/system/software/gnome/gnome_screen_locking/dconf_gnome_screensaver_idle_delay/tests/wrong_value.fail.sh
@@ -7,3 +7,7 @@
 clean_dconf_settings
 add_dconf_profiles
 add_dconf_setting "org/gnome/desktop/session" "idle-delay" "uint32 2900" "local.d" "00-security-settings"
+
+{{% if 'ubuntu' in product %}}
+add_dconf_lock "org/gnome/desktop/screensaver" "idle-delay" "local.d" "00-security-settings"
+{{% endif %}}
