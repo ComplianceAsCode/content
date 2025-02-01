b8dbc839685291a62cf52edff6c2fb77abf62c62
Miha Purg <miha.purg@canonical.com>
2025-02-01 14:12:57 +0100

    Disable tests not applicable to Ubuntu

--- linux_os/guide/system/permissions/files/file_permissions_ungroupowned/tests/group_in_usr_lib.pass.sh
+++ linux_os/guide/system/permissions/files/file_permissions_ungroupowned/tests/group_in_usr_lib.pass.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 #
+# remediation = none
+{{% if 'ubuntu' in product %}}
+# platform = Not Applicable
+{{% endif %}}
+
 UNOWNED_FILES=$(df --local -P | awk {'if (NR!=1) print $6'} | xargs -I '{}' find '{}' -xdev -nogroup)
 
 IFS=$"\n"
