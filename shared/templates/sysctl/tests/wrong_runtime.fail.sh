e149b232c8aae258b042e4630a9793f4e782af82
Alan Moore <alan.moore@canonical.com>
2025-02-06 14:44:03 +0000

    Clean up ufw sysctl.conf in all tests Created tests for ufw case

--- shared/templates/sysctl/tests/wrong_runtime.fail.sh
+++ shared/templates/sysctl/tests/wrong_runtime.fail.sh
@@ -4,7 +4,11 @@
 {{% endif %}}
 
 # Clean sysctl config directories
+{{% if "ubuntu" in product %}}
+rm -rf /usr/lib/sysctl.d/* /run/sysctl.d/* /etc/sysctl.d/* /etc/ufw/sysctl.conf
+{{% else %}}
 rm -rf /usr/lib/sysctl.d/* /run/sysctl.d/* /etc/sysctl.d/*
+{{% endif %}}
 
 sed -i "/{{{ SYSCTLVAR }}}/d" /etc/sysctl.conf
 echo "{{{ SYSCTLVAR }}} = {{{ SYSCTL_CORRECT_VALUE }}}" >> /etc/sysctl.conf
