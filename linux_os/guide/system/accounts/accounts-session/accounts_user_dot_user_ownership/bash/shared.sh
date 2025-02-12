bc7b6200e4e3fcc0f7605fc93cc9dd1274fdddd8
Alan Moore <alan.moore@canonical.com>
2025-02-12 15:55:02 +0000

    Use pipeline instead process substitution

--- linux_os/guide/system/accounts/accounts-session/accounts_user_dot_user_ownership/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-session/accounts_user_dot_user_ownership/bash/shared.sh
@@ -4,6 +4,4 @@
 # complexity = low
 # disruption = low
 
-while IFS=: read -r uid home; do
-    find "$home" -maxdepth 1 -name "\.[^.]*" -exec chown -f "$uid" {} \;
-done < <(awk -F: '{if ($3 >= {{{ uid_min }}} && $3 != {{{ nobody_uid }}}) print $3":"$6}' /etc/passwd)
+awk -F: '{if ($3 >= {{{ uid_min }}} && $3 != {{{ nobody_uid }}}) print $3":"$6}' /etc/passwd | while IFS=: read -r uid home; do find "$home" -maxdepth 1 -name "\.[^.]*" -exec chown -f $uid "{}" \;; done
