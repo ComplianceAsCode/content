a993c124e3caaebf8d469688c7f1dc02adcf1da3
Alan Moore <alan.moore@canonical.com>
2025-02-12 14:29:33 +0000

    Use while loop

--- linux_os/guide/system/accounts/accounts-session/accounts_user_dot_user_ownership/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-session/accounts_user_dot_user_ownership/bash/shared.sh
@@ -4,4 +4,6 @@
 # complexity = low
 # disruption = low
 
-awk -F':' '{ if ($3 >= {{{ uid_min }}} && $3 != {{{ nobody_uid }}}) system("find "$6" -maxdepth 1 -name \.[^.]?* -exec chown -f "$3" {} \;") }' /etc/passwd
+while IFS=: read -r uid home; do
+    find "$home" -maxdepth 1 -name "\.[^.]*" -exec chown -f "$uid" {} \;
+done < <(awk -F: '{if ($3 >= {{{ uid_min }}} && $3 != {{{ nobody_uid }}}) print $3":"$6}' /etc/passwd)
