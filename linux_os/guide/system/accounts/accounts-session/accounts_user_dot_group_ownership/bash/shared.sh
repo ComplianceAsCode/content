56f9a7852a51e614d0fb9c3d5c12ef8b0f6f1ff0
Alan Moore <alan.moore@canonical.com>
2025-02-12 14:45:19 +0000

    Use while loop

--- linux_os/guide/system/accounts/accounts-session/accounts_user_dot_group_ownership/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-session/accounts_user_dot_group_ownership/bash/shared.sh
@@ -4,4 +4,6 @@
 # complexity = low
 # disruption = low
 
-awk -F':' '{ if ($4 >= {{{ gid_min }}} && $4 != {{{ nobody_gid }}}) system("find "$6" -maxdepth 1 -name \.[^.]?* -exec chgrp -f "$4" {} \;") }' /etc/passwd
+while IFS=: read -r gid home; do
+    find "$home" -maxdepth 1 -name "\.[^.]*" -exec chgrp -f $gid {} \;
+done < <(awk -F: '{if ($4 >= {{{ gid_min }}} && $4 != {{{ nobody_gid }}}) print $4":"$6}' /etc/passwd)
