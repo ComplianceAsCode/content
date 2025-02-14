a5977547c9ad2ec7c76604cfc2c717e120d21b26
Miha Purg <miha.purg@canonical.com>
2025-02-14 12:49:58 +0100

    Fix oval and remediations for journald-upload rules
    - fix commenting by replacing broken macros with sed
    - fix missing section in config file by switching to
      macro bash_ensure_ini_config
    - improve OVAL to check for correct section

--- linux_os/guide/system/logging/journald/systemd_journal_upload_url/bash/shared.sh
+++ linux_os/guide/system/logging/journald/systemd_journal_upload_url/bash/shared.sh
@@ -1,15 +1,4 @@
-# platform = multi_platform_slmicro,multi_platform_ubuntu
-
-{{% if 'ubuntu' in product %}}
-var_journal_upload_conf_file=/etc/systemd/journal-upload.conf.d/60-journald_upload.conf
-mkdir -p /etc/systemd/journal-upload.conf.d
-touch /etc/systemd/journal-upload.conf.d/60-journald_upload.conf
-
-{{{ bash_comment_config_line("/etc/systemd/journal-upload.conf(\.d/[^/]+\.conf)", '^URL') }}}
-{{% else %}}
-var_journal_upload_conf_file=/etc/systemd/journal-upload.conf
-{{% endif %}}
+# platform = multi_platform_slmicro
 
 {{{ bash_instantiate_variables("var_journal_upload_url") }}}
-{{{ bash_replace_or_append('$var_journal_upload_conf_file', '^URL', "$var_journal_upload_url", '%s=%s') }}}
-
+{{{ bash_replace_or_append('/etc/systemd/journal-upload.conf', '^URL', "$var_journal_upload_url", '%s=%s') }}}
