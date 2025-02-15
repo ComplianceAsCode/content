212a2cde7a6e7c03762e737616881c9b90673fbb
Blasci <lucas.blaise.app@gmail.com>
2025-02-15 21:51:05 +0100

    [Debian12 CIS] Add support debian to nftables firewall rules

--- linux_os/guide/system/network/network-nftables/set_nftables_table/sce/shared.sh
+++ linux_os/guide/system/network/network-nftables/set_nftables_table/sce/shared.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# platform = multi_platform_rhel,multi_platform_ubuntu
+# platform = multi_platform_rhel,multi_platform_ubuntu,multi_platform_debian
 # check-import = stdout
 
 tbl_output=$(nft list tables | grep inet)
