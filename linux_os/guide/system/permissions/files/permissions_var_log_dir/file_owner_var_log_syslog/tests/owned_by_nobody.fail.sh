f7aa3f2be28cad2775c1a8a4a299ce88effcd827
Eric Berry <eric.berry@canonical.com>
2025-02-16 23:48:21 -0800

    Update file_owner_var_log_syslog for CIS Ubuntu 24.04

---
+++ linux_os/guide/system/permissions/files/permissions_var_log_dir/file_owner_var_log_syslog/tests/owned_by_nobody.fail.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# platform = Ubuntu 24.04
+# packages = rsyslog
+
+touch /var/log/syslog
+chown nobody /var/log/syslog*
---
+++ linux_os/guide/system/permissions/files/permissions_var_log_dir/file_owner_var_log_syslog/tests/owned_by_syslog.pass.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# platform = Ubuntu 24.04
+# platform = Ubuntu 24.04
+# packages = rsyslog
+
+touch /var/log/syslog
+chown syslog /var/log/syslog*
