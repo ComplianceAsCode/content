940ab7b0a8ddfb34cc1f838e7e487b52d24188c5
Eric Berry <eric.berry@canonical.com>
2025-02-16 23:40:34 -0800

    Implement file_groupowner_var_log_wbtmp

---
+++ linux_os/guide/system/permissions/files/permissions_var_log_dir/file_groupowner_var_log_wbtmp/tests/owned_by_nogroup.fail.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# platform = Ubuntu 24.04
+
+touch /var/log/btmp
+touch /var/log/btmp.1
+touch /var/log/btmp-1
+chgrp nogroup /var/log/btmp*
+touch /var/log/wtmp
+touch /var/log/wtmp.1
+touch /var/log/wtmp-1
+chgrp nogroup /var/log/wtmp*
