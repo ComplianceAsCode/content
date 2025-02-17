a6f6e944434998ac72affc47d615628c238b11f8
Eric Berry <eric.berry@canonical.com>
2025-02-16 23:49:37 -0800

    Implement file_owner_var_log_wbtmp

---
+++ linux_os/guide/system/permissions/files/permissions_var_log_dir/file_owner_var_log_wbtmp/tests/invalid_owner.fail.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+touch /var/log/wtmp.1
+chown syslog /var/log/wtmp.1
+touch /var/log/btmp.1
+chown syslog /var/log/btmp.1
+touch /var/log/wtmp-1
+chown syslog /var/log/wtmp-1
+touch /var/log/btmp-1
+chown syslog /var/log/btmp-1
---
+++ linux_os/guide/system/permissions/files/permissions_var_log_dir/file_owner_var_log_wbtmp/tests/valid_permissions.pass.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+touch /var/log/wtmp.1
+chown root /var/log/wtmp.1
+touch /var/log/btmp.1
+chown root /var/log/btmp.1
+touch /var/log/wtmp-1
+chown root /var/log/wtmp-1
+touch /var/log/btmp-1
+chown root /var/log/btmp-1
+
