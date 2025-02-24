ab2388a744ef73bfaa1e5f5015a2a8f6f1001300
Alan Moore <alan.moore@canonical.com>
2025-02-24 20:55:44 +0800

    correct the path on ubuntu

--- linux_os/guide/services/ntp/service_timesyncd_configured/tests/common.sh
+++ linux_os/guide/services/ntp/service_timesyncd_configured/tests/common.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+{{% if "ubuntu" in product %}}
+mkdir -p /etc/systemd/timesyncd.conf.d/
+echo "" > /etc/systemd/timesyncd.conf.d/oscap-remedy.conf
+{{% else %}}
 mkdir -p /etc/systemd/timesyncd.d/
-echo "" > /etc/systemd/timesyncd.conf
 echo "" > /etc/systemd/timesyncd.d/oscap-remedy.conf
+{{% endif %}}
+echo "" > /etc/systemd/timesyncd.conf
