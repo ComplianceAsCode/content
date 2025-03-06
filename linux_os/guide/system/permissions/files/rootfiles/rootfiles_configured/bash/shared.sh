8923027c4920eb88b902997c8aed087dc4c00f28
Matthew Burket <mburket@redhat.com>
2025-03-06 11:28:27 -0600

    Fix bug in rootfiles_configured bash remediation

--- linux_os/guide/system/permissions/files/rootfiles/rootfiles_configured/bash/shared.sh
+++ linux_os/guide/system/permissions/files/rootfiles/rootfiles_configured/bash/shared.sh
@@ -11,7 +11,7 @@
     {{% set dest_path = '/root/.' ~ file -%}}
     {{% set source_path = '/usr/share/rootfiles/.' ~ file -%}}
     {{% set new_line = 'C ' ~ dest_path ~ ' 600 root root - '  ~ source_path ~ "\n"  -%}}
-    find "/etc/tmpfiles.d/" -name "*.conf " -print0 | xargs -0 sed -i  "/C[[:space:]]*{{{ dest_path.replace('/', '\\/') }}}/d"
+    find "/etc/tmpfiles.d/" -name "*.conf" -print0 | xargs -0 sed -i  "/C[[:space:]]*{{{ dest_path.replace('/', '\\/') }}}/d"
     {{%- set ns.contents = ns.contents ~ new_line -%}}
 {{%- endfor %}}
 
