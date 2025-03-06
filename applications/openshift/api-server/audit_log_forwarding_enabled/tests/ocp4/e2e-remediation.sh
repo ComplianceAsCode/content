60130208265c72e1845c09d2e2fbab5533bea976
Xiaojie Yuan <xiyuan@redhat.com>
2025-03-06 16:52:48 +0800

    Update openshift cluster logging version to 6.1

--- applications/openshift/api-server/audit_log_forwarding_enabled/tests/ocp4/e2e-remediation.sh
+++ applications/openshift/api-server/audit_log_forwarding_enabled/tests/ocp4/e2e-remediation.sh
@@ -2,15 +2,15 @@
 set -xe
 
 ocp_version=$(oc version -ojson | jq -r '.openshiftVersion')
-clo_v6_available_from="4.14.0"
+clo_v6_1_available_from="4.15.0"
 
-if [ "$(printf '%s\n' "$ocp_version" "$clo_v6_available_from" | sort -V | head -n1)" = "$clo_v6_available_from" ]; then
-    echo "OCP ${ocp_version} has CLO 6.0 is available for install";
+if [ "$(printf '%s\n' "$ocp_version" "$clo_v6_1_available_from" | sort -V | head -n1)" = "$clo_v6_1_available_from" ]; then
+    echo "OCP ${ocp_version} has CLO 6.1 is available for install";
     install_v6=true
 fi
 
 if [ "$install_v6" = true ] ; then
-    echo "installing cluster-logging-operator V6.0"
+    echo "installing cluster-logging-operator V6.1"
     oc apply -f ${ROOT_DIR}/ocp-resources/e2e/cluster-logging-install-observability.yaml
 else
     echo "installing cluster-logging-operator"
@@ -29,7 +29,7 @@ oc wait -n openshift-logging --for=condition=Available  --timeout=120s \
     deployment/cluster-logging-operator
 
 if [ "$install_v6" = true ] ; then
-    echo "installing clusterlogforwarder 6.0"
+    echo "installing clusterlogforwarder 6.1"
     oc apply -f ${ROOT_DIR}/ocp-resources/e2e/forward-logs-observability.yaml
 else
     echo "installing clusterlogging instance"
