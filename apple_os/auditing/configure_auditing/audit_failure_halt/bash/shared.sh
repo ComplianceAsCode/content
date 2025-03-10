b0655f0bb664474911ba98542065968df32aec2b
Matthew Burket <mburket@redhat.com>
2025-03-10 14:32:31 -0500

    Remove macOS content

--- apple_os/auditing/configure_auditing/audit_failure_halt/bash/shared.sh
+++
@@ -1,7 +0,0 @@
-# platform = Apple macOS 10.15
-# reboot = false
-# strategy = enable
-# complexity = low
-# disruption = low
-
-/usr/bin/sed -i.bak '/^policy/ s/$/,ahlt/' /etc/security/audit_control; sudo /usr/sbin/audit -s
