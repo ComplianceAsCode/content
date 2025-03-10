5f9f27d9ff39289ecfd41547e6d6696d711d877c
Jan Černý <jcerny@redhat.com>
2025-03-10 11:24:53 +0100

    Introduce rule enable_gpgcheck_for_all_repositories
    This commit introduces new rule `enable_gpgcheck_for_all_repositories`
    which is better aligned with DISA rule `SV-230264r1017377_rule` than
    currently existing rules `ensure_gpgcheck_globally_activated` and
    co-existing `ensure_gpgcheck_never_disabled`.  The rule
    `ensure_gpgcheck_globally_activated` checks for `gpgcheck=1` in
    `/etc/yum.conf` ie. in global configuration.  The rule
    `ensure_gpgcheck_never_disabled` checks that no `.repo file in
    `/etc/yum.repos.d` contains `gpgcheck=0`. DISA's rule
    `SV-230264r1017377_rule` checks for `gpgcheck` settings in all `.repo`
    files in `/etc/yum.repos.d` ie. per-repo configuration. We need to
    realize that per-repo settings take precendence over global
    configuration.  The new rule `enable_gpgcheck_for_all_repositories` will
    check for `gpgcheck` settings in all `.repo` files in
    `/etc/yum.repos.d`.  The commit will add the new rule to RHEL 8 STIG
    profile.
    
    Fixes: #13110

---
+++ linux_os/guide/system/software/updating/enable_gpgcheck_for_all_repositories/tests/gpgcheck_missing_everywhere.fail.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+rm -rf /etc/yum.repos.d/
+mkdir -p /etc/yum.repos.d/
+cat > /etc/yum.repos.d/fedora.repo <<EOF
+[fedora]
+name=Fedora 41 - x86_64
+#baseurl=http://download.example/pub/fedora/linux/releases/41/Everything/x86_64/os/
+metalink=https://mirrors.fedoraproject.org/metalink?repo=fedora-41&arch=x86_64
+enabled=1
+countme=1
+metadata_expire=7d
+repo_gpgcheck=0
+type=rpm
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-fedora-41-x86_64
+skip_if_unavailable=False
+
+[fedora-debuginfo]
+name=Fedora 41 - x86_64 - Debug
+#baseurl=http://download.example/pub/fedora/linux/releases/41/Everything/x86_64/debug/tree/
+metalink=https://mirrors.fedoraproject.org/metalink?repo=fedora-debug-41&arch=x86_64
+enabled=1
+metadata_expire=7d
+repo_gpgcheck=0
+type=rpm
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-fedora-41-x86_64
+skip_if_unavailable=False
+
+[fedora-source]
+name=Fedora 41 - Source
+#baseurl=http://download.example/pub/fedora/linux/releases/41/Everything/source/tree/
+metalink=https://mirrors.fedoraproject.org/metalink?repo=fedora-source-41&arch=x86_64
+enabled=1
+metadata_expire=7d
+repo_gpgcheck=0
+type=rpm
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-fedora-41-x86_64
+skip_if_unavailable=False
+EOF
