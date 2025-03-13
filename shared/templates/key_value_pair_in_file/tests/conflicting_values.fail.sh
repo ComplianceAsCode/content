e4ef1f60ed67ec5c63dd89a836a1302b1b50d961
vojtapolasek <krecoun@gmail.com>
2025-03-13 11:31:12 +0100

    add two new parameters to the template to configure correct and wrong test value
    See updated template documentation for explanation.

--- shared/templates/key_value_pair_in_file/tests/conflicting_values.fail.sh
+++ shared/templates/key_value_pair_in_file/tests/conflicting_values.fail.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 {{%- if XCCDF_VARIABLE %}}
-# variables = {{{ XCCDF_VARIABLE }}}={{{ CORRECT_VALUE }}}
+# variables = {{{ XCCDF_VARIABLE }}}={{{ TEST_CORRECT_VALUE }}}
 {{% endif %}}
 
 mkdir -p $(dirname {{{ PATH }}})
 touch {{{ PATH }}}
 
 sed -i "/{{{ KEY }}}/d" "{{{ PATH }}}"
-echo "{{{ KEY }}}{{{ SEP }}}{{{ CORRECT_VALUE }}}" >> "{{{ PATH }}}"
-echo "{{{ KEY }}}{{{ SEP }}}wrong_value" >> "{{{ PATH }}}"
+echo "{{{ KEY }}}{{{ SEP }}}{{{ TEST_CORRECT_VALUE }}}" >> "{{{ PATH }}}"
+echo "{{{ KEY }}}{{{ SEP }}}{{{ TEST_WRONG_VALUE }}}" >> "{{{ PATH }}}"
