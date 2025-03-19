eebb3ddb16a99a5193881ea1e241d5221c121525
Alan Moore <alan.moore@canonical.com>
2025-03-19 10:11:36 +0000

    Make dirs one line

--- linux_os/guide/system/permissions/files/permissions_within_important_dirs/file_ownership_binary_dirs/tests/right_owner.pass.sh
+++ linux_os/guide/system/permissions/files/permissions_within_important_dirs/file_ownership_binary_dirs/tests/right_owner.pass.sh
@@ -1,21 +1,7 @@
 #!/bin/bash
 
 {{% if 'ubuntu' in product %}}
-find /bin/ \
-/usr/bin/ \
-/usr/local/bin/ \
-/sbin/ \
-/usr/sbin/ \
-/usr/local/sbin/ \
-/usr/libexec \
-\! -uid -100 -execdir chown root {} \;
+find /bin/ /usr/bin/ /usr/local/bin/ /sbin/ /usr/sbin/ /usr/local/sbin/ /usr/libexec \! -uid -100 -execdir chown root {} \;
 {{% else %}}
-find /bin/ \
-/usr/bin/ \
-/usr/local/bin/ \
-/sbin/ \
-/usr/sbin/ \
-/usr/local/sbin/ \
-/usr/libexec \
-\! -user root -execdir chown root {} \;
+find /bin/ /usr/bin/ /usr/local/bin/ /sbin/ /usr/sbin/ /usr/local/sbin/ /usr/libexec \! -user root -execdir chown root {} \;
 {{% endif %}}
