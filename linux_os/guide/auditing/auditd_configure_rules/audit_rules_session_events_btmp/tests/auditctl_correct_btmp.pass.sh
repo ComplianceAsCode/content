ed88af644a1d03ccbb065d54f93b489ee263afc2
Jan Černý <jcerny@redhat.com>
2025-04-07 14:44:16 +0200

    Remove static content from rules
    The remediations and test are now covered by the templated code
    in the audit_rules_watch template, therefore, we will remove
    the static code from rules audit_rules_session_events_btmp,
    audit_rules_session_events_utmp, audit_rules_session_events_wtmp.

--- linux_os/guide/auditing/auditd_configure_rules/audit_rules_session_events_btmp/tests/auditctl_correct_btmp.pass.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# packages = audit
-
-echo "-w /var/log/btmp -p wa -k logins" >> /etc/audit/audit.rules
-sed -i "s%^ExecStartPost=.*%ExecStartPost=-/sbin/auditctl%" /usr/lib/systemd/system/auditd.service
--- linux_os/guide/auditing/auditd_configure_rules/audit_rules_session_events_utmp/tests/auditctl_wrong_rule_utmp.fail.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# packages = audit
-
-echo "-w /run/something -p wa -k session" >> /etc/audit/audit.rules
-sed -i "s%^ExecStartPost=.*%ExecStartPost=-/sbin/auditctl%" /usr/lib/systemd/system/auditd.service
