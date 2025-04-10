453c7b0a70fd6a5a01abcec8e2901ac2b6ba7aa4
vojtapolasek <krecoun@gmail.com>
2025-04-10 11:27:08 +0200

    sysctl template: add new option no_remediation
    see the documentation update in this commit for explanation

--- shared/templates/sysctl/tests/wrong_value_d_directory.fail.sh
+++ shared/templates/sysctl/tests/wrong_value_d_directory.fail.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+{{%- if NO_REMEDIATION %}}
+# remediation = none
+{{%- endif -%}}
 {{% if SYSCTLVAL == "" %}}
 # variables = sysctl_{{{ SYSCTLID }}}_value={{{ SYSCTL_CORRECT_VALUE }}}
 {{% endif %}}
