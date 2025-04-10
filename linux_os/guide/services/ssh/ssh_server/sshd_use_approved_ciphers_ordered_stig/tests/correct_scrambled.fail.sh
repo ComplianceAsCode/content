9f4936915e212d8de8aeebf26616bca315d4c93a
Miha Purg <miha.purg@canonical.com>
2025-04-10 17:39:39 +0200

    Switch to using hardcoded sshd ciphers in Ubuntu STIGs
    FIPS approved ciphers should not be modifiable via a tailoring file.
    This fix replaces XCCDF variable sshd_approved_ciphers with hardcoded values.

--- linux_os/guide/services/ssh/ssh_server/sshd_use_approved_ciphers_ordered_stig/tests/correct_scrambled.fail.sh
+++ linux_os/guide/services/ssh/ssh_server/sshd_use_approved_ciphers_ordered_stig/tests/correct_scrambled.fail.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+{{% if product == "ubuntu2204" %}}
+sshd_scrambled_ciphers="aes128-gcm@openssh.com,aes256-ctr,aes256-gcm@openssh.com,aes192-ctr,aes128-ctr"
+{{% else %}}
+sshd_scrambled_ciphers="aes128-ctr,aes192-ctr,aes256-ctr"
+{{% endif %}}
+
 if grep -q "^Ciphers" /etc/ssh/sshd_config; then
-	sed -i "s/^Ciphers.*/Ciphers aes128-ctr,aes192-ctr,aes256-ctr/" /etc/ssh/sshd_config
+	sed -i "s/^Ciphers.*/Ciphers $sshd_scrambled_ciphers/" /etc/ssh/sshd_config
 else
-	echo "Ciphers aes128-ctr,aes192-ctr,aes256-ctr" >> /etc/ssh/sshd_config
+	echo "Ciphers $sshd_scrambled_ciphers" >> /etc/ssh/sshd_config
 fi
