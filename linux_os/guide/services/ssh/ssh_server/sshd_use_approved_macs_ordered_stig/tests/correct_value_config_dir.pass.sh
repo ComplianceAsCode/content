08b1919a3ac883c6b0fc93aa4d1a94f5ff65bf5b
Miha Purg <miha.purg@canonical.com>
2025-04-10 18:08:49 +0200

    Switch to using hardcoded sshd MACs in Ubuntu STIGs
    FIPS approved MACs should not be modifiable via a tailoring file.
    This fix replaces XCCDF variable sshd_approved_macs with hardcoded values.

--- linux_os/guide/services/ssh/ssh_server/sshd_use_approved_macs_ordered_stig/tests/correct_value_config_dir.pass.sh
+++ linux_os/guide/services/ssh/ssh_server/sshd_use_approved_macs_ordered_stig/tests/correct_value_config_dir.pass.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 # platform = multi_platform_ubuntu
 
+{{% if product == "ubuntu2204" %}}
 sshd_approved_macs="hmac-sha2-512,hmac-sha2-512-etm@openssh.com,hmac-sha2-256,hmac-sha2-256-etm@openssh.com"
+{{% else %}}
+sshd_approved_macs="hmac-sha2-512,hmac-sha2-256"
+{{% endif %}}
 
 sed -i "/^MACs.*/d" /etc/ssh/sshd_config
 sed -i "/^MACs.*/d" /etc/ssh/sshd_config.d/*
