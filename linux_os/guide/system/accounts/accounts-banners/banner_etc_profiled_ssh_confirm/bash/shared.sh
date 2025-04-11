91b86e490566ae05178559ca4610b39cbc5ca0ad
Eric Berry <eric.berry@canonical.com>
2025-04-11 16:08:03 -0700

    Ubuntu 24.04 STIG rule UBTU-24-200680

---
+++ linux_os/guide/system/accounts/accounts-banners/banner_etc_profiled_ssh_confirm/bash/shared.sh
@@ -0,0 +1,20 @@
+# platform = multi_platform_all
+
+{{{ bash_instantiate_variables("var_ssh_confirm_text") }}}
+
+# Multiple regexes transform the banner regex into a usable banner
+# 0 - Remove anchors around the banner text
+{{{ bash_deregexify_banner_anchors("var_ssh_confirm_text") }}}
+# 1 - Add spaces ' '. (Transforms regex for "space or newline" into a " ")
+{{{ bash_deregexify_banner_space("var_ssh_confirm_text") }}}
+# 2 - Adds newlines. (Transforms "(?:\[\n\]+|(?:\n)+)" into "\n")
+{{{ bash_deregexify_banner_newline("var_ssh_confirm_text", "\\n") }}}
+# 3 - Remove any leftover backslash. (From any parethesis in the banner, for example).
+{{{ bash_deregexify_banner_backslash("var_ssh_confirm_text") }}}
+formatted=$(echo "$var_ssh_confirm_text" | fold -sw 80)
+
+cat <<EOF >/etc/profile.d/ssh_confirm.sh
+$formatted
+EOF
+
+chmod a+x /etc/profile.d/ssh_confirm.sh
---
+++ linux_os/guide/system/accounts/accounts-banners/banner_etc_profiled_ssh_confirm/tests/banner_etc_profiled_ssh_confirm.fail.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# platform = multi_platform_all
+
+rm -f /etc/profile.d/ssh_confirm.sh
+touch /etc/profile.d/ssh_confirm.sh
