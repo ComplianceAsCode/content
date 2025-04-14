4f576b60fb9955038580ca33c39c1a2836986e07
Jan Černý <jcerny@redhat.com>
2025-04-14 14:05:23 +0200

    Align template audit_rules_privileged_commands with DISA
    Recently, we have updated the `audit_rules_privileged_commands` template
    to align the audit rules format with audit rules recommendations for
    RHEL 10. However, our productization test runs revealed that the new
    form isn't aligned with DISA STIG requirements.
    
    The misalignment is that we expect 2 lines for each binary, the lines
    contain architecture filters `-F arch=b32` or `-F arch=b64`, but DISA
    expects a single line for each binary, without architecture filters.
    
    Both our variant and DISA's variant are valid. Both of them work and
    could be accepted. But, our variant is better, because by having 2 rules
    each for different architecture we are telling audit which syscall table
    should be used. OTOH DISA's variant leaves everything up to the kernel's
    decision. As a result, our variant provides higher performance of the
    watched application than DISA's variant.
    
    To solve the misalignment with DISA content, we will use the
    architecture specific two line rule only on RHEL 10 and we
    will use the architecture independent single line rule format
    on RHELs older than 10. The reason is that for RHEL 10 there
    is no DISA STIG yet, so on RHEL 10 we couldn't experience
    any misalignment with DISA content.
    
    Fixes: https://github.com/ComplianceAsCode/content/issues/13272

--- shared/templates/audit_rules_privileged_commands/tests/auditctl_missing_perm_x.fail.sh
+++ shared/templates/audit_rules_privileged_commands/tests/auditctl_missing_perm_x.fail.sh
@@ -6,5 +6,9 @@ source common.sh
 
 {{{ setup_auditctl_environment() }}}
 
+{{% if product == "rhel10" %}}
 echo "-a always,exit -F arch=b32 -F path={{{ PATH }}} -F auid>={{{ auid }}} -F auid!=unset -k test_key" >> /etc/audit/audit.rules
 echo "-a always,exit -F arch=b64 -F path={{{ PATH }}} -F auid>={{{ auid }}} -F auid!=unset -k test_key" >> /etc/audit/audit.rules
+{{% else %}}
+echo "-a always,exit -F path={{{ PATH }}} -F auid>={{{ auid }}} -F auid!=unset -k test_key" >> /etc/audit/audit.rules
+{{% endif %}}
--- shared/templates/audit_rules_privileged_commands/tests/augenrules_missing_auid.fail.sh
+++ shared/templates/audit_rules_privileged_commands/tests/augenrules_missing_auid.fail.sh
@@ -2,5 +2,9 @@
 
 source common.sh
 
+{{% if product == "rhel10" %}}
 echo "-a always,exit -F arch=b32 -F path={{{ PATH }}} ${perm_x} -F auid!=unset -k test_key" >> /etc/audit/rules.d/test_key.rules
 echo "-a always,exit -F arch=b64 -F path={{{ PATH }}} ${perm_x} -F auid!=unset -k test_key" >> /etc/audit/rules.d/test_key.rules
+{{% else %}}
+echo "-a always,exit -F path={{{ PATH }}} ${perm_x} -F auid!=unset -k test_key" >> /etc/audit/rules.d/test_key.rules
+{{% endif %}}
