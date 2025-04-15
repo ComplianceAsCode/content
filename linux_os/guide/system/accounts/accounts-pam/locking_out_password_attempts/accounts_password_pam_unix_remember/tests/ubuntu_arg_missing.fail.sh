f207f50d322a561c26bb9e0ae2c41b2e1359f609
Alan Moore <alan.moore@canonical.com>
2025-04-15 16:34:29 +0100

    Ensure single line of pam_unix exist during testing

--- linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_password_pam_unix_remember/tests/ubuntu_arg_missing.fail.sh
+++ linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_password_pam_unix_remember/tests/ubuntu_arg_missing.fail.sh
@@ -5,7 +5,8 @@ config_file=/usr/share/pam-configs/tmpunix
 cat << EOF > "$config_file"
 Name: Unix authentication
 Default: yes
-Priority: 256
+Priority: 257
+Conflicts: unix
 Auth-Type: Primary
 Auth:
         [success=end default=ignore]    pam_unix.so try_first_pass
