3207a8df4f75f4fd7e8e5b48f50ac57d4af53c5f
Jan Černý <jcerny@redhat.com>
2025-04-16 18:01:47 +0200

    Promote cafile variable to product property

--- linux_os/guide/system/logging/rsyslog_sending_messages/rsyslog_remote_tls_cacert/tests/cacert_configured.pass.sh
+++ linux_os/guide/system/logging/rsyslog_sending_messages/rsyslog_remote_tls_cacert/tests/cacert_configured.pass.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-{{% if product in ["fedora", "rhel10" ] %}}
-{{# https://fedoraproject.org/wiki/Changes/dropingOfCertPemFile #}}
-{{% set cafile = "/etc/pki/ca-trust/extracted/pem/tls-ca-bundle.pem" %}}
-{{% else %}}
-{{% set cafile = "/etc/pki/tls/cert.pem" %}}
-{{% endif %}}
-echo 'global(DefaultNetstreamDriverCAFile="{{{ cafile }}}")' >> /etc/rsyslog.conf
+
+echo 'global(DefaultNetstreamDriverCAFile="{{{ rsyslog_cafile }}}")' >> /etc/rsyslog.conf
--- linux_os/guide/system/logging/rsyslog_sending_messages/rsyslog_remote_tls_cacert/tests/syntax_error.fail.sh
+++ linux_os/guide/system/logging/rsyslog_sending_messages/rsyslog_remote_tls_cacert/tests/syntax_error.fail.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 # remediation = none
-{{% if product in ["fedora", "rhel10" ] %}}
-{{# https://fedoraproject.org/wiki/Changes/dropingOfCertPemFile #}}
-{{% set cafile = "/etc/pki/ca-trust/extracted/pem/tls-ca-bundle.pem" %}}
-{{% else %}}
-{{% set cafile = "/etc/pki/tls/cert.pem" %}}
-{{% endif %}}
-echo 'global(DefaultNetstreamDriverCAFile="{{{ cafile }}}") *.*' >> /etc/rsyslog.conf
+
+echo 'global(DefaultNetstreamDriverCAFile="{{{ rsyslog_cafile }}}") *.*' >> /etc/rsyslog.conf
