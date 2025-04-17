b590d4a49d769f2fc1613f2958f91aa07e2c13a9
Eric Berry <eric.berry@canonical.com>
2025-04-17 11:38:02 -0700

    Ubuntu 24.04 SITG rule UBTU-24-400360 sssd_certification_path_trust_anchor

---
+++ linux_os/guide/services/sssd/sssd_certification_path_trust_anchor/tests/partial_config.fail.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# packages = sssd-common
+
+mkdir -p /etc/sssd/conf.d
+touch /etc/sssd/sssd.conf
+echo -e "$ sudo vi /etc/sssd/sssd.conf
+[sssd]
+services = nss,pam,ssh
+config_file_version = 2
+
+[pam]
+pam_cert_auth = True
+
+[domain/test.com]
+ldap_user_certificate = usercertificate;binary
+certificate_verification = ca_cert
+ca_cert = /etc/ssl/certs/ca-certificates.crt
+" >> /etc/sssd/sssd.conf
+
