d5f67f03106132b76f6a778fae74e820434ea79a
Alan Moore <alan.moore@canonical.com>
2025-04-18 16:27:47 +0100

    Fix faillock_silent tests

--- linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_passwords_pam_faillock_silent/tests/ubuntu_correct_pamd.pass.sh
+++ linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_passwords_pam_faillock_silent/tests/ubuntu_correct_pamd.pass.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
 # platform = multi_platform_ubuntu
 
-source ubuntu_common.sh
+cat << EOF > /usr/share/pam-configs/tmp_faillock
+Name: Enable pam_faillock to deny access
+Default: yes
+Priority: 0
+Auth-Type: Primary
+Auth:
+    [default=die]                   pam_faillock.so authfail silent
+    sufficient                      pam_faillock.so authsucc silent
+EOF
 
-sed -i 's/\(.*pam_faillock.so.*\)/\1 silent/g' /etc/pam.d/common-auth
+cat << EOF > /usr/share/pam-configs/tmp_faillock_notify
+Name: Notify of failed login attempts and reset count upon success
+Default: yes
+Priority: 1024
+Auth-Type: Primary
+Auth:
+    requisite                       pam_faillock.so preauth silent
+Account-Type: Primary
+Account:
+    required                        pam_faillock.so
+EOF
 
+DEBIAN_FRONTEND=noninteractive pam-auth-update
+rm /usr/share/pam-configs/tmp_faillock /usr/share/pam-configs/tmp_faillock_notify
