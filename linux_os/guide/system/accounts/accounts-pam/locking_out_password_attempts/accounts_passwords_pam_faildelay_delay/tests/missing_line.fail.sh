907c9dd199cf9d7ccbf728e9d47ba753cd7f205f
Alan Moore <alan.moore@canonical.com>
2025-04-23 19:05:44 +0100

    Adjust tests

--- linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_passwords_pam_faildelay_delay/tests/missing_line.fail.sh
+++ linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_passwords_pam_faildelay_delay/tests/missing_line.fail.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+# variables = var_password_pam_delay=4000000
 
+{{%- if product == "ubuntu2404" %}}
+{{%- else %}}
 if grep -q 'pam_faildelay.so' /etc/pam.d/common-auth; then
     sed -i --follow-symlinks "/pam_faildelay\.so/d" /etc/pam.d/common-auth
 fi
+{{%- endif %}}
