6e6f60203eb32ea79d049654d28f39cdebdf439a
Alan Moore <alan.moore@canonical.com>
2025-04-28 14:49:33 +0100

    Add packages applicability

--- linux_os/guide/services/sssd/sssd_enable_user_cert/tests/default.fail.sh
+++ linux_os/guide/services/sssd/sssd_enable_user_cert/tests/default.fail.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # remediation = none
+# packages = sssd
 
 touch /etc/sssd/sssd.conf
 sed -i "s/ldap_user_certificate.*//g" /etc/sssd/sssd.conf
--- linux_os/guide/services/sssd/sssd_enable_user_cert/tests/with_section.pass.sh
+++ linux_os/guide/services/sssd/sssd_enable_user_cert/tests/with_section.pass.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # remediation = none
+# packages = sssd
+
 cat >> /etc/sssd/sssd.conf<< EOF
 ldap_user_certificate = userCertificate;binary
 EOF
