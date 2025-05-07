39e566a32de7d8d86c55fa59190710e1c6f040c1
Alan Moore <alan.moore@canonical.com>
2025-05-07 16:42:14 +0100

    Update tests' conf path

--- linux_os/guide/system/logging/rsyslog_filecreatemode/tests/filecreatemode_0600.pass.sh
+++ linux_os/guide/system/logging/rsyslog_filecreatemode/tests/filecreatemode_0600.pass.sh
@@ -2,4 +2,4 @@
 # packages = rsyslog
 
 sed -i '/^\s*$FileCreateMode/d' /etc/rsyslog.conf /etc/rsyslog.d/* || true
-echo '$FileCreateMode 0600' > /etc/rsyslog.d/99-rsyslog_filecreatemode.conf
+echo '$FileCreateMode 0600' > /etc/rsyslog.d/00-rsyslog_filecreatemode.conf
--- linux_os/guide/system/logging/rsyslog_filecreatemode/tests/filecreatemode_0601.fail.sh
+++ linux_os/guide/system/logging/rsyslog_filecreatemode/tests/filecreatemode_0601.fail.sh
@@ -2,4 +2,4 @@
 # packages = rsyslog
 
 sed -i '/^\s*$FileCreateMode/d' /etc/rsyslog.conf /etc/rsyslog.d/* || true
-echo '$FileCreateMode 0601' > /etc/rsyslog.d/99-rsyslog_filecreatemode.conf
+echo '$FileCreateMode 0601' > /etc/rsyslog.d/00-rsyslog_filecreatemode.conf
