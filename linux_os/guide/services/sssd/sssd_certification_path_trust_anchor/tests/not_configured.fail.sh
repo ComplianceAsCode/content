7989a5d6410664a71b03c57c3751796c99d8956f
Eric Berry <eric.berry@canonical.com>
2025-05-08 11:46:40 +0200

    Ubuntu 24.04 STIG rule UBTU-24-200680

--- linux_os/guide/services/sssd/sssd_certification_path_trust_anchor/tests/not_configured.fail.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# packages = sssd-common
-
-mkdir -p /etc/sssd/conf.d
-touch /etc/sssd/sssd.conf
--- linux_os/guide/services/sssd/sssd_certification_path_trust_anchor/tests/wrong_section.fail.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# packages = sssd-common
-
-mkdir -p /etc/sssd/conf.d
-touch /etc/sssd/sssd.conf
-echo -e "[sssd]\ncertificate_verification = ca_cert,ocsp" >> /etc/sssd/sssd.conf
