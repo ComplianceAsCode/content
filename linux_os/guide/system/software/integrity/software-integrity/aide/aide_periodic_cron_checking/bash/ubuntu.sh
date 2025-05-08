fa754f1ad21fcff3eb93796497a1e2f4ca79f4f3
Alan Moore <alan.moore@canonical.com>
2025-05-08 14:15:12 +0100

    Update the aide path for Ubuntu2404

--- linux_os/guide/system/software/integrity/software-integrity/aide/aide_periodic_cron_checking/bash/ubuntu.sh
+++ linux_os/guide/system/software/integrity/software-integrity/aide/aide_periodic_cron_checking/bash/ubuntu.sh
@@ -2,8 +2,13 @@
 
 {{{ bash_package_install("aide") }}}
 
+{{% if product == 'ubuntu2404' %}}
+if ! grep -q 'SCRIPT="/usr/share/aide/bin/dailyaidecheck"' /etc/cron.*/*; then
+    cp -f /usr/share/aide/config/cron.daily/dailyaidecheck /etc/cron.daily/
+{{% else %}}
 # AiDE usually adds its own cron jobs to /etc/cron.daily. If script is there, this rule is
 # compliant. Otherwise, we copy the script to the /etc/cron.weekly
 if ! grep -Eq '^(\/usr\/bin\/)?aide(\.wrapper)?\s+' /etc/cron.*/*; then
     cp -f /usr/share/aide/config/cron.daily/aide /etc/cron.weekly/
+{{% endif %}}
 fi
