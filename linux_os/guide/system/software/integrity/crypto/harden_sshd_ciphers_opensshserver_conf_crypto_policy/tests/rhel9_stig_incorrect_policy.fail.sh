a4b559ee6165c18f096ffbbdcdea3ce84c0f5f9c
Armando Acosta <armando.acosta@oracle.com>
2025-05-12 14:09:15 -0600

    Add tests to harden_sshd_macs/ciphers.. for OL9
    Add new test to harden_sshd_macs_opensshserver_conf_crypto_policy for OL9
    Update test to harden_sshd_ciphers_opensshserver_conf_crypto_policy rule to inclue OL9

--- linux_os/guide/system/software/integrity/crypto/harden_sshd_ciphers_opensshserver_conf_crypto_policy/tests/rhel9_stig_incorrect_policy.fail.sh
+++ linux_os/guide/system/software/integrity/crypto/harden_sshd_ciphers_opensshserver_conf_crypto_policy/tests/rhel9_stig_incorrect_policy.fail.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# platform = Red Hat Enterprise Linux 9
+# platform = Oracle Linux 9,Red Hat Enterprise Linux 9
 # variables = sshd_approved_ciphers=aes256-gcm@openssh.com,aes256-ctr,aes128-gcm@openssh.com,aes128-ctr
 # remediation = none
 
--- linux_os/guide/system/software/integrity/crypto/harden_sshd_macs_opensshserver_conf_crypto_policy/tests/rhel9_stig_correct.pass.sh
+++ linux_os/guide/system/software/integrity/crypto/harden_sshd_macs_opensshserver_conf_crypto_policy/tests/rhel9_stig_correct.pass.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 # platform = Oracle Linux 9,Red Hat Enterprise Linux 9
-# variables = sshd_approved_ciphers=aes256-gcm@openssh.com,aes256-ctr,aes128-gcm@openssh.com,aes128-ctr
+# variables = sshd_approved_macs=hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com,hmac-sha2-512,hmac-sha2-256
 
-sshd_approved_ciphers=aes256-gcm@openssh.com,aes256-ctr,aes128-gcm@openssh.com,aes128-ctr
+sshd_approved_macs=hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com,hmac-sha2-512,hmac-sha2-256
 
 configfile=/etc/crypto-policies/back-ends/opensshserver.config
-correct_value="Ciphers ${sshd_approved_ciphers}"
+correct_value="MACs ${sshd_approved_macs}"
 
 # Ensure directory + file is there
 test -d /etc/crypto-policies/back-ends || mkdir -p /etc/crypto-policies/back-ends
 
 # Proceed when file exists
 if [[ -f $configfile ]]; then
-    sed -i -r "s/Ciphers\s+\S+/${correct_value}/" $configfile
+    sed -i -r "s/^(?!\s*#)MACs\s+\S+/${correct_value}/" $configfile
 else
     echo "${correct_value}" > "$configfile"
 fi
