1f90189dbe314af0329ada586cc1514d1f3b7ee2
Armando Acosta <armando.acosta@oracle.com>
2025-05-14 18:14:39 -0600

    Implement rpm_verify_crypto_policies
    Signed-off-by: Armando Acosta <armando.acosta@oracle.com>

---
+++ linux_os/guide/system/software/integrity/software-integrity/rpm_verification/rpm_verify_crypto_policies/bash/shared.sh
@@ -0,0 +1,7 @@
+# platform =multi_platform_ol
+
+files_with_incorrect_hash="$(rpm -V crypto-policies)"
+
+if [ -n "$files_with_incorrect_hash" ]; then
+    {{{ pkg_manager }}} reinstall -y crypto-policies
+fi
