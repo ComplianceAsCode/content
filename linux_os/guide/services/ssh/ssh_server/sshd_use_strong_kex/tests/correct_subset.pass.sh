103fa33d7e96f77693f06f0576bdf707144a6264
Miha Purg <miha.purg@canonical.com>
2025-05-26 14:32:54 +0200

    Add tests for sshd_use_strong_kex

--- linux_os/guide/services/ssh/ssh_server/sshd_use_strong_kex/tests/correct_subset.pass.sh
+++ linux_os/guide/services/ssh/ssh_server/sshd_use_strong_kex/tests/correct_subset.pass.sh
@@ -2,4 +2,5 @@
 # platform = multi_platform_all
 # variables = sshd_strong_kex=curve25519-sha256,curve25519-sha256@libssh.org,ecdh-sha2-nistp256,ecdh-sha2-nistp384,ecdh-sha2-nistp521,diffie-hellman-group-exchange-sha256,diffie-hellman-group16-sha512,diffie-hellman-group18-sha512,diffie-hellman-group14-256
 
-sed -i 's/^\s*KexAlgorithms\s/# &/i' /etc/ssh/sshd_config
+sed -i '/^\s*KexAlgorithms\s/Id' /etc/ssh/sshd_config
+echo "KexAlgorithms diffie-hellman-group16-sha512,diffie-hellman-group18-sha512" >> /etc/ssh/sshd_config
