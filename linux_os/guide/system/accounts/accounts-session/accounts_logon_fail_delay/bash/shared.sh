11fbe0004e81c5b86dd78d6eaae3ea46b1d16f80
teacup-on-rockingchair <315160+teacup-on-rockingchair@users.noreply.github.com>
2025-06-04 18:49:27 +0300

    Add fix for accounts_logon_fail_delay - /etc/login.defs moved to /usr/etc/login.defs

--- linux_os/guide/system/accounts/accounts-session/accounts_logon_fail_delay/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-session/accounts_logon_fail_delay/bash/shared.sh
@@ -1,5 +1,11 @@
-# platform = Red Hat Virtualization 4,multi_platform_ol,multi_platform_rhel,multi_platform_sle,multi_platform_slmicro5,multi_platform_almalinux
+# platform = Red Hat Virtualization 4,multi_platform_ol,multi_platform_rhel,multi_platform_sle,multi_platform_slmicro,multi_platform_almalinux
+
+{{% if product == 'slmicro6' %}}
+LOGIN_DEFS_PATH=/usr/etc/login.defs
+{{% else %}}
+LOGIN_DEFS_PATH=/etc/login.defs
+{{% endif %}}
 
 {{{ bash_instantiate_variables("var_accounts_fail_delay") }}}
 
-{{{ bash_replace_or_append('/etc/login.defs', '^FAIL_DELAY', "$var_accounts_fail_delay", '%s %s', cce_identifiers=cce_identifiers) }}}
+{{{ bash_replace_or_append("$LOGIN_DEFS_PATH", '^FAIL_DELAY', "$var_accounts_fail_delay", '%s %s', cce_identifiers=cce_identifiers) }}}
