dd7d9d8a937dbeeeb428e1194cd69c902657163d
rchikov <rumen.chikov@suse.com>
2025-06-04 10:47:20 +0200

    Fixed small bugs. Updated few rules to support SLEM 6

--- linux_os/guide/system/accounts/accounts-restrictions/password_expiration/accounts_password_set_max_life_existing/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-restrictions/password_expiration/accounts_password_set_max_life_existing/bash/shared.sh
@@ -7,7 +7,7 @@
 {{{ bash_instantiate_variables("var_accounts_maximum_age_login_defs") }}}
 
 {{% call iterate_over_command_output("i", "awk -v var=\"$var_accounts_maximum_age_login_defs\" -F: '(/^[^:]+:[^!*]/ && ($5 > var || $5 == \"\")) {print $1}' /etc/shadow") -%}}
-{{% if product in ["sle12", "sle15"] %}}
+{{% if product in ["sle12", "sle15", "slmicro6"] %}}
 passwd -q -x $var_accounts_maximum_age_login_defs $i
 {{% else %}}
 chage -M $var_accounts_maximum_age_login_defs $i
