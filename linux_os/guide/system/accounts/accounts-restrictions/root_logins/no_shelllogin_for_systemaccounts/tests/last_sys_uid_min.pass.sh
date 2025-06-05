4078344724934cebed3e9a68db23cd38c56b8ef3
teacup-on-rockingchair <315160+teacup-on-rockingchair@users.noreply.github.com>
2025-06-05 12:23:51 +0300

    Fix no_shelllogin_for_systemaccounts: /etc/login.defs replaced to /usr/etc/login.defs in OVAL check patch

--- linux_os/guide/system/accounts/accounts-restrictions/root_logins/no_shelllogin_for_systemaccounts/tests/last_sys_uid_min.pass.sh
+++ linux_os/guide/system/accounts/accounts-restrictions/root_logins/no_shelllogin_for_systemaccounts/tests/last_sys_uid_min.pass.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
+{{% if product == 'slmicro6' %}}
+LOGIN_DEFS_PATH=/usr/etc/login.defs
+{{% else %}}
+LOGIN_DEFS_PATH=/etc/login.defs
+{{% endif %}}
 
 useradd --system --shell /sbin/nologin -u 999 sysuser
 useradd -u {{{ uid_min }}} testuser
 
 key=SYS_UID_MAX
 
-printf "%s 999\n" "$key" >> /etc/login.defs
+printf "%s 999\n" "$key" >> "${LOGIN_DEFS_PATH}"
 
 key=SYS_UID_MIN
 
 # Add bogus key as 2nd last and valid last line w/o nl
-printf "%s 2000\n%s 201" "$key" "$key" >> /etc/login.defs
+printf "%s 2000\n%s 201" "$key" "$key" >> "${LOGIN_DEFS_PATH}"
--- linux_os/guide/system/accounts/accounts-restrictions/root_logins/no_shelllogin_for_systemaccounts/tests/no_sys_uids.pass.sh
+++ linux_os/guide/system/accounts/accounts-restrictions/root_logins/no_shelllogin_for_systemaccounts/tests/no_sys_uids.pass.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+{{% if product == 'slmicro6' %}}
+LOGIN_DEFS_PATH=/usr/etc/login.defs
+{{% else %}}
+LOGIN_DEFS_PATH=/etc/login.defs
+{{% endif %}}
 
 # Force unset of SYS_UID values
-sed -i '/^SYS_UID_MIN/d' /etc/login.defs
-sed -i '/^SYS_UID_MAX/d' /etc/login.defs
+sed -i '/^SYS_UID_MIN/d' "${LOGIN_DEFS_PATH}"
+sed -i '/^SYS_UID_MAX/d' "${LOGIN_DEFS_PATH}"
