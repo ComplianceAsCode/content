336905f58ce3b1710c227f67d1f33a2b41ab9a93
teacup-on-rockingchair <315160+teacup-on-rockingchair@users.noreply.github.com>
2025-06-05 12:26:07 +0300

    accounts_have_homedir_login_defs: /etc/login.defs replaced to /usr/etc/login.defs

--- linux_os/guide/system/accounts/accounts-session/accounts_have_homedir_login_defs/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-session/accounts_have_homedir_login_defs/bash/shared.sh
@@ -1,3 +1,9 @@
 # platform = multi_platform_all
 
-{{{ set_config_file("/etc/login.defs", "CREATE_HOME", "yes", create=true, insert_after="", insert_before="^\s*CREATE_HOME", insensitive=true, rule_id=rule_id) }}}
+{{% if product == 'slmicro6' %}}
+LOGIN_DEFS_PATH=/usr/etc/login.defs
+{{% else %}}
+LOGIN_DEFS_PATH=/etc/login.defs
+{{% endif %}}
+
+{{{ set_config_file("$LOGIN_DEFS_PATH", "CREATE_HOME", "yes", create=true, insert_after="", insert_before="^\s*CREATE_HOME", insensitive=true, rule_id=rule_id) }}}
