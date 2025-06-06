cde82c94f39d11c38a2b93d8511be3cfbe03554a
svet-se <svetlin.boychev@suse.com>
2025-06-06 17:41:36 +0300

    Update rule install_smartcard_packages to support slmicro6

---
+++ linux_os/guide/system/accounts/accounts-physical/screen_locking/smart_card_login/install_smartcard_packages/bash/slmicro6.sh
@@ -0,0 +1,12 @@
+# platform = multi_platform_slmicro
+# reboot = false
+# strategy = enable
+# complexity = low
+# disruption = low
+
+SMARTCARD_PACKAGES=( "pam_pkcs11"  "mozilla-nss"  "mozilla-nss-tools"  "pcsc-ccid"  "pcsc-lite"  "pcsc-tools"  "opensc" "coolkey")
+
+for PKGNAME in "${SMARTCARD_PACKAGES[@]}"
+do
+    {{{ bash_package_install(package="$PKGNAME") }}}
+done
