6c0b964bbd9c0dd032d48f230c5362f2e1c41a4b
teacup-on-rockingchair <315160+teacup-on-rockingchair@users.noreply.github.com>
2025-06-10 12:23:13 +0300

    Move login_defs_path to use /usr/etc/login.defs for slmicro6 for set_password_hashing_algorithm_logindefs rule

--- linux_os/guide/system/accounts/accounts-pam/set_password_hashing_algorithm/set_password_hashing_algorithm_logindefs/tests/default_sha256.fail.sh
+++ linux_os/guide/system/accounts/accounts-pam/set_password_hashing_algorithm/set_password_hashing_algorithm_logindefs/tests/default_sha256.fail.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 # variables = var_password_hashing_algorithm=SHA512
 
+{{% if product == 'slmicro6' %}}
+LOGIN_DEFS_PATH=/usr/etc/login.defs
+{{% else %}}
+LOGIN_DEFS_PATH=/etc/login.defs
+{{% endif %}}
+
 # Make sure ENCRYPT_METHOD is SHA256
-if grep -q "^ENCRYPT_METHOD" /etc/login.defs; then
-	sed -i "s/^ENCRYPT_METHOD\b.*/ENCRYPT_METHOD SHA256/" /etc/login.defs
+if grep -q "^ENCRYPT_METHOD" "$LOGIN_DEFS_PATH"; then
+	sed -i "s/^ENCRYPT_METHOD\b.*/ENCRYPT_METHOD SHA256/" "$LOGIN_DEFS_PATH"
 else
-	echo "ENCRYPT_METHOD SHA256" >> /etc/login.defs
+	echo "ENCRYPT_METHOD SHA256" >> "$LOGIN_DEFS_PATH"
 fi
