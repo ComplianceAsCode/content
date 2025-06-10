282b560f6b4da9eb4bf5e039bce2ab51c4856fdc
teacup-on-rockingchair <315160+teacup-on-rockingchair@users.noreply.github.com>
2025-06-10 12:23:13 +0300

    Move login_defs_path to use /usr/etc/login.defs for slmicro6 for set_password_hashing_algorithm_logindefs rule

--- linux_os/guide/system/accounts/accounts-pam/set_password_hashing_algorithm/set_password_hashing_algorithm_logindefs/tests/default_multivalue.pass.sh
+++ linux_os/guide/system/accounts/accounts-pam/set_password_hashing_algorithm/set_password_hashing_algorithm_logindefs/tests/default_multivalue.pass.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 # variables = var_password_hashing_algorithm=good_value1|good_value2
 
-if grep -q "^ENCRYPT_METHOD" /etc/login.defs; then
-	sed -i "s/^ENCRYPT_METHOD\b.*/ENCRYPT_METHOD good_value2/" /etc/login.defs
+{{% if product == 'slmicro6' %}}
+LOGIN_DEFS_PATH=/usr/etc/login.defs
+{{% else %}}
+LOGIN_DEFS_PATH=/etc/login.defs
+{{% endif %}}
+
+if grep -q "^ENCRYPT_METHOD" "$LOGIN_DEFS_PATH"; then
+	sed -i "s/^ENCRYPT_METHOD\b.*/ENCRYPT_METHOD good_value2/" "$LOGIN_DEFS_PATH"
 else
-	echo "ENCRYPT_METHOD good_value2" >> /etc/login.defs
+	echo "ENCRYPT_METHOD good_value2" >> "$LOGIN_DEFS_PATH"
 fi
--- linux_os/guide/system/accounts/accounts-pam/set_password_hashing_algorithm/set_password_hashing_algorithm_logindefs/tests/default_partial.fail.sh
+++ linux_os/guide/system/accounts/accounts-pam/set_password_hashing_algorithm/set_password_hashing_algorithm_logindefs/tests/default_partial.fail.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 # variables = var_password_hashing_algorithm=value1|value2
 
+{{% if product == 'slmicro6' %}}
+LOGIN_DEFS_PATH=/usr/etc/login.defs
+{{% else %}}
+LOGIN_DEFS_PATH=/etc/login.defs
+{{% endif %}}
+
 # test that partial match fails
-if grep -q "^ENCRYPT_METHOD" /etc/login.defs; then
-	sed -i "s/^ENCRYPT_METHOD\b.*/ENCRYPT_METHOD value/" /etc/login.defs
+if grep -q "^ENCRYPT_METHOD" "$LOGIN_DEFS_PATH"; then
+	sed -i "s/^ENCRYPT_METHOD\b.*/ENCRYPT_METHOD value/" "$LOGIN_DEFS_PATH"
 else
-	echo "ENCRYPT_METHOD value" >> /etc/login.defs
+	echo "ENCRYPT_METHOD value" >> "$LOGIN_DEFS_PATH"
 fi
