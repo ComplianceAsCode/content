f4b3ccec32eb74ee9cbf2208cf030ce293dc2b07
Eric Berry <eric.berry@canonical.com>
2025-06-17 14:59:16 -0700

    Ubuntu 24.04 STIG rule UBTU-24-100860

--- linux_os/guide/services/ssh/ssh_client/ssh_use_approved_macs_ordered_stig/bash/shared.sh
+++ linux_os/guide/services/ssh/ssh_client/ssh_use_approved_macs_ordered_stig/bash/shared.sh
@@ -1,3 +1,14 @@
 # platform = multi_platform_ubuntu
 
-{{{ bash_ssh_remediation(parameter="MACs", value="$ssh_approved_macs", config_is_distributed=ssh_distributed_config, rule_id=rule_id) }}}
+{{{ bash_instantiate_variables("ssh_approved_macs") }}}
+
+main_config="/etc/ssh/ssh_config"
+include_directory="/etc/ssh/ssh_config.d"
+
+sed -i '/^\s*MACs.*/d' "$main_config" "$include_directory"/*.conf || true
+
+if ! grep -qE '^[Hh]ost\s+\*$' /etc/ssh/ssh_config.d/00-mac-list.conf; then
+  echo 'Host *' >> /etc/ssh/ssh_config.d/00-mac-list.conf
+fi
+
+{{{ set_config_file(path="/etc/ssh/ssh_config.d/00-mac-list.conf", parameter="MACs", value='$ssh_approved_macs', create=true, insert_before="", insert_after="^Host\s+\*$", insensitive=false, separator=" ", separator_regex="\s\+", prefix_regex="^\s*", rule_id=rule_id) }}}
