b3a383d8d4f034ba491406a7af7a8db6a6d001d1
Jan Černý <jcerny@redhat.com>
2025-06-30 09:21:06 +0200

    Workaround OpenSCAP issue for Image Mode
    Use full path for some commands in /usr/sbin in Bash
    remediations.
    
    This is a workaround for OpenSCAP issue:
    https://github.com/OpenSCAP/openscap/issues/2242
    A proper fix would be in OpenSCAP, but it's less likely
    to update OpenSCAP in downstream than to update the content.
    
    Effectively, this change will fix some rules that fail when
    building a hardened bootable container image:
    https://github.com/ComplianceAsCode/content/issues/13550
    https://github.com/ComplianceAsCode/content/issues/13551
    https://github.com/ComplianceAsCode/content/issues/13552
    
    In future, this problem will be smaller, because starting from
    Fedora 42, /usr/sbin is a symlink to /usr/bin, see:
    https://fedoraproject.org/wiki/Changes/Unify_bin_and_sbin

--- linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/account_password_selinux_faillock_dir/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/account_password_selinux_faillock_dir/bash/shared.sh
@@ -4,15 +4,18 @@ FAILLOCK_CONF_FILES="/etc/security/faillock.conf /etc/pam.d/system-auth /etc/pam
 faillock_dirs=$(grep -oP "^\s*(?:auth.*pam_faillock.so.*)?dir\s*=\s*(\S+)" $FAILLOCK_CONF_FILES \
                | sed -r 's/.*=\s*(\S+)/\1/')
 
+# Workaround for https://github.com/OpenSCAP/openscap/issues/2242: Use full
+# path to semanage and restorecon commands to avoid the issue with the command
+# not being found.
 if [ -n "$faillock_dirs" ]; then
     for dir in $faillock_dirs; do
-        if ! semanage fcontext -a -t faillog_t "$dir(/.*)?"; then
-            semanage fcontext -m -t faillog_t "$dir(/.*)?"
+        if ! /usr/sbin/semanage fcontext -a -t faillog_t "$dir(/.*)?"; then
+            /usr/sbin/semanage fcontext -m -t faillog_t "$dir(/.*)?"
         fi
         if [ ! -e $dir ]; then
             mkdir -p $dir
         fi
-        restorecon -R -v $dir
+        /usr/sbin/restorecon -R -v $dir
     done
 else
 echo "
