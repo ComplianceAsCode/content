df649d6100228c8f88d9c5c108a43edf22d246ba
Eric Berry <eric.berry@canonical.com>
2025-07-01 15:01:15 -0700

    Add distributed config support

---
+++ linux_os/guide/services/ssh/ssh_server/sshd_use_strong_ciphers/tests/dist_good_cipher.pass.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# platform = multi_platform_fedora,multi_platform_ol,multi_platform_rhel,multi_platform_almalinux, multi_platform_ubuntu
+
+{{%- if sshd_distributed_config == "true" %}}
+mkdir -p /etc/ssh/sshd_config.d
+touch /etc/ssh/sshd_config.d/test.conf
+sed -i 's/^\s*Ciphers\s.*//i' /etc/ssh/sshd_config.d/test.conf
+echo "Ciphers aes256-ctr" >> /etc/ssh/sshd_config.d/test.conf
+{{% endif %}}
+
--- linux_os/guide/services/ssh/ssh_server/sshd_use_strong_ciphers/tests/no_ciphers.fail.sh
+++ linux_os/guide/services/ssh/ssh_server/sshd_use_strong_ciphers/tests/no_ciphers.fail.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# platform = multi_platform_fedora,multi_platform_ol,multi_platform_rhel,multi_platform_almalinux
+# platform = multi_platform_fedora,multi_platform_ol,multi_platform_rhel,multi_platform_almalinux, multi_platform_ubuntu
 
 sed -i 's/^\s*Ciphers\s/# &/i' /etc/ssh/sshd_config
