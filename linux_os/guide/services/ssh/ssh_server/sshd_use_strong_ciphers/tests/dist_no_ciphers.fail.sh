f164b9b4d37f35cf0bc95cdcb50a524771046f6b
ericeberry <ericeberry@gmail.com>
2025-07-02 09:24:13 -0700

    Apply suggestions from code review
    Co-authored-by: Matthew Burket <m@tthewburket.com>

--- linux_os/guide/services/ssh/ssh_server/sshd_use_strong_ciphers/tests/dist_no_ciphers.fail.sh
+++ linux_os/guide/services/ssh/ssh_server/sshd_use_strong_ciphers/tests/dist_no_ciphers.fail.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# platform = multi_platform_fedora,multi_platform_ol,multi_platform_rhel,multi_platform_almalinux, multi_platform_ubuntu
+# platform = multi_platform_fedora,multi_platform_ol,multi_platform_rhel,multi_platform_almalinux,multi_platform_ubuntu
 
 {{%- if sshd_distributed_config == "true" %}}
 mkdir -p /etc/ssh/sshd_config.d
