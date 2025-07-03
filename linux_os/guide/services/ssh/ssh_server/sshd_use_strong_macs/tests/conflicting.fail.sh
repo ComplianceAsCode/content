de9bc48bd519062eec04a4f0c518de99f6e188fb
Alan Moore <alan.moore@canonical.com>
2025-07-03 09:39:25 +0100

    Do no comment

--- linux_os/guide/services/ssh/ssh_server/sshd_use_strong_macs/tests/conflicting.fail.sh
+++ linux_os/guide/services/ssh/ssh_server/sshd_use_strong_macs/tests/conflicting.fail.sh
@@ -3,6 +3,6 @@
 # variables = sshd_strong_macs=hmac-sha2-256-etm@openssh.com,hmac-sha2-512-etm@openssh.com,hmac-sha2-256,hmac-sha2-512
 
 sed -i '/^\s*MACs\s/Id' /etc/ssh/sshd_config /etc/ssh/sshd_config.d/* || true
-echo "#MACs hmac-sha2-256-etm@openssh.com,hmac-sha2-512-etm@openssh.com,hmac-sha2-256,hmac-sha2-512" >> /etc/ssh/sshd_config
+echo "MACs hmac-sha2-256-etm@openssh.com,hmac-sha2-512-etm@openssh.com,hmac-sha2-256,hmac-sha2-512" >> /etc/ssh/sshd_config
 
 echo "MACs hmac-sha1" >> /etc/ssh/sshd_config
