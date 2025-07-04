066ce4af5bc5dee4a75fa9aa7b1adbe08c5ebd61
Alan Moore <alan.moore@canonical.com>
2025-07-04 15:08:49 +0100

    Exclude system group with gid < 1000

--- linux_os/guide/system/permissions/files/permissions_within_important_dirs/file_groupownership_system_commands_dirs/bash/ubuntu.sh
+++ linux_os/guide/system/permissions/files/permissions_within_important_dirs/file_groupownership_system_commands_dirs/bash/ubuntu.sh
@@ -1,11 +1,11 @@
 # platform = multi_platform_ubuntu
 
 {{% if product in ["ubuntu2404"] %}}
-find -L /bin/ /sbin/ /usr/bin/ /usr/sbin/ /usr/local/bin/ /usr/local/sbin/ -maxdepth 1 -type f  ! -group root ! -group daemon ! -group adm ! -group shadow ! -group mail ! -group crontab ! -group _ssh -regextype posix-extended -regex '.*' -exec chgrp -L root {} \;
+find -L /bin/ /sbin/ /usr/bin/ /usr/sbin/ /usr/local/bin/ /usr/local/sbin/ -maxdepth 1 -type f  \! -gid -{{{ gid_min }}} -regextype posix-extended -regex '.*' -exec chgrp -L root {} \;
 
 {{% else %}}
 for SYSCMDFILES in /bin /sbin /usr/bin /usr/sbin /usr/local/bin /usr/local/sbin
 do
-   find -L $SYSCMDFILES ! -group root -type f ! -perm /2000 -exec chgrp root '{}' \;
+   find -L $SYSCMDFILES \! -gid -{{{ gid_min }}} -type f ! -perm /2000 -exec chgrp root '{}' \;
 done
 {{% endif %}}
