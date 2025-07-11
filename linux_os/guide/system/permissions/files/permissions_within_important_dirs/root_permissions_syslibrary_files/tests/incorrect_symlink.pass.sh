014d5e6fc7ea8c79ce88d6660f2fa70d82dafd5c
Alan Moore <alan.moore@canonical.com>
2025-07-11 17:09:04 +0100

    Disable symbolic test for non-Ubuntu distro

--- linux_os/guide/system/permissions/files/permissions_within_important_dirs/root_permissions_syslibrary_files/tests/incorrect_symlink.pass.sh
+++ linux_os/guide/system/permissions/files/permissions_within_important_dirs/root_permissions_syslibrary_files/tests/incorrect_symlink.pass.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# platform = multi_platform_fedora,multi_platform_ol,multi_platform_rhel,multi_platform_sle,multi_platform_ubuntu,multi_platform_almalinux
+# platform = multi_platform_ubuntu
 
 groupadd group_test
 
