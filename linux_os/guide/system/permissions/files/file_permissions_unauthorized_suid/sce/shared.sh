236a504231532da32c493948b0024230acf907b7
Gabriel Becker <ggasparb@redhat.com>
2025-08-01 16:55:53 +0200

    Add SCE content for rules that traverse wholefilesystem.

---
+++ linux_os/guide/system/permissions/files/file_permissions_unauthorized_suid/sce/shared.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# platform = multi_platform_rhel
+# check-import = stdout
+
+{{{ find_files(find_parameters="-perm -4000", fail_message="Found SUID executables that are unauthorized") }}}
