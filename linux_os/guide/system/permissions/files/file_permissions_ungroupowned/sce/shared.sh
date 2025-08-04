8d437c0c49c784a536720a1b11dc1419d41f48fa
Gabriel Becker <ggasparb@redhat.com>
2025-08-04 10:58:42 +0200

    Add new parameter to SCE content to exclude certain directories from search.

--- linux_os/guide/system/permissions/files/file_permissions_ungroupowned/sce/shared.sh
+++ linux_os/guide/system/permissions/files/file_permissions_ungroupowned/sce/shared.sh
@@ -2,4 +2,4 @@
 # platform = multi_platform_rhel, Ubuntu 24.04
 # check-import = stdout
 
-{{{ find_files(find_parameters="-nogroup", fail_message="Found ungroupowned files") }}}
+{{{ find_files(find_parameters="-nogroup", fail_message="Found ungroupowned files", exclude_directories="sysroot") }}}
