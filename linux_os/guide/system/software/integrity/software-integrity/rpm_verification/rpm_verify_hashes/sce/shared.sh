dff64390ccae973bae4c5d2461e5fcb096719a53
Gabriel Becker <ggasparb@redhat.com>
2025-08-06 10:01:32 +0200

    Add SCE content for RPM verify rules.

---
+++ linux_os/guide/system/software/integrity/software-integrity/rpm_verification/rpm_verify_hashes/sce/shared.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# platform = multi_platform_rhel
+# check-import = stdout
+
+readarray -t FILES_WITH_INCORRECT_HASHES < <(rpm -Va --noconfig | grep -E '^..5' | awk '{print $NF}' )
+
+if (( ${#FILES_WITH_INCORRECT_HASHES[@]} > 0 )); then
+    echo "Files with incorrect hashes:\n"
+    printf '%s\n' "${FILES_WITH_INCORRECT_HASHES[@]}"
+    exit "${XCCDF_RESULT_FAIL}"
+fi
+
+exit "${XCCDF_RESULT_PASS}"
