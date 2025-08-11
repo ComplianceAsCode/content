0843e82e47d1c7de0f4051c429d59172cfb8b732
teacup-on-rockingchair <315160+teacup-on-rockingchair@users.noreply.github.com>
2025-08-11 12:13:56 +0300

    Update linux_os/guide/system/accounts/accounts-session/accounts_user_dot_group_ownership/tests/home_dirs_one_absent_group_ok.pass.sh
    Co-authored-by: Jan Černý <jcerny@redhat.com>

--- linux_os/guide/system/accounts/accounts-session/accounts_user_dot_group_ownership/tests/home_dirs_one_absent_group_ok.pass.sh
+++ linux_os/guide/system/accounts/accounts-session/accounts_user_dot_group_ownership/tests/home_dirs_one_absent_group_ok.pass.sh
@@ -7,5 +7,5 @@ useradd -m $USER1
 useradd -M $USER2
 
 touch /home/$USER1/.bashrc
-GROUP=$(id $USER -g)
-chgrp -f $GROUP /home/$USER/.bashrc
+GROUP=$(id $USER1 -g)
+chgrp -f $GROUP /home/$USER1/.bashrc
