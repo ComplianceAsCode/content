b4b960dd4203d11bf9b04b3a64452fa739cac4e4
Matthew Burket <mburket@redhat.com>
2025-08-20 07:58:14 -0500

    Adjust variables in fips_custom_stig_sub_policy

--- linux_os/guide/system/software/integrity/fips/fips_custom_stig_sub_policy/bash/shared.sh
+++ linux_os/guide/system/software/integrity/fips/fips_custom_stig_sub_policy/bash/shared.sh
@@ -4,10 +4,7 @@
 # complexity = low
 # disruption = low
 
-{{%- set ns = namespace(contents="") %}}
+{{%- set contents = "cipher@SSH=AES-256-GCM AES-256-CTR AES-128-GCM AES-128-CTR\nmac@SSH=HMAC-SHA2-512 HMAC-SHA2-256" -%}}
 
 
-{{%- set ns.contents = "cipher@SSH=AES-256-GCM AES-256-CTR AES-128-GCM AES-128-CTR\nmac@SSH=HMAC-SHA2-512 HMAC-SHA2-256" -%}}
-
-
-{{{ bash_file_contents('/etc/crypto-policies/policies/modules/STIG.pmod', ns.contents) }}}
+{{{ bash_file_contents('/etc/crypto-policies/policies/modules/STIG.pmod', contents) }}}
