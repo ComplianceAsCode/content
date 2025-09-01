38da6984dd25ea94edf024ed0b71429078b3cef5
vojtapolasek <krecoun@gmail.com>
2025-09-01 15:24:36 +0200

    auditd_lineinfile template: allow specifying data type of the XCCDF variable

--- shared/templates/auditd_lineinfile/template.py
+++ shared/templates/auditd_lineinfile/template.py
@@ -10,15 +10,20 @@ def preprocess(data, lang):
         raise ValueError(errmsg)
     data["missing_parameter_pass"] = parse_template_boolean_value(
         data, parameter="missing_parameter_pass", default_value=False)
+    if "variable_datatype" not in data:
+        data["variable_datatype"] = "string"
     return set_variables_for_test_scenarios(data)
 
 
 def set_variables_for_test_scenarios(data):
-    if not data.get("value"):
-        # this implies XCCDF variable is used
-        data["wrong_value"] = "wrong_value"
-        data["correct_value"] = "correct_value"
-    else:
-        data["wrong_value"] = "wrong_value"
-        data["correct_value"] = str(data["value"])
+    # if no correct value is specified, we will create one for testing purposes
+    if not data.get("test_correct_value"):
+        if not data.get("value"):
+            # this implies XCCDF variable is used
+            data["test_correct_value"] = "test_correct_value"
+        else:
+            data["test_correct_value"] = str(data["value"])
+    # if no wrong value is provided, we will create one for testing purposes
+    if not data.get("test_wrong_value"):
+        data["test_wrong_value"] = "test_wrong_value"
     return data
