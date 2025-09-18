ed8a8c70d4e51f348b67068f910fcd82c8cf22ec
Armando Acosta <armando.acosta@oracle.com>
2025-09-18 14:59:31 -0600

    Update root_permissions_syslibrary_files to check .so files for OL8
    Signed-off-by: Armando Acosta <armando.acosta@oracle.com>

--- linux_os/guide/system/permissions/files/permissions_within_important_dirs/root_permissions_syslibrary_files/tests/incorrect_groupowner.fail.sh
+++ linux_os/guide/system/permissions/files/permissions_within_important_dirs/root_permissions_syslibrary_files/tests/incorrect_groupowner.fail.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-# platform = multi_platform_sle,multi_platform_rhel,multi_platform_fedora,multi_platform_ubuntu
+# platform = multi_platform_sle,multi_platform_rhel,multi_platform_fedora,multi_platform_ubuntu,multi_platform_ol
 
 groupadd group_test
+{{% if 'ol8' in product %}}
+for TESTFILE in /lib/test_me.so /lib64/test_me.so /usr/lib/test_me.so /usr/lib64/test_me.so
+{{% else %}}
 for TESTFILE in /lib/test_me /lib64/test_me /usr/lib/test_me /usr/lib64/test_me
+{{% endif %}}
 do
    if [[ ! -f $TESTFILE ]]
    then
