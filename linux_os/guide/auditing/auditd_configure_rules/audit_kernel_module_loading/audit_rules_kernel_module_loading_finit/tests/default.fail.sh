0af380be9c4c9905d7d66ec23f674a3ee389c3e4
Jan Černý <jcerny@redhat.com>
2025-10-16 14:47:50 +0200

    Create templated test scenarios
    Create templated test scenarios for the
    audit_rules_kernel_module_loading template, based on existing scenarios
    in rule audit_rules_kernel_module_loading_delete.  Remove some per-rule
    test scenarios to prevent duplication.

--- linux_os/guide/auditing/auditd_configure_rules/audit_kernel_module_loading/audit_rules_kernel_module_loading_finit/tests/default.fail.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# remediation = bash
-# packages = audit
-
-rm -f /etc/audit/rules.d/*
-> /etc/audit/audit.rules
---
+++ shared/templates/audit_rules_kernel_module_loading/tests/missing_auid_filter.fail.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# platform = Oracle Linux 7,Oracle Linux 8,multi_platform_rhel,multi_platform_ubuntu
+# packages = audit
+
+rm -f /etc/audit/rules.d/*
+
+echo "-a always,exit -F arch=b32 -S {{{ NAME }}} -F key=modules" >> /etc/audit/rules.d/modules.rules
+echo "-a always,exit -F arch=b64 -S {{{ NAME }}} -F key=modules" >> /etc/audit/rules.d/modules.rules
