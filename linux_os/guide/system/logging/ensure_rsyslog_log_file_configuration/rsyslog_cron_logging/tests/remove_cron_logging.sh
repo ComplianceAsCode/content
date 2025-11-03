75d7c5c5fa1b9101274d7b7809d92858e66d90ee
Arden97 <arden2545@gmail.com>
2025-11-03 11:24:25 +0100

    add newline and fix a commit message

--- linux_os/guide/system/logging/ensure_rsyslog_log_file_configuration/rsyslog_cron_logging/tests/remove_cron_logging.sh
+++ linux_os/guide/system/logging/ensure_rsyslog_log_file_configuration/rsyslog_cron_logging/tests/remove_cron_logging.sh
@@ -5,7 +5,7 @@ function remove_cron_logging {
 	# remove all multilined cron.* entries
 	sed -i '/^[[:space:]]*cron\.\*.*action(/,/)/d' /etc/rsyslog.conf
 	find /etc/rsyslog.d -type f -name "*.conf" -exec sed -i '/^[[:space:]]*cron\.\*.*action(/,/)/d' {} +
-	# remove all legacy format cron.* entries
+	# remove all legacy format and one line cron.* entries
 	sed -i '/^[[:space:]]*cron\.\*/d' /etc/rsyslog.conf
 	find /etc/rsyslog.d -type f -name "*.conf" -exec sed -i '/^[[:space:]]*cron\.\*/d' {} +
 }
