21f1ae7deedc6083fdd938c53e8818b3cf843c73
Arden97 <arden2545@gmail.com>
2025-11-05 14:53:40 +0100

    fixing typo in default_no_pass.fail.sh tests

--- linux_os/guide/system/logging/ensure_rsyslog_log_file_configuration/rsyslog_encrypt_offload_actionsendstreamdriverauthmode/tests/default_no_pass.fail.sh
+++ linux_os/guide/system/logging/ensure_rsyslog_log_file_configuration/rsyslog_encrypt_offload_actionsendstreamdriverauthmode/tests/default_no_pass.fail.sh
@@ -3,6 +3,6 @@
 {{{ setup_rsyslog_encrypt_offload_actionsendstreamdriverauthmode() }}}
 
 if [[ -f $RSYSLOG_D_CONF ]]; then
-  sed -i "/^\$ActionSendStreamDriverMod.*/d" $RSYSLOG_D_CONF
+  sed -i "/^\$ActionSendStreamDriverAuthMode.*/d" $RSYSLOG_D_CONF
 fi
-  sed -i "/^\$ActionSendStreamDriverMod.*/d" $RSYSLOG_CONF
+  sed -i "/^\$ActionSendStreamDriverAuthMode.*/d" $RSYSLOG_CONF
--- linux_os/guide/system/logging/ensure_rsyslog_log_file_configuration/rsyslog_encrypt_offload_actionsendstreamdrivermode/tests/default_no_pass.fail.sh
+++ linux_os/guide/system/logging/ensure_rsyslog_log_file_configuration/rsyslog_encrypt_offload_actionsendstreamdrivermode/tests/default_no_pass.fail.sh
@@ -3,6 +3,6 @@
 {{{ setup_rsyslog_encrypt_offload_actionsendstreamdrivermode() }}}
 
 if [[ -f $RSYSLOG_D_CONF ]]; then
-  sed -i i/\$ActionSendStreamDriverMod//g $RSYSLOG_D_CONF
+  sed -i i/\$ActionSendStreamDriverMode//g $RSYSLOG_D_CONF
 fi
-  sed -i i/\$ActionSendStreamDriverMod//g $RSYSLOG_CONF
+  sed -i i/\$ActionSendStreamDriverMode//g $RSYSLOG_CONF
