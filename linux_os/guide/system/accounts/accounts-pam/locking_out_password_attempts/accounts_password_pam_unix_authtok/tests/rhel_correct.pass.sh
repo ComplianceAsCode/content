0fac9eee1634990afc6ef46b2d3ff7d1e953b490
Jan Černý <jcerny@redhat.com>
2025-11-10 11:35:01 +0100

    Configure use_authtok in RHEL 10 CIS
    Add rules `accounts_password_pam_pwhistory_use_authtok` and
    `accounts_password_pam_unix_authtok` to RHEL 10 CIS profiles
    and improve their OVAL, Bash and Ansible to work on RHEL 10.
    This change aligns our RHEL 10 content with RHEL 10 CIS Benchmark
    v1.0.1.
    
    Resolves: https://issues.redhat.com/browse/OPENSCAP-6098

--- linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_password_pam_unix_authtok/tests/rhel_correct.pass.sh
+++ linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_password_pam_unix_authtok/tests/rhel_correct.pass.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # platform = multi_platform_rhel
 {{{ bash_ensure_pam_module_option("/etc/pam.d/system-auth", "password", "sufficient", "pam_unix.so", "use_authtok") }}}
 {{{ bash_ensure_pam_module_option("/etc/pam.d/password-auth", "password", "sufficient", "pam_unix.so", "use_authtok") }}}
