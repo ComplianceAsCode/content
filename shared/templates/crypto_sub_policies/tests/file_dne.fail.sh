90bf0a286f3c571e1cc232ef653a695442aa617d
Jan Černý <jcerny@redhat.com>
2025-11-12 11:53:39 +0100

    Add packages directive to test scenarios

--- shared/templates/crypto_sub_policies/tests/file_dne.fail.sh
+++ shared/templates/crypto_sub_policies/tests/file_dne.fail.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# packages = crypto-policies-scripts
 {{% for sub_policy in SUB_POLICIES %}}
 if [[ -f /etc/crypto-policies/policies/modules/{{{ sub_policy.module_name }}}.pmod ]] ; then
   rm /etc/crypto-policies/policies/modules/{{{ sub_policy.module_name }}}.pmod
