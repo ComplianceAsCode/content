e0f7abcc4bbc22207fb28c1b125060cf1d64c536
teacup-on-rockingchair <315160+teacup-on-rockingchair@users.noreply.github.com>
2025-11-21 07:29:16 +0200

    Fix dconf_gnome_screensaver_lock_delay rule tests and remediations for sle15 and sle16 platforms

--- linux_os/guide/system/software/gnome/gnome_screen_locking/dconf_gnome_screensaver_lock_delay/bash/shared.sh
+++ linux_os/guide/system/software/gnome/gnome_screen_locking/dconf_gnome_screensaver_lock_delay/bash/shared.sh
@@ -6,6 +6,11 @@
 {{{ bash_dconf_lock("org/gnome/desktop/screensaver", "lock-delay", "local.d", "00-security-settings-lock") }}}
 {{% endif %}}
 
+# apply fix for enable_dconf_user_profile, OVAL checks it
+{{% if product in ['sle15', 'sle16']%}}
+{{{ bash_enable_dconf_user_profile(profile="gdm", database="gdm") }}}
+{{% endif %}}
+
 {{{ bash_instantiate_variables("var_screensaver_lock_delay") }}}
 
 {{{ bash_dconf_settings("org/gnome/desktop/screensaver", "lock-delay", "uint32 ${var_screensaver_lock_delay}", "local.d", "00-security-settings", rule_id=rule_id) }}}
