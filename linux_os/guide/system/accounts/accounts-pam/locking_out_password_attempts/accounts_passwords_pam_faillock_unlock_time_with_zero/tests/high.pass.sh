0fdbb27cf07a0f2cef77f20a88c62af6273a6535
Jan Černý <jcerny@redhat.com>
2025-11-25 17:59:51 +0100

    Add new rule accounts_passwords_pam_faillock_unlock_time_with_zero
    Add new rule `accounts_passwords_pam_faillock_unlock_time_with_zero`
    which is almost the same as
    `accounts_passwords_pam_faillock_unlock_time`, but it allows the
    `unlock_time` faillock parameter to be set either to zero or to
    value greater than the value of the
    `var_accounts_passwords_pam_faillock_unlock_time` variable.
    By allowing also 0, we better align with the requirement
    5.3.2.1.2 of the RHEL 10 CIS Benchmark v1.0.1.
    
    Resolves: https://issues.redhat.com/browse/OPENSCAP-6095

---
+++ linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_passwords_pam_faillock_unlock_time_with_zero/tests/high.pass.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# variables = var_accounts_passwords_pam_faillock_unlock_time=900
+echo "unlock_time = 1000" > "{{{ pam_faillock_conf_path }}}"
+authselect enable-feature with-faillock
+authselect apply-changes
