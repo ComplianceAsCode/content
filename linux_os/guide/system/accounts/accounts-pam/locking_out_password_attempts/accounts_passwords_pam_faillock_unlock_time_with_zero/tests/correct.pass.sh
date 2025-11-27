1619a7f2a52847b15d6ff1d1bf544dc2c74ab7ff
Jan Černý <jcerny@redhat.com>
2025-11-27 16:29:36 +0100

    Fix shebangs

--- linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_passwords_pam_faillock_unlock_time_with_zero/tests/correct.pass.sh
+++ linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_passwords_pam_faillock_unlock_time_with_zero/tests/correct.pass.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # variables = var_accounts_passwords_pam_faillock_unlock_time=900
 echo "unlock_time = 900" > "{{{ pam_faillock_conf_path }}}"
 authselect enable-feature with-faillock
--- linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_passwords_pam_faillock_unlock_time_with_zero/tests/zero.pass.sh
+++ linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_passwords_pam_faillock_unlock_time_with_zero/tests/zero.pass.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 echo "unlock_time = 0" > "{{{ pam_faillock_conf_path }}}"
 authselect enable-feature with-faillock
 authselect apply-changes
