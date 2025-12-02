bd401aa7f0e7ba4af51cb1a93e62d1d3ab3afda0
Gabriel Becker <ggasparb@redhat.com>
2025-12-02 17:10:03 +0100

    Extend mount_option template to support excluding certain type of filesystems.

---
+++ linux_os/guide/system/permissions/partitions/mount_option_boot_efi_nosuid/tests/ext2_without_nosuid.fail.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# remediation = none
+
+# This test verifies that the rule fails when /boot/efi is mounted with ext2 filesystem
+# without nosuid option
+
+. $SHARED/partition.sh
+
+clean_up_partition /boot/efi
+
+# Create an ext2 partition
+create_partition
+
+# Add to fstab without nosuid option
+mkdir -p /boot/efi
+make_fstab_given_partition_line /boot/efi ext2 rw
+
+# Mount the partition
+mount /boot/efi
