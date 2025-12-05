da7fc3aac5ded458f022c6d1ad9b6c43efece546
Vojtech Polasek <vpolasek@redhat.com>
2025-12-05 12:52:18 +0100

    RHEL test scenarios: cover cases when Authselect is present
    make scenarios applicable only for RHEL and Fedora

--- linux_os/guide/system/accounts/accounts-restrictions/password_storage/accounts_password_pam_unix_no_remember/tests/no_remember_rhel.pass.sh
+++ linux_os/guide/system/accounts/accounts-restrictions/password_storage/accounts_password_pam_unix_no_remember/tests/no_remember_rhel.pass.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
-# platform = multi_platform_rhel,multi_platform_fedora,multi_platform_ol,multi_platform_almalinux
+# platform = multi_platform_rhel,multi_platform_fedora
 # packages = pam
 
+authselect create-profile hardening -b sssd
+CUSTOM_PROFILE="custom/hardening"
+authselect select $CUSTOM_PROFILE --force
+CUSTOM_PASSWORD_AUTH="/etc/authselect/$CUSTOM_PROFILE/password-auth"
+CUSTOM_SYSTEM_AUTH="/etc/authselect/$CUSTOM_PROFILE/system-auth"
+
 # Ensure system-auth and password-auth don't have remember option
-sed -i --follow-symlinks '/pam_unix\.so.*remember=/d' /etc/pam.d/system-auth
-sed -i --follow-symlinks '/pam_unix\.so.*remember=/d' /etc/pam.d/password-auth
+sed -i --follow-symlinks '/pam_unix\.so.*remember=/d' $CUSTOM_SYSTEM_AUTH
+sed -i --follow-symlinks '/pam_unix\.so.*remember=/d' $CUSTOM_PASSWORD_AUTH
 
 # Add a clean pam_unix.so line without remember if it doesn't exist
-if ! grep -q "^password.*pam_unix\.so" /etc/pam.d/system-auth; then
-    echo "password    sufficient    pam_unix.so sha512 shadow try_first_pass use_authtok" >> /etc/pam.d/system-auth
+if ! grep -q "^password.*pam_unix\.so" $CUSTOM_SYSTEM_AUTH; then
+    echo "password    sufficient    pam_unix.so sha512 shadow try_first_pass use_authtok" >> $CUSTOM_SYSTEM_AUTH
 fi
 
-if ! grep -q "^password.*pam_unix\.so" /etc/pam.d/password-auth; then
-    echo "password    sufficient    pam_unix.so sha512 shadow try_first_pass use_authtok" >> /etc/pam.d/password-auth
+if ! grep -q "^password.*pam_unix\.so" $CUSTOM_PASSWORD_AUTH; then
+    echo "password    sufficient    pam_unix.so sha512 shadow try_first_pass use_authtok" >> $CUSTOM_PASSWORD_AUTH
 fi
+
+authselect apply-changes -b
