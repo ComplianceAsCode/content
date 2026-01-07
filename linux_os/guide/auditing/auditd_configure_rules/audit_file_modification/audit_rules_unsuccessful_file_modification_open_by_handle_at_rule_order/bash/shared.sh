64ac7514a0275c20fc2d10161804df58f63793c5
svet-se <svetlin.boychev@suse.com>
2026-01-07 15:58:41 +0200

    Update audit_rules_unsuccessful_file_modification_open_by_handle_at_rule_order to support SLE

--- linux_os/guide/auditing/auditd_configure_rules/audit_file_modification/audit_rules_unsuccessful_file_modification_open_by_handle_at_rule_order/bash/shared.sh
+++ linux_os/guide/auditing/auditd_configure_rules/audit_file_modification/audit_rules_unsuccessful_file_modification_open_by_handle_at_rule_order/bash/shared.sh
@@ -1,3 +1,3 @@
-# platform = multi_platform_rhel,multi_platform_ol,multi_platform_almalinux
+# platform = multi_platform_almalinux,multi_platform_ol,multi_platform_rhel,multi_platform_sle
 
 {{{ bash_create_audit_remediation_unsuccessful_file_modification_detailed("/etc/audit/rules.d/30-ospp-v42-remediation.rules") }}}
