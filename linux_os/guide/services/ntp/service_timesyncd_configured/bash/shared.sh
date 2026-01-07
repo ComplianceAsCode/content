9b56627f2a1aec1341dcd3bb11f4d33cca7905d5
Eric Berry <eric.berry@canonical.com>
2026-01-07 14:40:40 -0800

    Fix incorrect systemd-timesyncd configuration

--- linux_os/guide/services/ntp/service_timesyncd_configured/bash/shared.sh
+++ linux_os/guide/services/ntp/service_timesyncd_configured/bash/shared.sh
@@ -7,16 +7,12 @@
 {{{ bash_instantiate_variables("var_multiple_time_servers") }}}
 IFS=',' read -r -a time_servers_array <<< "$var_multiple_time_servers"
 preferred_ntp_servers_array=("${time_servers_array[@]:0:2}")
-preferred_ntp_servers=$( echo "${preferred_ntp_servers_array[@]}"|sed -e 's/\s\+/,/g' )
+preferred_ntp_servers=$( echo "${preferred_ntp_servers_array[@]}" )
 fallback_ntp_servers_array=("${time_servers_array[@]:2}")
-fallback_ntp_servers=$( echo "${fallback_ntp_servers_array[@]}"|sed -e 's/\s\+/,/g' )
+fallback_ntp_servers=$( echo "${fallback_ntp_servers_array[@]}" )
 
 IFS=" " mapfile -t current_cfg_arr < <(ls -1 /etc/systemd/timesyncd.d/* /etc/systemd/timesyncd.conf.d/* 2>/dev/null)
-{{% if "ubuntu" in product %}}
-config_file="/etc/systemd/timesyncd.conf.d/oscap-remedy.conf"
-{{% else %}}
-config_file="/etc/systemd/timesyncd.d/oscap-remedy.conf"
-{{% endif %}}
+
 current_cfg_arr+=( "/etc/systemd/timesyncd.conf" )
 # Comment existing NTP FallbackNTP settings
 for current_cfg in "${current_cfg_arr[@]}"
@@ -24,20 +20,26 @@ do
     sed -i 's/^NTP/#&/g' "$current_cfg"
     sed -i 's/^FallbackNTP/#&/g' "$current_cfg"
 done
+
+# Set primary fallback NTP servers in drop-in configuration
 {{% if "ubuntu" in product %}}
+# Create /etc/systemd/timesyncd.conf.d if it doesn't exist
 if [ ! -d "/etc/systemd/timesyncd.conf.d" ]
 then 
     mkdir /etc/systemd/timesyncd.conf.d
 fi
+
+{{{ bash_ini_file_set("/etc/systemd/timesyncd.conf.d/oscap-remedy.conf", "Time", "NTP", "$preferred_ntp_servers", rule_id=rule_id) }}}
+{{{ bash_ini_file_set("/etc/systemd/timesyncd.conf.d/oscap-remedy.conf", "Time", "FallbackNTP", "$fallback_ntp_servers", rule_id=rule_id) }}}
 {{% else %}}
 # Create /etc/systemd/timesyncd.d if it doesn't exist
 if [ ! -d "/etc/systemd/timesyncd.d" ]
 then 
     mkdir /etc/systemd/timesyncd.d
 fi
+
+{{{ bash_ini_file_set("/etc/systemd/timesyncd.d/oscap-remedy.conf", "Time", "NTP", "$preferred_ntp_servers", rule_id=rule_id) }}}
+{{{ bash_ini_file_set("/etc/systemd/timesyncd.d/oscap-remedy.conf", "Time", "FallbackNTP", "$fallback_ntp_servers", rule_id=rule_id) }}}
 {{% endif %}}
 
-# Set primary fallback NTP servers in drop-in configuration
-echo "NTP=$preferred_ntp_servers" >> "$config_file"
-echo "FallbackNTP=$fallback_ntp_servers" >> "$config_file"
 
