14fbb6e61f5d3a70ffcae88071f937dae02a74f2
Arden97 <arden2545@gmail.com>
2026-01-12 16:55:15 +0100

    fix accounts_password_pam_unix_enabled

--- linux_os/guide/system/accounts/accounts-pam/accounts_password_pam_unix_enabled/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-pam/accounts_password_pam_unix_enabled/bash/shared.sh
@@ -1,3 +1,7 @@
 # platform = multi_platform_ubuntu
 
-{{{ bash_pam_unix_enable() }}}
+{{% if 'ubuntu' in product or 'debian' in product %}}
+    {{{ bash_pam_unix_enable() }}}
+{{% else %}}
+    {{{ bash_ensure_pam_module_line_unix_enable(["password-auth", "system-auth"], ["auth", "account", "password", "session"]) }}}
+{{% endif %}}
--- linux_os/guide/system/accounts/accounts-pam/accounts_password_pam_unix_enabled/tests/correct.pass.sh
+++ linux_os/guide/system/accounts/accounts-pam/accounts_password_pam_unix_enabled/tests/correct.pass.sh
@@ -2,9 +2,4 @@
 # platform = multi_platform_rhel
 
 # Ensure pam_unix.so is present in all sections of both PAM files
-for pam_file in /etc/pam.d/system-auth /etc/pam.d/password-auth; do
-    {{{ bash_ensure_pam_module_line("$pam_file", "auth", "sufficient", "pam_unix.so") }}}
-    {{{ bash_ensure_pam_module_line("$pam_file", "account", "required", "pam_unix.so") }}}
-    {{{ bash_ensure_pam_module_line("$pam_file", "password", "sufficient", "pam_unix.so") }}}
-    {{{ bash_ensure_pam_module_line("$pam_file", "session", "required", "pam_unix.so") }}}
-done
+{{{ bash_ensure_pam_module_line_unix_enable(["password-auth", "system-auth"], ["auth", "account", "password", "session"]) }}}
