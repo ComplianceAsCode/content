78c8eb543ffebc6b57c02c70ec59c0df44cd969e
Arden97 <arden2545@gmail.com>
2026-01-13 10:05:28 +0100

    fix accounts_passwords_pam_faillock_unlock_time_with_zero

--- linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_passwords_pam_faillock_unlock_time_with_zero/tests/no_unlock_time.fail.sh
+++ linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/accounts_passwords_pam_faillock_unlock_time_with_zero/tests/no_unlock_time.fail.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-echo "unlock_time = 0" > "{{{ pam_faillock_conf_path }}}"
 authselect create-profile test_profile -b sssd
 authselect select "custom/test_profile" --force
 authselect enable-feature with-faillock
