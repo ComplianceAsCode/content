56d5ca7eb7303b0aa4c0b86b53f15daff1e6241f
Eric-Domeier <endome44@protonmail.com>
2026-01-18 17:01:57 -0500

    Add bordencastles authselect and sssd fixes

---
+++ linux_os/guide/system/software/system-tools/package_authselect_installed/tests/package_installed.pass.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# packages = authselect
+
+# authselect package is already installed by packages directive
---
+++ linux_os/guide/system/software/system-tools/package_authselect_installed/tests/package_not_installed.fail.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+dnf remove -y authselect || true
+yum remove -y authselect || true
