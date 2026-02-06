7a919da77c303b8355d84579ac1bf6d5c1d048db
Armando Acosta <armando.acosta@oracle.com>
2026-02-05 21:57:13 -0600

    Align audit_rules_suid_privilege_function rule to DISA OL8 STIG V2R7
    Added “-F euid=0”  filter to audit rule
    
    Signed-off-by: Armando Acosta <armando.acosta@oracle.com>

--- linux_os/guide/auditing/auditd_configure_rules/audit_rules_suid_privilege_function/tests/miss_c.fail.sh
+++ linux_os/guide/auditing/auditd_configure_rules/audit_rules_suid_privilege_function/tests/miss_c.fail.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 # packages = audit
 
-{{% if product not in ["ol8"] %}}
-OTHER_FILTERS_EUID=" -F euid=0"
-OTHER_FILTERS_EGID=" -F egid=0"
-{{% endif %}}
-
 echo "-a always,exit -F arch=b32 -S execve -C gid!=guid${OTHER_FILTERS_EGID} -k setgid" > /etc/audit/rules.d/privileged.rules
 echo "-a always,exit -F arch=b64 -S execve${OTHER_FILTERS_EGID} -k setgid" >> /etc/audit/rules.d/privileged.rules
 echo "-a always,exit -F arch=b32 -S execve${OTHER_FILTERS_EUID} -k setuid" >> /etc/audit/rules.d/privileged.rules
