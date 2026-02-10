e4c164cacc329aab86355a83b37a07d315b34c83
Arden97 <arden2545@gmail.com>
2026-02-10 17:49:29 +0100

    enable oval checks for audit tools on fedora and fix failing tests

--- tests/shared/audit_rules_watch/augenrules_wrong_rule_without_key.fail.sh
+++ tests/shared/audit_rules_watch/augenrules_wrong_rule_without_key.fail.sh
@@ -3,8 +3,10 @@
 
 
 if [[ "$style" == "modern" ]] ; then
+    sed -i "/$filter_type=$(echo "$path" | sed 's/\//\\\//g')/d" /etc/audit/rules.d/*.rules 2>/dev/null || true
     echo "-a always,exit -F arch=b32 -F $filter_type=$path -F perm=w" >> /etc/audit/rules.d/login.rules
     echo "-a always,exit -F arch=b64 -F $filter_type=$path -F perm=w" >> /etc/audit/rules.d/login.rules
 else
+    sed -i "\#-w $path#d" /etc/audit/rules.d/*.rules 2>/dev/null || true
     echo "-w $path -p w" >> /etc/audit/rules.d/login.rules
 fi
