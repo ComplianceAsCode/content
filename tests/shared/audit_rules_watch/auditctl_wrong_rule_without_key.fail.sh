805f03fe770d261e8ebc30f5eeb0b251ff4c9525
Arden97 <arden2545@gmail.com>
2026-02-11 00:15:15 +0100

    audit_rules_watch fail tests sed update

--- tests/shared/audit_rules_watch/auditctl_wrong_rule_without_key.fail.sh
+++ tests/shared/audit_rules_watch/auditctl_wrong_rule_without_key.fail.sh
@@ -2,7 +2,8 @@
 # packages = audit
 
 if [[ "$style" == "modern" ]] ; then
-    sed -i "/$filter_type=$(echo "$path" | sed 's/\//\\\//g')/d" /etc/audit/audit.rules
+    escaped_path=$(echo "$path" | sed 's/\//\\\//g')
+    sed -i "/$filter_type=$escaped_path/d" /etc/audit/audit.rules
     echo "-a always,exit -F arch=b32 -F $filter_type=$path -F perm=w" >> /etc/audit/audit.rules
     echo "-a always,exit -F arch=b64 -F $filter_type=$path -F perm=w" >> /etc/audit/audit.rules
 else
--- tests/shared/audit_rules_watch/augenrules_wrong_rule_without_key.fail.sh
+++ tests/shared/audit_rules_watch/augenrules_wrong_rule_without_key.fail.sh
@@ -3,7 +3,8 @@
 
 
 if [[ "$style" == "modern" ]] ; then
-    sed -i "/$filter_type=$(echo "$path" | sed 's/\//\\\//g')/d" /etc/audit/rules.d/*.rules 2>/dev/null || true
+    escaped_path=$(echo "$path" | sed 's/\//\\\//g')
+    sed -i "/$filter_type=$escaped_path/d" /etc/audit/rules.d/*.rules 2>/dev/null || true
     echo "-a always,exit -F arch=b32 -F $filter_type=$path -F perm=w" >> /etc/audit/rules.d/login.rules
     echo "-a always,exit -F arch=b64 -F $filter_type=$path -F perm=w" >> /etc/audit/rules.d/login.rules
 else
