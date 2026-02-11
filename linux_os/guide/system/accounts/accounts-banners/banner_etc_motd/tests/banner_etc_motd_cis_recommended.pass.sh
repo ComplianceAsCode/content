c01bd8c9372d927e97cbe61602d920d408501860
Jan Černý <jcerny@redhat.com>
2026-02-11 16:41:57 +0100

    Fix a typo
    The correct text is "users".

--- linux_os/guide/system/accounts/accounts-banners/banner_etc_motd/tests/banner_etc_motd_cis_recommended.pass.sh
+++ linux_os/guide/system/accounts/accounts-banners/banner_etc_motd/tests/banner_etc_motd_cis_recommended.pass.sh
@@ -2,4 +2,4 @@
 # profiles = xccdf_org.ssgproject.content_profile_cis, xccdf_org.ssgproject.content_profile_cis_server_l1, xccdf_org.ssgproject.content_profile_cis_workstation_l1, xccdf_org.ssgproject.content_profile_cis_workstation_l2
 
 # cis_default banner
-echo "Authorized uses only. All activity may be monitored and reported." > /etc/motd
+echo "Authorized users only. All activity may be monitored and reported." > /etc/motd
