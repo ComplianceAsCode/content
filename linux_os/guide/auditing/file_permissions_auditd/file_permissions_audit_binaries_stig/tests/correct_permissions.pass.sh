148507652a8612fd6555412b7c044235f3ab869e
Alan Moore <alan.moore@canonical.com>
2026-02-20 14:08:20 +0000

    Add file_permissions_audit_binaries_stig
    Signed-off-by: Alan Moore <alan.moore@canonical.com>

---
+++ linux_os/guide/auditing/file_permissions_auditd/file_permissions_audit_binaries_stig/tests/correct_permissions.pass.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+touch /sbin/audispd-zos-remote
+chmod 0600 /sbin/audispd-zos-remote
---
+++ linux_os/guide/auditing/file_permissions_auditd/file_permissions_audit_binaries_stig/tests/stricter_permisions.pass.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+touch /sbin/audispd-zos-remote
+chmod 0400 /sbin/audispd-zos-remote
