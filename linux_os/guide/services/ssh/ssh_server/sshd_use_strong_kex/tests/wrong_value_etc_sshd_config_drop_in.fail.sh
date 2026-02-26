5d65b420049525dd1cb8b849713625758116bad2
teacup-on-rockingchair <315160+teacup-on-rockingchair@users.noreply.github.com>
2026-02-26 07:59:10 +0200

    Use the new macros in sshd_use_strong_kex rule

---
+++ linux_os/guide/services/ssh/ssh_server/sshd_use_strong_kex/tests/wrong_value_etc_sshd_config_drop_in.fail.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# platform = SUSE Linux Enterprise 16
+# variables = sshd_strong_kex=curve25519-sha256,curve25519-sha256@libssh.org,ecdh-sha2-nistp256
+source include.sh
+
+echo "KexAlgorithms diffie-hellman-group-exchange-sha1" >> /etc/ssh/sshd_config.d/01-complianceascode.conf
