5759ea9b8926dad65152c224001e7ddcf66d9476
Arden97 <arden2545@gmail.com>
2026-02-27 15:39:00 +0100

    clear entries before logind_session_timeout remediation

--- linux_os/guide/system/accounts/accounts-physical/logind_session_timeout/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-physical/logind_session_timeout/bash/shared.sh
@@ -2,6 +2,13 @@
 
 {{{ bash_instantiate_variables("var_logind_session_timeout") }}}
 
+# Remove StopIdleSessionSec from both main config and drop-in files
+{{{ lineinfile_absent("/etc/systemd/logind.conf", "^\s*StopIdleSessionSec\s*=", insensitive=true, rule_id=rule_id) }}}
+
+if [ -d /etc/systemd/logind.conf.d/ ]; then
+{{{ lineinfile_absent_in_directory("/etc/systemd/logind.conf.d", "^\s*StopIdleSessionSec\s*=", insensitive=true, filename_glob="*.conf") | indent(4) }}}
+fi
+
 {{% if product in ["ol9", "ol10", "rhel9", "rhel10", "sle15", "sle16"] %}}
 # create drop-in in the /etc/systemd/logind.conf.d/ directory
 {{% set logind_conf_file = "/etc/systemd/logind.conf.d/oscap-idle-sessions.conf" %}}
