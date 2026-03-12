63f676cfd788f0c90899159fa100f7cf306fd4ed
Miha Purg <miha.purg@canonical.com>
2026-03-12 14:38:46 +0100

    Update dconf controls in Ubuntu 24.04 STIG
    Add and update controls as part of STIG V1R4 update:
    - UBTU-24-200040
    - UBTU-24-200041
    - UBTU-24-200042
    - UBTU-24-200043

--- linux_os/guide/system/software/gnome/gnome_screen_locking/dconf_gnome_screensaver_mode_blank/bash/shared.sh
+++ linux_os/guide/system/software/gnome/gnome_screen_locking/dconf_gnome_screensaver_mode_blank/bash/shared.sh
@@ -1,5 +1,10 @@
 # platform = multi_platform_all
 
+{{% if 'ubuntu' in product %}}
+{{{ bash_enable_dconf_user_profile(profile="user", database="local") }}}
+{{{ bash_enable_dconf_user_profile(profile="gdm", database="gdm") }}}
+{{% endif %}}
+
 {{% if product in ['sle15', 'sle16'] %}}
 {{{ bash_enable_dconf_user_profile(profile="gdm", database="gdm") }}}
 {{{ bash_dconf_settings("org/gnome/desktop/screensaver", "picture-uri", "string ''", dconf_gdm_dir, "00-security-settings", rule_id=rule_id) }}}
