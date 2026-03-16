ef933007c551e2a4e80ec4825d8fd890749b21c2
Azim Azizov <87415292+Azim-Azizov@users.noreply.github.com>
2026-03-16 10:00:24 +0400

    Add support for pre-0.17 database option and support post-0.17 in all remediations (updated tests)

--- linux_os/guide/system/software/integrity/software-integrity/aide/aide_build_database/bash/debian.sh
+++ linux_os/guide/system/software/integrity/software-integrity/aide/aide_build_database/bash/debian.sh
@@ -2,21 +2,21 @@
 
 {{{ bash_package_install("aide") }}}
 
+AIDE_VERSION=$(aide -v | grep -oP 'aide \K[0-9]+\.[0-9]+')
+if [ "$(echo "$AIDE_VERSION >= 0.17" | bc -l)" -eq 1 ]; then
+    AIDE_DB__KEY="database_in"
+else
+    AIDE_DB_IN_KEY="database"
+fi
 AIDE_CONFIG=/etc/aide/aide.conf
 DEFAULT_DB_PATH=/var/lib/aide/aide.db
 
 # Fix db path in the config file, if necessary
-{{% if product == 'debian13' %}}
-if ! grep -q '^database_in=file:' ${AIDE_CONFIG}; then
-    echo "database_in=file:${DEFAULT_DB_PATH}" >> ${AIDE_CONFIG}
-fi
-{{% else %}}
-if ! grep -q '^database=file:' ${AIDE_CONFIG}; then
+if ! grep -q "^${AIDE_DB_IN_KEY}=file:" ${AIDE_CONFIG}; then
     # replace_or_append gets confused by 'database=file' as a key, so should not be used.
     #replace_or_append "${AIDE_CONFIG}" '^database=file' "${DEFAULT_DB_PATH}" '@CCENUM@' '%s:%s'
-    echo "database=file:${DEFAULT_DB_PATH}" >> ${AIDE_CONFIG}
+    echo "${AIDE_DB_IN_KEY}=file:${DEFAULT_DB_PATH}" >> ${AIDE_CONFIG}
 fi
-{{% endif %}}
 
 # Fix db out path in the config file, if necessary
 if ! grep -q '^database_out=file:' ${AIDE_CONFIG}; then
