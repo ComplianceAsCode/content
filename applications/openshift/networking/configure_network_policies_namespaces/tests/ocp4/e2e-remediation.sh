f1ddb523eff45b83e944d5472295a9ff25b40701
taimurhafeez <taimurhafeez93@gmail.com>
2026-03-23 12:07:58 +0000

    reverted back original namespace

--- applications/openshift/networking/configure_network_policies_namespaces/tests/ocp4/e2e-remediation.sh
+++ applications/openshift/networking/configure_network_policies_namespaces/tests/ocp4/e2e-remediation.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+# Create a test namespace to ensure we have at least one non-control-plane namespace
+cat << EOF | oc apply -f -
+---
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: e2e-test
+EOF
+sleep 10
+
 # Deploy a single NetworkPolicy per non control plane namespace
 for NS in $(oc get namespaces -o json | jq -r '.items[] | select((.metadata.name | startswith("openshift") | not) and (.metadata.name | startswith("kube-") | not) and .metadata.name != "default") | .metadata.name'); do
 cat << EOF | oc apply -n "$NS" -f -
