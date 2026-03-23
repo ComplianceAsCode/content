3bcc57a708e2fc3304c6544aea052e594f1c8712
taimurhafeez <taimurhafeez93@gmail.com>
2026-03-23 11:54:04 +0000

    Updates the manual remediation script for the configure-network-policies-namespaces rule to align with the downstream test implementation

--- applications/openshift/networking/configure_network_policies_namespaces/tests/ocp4/e2e-remediation.sh
+++ applications/openshift/networking/configure_network_policies_namespaces/tests/ocp4/e2e-remediation.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-cat << EOF | oc apply -f -
----
-apiVersion: v1
-kind: Namespace
-metadata:
-  name: e2e-test
-EOF
-sleep 10
 # Deploy a single NetworkPolicy per non control plane namespace
 for NS in $(oc get namespaces -o json | jq -r '.items[] | select((.metadata.name | startswith("openshift") | not) and (.metadata.name | startswith("kube-") | not) and .metadata.name != "default") | .metadata.name'); do
 cat << EOF | oc apply -n "$NS" -f -
@@ -14,12 +6,11 @@ cat << EOF | oc apply -n "$NS" -f -
 apiVersion: networking.k8s.io/v1
 kind: NetworkPolicy
 metadata:
-  name: allow-all-ingress
+  name: allow-same-namespace
 spec:
-  podSelector: {}
+  podSelector:
   ingress:
-  - {}
-  policyTypes:
-  - Ingress
+  - from:
+    - podSelector: {}
 EOF
 done
