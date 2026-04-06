0e88997e2f00e68bb5e14fecf6bb784372af4d1d
teacup-on-rockingchair <315160+teacup-on-rockingchair@users.noreply.github.com>
2026-04-06 17:36:41 +0300

    Add tests for the new functionality

--- linux_os/guide/system/accounts/accounts-restrictions/password_expiration/accounts_password_warn_age_login_defs/tests/bad_value.fail.sh
+++ linux_os/guide/system/accounts/accounts-restrictions/password_expiration/accounts_password_warn_age_login_defs/tests/bad_value.fail.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [ -e {{{ login_defs_drop_in_path }}} ] ; then
+    rm {{{ login_defs_drop_in_path }}}
+fi
+
 grep -q "^PASS_WARN_AGE" {{{ login_defs_path }}} && \
   sed -i "s/PASS_WARN_AGE.*/PASS_WARN_AGE\t0/g" {{{ login_defs_path }}}
 if ! [ $? -eq 0 ]; then
--- linux_os/guide/system/accounts/accounts-restrictions/password_expiration/accounts_password_warn_age_login_defs/tests/line_missing.fail.sh
+++ linux_os/guide/system/accounts/accounts-restrictions/password_expiration/accounts_password_warn_age_login_defs/tests/line_missing.fail.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+if [ -e {{{ login_defs_drop_in_path }}} ] ; then
+    rm {{{ login_defs_drop_in_path }}}
+fi
+{{{ bash_copy_distro_defaults(login_defs_defaults_path, login_defs_path) }}}
 sed -i "/PASS_WARN_AGE.*/d" {{{ login_defs_path }}}
