86e988de27af98022066f8e708351f6db40e683d
Alan Moore <alan.moore@canonical.com>
2026-04-08 13:02:09 +0100

    Remove aes192-ctr
    Signed-off-by: Alan Moore <alan.moore@canonical.com>

--- linux_os/guide/services/ssh/ssh_server/sshd_use_approved_ciphers_ordered_stig/tests/common.sh
+++ linux_os/guide/services/ssh/ssh_server/sshd_use_approved_ciphers_ordered_stig/tests/common.sh
@@ -4,15 +4,15 @@
 sshd_approved_ciphers="aes256-gcm@openssh.com,aes128-gcm@openssh.com,aes256-ctr,aes128-ctr"
 sshd_scrambled_ciphers="aes128-gcm@openssh.com,aes256-gcm@openssh.com,aes256-ctr,aes128-ctr"
 {{%- elif product == "ubuntu2204" %}}
-sshd_approved_ciphers="aes256-ctr,aes256-gcm@openssh.com,aes192-ctr,aes128-ctr,aes128-gcm@openssh.com"
-sshd_scrambled_ciphers="aes128-gcm@openssh.com,aes256-ctr,aes256-gcm@openssh.com,aes192-ctr,aes128-ctr"
+sshd_approved_ciphers="aes256-ctr,aes256-gcm@openssh.com,aes128-ctr,aes128-gcm@openssh.com"
+sshd_scrambled_ciphers="aes128-gcm@openssh.com,aes256-ctr,aes256-gcm@openssh.com,aes128-ctr"
 {{%- else %}}
 sshd_approved_ciphers="aes256-ctr,aes192-ctr,aes128-ctr"
 sshd_scrambled_ciphers="aes128-ctr,aes192-ctr,aes256-ctr"
 {{%- endif %}}
 
 for config_file in /etc/ssh/sshd_config /etc/ssh/sshd_config.d/*
-do 
+do
     [[ -f "$config_file" ]] || continue
     sed -i "/^Ciphers.*/Id" "$config_file"
 done
