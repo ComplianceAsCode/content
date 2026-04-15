027d7d8aa99d4a96a25274bd424307db9fda0ed6
teacup-on-rockingchair <315160+teacup-on-rockingchair@users.noreply.github.com>
2026-04-15 05:18:35 +0300

    Make sure that both min and max rounds are set in either defs or drop-in configuration

--- linux_os/guide/system/accounts/accounts-pam/set_password_hashing_algorithm/set_password_hashing_min_rounds_logindefs/tests/correct_values.pass.sh
+++ linux_os/guide/system/accounts/accounts-pam/set_password_hashing_algorithm/set_password_hashing_min_rounds_logindefs/tests/correct_values.pass.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 # variables = var_password_hashing_min_rounds_login_defs=5000
 
+if [ -e {{{ login_defs_drop_in_path }}} ] ; then
+    rm {{{ login_defs_drop_in_path }}}
+fi
+
 echo "SHA_CRYPT_MIN_ROUNDS 5000" > {{{ login_defs_path }}}
 echo "SHA_CRYPT_MAX_ROUNDS 5000" >> {{{ login_defs_path }}}
