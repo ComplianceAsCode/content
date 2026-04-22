10bbf9eec58c125accf00e4e9a806b907af2b9e4
Vojtech Polasek <vpolasek@redhat.com>
2026-04-22 09:27:47 +0200

    account_password_pam_faillock_password_auth: remove redundant metadata from test scenario

--- linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/account_password_pam_faillock_password_auth/tests/correct_value_with_features.pass.sh
+++ linux_os/guide/system/accounts/accounts-pam/locking_out_password_attempts/account_password_pam_faillock_password_auth/tests/correct_value_with_features.pass.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # packages = authselect,pam
 # platform = Oracle Linux 8,Oracle Linux 9,multi_platform_rhel,multi_platform_fedora
-# remediation = ansible
 
 authselect create-profile test_profile -b sssd
 authselect select "custom/test_profile" --force
