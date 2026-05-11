815b6c9661deb05bdc70da58bc58746cff135a59
Jan Černý <jcerny@redhat.com>
2026-05-11 11:01:56 +0200

    Remove unreliable test scenarios
    This commit removes 3 test scenarios.  These scenarios create a
    violating file in `/tmp`.  However, these rules should skip remote and
    special file system.  That creates a problem that the evaluation result
    depends on whether `/tmp` on the target system is a partition on the
    disk or a `tmpfs` file system.
    
    If the `/tmp` is a partition on the disk, OpenSCAP examines the files in
    the `/tmp` directory, the violating file in `/tmp` is detected and the
    rule fails as expected by the test scenarios.
    
    If the `/tmp` is a `tmpfs` file system, OpenSCAP considers it a special
    file system, it skips it, doesn't examine the `/tmp` directory,
    therefore it doesn't detect the violating file in `/tmp` and rule result
    is pass, which isn't expected by test scenarios.
    
    In general, we don't want test scenarios with variable behavior.  Also,
    we don't have a way to control the expected test scenario result based
    on the target system properties. Therefore, we will remove the test
    scenarios.
    
    Fixes: https://github.com/ComplianceAsCode/content/issues/13778

--- linux_os/guide/system/permissions/files/no_files_unowned_by_user/tests/unowned_file_tmp.fail.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# platform = multi_platform_fedora,multi_platform_rhel,multi_platform_ubuntu
-# remediation = none
-
-mount tmpfs /tmp -t tmpfs
-
-touch /tmp/test
-chown 9999:9999 /tmp/test
