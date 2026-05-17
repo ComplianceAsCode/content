771f554e0fbcb73e55cf0d5466296b87c2d64ac9
teacup-on-rockingchair <315160+teacup-on-rockingchair@users.noreply.github.com>
2026-05-17 13:16:57 +0300

    Fixed tests to match latest behaviour in pam_options
    - Fix tests for use_pam_wheel_group_for_su and set_password_hashing_algorithm_commonauth
    - Add test for accounts_password_pam_pwhistory_remember

--- linux_os/guide/system/accounts/accounts-pam/set_password_hashing_algorithm/set_password_hashing_algorithm_commonauth/tests/common-auth.pam_unix_not_sha512.fail.sh
+++ linux_os/guide/system/accounts/accounts-pam/set_password_hashing_algorithm/set_password_hashing_algorithm_commonauth/tests/common-auth.pam_unix_not_sha512.fail.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# platform = SUSE Linux Enterprise 15
+# platform = SUSE Linux Enterprise 15, SUSE Linux Enterprise 16
 
-echo "auth	required 	pam_unix.so	try_first_pass" > /etc/pam.d/common-auth
+echo "auth	sufficient 	pam_unix.so	try_first_pass" > /etc/pam.d/common-auth
--- linux_os/guide/system/accounts/accounts-restrictions/root_logins/use_pam_wheel_group_for_su/tests/line_not_there.fail.sh
+++ linux_os/guide/system/accounts/accounts-restrictions/root_logins/use_pam_wheel_group_for_su/tests/line_not_there.fail.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 # variables = var_pam_wheel_group_for_su=sugroup
 
+{{% if product in ["sle16", "slmicro6"] %}}
+touch /etc/pam.d/su
+{{% endif %}}
+
 #clean possible lines
 sed -Ei '/^auth\b.*\brequired\b.*\bpam_wheel\.so/d' /etc/pam.d/su
