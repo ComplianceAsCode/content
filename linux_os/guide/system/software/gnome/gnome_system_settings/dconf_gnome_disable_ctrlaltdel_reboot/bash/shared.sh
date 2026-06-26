f47a133ef00a07f868430c6023a20e74c0d9992f
Alan Moore <alan.moore@canonical.com>
2026-06-26 12:29:48 +0100

    change to GVariant format
    Signed-off-by: Alan Moore <alan.moore@canonical.com>

--- linux_os/guide/system/software/gnome/gnome_system_settings/dconf_gnome_disable_ctrlaltdel_reboot/bash/shared.sh
+++ linux_os/guide/system/software/gnome/gnome_system_settings/dconf_gnome_disable_ctrlaltdel_reboot/bash/shared.sh
@@ -5,5 +5,9 @@
 {{{ bash_enable_dconf_user_profile(profile="gdm", database="gdm") }}}
 {{% endif %}}
 
+{{% if 'ubuntu' in product %}}
+{{{ bash_dconf_settings("org/gnome/settings-daemon/plugins/media-keys", "logout", "@as []", "local.d", "00-security-settings", rule_id=rule_id) }}}
+{{%- else %}}
 {{{ bash_dconf_settings("org/gnome/settings-daemon/plugins/media-keys", "logout", "['']", "local.d", "00-security-settings", rule_id=rule_id) }}}
+{{%- endif %}}
 {{{ bash_dconf_lock("org/gnome/settings-daemon/plugins/media-keys", "logout", "local.d", "00-security-settings-lock") }}}
