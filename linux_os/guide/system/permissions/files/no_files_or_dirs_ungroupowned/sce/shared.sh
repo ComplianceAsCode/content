c0627894ea50b031b88b839a89319e4e496af9f6
Gabriel Becker <ggasparb@redhat.com>
2026-07-10 11:55:05 +0200

    Fix SCE check for no_files_or_dirs_ungroupowned to also check directories
    The OVAL check verifies both files and directories with no owning group
    (using separate file_object entries for each type), but the SCE was using
    find_files() which only searches for files (-type f).
    
    Fix by switching to find_command() with -type f -o -type d so that both
    files and directories without a group owner are detected.
    
    Fixes: https://github.com/ComplianceAsCode/content/issues/14843

--- linux_os/guide/system/permissions/files/no_files_or_dirs_ungroupowned/sce/shared.sh
+++ linux_os/guide/system/permissions/files/no_files_or_dirs_ungroupowned/sce/shared.sh
@@ -2,4 +2,4 @@
 # platform = multi_platform_fedora,multi_platform_rhel,Ubuntu 24.04
 # check-import = stdout
 
-{{{ find_files(find_parameters="-nogroup", fail_message="Found ungroupowned files or directories", exclude_directories="sysroot") }}}
+{{{ find_command(find_parameters="-nogroup", fail_message="Found ungroupowned files or directories", exclude_directories="sysroot", find_type="\( -type f -o -type d \)") }}}
