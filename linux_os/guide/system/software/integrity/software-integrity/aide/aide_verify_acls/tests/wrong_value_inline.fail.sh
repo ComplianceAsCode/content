a79567a0ccc7b9d86f9f39761aba275f10d9f789
Jan Černý <jcerny@redhat.com>
2026-07-14 14:37:30 +0200

    Check selection lines with direct attributes in aide.conf
    AIDE config files can contain selection lines that specify attributes
    directly (e.g., /bin p+i+n+u+g) instead of referencing a group. These
    entries were not checked for the xattrs attribute. Extend the OVAL check,
    bash and ansible remediations, and test scenarios to cover this case in
    rules aide_verify_ext_attributes and aide_verify_acls.

---
+++ linux_os/guide/system/software/integrity/software-integrity/aide/aide_verify_acls/tests/wrong_value_inline.fail.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# packages = aide
+# platform = Red Hat Virtualization 4,multi_platform_ol,multi_platform_rhel,multi_platform_almalinux
+
+
+cat >/etc/aide.conf <<EOL
+All = p+i+n+u+g+s+m+S+sha512+acl+xattrs+selinux
+option = yes
+/bin p+i+n+u+g+s+m+S+sha512+xattrs+selinux
+/sbin p+i+n+u+g+s+m+S+sha512+xattrs+selinux
+EOL
