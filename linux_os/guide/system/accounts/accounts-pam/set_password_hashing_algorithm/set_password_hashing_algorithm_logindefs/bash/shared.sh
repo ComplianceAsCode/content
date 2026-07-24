e5bc843ecc55850936403ed6361fb729f59b7173
teacup-on-rockingchair <315160+teacup-on-rockingchair@users.noreply.github.com>
2026-07-24 08:17:58 +0300

    Enable SLE15 support for login.defs drop-in configuration
    Adapted rules
    - set_password_hashing_algorithm_logindefs
    - set_password_hashing_min_rounds_logindefs
    - accounts_password_warn_age_login_defs

--- linux_os/guide/system/accounts/accounts-pam/set_password_hashing_algorithm/set_password_hashing_algorithm_logindefs/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-pam/set_password_hashing_algorithm/set_password_hashing_algorithm_logindefs/bash/shared.sh
@@ -6,7 +6,7 @@
 #
 var_password_hashing_algorithm="$(echo $var_password_hashing_algorithm | cut -d \| -f 1)"
 
-{{% if product in [ 'slmicro6', 'sle16' ] %}}
+{{% if product in [ 'slmicro6', 'sle15', 'sle16' ] %}}
 {{{ bash_login_defs("ENCRYPT_METHOD", "$var_password_hashing_algorithm", cce_identifiers=cce_identifiers) }}}
 {{% else %}}
 {{{ bash_replace_or_append(login_defs_path, '^ENCRYPT_METHOD', "$var_password_hashing_algorithm", '%s %s', cce_identifiers=cce_identifiers) }}}
