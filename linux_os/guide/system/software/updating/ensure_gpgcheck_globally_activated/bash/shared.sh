71548a2340ee8a0bb733838b5b189c159cc811be
teacup-on-rockingchair <315160+teacup-on-rockingchair@users.noreply.github.com>
2026-08-08 20:06:29 +0300

    Simplify logic on copy distro default zypp config
    Do not create /etc/zypp dir, if it does not exist the installation is broken

--- linux_os/guide/system/software/updating/ensure_gpgcheck_globally_activated/bash/shared.sh
+++ linux_os/guide/system/software/updating/ensure_gpgcheck_globally_activated/bash/shared.sh
@@ -1,7 +1,6 @@
 # platform = multi_platform_rhel,multi_platform_ol,multi_platform_fedora,multi_platform_rhv,multi_platform_sle,multi_platform_slmicro,multi_platform_almalinux
 
 {{% if product in ['sle16', 'slmicro6'] %}}
-    mkdir -p "$(dirname '{{{ pkg_manager_config_file }}}')"
     {{{ bash_copy_distro_defaults("/usr/etc/zypp/zypp.conf", pkg_manager_config_file) }}}
 {{% endif %}}
 
