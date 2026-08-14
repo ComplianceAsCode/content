92a71955fef085adab06d6d8886394da5cb97378
Jan Černý <jcerny@redhat.com>
2026-08-14 13:55:05 +0200

    Create new rule audit_rules_cron_execution
    Create new rule `audit_rules_cron_execution` that aligns with RHEL 9
    STIG requirement RHEL-09-654097 The requirement was updated in RHEL 9
    STIG V2R9. The previous versions of STIG watched for modifications of
    `/etc/cron.d` `/var/spool/cron`. The V2R9 requires to watch any system
    call made by cron and requires to use different audit rules than the
    previous versions.

---
+++ linux_os/guide/auditing/auditd_configure_rules/audit_rules_cron_execution/tests/correct_value.pass.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# packages = audit
+# platform = multi_platform_rhel
+
+echo "-a always,exit -F arch=b64 -S execve -F subj_type=crond_t -F euid=0 -k cron_exec" >> /etc/audit/rules.d/cron_exec.rules
+echo "-a always,exit -F arch=b32 -S execve -F subj_type=crond_t -F euid=0 -k cron_exec" >> /etc/audit/rules.d/cron_exec.rules
+echo "-a always,exit -F arch=b64 -S execve -F subj_type=crond_t -F auid>=1000 -F auid!=unset -k cron_exec" >> /etc/audit/rules.d/cron_exec.rules
+echo "-a always,exit -F arch=b32 -S execve -F subj_type=crond_t -F auid>=1000 -F auid!=unset -k cron_exec" >> /etc/audit/rules.d/cron_exec.rules
