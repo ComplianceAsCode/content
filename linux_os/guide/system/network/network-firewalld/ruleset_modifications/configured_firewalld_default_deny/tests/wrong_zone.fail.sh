6998838e42e8ff680f24d895a17a777acde66c7b
pradeepkch6-ai <chilukurypradeepkumar@gmail.com>
2026-08-21 01:16:50 +0500

    Fix test scripts printing to STDERR on missing config file, which crashed testing-farm

--- linux_os/guide/system/network/network-firewalld/ruleset_modifications/configured_firewalld_default_deny/tests/wrong_zone.fail.sh
+++ linux_os/guide/system/network/network-firewalld/ruleset_modifications/configured_firewalld_default_deny/tests/wrong_zone.fail.sh
@@ -3,6 +3,7 @@
 # platform = multi_platform_all
 
 mkdir -p /etc/firewalld
+touch /etc/firewalld/firewalld.conf
 if grep -q "^DefaultZone=" /etc/firewalld/firewalld.conf; then
     sed -i 's/^DefaultZone=.*/DefaultZone=public/' /etc/firewalld/firewalld.conf
 else
