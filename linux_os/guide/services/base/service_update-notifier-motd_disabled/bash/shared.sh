42e79b325c6d627fe5e6f22fe5d9e40275eb21d9
Thomas Moore <moore.thomas123456789@gmail.com>
2026-09-05 15:31:16 +1200

    Complete Ubuntu 26.04 package and service remediations

--- linux_os/guide/services/base/service_update-notifier-motd_disabled/bash/shared.sh
+++ linux_os/guide/services/base/service_update-notifier-motd_disabled/bash/shared.sh
@@ -5,9 +5,12 @@
 # disruption = low
 
 SYSTEMCTL_EXEC=/usr/bin/systemctl
-if [[ $("$SYSTEMCTL_EXEC" is-system-running) != "offline" ]]; then
-    "$SYSTEMCTL_EXEC" stop update-notifier-motd.service
+unit=update-notifier-motd.service
+if "$SYSTEMCTL_EXEC" list-unit-files "$unit" --no-legend 2>/dev/null | grep -q "^${unit}[[:space:]]"; then
+    if [[ $("$SYSTEMCTL_EXEC" is-system-running) != "offline" ]]; then
+        "$SYSTEMCTL_EXEC" stop "$unit"
+    fi
+    "$SYSTEMCTL_EXEC" disable "$unit"
+    "$SYSTEMCTL_EXEC" mask "$unit"
+    "$SYSTEMCTL_EXEC" reset-failed "$unit" || true
 fi
-"$SYSTEMCTL_EXEC" disable update-notifier-motd.service
-"$SYSTEMCTL_EXEC" mask update-notifier-motd.service
-"$SYSTEMCTL_EXEC" reset-failed update-notifier-motd.service || true
