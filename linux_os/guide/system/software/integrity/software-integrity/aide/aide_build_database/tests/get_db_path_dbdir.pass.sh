6845504691804b67de30370434d31bee5f7253d8
Thomas Moore <moore.thomas123456789@gmail.com>
2026-09-08 14:39:47 +1200

    Fix cross-platform AIDE tests and YAML lint

--- linux_os/guide/system/software/integrity/software-integrity/aide/aide_build_database/tests/get_db_path_dbdir.pass.sh
+++ linux_os/guide/system/software/integrity/software-integrity/aide/aide_build_database/tests/get_db_path_dbdir.pass.sh
@@ -4,10 +4,14 @@
 aide --init
 DB_PATH=/var/lib/aide
 
-{{% if 'ubuntu' in product or 'sle' in product or 'slmicro' in product %}}
+{{% if 'ubuntu' in product or 'debian' in product %}}
 DB_NAME=aide.db
 DB_NAME_NEW=aide.db.new
 DB_CONF=/etc/aide/aide.conf
+{{% elif 'sle' in product or 'slmicro' in product %}}
+DB_NAME=aide.db
+DB_NAME_NEW=aide.db.new
+DB_CONF=/etc/aide.conf
 {{% else %}}
 DB_NAME=aide.db.gz
 DB_NAME_NEW=aide.db.new.gz
@@ -16,7 +20,7 @@ DB_CONF=/etc/aide.conf
 
 cp "$DB_PATH/$DB_NAME_NEW" "$DB_PATH/$DB_NAME"
 
-{{% if product in [ 'ol10', 'rhel10', 'fedora', 'ubuntu2604' ] %}}
+{{% if product in [ 'ol10', 'rhel10', 'fedora', 'ubuntu2604', 'debian13' ] %}}
 sed -i "s#^database_in=file:.*#database_in=file:@@{DBDIR}/$DB_NAME#" $DB_CONF
 {{% else %}}
 sed -i "s#^database=file:.*#database=file:@@{DBDIR}/$DB_NAME#" $DB_CONF
