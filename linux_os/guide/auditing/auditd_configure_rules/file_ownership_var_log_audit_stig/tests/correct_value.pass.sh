dc3f5aa2bb8b0666a50f29fe27b9ec5fff5e5f83
Alan Moore <alan.moore@canonical.com>
2026-09-10 16:57:38 +0100

    Enable test scripts for ubuntu
    Signed-off-by: Alan Moore <alan.moore@canonical.com>

--- linux_os/guide/auditing/auditd_configure_rules/file_ownership_var_log_audit_stig/tests/correct_value.pass.sh
+++ linux_os/guide/auditing/auditd_configure_rules/file_ownership_var_log_audit_stig/tests/correct_value.pass.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# platform = multi_platform_ol,multi_platform_rhel,multi_platform_almalinux,multi_platform_ubuntu
 # packages = audit
 
 sed -i "/^\s*log_file.*/d" /etc/audit/auditd.conf
