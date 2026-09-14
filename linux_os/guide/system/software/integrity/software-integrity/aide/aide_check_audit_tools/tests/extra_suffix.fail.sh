14256eb9fb7e8fa3fb2ace1477d497b94925d4da
teacup-on-rockingchair <315160+teacup-on-rockingchair@users.noreply.github.com>
2026-09-14 15:08:25 +0300

    xattrs is removed for sle16/opensuse16 since AIDE does not support it, aide_verify_ext_attributes rule is disabled also

--- linux_os/guide/system/software/integrity/software-integrity/aide/aide_check_audit_tools/tests/extra_suffix.fail.sh
+++ linux_os/guide/system/software/integrity/software-integrity/aide/aide_check_audit_tools/tests/extra_suffix.fail.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# platform = multi_platform_fedora,multi_platform_ol,multi_platform_rhel,multi_platform_ubuntu,multi_platform_almalinux
+# platform = multi_platform_almalinux,multi_platform_fedora,multi_platform_ol,multi_platform_rhel,multi_platform_sle,multi_platform_ubuntu
 # packages = aide
 
 declare -a bins
@@ -11,5 +11,9 @@ bins=(
 
 for theFile in "${bins[@]}"
 do
+    {{% if product in ['sle16'] %}}
+    echo "$theFile p+i+n+u+g+s+b+acl+selinux+sha5122" >> {{{ aide_conf_path }}}
+    {{% else %}}
     echo "$theFile p+i+n+u+g+s+b+acl+selinux+xattrs+sha5122" >> {{{ aide_conf_path }}}
+    {{% endif %}}
 done
