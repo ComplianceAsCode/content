f84340602224cfc62217d5675287d8a26d0706f8
teacup-on-rockingchair <315160+teacup-on-rockingchair@users.noreply.github.com>
2026-09-20 19:02:05 +0300

    Fix packages field syntax

--- linux_os/guide/system/software/sudo/sudoers_validate_passwd/tests/sudoers_validate_targetpw.fail.sh
+++ linux_os/guide/system/software/sudo/sudoers_validate_passwd/tests/sudoers_validate_targetpw.fail.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # platform = SUSE Linux Enterprise 15,multi_platform_almalinux,multi_platform_fedora,multi_platform_ol,multi_platform_opensuse,multi_platform_rhel,multi_platform_slmicro
-# # packages = sudo
+# packages = sudo
 
 touch /etc/sudoers.d/empty
 if [ $(grep -Ei '(!targetpw)' /etc/sudoers /etc/sudoers.d/* | grep -v '#' | wc -l) -ne 0 ]
